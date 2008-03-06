8528fb7c58d0a5cdd833de5a969e796de084d73f
Stefane Fermigier <sfermigier@nuxeo.com>
2008-03-06 10:45:16 +0100

    Whitespace.

--- src/main/java/org/nuxeo/common/collections/ListenerList.java
+++ src/main/java/org/nuxeo/common/collections/ListenerList.java
@@ -113,7 +113,7 @@ public class ListenerList {
         System.arraycopy(listeners, 0, newListeners, 0, oldSize);
         newListeners[oldSize] = listener;
         if (comparator != null) {
-        	Arrays.sort(newListeners, (Comparator<Object>)comparator);
+            Arrays.sort(newListeners, comparator);
         }
         //atomic assignment
         listeners = newListeners;
