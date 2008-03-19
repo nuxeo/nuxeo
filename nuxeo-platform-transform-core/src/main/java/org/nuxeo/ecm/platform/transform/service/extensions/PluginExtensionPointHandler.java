91e5f9c3388457fdc489d6ca39b7f8a269090e8e
Bogdan Stefanescu <bs@nuxeo.com>
2008-03-19 14:47:16 +0100

    added explicit cast

--- nuxeo-platform-transform-core/src/main/java/org/nuxeo/ecm/platform/transform/service/extensions/PluginExtensionPointHandler.java
+++ nuxeo-platform-transform-core/src/main/java/org/nuxeo/ecm/platform/transform/service/extensions/PluginExtensionPointHandler.java
@@ -98,7 +98,7 @@ public class PluginExtensionPointHandler extends
             log.warn("Plugin class not specified for plugin extension: " + pluginExtension);
             return;
         }
-        Class<Plugin> pluginClass = extension.getContext().loadClass(className);
+        Class<Plugin> pluginClass = (Class<Plugin>)extension.getContext().loadClass(className);
         if (null == pluginClass) {
             throw new TransformException("Unable to load plugin class: '" + className + "'");
         }
