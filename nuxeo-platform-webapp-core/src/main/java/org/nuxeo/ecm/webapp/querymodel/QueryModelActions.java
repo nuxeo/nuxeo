5915a4f70e5c2bdd53c0330c53af9818955a1e3f
Georges Racinet <gr@nuxeo.com>
2008-04-08 18:01:11 +0200

    NXP-2232: API for QueryModel persistance

--- nuxeo-platform-webapp-core/src/main/java/org/nuxeo/ecm/webapp/querymodel/QueryModelActions.java
+++ nuxeo-platform-webapp-core/src/main/java/org/nuxeo/ecm/webapp/querymodel/QueryModelActions.java
@@ -19,6 +19,8 @@
 
 package org.nuxeo.ecm.webapp.querymodel;
 
+import org.nuxeo.ecm.core.api.DocumentModel;
+import org.nuxeo.ecm.core.api.DocumentRef;
 import org.nuxeo.ecm.core.api.ClientException;
 import org.nuxeo.ecm.core.search.api.client.querymodel.QueryModel;
 import org.nuxeo.ecm.platform.ui.web.api.ResultsProviderFarm;
@@ -53,6 +55,78 @@ public interface QueryModelActions extends ResultsProviderFarm {
      */
     QueryModel get(String queryModelName) throws ClientException;
 
+    /**
+     * Save the specified <strong>stateful</strong> QueryModel.
+     * <p>This is equivalent to the other signature, with saveSession set to True</p>
+     *
+     * @since 5.2
+     * @param queryModelName
+     * @param parentPath The path of parent folder to save into
+     * @param name The local name to use.
+     * @return The saved query model.
+     * @throws ClientException forwarding from the Core, stateless QueryModel,
+     *         if QueryModel has already been persisted
+     */
+    QueryModel persist(String queryModelName, String parentPath, String name) throws ClientException;
+
+    /**
+     * Save the specified <strong>stateful</strong> QueryModel.
+     *
+     * <p>The DocumentModel instance that backs the QueryModel is saved
+     * in the CoreSession available in Seam's context. Necessary updates
+     * are performed. The returned QueryModel instance is identical to the
+     * one obtained by a subsequent call to {@see get}. </p>
+     * <p>Further document operations, e.g., modifications, for this QueryModel
+     * can be done on the DocumentModel instance directly, but CoreSession methods
+     * returning a new DocumentModel instance must be followed by a call to
+     * {@see load} and dependent objects (page providers, etc.) must be updated
+     * as well</p>
+     * <p>It is not possible to call again this method on the same QueryModel, to
+     * avoid consistency problems at the DocumentModel level. Therefore
+     * If one wants to save a QueryModel, then change and eventually save it to
+     * a different target in Nuxeo Core, one <strong>must</strong> call the
+     * {@see reset} method before performing the changes.</p>
+     *
+     * @since 5.2
+     * @param queryModelName
+     * @param parentPath The path of parent folder to save into
+     * @param name The local name to use.
+     * @param saveSession if true, the Core Session is saved
+     * @return The saved query model.
+     * @throws ClientException forwarding from the Core, stateless QueryModel,
+     *         if QueryModel has already been persisted
+     */
+    QueryModel persist(String queryModelName, String parentPath, String name, boolean saveSession) throws ClientException;
+
+    /**
+     * Load a stateful QueryModel from the specified DocumentRef.
+     * <p>The queryModelChanged event is thrown, meaning in particular that
+     * {@link ResultsProviderCache} invalidation is performed.</p>
+     * @since 5.2
+     * @param queryModelName
+     * @param ref
+     * @return The loaded QueryModel instance.
+     * @throws ClientException if qm is not stateful, document could not be retrieved.
+     */
+    QueryModel load(String queryModelName, DocumentRef ref) throws ClientException;
+
+    /**
+     * Tell if the DocumentModel behind the specified QueryModel has been
+     * persisted in Nuxeo Core.
+     * @since 5.2
+     * @param queryModelName The query model name
+     */
+    boolean isPersisted(String queryModelName) throws ClientException;
+
+    /**
+     * Reset the specified QueryModel.
+     * <p>Start over with a fresh,
+     * transient DocumentModel instance.
+     * </p>
+     *
+     * @param queryModelName
+     * @throws ClientException
+     */
     void reset(String queryModelName) throws ClientException;
 
     /**
