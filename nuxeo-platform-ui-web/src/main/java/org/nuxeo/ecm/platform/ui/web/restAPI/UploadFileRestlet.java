87c0a88d9fe93faf7766e05752ecfa2704609be4
Olivier Grisel <ogrisel@nuxeo.com>
2008-04-11 17:39:32 +0200

    NXP-2276: actual server side fix for the empty live edit document bug (reset blob after mimetype sniffing)

--- nuxeo-platform-ui-web/src/main/java/org/nuxeo/ecm/platform/ui/web/restAPI/UploadFileRestlet.java
+++ nuxeo-platform-ui-web/src/main/java/org/nuxeo/ecm/platform/ui/web/restAPI/UploadFileRestlet.java
@@ -24,6 +24,8 @@ import static org.jboss.seam.ScopeType.EVENT;
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
 
+import org.apache.myfaces.custom.fileupload.UploadedFileConverter;
+import org.apache.myfaces.custom.fileupload.UploadedFileDefaultFileImpl;
 import org.jboss.seam.annotations.In;
 import org.jboss.seam.annotations.Name;
 import org.jboss.seam.annotations.Scope;
@@ -70,7 +72,6 @@ public class UploadFileRestlet extends BaseNuxeoRestlet implements
             return;
         }
 
-
         if (repo == null || repo.equals("*")) {
             handleError(res, "you must specify a repository");
             return;
@@ -98,19 +99,29 @@ public class UploadFileRestlet extends BaseNuxeoRestlet implements
                 DEFAULT_FILENAME_FIELD);
 
         try {
+            // the stream can be read only once, thus save it first into the
+            // core before attempting to detect the mimetype that might rely on
+            // binary sniffing
             Blob blob = StreamingBlob.createFromStream(req.getEntity().getStream());
+            dm.setProperty(schemaName, blobFieldName, blob);
+            dm.setProperty(schemaName, filenameFieldName, fileName);
+            dm = documentManager.saveDocument(dm);
 
             // ask the mimetype service for the blob mimetype first according to
-            // filename extension with fallback to binary sniffing
+            // filename extension with a fallback to binary sniffing
+            blob = (Blob) dm.getProperty(schemaName, blobFieldName);
             MimetypeRegistry mimeService = Framework.getService(MimetypeRegistry.class);
             String mimetype = mimeService.getMimetypeFromFilenameAndBlobWithDefault(
                     fileName, blob, "application/octet-stream");
             blob.setMimeType(mimetype);
 
-            // save the properties on the document model
-            dm.setProperty(schemaName, blobFieldName, blob);
-            dm.setProperty(schemaName, filenameFieldName, fileName);
+            // reset the blob source before saving back since it might have
+            // exhausted by the binary sniffing of the MimetypeRegistry service
+            // (NB: LazyBlobs fetched from the Core are always resetable)
+            blob.getStream().reset();
 
+            // re-save the blob with the detected mimetype
+            dm.setProperty(schemaName, blobFieldName, blob);
             documentManager.saveDocument(dm);
             documentManager.save();
 
