f494e9aaf3bef0be9301ea3b3df861293b5af508
Florent Guillaume <fg@nuxeo.com>
2008-05-04 22:10:06 +0200

    NXP-2367: cleanup unit test deployment

--- nuxeo-core-schema/src/test/java/org/nuxeo/ecm/core/schema/TestSchemaLoader.java
+++ nuxeo-core-schema/src/test/java/org/nuxeo/ecm/core/schema/TestSchemaLoader.java
@@ -47,7 +47,7 @@ public class TestSchemaLoader extends NXRuntimeTestCase {
     @Override
     protected void setUp() throws Exception {
         super.setUp();
-        deployContrib("nuxeo-core-schema", "OSGI-INF/SchemaService.xml");
+        deployContrib("org.nuxeo.ecm.core.schema", "OSGI-INF/SchemaService.xml");
         typeMgr = getTypeManager();
         reader = new XSDLoader(typeMgr);
     }
