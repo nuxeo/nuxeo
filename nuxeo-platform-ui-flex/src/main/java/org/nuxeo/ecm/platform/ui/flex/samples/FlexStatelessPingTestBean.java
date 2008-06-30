b50d2e03e14b4e7f5856e64b15f99219ce2a1e2d
Thierry Delprat <td@nuxeo.com>
2008-06-30 02:30:09 +0200

    add flex integration module

---
+++ nuxeo-platform-ui-flex/src/main/java/org/nuxeo/ecm/platform/ui/flex/samples/FlexStatelessPingTestBean.java
@@ -0,0 +1,40 @@
+package org.nuxeo.ecm.platform.ui.flex.samples;
+
+import java.io.Serializable;
+
+import org.jboss.seam.ScopeType;
+import org.jboss.seam.annotations.In;
+import org.jboss.seam.annotations.Name;
+import org.jboss.seam.annotations.Scope;
+import org.jboss.seam.annotations.remoting.WebRemote;
+import org.nuxeo.ecm.core.api.NuxeoPrincipal;
+
+@Name("flexStatelessPingTestBean")
+@Scope(ScopeType.STATELESS)
+public class FlexStatelessPingTestBean implements Serializable {
+
+    /**
+     *
+     */
+
+    @In(create=false,required=false)
+    NuxeoPrincipal flexUser;
+
+    private static final long serialVersionUID = 1L;
+
+    @WebRemote
+    public String ping() {
+        return "Hello from stateless Nuxeo Seam Bean";
+    }
+
+
+    @WebRemote
+    public String pingUser() {
+
+        if (flexUser==null)
+            return "Stateless Nuxeo Seam bean saye Hello to null user";
+        else
+            return "Stateless Nuxeo Seam bean saye Hello to " + flexUser.getName();
+    }
+
+}
