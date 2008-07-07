a2ccb9c732a9662a9a71180c18f84bf8979c1d38
Anahide Tchertchian <at@nuxeo.com>
2008-07-07 18:02:07 +0200

    revert, wrong branch

--- nuxeo-theme-html/src/main/resources/nxthemes/html/scripts/seam-remote.js
+++ nuxeo-theme-html/src/main/resources/nxthemes/html/scripts/seam-remote.js
@@ -588,7 +588,7 @@ Seam.Remoting.sendAjaxRequest = function(envelope, path, callback, silent)
     asyncReq = new ActiveXObject("Microsoft.XMLHTTP");
 
   asyncReq.onreadystatechange = function() {Seam.Remoting.requestCallback(asyncReq, callback); }
-  asyncReq.open("POST", Seam.Remoting.contextPath + "/seam/resource/remoting" + path, true);
+  asyncReq.open("POST", Seam.Remoting.contextPath + "/seam/remoting" + path, true);
   asyncReq.send(envelope);
   return asyncReq;
 }
