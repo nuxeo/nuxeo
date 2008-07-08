ef2ce8f958c5afcef84d1c99751fa613d3d02f31
Bogdan Stefanescu <bs@nuxeo.com>
2008-07-08 17:03:52 +0200

    fixed launcher scripts

--- nuxeo-webengine-server/src/main/resources/nxserver/nxserver.cmd
+++ nuxeo-webengine-server/src/main/resources/nxserver/nxserver.cmd
@@ -1,6 +1,6 @@
 @echo off
 
-set JAVA_OPTS=-Djava.rmi.server.RMIClassLoaderSpi=org.nuxeo.runtime.loader.NuxeoRMIClassLoader -Dsun.lang.ClassLoader.allowArraySyntax=true
+set JAVA_OPTS=-Djava.rmi.server.RMIClassLoaderSpi=org.nuxeo.runtime.launcher.NuxeoRMIClassLoader -Dsun.lang.ClassLoader.allowArraySyntax=true
 set JAVA_OPTS=%JAVA_OPTS% -Dderby.system.home=data/derby
 
 if "%1" == "-debug" set JAVA_OPTS=%JAVA_OPTS% -Xdebug -Xrunjdwp:transport=dt_socket,address=127.0.0.1:8788,server=y,suspend=y
