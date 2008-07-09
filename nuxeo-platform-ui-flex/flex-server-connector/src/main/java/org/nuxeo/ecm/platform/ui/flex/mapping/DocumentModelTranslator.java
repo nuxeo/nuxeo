795abaf1a2ff87d22e2e5732907dc7b676354cdc
Thierry Delprat <td@nuxeo.com>
2008-07-09 19:05:52 +0200

    working on login system

--- nuxeo-platform-ui-flex/flex-server-connector/src/main/java/org/nuxeo/ecm/platform/ui/flex/mapping/DocumentModelTranslator.java
+++ nuxeo-platform-ui-flex/flex-server-connector/src/main/java/org/nuxeo/ecm/platform/ui/flex/mapping/DocumentModelTranslator.java
@@ -9,6 +9,7 @@ import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 import org.nuxeo.common.utils.Path;
+import org.nuxeo.ecm.core.api.Blob;
 import org.nuxeo.ecm.core.api.CoreSession;
 import org.nuxeo.ecm.core.api.DocumentModel;
 import org.nuxeo.ecm.core.api.DocumentRef;
@@ -28,70 +29,65 @@ import org.nuxeo.runtime.api.Framework;
 public class DocumentModelTranslator {
 
     private static SchemaManager sm;
-    private static Map<String,String> schemaCache = new ConcurrentHashMap<String, String>();
 
+    private static Map<String, String> schemaCache = new ConcurrentHashMap<String, String>();
 
-    private static FlexDocumentModel addIconURL(FlexDocumentModel fdm,  DocumentModel doc)
-    {
+    private static FlexDocumentModel addIconURL(FlexDocumentModel fdm,
+            DocumentModel doc) {
         String iconURL = DocumentModelFunctions.iconPath(doc);
         fdm.setProperty("common", "icon", iconURL);
         return fdm;
     }
 
-    private static String getSchemaFromPrefix(String prefix) throws Exception
-    {
+    private static String getSchemaFromPrefix(String prefix) throws Exception {
 
-        String schemaName=schemaCache.get(prefix);
+        String schemaName = schemaCache.get(prefix);
 
-        if (schemaName!=null)
+        if (schemaName != null)
             return schemaName;
 
-        if (sm==null)
+        if (sm == null)
             sm = Framework.getService(SchemaManager.class);
 
         Schema schema = sm.getSchemaFromPrefix(prefix);
 
         schemaName = schema.getSchemaName();
 
-        if (schemaName==null)
-        {
-            schemaName=prefix;
+        if (schemaName == null) {
+            schemaName = prefix;
         }
         schemaCache.put(prefix, schemaName);
         return schemaName;
     }
 
-    public static FlexDocumentModel toFlexTypeFromPrefetch(DocumentModel doc) throws Exception
-    {
-        FlexDocumentModel fdm = new FlexDocumentModel(doc.getRef(),doc.getName(),doc.getPathAsString(), doc.getCurrentLifeCycleState(), doc.getType());
+    public static FlexDocumentModel toFlexTypeFromPrefetch(DocumentModel doc)
+            throws Exception {
+        FlexDocumentModel fdm = new FlexDocumentModel(doc.getRef(),
+                doc.getName(), doc.getPathAsString(),
+                doc.getCurrentLifeCycleState(), doc.getType());
 
         fdm.setIsFolder(doc.isFolder());
 
-        Map<String,Serializable> prefetch = doc.getPrefetch();
+        Map<String, Serializable> prefetch = doc.getPrefetch();
         String[] schemas = doc.getDeclaredSchemas();
 
-        for (int i=0; i<schemas.length; i++)
-        {
-            Map<String,Serializable> map = new HashMap<String, Serializable>();
+        for (int i = 0; i < schemas.length; i++) {
+            Map<String, Serializable> map = new HashMap<String, Serializable>();
 
             fdm.feed(schemas[i], map);
         }
 
-        for (String prefetchKey : prefetch.keySet())
-        {
+        for (String prefetchKey : prefetch.keySet()) {
             String schemaName;
             String fieldName;
 
-            if (prefetchKey.contains(":"))
-            {
-                schemaName=prefetchKey.split(":")[0];
-                fieldName=prefetchKey.split(":")[1];
+            if (prefetchKey.contains(":")) {
+                schemaName = prefetchKey.split(":")[0];
+                fieldName = prefetchKey.split(":")[1];
                 schemaName = getSchemaFromPrefix(schemaName);
-            }
-            else
-            {
-                schemaName=prefetchKey.split("\\.")[0];
-                fieldName=prefetchKey.split("\\.")[1];
+            } else {
+                schemaName = prefetchKey.split("\\.")[0];
+                fieldName = prefetchKey.split("\\.")[1];
             }
 
             fdm.setProperty(schemaName, fieldName, prefetch.get(prefetchKey));
@@ -99,57 +95,64 @@ public class DocumentModelTranslator {
         return fdm;
     }
 
-    public static FlexDocumentModel toFlexType(DocumentModel doc) throws Exception
-    {
+    public static FlexDocumentModel toFlexType(DocumentModel doc)
+            throws Exception {
 
-
-        FlexDocumentModel fdm = new FlexDocumentModel(doc.getRef(),doc.getName(),doc.getPathAsString(), doc.getCurrentLifeCycleState(), doc.getType());
+        FlexDocumentModel fdm = new FlexDocumentModel(doc.getRef(),
+                doc.getName(), doc.getPathAsString(),
+                doc.getCurrentLifeCycleState(), doc.getType());
 
         fdm.setIsFolder(doc.isFolder());
 
         DocumentPart[] parts = doc.getParts();
 
-        for (int i=0;i<parts.length;i++)
-        {
-            Map<String,Serializable> map = new HashMap<String, Serializable>();
+        for (int i = 0; i < parts.length; i++) {
+            Map<String, Serializable> map = new HashMap<String, Serializable>();
             Collection<Property> props = parts[i].getChildren();
 
-            String schemaPrefix=parts[i].getSchema().getNamespace().prefix;
-            if (schemaPrefix=="")
-                schemaPrefix=parts[i].getSchema().getName();
+            String schemaPrefix = parts[i].getSchema().getNamespace().prefix;
+            if (schemaPrefix == "")
+                schemaPrefix = parts[i].getSchema().getName();
 
-            for (Property prop : props)
-            {
+            for (Property prop : props) {
                 String fieldName = prop.getName();
-                fieldName=fieldName.replace(schemaPrefix+":", "");
+                fieldName = fieldName.replace(schemaPrefix + ":", "");
 
-                if (prop.getType().isSimpleType())
-                {
+                if (prop.getType().isSimpleType()) {
                     map.put(fieldName, prop.getValue());
-                }
-                else if (prop.getType().isComplexType())
-                {
+                } else if (prop.getType().isComplexType()) {
                     if (prop instanceof BlobProperty) {
-                        String dwURL = DocumentModelFunctions.fileUrl("downloadFile", doc, prop.getName(), null);
+
                         BlobProperty blobProp = (BlobProperty) prop;
-                        map.put(fieldName,dwURL);
-                    }
-                    else if (prop instanceof MapProperty) {
+                        Blob blob = (Blob) blobProp.getValue();
+                        if (blob != null) {
+                            String fileName = blob.getFilename();
+                            if (fileName==null)
+                            {
+                                // XXX Hack !
+                                if (parts[i].getSchema().getName().equals("file"))
+                                {
+                                    fileName=(String) doc.getProperty("file", "filename");
+                                }
+                            }
+                            String dwURL = DocumentModelFunctions.fileUrl(
+                                    "downloadFile", doc, prop.getName(),
+                                    fileName);
+                            map.put(fieldName, dwURL);
+                        }
+                    } else if (prop instanceof MapProperty) {
                         MapProperty mapProp = (MapProperty) prop;
-                        map.put(fieldName,mapProp.getValue());
+                        map.put(fieldName, mapProp.getValue());
                     }
-                }
-                else if (prop.getType().isListType())
-                {
+                } else if (prop.getType().isListType()) {
                     if (prop instanceof ListProperty) {
                         ListProperty listProp = (ListProperty) prop;
                         List<Serializable> lstProp = new ArrayList<Serializable>();
                         lstProp.addAll(listProp.getChildren());
-                        map.put(fieldName,(Serializable)lstProp);
-                    }
-                    else if (prop instanceof ArrayProperty) {
+                        map.put(fieldName, (Serializable) lstProp);
+                    } else if (prop instanceof ArrayProperty) {
                         ArrayProperty arrayProp = (ArrayProperty) prop;
-                        map.put(fieldName,arrayProp.getValue());
+                        map.put(fieldName, arrayProp.getValue());
                     }
                 }
             }
@@ -158,30 +161,26 @@ public class DocumentModelTranslator {
         return fdm;
     }
 
-    public static DocumentModel toDocumentModel(FlexDocumentModel fdoc, CoreSession session) throws Exception
-    {
+    public static DocumentModel toDocumentModel(FlexDocumentModel fdoc,
+            CoreSession session) throws Exception {
 
         String refAsString = fdoc.getDocRef();
-        DocumentModel doc=null;
-        if (refAsString==null || "".equals(refAsString))
-        {
+        DocumentModel doc = null;
+        if (refAsString == null || "".equals(refAsString)) {
             String docType = fdoc.getType();
-            String name=fdoc.getName();
+            String name = fdoc.getName();
             String docPath = fdoc.getPath();
             String parentPath = new Path(docPath).removeLastSegments(1).toString();
-            doc=session.createDocumentModel(parentPath, name, docType);
+            doc = session.createDocumentModel(parentPath, name, docType);
             doc = session.createDocument(doc);
-        }
-        else
-        {
+        } else {
             DocumentRef docRef = new IdRef(refAsString);
             doc = session.getDocument(docRef);
         }
 
         Map<String, Serializable> dirtyFields = fdoc.getDirtyFields();
 
-        for (String path : dirtyFields.keySet())
-        {
+        for (String path : dirtyFields.keySet()) {
             doc.setPropertyValue(path, dirtyFields.get(path));
         }
 
--- nuxeo-platform-ui-flex/samples/flex-login/server/src/main/java/org/nuxeo/ecm/platform/ui/flex/auth/FlexLoginServlet.java
+++ nuxeo-platform-ui-flex/samples/flex-login/server/src/main/java/org/nuxeo/ecm/platform/ui/flex/auth/FlexLoginServlet.java
@@ -1,11 +1,14 @@
 package org.nuxeo.ecm.platform.ui.flex.auth;
 
 import java.io.IOException;
+import java.security.Principal;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.nuxeo.ecm.core.api.NuxeoPrincipal;
 
 public class FlexLoginServlet extends HttpServlet {
 
@@ -33,8 +36,32 @@ public class FlexLoginServlet extends HttpServlet {
     private void sendLoginResponse(HttpServletRequest req,HttpServletResponse resp) throws IOException
     {
 
+        Principal principal = req.getUserPrincipal();
+        NuxeoPrincipal nuxeoPrincipal = (NuxeoPrincipal) principal;
+
         resp.setContentType("test/xml");
-        String xmlResponse="<?xml version=\"1.0\" encoding=\"UTF-8\"?><response><status>OK</status></response>";
-        resp.getWriter().write(xmlResponse);
+
+        StringBuffer sb = new StringBuffer();
+
+        sb.append("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
+        sb.append("<response>");
+        sb.append("<status>OK</status>");
+        sb.append("<user>");
+        sb.append("<firstName>" + nuxeoPrincipal.getFirstName() + "</firstName>");
+        sb.append("<lastName>" + nuxeoPrincipal.getLastName() + "</lastName>");
+        sb.append("<login>" + nuxeoPrincipal.getPrincipalId() + "</login>");
+        sb.append("<name>" + nuxeoPrincipal.getName() + "</name>");
+        sb.append("<company>" + nuxeoPrincipal.getCompany() + "</company>");
+        sb.append("<groups>");
+        for (String group : nuxeoPrincipal.getAllGroups())
+        {
+        sb.append("<group> " + group + "</group>");
+        }
+        sb.append("</groups>");
+
+        sb.append("</user>");
+        sb.append("</response>");
+
+        resp.getWriter().write(sb.toString());
     }
 }
