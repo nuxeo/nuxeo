6e509fb709a209712499072b4f1973112b8399f2
Anahide Tchertchian <at@nuxeo.com>
2008-07-31 18:50:53 +0200

    NXP-2570: don't try to write a null text to the response writer, sun ri does not allow it

--- nuxeo-platform-ui-web/src/main/java/org/nuxeo/ecm/platform/ui/web/directory/DirectoryEntryOutputRenderer.java
+++ nuxeo-platform-ui-web/src/main/java/org/nuxeo/ecm/platform/ui/web/directory/DirectoryEntryOutputRenderer.java
@@ -19,7 +19,6 @@
 
 package org.nuxeo.ecm.platform.ui.web.directory;
 
-import java.io.IOException;
 import java.util.Locale;
 
 import javax.faces.component.UIComponent;
@@ -51,18 +50,17 @@ public class DirectoryEntryOutputRenderer extends Renderer {
             entryId = dirComponent.getEntryId();
         }
         String directoryName = dirComponent.getDirectoryName();
-        ResponseWriter writer = context.getResponseWriter();
         String toWrite = null;
         if (directoryName != null) {
-                // get the entry information
+            // get the entry information
             String keySeparator = (String) dirComponent.getAttributes().get(
                     "keySeparator");
             if (keySeparator != null) {
-                entryId = entryId
-                        .substring(entryId.lastIndexOf(keySeparator) + 1,
-                                entryId.length());
+                entryId = entryId.substring(
+                        entryId.lastIndexOf(keySeparator) + 1, entryId.length());
             }
-            DocumentModel entry = DirectoryHelper.getEntry(directoryName, entryId);
+            DocumentModel entry = DirectoryHelper.getEntry(directoryName,
+                    entryId);
 
             if (entry != null) {
                 Boolean displayIdAndLabel = dirComponent.getDisplayIdAndLabel();
@@ -82,9 +80,9 @@ public class DirectoryEntryOutputRenderer extends Renderer {
                 if (Boolean.TRUE.equals(translate)) {
                     label = translate(context, label);
                 }
-                toWrite = display != null ? DirectoryHelper.instance()
-                        .getOptionValue(entryId, label, display,
-                                displayIdAndLabel, " ") : label;
+                toWrite = display != null ? DirectoryHelper.instance().getOptionValue(
+                        entryId, label, display, displayIdAndLabel, " ")
+                        : label;
 
             }
         }
@@ -93,9 +91,12 @@ public class DirectoryEntryOutputRenderer extends Renderer {
             toWrite = entryId;
         }
         try {
-            writer.writeText(toWrite, null);
-            writer.flush();
-        } catch (IOException e) {
+            if (toWrite != null) {
+                ResponseWriter writer = context.getResponseWriter();
+                writer.writeText(toWrite, null);
+                writer.flush();
+            }
+        } catch (Exception e) {
             log.error("IOException trying to write on the response", e);
         }
     }
