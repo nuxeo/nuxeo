d265c2e96d442a72dd8efa75a13deebd25c04dcc
Bogdan Stefanescu <bs@nuxeo.com>
2008-08-06 09:30:41 +0200

    added option to lazy start nxrelations since webengine startup is really slow

--- nuxeo-platform-relations-core/src/main/java/org/nuxeo/ecm/platform/relations/services/RelationService.java
+++ nuxeo-platform-relations-core/src/main/java/org/nuxeo/ecm/platform/relations/services/RelationService.java
@@ -42,6 +42,7 @@ import org.nuxeo.ecm.platform.relations.api.Statement;
 import org.nuxeo.ecm.platform.relations.descriptors.GraphDescriptor;
 import org.nuxeo.ecm.platform.relations.descriptors.GraphTypeDescriptor;
 import org.nuxeo.ecm.platform.relations.descriptors.ResourceAdapterDescriptor;
+import org.nuxeo.runtime.api.Framework;
 import org.nuxeo.runtime.model.ComponentContext;
 import org.nuxeo.runtime.model.ComponentInstance;
 import org.nuxeo.runtime.model.ComponentName;
@@ -473,11 +474,15 @@ public class RelationService extends DefaultComponent implements
 
     @Override
     public void activate(ComponentContext context) throws Exception {
-        context.getRuntimeContext().getBundle().getBundleContext().addFrameworkListener(this);
+        if (Boolean.parseBoolean(
+                Framework.getProperty("org.nuxeo.ecm.platform.relations.initOnStartup", "true"))) {
+            context.getRuntimeContext().getBundle().getBundleContext().addFrameworkListener(this);
+        }
     }
 
     @Override
     public void deactivate(ComponentContext context) throws Exception {
+        // this is doing nothing if listener was not registered
         context.getRuntimeContext().getBundle().getBundleContext().removeFrameworkListener(this);
     }
 
