d44df2e9d7a5b42c8232f0b7bbcd7cf71b5e1266
Anahide Tchertchian <at@nuxeo.com>
2008-08-12 12:22:22 +0200

    NXP-2593: fix repository initialization reverting bogdan's changes

--- nuxeo-core/src/main/java/org/nuxeo/ecm/core/repository/RepositoryInitializationHandler.java
+++ nuxeo-core/src/main/java/org/nuxeo/ecm/core/repository/RepositoryInitializationHandler.java
@@ -19,8 +19,6 @@
 
 package org.nuxeo.ecm.core.repository;
 
-import java.util.LinkedList;
-
 import org.nuxeo.ecm.core.api.ClientException;
 import org.nuxeo.ecm.core.api.CoreSession;
 
@@ -73,65 +71,11 @@ public abstract class RepositoryInitializationHandler {
         return instance;
     }
 
-    /**
-     * The parent handler if any otherwise null
-     */
-    protected RepositoryInitializationHandler previous;
-    /**
-     * The next handler in the chain if any or null otherwise
-     */
-    protected RepositoryInitializationHandler next;
-
-    public abstract void doInitializeRepository(CoreSession session) throws ClientException;
-
-    /**
-     * Must be implemented by custom initializers.
-     *
-     * @param session the current session
-     * @throws ClientException
-     */
-    public void initializeRepository(CoreSession session) throws ClientException {
-        synchronized (RepositoryInitializationHandler.class) {
-            if (previous != null) {
-                previous.initializeRepository(session);
-            }
-            doInitializeRepository(session);
-        }
+    public static void setInstance(RepositoryInitializationHandler handler) {
+        instance = handler;
     }
 
-
-    /**
-     * @param parent the parent to set.
-     */
-    public void install() {
-        synchronized (RepositoryInitializationHandler.class) {
-            this.previous = RepositoryInitializationHandler.instance;
-            if (this.previous != null) {
-                this.previous.next = this;
-            }
-        }
-    }
-
-    public void uninstall() {
-        synchronized (RepositoryInitializationHandler.class) {
-            if (previous != null) {
-                this.previous.next = next;
-                if (next != null) {
-                    next.previous = this.previous;
-                }
-            }
-        }
-    }
-
-    /**
-     * @return the parent.
-     */
-    public RepositoryInitializationHandler getPrevious() {
-        return previous;
-    }
-
-    public RepositoryInitializationHandler getNext() {
-        return next;
-    }
+    public abstract void initializeRepository(CoreSession session)
+            throws ClientException;
 
 }
