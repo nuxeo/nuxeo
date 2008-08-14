a6276b6be29bee5cc19719ba8396cf68d7c84f77
Sun Seng David TAN <stan@nuxeo.com>
2008-08-14 15:21:25 +0200

    Temporary removing post default action view, while finding a better fix

--- nuxeo-webengine-core/src/main/java/org/nuxeo/ecm/webengine/DefaultRequestHandler.java
+++ nuxeo-webengine-core/src/main/java/org/nuxeo/ecm/webengine/DefaultRequestHandler.java
@@ -50,7 +50,7 @@ public class DefaultRequestHandler implements RequestHandler, Actions {
 
     public void doPost(WebObject object) throws WebException {
         // assuming UPDATE / DELETE / VIEW actions from the request method is buggy
-        doAction(object, VIEW);
+        doAction(object, null);
     }
 
     public void doPut(WebObject object) throws WebException {
