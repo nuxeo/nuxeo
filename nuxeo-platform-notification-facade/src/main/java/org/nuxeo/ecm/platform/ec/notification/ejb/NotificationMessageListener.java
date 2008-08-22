76d8d5e8ee9083ac1bde0982fe60dcb814dfec6b
Anahide Tchertchian <at@nuxeo.com>
2008-08-22 17:44:09 +0200

    NXP-2606: Remove harcoded view in url of documents in notifications

--- nuxeo-platform-notification-facade/src/main/java/org/nuxeo/ecm/platform/ec/notification/ejb/NotificationMessageListener.java
+++ nuxeo-platform-notification-facade/src/main/java/org/nuxeo/ecm/platform/ec/notification/ejb/NotificationMessageListener.java
@@ -52,6 +52,7 @@ import org.nuxeo.ecm.platform.ec.notification.service.NotificationServiceHelper;
 import org.nuxeo.ecm.platform.events.api.DocumentMessage;
 import org.nuxeo.ecm.platform.events.api.JMSConstant;
 import org.nuxeo.ecm.platform.notification.api.Notification;
+import org.nuxeo.ecm.platform.types.adapter.TypeInfo;
 import org.nuxeo.ecm.platform.url.DocumentLocationImpl;
 import org.nuxeo.ecm.platform.url.DocumentViewImpl;
 import org.nuxeo.ecm.platform.url.api.DocumentLocation;
@@ -328,8 +329,8 @@ public class NotificationMessageListener implements MessageListener {
         if (!documentHasBeenDeleted(message)) {
             DocumentLocation docLoc = new DocumentLocationImpl(
                     message.getRepositoryName(), message.getRef());
-            DocumentView docView = new DocumentViewImpl(docLoc);
-            docView.setViewId("view_documents");
+            TypeInfo typeInfo = message.getAdapter(TypeInfo.class);
+            DocumentView docView = new DocumentViewImpl(docLoc, typeInfo.getDefaultView());
             eventInfo.put(
                     "docUrl",
                     getDocLocator().getUrlFromDocumentView(
