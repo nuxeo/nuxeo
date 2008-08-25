c739414711fe71ce179edd11cbab5b38c599b428
M.-A. Darche <madarche@nuxeo.com>
2008-08-25 15:32:52 +0200

    Fixed method API description.

--- nuxeo-platform-ui-web/src/main/java/org/nuxeo/ecm/platform/ui/web/auth/interfaces/NuxeoAuthenticationPlugin.java
+++ nuxeo-platform-ui-web/src/main/java/org/nuxeo/ecm/platform/ui/web/auth/interfaces/NuxeoAuthenticationPlugin.java
@@ -1,5 +1,5 @@
 /*
- * (C) Copyright 2006-2007 Nuxeo SAS (http://nuxeo.com/) and contributors.
+ * (C) Copyright 2006-2008 Nuxeo SAS (http://nuxeo.com/) and contributors.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the GNU Lesser General Public License
@@ -41,7 +41,7 @@ public interface NuxeoAuthenticationPlugin {
             HttpServletResponse httpResponse, String baseURL);
 
     /**
-     * Retries user indentification information from the request.
+     * Retrieves user indentification information from the request.
      *
      * @param httpRequest
      * @param httpResponse
