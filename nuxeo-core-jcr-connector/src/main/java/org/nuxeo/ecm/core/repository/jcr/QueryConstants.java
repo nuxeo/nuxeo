b68542a1a29c50f274785691dbec6dffabc811de
Stefane Fermigier <sfermigier@nuxeo.com>
2008-09-07 17:45:44 +0200

    Remove annotation that confuses NCSS.

--- nuxeo-core-jcr-connector/src/main/java/org/nuxeo/ecm/core/repository/jcr/QueryConstants.java
+++ nuxeo-core-jcr-connector/src/main/java/org/nuxeo/ecm/core/repository/jcr/QueryConstants.java
@@ -38,7 +38,6 @@ public interface QueryConstants {
      *  only for compatibility
      *  @deprecated use {@link #ECM_VERSION}
      */
-    @Deprecated
     static final String ECM_IS_CHECKED_IN_VERSION = "ecm:isCheckedInVersion";
 
 }
