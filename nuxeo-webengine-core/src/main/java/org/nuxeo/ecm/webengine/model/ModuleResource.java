337fd778f3bbc5edc2e793cddb4e531fb601e26d
Stefane Fermigier <sfermigier@nuxeo.com>
2008-10-12 13:32:36 +0200

    Cleanup.

--- nuxeo-webengine-core/src/main/java/org/nuxeo/ecm/webengine/model/ModuleResource.java
+++ nuxeo-webengine-core/src/main/java/org/nuxeo/ecm/webengine/model/ModuleResource.java
@@ -2,14 +2,14 @@
  * (C) Copyright 2006-2008 Nuxeo SAS (http://nuxeo.com/) and contributors.
  *
  * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the GNU Lesser General Public License
+ * are made available under the terms of the GNU Lesser General License
  * (LGPL) version 2.1 which accompanies this distribution, and is available at
  * http://www.gnu.org/licenses/lgpl.html
  *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Lesser General Public License for more details.
+ * Lesser General License for more details.
  *
  * Contributors:
  *     bstefanescu
@@ -30,30 +30,30 @@ import org.nuxeo.ecm.webengine.scripting.ScriptFile;
  * @author <a href="mailto:bs@nuxeo.com">Bogdan Stefanescu</a>
  *
  */
-public interface ModuleResource extends Resource{
+public interface ModuleResource extends Resource {
 
-    public String getName();
+    String getName();
 
-    public Object getErrorView(WebApplicationException e);
+    Object getErrorView(WebApplicationException e);
 
-    public ScriptFile getFile(String path) throws WebException;
+    ScriptFile getFile(String path) throws WebException;
 
-    public Class<?> loadClass(String className) throws ClassNotFoundException;
+    Class<?> loadClass(String className) throws ClassNotFoundException;
 
-    public ResourceType getType(String typeName) throws TypeNotFoundException;
+    ResourceType getType(String typeName) throws TypeNotFoundException;
 
-    public ResourceType[] getTypes();
+    ResourceType[] getTypes();
 
-    public ServiceType[] getServices();
+    ServiceType[] getServices();
 
-    public ServiceType getService(Resource ctx, String name) throws ServiceNotFoundException;
+    ServiceType getService(Resource ctx, String name) throws ServiceNotFoundException;
 
-    public List<ServiceType> getServices(Resource ctx);
+    List<ServiceType> getServices(Resource ctx);
 
-    public List<String> getServiceNames(Resource ctx);
+    List<String> getServiceNames(Resource ctx);
 
-    public List<ServiceType> getEnabledServices(Resource ctx);
+    List<ServiceType> getEnabledServices(Resource ctx);
 
-    public List<String> getEnabledServiceNames(Resource ctx);
+    List<String> getEnabledServiceNames(Resource ctx);
 
 }
