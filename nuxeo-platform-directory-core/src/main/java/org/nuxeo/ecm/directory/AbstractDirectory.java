b21e3b2e0ee0d62af49689da743fa73f1580207e
Anahide Tchertchian <at@nuxeo.com>
2008-11-03 15:07:36 +0100

    NXP-2796: use default document model comparator to sort directory entries

--- nuxeo-platform-directory-core/src/main/java/org/nuxeo/ecm/directory/AbstractDirectory.java
+++ nuxeo-platform-directory-core/src/main/java/org/nuxeo/ecm/directory/AbstractDirectory.java
@@ -19,19 +19,15 @@
 
 package org.nuxeo.ecm.directory;
 
-import java.io.Serializable;
-import java.text.Collator;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
 
 import org.nuxeo.ecm.core.api.ClientException;
-import org.nuxeo.ecm.core.api.DataModel;
 import org.nuxeo.ecm.core.api.DocumentModel;
+import org.nuxeo.ecm.core.api.DocumentModelComparator;
 
 public abstract class AbstractDirectory implements Directory {
 
@@ -43,12 +39,13 @@ public abstract class AbstractDirectory implements Directory {
     protected final DirectoryCache cache = new DirectoryCache();
 
     /**
-     * Invalidate my cache and the caches of linked directories by references
-     * @throws ClientException
+     * Invalidate my cache and the caches of linked directories by references.
+     *
+     * @throws DirectoryException
      */
     public void invalidateCaches() throws DirectoryException {
         cache.invalidateAll();
-        for (Reference ref: references.values()) {
+        for (Reference ref : references.values()) {
             ref.getTargetDirectory().getCache().invalidateAll();
         }
     }
@@ -92,72 +89,8 @@ public abstract class AbstractDirectory implements Directory {
      */
     public void orderEntries(List<DocumentModel> entries,
             Map<String, String> orderBy) throws DirectoryException {
-        Collections.sort(entries, new EntryComparator(getSchema(), orderBy));
-    }
-
-    public static final String ORDER_ASC = "asc";
-
-    /**
-     * DocumentModel comparator. Uses ordering independent of case or accent. If
-     * two values are integers/longs, numbering comparison is used.
-     */
-    public static class EntryComparator implements Comparator<DocumentModel>,
-            Serializable {
-
-        private static final long serialVersionUID = -5027588251188034085L;
-
-        static final Collator collator = Collator.getInstance();
-
-        static {
-            collator.setStrength(Collator.PRIMARY); // case+accent independent
-        }
-
-        final String schemaName;
-
-        final Map<String, String> orderBy;
-
-        EntryComparator(String schemaName, Map<String, String> orderBy) {
-            this.schemaName = schemaName;
-            this.orderBy = orderBy;
-        }
-
-        public int compare(DocumentModel e1, DocumentModel e2) {
-            final DataModel d1 = e1.getDataModel(schemaName);
-            final DataModel d2 = e2.getDataModel(schemaName);
-            for (Entry<String, String> e : orderBy.entrySet()) {
-                final String fieldName = e.getKey();
-                final boolean asc = ORDER_ASC.equals(e.getValue());
-                final Object v1 = d1.getData(fieldName);
-                final Object v2 = d2.getData(fieldName);
-                if (v1 == null && v2 == null) {
-                    continue;
-                } else if (v1 == null) {
-                    return asc ? -1 : 1;
-                } else if (v2 == null) {
-                    return asc ? 1 : -1;
-                }
-                final int cmp;
-                if (v1 instanceof Long && v2 instanceof Long) {
-                    cmp = ((Long) v1).compareTo((Long) v2);
-                } else if (v1 instanceof Integer && v2 instanceof Integer) {
-                    cmp = ((Integer) v1).compareTo((Integer) v2);
-                } else {
-                    cmp = collator.compare(v1.toString(), v2.toString());
-                }
-                if (cmp == 0) {
-                    continue;
-                }
-                return asc ? cmp : -cmp;
-            }
-            // everything being equal, provide consistent ordering
-            if (e1.hashCode() == e2.hashCode()) {
-                return 0;
-            } else if (e1.hashCode() < e2.hashCode()) {
-                return -1;
-            } else {
-                return 1;
-            }
-        }
+        Collections.sort(entries, new DocumentModelComparator(getSchema(),
+                orderBy));
     }
 
     public DirectoryCache getCache() {
