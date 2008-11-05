9d431b3aec97409395b365615bc9b83d20f8ebab
Florent Guillaume <fg@nuxeo.com>
2008-11-05 15:58:30 +0100

    fix tests for NXP-2808 (ran badly under maven)

--- nuxeo-core-facade/src/test/java/org/nuxeo/ecm/core/api/TestLocalAPI.java
+++ nuxeo-core-facade/src/test/java/org/nuxeo/ecm/core/api/TestLocalAPI.java
@@ -40,6 +40,8 @@ import org.nuxeo.ecm.core.api.security.ACE;
 import org.nuxeo.ecm.core.api.security.ACL;
 import org.nuxeo.ecm.core.api.security.ACP;
 import org.nuxeo.ecm.core.api.security.SecurityConstants;
+import org.nuxeo.ecm.core.api.security.impl.ACLImpl;
+import org.nuxeo.ecm.core.api.security.impl.ACPImpl;
 import org.nuxeo.runtime.RuntimeService;
 
 /**
@@ -583,6 +585,63 @@ public class TestLocalAPI extends TestAPI {
         // make it visible for others
         remote.save();
         return doc.getRef();
+    }
+
+    // see identical test in TestSQLRepositoryVersioning
+
+    public void testVersionSecurity() throws Exception {
+        CoreSession session = remote;
+        DocumentModel folder = new DocumentModelImpl("/", "folder", "Folder");
+        folder = session.createDocument(folder);
+        ACP acp = new ACPImpl();
+        ACE ace = new ACE("princ1", "perm1", true);
+        ACL acl = new ACLImpl("acl1", false);
+        acl.add(ace);
+        acp.addACL(acl);
+        session.setACP(folder.getRef(), acp, true);
+        DocumentModel file = new DocumentModelImpl("/folder", "file", "File");
+        file = session.createDocument(file);
+        // set security
+        acp = new ACPImpl();
+        ace = new ACE("princ2", "perm2", true);
+        acl = new ACLImpl("acl2", false);
+        acl.add(ace);
+        acp.addACL(acl);
+        session.setACP(file.getRef(), acp, true);
+        session.save();
+        VersionModel vm = new VersionModelImpl();
+        vm.setLabel("v1");
+        session.checkIn(file.getRef(), vm);
+        session.checkOut(file.getRef());
+
+        // check security on version
+        DocumentModel version = session.getDocumentWithVersion(file.getRef(), vm);
+        acp = session.getACP(version.getRef());
+        ACL[] acls = acp.getACLs();
+        if (this.getClass().getName().equals(TestLocalAPI.class.getName())) {
+            // JCR versioning (unused) does something incorrect here
+            return;
+        }
+        // the following is only run with TestLocalAPIWithCustomVersioning
+        assertEquals(2, acls.length);
+        acl = acls[0];
+        assertEquals(1, acl.size());
+        assertEquals("princ2", acl.get(0).getUsername());
+        acl = acls[1];
+        assertEquals(1 + 4, acl.size()); // 1 + 4 root defaults
+        assertEquals("princ1", acl.get(0).getUsername());
+
+        // remove live document (create a proxy so the version stays)
+        session.createProxy(folder.getRef(), file.getRef(), vm, true);
+        session.save();
+        session.removeDocument(file.getRef());
+        // recheck security on version
+        try {
+            session.getACP(version.getRef());
+            fail();
+        } catch (DocumentSecurityException e) {
+            // ok
+        }
     }
 
 }
--- nuxeo-core-facade/src/test/java/org/nuxeo/ecm/core/api/TestLocalAPIWithCustomVersioning.java
+++ nuxeo-core-facade/src/test/java/org/nuxeo/ecm/core/api/TestLocalAPIWithCustomVersioning.java
@@ -17,14 +17,6 @@
 
 package org.nuxeo.ecm.core.api;
 
-import org.nuxeo.ecm.core.api.impl.DocumentModelImpl;
-import org.nuxeo.ecm.core.api.impl.VersionModelImpl;
-import org.nuxeo.ecm.core.api.security.ACE;
-import org.nuxeo.ecm.core.api.security.ACL;
-import org.nuxeo.ecm.core.api.security.ACP;
-import org.nuxeo.ecm.core.api.security.impl.ACLImpl;
-import org.nuxeo.ecm.core.api.security.impl.ACPImpl;
-
 /**
  * @author Florent Guillaume
  */
@@ -35,56 +27,6 @@ public class TestLocalAPIWithCustomVersioning extends TestLocalAPI {
         super.doDeployments();
         deployContrib(CoreFacadeTestConstants.CORE_FACADE_TESTS_BUNDLE,
                 "CustomVersioningService.xml");
-    }
-
-    public void testVersionSecurity() throws Exception {
-        CoreSession session = remote;
-        DocumentModel folder = new DocumentModelImpl("/", "folder", "Folder");
-        folder = session.createDocument(folder);
-        ACP acp = new ACPImpl();
-        ACE ace = new ACE("princ1", "perm1", true);
-        ACL acl = new ACLImpl("acl1", false);
-        acl.add(ace);
-        acp.addACL(acl);
-        session.setACP(folder.getRef(), acp, true);
-        DocumentModel file = new DocumentModelImpl("/folder", "file", "File");
-        file = session.createDocument(file);
-        // set security
-        acp = new ACPImpl();
-        ace = new ACE("princ2", "perm2", true);
-        acl = new ACLImpl("acl2", false);
-        acl.add(ace);
-        acp.addACL(acl);
-        session.setACP(file.getRef(), acp, true);
-        session.save();
-        VersionModel vm = new VersionModelImpl();
-        vm.setLabel("v1");
-        session.checkIn(file.getRef(), vm);
-        session.checkOut(file.getRef());
-
-        // check security on version
-        DocumentModel version = session.getDocumentWithVersion(file.getRef(), vm);
-        acp = session.getACP(version.getRef());
-        ACL[] acls = acp.getACLs();
-        assertEquals(2, acls.length);
-        acl = acls[0];
-        assertEquals(1, acl.size());
-        assertEquals("princ2", acl.get(0).getUsername());
-        acl = acls[1];
-        assertEquals(1 + 4, acl.size()); // 1 + 4 root defaults
-        assertEquals("princ1", acl.get(0).getUsername());
-
-        // remove live document (create a proxy so the version stays)
-        session.createProxy(folder.getRef(), file.getRef(), vm, true);
-        session.save();
-        session.removeDocument(file.getRef());
-        // recheck security on version
-        try {
-            session.getACP(version.getRef());
-            fail();
-        } catch (DocumentSecurityException e) {
-            // ok
-        }
     }
 
 }
