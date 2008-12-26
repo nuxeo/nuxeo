9c3b1b0af6e998f03abff2f9c97783ee8ab70cff
Florent Guillaume <fg@nuxeo.com>
2008-12-26 23:37:37 +0100

    NXP-2833: fix unit tests

--- nuxeo-core-storage-sql/src/test/java/org/nuxeo/ecm/core/storage/sql/db/TestEmbeddedFunctions.java
+++ nuxeo-core-storage-sql/src/test/java/org/nuxeo/ecm/core/storage/sql/db/TestEmbeddedFunctions.java
@@ -56,15 +56,18 @@ public class TestEmbeddedFunctions extends TestCase {
         assertEquals("foo", EmbeddedFunctions.parseWord("fOoS"));
     }
 
+    protected static void checkParseFullText(String expected, String text)
+            throws Exception {
+        assertEquals(new HashSet<String>(Arrays.asList(expected.split(" "))),
+                EmbeddedFunctions.parseFullText(text));
+    }
+
     public void testParseFullText() throws Exception {
-        assertEquals(
-                "brown dog fail fox jump lazy over quick",
-                EmbeddedFunctions.parseFullText("The quick brown fox jumps over the lazy dog -- and fails!"));
-        assertEquals(
-                "aime cafe jure pas",
-                EmbeddedFunctions.parseFullText("J'aime PAS le caf\u00e9, je te jure."));
-        assertEquals("007 bond jame thx1138",
-                EmbeddedFunctions.parseFullText("James Bond 007 && THX1138"));
+        checkParseFullText("brown dog fail fox jump lazy over quick",
+                "The quick brown fox jumps over the lazy dog -- and fails!");
+        checkParseFullText("aime cafe jure pas",
+                "J'aime PAS le caf\u00e9, je te jure.");
+        checkParseFullText("007 bond jame thx1138", "James Bond 007 && THX1138");
     }
 
 }
