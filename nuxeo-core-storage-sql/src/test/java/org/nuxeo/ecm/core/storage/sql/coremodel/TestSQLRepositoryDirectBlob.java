f211c061c271d18621e604ac37466610b666e8fb
Bogdan Stefanescu <bs@nuxeo.com>
2009-01-15 00:00:46 +0100

    NXP-2857: fixed tests

--- nuxeo-core-storage-sql/src/test/java/org/nuxeo/ecm/core/storage/sql/coremodel/TestSQLRepositoryDirectBlob.java
+++ nuxeo-core-storage-sql/src/test/java/org/nuxeo/ecm/core/storage/sql/coremodel/TestSQLRepositoryDirectBlob.java
@@ -51,6 +51,7 @@ public class TestSQLRepositoryDirectBlob extends SQLRepositoryTestCase {
         super.setUp();
         deployContrib("org.nuxeo.ecm.core.storage.sql.tests",
                 "OSGI-INF/test-repo-core-types-contrib.xml");
+        deployBundle("org.nuxeo.ecm.core.event");
         openSession();
     }
 
