16a84afb1f8ba9bd3f401c6ae730facd828a2504
Stephane Lacoin <slacoin@nuxeo.com>
2009-01-25 21:42:14 +0100

    NXP-2981 corrected named queries

--- nuxeo-platform-audit-api/src/main/java/org/nuxeo/ecm/platform/audit/api/LogEntry.java
+++ nuxeo-platform-audit-api/src/main/java/org/nuxeo/ecm/platform/audit/api/LogEntry.java
@@ -56,7 +56,7 @@ import org.nuxeo.ecm.platform.events.api.DocumentMessage;
         @NamedQuery(name = "LogEntry.findByEventIdAndPath", query = "from LogEntry log where log.eventId=:eventId and log.docPath LIKE :pathPattern"),
         @NamedQuery(name = "LogEntry.findByHavingExtendedInfo", query = "from LogEntry log where log.extendedInfos['one'] is not null order by log.eventDate DESC"),
         @NamedQuery(name = "LogEntry.countEventsById", query = "select count(log.eventId) from LogEntry log where log.eventId=:eventId"),
-        @NamedQuery(name = "LogEntry.", query = "select distinct log.eventId from LogEntry log")
+        @NamedQuery(name = "LogEntry.findEventIds", query = "select distinct log.eventId from LogEntry log")
             })
         @Table(name = "NXP_LOGS")
 public class LogEntry {
