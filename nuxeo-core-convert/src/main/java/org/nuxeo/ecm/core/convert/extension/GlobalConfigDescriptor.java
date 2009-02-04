4f86d69f2dfcb4dd2e21590890def045cc17e9e6
Julien Carsique <jcarsique@nuxeo.com>
2009-02-04 16:53:41 +0100

    NXP-3152 - use another cache directory in case of access denied

--- nuxeo-core-convert/src/main/java/org/nuxeo/ecm/core/convert/extension/GlobalConfigDescriptor.java
+++ nuxeo-core-convert/src/main/java/org/nuxeo/ecm/core/convert/extension/GlobalConfigDescriptor.java
@@ -17,25 +17,33 @@
  */
 package org.nuxeo.ecm.core.convert.extension;
 
+import java.io.File;
+import java.io.IOException;
 import java.io.Serializable;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.nuxeo.common.xmap.annotation.XNode;
 import org.nuxeo.common.xmap.annotation.XObject;
 import org.nuxeo.ecm.core.convert.api.ConversionService;
 
 /**
  * XMap Descriptor for the {@link ConversionService} configuration.
- *
+ * 
  * @author tiry
  */
 @XObject("configuration")
 public class GlobalConfigDescriptor implements Serializable {
+
+    private static final String CACHING_DIRECTORY = "convertcache";
 
     private static final long serialVersionUID = 1L;
 
     public static final long DEFAULT_GC_INTERVAL_IN_MIN = 10;
 
     public static final int DEFAULT_DISK_CACHE_IN_KB = 10 * 1024;
+
+    private static final Log log = LogFactory.getLog(GlobalConfigDescriptor.class);
 
     @XNode("gcInterval")
     protected long GCInterval;
@@ -84,9 +92,19 @@ public class GlobalConfigDescriptor implements Serializable {
 
     public String getCachingDirectory() {
         if (cachingDirectory == null) {
-            cachingDirectory = System.getProperty("java.io.tmpdir");
+            File cacheFile=new File(System.getProperty("java.io.tmpdir"),CACHING_DIRECTORY);
+            if (!cacheFile.canWrite()) {
+                // change directory to avoid FileNotFoundException (permission denied)
+                try {
+                    cacheFile=File.createTempFile(CACHING_DIRECTORY, null, cacheFile.getParentFile());
+                    cacheFile.delete();
+                    cacheFile.mkdirs();
+                } catch (IOException e) {
+                    log.error("Could not create caching directory",e);
+                }
+            }
+            cachingDirectory = cacheFile.getPath();
         }
         return cachingDirectory;
     }
-
 }
