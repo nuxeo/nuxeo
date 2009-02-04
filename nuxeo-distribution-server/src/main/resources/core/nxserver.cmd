b71a0aead69016553beee08c77e741805cadf889
Bogdan Stefanescu <bs@nuxeo.com>
2009-02-04 20:09:20 +0100

    fixed windows nxserver.cmd launcher

--- nuxeo-distribution-server/src/main/resources/core/nxserver.cmd
+++ nuxeo-distribution-server/src/main/resources/core/nxserver.cmd
@@ -1,9 +1,24 @@
 @echo off
+SETLOCAL ENABLEEXTENSIONS
+Setlocal Enabledelayedexpansion
 
 set JAVA_OPTS=-Djava.rmi.server.RMIClassLoaderSpi=org.nuxeo.runtime.launcher.NuxeoRMIClassLoader -Dsun.lang.ClassLoader.allowArraySyntax=true
 set JAVA_OPTS=%JAVA_OPTS% -Dderby.system.home=data/derby
 
-if "%1" == "-debug" set JAVA_OPTS=%JAVA_OPTS% -Xdebug -Xrunjdwp:transport=dt_socket,address=127.0.0.1:8788,server=y,suspend=y
+set DEV_OPTS=""
+
+if "%1" == "-dev" (goto :devmode) else (goto :normal_mode) 
+
+:devmode
+set CMD_ARGS=%2 %3 %4 %5 %6 %7 %8 %9
+set JAVA_OPTS=%JAVA_OPTS% -Dorg.nuxeo.dev=true -Xdebug -Xrunjdwp:transport=dt_socket,address=127.0.0.1:8788,server=y,suspend=n
+set DEV_OPTS=-clear -console
+goto :launch
+
+:normal_mode
+set CMD_ARGS=%*
+
+:launch
 
 if not "%JAVA_HOME%" == "" goto SET_JAVA
 
@@ -19,17 +34,17 @@ set JAVA=%JAVA_HOME%\bin\java
 
 :SKIP_SET_JAVA
 
-Setlocal Enabledelayedexpansion
 for %%A in ("nuxeo-runtime-launcher-*.jar") do (
     set "FileNameWithoutExtension=%%~nA"
     set "SubString=!FileNameWithoutExtension:*nuxeo-runtime-launcher-=!"
     set NXC_VERSION=!SubString!
 )
-for %%A in ("bundles\nuxeo-shell-commands-base-*.jar") do (
-    set "FileNameWithoutExtension=%%~nA"
-    set "SubString=!FileNameWithoutExtension:*bundles\nuxeo-shell-commands-base-=!"
-    set NXP_VERSION=!SubString!
-)
+
+rem example on how to add external bundles to your environment. Usefull to dev. using IDEs.
+rem the eclipse plugin is using this option to start webengine.
+rem POST_BUNDLES="-post-bundles /path/to/your/external/bundle:/path/to/second/bundle:/etc"
 
 
-"%JAVA%" %JAVA_OPTS% -jar nuxeo-runtime-launcher-%NXC_VERSION%.jar bundles/nuxeo-runtime-osgi-%NXC_VERSION%.jar/org.nuxeo.osgi.application.Main bundles/.:lib/.:config -bundles=bundles/nuxeo-shell-commands-base-%NXP_VERSION%.jar@3 -home . %*
+"%JAVA%" %JAVA_OPTS% -jar nuxeo-runtime-launcher-%NXC_VERSION%.jar bundles/nuxeo-runtime-osgi-%NXC_VERSION%.jar/org.nuxeo.osgi.application.Main bundles/.:lib/.:config %POST_BUNDLES% -home . %DEV_OPTS% %CMD_ARGS%
+
+
