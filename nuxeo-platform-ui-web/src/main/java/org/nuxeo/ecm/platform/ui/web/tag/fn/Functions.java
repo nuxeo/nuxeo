6251d7becb4cbb2e1aeb6da386412383c469e900
Radu Darlea <rdarlea@nuxeo.com>
2009-02-27 19:19:39 +0200

    ACR-72: meaningful link for mini-site

--- nuxeo-platform-ui-web/src/main/java/org/nuxeo/ecm/platform/ui/web/tag/fn/Functions.java
+++ nuxeo-platform-ui-web/src/main/java/org/nuxeo/ecm/platform/ui/web/tag/fn/Functions.java
@@ -32,6 +32,7 @@ import javax.faces.context.FacesContext;
 import org.apache.commons.lang.StringUtils;
 import org.nuxeo.ecm.core.api.ClientException;
 import org.nuxeo.ecm.core.api.NuxeoPrincipal;
+import org.nuxeo.ecm.platform.ui.web.util.BaseURL;
 import org.nuxeo.ecm.platform.usermanager.UserManager;
 import org.nuxeo.runtime.api.Framework;
 
@@ -280,6 +281,10 @@ public final class Functions {
             size /= base;
         }
         return "" + size + " " + suffix[ex] + "B";
+    }
+    
+    public static String printWebContainerCompleteUrl(String lastUriName) {
+        return BaseURL.getBaseURL() + "site/sites/" + lastUriName;
     }
 
 }
