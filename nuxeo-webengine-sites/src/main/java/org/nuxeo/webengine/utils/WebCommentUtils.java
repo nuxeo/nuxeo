a027e6725e45f41b636311007890f23e6aa2ecb4
Mariana Cedica <mcedica@nuxeo.com>
2009-03-10 13:37:09 +0200

    WEB-46: adding comments on webpages

--- nuxeo-webengine-sites/src/main/java/org/nuxeo/webengine/utils/WebCommentUtils.java
+++ nuxeo-webengine-sites/src/main/java/org/nuxeo/webengine/utils/WebCommentUtils.java
@@ -1,3 +1,21 @@
+/*
+ * (C) Copyright 2009 Nuxeo SA (http://nuxeo.com/) and contributors.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser General Public License
+ * (LGPL) version 2.1 which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl.html
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * Contributors:
+ *     mcedica
+ */
+
+
 package org.nuxeo.webengine.utils;
 
 import java.util.ArrayList;
@@ -7,7 +25,11 @@ import java.util.List;
 import org.nuxeo.ecm.core.api.CoreSession;
 import org.nuxeo.ecm.core.api.DocumentModel;
 
-
+/**
+ * Utilities for Comments implementation into WebEngine
+ * @author mariana
+ *
+ */
 public class WebCommentUtils {
 
     /**
@@ -18,7 +40,7 @@ public class WebCommentUtils {
         List<DocumentModel> parents = session.getParentDocuments(doc.getRef());
         for (DocumentModel documentModel : parents) {
             if (documentModel.getType().equals("Workspace")) {
-                // TO DO: test for groups eg. administrators
+                // TODO: test for groups eg. administrators
                 String[] moderators = documentModel.getACP().listUsernamesForPermission(
                         permission);
                 return new ArrayList<String>(Arrays.asList(moderators));
@@ -56,8 +78,8 @@ public class WebCommentUtils {
      */
     public static boolean currentUserHasCommentPermision(CoreSession session,
             DocumentModel doc) throws Exception {
-        ArrayList<String> moderators = getUsersWithPermission(session, doc, WebCommentConstants.PERMISSION_COMMENT);
-        if (moderators.contains(session.getPrincipal().getName())) {
+        ArrayList<String> users = getUsersWithPermission(session, doc, WebCommentConstants.PERMISSION_COMMENT);
+        if (users.contains(session.getPrincipal().getName())) {
             return true;
         }
 
