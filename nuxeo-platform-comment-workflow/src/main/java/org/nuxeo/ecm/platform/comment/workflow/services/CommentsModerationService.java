3c4b30e8c3586dbbc71de44b46ac272ab317f533
Mariana Cedica <mcedica@nuxeo.com>
2009-04-06 17:11:42 +0300

    WEB-225 : adding comments moderation for nuxeo-platform-comment

---
+++ nuxeo-platform-comment-workflow/src/main/java/org/nuxeo/ecm/platform/comment/workflow/services/CommentsModerationService.java
@@ -0,0 +1,87 @@
+/*
+ * (C) Copyright 2009 Nuxeo SA (http://nuxeo.com/) and contributors.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser General Public License
+ * (LGPL) version 2.1 which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl.html
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * Contributors:
+ *     mcedica
+ */
+package org.nuxeo.ecm.platform.comment.workflow.services;
+
+import java.util.ArrayList;
+
+import org.jbpm.graph.exe.ProcessInstance;
+import org.nuxeo.ecm.core.api.ClientException;
+import org.nuxeo.ecm.core.api.CoreSession;
+import org.nuxeo.ecm.core.api.DocumentModel;
+import org.nuxeo.ecm.platform.jbpm.JbpmService;
+
+public interface CommentsModerationService {
+
+    /**
+     * Starts the moderation process on given Comment posted on a documentModel.
+     * 
+     * @param session - the coreSession
+     * @param document - the document were the comment is posted
+     * @param commentId -the commentId
+     * @throws ClientException
+     */
+    void startModeration(CoreSession session, DocumentModel document,
+            String commentID, ArrayList<String> moderators)
+            throws ClientException;
+    
+    /**
+     * Gets the moderation process for the given Comment posted on a documentModel.
+     * 
+     * @param session - the coreSession
+     * @param document - the document were the comment is posted
+     * @param commentId -the commentId
+     * @throws ClientException
+     */
+    public ProcessInstance getModerationProcess(JbpmService jbpmService,
+            CoreSession session, DocumentModel doc, String commentId)
+            throws ClientException;
+    
+    /**
+     * Approve the comment with the given commentId.
+     * 
+     * @param session - the coreSession
+     * @param document - the document were the comment is posted
+     * @param commentId -the commentId
+     * @throws ClientException
+     */
+    void approveComent(CoreSession session, DocumentModel document,
+            String commentID) throws ClientException;
+    
+    /**
+     * Reject the comment with the given commentId.
+     * 
+     * @param session - the coreSession
+     * @param document - the document were the comment is posted
+     * @param commentId -the commentId
+     * @throws ClientException
+     */
+    void rejectComment(CoreSession session, DocumentModel document,
+            String commentID) throws ClientException;
+
+
+    /**
+     * Publish the comment with the given commentId.
+     * 
+     * @param session - the coreSession
+     * @param document - the document were the comment is posted
+     * @param commentId -the commentId
+     * @throws ClientException
+     */
+    public void publishComment(CoreSession session, DocumentModel comment)
+            throws ClientException;
+
+}
---
+++ nuxeo-platform-comment-workflow/src/main/resources/templates/comment.ftl
@@ -0,0 +1,5 @@
+<HTML>
+<BODY>
+<P>A new comment (UID: ${docId}) has been added by ${author} at ${dateTime?datetime?string("dd/MM/yyyy - HH:mm")}.<BR>
+</BODY>
+<HTML>
