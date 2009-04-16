f38d5ecc47a6882dac347a9d65d90857af6510b5
Thierry Delprat <td@nuxeo.com>
2009-04-16 18:16:40 +0200

    NXP-3425 : removing webapp !

--- nuxeo-platform-webapp/src/main/resources/nuxeo.war/scripts/custom-contextmenu-actions.js
+++
@@ -1,2 +0,0 @@
-// this script is designed to be overloaded by third-party projects
-// it can receive javascript functions called by contextmenu actions
--- nuxeo-platform-webapp/src/main/resources/nuxeo.war/scripts/custom-javascript.js
+++
@@ -1,2 +0,0 @@
-// this script is designed to be overloaded by third-party projects
-// it is included by includes.xhtml
