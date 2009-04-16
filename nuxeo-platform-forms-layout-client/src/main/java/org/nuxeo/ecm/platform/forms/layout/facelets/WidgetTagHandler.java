9514b265e421b22ab0d59b6009ecb99b62150cd8
Thierry Delprat <td@nuxeo.com>
2009-04-16 18:02:37 +0200

    NXP-3425 : add layouts

---
+++ nuxeo-platform-forms-layout-client/src/main/java/org/nuxeo/ecm/platform/forms/layout/facelets/WidgetTagHandler.java
@@ -0,0 +1,202 @@
+/*
+ * (C) Copyright 2006-2007 Nuxeo SAS (http://nuxeo.com/) and contributors.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser General Public License
+ * (LGPL) version 2.1 which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl.html
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * Contributors:
+ *     <a href="mailto:at@nuxeo.com">Anahide Tchertchian</a>
+ *
+ * $Id: WidgetTagHandler.java 30553 2008-02-24 15:51:31Z atchertchian $
+ */
+
+package org.nuxeo.ecm.platform.forms.layout.facelets;
+
+import java.io.IOException;
+
+import javax.el.ELException;
+import javax.el.ValueExpression;
+import javax.el.VariableMapper;
+import javax.faces.FacesException;
+import javax.faces.component.UIComponent;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.nuxeo.ecm.platform.forms.layout.api.Widget;
+import org.nuxeo.ecm.platform.forms.layout.service.WebLayoutManager;
+import org.nuxeo.ecm.platform.ui.web.util.ComponentTagUtils;
+import org.nuxeo.runtime.api.Framework;
+
+import com.sun.facelets.FaceletContext;
+import com.sun.facelets.FaceletHandler;
+import com.sun.facelets.TemplateClient;
+import com.sun.facelets.el.VariableMapperWrapper;
+import com.sun.facelets.tag.MetaTagHandler;
+import com.sun.facelets.tag.TagAttribute;
+import com.sun.facelets.tag.TagConfig;
+
+/**
+ * Widget tag handler.
+ * <p>
+ * Applies {@link  WidgetTypeHandler} found for given widget, in given mode and
+ * for given value.
+ *
+ * @author <a href="mailto:at@nuxeo.com">Anahide Tchertchian</a>
+ *
+ */
+public class WidgetTagHandler extends MetaTagHandler implements TemplateClient {
+
+    @SuppressWarnings("unused")
+    private static final Log log = LogFactory.getLog(WidgetTagHandler.class);
+
+    protected final TagConfig config;
+
+    protected final TagAttribute widget;
+
+    protected final TagAttribute mode;
+
+    protected final TagAttribute value;
+
+    // composition attribute
+
+    // XXX AT: maybe not keeping this value as a tag instance would avoid the
+    // apply method to be synchronized
+    protected Widget widgetInstance = null;
+
+    public WidgetTagHandler(TagConfig config) {
+        super(config);
+        this.config = config;
+        widget = getRequiredAttribute("widget");
+        mode = getRequiredAttribute("mode");
+        value = getRequiredAttribute("value");
+    }
+
+    // XXX same handler is used in different threads => synchronize it since
+    // some member fields are not supposed to be shared
+    public synchronized void apply(FaceletContext ctx, UIComponent parent)
+            throws IOException, FacesException, ELException {
+        // build handler
+        widgetInstance = (Widget) widget.getObject(ctx, Widget.class);
+        if (widgetInstance != null) {
+            // set value name on widget instance in case it's changed from first
+            // computation
+            String valueName = value.getValue();
+            if (ComponentTagUtils.isValueReference(valueName)) {
+                valueName = valueName.substring(2, valueName.length() - 1);
+            }
+            widgetInstance.setValueName(valueName);
+            ctx.pushClient(this);
+            applyWidgetHandler(ctx, parent, config, widgetInstance, value, true);
+            ctx.popClient(this);
+        }
+    }
+
+    public static void generateWidgetIdsRecursive(FaceletHandlerHelper helper,
+            Widget widget) {
+        if (widget == null) {
+            return;
+        }
+        widget.setId(helper.generateWidgetId(widget.getName()));
+        Widget[] subWidgets = widget.getSubWidgets();
+        if (subWidgets != null) {
+            for (Widget subWidget : subWidgets) {
+                generateWidgetIdsRecursive(helper, subWidget);
+            }
+        }
+    }
+
+    public static void applyWidgetHandler(FaceletContext ctx,
+            UIComponent parent, TagConfig config, Widget widget,
+            TagAttribute value, boolean fillVariables) throws IOException {
+        if (widget == null) {
+            return;
+        }
+        WebLayoutManager layoutService;
+        try {
+            layoutService = Framework.getService(WebLayoutManager.class);
+        } catch (Exception e) {
+            throw new FacesException(e);
+        }
+
+        // set unique id on widget and sub widgets before building handler.
+        FaceletHandlerHelper helper = new FaceletHandlerHelper(ctx, config);
+        generateWidgetIdsRecursive(helper, widget);
+
+        // log.debug("thread=" + String.valueOf(Thread.currentThread().getId())
+        // + ", handler=" + tHandler.hashCode() + ", w=" + widget.getId());
+
+        FaceletHandler handler = layoutService.getFaceletHandler(ctx, config,
+                widget);
+        if (handler == null) {
+            return;
+        }
+        if (fillVariables) {
+            // expose widget variables to the variable mapper
+            VariableMapper orig = ctx.getVariableMapper();
+            VariableMapper vm = new VariableMapperWrapper(orig);
+            ctx.setVariableMapper(vm);
+            ValueExpression valueExpr = value.getValueExpression(ctx,
+                    Object.class);
+            vm.setVariable(RenderVariables.globalVariables.value.name(),
+                    valueExpr);
+            vm.setVariable(String.format("%s_%s",
+                    RenderVariables.globalVariables.value.name(),
+                    widget.getLevel()), valueExpr);
+            // document as alias to value
+            vm.setVariable(RenderVariables.globalVariables.document.name(),
+                    valueExpr);
+            // apply
+            handler.apply(ctx, parent);
+            ctx.setVariableMapper(orig);
+        } else {
+            // just apply
+            handler.apply(ctx, parent);
+        }
+    }
+
+    public boolean apply(FaceletContext ctx, UIComponent parent, String name)
+            throws IOException, FacesException, ELException {
+        if (widgetInstance == null) {
+            return false;
+        }
+        Integer subWidgetNumber = TemplateClientHelper.getSubWidgetNumber(name);
+        if (subWidgetNumber != null) {
+            Widget[] subWidgets = widgetInstance.getSubWidgets();
+            if (subWidgets == null || subWidgetNumber > subWidgets.length - 1) {
+                return false;
+            }
+            Widget subWidget = subWidgets[subWidgetNumber];
+            // expose widget variables
+            VariableMapper vm = ctx.getVariableMapper();
+            ValueExpression subWidgetVe = ctx.getExpressionFactory().createValueExpression(
+                    subWidget, Widget.class);
+            vm.setVariable(RenderVariables.widgetVariables.widget.name(),
+                    subWidgetVe);
+            vm.setVariable(String.format("%s_%s",
+                    RenderVariables.widgetVariables.widget.name(),
+                    subWidget.getLevel()), subWidgetVe);
+            vm.setVariable(RenderVariables.subWidgetVariables.subWidget.name(),
+                    subWidgetVe);
+            ValueExpression subWidgetIndexVe = ctx.getExpressionFactory().createValueExpression(
+                    subWidgetNumber, Integer.class);
+            vm.setVariable(RenderVariables.widgetVariables.widgetIndex.name(),
+                    subWidgetIndexVe);
+            vm.setVariable(String.format("%s_%s",
+                    RenderVariables.widgetVariables.widgetIndex.name(),
+                    subWidget.getLevel()), subWidgetIndexVe);
+            // BBB
+            vm.setVariable(
+                    RenderVariables.subWidgetVariables.subWidgetIndex.name(),
+                    subWidgetIndexVe);
+            return true;
+        }
+        return false;
+    }
+}
---
+++ nuxeo-platform-forms-layout-client/src/main/java/org/nuxeo/ecm/platform/forms/layout/service/WebLayoutManager.java
@@ -0,0 +1,101 @@
+/*
+ * (C) Copyright 2006-2007 Nuxeo SAS (http://nuxeo.com/) and contributors.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser General Public License
+ * (LGPL) version 2.1 which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl.html
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * Contributors:
+ *     <a href="mailto:at@nuxeo.com">Anahide Tchertchian</a>
+ *
+ * $Id: WebLayoutManager.java 26053 2007-10-16 01:45:43Z atchertchian $
+ */
+
+package org.nuxeo.ecm.platform.forms.layout.service;
+
+import java.io.Serializable;
+import java.util.Map;
+
+import org.nuxeo.ecm.platform.forms.layout.api.Layout;
+import org.nuxeo.ecm.platform.forms.layout.api.Widget;
+import org.nuxeo.ecm.platform.forms.layout.api.exceptions.WidgetException;
+import org.nuxeo.ecm.platform.forms.layout.api.service.LayoutManager;
+import org.nuxeo.ecm.platform.forms.layout.facelets.WidgetTypeHandler;
+
+import com.sun.facelets.FaceletContext;
+import com.sun.facelets.FaceletHandler;
+import com.sun.facelets.tag.TagConfig;
+
+/**
+ * Web Layout manager interface.
+ * <p>
+ * It manages registries of layout definitions and widget types and handles the
+ * creation of layouts and widgets instances.
+ *
+ * @author <a href="mailto:at@nuxeo.com">Anahide Tchertchian</a>
+ *
+ */
+public interface WebLayoutManager extends LayoutManager {
+
+    /**
+     * Returns the widget type handler for the registered widget type with this
+     * type name.
+     * <p>
+     * If the no widget type is found with this name, return null.
+     */
+    WidgetTypeHandler getWidgetTypeHandler(String typeName)
+            throws WidgetException;
+
+    /**
+     * Returns the computed layout for this name and mode in given context.
+     * <p>
+     * When a widget is configured not to be rendered in this mode, the layout
+     * will hold a null value instead. As well, when a row does not hold any
+     * non-null widget in this mode, the layout will not hold it.
+     *
+     * @param ctx the facelet context this layout will be computed in. If
+     *            context is null, no expressions can be resolved during
+     *            computing.
+     * @param layoutName the layout definition name.
+     * @param mode the mode.
+     * @param valueName the value name to use when computing tag attributes.
+     * @return a layout computed in this context.
+     */
+    Layout getLayout(FaceletContext ctx, String layoutName, String mode,
+            String valueName);
+
+    /**
+     * Returns the facelet handler for given widget.
+     *
+     * @param ctx the facelet context.
+     * @param config the tag config, used to hook the handler in the jsf tree.
+     * @param widget the computed widget.
+     * @return a facelet handler.
+     */
+    FaceletHandler getFaceletHandler(FaceletContext ctx, TagConfig config,
+            Widget widget);
+
+    /**
+     * Returns a widget computed from given information.
+     *
+     * @param ctx the facelet context this layout will be computed in. If
+     *            context is null, no expressions can be resolved during
+     *            computing.
+     * @param type the widget type name.
+     * @param mode the mode.
+     * @param valueName the value name to use when computing tag attributes.
+     * @param properties optional properties to use when computing the widget.
+     * @param subWidgets optional sub widgets for this widget.
+     * @return a widget computed in this context.
+     */
+    Widget createWidget(FaceletContext ctx, String type, String mode,
+            String valueName, Map<String, Serializable> properties,
+            Widget[] subWidgets);
+
+}
