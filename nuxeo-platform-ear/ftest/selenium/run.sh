c60e4a149ee766b97dd42dd10e5c6315943ff2a0
Benoit Delbosc <bdelbosc@nuxeo.com>
2009-04-30 10:35:02 +0200

    NXBT-44: Launch all tests suites, continue on error mode

--- nuxeo-platform-ear/ftest/selenium/run.sh
+++ nuxeo-platform-ear/ftest/selenium/run.sh
@@ -29,12 +29,25 @@ sed "s,\(storedVars\['testfolderpath'\]\ \=\).*$,\1\ \"$HERE\";,g" < user-extens
 # Update url in profile
 sed "s,\(capability.principal.codebase.p0.id...\).*$,\1\"$URL\");,g" < ffprofile/prefs.js.sample > ffprofile/prefs.js
 
+exit_code=0
 # Launch suites
 for suite in $SUITES; do
-    echo "### Running test suite $suite ..."
-    $CMD "$PWD/tests/$suite.html" "$PWD/result-$suite.html" $CMD_END || exit 1
+    echo "### [INFO] Running test suite $suite ..."
+    $CMD "$PWD/tests/$suite.html" "$PWD/result-$suite.html" $CMD_END
+    if [ $? != 0 ]; then
+        echo "### [ERROR] $suite TEST FAILURE"
+        exit_code=9
+    else
+        echo "### [INFO] $suite TEST SUCCESSFUL"
+    fi
     # pause to prevent "Xvfb failed to start"
     sleep 5
 done
 
-echo "### Successful"
+if [ $exit_code != 0 ]; then
+    echo "### [ERROR] TESTS FAILURE"
+else
+    echo "### [INFO] TESTS SUCCESSFUL"
+fi
+
+exit $exit_code
