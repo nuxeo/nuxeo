1509587f96bc04b7db47987700221ae831b9031c
Thierry Delprat <td@nuxeo.com>
2009-05-04 18:58:20 +0200

    NXP-2974 readd

--- nuxeo-platform-webwidgets/nuxeo-theme-webwidgets-ui/src/main/resources/nxthemes/webwidgets/scripts/uwa.js
+++ nuxeo-platform-webwidgets/nuxeo-theme-webwidgets-ui/src/main/resources/nxthemes/webwidgets/scripts/uwa.js
@@ -212,29 +212,110 @@ UWA.Widget.prototype = {
 
 };
 
+UWA.proxies = {
+};
 
-UWA.proxies = {};
+NV_PATH='/nuxeo';
+NV_MODULES='/nuxeo';
 
 UWA.Data = {
 
+  AJAX_PROXY_URL:  '/nuxeo/ajaxProxy/',
+
+  needProxy: function(url) {
+      if (url.indexOf("http")==0) {
+          return true;
+      }
+      return false;
+  },
+
   getText: function(url, callback) {
-    alert('UWA.Data.getText not implemented');
+    var onComplete = function(req) {
+      if (typeof callback == "function") {
+        callback(req.responseText);
+      }
+    };
+
+    if (this.needProxy(url)) {
+        this.request(this.AJAX_PROXY_URL,
+          {parameters: "?url=" + encodeURIComponent(url), onComplete: onComplete});
+    }
+    else {
+        this.request(url, {onComplete: onComplete});
+    }
   },
 
   getXml: function(url, callback) {
-    alert('UWA.Data.getXml not implemented');
+    var onComplete = function(req) {
+      if (typeof callback == "function") {
+        var xml = req.responseText;
+        var xotree = new XML.ObjTree();
+        var tree = xotree.parseXML(xml);
+        callback(tree);
+      }
+    };
+    if (this.needProxy(url)) {
+        this.request(this.AJAX_PROXY_URL,
+          {parameters: "?url=" + encodeURIComponent(url), onComplete: onComplete});
+    }
+    else {
+        this.request(url, {onComplete: onComplete});
+    }
   },
 
   getFeed: function(url, callback) {
-    alert('UWA.Data.getFeed not implemented');
+    var onComplete = function(req) {
+      if (typeof callback == "function") {
+       var xml = req.responseText;
+       var feed = new FeedParser(xml);
+       feed.parse();
+       callback(feed);
+      }
+    };
+    if (this.needProxy(url)) {
+        this.request(this.AJAX_PROXY_URL,
+          {parameters: "?url=" + encodeURIComponent(url), onComplete: onComplete});
+    }
+    else {
+        this.request(url, {onComplete: onComplete});
+    }
   },
 
   getJson: function(url, callback) {
-    alert('UWA.Data.getJson not implemented');
+    var onComplete = function(req) {
+      try {
+        eval("var j = " + req.responseText);
+        if (typeof callback == "function") {
+          callback(j);
+        }
+      } catch(e) {
+        UWA.log(e);
+      }
+    }
+    if (this.needProxy(url)) {
+        this.request(this.AJAX_PROXY_URL,
+          {parameters: "?url=" + encodeURIComponent(url), onComplete: onComplete});
+    }
+    else {
+        this.request(url, {onComplete: onComplete});
+    }
   },
 
   request: function(url, request) {
-    alert('UWA.Data.request not implemented');
+    var method = request.method || 'GET';
+    var parameters = request.parameters;
+    var cache = request.cache;
+    if (cache) {
+      if (parameters) {
+        parameters += '&cache=' + encodeURIComponent(cache);
+      } else {
+        parameters = '?cache=' + encodeURIComponent(cache);
+      }
+    }
+    new Ajax.Request(url, {
+      parameters: parameters,
+      method: method.toLowerCase(),
+      onComplete: request.onComplete || function(req) {}
+    });
   }
-
 };
