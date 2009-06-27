034a356d204e6dd3e61027143350fb3d72c2cc1d
Stefane Fermigier <sfermigier@nuxeo.com>
2009-06-27 13:15:45 +0200

    Forgot to rename file in hg.

--- nuxeo-core-convert-api/src/main/java/org/nuxeo/ecm/core/convert/api/ConverterNotRegistered.java
+++ nuxeo-core-convert-api/src/main/java/org/nuxeo/ecm/core/convert/api/ConverterNotRegistered.java
@@ -24,11 +24,11 @@ package org.nuxeo.ecm.core.convert.api;
  *
  * @author tiry
  */
-public class ConverterNotRegistred extends ConversionException {
+public class ConverterNotRegistered extends ConversionException {
 
     private static final long serialVersionUID = 1L;
 
-    public ConverterNotRegistred(String message) {
+    public ConverterNotRegistered(String message) {
         super("Converter " + message + " is not registred");
     }
 
