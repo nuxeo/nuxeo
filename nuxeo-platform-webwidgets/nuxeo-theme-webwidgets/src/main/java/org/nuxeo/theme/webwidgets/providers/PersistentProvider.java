dedc1fedea34a062fba54a75bd24bc01480026f9
Anahide Tchertchian <at@nuxeo.com>
2009-06-29 11:24:51 +0200

    NXP-3802: fix per-user provider that was caching current user name, making per-user webwidgets appear for all users

--- nuxeo-platform-webwidgets/nuxeo-theme-webwidgets/src/main/java/org/nuxeo/theme/webwidgets/providers/PersistentProvider.java
+++ nuxeo-platform-webwidgets/nuxeo-theme-webwidgets/src/main/java/org/nuxeo/theme/webwidgets/providers/PersistentProvider.java
@@ -42,14 +42,16 @@ public class PersistentProvider implements Provider {
 
     protected EntityTransaction et;
 
-    protected Principal currentNuxeoPrincipal;
-
     public PersistentProvider() {
-        WebContext ctx = WebEngine.getActiveContext();
         em = PersistenceConfigurator.getEntityManager();
+    }
+
+    public Principal getCurrentPrincipal() {
+        WebContext ctx = WebEngine.getActiveContext();
         if (ctx != null) {
-            currentNuxeoPrincipal = ctx.getPrincipal();
+            return ctx.getPrincipal();
         }
+        return null;
     }
 
     public void addWidget(Widget widget, String regionName, int order)
@@ -272,6 +274,7 @@ public class PersistentProvider implements Provider {
     }
 
     public boolean canWrite() {
+        Principal currentNuxeoPrincipal = getCurrentPrincipal();
         if (currentNuxeoPrincipal == null) {
             log.warn("Could not get the current user from the context.");
             return false;
