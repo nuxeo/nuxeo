73fd6e5c03e2cbfabbf5516c94b2d7e385a49c05
Catalin Baican <cbaican@nuxeo.com>
2009-07-01 19:22:36 +0300

    DAM-99 Simple document view for main metadatas (first draft, currentSelection mechanism added)

--- nuxeo-dam-webapp-core/src/main/java/org/nuxeo/dam/webapp/contentbrowser/DocumentActions.java
+++ nuxeo-dam-webapp-core/src/main/java/org/nuxeo/dam/webapp/contentbrowser/DocumentActions.java
@@ -1,5 +1,6 @@
 package org.nuxeo.dam.webapp.contentbrowser;
 
+import java.io.Serializable;
 import java.util.List;
 import java.util.ArrayList;
 
@@ -22,7 +23,12 @@ import org.apache.commons.logging.Log;
 
 @Name("documentActions")
 @Scope(ScopeType.CONVERSATION)
-public class DocumentActions {
+// TODO: All the commented coded from below will be moved to a new WebActionBean
+// SEAM bean which will be defined as specified in
+// http://jira.nuxeo.org/browse/DAM-167
+public class DocumentActions implements Serializable {
+
+    private static final long serialVersionUID = 1L;
 
     private static final Log log = LogFactory.getLog(DocumentActions.class);
 
@@ -32,79 +38,91 @@ public class DocumentActions {
     @In(required = false, create = true)
     protected transient DocumentsListsManager documentsListsManager;
 
-    @In(create = true)
-    protected transient WebActions webActions;
+//    @In(create = true)
+//    protected transient WebActions webActions;
 
-    @WebRemote
-    public String processSelectRow(String docRef, String providerName,
-            String listName, Boolean selection) {
-        PagedDocumentsProvider provider;
-        try {
-            provider = resultsProvidersCache.get(providerName);
-        } catch (ClientException e) {
-            return handleError(e.getMessage());
-        }
-        DocumentModel doc = null;
-        for (DocumentModel pagedDoc : provider.getCurrentPage()) {
-            if (pagedDoc.getRef().toString().equals(docRef)) {
-                doc = pagedDoc;
-                break;
-            }
-        }
-        if (doc == null) {
-            return handleError(String.format(
-                    "could not find doc '%s' in the current page of provider '%s'",
-                    docRef, providerName));
-        }
-        String lName = (listName == null) ? DocumentsListsManager.CURRENT_DOCUMENT_SELECTION
-                : listName;
-        if (selection) {
-            documentsListsManager.addToWorkingList(lName, doc);
-        } else {
-            documentsListsManager.removeFromWorkingList(lName, doc);
-        }
-        return computeSelectionActions(lName);
+    /**
+     * Current selected asset
+     */
+    protected DocumentModel currentSelection;
+
+//    @WebRemote
+//    public String processSelectRow(String docRef, String providerName,
+//            String listName, Boolean selection) {
+//        PagedDocumentsProvider provider;
+//        try {
+//            provider = resultsProvidersCache.get(providerName);
+//        } catch (ClientException e) {
+//            return handleError(e.getMessage());
+//        }
+//        DocumentModel doc = null;
+//        for (DocumentModel pagedDoc : provider.getCurrentPage()) {
+//            if (pagedDoc.getRef().toString().equals(docRef)) {
+//                doc = pagedDoc;
+//                break;
+//            }
+//        }
+//        if (doc == null) {
+//            return handleError(String.format(
+//                    "could not find doc '%s' in the current page of provider '%s'",
+//                    docRef, providerName));
+//        }
+//        String lName = (listName == null) ? DocumentsListsManager.CURRENT_DOCUMENT_SELECTION
+//                : listName;
+//        if (selection) {
+//            documentsListsManager.addToWorkingList(lName, doc);
+//        } else {
+//            documentsListsManager.removeFromWorkingList(lName, doc);
+//        }
+//        return computeSelectionActions(lName);
+//    }
+//
+//    @WebRemote
+//    public String processSelectPage(String providerName, String listName,
+//            Boolean selection) {
+//        PagedDocumentsProvider provider;
+//        try {
+//            provider = resultsProvidersCache.get(providerName);
+//        } catch (ClientException e) {
+//            return handleError(e.getMessage());
+//        }
+//        DocumentModelList documents = provider.getCurrentPage();
+//        String lName = (listName == null) ? DocumentsListsManager.CURRENT_DOCUMENT_SELECTION
+//                : listName;
+//        if (selection) {
+//            documentsListsManager.addToWorkingList(lName, documents);
+//        } else {
+//            documentsListsManager.removeFromWorkingList(lName, documents);
+//        }
+//        return computeSelectionActions(lName);
+//    }
+//
+//    private String handleError(String errorMessage) {
+//        log.error(errorMessage);
+//        return "ERROR: " + errorMessage;
+//    }
+
+//    private String computeSelectionActions(String listName) {
+//        List<Action> availableActions = webActions.getUnfiltredActionsList(listName
+//                + "_LIST");
+//        List<String> availableActionIds = new ArrayList<String>();
+//        for (Action a : availableActions) {
+//            if (a.getAvailable()) {
+//                availableActionIds.add(a.getId());
+//            }
+//        }
+//        String res = "";
+//        if (!availableActionIds.isEmpty()) {
+//            res = StringUtils.join(availableActionIds.toArray(), "|");
+//        }
+//        return res;
+//    }
+
+    public DocumentModel getCurrentSelection() {
+        return currentSelection;
     }
 
-    @WebRemote
-    public String processSelectPage(String providerName, String listName,
-            Boolean selection) {
-        PagedDocumentsProvider provider;
-        try {
-            provider = resultsProvidersCache.get(providerName);
-        } catch (ClientException e) {
-            return handleError(e.getMessage());
-        }
-        DocumentModelList documents = provider.getCurrentPage();
-        String lName = (listName == null) ? DocumentsListsManager.CURRENT_DOCUMENT_SELECTION
-                : listName;
-        if (selection) {
-            documentsListsManager.addToWorkingList(lName, documents);
-        } else {
-            documentsListsManager.removeFromWorkingList(lName, documents);
-        }
-        return computeSelectionActions(lName);
+    public void setCurrentSelection(DocumentModel selection) {
+        currentSelection = selection;
     }
-
-    private String handleError(String errorMessage) {
-        log.error(errorMessage);
-        return "ERROR: " + errorMessage;
-    }
-
-    private String computeSelectionActions(String listName) {
-        List<Action> availableActions = webActions.getUnfiltredActionsList(listName
-                + "_LIST");
-        List<String> availableActionIds = new ArrayList<String>();
-        for (Action a : availableActions) {
-            if (a.getAvailable()) {
-                availableActionIds.add(a.getId());
-            }
-        }
-        String res = "";
-        if (!availableActionIds.isEmpty()) {
-            res = StringUtils.join(availableActionIds.toArray(), "|");
-        }
-        return res;
-    }
-
 }
