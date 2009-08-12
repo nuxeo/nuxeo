712a17a5b4c81542509e2c1e82bcc0a5f624b740
Alexandre Russel <arussel@nuxeo.com>
2009-08-12 10:56:15 +0200

    NXP-3917 -  theme doesn\'t find the application when behind a proxy in webengine

--- nuxeo-theme-core/src/main/java/org/nuxeo/theme/types/TypeRegistry.java
+++ nuxeo-theme-core/src/main/java/org/nuxeo/theme/types/TypeRegistry.java
@@ -61,6 +61,18 @@ public final class TypeRegistry implements Registrable {
         return registry.get(key);
     }
 
+    public Type lookup(final TypeFamily typeFamily, final String ... names) {
+        for(String name : names) {
+            String key = computeKey(typeFamily, name);
+            Type type = registry.get(key);
+            if(type != null) {
+                return type;
+            }
+        }
+        return null;
+
+    }
+
     public List<String> getTypeNames(final TypeFamily typeFamily) {
         if (!typeNames.containsKey(typeFamily)) {
             return new ArrayList<String>();
