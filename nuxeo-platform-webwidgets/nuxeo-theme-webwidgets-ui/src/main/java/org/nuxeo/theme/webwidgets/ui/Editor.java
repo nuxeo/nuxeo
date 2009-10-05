36def94b30149763aaba6bb49456f9d5b5172491
Florent Guillaume <fg@nuxeo.com>
2009-10-05 16:12:56 +0200

    Copyright, license

--- nuxeo-platform-webwidgets/nuxeo-theme-webwidgets-ui/src/main/java/org/nuxeo/theme/webwidgets/ui/Editor.java
+++ nuxeo-platform-webwidgets/nuxeo-theme-webwidgets-ui/src/main/java/org/nuxeo/theme/webwidgets/ui/Editor.java
@@ -1,4 +1,5 @@
 /*
+ * (C) Copyright 2006-2007 Nuxeo SAS <http://nuxeo.com> and others
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
