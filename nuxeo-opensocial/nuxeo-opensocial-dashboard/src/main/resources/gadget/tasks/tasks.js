cc2c5515cc6743f376c9763090cd5418b1247463
Florent Guillaume <fg@nuxeo.com>
2009-10-12 19:24:09 +0200

    reformat

--- nuxeo-opensocial/nuxeo-opensocial-dashboard/src/main/resources/gadget/tasks/tasks.js
+++ nuxeo-opensocial/nuxeo-opensocial-dashboard/src/main/resources/gadget/tasks/tasks.js
@@ -2,257 +2,257 @@ var testMode = false;
 
 function requestTasks() {
 
-	var prefs = new gadgets.Prefs();
-	var hostname = prefs.getString("nuxeo_host");
-	//console.log("my nuxeo host is "+hostname);
+    var prefs = new gadgets.Prefs();
+    var hostname = prefs.getString("nuxeo_host");
+    //console.log("my nuxeo host is "+hostname);
 
-	var params = {};
-	var headers = {};
+    var params = {};
+    var headers = {};
 
-	//var cookie = readCookie('JSESSIONID');
-	//console.log("we are using cookie:"+cookie);
+    //var cookie = readCookie('JSESSIONID');
+    //console.log("we are using cookie:"+cookie);
 
-	params[gadgets.io.RequestParameters.AUTHORIZATION] = gadgets.io.AuthorizationType.NONE;
-	params[gadgets.io.RequestParameters.CONTENT_TYPE] = gadgets.io.ContentType.JSON;
+    params[gadgets.io.RequestParameters.AUTHORIZATION] = gadgets.io.AuthorizationType.NONE;
+    params[gadgets.io.RequestParameters.CONTENT_TYPE] = gadgets.io.ContentType.JSON;
 
-	var now = new Date().toUTCString();
-	headers["Date", now];
+    var now = new Date().toUTCString();
+    headers["Date", now];
 
-	headers["Expires", "Fri, 01 Jan 1990 00:00:00 GMT"];
-	headers["Pragma", "no-cache"];
-	headers["Cache-control"] = "no-cache, must-revalidate";
-	headers["X-NUXEO-INTEGRATED-AUTH"] = readCookie("JSESSIONID");
+    headers["Expires", "Fri, 01 Jan 1990 00:00:00 GMT"];
+    headers["Pragma", "no-cache"];
+    headers["Cache-control"] = "no-cache, must-revalidate";
+    headers["X-NUXEO-INTEGRATED-AUTH"] = readCookie("JSESSIONID");
 
-	params[gadgets.io.RequestParameters.HEADERS] = headers
-	params[gadgets.io.RequestParameters.REFRESH_INTERVAL] = 0;
+    params[gadgets.io.RequestParameters.HEADERS] = headers
+    params[gadgets.io.RequestParameters.REFRESH_INTERVAL] = 0;
 
-	var url = getRestletUrl();
-	gadgets.io.makeRequest(url, response, params);
+    var url = getRestletUrl();
+    gadgets.io.makeRequest(url, response, params);
 }
 
 function displayTaskList(data) {
-	var htmlContent = "";
+    var htmlContent = "";
 
-	//validation
-	var validation = data.data['workflowDirectiveValidation'];
-	var directive = data.translations['workflowDirectiveValidation'];
-	var i;
+    //validation
+    var validation = data.data['workflowDirectiveValidation'];
+    var directive = data.translations['workflowDirectiveValidation'];
+    var i;
 
-	if (validation) {
-		for (i = 0; i < validation.length; i++) {
-			htmlContent += mkRow(validation[i], i, directive);
-		}
-	}
-	validation = data.data['workflowDirectiveCheck'];
-	directive = data.translations['workflowDirectiveCheck'];
-	if (validation) {
-		for (i = 0; i < validation.length; i++) {
-			htmlContent += mkRow(validation[i], i, directive);
-		}
-	}
+    if (validation) {
+        for (i = 0; i < validation.length; i++) {
+            htmlContent += mkRow(validation[i], i, directive);
+        }
+    }
+    validation = data.data['workflowDirectiveCheck'];
+    directive = data.translations['workflowDirectiveCheck'];
+    if (validation) {
+        for (i = 0; i < validation.length; i++) {
+            htmlContent += mkRow(validation[i], i, directive);
+        }
+    }
 
-	validation = data.data['workflowDirectiveDiffusion'];
-	directive = data.translations['workflowDirectiveDiffusion'];
-	if (validation) {
-		for (i = 0; i < validation.length; i++) {
-			htmlContent += mkRow(validation[i], i, directive);
-		}
-	}
-	validation = data.data['workflowDirectiveOpinion'];
-	directive = data.translations['workflowDirectiveOpinion'];
-	if (validation) {
-		for (i = 0; i < validation.length; i++) {
-			htmlContent += mkRow(validation[i], i, directive);
-		}
-	}
+    validation = data.data['workflowDirectiveDiffusion'];
+    directive = data.translations['workflowDirectiveDiffusion'];
+    if (validation) {
+        for (i = 0; i < validation.length; i++) {
+            htmlContent += mkRow(validation[i], i, directive);
+        }
+    }
+    validation = data.data['workflowDirectiveOpinion'];
+    directive = data.translations['workflowDirectiveOpinion'];
+    if (validation) {
+        for (i = 0; i < validation.length; i++) {
+            htmlContent += mkRow(validation[i], i, directive);
+        }
+    }
 
-	validation = data.data['workflowDirectiveVerification'];
-	directive = data.translations['workflowDirectiveVerification'];
-	if (validation) {
-		for (i = 0; i < validation.length; i++) {
-			htmlContent += mkRow(validation[i], i, directive);
-		}
-	}
+    validation = data.data['workflowDirectiveVerification'];
+    directive = data.translations['workflowDirectiveVerification'];
+    if (validation) {
+        for (i = 0; i < validation.length; i++) {
+            htmlContent += mkRow(validation[i], i, directive);
+        }
+    }
 
-	document.getElementById("nxDocumentListData").innerHTML = htmlContent;
-	// page info
-	//alert("page info " + data.summary.pageNumber)
-	var pageInfoLabel = data.summary.pageNumber + 1;
-	pageInfoLabel += "/";
-	maxPage = data.summary.pages;
-	pageInfoLabel += maxPage + 1;
-	document.getElementById("nxDocumentListPage").innerHTML = pageInfoLabel;
+    document.getElementById("nxDocumentListData").innerHTML = htmlContent;
+    // page info
+    //alert("page info " + data.summary.pageNumber)
+    var pageInfoLabel = data.summary.pageNumber + 1;
+    pageInfoLabel += "/";
+    maxPage = data.summary.pages;
+    pageInfoLabel += maxPage + 1;
+    document.getElementById("nxDocumentListPage").innerHTML = pageInfoLabel;
 
-	// labels
-	var labelInfo = data.translations;
-	if (labelInfo != null && labelInfo != 'undefined') {
-		//alert("here");
-		document.getElementById("title").innerHTML = labelInfo['label.workflow.task.name'];
-		document.getElementById("modified").innerHTML = labelInfo['label.workflow.task.duedate'];
-		document.getElementById("creator").innerHTML = labelInfo['label.workflow.task.directive'];
-	}
+    // labels
+    var labelInfo = data.translations;
+    if (labelInfo != null && labelInfo != 'undefined') {
+        //alert("here");
+        document.getElementById("title").innerHTML = labelInfo['label.workflow.task.name'];
+        document.getElementById("modified").innerHTML = labelInfo['label.workflow.task.duedate'];
+        document.getElementById("creator").innerHTML = labelInfo['label.workflow.task.directive'];
+    }
 }
 
 function getDateForDisplay(datestr) {
-	try {
-		datestr = datestr.replace("-", "/").replace("-", "/");
-		var d = new Date(datestr);
-		var result = d.toLocaleDateString() + " "
-				+ d.toLocaleTimeString().substring(0, 5);
-		return result;
-	} catch (e) {
-		return datestr;
-	}
+    try {
+        datestr = datestr.replace("-", "/").replace("-", "/");
+        var d = new Date(datestr);
+        var result = d.toLocaleDateString() + " "
+                + d.toLocaleTimeString().substring(0, 5);
+        return result;
+    } catch (e) {
+        return datestr;
+    }
 }
 
 function getNuxeoClientSideUrl() {
-	return top.nxBaseUrl;
+    return top.nxBaseUrl;
 }
 function getImageBaseUrl() {
-	return getNuxeoClientSideUrl();
+    return getNuxeoClientSideUrl();
 }
 
 function getBaseUrl() {
-	return getNuxeoClientSideUrl();
+    return getNuxeoClientSideUrl();
 }
 
 function mkRow(dashBoardItem, i, directive) {
-	var htmlRow = "<tr class=\"";
-	if (i % 2 == 2) {
-		htmlRow += "dataRowEven";
-	} else {
-		htmlRow += "dataRowOdd";
-	}
-	htmlRow += "\">";
-	htmlRow += "<td class=\"iconColumn\">"
-	htmlRow += "<img alt=\"File\" src=\""
-	htmlRow += getImageBaseUrl();
-	htmlRow += "icons/file.gif";
-	htmlRow += "\"/>";
-	htmlRow += "</td><td><a target = \"_top\" title=\"";
-	var title = dashBoardItem.name;
-	if ((dashBoardItem.title != null) && (dashBoardItem.title != "")) {
-		title = dashBoardItem.title;
-	}
-	htmlRow += title;
-	htmlRow += "\" href=\"";
-	htmlRow += getBaseUrl();
-	htmlRow += dashBoardItem.link.substring(1);
-	htmlRow += "\" />";
-	htmlRow += title;
-	htmlRow += "</a></td><td class=\"iconColumn\"/>";
-	htmlRow += "<td>";
-	var dateToDisplay = null;
-	if ((dashBoardItem.dueDate != null) && (dashBoardItem.dueDate != "")) {
-		dateToDisplay = dashBoardItem.dueDate;
-	}
-	if (dateToDisplay != null) {
-		htmlRow += getDateForDisplay(dateToDisplay);
-	}
-	htmlRow += "</td>";
-	htmlRow += "<td>";
-	htmlRow += directive;
-	htmlRow += "</td>";
-	htmlRow += "<td class=\"iconColumn\"/>";
-	htmlRow += "</tr>";
-	return htmlRow;
+    var htmlRow = "<tr class=\"";
+    if (i % 2 == 2) {
+        htmlRow += "dataRowEven";
+    } else {
+        htmlRow += "dataRowOdd";
+    }
+    htmlRow += "\">";
+    htmlRow += "<td class=\"iconColumn\">"
+    htmlRow += "<img alt=\"File\" src=\""
+    htmlRow += getImageBaseUrl();
+    htmlRow += "icons/file.gif";
+    htmlRow += "\"/>";
+    htmlRow += "</td><td><a target = \"_top\" title=\"";
+    var title = dashBoardItem.name;
+    if ((dashBoardItem.title != null) && (dashBoardItem.title != "")) {
+        title = dashBoardItem.title;
+    }
+    htmlRow += title;
+    htmlRow += "\" href=\"";
+    htmlRow += getBaseUrl();
+    htmlRow += dashBoardItem.link.substring(1);
+    htmlRow += "\" />";
+    htmlRow += title;
+    htmlRow += "</a></td><td class=\"iconColumn\"/>";
+    htmlRow += "<td>";
+    var dateToDisplay = null;
+    if ((dashBoardItem.dueDate != null) && (dashBoardItem.dueDate != "")) {
+        dateToDisplay = dashBoardItem.dueDate;
+    }
+    if (dateToDisplay != null) {
+        htmlRow += getDateForDisplay(dateToDisplay);
+    }
+    htmlRow += "</td>";
+    htmlRow += "<td>";
+    htmlRow += directive;
+    htmlRow += "</td>";
+    htmlRow += "<td class=\"iconColumn\"/>";
+    htmlRow += "</tr>";
+    return htmlRow;
 }
 
 function response(obj) {
-	//var jsonObject = eval('(' + obj + ')');
-	//var data = jsonObject;
-	//alert("eval is "+eval(obj));
-	var jsonObject = obj.data;
-	//alert("howdy"+jsonObject);
+    //var jsonObject = eval('(' + obj + ')');
+    //var data = jsonObject;
+    //alert("eval is "+eval(obj));
+    var jsonObject = obj.data;
+    //alert("howdy"+jsonObject);
 
-	displayTaskList(jsonObject);
+    displayTaskList(jsonObject);
 };
 
 function readCookie(name) {
-	var nameEQ = name + "=";
-	var ca = document.cookie.split(';');
-	for ( var i = 0; i < ca.length; i++) {
-		var c = ca[i];
-		while (c.charAt(0) == ' ')
-			c = c.substring(1, c.length);
-		if (c.indexOf(nameEQ) == 0)
-			return c.substring(nameEQ.length, c.length);
-	}
-	return null;
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for ( var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ')
+            c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) == 0)
+            return c.substring(nameEQ.length, c.length);
+    }
+    return null;
 }
 
 function closeDiv(name) {
-	return "</div></div>";
+    return "</div></div>";
 }
 
 function getNuxeoServerSideUrl() {
-	return top.nxServerSideUrl;
+    return top.nxServerSideUrl;
 }
 
 function getNuxeoClientSideUrl() {
-	return top.nxBaseUrl;
+    return top.nxBaseUrl;
 }
 
 function getUserLang() {
-	return top.nxUserLang;
+    return top.nxUserLang;
 }
 
 function getRestletUrl() {
-	var url = getNuxeoServerSideUrl();
-	if (testMode) {
-		url = 'http://localhost:8080/';
-	}
-	url += "nuxeo/restAPI/workflowTasks/default?format=JSON";
-	var ts = new Date().getTime() + "" + Math.random() * 11
-	url += "&ts=" + ts;
+    var url = getNuxeoServerSideUrl();
+    if (testMode) {
+        url = 'http://localhost:8080/';
+    }
+    url += "nuxeo/restAPI/workflowTasks/default?format=JSON";
+    var ts = new Date().getTime() + "" + Math.random() * 11
+    url += "&ts=" + ts;
 
-	var lang = getUserLang();
-	if (lang != null && lang != "") {
-		url += "&lang=" + lang;
-	}
-	if (testMode) {
-		url += "&lang=en";
-	}
-	url += "&labels=";
-	//labels
-	url += "workflowDirectiveValidation,";
-	url += "workflowDirectiveOpinion,";
-	url += "workflowDirectiveVerification,";
-	url += "workflowDirectiveCheck,";
-	url += "workflowDirectiveDiffusion,";
-	url += "label.workflow.task.name,";
-	url += "label.workflow.task.duedate,";
-	url += "label.workflow.task.directive";
+    var lang = getUserLang();
+    if (lang != null && lang != "") {
+        url += "&lang=" + lang;
+    }
+    if (testMode) {
+        url += "&lang=en";
+    }
+    url += "&labels=";
+    //labels
+    url += "workflowDirectiveValidation,";
+    url += "workflowDirectiveOpinion,";
+    url += "workflowDirectiveVerification,";
+    url += "workflowDirectiveCheck,";
+    url += "workflowDirectiveDiffusion,";
+    url += "label.workflow.task.name,";
+    url += "label.workflow.task.duedate,";
+    url += "label.workflow.task.directive";
 
-	return url;
+    return url;
 }
 
 function testHandleJSONResponse(req) {
-	if (req.readyState == 4) {
-		var jsonObject = eval('(' + req.responseText + ')');
-		var data = jsonObject;
-		displayTaskList(data);
-	}
+    if (req.readyState == 4) {
+        var jsonObject = eval('(' + req.responseText + ')');
+        var data = jsonObject;
+        displayTaskList(data);
+    }
 }
 function testGetTaskList() {
-	var req = new XMLHttpRequest();
-	req.open("GET", getRestletUrl(), true);
-	req.onreadystatechange = function() {
-		testHandleJSONResponse(req)
-	};
-	req.send(null);
+    var req = new XMLHttpRequest();
+    req.open("GET", getRestletUrl(), true);
+    req.onreadystatechange = function() {
+        testHandleJSONResponse(req)
+    };
+    req.send(null);
 }
 
 function refresh() {
-	if (testMode) {
-		testGetTaskList();
-	} else {
-		requestTasks();
-	}
+    if (testMode) {
+        testGetTaskList();
+    } else {
+        requestTasks();
+    }
 }
 
 if (testMode) {
-	refresh();
+    refresh();
 } else {
-	gadgets.util.registerOnLoadHandler(refresh);
+    gadgets.util.registerOnLoadHandler(refresh);
 }
