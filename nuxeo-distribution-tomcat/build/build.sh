a798a10790f3a2c17ce9e64fe6d761c53ef3b498
Bogdan Stefanescu <bstefanescu@nuxeo.com>
2009-10-21 18:47:22 +0200

    NXP-4196: fixed repository deps

--- nuxeo-distribution-tomcat/build/build.sh
+++ nuxeo-distribution-tomcat/build/build.sh
@@ -2,7 +2,6 @@
 
 if [ ! -f nuxeo-build.jar ]; then
   echo "Downloading nuxeo distribution tools jar"
-  #mvn org.apache.maven.plugins:maven-nuxeo-plugin:1.0.12-SNAPSHOT:download -Dartifact=org.nuxeo.build:nuxeo-distribution-tools:0.1-SNAPSHOT:jar:all -Dfile=nuxeo-build.jar
   mvn nuxeo:download -Dartifact=org.nuxeo.build:nuxeo-distribution-tools:0.1:jar:all -Dfile=target/nuxeo-build.jar
 fi
 
