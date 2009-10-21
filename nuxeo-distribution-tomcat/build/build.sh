ec2a240a0e714528236587964216d3b1940bd27b
Bogdan Stefanescu <bstefanescu@nuxeo.com>
2009-10-21 18:19:46 +0200

    NXP-4196: fixing remote dependencies

--- nuxeo-distribution-tomcat/build/build.sh
+++ nuxeo-distribution-tomcat/build/build.sh
@@ -3,7 +3,7 @@
 if [ ! -f nuxeo-build.jar ]; then
   echo "Downloading nuxeo distribution tools jar"
   #mvn org.apache.maven.plugins:maven-nuxeo-plugin:1.0.12-SNAPSHOT:download -Dartifact=org.nuxeo.build:nuxeo-distribution-tools:0.1-SNAPSHOT:jar:all -Dfile=nuxeo-build.jar
-  mvn nuxeo:download -Dartifact=org.nuxeo.build:nuxeo-distribution-tools:0.1-SNAPSHOT:jar:all -Dfile=target/nuxeo-build.jar
+  mvn nuxeo:download -Dartifact=org.nuxeo.build:nuxeo-distribution-tools:0.1:jar:all -Dfile=target/nuxeo-build.jar
 fi
 
 java -Xrunjdwp:transport=dt_socket,address=8001,server=y,suspend=n -jar target/nuxeo-build.jar $@
