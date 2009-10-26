d06886a60f2285f66880eecbcb9734f278ea978d
Catalin Baican <cbaican@nuxeo.com>
2009-10-26 18:34:41 +0200

    DAM-94 - Simple asset delivery for download of asset.

--- nuxeo-dam-api/src/main/java/org/nuxeo/dam/api/Constants.java
+++ nuxeo-dam-api/src/main/java/org/nuxeo/dam/api/Constants.java
@@ -5,5 +5,5 @@ public interface Constants {
     String DAM_COMMON_SCHEMA = "dam_common";
     String DUBLINCORE_SCHEMA = "dublincore";
     String IMPORT_SET_TYPE = "ImportSet";
-    
+
 }
--- nuxeo-dam-webapp-core/src/main/java/org/nuxeo/dam/webapp/contentbrowser/DocumentActions.java
+++ nuxeo-dam-webapp-core/src/main/java/org/nuxeo/dam/webapp/contentbrowser/DocumentActions.java
@@ -3,7 +3,9 @@ package org.nuxeo.dam.webapp.contentbrowser;
 import java.io.IOException;
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
@@ -17,12 +19,19 @@ import org.jboss.seam.annotations.In;
 import org.jboss.seam.annotations.Name;
 import org.jboss.seam.annotations.Scope;
 import org.jboss.seam.annotations.remoting.WebRemote;
+import org.jboss.seam.contexts.Context;
+import org.jboss.seam.contexts.Contexts;
 import org.nuxeo.common.utils.StringUtils;
+import org.nuxeo.common.utils.URIUtils;
+import org.nuxeo.dam.webapp.PictureActions;
+import org.nuxeo.ecm.core.api.Blob;
 import org.nuxeo.ecm.core.api.ClientException;
 import org.nuxeo.ecm.core.api.CoreSession;
+import org.nuxeo.ecm.core.api.DocumentLocation;
 import org.nuxeo.ecm.core.api.DocumentModel;
 import org.nuxeo.ecm.core.api.DocumentModelList;
 import org.nuxeo.ecm.core.api.PagedDocumentsProvider;
+import org.nuxeo.ecm.core.api.impl.DocumentLocationImpl;
 import org.nuxeo.ecm.platform.actions.Action;
 import org.nuxeo.ecm.platform.preview.helper.PreviewHelper;
 import org.nuxeo.ecm.platform.ui.web.api.WebActions;
@@ -30,6 +39,12 @@ import org.nuxeo.ecm.platform.ui.web.auth.NXAuthConstants;
 import org.nuxeo.ecm.platform.ui.web.rest.api.URLPolicyService;
 import org.nuxeo.ecm.platform.ui.web.tag.fn.DocumentModelFunctions;
 import org.nuxeo.ecm.platform.ui.web.util.BaseURL;
+import org.nuxeo.ecm.platform.ui.web.util.ComponentUtils;
+import org.nuxeo.ecm.platform.url.DocumentViewImpl;
+import org.nuxeo.ecm.platform.url.api.DocumentView;
+import org.nuxeo.ecm.platform.url.codec.DocumentFileCodec;
+import org.nuxeo.ecm.platform.util.RepositoryLocation;
+import org.nuxeo.ecm.webapp.delegate.DocumentManagerBusinessDelegate;
 import org.nuxeo.ecm.webapp.documentsLists.DocumentsListsManager;
 import org.nuxeo.ecm.webapp.helpers.ResourcesAccessor;
 import org.nuxeo.ecm.webapp.pagination.ResultsProvidersCache;
@@ -42,6 +57,8 @@ public class DocumentActions implements Serializable {
 
     private static final Log log = LogFactory.getLog(DocumentActions.class);
 
+    protected static final long BIG_FILE_SIZE_LIMIT = 1024 * 1024 * 5;
+
     @In(create = true)
     protected transient ResultsProvidersCache resultsProvidersCache;
 
@@ -50,6 +67,12 @@ public class DocumentActions implements Serializable {
 
     @In(create = true, required = false)
     protected transient CoreSession documentManager;
+
+    @In
+    protected transient Context conversationContext;
+
+    @In(create = true)
+    protected PictureActions pictureActions;
 
     @In(create = true)
     protected transient WebActions webActions;
@@ -69,6 +92,8 @@ public class DocumentActions implements Serializable {
     protected String currentSelectionLink;
 
     protected String displayMode = "view";
+
+    protected String downloadSize = "Original:content";
 
     @WebRemote
     public String processSelectRow(String docRef, String providerName,
@@ -160,6 +185,8 @@ public class DocumentActions implements Serializable {
             webActions.setCurrentTabAction(currentAction);
             currentSelectionLink = currentAction.getLink();
         }
+
+        resetData();
     }
 
     public String getCurrentSelectionLink() {
@@ -234,66 +261,173 @@ public class DocumentActions implements Serializable {
         }
         return null;
     }
-    
+
     /**
-     * Takes in a DocumentModel, gets the 'title' from it, and crops
-     * it to a maximum of maxLength characters. If the Title is more than maxLength characters
-     * it will return the Beginning of the title, followed by 3 ellipses (...) 
-     * followed by the End of the title.
-     * 
-     * A minimum of 6 characters is needed before cropping takes effect.
-     * If you specify a maxLength of less than 5, it is ignored - in this case maxLength will be set to begin at 5.
+     * Takes in a DocumentModel, gets the 'title' from it, and crops it to a
+     * maximum of maxLength characters. If the Title is more than maxLength
+     * characters it will return the Beginning of the title, followed by 3
+     * ellipses (...) followed by the End of the title.
+     *
+     * A minimum of 6 characters is needed before cropping takes effect. If you
+     * specify a maxLength of less than 5, it is ignored - in this case
+     * maxLength will be set to begin at 5.
      *
      * @param DocumentModel document to extract the title from
-     * @param int maxLength the maximum length of the title before cropping will occur
-     * @return String with the cropped title restricted to maximum of maxLength characters
+     * @param int maxLength the maximum length of the title before cropping will
+     *        occur
+     * @return String with the cropped title restricted to maximum of maxLength
+     *         characters
      */
     public String getTitleCropped(DocumentModel document, int maxLength) {
-        
-    	String title = null;
-    	String beginningChars = null;
-    	int nbrBeginningChars = -1;
-    	String endChars = null;
-    	int nbrEndChars = -1;
-    	int nbrEllipses = 3;
-    	int minLength = 5;
-    	
-    	title = DocumentModelFunctions.titleOrId(document);
 
-    	int length = title.length();
-    	
-    	// a minimum of 5 characters needed before we crop
-    	if(length <= minLength) {
-    		return title;
-    	}
-    	
-    	// if maxLength is crazy, set it to a proper value
-    	if(maxLength <= minLength ) {
-    		maxLength = minLength;
-    	}
-    	
-    	if(length <= maxLength) {
-    		return title;
-    	}
+        String title = null;
+        String beginningChars = null;
+        int nbrBeginningChars = -1;
+        String endChars = null;
+        int nbrEndChars = -1;
+        int nbrEllipses = 3;
+        int minLength = 5;
 
-    	// at this point we should be ok to start cropping to our heart's content
-    	// length is more than maxLength characters: construct the new title
-    	
-    	// get the first (maxLength-3)/2 characters:
-    	if((maxLength-nbrEllipses)%2==0) {
-    		nbrBeginningChars = (maxLength-nbrEllipses)/2;
-    	} else {
-    		nbrBeginningChars = (maxLength-nbrEllipses)/2 + 1;
-    	}
-    	
-    	beginningChars = title.substring(0, nbrBeginningChars);
-    	// get the last n characters:
-    	nbrEndChars = maxLength - nbrBeginningChars - nbrEllipses;
-    	endChars = title.substring(length-nbrEndChars, length);
-    	
-    	String croppedTitle = beginningChars + "..." + endChars;
-    	return croppedTitle;
-    	
+        title = DocumentModelFunctions.titleOrId(document);
+
+        int length = title.length();
+
+        // a minimum of 5 characters needed before we crop
+        if (length <= minLength) {
+            return title;
+        }
+
+        // if maxLength is crazy, set it to a proper value
+        if (maxLength <= minLength) {
+            maxLength = minLength;
+        }
+
+        if (length <= maxLength) {
+            return title;
+        }
+
+        // at this point we should be ok to start cropping to our heart's
+        // content
+        // length is more than maxLength characters: construct the new title
+
+        // get the first (maxLength-3)/2 characters:
+        if ((maxLength - nbrEllipses) % 2 == 0) {
+            nbrBeginningChars = (maxLength - nbrEllipses) / 2;
+        } else {
+            nbrBeginningChars = (maxLength - nbrEllipses) / 2 + 1;
+        }
+
+        beginningChars = title.substring(0, nbrBeginningChars);
+        // get the last n characters:
+        nbrEndChars = maxLength - nbrBeginningChars - nbrEllipses;
+        endChars = title.substring(length - nbrEndChars, length);
+
+        String croppedTitle = beginningChars + "..." + endChars;
+        return croppedTitle;
+
+    }
+
+    public void download(DocumentView docView) throws ClientException {
+        if (docView != null) {
+            DocumentLocation docLoc = docView.getDocumentLocation();
+            if (documentManager == null) {
+                RepositoryLocation loc = new RepositoryLocation(
+                        docLoc.getServerName());
+                documentManager = getOrCreateDocumentManager(loc);
+            }
+            DocumentModel doc = documentManager.getDocument(docLoc.getDocRef());
+            if (doc != null) {
+                // get properties from document view
+                Blob blob = DocumentFileCodec.getBlob(doc, docView);
+                String filename = DocumentFileCodec.getFilename(doc, docView);
+                // download
+                FacesContext context = FacesContext.getCurrentInstance();
+                if (blob.getLength() > BIG_FILE_SIZE_LIMIT) {
+                    HttpServletResponse response = (HttpServletResponse) context.getExternalContext().getResponse();
+                    HttpServletRequest request = (HttpServletRequest) context.getExternalContext().getRequest();
+
+                    String bigDownloadURL = BaseURL.getBaseURL(request);
+                    bigDownloadURL += "nxbigfile" + "/";
+                    bigDownloadURL += doc.getRepositoryName() + "/";
+                    bigDownloadURL += doc.getRef().toString() + "/";
+                    bigDownloadURL += docView.getParameter(DocumentFileCodec.FILE_PROPERTY_PATH_KEY)
+                            + "/";
+                    bigDownloadURL += URIUtils.quoteURIPathComponent(filename,
+                            true);
+                    try {
+                        response.sendRedirect(bigDownloadURL);
+                    } catch (IOException e) {
+                        log.error(
+                                "Error while redirecting for big file downloader",
+                                e);
+                    }
+                } else {
+                    ComponentUtils.download(context, blob, filename);
+                }
+            }
+        }
+    }
+
+    protected CoreSession getOrCreateDocumentManager(
+            RepositoryLocation repositoryLocation) throws ClientException {
+        if (documentManager != null) {
+            return documentManager;
+        }
+        DocumentManagerBusinessDelegate documentManagerBD = (DocumentManagerBusinessDelegate) Contexts.lookupInStatefulContexts("documentManager");
+        if (documentManagerBD == null) {
+            // this is the first time we select the location, create a
+            // DocumentManagerBusinessDelegate instance
+            documentManagerBD = new DocumentManagerBusinessDelegate();
+            conversationContext.set("documentManager", documentManagerBD);
+        }
+        documentManager = documentManagerBD.getDocumentManager(repositoryLocation);
+        return documentManager;
+    }
+
+    public String downloadBlob() throws ClientException {
+        if (currentSelection != null) {
+            if (currentSelection.hasSchema("file")) {
+                DocumentLocation docLoc = new DocumentLocationImpl(
+                        currentSelection);
+                Map<String, String> params = new HashMap<String, String>();
+                params.put(DocumentFileCodec.FILE_PROPERTY_PATH_KEY,
+                        "file:content");
+                params.put(
+                        DocumentFileCodec.FILENAME_KEY,
+                        (String) currentSelection.getPropertyValue("file:filename"));
+                DocumentView docView = new DocumentViewImpl(docLoc, null,
+                        params);
+
+                download(docView);
+            }
+
+            if (currentSelection.hasSchema("picture")) {
+                DocumentLocation docLoc = new DocumentLocationImpl(
+                        currentSelection);
+                Map<String, String> params = new HashMap<String, String>();
+                params.put(DocumentFileCodec.FILE_PROPERTY_PATH_KEY,
+                        downloadSize);
+                DocumentView docView = new DocumentViewImpl(docLoc, null,
+                        params);
+
+                pictureActions.downloadPicture(docView);
+            }
+        }
+
+        return null;
+    }
+
+    private void resetData() {
+        // Data to reset on asset selection is changed
+        downloadSize = "Original:content";
+    }
+
+    public String getDownloadSize() {
+        return downloadSize;
+    }
+
+    public void setDownloadSize(String downloadSize) {
+        this.downloadSize = downloadSize;
     }
 
 }
