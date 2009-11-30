b8df5d4da8369a2491d4b0660ca9141274ad818e
Olivier Grisel <ogrisel@nuxeo.com>
2009-11-30 14:42:05 +0100

    various cleanups

--- nuxeo-dam-webapp-core/src/main/java/org/nuxeo/dam/webapp/BulkSelectActions.java
+++ nuxeo-dam-webapp-core/src/main/java/org/nuxeo/dam/webapp/BulkSelectActions.java
@@ -13,8 +13,6 @@ import java.util.Set;
 
 import javax.faces.application.FacesMessage;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.jboss.seam.annotations.In;
 import org.jboss.seam.annotations.Name;
 import org.jboss.seam.annotations.Scope;
@@ -29,7 +27,6 @@ import org.nuxeo.ecm.core.api.CoreSession;
 import org.nuxeo.ecm.core.api.DocumentModel;
 import org.nuxeo.ecm.core.api.DocumentRef;
 import org.nuxeo.ecm.core.api.security.SecurityConstants;
-import org.nuxeo.ecm.platform.ui.web.api.NavigationContext;
 import org.nuxeo.ecm.platform.ui.web.util.DocumentsListsUtils;
 import org.nuxeo.ecm.webapp.documentsLists.DocumentsListsManager;
 import org.nuxeo.ecm.webapp.helpers.EventNames;
@@ -41,42 +38,35 @@ import org.nuxeo.ecm.webapp.helpers.ResourcesAccessor;
 @Scope(CONVERSATION)
 @Name("bulkSelectActions")
 public class BulkSelectActions implements Serializable {
-    
+
     private static final long serialVersionUID = 1L;
+
     protected static final String CACHED_SELECTED_DOCUMENT_IDS = "cachedSelectedDocumentIds";
-    private static final Log log = LogFactory.getLog(BulkSelectActions.class);
-    
+
     @In(create = true, required = false)
-    private transient CoreSession documentManager;
-    
+    protected CoreSession documentManager;
+
     @In(create = true)
-    private transient DocumentsListsManager documentsListsManager;
-    
-    @In(create = true, required = false)
-    private transient NavigationContext navigationContext;
-    
+    protected DocumentsListsManager documentsListsManager;
+
     @In(create = true, required = false)
     protected FacesMessages facesMessages;
 
     @In(create = true)
-    // won't inject this because of seam problem after activation
-    // ::protected Map<String, String> messages;
     protected ResourcesAccessor resourcesAccessor;
- 
-    public String deleteSelection() throws ClientException {
+
+    public void deleteSelection() throws ClientException {
         if (!documentsListsManager.isWorkingListEmpty(DocumentsListsManager.CURRENT_DOCUMENT_SELECTION)) {
-            return deleteSelection(documentsListsManager.getWorkingList(DocumentsListsManager.CURRENT_DOCUMENT_SELECTION));
-        } else {
-            log.debug("No documents selection in context to process delete on...");
-            return null;
+            deleteSelection(documentsListsManager.getWorkingList(DocumentsListsManager.CURRENT_DOCUMENT_SELECTION));
         }
     }
-    
-    public String deleteSelection(List<DocumentModel> docsToDelete) throws ClientException {
+
+    public void deleteSelection(List<DocumentModel> docsToDelete)
+            throws ClientException {
         if (null != docsToDelete) {
-        
+
             List<DocumentModel> docsThatCanBeDeleted = filterDeleteListAccordingToPerms(docsToDelete);
-        
+
             // Keep only topmost documents (see NXP-1411)
             // This is not strictly necessary with Nuxeo Core >= 1.3.2
             Collections.sort(docsThatCanBeDeleted, new PathComparator());
@@ -89,7 +79,7 @@ public class BulkSelectActions implements Serializable {
                     previousPath = doc.getPath();
                 }
             }
-        
+
             // Three auxiliary collections deduced from rootDocsToDelete:
             // references, paths, and references of parents.
             // Computed before actual removal for robustness
@@ -100,35 +90,14 @@ public class BulkSelectActions implements Serializable {
             }
             Set<DocumentRef> parentsRefs = new HashSet<DocumentRef>();
             parentsRefs.addAll(DocumentsListsUtils.getParentRefFromDocumentList(rootDocsToDelete));
-        
-            // Treat the cases where the navigation context is under one of the
-            // deleted documents by climbing up.
-            // Target document computation is before removal for robustness.
-            DocumentModel targetContext = navigationContext.getCurrentDocument();
-            while (underOneOf(targetContext.getPath(), paths)) {
-                targetContext = documentManager.getParentDocument(targetContext.getRef());
-            }
-        
+
             // remove from all lists
             documentsListsManager.removeFromAllLists(docsThatCanBeDeleted);
-        
-            // Don't implement trash management in DAM for now
-            /*
-            if (trashManager.isTrashManagementEnabled()) {
-                // Change state
-                moveDocumentsToTrash(docsThatCanBeDeleted);
-            } else {
-                // Trash bin is not enabled
-                documentManager.removeDocuments(references.toArray(new DocumentRef[references.size()]));
-            }
-            */
+
+            // TODO: factorize trash management from Nuxeo DM
             documentManager.removeDocuments(references.toArray(new DocumentRef[references.size()]));
-        
             documentManager.save();
-        
-            // Update context if needed
-            navigationContext.setCurrentDocument(targetContext);
-        
+
             // Notify parents
             for (DocumentRef parentRef : parentsRefs) {
                 DocumentModel parent = documentManager.getDocument(parentRef);
@@ -137,7 +106,7 @@ public class BulkSelectActions implements Serializable {
                             EventNames.DOCUMENT_CHILDREN_CHANGED, parent);
                 }
             }
-        
+
             // User feedback
             Object[] params = { references.size() };
             FacesMessage message = FacesMessages.createFacesMessage(
@@ -145,15 +114,9 @@ public class BulkSelectActions implements Serializable {
                             + resourcesAccessor.getMessages().get(
                                     "n_deleted_docs"), params);
             facesMessages.add(message);
-            log.debug("documents deleted...");
-        } else {
-            log.debug("nothing to delete...");
         }
-        
-        // return computeOutcome(DELETE_OUTCOME);
-        return null;
     }
-    
+
     private List<DocumentModel> filterDeleteListAccordingToPerms(
             List<DocumentModel> docsToDelete) throws ClientException {
         // first filter on parents
@@ -168,21 +131,7 @@ public class BulkSelectActions implements Serializable {
             if (documentManager.hasPermission(docToDelete.getRef(),
                     SecurityConstants.REMOVE)) {
 
-                // Dont implement the document locking stuff in DAM yet
-                /*
-                // Check if document is locker
-                if (docToDelete.isLocked()) {
-                    String locker = lockActions.getLockDetails(docToDelete).get(
-                            LockActions.LOCKER);
-                    if (currentUser.getName().equals(locker)) {
-                        docsThatCanBeDeleted.add(docToDelete);
-                    } else {
-                        lockedDocs += 1;
-                    }
-                } else {
-                    docsThatCanBeDeleted.add(docToDelete);
-                }
-                */
+                // TODO: factorize locked document filtering from DAM
                 docsThatCanBeDeleted.add(docToDelete);
             } else {
                 forbiddenDocs += 1;
@@ -206,7 +155,6 @@ public class BulkSelectActions implements Serializable {
         return docsThatCanBeDeleted;
     }
 
-    
     private List<DocumentModel> filterDeleteListAccordingToPermsOnParents(
             List<DocumentModel> docsToDelete) throws ClientException {
 
@@ -225,34 +173,24 @@ public class BulkSelectActions implements Serializable {
         }
         return docsThatCanBeDeleted;
     }
-    
-    private static boolean underOneOf(Path testedPath, Set<Path> paths) {
-        for (Path path : paths) {
-            if (path.isPrefixOf(testedPath)) {
-                return true;
-            }
-        }
-        return false;
-    }
-    
-    
-    
+
     private static class PathComparator implements Comparator<DocumentModel>,
-    Serializable {
+            Serializable {
 
         private static final long serialVersionUID = -6449747704324789701L;
-        
+
         public int compare(DocumentModel o1, DocumentModel o2) {
             return o1.getPathAsString().compareTo(o2.getPathAsString());
         }
-        
-        }
+
+    }
+
     /**
      * Tests if a document is in the working list of selected documents.
      * 
      * @param String docId The DocumentRef of the document
-     * @param String lName The name of the working list of selected documents.
-     *            If null, the default list will be used.
+     * @param String listName The name of the working list of selected
+     *            documents. If null, the default list will be used.
      * @return boolean true if the document is in the list, false if it isn't.
      */
     @SuppressWarnings("unchecked")
@@ -280,48 +218,40 @@ public class BulkSelectActions implements Serializable {
         }
         return selectedIds.contains(docId);
     }
-    
+
     /**
      * Clears the working list of selected documents.
      * 
      * @param String lName The name of the working list of selected documents.
      *            If null, the default list will be used.
-     * @return void 
      */
-    @SuppressWarnings("unchecked")
     @WebRemote
     public void clearWorkingList(String listName) {
 
         String lName = (listName == null) ? DocumentsListsManager.CURRENT_DOCUMENT_SELECTION
                 : listName;
-        
 
         List<DocumentModel> selectedDocumentsList = documentsListsManager.getWorkingList(lName);
         selectedDocumentsList.clear();
-        System.err.println("cleared working list");
     }
-    
+
+    // TODO: move this API to documentsListsManager directly
     /**
      * Tests whether the current working list of selected documents is empty.
      * 
-     * @param String lName The name of the working list of selected documents.
+     * @param String listName The name of the working list of selected documents.
      *            If null, the default list will be used.
-     * @return boolean true if empty, false otherwise 
+     * @return boolean true if empty, false otherwise
      */
-    @SuppressWarnings("unchecked")
     public boolean getIsCurrentWorkingListEmpty(String listName) {
 
         String lName = (listName == null) ? DocumentsListsManager.CURRENT_DOCUMENT_SELECTION
                 : listName;
-        
-
         List<DocumentModel> selectedDocumentsList = documentsListsManager.getWorkingList(lName);
-        if(selectedDocumentsList == null) {
+        if (selectedDocumentsList == null) {
             return true;
         }
-        
         return selectedDocumentsList.isEmpty();
-      
     }
 
 }
