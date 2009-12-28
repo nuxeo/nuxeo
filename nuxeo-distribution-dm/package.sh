48c0302fcca9909c9f85f40f53eab85dfb59a687
Julien Carsique <jcarsique@nuxeo.com>
2009-12-28 16:41:44 +0100

    NXP-4472 - Build all artifacts with same name but various classifiers during the same Maven execution

--- nuxeo-distribution-dm/package.sh
+++ nuxeo-distribution-dm/package.sh
@@ -4,24 +4,14 @@ ASSEMBLY=$1
 shift;
 
 case "$ASSEMBLY" in
-    nuxeo)
-        echo "building standard nuxeo.ear"
-        mvn $MVN_OPTS package $@
-  ;;
-    nuxeo-2parts)
-        echo "building 2 JVMs nuxeo distrib"
-        mvn -Dnuxeo.ear.assembly=nuxeo-platform-stateful $MVN_OPTS install package $@
-        mvn -Dnuxeo.ear.assembly=nuxeo-web-stateless $MVN_OPTS package -N $@
-  ;;
     help)
-        echo "Usage: $0 {help|<target config>} [-Pmaven_profile]"
-        echo "target config can be :"
-        echo "nuxeo : building standard nuxeo.ear"
-        echo "nuxeo-2parts : building 2 ears for stateful (platform) and stateless parts (web)"
+        echo "Usage: $0 [help] [-Pmaven_profile]"
+        echo "maven_profile can be : derby, mysql, postgresql, oracle, h2, jcr, jcr-pgsql"
+        exit 1
         ;;
     *)
-        echo "Usage: $0 {help|nuxeo|nuxeo-2parts} [-Pmaven_profile]"
-        exit 1
+        echo "building standard nuxeo.ear"
+        mvn $MVN_OPTS package $@
         ;;
 esac
 
