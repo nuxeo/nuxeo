82ecf82b660f00229dc4dc065654c7edd4cf2af1
Thomas Roger <troger@nuxeo.com>
2010-01-11 21:24:14 +0100

    Update jetty functionnal tests script

--- build-and-test-jetty.sh
+++ build-and-test-jetty.sh
@@ -6,14 +6,14 @@ ant -f ft-build.xml make-jetty-distribution -Dmvn.profiles=$MAVEN_PROFILES || ex
 # Start Jetty
 (cd nuxeo-dam-distribution/nuxeo-dam-distribution-jetty/target && unzip nuxeo-dam-distribution-jetty-*.zip && rm *.zip && mv nuxeo-dam-distribution-jetty-* nuxeo-dam-jetty) || exit 1
 chmod +x nuxeo-dam-distribution/nuxeo-dam-distribution-jetty/target/nuxeo-dam-jetty/*.sh || exit 1
-(cd ./nuxeo-dam-distribution/nuxeo-dam-distribution-jetty/target/nuxeo-dam-jetty/ && nxserverctl.sh start) || exit 1
+(cd ./nuxeo-dam-distribution/nuxeo-dam-distribution-jetty/target/nuxeo-dam-jetty/ && ./nxserverctl.sh start) || exit 1
 
 # Run selenium tests
 HIDE_FF=true ./nuxeo-dam-distribution/nuxeo-dam-ear/ftest/selenium/run.sh
 ret1=$?
 
 # Stop Jetty
-(cd ./nuxeo-dam-distribution/nuxeo-dam-distribution-jetty/target/nuxeo-dam-jetty/ && nxserverctl.sh stop) || exit 1
+(cd ./nuxeo-dam-distribution/nuxeo-dam-distribution-jetty/target/nuxeo-dam-jetty/ && ./nxserverctl.sh stop) || exit 1
 
 # Exit if some tests failed
 [ $ret1 -eq 0 ] || exit 9
