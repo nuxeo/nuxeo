8defab71dc097751ede63e76445b188a4f3158e4
Guillaume Cusnieux <guillaume.cusnieux@leroymerlin.fr>
2010-01-14 15:25:26 +0100

    NXP-4541 bug gadget photo

--- nuxeo-opensocial/nuxeo-opensocial-gadgets/src/main/resources/gadget/photo/photo.js
+++ nuxeo-opensocial/nuxeo-opensocial-gadgets/src/main/resources/gadget/photo/photo.js
@@ -41,14 +41,9 @@ jQuery(document).ready(function(){
     gadgets.window.adjustHeight();
   });
 
-  if(prefs.getString("id")!=""){
-    loadImage(prefs.getString("id"));
-  } else {
     gadgets.nuxeo.getGadgetId(function(id) {
-      prefs.set("id",id);
+    	loadImage(id);
     });
-  }
-
   });
 }
 
@@ -87,7 +82,6 @@ function savePrefs(){
 };
 
 function loadImage(id){
-
   var actionUrl = [action,id].join("");
   var photoUrl = [url,id,'?junk=',Math.random()].join("");
   jQuery("#formUpload").attr("action", actionUrl);
@@ -100,21 +94,12 @@ function loadImage(id){
         showImage();
     },
     success : function(data, textStatus) {
-
-      /*<a id="link" href="#" target="_tab" style="display:none;">
-        <img style="border:0;" id="photo" src="">
-      </a>
-      */
-        if (_isSet(prefs.getString("link"))){
+        if (_isSet(prefs.getString("link")))
           var imgContainer = jQuery("<a id=\"link\" href=\""+prefs.getString("link")+"\" target=\"_tab\" ><img style=\"border:0;\" id=\"photo\" src=\"\"></a>");
-        }
         else
-        {
           var imgContainer = jQuery("<img style=\"border:0;\" id=\"photo\" src=\"\">");
-        }
 
         jQuery("#pictureContainer").append(imgContainer);
-
         jQuery("#photo").attr("src", photoUrl);
         jQuery("#photo").width("100%");
         showImage();
