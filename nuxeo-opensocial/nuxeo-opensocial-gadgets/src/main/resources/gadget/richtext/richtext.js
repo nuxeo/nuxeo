c8b1abb87cafda91aa6b494f09c33ac2d808e726
Guillaume Cusnieux <guillaume.cusnieux@leroymerlin.fr>
2010-01-14 17:19:33 +0100

    NXP-4511 save work richtext gadget

--- nuxeo-opensocial/nuxeo-opensocial-gadgets/src/main/resources/gadget/richtext/richtext.js
+++ nuxeo-opensocial/nuxeo-opensocial-gadgets/src/main/resources/gadget/richtext/richtext.js
@@ -1,46 +1,43 @@
 function launchGadget(){
 jQuery(document).ready(function(){
 	jQuery('#show').click(function(){
-		var callback = function(){
-			jQuery('#show').hide();
-			jQuery('#form').show();
-			setWidthAndBindEvents();
-			gadgets.window.adjustHeight();
-		};
-		gadgets.nuxeo.maximizeGadget("edit",callback);
+		jQuery('#show').hide();
+		jQuery('#form').show();
+		gadgets.window.adjustHeight();
 	});
-	
-	
-});
-};
-
-
-function launchEditGadget(){
-jQuery(document).ready(function(){
-	
-	new nicEditor({iconsPath : '/nuxeo/site/gadgets/richtext/nicEditorIcons.gif', width : '500px'}).panelInstance('richtext');
 	
 	jQuery('#hide').click(function(){
-		var callback = function(){
-			jQuery('#form').hide();
-			jQuery('#show').show();
-			gadgets.window.adjustHeight();
-		};
-		gadgets.nuxeo.minimizeGadget(callback);
+		jQuery('#form').hide();
+		jQuery('#show').show();
+		gadgets.window.adjustHeight();
 	});
+	
+	new nicEditor({iconsPath : '/nuxeo/site/gadgets/richtext/nicEditorIcons.gif'}).panelInstance('richtext');
+	
+	setWidthAndBindEvents();
+	jQuery('#loader').remove();
+	gadgets.window.adjustHeight();
 	
 });
 };
+
 
 var firstTime = true;
 function setWidthAndBindEvents(){
 	if(firstTime){
 		var width = gadgets.window.getViewportDimensions().width;
 		var area = jQuery('#richtext').prev();
+		area.css("background-color","white");
 		area.width(width);
-		jQuery(area).prev().width(width);
-		jQuery(area).keydown(function(){
+		jQuery(area).keydown(function(e){
+			var keycode = (e.keyCode ? e.keyCode : (e.which ? e.which : e.charCode));
+ 			if (keycode == 13 || keycode == 8) 
+				gadgets.window.adjustHeight();
+      		return true;
 		});
+		jQuery('.nicEdit-main').width("100%");
+		var prev = jQuery(area).prev();
+		prev.width(width);
 		firstTime = false;
 	}
 };
