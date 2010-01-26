e976e687e38bcb09bc05f4878f3d363e6145fb19
Benjamin JALON <bjalon@nuxeo.com>
2010-01-26 10:14:48 +0100

    DAM-422: add user deletion

--- nuxeo-dam-webapp-core/src/main/java/org/nuxeo/dam/platform/security/DamUserManagerActions.java
+++ nuxeo-dam-webapp-core/src/main/java/org/nuxeo/dam/platform/security/DamUserManagerActions.java
@@ -6,8 +6,10 @@ import static org.jboss.seam.annotations.Install.APPLICATION;
 import org.jboss.seam.annotations.Install;
 import org.jboss.seam.annotations.Name;
 import org.jboss.seam.annotations.Scope;
+import org.jboss.seam.annotations.web.RequestParameter;
 import org.nuxeo.ecm.core.api.ClientException;
 import org.nuxeo.ecm.core.api.DocumentModel;
+import org.nuxeo.ecm.platform.forms.layout.api.BuiltinModes;
 import org.nuxeo.ecm.webapp.security.UserManagerActionsBean;
 
 @Name("userManagerActions")
@@ -16,15 +18,41 @@ import org.nuxeo.ecm.webapp.security.UserManagerActionsBean;
 public class DamUserManagerActions extends UserManagerActionsBean {
 
     private static final long serialVersionUID = 1L;
+    
+    protected String displayMode = BuiltinModes.VIEW;
+    
+    @RequestParameter("userId")
+    protected String selectedUserId;
 
-    public String deleteUser(DocumentModel userModel) throws ClientException {
+
+    public String deleteUser() throws ClientException {
+        if (selectedUserId == null) {
+            return super.deleteUser();
+        }
+        
         try {
-            userManager.deleteUser(userModel);
-            // refresh users and groups list
+            userManager.deleteUser(selectedUserId);
+
             resetUsers();
-            return null;
+            return viewUsers();
         } catch (Exception t) {
             throw ClientException.wrap(t);
+        }
+    }
+    
+    public void setSelectedUser(DocumentModel selectedUser) {
+        this.selectedUser = selectedUser;
+    }
+
+    public String getDisplayMode() {
+        return displayMode;
+    }
+
+    public void toggleDisplayMode() {
+        if (BuiltinModes.VIEW.equals(displayMode)) {
+            displayMode = BuiltinModes.EDIT;
+        } else {
+            displayMode = BuiltinModes.VIEW;
         }
     }
 
