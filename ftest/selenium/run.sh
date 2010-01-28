bbafb5303402048213e233920c4cb5ed59491930
Julien Carsique <jcarsique@nuxeo.com>
2010-01-28 18:37:58 +0100

    NXP-4645 - Improve selenium scripts used for integration tests avoiding building downloadable distributions

--- ftest/selenium/run.sh
+++ ftest/selenium/run.sh
@@ -13,19 +13,22 @@ URL=${URL:-http://localhost:8080/nuxeo/}
 FIREFOX_CONF="*firefox"
 USER_EXTENSIONS="$HERE/user-extensions.js"
 
-# Download selenium-server.jar
-(cd $HERE; mvn dependency:copy) || exit 1
+JBOSS_HOME=$1
+OUTPUT=$2
+
+echo "start jboss"
+$JBOSS_HOME/bin/jbossctl start || exit 1
 
 # Build command line
-CMD="java -jar selenium-server.jar -log log.txt -port 14440 -timeout 7200 \
+CMD="java -jar $OUTPUT/selenium-server.jar -log log.txt -port 14440 -timeout 7200 \
       -htmlSuite "*chrome" $URL "
 if [ ! -z $HIDE_FF ]; then
     CMD="xvfb-run $CMD"
 fi
-CMD_END="-firefoxProfileTemplate ffprofile -userExtensions user-extensions.js"
+CMD_END="-firefoxProfileTemplate $HERE/ffprofile -userExtensions $HERE/user-extensions.js"
 
 # Clean old results
-rm -rf $HERE/result-*.html
+rm -rf $OUTPUT/result-*.html
 
 cd $HERE
 # Update path in user-extensions.js
@@ -35,11 +38,12 @@ sed "s,/path/to/project-ear/ftest/selenium,$PWD,g" < $USER_EXTENSIONS.sample > $
 # Update url in profile
 sed "s,\(capability.principal.codebase.p0.id...\).*$,\1\"$URL\");,g" < ffprofile/prefs.js.sample > ffprofile/prefs.js
 
+cd -
 exit_code=0
 # Launch suites
 for suite in $SUITES; do
     echo "### [INFO] Running test suite $suite ..."
-    $CMD "$PWD/tests/$suite.html" "$PWD/result-$suite.html" $CMD_END
+    $CMD "$HERE/tests/$suite.html" "$OUTPUT/result-$suite.html" $CMD_END
     if [ $? != 0 ]; then
         echo "### [ERROR] $suite TEST FAILURE"
         exit_code=9
@@ -55,5 +59,8 @@ if [ $exit_code != 0 ]; then
 else
     echo "### [INFO] TESTS SUCCESSFUL"
 fi
+
+echo "stop jboss"
+$JBOSS_HOME/bin/jbossctl stop || exit 1
 
 exit $exit_code
