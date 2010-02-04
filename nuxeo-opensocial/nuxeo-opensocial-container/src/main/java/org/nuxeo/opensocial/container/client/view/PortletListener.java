27c432798044a74a59db44489656a355030dc089
Guillaume Cusnieux <guillaume.cusnieux@leroymerlin.fr>
2010-02-04 18:11:13 +0100

    NXP-4589 Fixed several bugs

--- nuxeo-opensocial/nuxeo-opensocial-container/src/main/java/org/nuxeo/opensocial/container/client/view/PortletListener.java
+++ nuxeo-opensocial/nuxeo-opensocial-container/src/main/java/org/nuxeo/opensocial/container/client/view/PortletListener.java
@@ -18,7 +18,6 @@
 package org.nuxeo.opensocial.container.client.view;
 
 import org.nuxeo.opensocial.container.client.ContainerEntryPoint;
-import org.nuxeo.opensocial.container.client.JsLibrary;
 import org.nuxeo.opensocial.container.client.bean.GadgetBean;
 
 import com.gwtext.client.widgets.Panel;
