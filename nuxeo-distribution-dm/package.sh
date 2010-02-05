304bb96177ef54bb8230a03eb0e5e807e0e2d742
Julien Carsique <jcarsique@nuxeo.com>
2010-02-05 18:57:17 +0100

    NXP-4677 - Add a profile for building only nuxeo-distribution-dm stateful and stateless EARs

--- nuxeo-distribution-dm/package.sh
+++ nuxeo-distribution-dm/package.sh
@@ -9,6 +9,10 @@ case "$ASSEMBLY" in
         echo "maven_profile can be : derby, mysql, postgresql, oracle, h2, jcr, jcr-pgsql"
         exit 1
         ;;
+    nuxeo-2parts)
+        echo "building nuxeo two parts (stateless/stateful)"
+        mvn $MVN_OPTS clean package -Pnuxeo-2parts
+        ;;
     *)
         echo "building standard nuxeo.ear"
         mvn $MVN_OPTS package $@
