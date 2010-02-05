d3c57d393aefb76653bcdbc0e530cf1ca621cfc5
Olivier Grisel <ogrisel@nuxeo.com>
2010-02-05 11:39:30 +0100

    added missing export DISPLAY statement for the HIDE_FF mode

--- nuxeo-distribution-dm/ftest/selenium/run.sh
+++ nuxeo-distribution-dm/ftest/selenium/run.sh
@@ -18,7 +18,9 @@ URL=${URL:-http://localhost:8080/nuxeo/}
 CMD="java -jar selenium-server.jar -port 14440 -timeout 7200 \
       -htmlSuite "*chrome" $URL "
 if [ ! -z $HIDE_FF ]; then
-    DISPLAY=${DISPLAY:-":1"}
+    # we assume that there is a long running xvfb-run process running and
+    # listening on display :1
+    export DISPLAY=${DISPLAY:-":1"}
 #    CMD="xvfb-run -a $CMD"
 fi
 CMD_END="-firefoxProfileTemplate ffprofile -userExtensions user-extensions.js"
