d51000c6660ac15f875e5f493513bfc1c94108b0
Damien Metzler <damien.metzler@leroymerlin.fr>
2010-02-24 09:56:51 +0100

    NXP-4692 Reintegrate contextPath rewriting in gadgets

--- nuxeo-opensocial/nuxeo-opensocial-server/src/main/java/org/nuxeo/opensocial/shindig/gadgets/NXGadgetModule.java
+++ nuxeo-opensocial/nuxeo-opensocial-server/src/main/java/org/nuxeo/opensocial/shindig/gadgets/NXGadgetModule.java
@@ -1,6 +1,8 @@
 package org.nuxeo.opensocial.shindig.gadgets;
 
+import org.apache.shindig.gadgets.rewrite.ProxyingLinkRewriterFactory;
 import org.apache.shindig.gadgets.servlet.MakeRequestHandler;
+import org.nuxeo.opensocial.shindig.gadgets.rewrite.NXProxyingLinkRewriterFactory;
 
 import com.google.inject.AbstractModule;
 import com.google.inject.Scopes;
@@ -8,10 +10,13 @@ import com.google.inject.Scopes;
 /** @author <a href="mailto:troger@nuxeo.com">Thomas Roger</a> */
 public class NXGadgetModule extends AbstractModule {
 
-    @Override
-    protected void configure() {
-        bind(MakeRequestHandler.class).to(NXMakeRequestHandler.class).in(
-                Scopes.SINGLETON);
-    }
+  @Override
+  protected void configure() {
+    bind(MakeRequestHandler.class).to(NXMakeRequestHandler.class)
+        .in(Scopes.SINGLETON);
+    bind(ProxyingLinkRewriterFactory.class).to(
+        NXProxyingLinkRewriterFactory.class)
+        .in(Scopes.SINGLETON);
+  }
 
 }
