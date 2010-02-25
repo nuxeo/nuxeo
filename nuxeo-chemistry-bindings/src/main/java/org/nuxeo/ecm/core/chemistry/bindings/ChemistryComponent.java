13a156e06ff2e84f125dc42a4185116775279961
Florent Guillaume <fg@nuxeo.com>
2010-02-25 18:43:10 +0100

    Reorganize SOAP code (most moved to Chemistry); SOAP auth bridge to Nuxeo; merge JAX-WS and JAX-RS init in single module

--- nuxeo-chemistry-bindings/src/main/java/org/nuxeo/ecm/core/chemistry/bindings/ChemistryComponent.java
+++ nuxeo-chemistry-bindings/src/main/java/org/nuxeo/ecm/core/chemistry/bindings/ChemistryComponent.java
@@ -1,5 +1,5 @@
 /*
- * (C) Copyright 2009 Nuxeo SA (http://nuxeo.com/) and contributors.
+ * (C) Copyright 2009-2010 Nuxeo SA (http://nuxeo.com/) and contributors.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the GNU Lesser General Public License
@@ -14,16 +14,22 @@
  * Contributors:
  *     Florent Guillaume
  */
-package org.nuxeo.ecm.core.chemistry.jaxrs;
+package org.nuxeo.ecm.core.chemistry.bindings;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.apache.chemistry.Repository;
 import org.apache.chemistry.RepositoryManager;
 import org.apache.chemistry.RepositoryService;
 import org.apache.chemistry.atompub.server.jaxrs.AbderaResource;
 import org.apache.chemistry.atompub.server.jaxrs.AbderaResponseProvider;
 import org.apache.chemistry.atompub.server.jaxrs.AbderaResource.PathMunger;
 import org.apache.chemistry.impl.simple.SimpleRepositoryService;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.nuxeo.ecm.core.chemistry.impl.NuxeoRepository;
 import org.nuxeo.ecm.webengine.WebEngine;
 import org.nuxeo.ecm.webengine.model.WebContext;
@@ -33,12 +39,12 @@ import org.nuxeo.runtime.model.DefaultComponent;
 
 /**
  * Nuxeo Runtime Component whose activation is used to register things not
- * available through extension points: additional JAX-RS Providers, repository
+ * available through extension points: additional JAX-RS Providers, repositories
  * for Chemistry.
- *
- * @author Florent Guillaume
  */
-public class JaxrsNuxeoComponent extends DefaultComponent {
+public class ChemistryComponent extends DefaultComponent {
+
+    private static final Log log = LogFactory.getLog(ChemistryComponent.class);
 
     protected RepositoryService repositoryService;
 
@@ -48,14 +54,6 @@ public class JaxrsNuxeoComponent extends DefaultComponent {
         WebEngine engine = Framework.getLocalService(WebEngine.class);
         engine.getRegistry().addMessageBodyWriter(new AbderaResponseProvider());
 
-        // register the default repository with the RepositoryManager
-        RepositoryManager rm = RepositoryManager.getInstance();
-        if (rm.getRepository("default") == null) {
-            repositoryService = new SimpleRepositoryService(
-                    new NuxeoRepository("default"));
-            RepositoryManager.getInstance().registerService(repositoryService);
-        }
-
         AbderaResource.pathMunger = new WebEnginePathMunger();
 
         // We have to set this so that a %2F is allowed in a URL. This is needed
@@ -63,18 +61,62 @@ public class JaxrsNuxeoComponent extends DefaultComponent {
         System.setProperty(
                 "org.apache.tomcat.util.buf.UDecoder.ALLOW_ENCODED_SLASH",
                 "true");
+
+        // use an activator to delay registering repositories until the first
+        // call to Chemistry is made, to be sure all Nuxeo repositories have
+        // been initialized
+        RepositoryManager.getInstance().registerActivator(new Runnable() {
+            public void run() {
+                try {
+                    registerRepositories();
+                } catch (Exception e) {
+                    log.error("Cannot register repositories with Chemistry", e);
+                }
+            }
+        });
     }
 
     @Override
     public void deactivate(ComponentContext context) throws Exception {
-        if (repositoryService != null) {
-            RepositoryManager.getInstance().unregisterService(repositoryService);
-            repositoryService = null;
-        }
+        unregisterRepositories();
     }
 
     /**
-     * Munges the request path according to WebEngine rules.
+     * Registers all the Nuxeo repositories with Chemistry.
+     */
+    protected void registerRepositories() throws Exception {
+        org.nuxeo.ecm.core.api.repository.RepositoryManager mgr = Framework.getService(org.nuxeo.ecm.core.api.repository.RepositoryManager.class);
+        if (mgr == null) {
+            throw new RuntimeException("Cannot lookup Nuxeo RepositoryManager");
+        }
+        List<Repository> repositories = new ArrayList<Repository>();
+        for (org.nuxeo.ecm.core.api.repository.Repository nxrepo : mgr.getRepositories()) {
+            NuxeoRepository repo = new NuxeoRepository(nxrepo.getName());
+            if ("default".equals(repo.getName())) {
+                // put "default" repository first (SimpleRepositoryService
+                // convention)
+                repositories.add(0, repo);
+            } else {
+                repositories.add(repo);
+            }
+        }
+        repositoryService = new SimpleRepositoryService(repositories);
+        RepositoryManager.getInstance().registerService(repositoryService);
+    }
+
+    /**
+     * Unregisters the Nuxeo repositories from Chemistry.
+     */
+    protected void unregisterRepositories() {
+        if (repositoryService == null) {
+            return;
+        }
+        RepositoryManager.getInstance().unregisterService(repositoryService);
+        repositoryService = null;
+    }
+
+    /**
+     * Munges the AtomPub request path according to WebEngine rules.
      * <p>
      * Uses the Nuxeo-Webengine-Base-Path header to provide a fake context &
      * servlet path.
