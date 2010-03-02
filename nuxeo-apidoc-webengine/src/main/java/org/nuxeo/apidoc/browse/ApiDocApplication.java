ba91d0b0c1951d2a2f2a6f89fea90345f1848fcf
Bogdan Stefanescu <bstefanescu@nuxeo.com>
2010-03-02 14:00:01 +0100

    message body writer has now per requests scope

--- nuxeo-apidoc-webengine/src/main/java/org/nuxeo/apidoc/browse/ApiDocApplication.java
+++ nuxeo-apidoc-webengine/src/main/java/org/nuxeo/apidoc/browse/ApiDocApplication.java
@@ -1,6 +1,5 @@
 package org.nuxeo.apidoc.browse;
 
-import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -9,11 +8,11 @@ import javax.ws.rs.core.Application;
 public class ApiDocApplication extends Application {
 
     @Override
-    public Set<Object> getSingletons() {
-        return new HashSet<Object>(Arrays.asList(
-                new DocumentationItemReader()
-                ));
+    public Set<Class<?>> getClasses() {
+        HashSet<Class<?>> result = new HashSet<Class<?>>();
+        result.add(DocumentationItemReader.class);
+        return result;
     }
-
+    
 
 }
