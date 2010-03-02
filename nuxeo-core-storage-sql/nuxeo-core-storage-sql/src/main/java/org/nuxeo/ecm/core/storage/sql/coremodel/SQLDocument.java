3fec91f562c09ad66f185b779a474e350a50d7c4
Florent Guillaume <fg@nuxeo.com>
2010-03-02 16:50:23 +0100

    NXP-4819: proxies to live documents (multifiling)

---
+++ nuxeo-core-storage-sql/nuxeo-core-storage-sql/src/main/java/org/nuxeo/ecm/core/storage/sql/coremodel/SQLDocument.java
@@ -0,0 +1,48 @@
+/*
+ * (C) Copyright 2008-2010 Nuxeo SA (http://nuxeo.com/) and contributors.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser General Public License
+ * (LGPL) version 2.1 which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl.html
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * Contributors:
+ *     Florent Guillaume
+ */
+
+package org.nuxeo.ecm.core.storage.sql.coremodel;
+
+import org.nuxeo.ecm.core.api.DocumentException;
+import org.nuxeo.ecm.core.model.Document;
+import org.nuxeo.ecm.core.model.PropertyContainer;
+import org.nuxeo.ecm.core.model.Property;
+import org.nuxeo.ecm.core.storage.sql.Node;
+
+/**
+ * Base interface for SQL documents.
+ */
+public interface SQLDocument extends Document, PropertyContainer, Property {
+
+    String BINARY_TEXT_SYS_PROP = "binaryText";
+
+    /**
+     * Returns the node with info about the hierarchy location.
+     */
+    Node getNode();
+
+    /**
+     * Returns the property holding the ACL.
+     */
+    org.nuxeo.ecm.core.model.Property getACLProperty() throws DocumentException;
+
+    /**
+     * Raises an exception if the document is read-only.
+     */
+    void checkWritable() throws DocumentException;
+
+}
