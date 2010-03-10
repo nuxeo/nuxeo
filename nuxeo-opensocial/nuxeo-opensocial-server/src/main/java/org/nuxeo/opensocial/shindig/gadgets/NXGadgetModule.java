2793140be3366aa2278fcedec3af7baff6ea5829
Damien Metzler <damien.metzler@leroymerlin.fr>
2010-03-10 12:22:50 +0100

    NXP-4864 Correctly configure the HTTP proxy for httpclient

--- nuxeo-opensocial/nuxeo-opensocial-server/src/main/java/org/nuxeo/opensocial/shindig/gadgets/NXGadgetModule.java
+++ nuxeo-opensocial/nuxeo-opensocial-server/src/main/java/org/nuxeo/opensocial/shindig/gadgets/NXGadgetModule.java
@@ -1,8 +1,10 @@
 package org.nuxeo.opensocial.shindig.gadgets;
 
+import org.apache.shindig.gadgets.http.HttpFetcher;
 import org.apache.shindig.gadgets.servlet.MakeRequestHandler;
 
 import com.google.inject.AbstractModule;
+import com.google.inject.Scopes;
 
 /** @author <a href="mailto:troger@nuxeo.com">Thomas Roger</a> */
 public class NXGadgetModule extends AbstractModule {
@@ -10,6 +12,7 @@ public class NXGadgetModule extends AbstractModule {
     @Override
     protected void configure() {
         bind(MakeRequestHandler.class).to(NXMakeRequestHandler.class);
+        bind(HttpFetcher.class).to(NXHttpFetcher.class).in(Scopes.SINGLETON);
     }
 
 }
