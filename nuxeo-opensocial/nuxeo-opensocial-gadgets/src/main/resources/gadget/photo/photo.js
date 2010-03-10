cdaaaa2cfb836b3e18db8bd2cbdb56eed15eb708
Guillaume Cusnieux <guillaume.cusnieux@leroymerlin.fr>
2010-03-10 15:19:30 +0100

    NXP-4541 refactoring gadget photo

--- nuxeo-opensocial/nuxeo-opensocial-gadgets/src/main/resources/gadget/photo/photo.js
+++ nuxeo-opensocial/nuxeo-opensocial-gadgets/src/main/resources/gadget/photo/photo.js
@@ -1,13 +1,14 @@
 var firstTime = true;
-var pUrl=top.nxContextPath + "/site/gadgets/photo/thumbnail.png";
+var pUrl= [top.nxContextPath,"/site/gadgets/photo/thumbnail.png"].join("");
 var perm = gadgets.nuxeo.isEditable();
+var formUrl;
 
 
 function launchGadget() {
 jQuery(document).ready(function(){
   if(firstTime){
-    var idGadget = gadgets.nuxeo.getGadgetId();
-    jQuery("#formUpload").attr("action", gadgets.nuxeo.getFormActionUrl(idGadget));
+    formUrl = gadgets.nuxeo.getFormActionUrl(gadgets.nuxeo.getGadgetId());
+    jQuery("#formUpload").attr("action", formUrl);
     firstTime =false;
   }
 
@@ -29,10 +30,9 @@ jQuery(document).ready(function(){
             alert(xhr.responseText);
           }
       });
-    }
-    else{
-    savePrefs();
-    launchGadget();
+    } else {
+      savePrefs();
+      launchGadget();
     }
     return false;
   });
@@ -56,8 +56,9 @@ jQuery(document).ready(function(){
 
 function setTitle(title){
   if(_isSet(title)){
-    jQuery("#title-field").val(gadgets.util.unescapeString(title));
-    jQuery("#title").text(gadgets.util.unescapeString(title));
+  	var _title = gadgets.util.unescapeString(title);
+    jQuery("#title-field").val(_title);
+    jQuery("#title").text(_title);
   }
 };
 
@@ -67,8 +68,9 @@ function setLink(link){
 
 function setLegend(legend){
   if(_isSet(legend)){
-    jQuery("#legend-field").val(gadgets.util.unescapeString(legend));
-    jQuery("#legend").text(gadgets.util.unescapeString(legend));
+  	var _legend = gadgets.util.unescapeString(legend);
+    jQuery("#legend-field").val(_legend);
+    jQuery("#legend").text(_legend);
   }
 };
 
@@ -87,7 +89,7 @@ function savePrefs(){
 
 function loadImage(id){
   var photoUrl = [gadgets.nuxeo.getFileActionUrl(id),'?junk=',Math.random()].join("");
-  jQuery("#formUpload").attr("action", gadgets.nuxeo.getFormActionUrl(id));
+  jQuery("#formUpload").attr("action", formUrl);
   jQuery.ajax({
     type : "GET",
     url : photoUrl,
@@ -97,16 +99,18 @@ function loadImage(id){
         showImage();
     },
     success : function(data, textStatus) {
-        if (_isSet(prefs.getString("link")))
-          var imgContainer = jQuery("<a id=\"link\" href=\""+prefs.getString("link")+"\" target=\"_tab\" ><img style=\"border:0;\" id=\"photo\" src=\"\" onload=\"gadgets.window.adjustHeight()\"></a>");
-        else
-          var imgContainer = jQuery("<img style=\"border:0;\" id=\"photo\" src=\"\" onload=\"gadgets.window.adjustHeight()\">");
-
-        jQuery("#pictureContainer").append(imgContainer);
-        jQuery("#photo").attr("src", photoUrl);
-        jQuery("#pictureContainer").append("<span id=\"legend\"></span>");
-        jQuery("#legend").text(gadgets.util.unescapeString(prefs.getString("legend")));
-        jQuery("#photo").width("100%");
+    	jQuery("#photo").remove();
+    	jQuery("#link").remove();
+    	jQuery("#legend").remove();
+    	var imgContainer = jQuery("<img onload=\"gadgets.window.adjustHeight()\"></img>").attr("id","photo").attr("src",photoUrl).attr("style","border:0;").width("100%");
+        if (_isSet(prefs.getString("link"))){
+          var link = jQuery("<a onload=\"gadgets.window.adjustHeight()\"></a>").attr("id","link").attr("href",prefs.getString("link")).attr("target","_tab");
+          link.append(imgContainer);
+          jQuery("#pictureContainer").append(link);
+        } else {
+          jQuery("#pictureContainer").append(imgContainer);
+        }        
+        jQuery("#pictureContainer").append(jQuery("<span></span>").attr("id","legend").text(gadgets.util.unescapeString(prefs.getString("legend"))));
         showImage();
       }
     });
