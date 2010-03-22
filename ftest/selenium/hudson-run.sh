30a796d829cd344ca9173658af49a9018ebff42a
Anahide Tchertchian <at@nuxeo.com>
2010-03-22 11:18:22 +0100

    NXP-4920: make sure commands are executable

--- ftest/selenium/hudson-run.sh
+++ ftest/selenium/hudson-run.sh
@@ -5,9 +5,11 @@ JBOSS_HOME=$1
 OUTPUT=$2
 
 echo "start jboss"
+chmod +x $JBOSS_HOME/bin/jbossctl || exit 1
 $JBOSS_HOME/bin/jbossctl start || exit 1
 
 echo "run selenium tests"
+chmod +x "$HERE"/run.sh $OUTPUT || exit 1
 HIDE_FF=true "$HERE"/run.sh $OUTPUT
 ret1=$?
 
