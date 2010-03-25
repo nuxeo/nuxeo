4d4b22d36b34676f23d1bf1e48920fd75a921760
Julien Carsique <jcarsique@nuxeo.com>
2010-03-25 18:55:18 +0100

    align scripts on 5.3.1

--- clone.bat
+++ clone.bat
@@ -1,17 +1,17 @@
 rem This script will checkout the needed sources
 
-hg clone -r 1.6 http://hg.nuxeo.org/nuxeo/nuxeo-common
-hg clone -r 1.6 http://hg.nuxeo.org/nuxeo/nuxeo-runtime
-hg clone -r 1.6 http://hg.nuxeo.org/nuxeo/nuxeo-core
+hg clone -r 1.6.1 http://hg.nuxeo.org/nuxeo/nuxeo-common
+hg clone -r 1.6.1 http://hg.nuxeo.org/nuxeo/nuxeo-runtime
+hg clone -r 1.6.1 http://hg.nuxeo.org/nuxeo/nuxeo-core
 
-hg clone -r 5.3 http://hg.nuxeo.org/nuxeo/nuxeo-services
-hg clone -r 5.3 http://hg.nuxeo.org/nuxeo/nuxeo-theme
-hg clone -r 5.3 http://hg.nuxeo.org/nuxeo/nuxeo-webengine
-hg clone -r 5.3 http://hg.nuxeo.org/nuxeo/nuxeo-jsf
-hg clone -r 5.3 http://hg.nuxeo.org/nuxeo/nuxeo-gwt
-hg clone -r 5.3 http://hg.nuxeo.org/nuxeo/nuxeo-features
-hg clone -r 5.3 http://hg.nuxeo.org/nuxeo/nuxeo-dm
+hg clone -r 5.3.1 http://hg.nuxeo.org/nuxeo/nuxeo-services
+hg clone -r 5.3.1 http://hg.nuxeo.org/nuxeo/nuxeo-theme
+hg clone -r 5.3.1 http://hg.nuxeo.org/nuxeo/nuxeo-webengine
+hg clone -r 5.3.1 http://hg.nuxeo.org/nuxeo/nuxeo-jsf
+hg clone -r 5.3.1 http://hg.nuxeo.org/nuxeo/nuxeo-gwt
+hg clone -r 5.3.1 http://hg.nuxeo.org/nuxeo/nuxeo-features
+hg clone -r 5.3.1 http://hg.nuxeo.org/nuxeo/nuxeo-dm
 
-hg clone -r 5.3 http://hg.nuxeo.org/nuxeo/nuxeo-distribution
+hg clone -r 5.3.1 http://hg.nuxeo.org/nuxeo/nuxeo-distribution
 
-hg clone -r 5.3 http://hg.nuxeo.org/addons ../nuxeo-addons
+hg clone http://hg.nuxeo.org/addons ../nuxeo-addons
