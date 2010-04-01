24d25671ee1834aa2999f4a6659734c2b3eab68c
Laurent Doguin <ldoguin@nuxeo.com>
2010-04-01 17:28:09 +0200

    NXP-4357 add security check for drag n drop imagePlugin

--- nuxeo-platform-imaging/nuxeo-platform-imaging-core/src/main/java/org/nuxeo/ecm/platform/picture/extension/ImagePlugin.java
+++ nuxeo-platform-imaging/nuxeo-platform-imaging-core/src/main/java/org/nuxeo/ecm/platform/picture/extension/ImagePlugin.java
@@ -19,8 +19,8 @@ package org.nuxeo.ecm.platform.picture.extension;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Map;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -34,30 +34,31 @@ import org.nuxeo.ecm.core.api.VersionModel;
 import org.nuxeo.ecm.core.api.impl.VersionModelImpl;
 import org.nuxeo.ecm.platform.filemanager.service.extension.AbstractFileImporter;
 import org.nuxeo.ecm.platform.filemanager.utils.FileManagerUtils;
+import org.nuxeo.ecm.platform.picture.api.ImagingDocumentConstants;
 import org.nuxeo.ecm.platform.picture.api.adapters.PictureResourceAdapter;
-import org.nuxeo.ecm.platform.types.Type;
 import org.nuxeo.ecm.platform.types.TypeManager;
 
 public class ImagePlugin extends AbstractFileImporter {
 
     private static final long serialVersionUID = 5850210255138418118L;
+
     private static final Log log = LogFactory.getLog(ImagePlugin.class);
 
     public DocumentModel create(CoreSession documentManager, Blob content,
             String path, boolean overwrite, String fullname,
             TypeManager typeService) throws ClientException, IOException {
+        path = getNearestContainerPath(documentManager, path);
+        doSecurityCheck(documentManager, path,
+                ImagingDocumentConstants.PICTURE_TYPE_NAME, typeService);
 
         String filename = FileManagerUtils.fetchFileName(fullname);
-
-        DocumentModel docModel;
-        String title = FileManagerUtils.fetchTitle(filename);
+        content.setFilename(filename);
 
         // Looking if an existing Document with the same filename exists.
-        DocumentModel existing = FileManagerUtils.getExistingDocByTitle(
-                documentManager, path, title);
+        DocumentModel docModel = FileManagerUtils.getExistingDocByFileName(
+                documentManager, path, filename);
 
-        if (overwrite && existing != null) {
-            docModel = existing;
+        if (overwrite && docModel != null) {
 
             // Do a checkin / checkout of the current version first
             DocumentRef docRef = docModel.getRef();
@@ -66,40 +67,34 @@ public class ImagePlugin extends AbstractFileImporter {
             documentManager.checkIn(docRef, newVersion);
             documentManager.checkOut(docRef);
 
-            ((Map) ((List) docModel.getDataModel("picture").getData(
-                    "views")).get(0)).put("content", content.persist());
+            ((Map) ((List) docModel.getDataModel("picture").getData("views")).get(0)).put(
+                    "content", content.persist());
             // To do: Generate Picture views
             // docModel.setProperty("file", "content", content.persist());
 
         } else {
-
+            String title = FileManagerUtils.fetchTitle(filename);
             // Creating an unique identifier
             String docId = IdUtils.generateId(title);
-            DocumentModel doc = documentManager.createDocumentModel(path, docId, "Picture");
-            docModel = documentManager.createDocument(doc);
+            DocumentModel doc = documentManager.createDocumentModel(path,
+                    docId, ImagingDocumentConstants.PICTURE_TYPE_NAME);
             try {
                 DocumentModel parent = documentManager.getDocument(docModel.getParentRef());
                 ArrayList<Map<String, Object>> pictureTemplates = null;
-                if (parent.getType().equals("PictureBook")) {
+                if (parent.getType().equals(
+                        ImagingDocumentConstants.PICTUREBOOK_TYPE_NAME)) {
                     // Use PictureBook Properties
-                    pictureTemplates = (ArrayList<Map<String, Object>>) parent.getProperty(
-                            "picturebook", "picturetemplates");
+                    pictureTemplates = (ArrayList<Map<String, Object>>) parent.getPropertyValue(ImagingDocumentConstants.PICTURETEMPLATES_PROPERTY_NAME);
                 }
                 PictureResourceAdapter picture = docModel.getAdapter(PictureResourceAdapter.class);
-                picture.createPicture(content, filename, title, pictureTemplates);
+                picture.createPicture(content, filename, title,
+                        pictureTemplates);
 
             } catch (Exception e) {
                 log.error("Picture.views generation failed", e);
             }
-            // updating icon
-
-            Type imageType = typeService.getType("Picture");
-            if (imageType != null) {
-                String iconPath = imageType.getIcon();
-                docModel.setProperty("common", "icon", iconPath);
-            }
+            docModel = documentManager.createDocument(doc);
         }
-        documentManager.saveDocument(docModel);
         documentManager.save();
 
         log.debug("Created the Picture: " + docModel.getName()
