2e51df8d7916837b3b14bad248fc82ae60428683
Anahide Tchertchian <at@nuxeo.com>
2010-04-29 14:49:26 +0200

    format

--- nuxeo-platform-usermanager-core/src/main/java/org/nuxeo/ecm/platform/computedgroups/GroupComputer.java
+++ nuxeo-platform-usermanager-core/src/main/java/org/nuxeo/ecm/platform/computedgroups/GroupComputer.java
@@ -26,13 +26,12 @@ import java.util.Map;
 import org.nuxeo.ecm.platform.usermanager.NuxeoPrincipalImpl;
 
 /**
-* Interface that must be implemented by all contributed {@link GroupComputer}
-*
-* @author Thierry Delprat
-*
-*/
+ * Interface that must be implemented by all contributed {@link GroupComputer}
+ *
+ * @author Thierry Delprat
+ *
+ */
 public interface GroupComputer {
-
 
     /**
      *
@@ -42,17 +41,18 @@ public interface GroupComputer {
      * @return
      * @throws Exception
      */
-    List<String> getGroupsForUser(NuxeoPrincipalImpl nuxeoPrincipal) throws Exception ;
+    List<String> getGroupsForUser(NuxeoPrincipalImpl nuxeoPrincipal)
+            throws Exception;
 
     /**
-     * return all group ids.
-     * If you class can not efficiently compute this list, you can return an empty list.
-     * In this case you need to implement the searchGroups method
+     * return all group ids. If you class can not efficiently compute this list,
+     * you can return an empty list. In this case you need to implement the
+     * searchGroups method
      *
      * @return
      * @throws Exception
      */
-    List<String> getAllGroupIds() throws Exception ;
+    List<String> getAllGroupIds() throws Exception;
 
     /**
      *
@@ -83,14 +83,15 @@ public interface GroupComputer {
     List<String> getSubGroupsNames(String groupName) throws Exception;
 
     /**
-     * Search for a group.
-     * (This method is used in particular from UI to search/select a group)
+     * Search for a group. (This method is used in particular from UI to
+     * search/select a group)
      *
      * @param filter
      * @param fulltext
      * @return
      * @throws Exception
      */
-    List<String> searchGroups(Map<String, Serializable> filter,HashSet<String> fulltext) throws Exception;
+    List<String> searchGroups(Map<String, Serializable> filter,
+            HashSet<String> fulltext) throws Exception;
 
 }
