0715be161a9908fa597d267aa185c602a81f23dc
Julien Carsique <jcarsique@nuxeo.com>
2010-04-29 12:30:16 +0200

    parameterized 5.3 and 1.6 in clone.bat

--- clone.bat
+++ clone.bat
@@ -1,17 +1,20 @@
 rem This script will checkout the needed sources
 
-hg clone -r 1.6 http://hg.nuxeo.org/nuxeo/nuxeo-common
-hg clone -r 1.6 http://hg.nuxeo.org/nuxeo/nuxeo-runtime
-hg clone -r 1.6 http://hg.nuxeo.org/nuxeo/nuxeo-core
+set CV=1.6
+set PV=5.3
 
-hg clone -r 5.3 http://hg.nuxeo.org/nuxeo/nuxeo-services
-hg clone -r 5.3 http://hg.nuxeo.org/nuxeo/nuxeo-theme
-hg clone -r 5.3 http://hg.nuxeo.org/nuxeo/nuxeo-webengine
-hg clone -r 5.3 http://hg.nuxeo.org/nuxeo/nuxeo-jsf
-hg clone -r 5.3 http://hg.nuxeo.org/nuxeo/nuxeo-gwt
-hg clone -r 5.3 http://hg.nuxeo.org/nuxeo/nuxeo-features
-hg clone -r 5.3 http://hg.nuxeo.org/nuxeo/nuxeo-dm
+hg clone -r %CV% http://hg.nuxeo.org/nuxeo/nuxeo-common
+hg clone -r %CV% http://hg.nuxeo.org/nuxeo/nuxeo-runtime
+hg clone -r %CV% http://hg.nuxeo.org/nuxeo/nuxeo-core
 
-hg clone -r 5.3 http://hg.nuxeo.org/nuxeo/nuxeo-distribution
+hg clone -r %PV% http://hg.nuxeo.org/nuxeo/nuxeo-services
+hg clone -r %PV% http://hg.nuxeo.org/nuxeo/nuxeo-theme
+hg clone -r %PV% http://hg.nuxeo.org/nuxeo/nuxeo-webengine
+hg clone -r %PV% http://hg.nuxeo.org/nuxeo/nuxeo-jsf
+hg clone -r %PV% http://hg.nuxeo.org/nuxeo/nuxeo-gwt
+hg clone -r %PV% http://hg.nuxeo.org/nuxeo/nuxeo-features
+hg clone -r %PV% http://hg.nuxeo.org/nuxeo/nuxeo-dm
 
-hg clone -r 5.3 http://hg.nuxeo.org/addons ../nuxeo-addons
+hg clone -r %PV% http://hg.nuxeo.org/nuxeo/nuxeo-distribution
+
+hg clone -r %PV% http://hg.nuxeo.org/addons ../nuxeo-addons
