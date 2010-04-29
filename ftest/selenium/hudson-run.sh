68417f8291af30b6c65f9f15c55da751d42aa3b2
Julien Carsique <jcarsique@nuxeo.com>
2010-04-29 16:07:39 +0200

    NXBT-150 - use nuxeoctl

--- ftest/selenium/hudson-run.sh
+++ ftest/selenium/hudson-run.sh
@@ -5,8 +5,8 @@ JBOSS_HOME=$1
 OUTPUT=$2
 
 echo "start jboss"
-chmod +x $JBOSS_HOME/bin/jbossctl || exit 1
-$JBOSS_HOME/bin/jbossctl start || exit 1
+chmod +x $JBOSS_HOME/bin/*ctl || exit 1
+$JBOSS_HOME/bin/nuxeoctl start || exit 1
 
 echo "run selenium tests"
 chmod +x "$HERE"/run.sh $OUTPUT || exit 1
@@ -14,6 +14,6 @@ HIDE_FF=true "$HERE"/run.sh $OUTPUT
 ret1=$?
 
 echo "stop jboss"
-$JBOSS_HOME/bin/jbossctl stop || exit 1
+$JBOSS_HOME/bin/nuxeoctl stop || exit 1
 
 exit $ret1
