656f070108096ccf78ba648c4ad136dbbd50e48d
Anahide Tchertchian <at@nuxeo.com>
2010-06-02 17:18:30 +0200

    NXP-5138: add license where it's missing

--- nuxeo-platform-ui-web/src/main/java/org/nuxeo/ecm/platform/ui/web/multipart/MultipartFilter.java
+++ nuxeo-platform-ui-web/src/main/java/org/nuxeo/ecm/platform/ui/web/multipart/MultipartFilter.java
@@ -1,3 +1,24 @@
+/**
+ * License Agreement.
+ *
+ *  JBoss RichFaces - Ajax4jsf Component Library
+ *
+ * Copyright (C) 2007  Exadel, Inc.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License version 2.1 as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
+ */
+
 package org.nuxeo.ecm.platform.ui.web.multipart;
 
 import static org.jboss.seam.ScopeType.APPLICATION;
