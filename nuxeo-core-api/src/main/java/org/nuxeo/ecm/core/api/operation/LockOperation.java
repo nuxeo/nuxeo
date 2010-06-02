3b380938fcf273ef94e4a5204b910487a3a0ab34
Stephane Lacoin <slacoin@nuxeo.com>
2010-06-02 19:49:18 +0200

    NXP-4914 adding missing headers

--- nuxeo-core-api/src/main/java/org/nuxeo/ecm/core/api/operation/LockOperation.java
+++ nuxeo-core-api/src/main/java/org/nuxeo/ecm/core/api/operation/LockOperation.java
@@ -1,3 +1,21 @@
+/*
+ * (C) Copyright 2006-2008 Nuxeo SAS (http://nuxeo.com/) and contributors.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser General Public License
+ * (LGPL) version 2.1 which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl.html
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * Contributors:
+ *     slacoin
+ *
+ * $Id$
+ */
 package org.nuxeo.ecm.core.api.operation;
 
 import org.nuxeo.ecm.core.api.DocumentRef;
@@ -13,7 +31,7 @@ public class LockOperation extends  Operation<String> {
     private static final long serialVersionUID = 1L;
     protected final DocumentRef ref;
     protected final String key;
-    
+
     @Override
     public String doRun(ProgressMonitor montior) throws Exception {
        session.setLock(ref, key);
