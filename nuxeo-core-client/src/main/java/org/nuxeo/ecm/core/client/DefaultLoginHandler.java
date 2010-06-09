15c43f748843fd6797f82d21ed349e910a35b57d
Julien Carsique <jcarsique@nuxeo.com>
2010-06-09 16:50:43 +0200

    forgot to update Copyright

--- nuxeo-core-client/src/main/java/org/nuxeo/ecm/core/client/DefaultLoginHandler.java
+++ nuxeo-core-client/src/main/java/org/nuxeo/ecm/core/client/DefaultLoginHandler.java
@@ -1,5 +1,5 @@
 /*
- * (C) Copyright 2006-2007 Nuxeo SAS (http://nuxeo.com/) and contributors.
+ * (C) Copyright 2006-2010 Nuxeo SAS (http://nuxeo.com/) and contributors.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the GNU Lesser General Public License
@@ -12,7 +12,7 @@
  * Lesser General Public License for more details.
  *
  * Contributors:
- *     bstefanescu
+ *     bstefanescu, jcarsique
  *
  * $Id$
  */
