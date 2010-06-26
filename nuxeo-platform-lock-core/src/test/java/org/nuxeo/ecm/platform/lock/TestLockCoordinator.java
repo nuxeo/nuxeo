90f69604c44900a0a5b470dd07aa53fea18ba4d1
Sun Seng David TAN <stan@nuxeo.com>
2010-06-26 13:04:49 +0200

    NXP-5132 cleaning

--- nuxeo-platform-lock-core/src/test/java/org/nuxeo/ecm/platform/lock/TestLockCoordinator.java
+++ nuxeo-platform-lock-core/src/test/java/org/nuxeo/ecm/platform/lock/TestLockCoordinator.java
@@ -1,16 +1,16 @@
 /*
  * (C) Copyright 2010 Nuxeo SA (http://nuxeo.com/) and contributors.
- * 
+ *
  * All rights reserved. This program and the accompanying materials are made
  * available under the terms of the GNU Lesser General Public License (LGPL)
  * version 2.1 which accompanies this distribution, and is available at
  * http://www.gnu.org/licenses/lgpl.html
- * 
+ *
  * This library is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
  * details.
- * 
+ *
  * Contributors: Sun Seng David TAN (a.k.a. sunix) <stan@nuxeo.com>
  */
 package org.nuxeo.ecm.platform.lock;
@@ -27,9 +27,9 @@ import org.nuxeo.runtime.api.Framework;
 import org.nuxeo.runtime.test.NXRuntimeTestCase;
 
 /**
- * 
+ *
  * @author Sun Seng David TAN (a.k.a. sunix) <stan@nuxeo.com>
- * 
+ *
  */
 public class TestLockCoordinator extends NXRuntimeTestCase {
 
@@ -41,8 +41,7 @@ public class TestLockCoordinator extends NXRuntimeTestCase {
         deployBundle("org.nuxeo.ecm.core.persistence");
         deployBundle("org.nuxeo.ecm.platform.lock.api");
         deployBundle("org.nuxeo.ecm.platform.lock.core");
-        deployTestContrib("org.nuxeo.ecm.platform.lock.core",
-                "nxlocks-tests.xml");
+        deployTestContrib("org.nuxeo.ecm.platform.lock.core", "nxlocks-tests.xml");
     }
 
     // TODO if we reduced the delay to less that
@@ -54,7 +53,7 @@ public class TestLockCoordinator extends NXRuntimeTestCase {
      * winner is getting the lock for a certain time and need to unlock before,
      * looser is waiting (until the timeout is reached), winner unlocks, looser
      * finally has lost (AlreadyLockedException)
-     * 
+     *
      * @throws Exception
      */
     public void testLockService() throws Exception {
