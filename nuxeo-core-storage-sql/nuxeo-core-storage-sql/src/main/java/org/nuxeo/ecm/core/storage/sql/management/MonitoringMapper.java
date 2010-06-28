384512066aa629723e81638855dd7b7341e679e8
Stephane Lacoin <slacoin@nuxeo.com>
2010-06-28 21:07:03 +0200

    APG-240 adding missing stopwatch logic

---
+++ nuxeo-core-storage-sql/nuxeo-core-storage-sql/src/main/java/org/nuxeo/ecm/core/storage/sql/management/MonitoringMapper.java
@@ -0,0 +1,63 @@
+package org.nuxeo.ecm.core.storage.sql.management;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
+import org.javasimon.SimonManager;
+import org.javasimon.Split;
+import org.javasimon.Stopwatch;
+import org.nuxeo.ecm.core.storage.sql.Mapper;
+import org.nuxeo.ecm.core.storage.sql.net.NetMapper;
+
+public class MonitoringMapper implements InvocationHandler {
+
+    protected Mapper proxied;
+
+    protected MonitoringMapper(Mapper mapper) {
+        this.proxied = mapper;
+    }
+
+    public static Mapper newProxy(Mapper mapper) {
+        MonitoringMapper handler = new MonitoringMapper(mapper);
+        return (Mapper) Proxy.newProxyInstance(NetMapper.class.getClassLoader(), new Class<?>[] { Mapper.class }, handler);
+    }
+
+    protected String formatParms(Object... parms) {
+        if (parms == null) {
+            return "";
+        }
+        StringBuffer buffer = new StringBuffer();
+        for (Object parm : parms) {
+            buffer.append(".").append(parm);
+        }
+        return buffer.toString();
+    }
+
+    protected String formatName(Method m, Object[] parms) {
+        Class<?> declaringClass = m.getDeclaringClass();
+        return String.format("%s.%s", declaringClass.getSimpleName(), m.getName());
+    }
+
+    protected String formatNote(Method m, Object[] parms) {
+        return String.format("%s#%s(%s)", m.getDeclaringClass().getSimpleName(), m.getName(), formatParms(parms));
+    }
+
+    protected Stopwatch getStopwatch(Method m, Object[] parms) {
+        String name = formatName(m, parms);
+        Stopwatch stopwatch = SimonManager.getStopwatch(name);
+        stopwatch.setNote(formatNote(m, parms));
+        return stopwatch;
+    }
+
+    public Object invoke(Object proxy, Method m, Object[] parms) throws Throwable {
+        Split split = getStopwatch(m, parms).start();
+        try {
+            return m.invoke(proxied, parms);
+        } finally {
+            split.stop();
+        }
+
+    }
+
+}
