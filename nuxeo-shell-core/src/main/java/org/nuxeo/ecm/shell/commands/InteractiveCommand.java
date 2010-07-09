d3b14a71c89933266580416f8f4eff62026013d7
Quentin Lamerand <qlamerand@nuxeo.com>
2010-07-09 18:31:02 +0200

    NXP-5368: Don't split double-quoted arguments containing spaces

--- nuxeo-shell-core/src/main/java/org/nuxeo/ecm/shell/commands/InteractiveCommand.java
+++ nuxeo-shell-core/src/main/java/org/nuxeo/ecm/shell/commands/InteractiveCommand.java
@@ -22,6 +22,10 @@ package org.nuxeo.ecm.shell.commands;
 import java.io.File;
 import java.io.PrintStream;
 import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import jline.ConsoleReader;
 import jline.History;
@@ -106,7 +110,14 @@ public class InteractiveCommand implements Command {
      *         result of the command.
      */
     CommandLineReturn processInput(String input) {
-        String[] args = input.split("[ ]+");
+        Pattern p = Pattern.compile("\\s*\"(.*?)\"\\s*|(?<=^| )[^ ]*");
+        Matcher m = p.matcher(input);
+        List<String> result = new ArrayList<String>();
+        while (m.find()) {
+            //strip off quotes:
+            result.add(m.group(1) != null ? m.group(1) : m.group());
+        }
+        String[] args = result.toArray(new String[result.size()]);
         try {
             CommandLine cmdLine = service.parse(args, true);
             String cmdName = cmdLine.getCommand();
