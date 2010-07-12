4484abeff9a5bc90a941097654472bbf09031a3d
Quentin Lamerand <qlamerand@nuxeo.com>
2010-07-12 16:15:00 +0200

    NXP-5374: Fix isEditable function in 'nuxeo' feature

--- nuxeo-opensocial/nuxeo-opensocial-features/src/main/js/nuxeo/nuxeo.js
+++ nuxeo-opensocial/nuxeo-opensocial-features/src/main/js/nuxeo/nuxeo.js
@@ -63,26 +63,8 @@ gadgets.nuxeo.getGadgetId = function() {
   return window.name.split("-").slice(2).join("-");
 };
 
-gadgets.nuxeo.hasPermission = function(permissionName) {
-  var tmp = gadgets.util.getUrlParameters().permission;
-  var perms = tmp.substring(1, tmp.length-1).split(",");
-
-  if (perms.indexOf("Everything")>=0) {
-    return true;
-  }
-
-  var b = false;
-  jQuery.each(perms, function(i, p){
-    if(jQuery.trim(p) == jQuery.trim(permissionName)) {
-      b = true;
-      return;
-    }
-  });
-  return b;
-};
-
 gadgets.nuxeo.isEditable = function() {
-  return gadgets.nuxeo.hasPermission("Write");
+  return gadgets.util.getUrlParameters().permission == "1";
 };
 
 gadgets.nuxeo.getNXIDPreference = function(name, id) {
