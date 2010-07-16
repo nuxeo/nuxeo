aa5ddc946fa354bab617414362c9a5c761451ca9
Julien Carsique <jcarsique@nuxeo.com>
2010-07-16 19:04:55 +0200

    check ports and kill ghost process before starting a new JBoss

--- ftest/selenium/hudson-run.sh
+++ ftest/selenium/hudson-run.sh
@@ -4,6 +4,16 @@ HERE=$(cd $(dirname $0); pwd -P)
 JBOSS_HOME=$1
 OUTPUT=$2
 
+hostname=${hostname:-0.0.0.0}
+port=${port:-8080}
+RUNNING_PID=`lsof -i@$hostname:$port -sTCP:LISTEN -n -t`
+if [ ! -z $RUNNING_PID ]; then
+    echo [WARN] A process is already using port $port: $RUNNING_PID
+    echo [WARN] Storing jstack in $PWD/$RUNNING_PID.jstack then killing process
+    [ -e /usr/lib/jvm/java-6-sun/bin/jstack ] && /usr/lib/jvm/java-6-sun/bin/jstack $RUNNING_PID >$PWD/$RUNNING_PID.jstack
+    kill $RUNNING_PID || kill -9 $RUNNING_PID
+fi
+
 echo "start jboss"
 chmod +x $JBOSS_HOME/bin/*ctl || exit 1
 $JBOSS_HOME/bin/nuxeoctl start || exit 1
