0c42e135a1c9687f3ef90883946a31bd08a554e2
Stephane Lacoin <slacoin@nuxeo.com>
2010-07-22 14:48:32 +0200

    APG-240 optimized reflection use

--- nuxeo-theme-html/src/main/java/org/nuxeo/theme/html/JSUtils.java
+++ nuxeo-theme-html/src/main/java/org/nuxeo/theme/html/JSUtils.java
@@ -14,7 +14,6 @@
 
 package org.nuxeo.theme.html;
 
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 import org.apache.commons.logging.Log;
@@ -26,20 +25,25 @@ import org.mozilla.javascript.Scriptable;
 import org.mozilla.javascript.tools.ToolErrorReporter;
 import org.mozilla.javascript.tools.shell.Global;
 import org.mozilla.javascript.tools.shell.Main;
-import org.omg.CORBA.portable.InvokeHandler;
 
 public final class JSUtils {
 
     static final Log log = LogFactory.getLog(JSUtils.class);
 
     private static final Global global = Main.getGlobal();
+    private static Method compress;
 
     static {
         ToolErrorReporter errorReporter = new ToolErrorReporter(false,
                 global.getErr());
         Main.shellContextFactory.setErrorReporter(errorReporter);
         global.init(Main.shellContextFactory);
-    }
+        try {
+            compress = Context.class.getMethod("compressReader", Context.class, Scriptable.class, Script.class, String.class, String.class, Integer.TYPE, Object.class);
+        } catch (NoSuchMethodException e) {
+            log.warn("No javascript compressor support available, check for your rhino engine.");
+        }
+   }
 
     public static String compressSource(final String source) {
         IProxy iproxy = new IProxy(source);
@@ -54,16 +58,12 @@ public final class JSUtils {
         }
 
         public Object run(final Context cx) {
+            if (compress == null) {
+                return source;
+            }
             try {
-                final Script script = Main.loadScriptFromSource(cx, source,
-                        "compress", 1, null);
-                Method method = Context.class.getMethod("compressReader", Context.class, Scriptable.class, Script.class, String.class, String.class, Integer.TYPE, Object.class);
-                if (method == null) {
-                    log.info("Could not compress javascript source. custom_rhino.jar is probably not installed.");
-                    return source;
-                }
-                return method.invoke(cx, global, script, source, "compress", 1,
-                        null);
+                Script script = Main.loadScriptFromSource(cx, source, "compress", 1, null);
+                    return compress.invoke(cx, global, script, source, "compress", 1, null);
             } catch (Exception e) {
                 // Can happen on very large files (> 500K) with JDK 5
                 log.error("Could not compress javascript source.", e);
