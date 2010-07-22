70113310deb1dddf1ac3fd5b5d122391ed6aca8e
Stephane Lacoin <slacoin@nuxeo.com>
2010-07-22 20:32:28 +0200

    APG-240 fixing audit tests, load required persistence module

--- nuxeo-platform-audit/nuxeo-platform-audit-core/src/test/java/org/nuxeo/ecm/platform/audit/TestAdapterRegistration.java
+++ nuxeo-platform-audit/nuxeo-platform-audit-core/src/test/java/org/nuxeo/ecm/platform/audit/TestAdapterRegistration.java
@@ -7,26 +7,28 @@ import org.nuxeo.runtime.api.Framework;
 import org.nuxeo.runtime.test.NXRuntimeTestCase;
 
 public class TestAdapterRegistration extends NXRuntimeTestCase{
-	
+
 	@Override
 	public void setUp() throws Exception {
 		super.setUp();
 		deployBundle("org.nuxeo.ecm.platform.audit.api");
 	    deployBundle("org.nuxeo.ecm.platform.audit"); // the audit.core
+	    deployBundle("org.nuxeo.ecm.core.persistence");
 	    //deployTestContrib("org.nuxeo.ecm.platform.audit", "OSGI-INF/test-audit-contrib.xml");
 	    deployTestContrib("org.nuxeo.ecm.platform.audit", "test-audit-contrib.xml");
+	    fireFrameworkStarted();
 	}
 
-	
+
 	public void testAuditContribution() throws Exception {
 		NXAuditEventsService auditService = (NXAuditEventsService) Framework.getLocalService(NXAuditEvents.class);
 		assertNotNull(auditService);
 		AdapterDescriptor[] registeredAdapters = auditService.getRegisteredAdapters();
 		assertEquals(1, registeredAdapters.length);
-		
+
 		AdapterDescriptor ad = registeredAdapters[0];
 		assertEquals("myadapter", ad.getName());
-		
+
 	}
-	
+
 }
