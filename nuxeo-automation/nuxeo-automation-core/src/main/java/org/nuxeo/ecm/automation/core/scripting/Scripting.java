2b808b2cc74d0b4a78fa072d5c74ef66ec19e85e
Bogdan Stefanescu <bstefanescu@nuxeo.com>
2010-08-19 16:13:01 +0200

    NXS-224 - added PrincipalWrapper to expose easier API in scripting expressions

--- nuxeo-automation/nuxeo-automation-core/src/main/java/org/nuxeo/ecm/automation/core/scripting/Scripting.java
+++ nuxeo-automation/nuxeo-automation-core/src/main/java/org/nuxeo/ecm/automation/core/scripting/Scripting.java
@@ -28,6 +28,7 @@ import org.nuxeo.common.utils.FileUtils;
 import org.nuxeo.ecm.automation.OperationContext;
 import org.nuxeo.ecm.automation.OperationException;
 import org.nuxeo.ecm.core.api.DocumentModel;
+import org.nuxeo.ecm.core.api.NuxeoPrincipal;
 import org.nuxeo.runtime.api.Framework;
 
 /**
@@ -84,7 +85,8 @@ public class Scripting {
         map.put("Context", ctx);
         map.put("This", input);
         map.put("Session", ctx.getCoreSession());
-        map.put("Principal", ctx.getPrincipal());
+        map.put("CurrentUser",
+                new PrincipalWrapper((NuxeoPrincipal) ctx.getPrincipal()));
         map.put("Env", Framework.getProperties());
         map.put("Fn", Functions.INSTANCE);
         if (input instanceof DocumentModel) {
