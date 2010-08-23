8fec5e13912d91e25cf65ccb193e200920836a7a
Stephane Lacoin <slacoin@nuxeo.com>
2010-08-23 13:24:30 +0200

    NXP-5417 better principal uses and cosmetics

--- nuxeo-platform-management/src/main/java/org/nuxeo/ecm/platform/management/statuses/AdministrativeStatus.java
+++ nuxeo-platform-management/src/main/java/org/nuxeo/ecm/platform/management/statuses/AdministrativeStatus.java
@@ -7,18 +7,17 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.apache.commons.lang.StringUtils;
-import org.nuxeo.ecm.core.api.AbstractSession;
 import org.nuxeo.ecm.core.api.ClientRuntimeException;
+import org.nuxeo.ecm.core.api.SimplePrincipal;
+import org.nuxeo.ecm.core.api.security.SecurityConstants;
 import org.nuxeo.ecm.core.event.Event;
 import org.nuxeo.ecm.core.event.EventContext;
 import org.nuxeo.ecm.core.event.EventProducer;
 import org.nuxeo.ecm.core.event.impl.InlineEventContext;
-import org.nuxeo.runtime.RuntimeServiceEvent;
-import org.nuxeo.runtime.RuntimeServiceListener;
 import org.nuxeo.runtime.api.Framework;
 
 public class AdministrativeStatus {
-	
+
     public static final String ACTIVE = "active";
 
     public static final String PASSIVE = "passive";
@@ -37,7 +36,7 @@ public class AdministrativeStatus {
 	protected void notifyEvent(String name) {
 		Map<String, Serializable> eventProperties = new HashMap<String, Serializable>();
 		eventProperties.put("category", ADMINISTRATIVE_EVENT_CATEGORY);
-		EventContext ctx = new InlineEventContext(AbstractSession.ADMINISTRATOR,eventProperties);
+		EventContext ctx = new InlineEventContext(new SimplePrincipal(SecurityConstants.SYSTEM_USERNAME),eventProperties);
 		Event event = ctx.newEvent(name);
 		try {
 			Framework.getService(EventProducer.class).fireEvent(event);
@@ -45,13 +44,13 @@ public class AdministrativeStatus {
 			throw new ClientRuntimeException(e);
 		}
 	}
-	
+
 	protected AdministrativeStatusPersister persister = new NuxeoAdministrativeStatusPersister();
-	
+
 	protected String value;
-	
+
 	protected String serverInstanceName;
-	
+
 	protected void activate() {
 		 serverInstanceName = Framework.getProperties().getProperty(
 				ADMINISTRATIVE_INSTANCE_ID);
@@ -66,28 +65,28 @@ public class AdministrativeStatus {
 		}
 		value = persister.getValue(serverInstanceName);
 	}
-	
+
 	protected void deactivate() {
 		serverInstanceName = null;
 		value = null;
 	}
-	
+
 	/**
 	 * Disable services for this server
-	 * 
+	 *
 	 */
 	public void setPassive() {
 		value = PASSIVE;
 		String lastValue = persister.setValue(serverInstanceName, value);
 		if (!lastValue.equals(value)) {
-			notifyEvent(PASSIVATED_EVENT);			
+			notifyEvent(PASSIVATED_EVENT);
 		}
 	}
 
-	
+
 	/**
 	 * Enable services for this server
-	 * 
+	 *
 	 */
 	public void setActive() {
 		value = ACTIVE;
@@ -99,31 +98,31 @@ public class AdministrativeStatus {
 
 	/**
 	 * Returns the stringified value of this status, ie: "passive" or "active"
-	 * 
+	 *
 	 * @return
 	 */
 
 	public String getValue() {
 		return value;
 	}
-	
+
 	/**
 	 * Return this server unique name
-	 * 
+	 *
 	 */
 	public String getServerInstanceName() {
 		return serverInstanceName;
 	}
-	
+
 	/**
 	 * Returns true if server is in active state
-	 * 
+	 *
 	 * @return
 	 */
 	public boolean isActive() {
 		return value.equals(ACTIVE);
 	}
-	
+
 	/**
 	 * Returns true if server is in passive state
 	 * @return
