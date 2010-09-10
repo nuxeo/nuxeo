2a6456d09184f66d43441874ad1b56860b249e17
Alexandre Russel <arussel@nuxeo.com>
2010-09-10 08:07:59 +0200

    NXCM-108 - create the DocumentRoutingPersistenceService

---
+++ nuxeo-platform-document-routing-api/src/main/java/org/nuxeo/ecm/platform/routing/api/DocumentRoutingConstants.java
@@ -0,0 +1,34 @@
+/*
+ * (C) Copyright 2009 Nuxeo SA (http://nuxeo.com/) and contributors.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser General Public License
+ * (LGPL) version 2.1 which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl.html
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * Contributors:
+ *     arussel
+ */
+package org.nuxeo.ecm.platform.routing.api;
+
+/**
+ * @author arussel
+ *
+ */
+public interface DocumentRoutingConstants {
+    String DOCUMENT_ROUTE_INSTANCES_ROOT_DOCUMENT_TYPE = "DocumentRouteInstancesRoot";
+
+    String DOCUMENT_ROUTE_INSTANCES_ROOT_ID = "document-route-instances-root";
+
+    String DOCUMENT_ROUTE_DOCUMENT_TYPE = "DocumentRoute";
+
+    String STEP_DOCUMENT_TYPE = "Step";
+
+    String STEP_FOLDER_DOCUMENT_TYPE = "StepFolder";
+
+}
---
+++ nuxeo-platform-document-routing-core/src/main/java/org/nuxeo/ecm/platform/routing/core/impl/DocumentRoutingServiceImpl.java
@@ -0,0 +1,46 @@
+/*
+ * (C) Copyright 2009 Nuxeo SA (http://nuxeo.com/) and contributors.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser General Public License
+ * (LGPL) version 2.1 which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl.html
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * Contributors:
+ *     arussel
+ */
+package org.nuxeo.ecm.platform.routing.core.impl;
+
+import java.util.List;
+
+import org.nuxeo.ecm.core.api.CoreSession;
+import org.nuxeo.ecm.core.api.DocumentModel;
+import org.nuxeo.ecm.platform.routing.api.DocumentRoute;
+import org.nuxeo.ecm.platform.routing.api.DocumentRoutingService;
+import org.nuxeo.runtime.model.DefaultComponent;
+
+/**
+ * @author arussel
+ *
+ */
+public class DocumentRoutingServiceImpl extends DefaultComponent implements
+        DocumentRoutingService {
+
+    @Override
+    public DocumentRoute createNewInstance(DocumentRoute model,
+            List<DocumentModel> documents, boolean startInstance) {
+        return null;
+    }
+
+    @Override
+    public List<DocumentModel> getAvailableDocumentRouteModel(
+            CoreSession session) {
+        return null;
+    }
+
+}
