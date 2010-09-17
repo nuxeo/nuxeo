fd6850b07296fe28d45a8cc21382a69c0b7d095c
Anahide Tchertchian <at@nuxeo.com>
2010-09-17 17:03:31 +0200

    NXP-5555: make sure request encoding is set to 'UTF-8' before trying to parse request parameters

--- nuxeo-platform-web-common/src/main/java/org/nuxeo/ecm/platform/ui/web/auth/NuxeoAuthenticationFilter.java
+++ nuxeo-platform-web-common/src/main/java/org/nuxeo/ecm/platform/ui/web/auth/NuxeoAuthenticationFilter.java
@@ -307,6 +307,16 @@ public class NuxeoAuthenticationFilter implements Filter {
     public void doFilter(ServletRequest request, ServletResponse response,
             FilterChain chain) throws IOException, ServletException {
 
+        // NXP-5555: set encoding to UTF-8 in case this method is called before
+        // encoding is set to UTF-8 on the request
+        if (request.getCharacterEncoding() == null) {
+            try {
+                request.setCharacterEncoding("UTF-8");
+            } catch (UnsupportedEncodingException e) {
+                log.error(e);
+            }
+        }
+
         String tokenPage = getRequestedPage(request);
         if (tokenPage.equals(NXAuthConstants.SWITCH_USER_PAGE)) {
             boolean result = switchUser(request, response, chain);
