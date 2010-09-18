14b7e5f0291fd81f029cde977729b4e53c3434bd
Stephane Lacoin <slacoin@nuxeo.com>
2010-09-18 10:34:32 +0200

    Cleanup (javadoc and licenses)

--- nuxeo-platform-queue/nuxeo-platform-queue-core/src/main/java/org/nuxeo/ecm/platform/queue/core/TransactedInstanceHandler.java
+++ nuxeo-platform-queue/nuxeo-platform-queue-core/src/main/java/org/nuxeo/ecm/platform/queue/core/TransactedInstanceHandler.java
@@ -1,3 +1,20 @@
+/*
+ * (C) Copyright 2006-2008 Nuxeo SAS (http://nuxeo.com/) and contributors.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser General Public License
+ * (LGPL) version 2.1 which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl.html
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * Contributors:
+ *     matic
+ */
+
 package org.nuxeo.ecm.platform.queue.core;
 
 import java.lang.reflect.InvocationHandler;
@@ -7,6 +24,13 @@ import java.lang.reflect.Proxy;
 import org.nuxeo.ecm.platform.queue.api.Transacted;
 import org.nuxeo.runtime.transaction.TransactionHelper;
 
+/**
+ * Start a user transaction if needed
+ *
+ * @author matic
+ *
+ * @param <T>
+ */
 public class TransactedInstanceHandler<T> implements InvocationHandler{
 
     protected T object;
