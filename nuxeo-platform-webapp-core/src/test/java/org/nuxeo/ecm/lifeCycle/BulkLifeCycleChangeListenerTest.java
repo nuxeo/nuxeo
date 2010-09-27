6c1a31be69ff234f12331814c4c02448cf6eb16e
Florent Guillaume <fg@nuxeo.com>
2010-09-27 04:29:39 +0200

    NXP-5658: remove JCR backend

--- nuxeo-platform-webapp-core/src/test/java/org/nuxeo/ecm/lifeCycle/BulkLifeCycleChangeListenerTest.java
+++ nuxeo-platform-webapp-core/src/test/java/org/nuxeo/ecm/lifeCycle/BulkLifeCycleChangeListenerTest.java
@@ -1,5 +1,5 @@
 /*
- * (C) Copyright 2006-2009 Nuxeo SAS (http://nuxeo.com/) and contributors.
+ * (C) Copyright 2006-2010 Nuxeo SAS (http://nuxeo.com/) and contributors.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the GNU Lesser General Public License
@@ -12,9 +12,8 @@
  * Lesser General Public License for more details.
  *
  * Contributors:
- *     Nuxeo - initial API and implementation
- *
- * $Id$
+ *     Julien Thimonier
+ *     Florent Guillaume
  */
 
 package org.nuxeo.ecm.lifeCycle;
@@ -24,20 +23,17 @@ import java.util.Collection;
 import org.nuxeo.ecm.core.api.ClientException;
 import org.nuxeo.ecm.core.api.DocumentModel;
 import org.nuxeo.ecm.core.event.EventService;
-import org.nuxeo.ecm.core.repository.jcr.testing.RepositoryOSGITestCase;
+import org.nuxeo.ecm.core.storage.sql.SQLRepositoryTestCase;
 import org.nuxeo.runtime.api.Framework;
 
 /**
  * Simple test Case for MassLifeCycleChangeListener
- *
- * @author Julien Thimonier <jt@nuxeo.com>
  */
-public class BulkLifeCycleChangeListenerTest extends RepositoryOSGITestCase {
+public class BulkLifeCycleChangeListenerTest extends SQLRepositoryTestCase {
 
     @Override
     public void setUp() throws Exception {
         super.setUp();
-        openRepository();
         deployBundle("org.nuxeo.ecm.webapp.core");
     }
 
@@ -46,36 +42,36 @@ public class BulkLifeCycleChangeListenerTest extends RepositoryOSGITestCase {
     }
 
     public void testLifeCycleAPI() throws ClientException {
+        openSession();
 
-        DocumentModel folderDoc = getCoreSession().createDocumentModel("/",
+        DocumentModel folderDoc = session.createDocumentModel("/",
                 "testFolder", "Folder");
-        folderDoc = getCoreSession().createDocument(folderDoc);
-        DocumentModel testFile1 = getCoreSession().createDocumentModel(
+        folderDoc = session.createDocument(folderDoc);
+        DocumentModel testFile1 = session.createDocumentModel(
                 folderDoc.getPathAsString(), "testFile1", "File");
-        testFile1 = getCoreSession().createDocument(testFile1);
-        DocumentModel testFile2 = getCoreSession().createDocumentModel(
+        testFile1 = session.createDocument(testFile1);
+        DocumentModel testFile2 = session.createDocumentModel(
                 folderDoc.getPathAsString(), "testFile2", "File");
-        testFile2 = getCoreSession().createDocument(testFile2);
+        testFile2 = session.createDocument(testFile2);
 
-        getCoreSession().saveDocument(folderDoc);
-        getCoreSession().saveDocument(testFile1);
-        getCoreSession().saveDocument(testFile2);
+        session.saveDocument(folderDoc);
+        session.saveDocument(testFile1);
+        session.saveDocument(testFile2);
 
-        Collection<String> allowedStateTransitions = getCoreSession().getAllowedStateTransitions(
-                folderDoc.getRef());
+        Collection<String> allowedStateTransitions = session.getAllowedStateTransitions(folderDoc.getRef());
         assertTrue(allowedStateTransitions.contains("approve"));
 
-        assertTrue(getCoreSession().followTransition(folderDoc.getRef(),
-                "approve"));
+        assertTrue(session.followTransition(folderDoc.getRef(), "approve"));
 
-        getCoreSession().save();
+        session.save();
         waitForAsyncExec();
+        session.save(); // process async invalidations
 
-        //Check that the MassCycleListener has changed child files to approved
-        assertEquals("approved", getCoreSession().getCurrentLifeCycleState(
-                testFile1.getRef()));
-        assertEquals("approved", getCoreSession().getCurrentLifeCycleState(
-                testFile2.getRef()));
+        // Check that the MassCycleListener has changed child files to approved
+        assertEquals("approved",
+                session.getCurrentLifeCycleState(testFile1.getRef()));
+        assertEquals("approved",
+                session.getCurrentLifeCycleState(testFile2.getRef()));
     }
 
 }
