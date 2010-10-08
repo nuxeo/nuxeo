5347f6abb05a47fd8c13d3d75b4756477df679ce
Anahide Tchertchian <at@nuxeo.com>
2010-10-08 10:42:55 +0200

    NXP-5665: add multi coverage selection + implement undo/redo behaviours

--- nuxeo-platform-smart-query-jsf/src/main/java/org/nuxeo/ecm/platform/smart/query/jsf/SmartNXQLQueryActions.java
+++ nuxeo-platform-smart-query-jsf/src/main/java/org/nuxeo/ecm/platform/smart/query/jsf/SmartNXQLQueryActions.java
@@ -34,6 +34,7 @@ import org.jboss.seam.annotations.Scope;
 import org.jboss.seam.annotations.web.RequestParameter;
 import org.nuxeo.ecm.core.api.ClientException;
 import org.nuxeo.ecm.core.api.SortInfo;
+import org.nuxeo.ecm.platform.smart.query.HistoryList;
 import org.nuxeo.ecm.platform.smart.query.SmartQuery;
 import org.nuxeo.ecm.platform.ui.web.util.ComponentUtils;
 
@@ -46,7 +47,13 @@ public class SmartNXQLQueryActions implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
+    public static final int HISTORY_CAPACITY = 20;
+
     protected String queryPart;
+
+    protected HistoryList<String> queryPartHistory;
+
+    protected HistoryList<String> redoQueryPartHistory;
 
     protected IncrementalSmartNXQLQuery currentSmartQuery;
 
@@ -66,6 +73,7 @@ public class SmartNXQLQueryActions implements Serializable {
 
     public void setQueryPart(String queryPart) {
         this.queryPart = queryPart;
+        addToQueryPartHistory(queryPart);
     }
 
     public List<String> getSelectedLayoutColumns() {
@@ -92,13 +100,22 @@ public class SmartNXQLQueryActions implements Serializable {
         return new SortInfo("", true);
     }
 
-    public void initCurrentSmartQuery(String existingQueryPart) {
+    public void initCurrentSmartQuery(String existingQueryPart,
+            boolean resetHistory) {
         currentSmartQuery = new IncrementalSmartNXQLQuery(existingQueryPart);
+        if (resetHistory) {
+            queryPartHistory = null;
+            addToQueryPartHistory(existingQueryPart);
+        }
+    }
+
+    public void initCurrentSmartQuery(String existingQueryPart) {
+        initCurrentSmartQuery(existingQueryPart, true);
     }
 
     public SmartQuery getCurrentSmartQuery() {
         if (currentSmartQuery == null) {
-            initCurrentSmartQuery("");
+            initCurrentSmartQuery("", true);
         }
         return currentSmartQuery;
     }
@@ -118,6 +135,8 @@ public class SmartNXQLQueryActions implements Serializable {
                 // also set it on source component in case user navigates
                 // somewhere else
                 setQueryPart(newQuery);
+            } else {
+                addToQueryPartHistory(newQuery);
             }
         } else {
             throw new ClientException("Component not found");
@@ -134,8 +153,7 @@ public class SmartNXQLQueryActions implements Serializable {
             // find component to update in the hierarchy of JSF components:
             // this is specific to rendering structure...
             EditableValueHolder queryPartComp = ComponentUtils.getComponent(
-                    component, queryPartComponentId,
-                    EditableValueHolder.class);
+                    component, queryPartComponentId, EditableValueHolder.class);
             if (queryPartComp != null) {
                 // set submitted value to ensure validation
                 queryPartComp.setSubmittedValue(newQuery);
@@ -143,11 +161,13 @@ public class SmartNXQLQueryActions implements Serializable {
                 // when adding a new item to the query
                 queryPartComp.setValue(newQuery);
                 // rebuild smart query
-                initCurrentSmartQuery(newQuery);
+                initCurrentSmartQuery(newQuery, false);
                 if (Boolean.TRUE.equals(updateQueryPart)) {
                     // also set current query part in case user navigates
                     // somewhere else
                     setQueryPart(newQuery);
+                } else {
+                    addToQueryPartHistory(newQuery);
                 }
             } else {
                 throw new ClientException("Component not found");
@@ -164,6 +184,91 @@ public class SmartNXQLQueryActions implements Serializable {
 
     public void clearQueryPart(ActionEvent event) throws ClientException {
         setQueryPart(event, "");
+    }
+
+    protected String getCurrentQueryPart() {
+        if (currentSmartQuery != null) {
+            return currentSmartQuery.getExistingQueryPart();
+        }
+        return null;
+    }
+
+    protected boolean hasQueryPartHistory(HistoryList<String> history) {
+        if (history == null || history.isEmpty()) {
+            return false;
+        }
+        String lastQueryPart = history.getLast();
+        // lastQueryPart cannot be null
+        if (history.size() == 1 && lastQueryPart.equals(getCurrentQueryPart())) {
+            return false;
+        }
+        return true;
+    }
+
+    public boolean getCanUndoQueryPartChanges() {
+        return hasQueryPartHistory(queryPartHistory);
+    }
+
+    public boolean getCanRedoQueryPartChanges() {
+        return hasQueryPartHistory(redoQueryPartHistory);
+    }
+
+    public void undoHistoryChanges(ActionEvent event,
+            HistoryList<String> history, HistoryList<String> redoHistory)
+            throws ClientException {
+        if (!hasQueryPartHistory(history)) {
+            return;
+        }
+        String lastQueryPart = history.getLast();
+        history.removeLast();
+        String currentQueryPart = getCurrentQueryPart();
+        // lastQueryPart cannot be null
+        if (!lastQueryPart.equals(currentQueryPart)) {
+            setQueryPart(event, lastQueryPart);
+        } else if (history.size() > 0) {
+            lastQueryPart = history.getLast();
+            setQueryPart(event, lastQueryPart);
+            history.removeLast();
+        }
+        if (redoHistory != null) {
+            addToHistory(currentQueryPart, redoHistory);
+        }
+    }
+
+    public void undoQueryPartChanges(ActionEvent event) throws ClientException {
+        if (redoQueryPartHistory == null) {
+            redoQueryPartHistory = new HistoryList<String>(HISTORY_CAPACITY);
+        }
+        undoHistoryChanges(event, queryPartHistory, redoQueryPartHistory);
+    }
+
+    public void redoQueryPartChanges(ActionEvent event) throws ClientException {
+        undoHistoryChanges(event, redoQueryPartHistory, null);
+    }
+
+    protected void addToHistory(String queryPart,
+            HistoryList<String> queryPartHistory) {
+        if (queryPartHistory == null) {
+            return;
+        }
+        if (queryPart == null) {
+            queryPart = "";
+        }
+        if (queryPartHistory.size() == 0) {
+            queryPartHistory.addLast(queryPart);
+        } else {
+            String lastQueryPart = queryPartHistory.getLast();
+            if (!queryPart.equals(lastQueryPart)) {
+                queryPartHistory.addLast(queryPart);
+            }
+        }
+    }
+
+    protected void addToQueryPartHistory(String queryPart) {
+        if (queryPartHistory == null) {
+            queryPartHistory = new HistoryList<String>(HISTORY_CAPACITY);
+        }
+        addToHistory(queryPart, queryPartHistory);
     }
 
     public void validateQueryPart(FacesContext context, UIComponent component,
