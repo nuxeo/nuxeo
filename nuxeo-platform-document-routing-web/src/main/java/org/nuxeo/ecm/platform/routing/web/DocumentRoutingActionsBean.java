fe5dc70a0dc26c2d79298b71d9c58643b71cd83f
Laurent Doguin <ldoguin@nuxeo.com>
2010-10-14 18:06:18 +0200

    NXCM-151 add button to add or remove route elements

--- nuxeo-platform-document-routing-web/src/main/java/org/nuxeo/ecm/platform/routing/web/DocumentRoutingActionsBean.java
+++ nuxeo-platform-document-routing-web/src/main/java/org/nuxeo/ecm/platform/routing/web/DocumentRoutingActionsBean.java
@@ -30,6 +30,8 @@ import javax.faces.application.FacesMessage;
 import javax.faces.convert.Converter;
 
 import org.apache.commons.lang.StringUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.jboss.seam.annotations.Factory;
 import org.jboss.seam.annotations.In;
 import org.jboss.seam.annotations.Install;
@@ -38,20 +40,29 @@ import org.jboss.seam.annotations.Observer;
 import org.jboss.seam.annotations.Scope;
 import org.jboss.seam.core.Events;
 import org.jboss.seam.faces.FacesMessages;
+import org.nuxeo.common.collections.ScopedMap;
 import org.nuxeo.ecm.core.api.ClientException;
 import org.nuxeo.ecm.core.api.ClientRuntimeException;
 import org.nuxeo.ecm.core.api.CoreSession;
 import org.nuxeo.ecm.core.api.DocumentModel;
+import org.nuxeo.ecm.core.api.DocumentModelList;
+import org.nuxeo.ecm.core.api.DocumentRef;
 import org.nuxeo.ecm.core.api.IdRef;
+import org.nuxeo.ecm.core.api.NuxeoPrincipal;
+import org.nuxeo.ecm.core.api.PathRef;
+import org.nuxeo.ecm.core.api.event.CoreEventConstants;
 import org.nuxeo.ecm.platform.routing.api.DocumentRoute;
 import org.nuxeo.ecm.platform.routing.api.DocumentRouteElement;
 import org.nuxeo.ecm.platform.routing.api.DocumentRoutingConstants;
 import org.nuxeo.ecm.platform.routing.api.DocumentRoutingService;
 import org.nuxeo.ecm.platform.routing.api.LocalizableDocumentRouteElement;
+import org.nuxeo.ecm.platform.types.Type;
+import org.nuxeo.ecm.platform.types.TypeManager;
 import org.nuxeo.ecm.platform.ui.web.api.NavigationContext;
 import org.nuxeo.ecm.platform.ui.web.api.WebActions;
 import org.nuxeo.ecm.platform.ui.web.model.SelectDataModel;
 import org.nuxeo.ecm.platform.ui.web.model.impl.SelectDataModelImpl;
+import org.nuxeo.ecm.webapp.action.TypesTool;
 import org.nuxeo.ecm.webapp.helpers.EventManager;
 import org.nuxeo.ecm.webapp.helpers.EventNames;
 import org.nuxeo.ecm.webapp.helpers.ResourcesAccessor;
@@ -68,6 +79,12 @@ import org.nuxeo.runtime.api.Framework;
 public class DocumentRoutingActionsBean implements Serializable {
     private static final long serialVersionUID = 1L;
 
+    private static final Log log = LogFactory.getLog(DocumentRoutingActionsBean.class);
+
+    public static final String SOURCE_DOC_NAME = "source_doc_name";
+
+    public static final String ROUTE_DOCUMENT_REF = "route_doc_ref";
+
     @In(required = true, create = true)
     protected NavigationContext navigationContext;
 
@@ -80,13 +97,30 @@ public class DocumentRoutingActionsBean implements Serializable {
     @In(create = true)
     protected WebActions webActions;
 
+    @In(create = true, required = false)
+    protected TypesTool typesTool;
+
     @In(create = true)
     protected ResourcesAccessor resourcesAccessor;
 
     @In(create = true)
+    protected TypeManager typeManager;
+
+    @In(create = true)
     protected EventManager eventManager;
 
+    @In(required = true, create = false)
+    protected NuxeoPrincipal currentUser;
+
     protected String relatedRouteModelDocumentId;
+
+    protected String hiddenSourceDocId;
+
+    protected String hiddenDocOrder;
+
+    enum StepOrder {
+        before, in, after
+    }
 
     public DocumentRoutingService getDocumentRoutingService() {
         try {
@@ -248,12 +282,26 @@ public class DocumentRoutingActionsBean implements Serializable {
         return (doc.hasFacet(DocumentRoutingConstants.ROUTE_STEP_FACET));
     }
 
-    public boolean isUpdatableStep(DocumentModel stepDoc){
-        if (!isStep(stepDoc)){
+    public boolean isUpdatableStep(DocumentModel stepDoc) {
+        if (!isStep(stepDoc)) {
             return false;
         }
         DocumentRouteElement docRouteElement = stepDoc.getAdapter(DocumentRouteElement.class);
         return (docRouteElement.isDraft() || docRouteElement.isReady());
+    }
+
+    public String removeStep(String docId) throws ClientException {
+        if (!getDocumentRoutingService().canUserCreateRoute(currentUser)) {
+            facesMessages.add(FacesMessage.SEVERITY_INFO,
+                    resourcesAccessor.getMessages().get("feedback.document.route.no.creation.rights"));
+            return "";
+        }
+        DocumentRef docRef = new IdRef(docId);
+        DocumentModel stepToDelete = documentManager.getDocument(docRef);
+        getDocumentRoutingService().removeRouteElement(stepToDelete, documentManager);
+        Events.instance().raiseEvent(EventNames.DOCUMENT_CHILDREN_CHANGED,
+                navigationContext.getCurrentDocument());
+        return "";
     }
 
     public String editDocument(DocumentModel doc) throws ClientException {
@@ -263,11 +311,11 @@ public class DocumentRoutingActionsBean implements Serializable {
     public String updateRouteElement() throws ClientException {
         DocumentModel changeableDocument = navigationContext.getChangeableDocument();
         changeableDocument = documentManager.saveDocument(changeableDocument);
-
         documentManager.save();
         // some changes (versioning) happened server-side, fetch new one
         navigationContext.invalidateCurrentDocument();
-        facesMessages.add(FacesMessage.SEVERITY_INFO,
+        facesMessages.add(
+                FacesMessage.SEVERITY_INFO,
                 resourcesAccessor.getMessages().get("document_modified"),
                 resourcesAccessor.getMessages().get(
                         changeableDocument.getType()));
@@ -285,4 +333,105 @@ public class DocumentRoutingActionsBean implements Serializable {
                 docRouteElement.getDocumentRoute(documentManager).getDocument(),
                 "TAB_DOCUMENT_ROUTE_ELEMENTS");
     }
+
+    public String createRouteElement(String typeName)
+            throws ClientException {
+        if (!getDocumentRoutingService().canUserCreateRoute(currentUser)) {
+            facesMessages.add(FacesMessage.SEVERITY_INFO,
+                    resourcesAccessor.getMessages().get("feedback.document.route.no.creation.rights"));
+            return "";
+        }
+        DocumentModel currentDocument = navigationContext.getCurrentDocument();
+        DocumentRef routeRef = currentDocument.getRef();
+        DocumentRef sourceDocRef = new IdRef(
+                hiddenSourceDocId);
+        DocumentModel sourceDoc = documentManager.getDocument(sourceDocRef);
+        String sourceDocName = null;
+        String parentPath = null;
+        if (StepOrder.in.toString().equals(hiddenDocOrder)) {
+            parentPath = sourceDoc.getPathAsString();
+        } else {
+            DocumentModel parentDoc = documentManager.getParentDocument(sourceDocRef);
+            parentPath = parentDoc.getPathAsString();
+            if (StepOrder.before.toString().equals(hiddenDocOrder)) {
+                sourceDocName = sourceDoc.getName();
+            } else {
+                DocumentModelList orderedChilds = getDocumentRoutingService().getOrderedRouteElement(
+                        parentDoc.getId(), documentManager);
+                int selectedDocumentIndex = orderedChilds.indexOf(sourceDoc);
+                int nextIndex = selectedDocumentIndex + 1;
+                if (nextIndex >= orderedChilds.size()) {
+                    sourceDocName = null;
+                } else {
+                    sourceDocName = orderedChilds.get(nextIndex).getName();
+                }
+            }
+        }
+        Type docType = typeManager.getType(typeName);
+        // we cannot use typesTool as intermediary since the DataModel callback
+        // will alter whatever type we set
+        typesTool.setSelectedType(docType);
+        try {
+            DocumentModel changeableDocument = documentManager.createDocumentModel(typeName);
+            ScopedMap context = changeableDocument.getContextData();
+            context.put(CoreEventConstants.PARENT_PATH, parentPath);
+            context.put(SOURCE_DOC_NAME, sourceDocName);
+            context.put(ROUTE_DOCUMENT_REF, routeRef);
+            navigationContext.setChangeableDocument(changeableDocument);
+            return "create_route_element";
+        } catch (Throwable t) {
+            throw ClientException.wrap(t);
+        }
+    }
+
+    public String saveRouteElement() throws ClientException {
+        DocumentModel newDocument = navigationContext.getChangeableDocument();
+        // Document has already been created if it has an id.
+        // This will avoid creation of many documents if user hit create button
+        // too many times.
+        if (newDocument.getId() != null) {
+            log.debug("Document " + newDocument.getName() + " already created");
+            return navigationContext.navigateToDocument(newDocument,
+                    "after-create");
+        }
+        try {
+            String parentDocumentPath = (String) newDocument.getContextData().get(
+                    CoreEventConstants.PARENT_PATH);
+            String sourceDocumentName = (String) newDocument.getContextData().get(
+                    SOURCE_DOC_NAME);
+            DocumentRef routeDocRef = (DocumentRef) newDocument.getContextData().get(
+                    ROUTE_DOCUMENT_REF);
+            getDocumentRoutingService().addRouteElementToRoute(
+                    new PathRef(parentDocumentPath), sourceDocumentName,
+                    newDocument, documentManager);
+
+            DocumentModel routeDocument = documentManager.getDocument(routeDocRef);
+            facesMessages.add(FacesMessage.SEVERITY_INFO,
+                    resourcesAccessor.getMessages().get("document_saved"),
+                    resourcesAccessor.getMessages().get(newDocument.getType()));
+
+            Events.instance().raiseEvent(EventNames.DOCUMENT_CHILDREN_CHANGED,
+                    routeDocument);
+            return navigationContext.navigateToDocument(routeDocument);
+        } catch (Throwable t) {
+            throw ClientException.wrap(t);
+        }
+    }
+
+    public String getHiddenSourceDocId() {
+        return hiddenSourceDocId;
+    }
+
+    public void setHiddenSourceDocId(String hiddenSourceDocId) {
+        this.hiddenSourceDocId = hiddenSourceDocId;
+    }
+
+    public String getHiddenDocOrder() {
+        return hiddenDocOrder;
+    }
+
+    public void setHiddenDocOrder(String hiddenDocOrder) {
+        this.hiddenDocOrder = hiddenDocOrder;
+    }
+
 }
