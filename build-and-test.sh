2db0741d3ab70f7f2a5f181182c1f36ee419eb0e
Alexandre Russel <arussel@nuxeo.com>
2010-10-22 11:35:33 +0200

    NXCM-175 - improve routing FT test

--- build-and-test.sh
+++ build-and-test.sh
@@ -1,20 +1,26 @@
 #!/bin/bash -x
 
-# Build Nuxeo Case Management
-mvn clean install || exit 1
-mvn -Pserver -f nuxeo-platform-document-routing-distribution/pom.xml clean install || exit 1
+# install library
+svn export --force https://svn.nuxeo.org/nuxeo/tools/qa/nuxeo-integration-release/trunk/integration-lib.sh
+HERE=$(cd $(dirname $0); pwd -P)
+. $HERE/integration-lib.sh
+
+# check no one is running on this server
+check_ports_and_kill_ghost_process || exit 1
+
+# Build Document Platform routing
+mvn -f nuxeo-platform-document-routing-distribution/pom.xml clean package || exit 1
 
 # start JBoss
-(cd nuxeo-platform-document-routing-distribution/target && unzip nuxeo-platform-document-routing-distribution-*.zip && rm *.zip) || exit 1
-chmod +x nuxeo-platform-document-routing-distribution/target/nuxeo-dm-server/bin/nuxeoctl || exit 1
-nuxeo-platform-document-routing-distribution/target/nuxeo-dm-server/bin/nuxeoctl start || exit 1
+chmod +x nuxeo-platform-document-routing-distribution/target/stage/nuxeo-dm-server/bin/nuxeoctl || exit 1
+nuxeo-platform-document-routing-distribution/target/stage/nuxeo-dm-server/bin/nuxeoctl start || exit 1
 
 # Run selenium tests
 HIDE_FF=true nuxeo-platform-document-routing-distribution/ftest/selenium/run.sh
 ret1=$?
 
 # Strop JBoss
-nuxeo-platform-document-routing-distribution/target/nuxeo-dm-server/bin/nuxeoctl stop || exit 1
+nuxeo-platform-document-routing-distribution/target/stage/nuxeo-dm-server/bin/nuxeoctl stop || exit 1
 
 # Exit if some tests failed
 [ $ret1 -eq 0 ] || exit 9
