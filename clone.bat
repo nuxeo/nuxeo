9b2edc607a155dcac6efa6ada53117750523eaa9
Julien Carsique <jcarsique@nuxeo.com>
2010-10-25 18:52:51 +0200

    avoid cloning incomplete repositories, preventing tags listing, update on a release from another branch, ...

--- clone.bat
+++ clone.bat
@@ -1,25 +1,24 @@
 @echo off
 rem This script will checkout the needed sources
 
-set CV=5.4
-set PV=5.4
+set VERSION=5.4
 
-hg clone -r %CV% http://hg.nuxeo.org/nuxeo/nuxeo-common
-hg clone -r %CV% http://hg.nuxeo.org/nuxeo/nuxeo-runtime
-hg clone -r %CV% http://hg.nuxeo.org/nuxeo/nuxeo-core
+hg clone http://hg.nuxeo.org/nuxeo/nuxeo-common
+hg clone http://hg.nuxeo.org/nuxeo/nuxeo-runtime
+hg clone http://hg.nuxeo.org/nuxeo/nuxeo-core
 
-hg clone -r %PV% http://hg.nuxeo.org/nuxeo/nuxeo-services
-hg clone -r %PV% http://hg.nuxeo.org/nuxeo/nuxeo-theme
-hg clone -r %PV% http://hg.nuxeo.org/nuxeo/nuxeo-webengine
-hg clone -r %PV% http://hg.nuxeo.org/nuxeo/nuxeo-jsf
-hg clone -r %PV% http://hg.nuxeo.org/nuxeo/nuxeo-gwt
-hg clone -r %PV% http://hg.nuxeo.org/nuxeo/nuxeo-features
-hg clone -r %PV% http://hg.nuxeo.org/nuxeo/nuxeo-dm
+hg clone http://hg.nuxeo.org/nuxeo/nuxeo-services
+hg clone http://hg.nuxeo.org/nuxeo/nuxeo-theme
+hg clone http://hg.nuxeo.org/nuxeo/nuxeo-webengine
+hg clone http://hg.nuxeo.org/nuxeo/nuxeo-jsf
+hg clone http://hg.nuxeo.org/nuxeo/nuxeo-gwt
+hg clone http://hg.nuxeo.org/nuxeo/nuxeo-features
+hg clone http://hg.nuxeo.org/nuxeo/nuxeo-dm
 
-hg clone -r %PV% http://hg.nuxeo.org/nuxeo/nuxeo-distribution
+hg clone http://hg.nuxeo.org/nuxeo/nuxeo-distribution
 
-hg clone -r %PV% http://hg.nuxeo.org/addons ../nuxeo-addons
+hg clone http://hg.nuxeo.org/addons ../nuxeo-addons
 
 svn export https://svn.nuxeo.org/nuxeo/tools/mercurial .
 
-hgx %PV% %CV% up -C
+hgf up -C %VERSION%
--- clone.sh
+++ clone.sh
@@ -1,25 +1,23 @@
 # This script will check out the needed sources from mercurial.
 
-CV=5.4 # Core version
-PV=5.4 # Platform version
+VERSION=5.4
 
-hg clone -r $CV http://hg.nuxeo.org/nuxeo/nuxeo-common
-hg clone -r $CV http://hg.nuxeo.org/nuxeo/nuxeo-runtime
-hg clone -r $CV http://hg.nuxeo.org/nuxeo/nuxeo-core
+hg clone http://hg.nuxeo.org/nuxeo/nuxeo-common
+hg clone http://hg.nuxeo.org/nuxeo/nuxeo-runtime
+hg clone http://hg.nuxeo.org/nuxeo/nuxeo-core
 
-hg clone -r $PV http://hg.nuxeo.org/nuxeo/nuxeo-services
-hg clone -r $PV http://hg.nuxeo.org/nuxeo/nuxeo-theme
-hg clone -r $PV http://hg.nuxeo.org/nuxeo/nuxeo-webengine
-hg clone -r $PV http://hg.nuxeo.org/nuxeo/nuxeo-jsf
-hg clone -r $PV http://hg.nuxeo.org/nuxeo/nuxeo-gwt
-hg clone -r $PV http://hg.nuxeo.org/nuxeo/nuxeo-features
-hg clone -r $PV http://hg.nuxeo.org/nuxeo/nuxeo-dm
+hg clone http://hg.nuxeo.org/nuxeo/nuxeo-services
+hg clone http://hg.nuxeo.org/nuxeo/nuxeo-theme
+hg clone http://hg.nuxeo.org/nuxeo/nuxeo-webengine
+hg clone http://hg.nuxeo.org/nuxeo/nuxeo-jsf
+hg clone http://hg.nuxeo.org/nuxeo/nuxeo-gwt
+hg clone http://hg.nuxeo.org/nuxeo/nuxeo-features
+hg clone http://hg.nuxeo.org/nuxeo/nuxeo-dm
 
-hg clone -r $PV http://hg.nuxeo.org/nuxeo/nuxeo-distribution
+hg clone http://hg.nuxeo.org/nuxeo/nuxeo-distribution
 
-hg clone -r $PV http://hg.nuxeo.org/addons ../nuxeo-addons
+hg clone http://hg.nuxeo.org/addons ../nuxeo-addons
 
 svn export https://svn.nuxeo.org/nuxeo/tools/mercurial/.hgfunctions
 . .hgfunctions
-
-hgx $PV $CV up -C
+hgf up -C $VERSION
