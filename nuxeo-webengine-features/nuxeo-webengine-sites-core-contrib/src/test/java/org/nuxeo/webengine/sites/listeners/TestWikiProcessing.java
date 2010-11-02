637ab44ad8e8869d6a377386d110cb8d6eb0f0ad
Stefane Fermigier <sfermigier@nuxeo.com>
2010-11-02 19:56:48 +0100

    Cleanup (slightly) test.

--- nuxeo-webengine-features/nuxeo-webengine-sites-core-contrib/src/test/java/org/nuxeo/webengine/sites/listeners/TestWikiProcessing.java
+++ nuxeo-webengine-features/nuxeo-webengine-sites-core-contrib/src/test/java/org/nuxeo/webengine/sites/listeners/TestWikiProcessing.java
@@ -1,5 +1,5 @@
 package org.nuxeo.webengine.sites.listeners;
-import java.io.File;
+
 import java.io.FileInputStream;
 import java.io.InputStream;
 import java.util.List;
@@ -10,39 +10,31 @@ import org.nuxeo.common.utils.FileUtils;
 import org.nuxeo.runtime.test.NXRuntimeTestCase;
 import org.nuxeo.webengine.sites.listeners.SitesWikiListener.WikiProcessingResult;
 
+import com.google.inject.internal.Lists;
+
 public class TestWikiProcessing extends NXRuntimeTestCase {
 
     protected Log log = LogFactory.getLog(TestWikiProcessing.class);
 
     public void testProcessing() throws Exception {
-
         InputStream is = new FileInputStream(FileUtils
                 .getResourceFileFromContext("test-data/page1.wiki"));
-
         String wikiInput = FileUtils.read(is);
-
         SitesWikiListener listener = new SitesWikiListener();
 
         WikiProcessingResult result = listener.processWikiContent(wikiInput,
                 "/nuxeo/site/sites", "/nuxeo/site/sites/mypage");
-
         List<String> relations = result.getRelationLinks();
 
+        List<String> expected = Lists.newArrayList("/nuxeo/site/sites/mypage/WikiPage1",
+        		"/nuxeo/site/sites/mypage/WikiPage2", "/nuxeo/site/sites/mypage/Page"); 
+        assertTrue(relations.containsAll(expected));
+
         String content = result.getWikiContent().trim();
-
-        log.debug(content);
-
-        log.debug(relations);
-
         WikiProcessingResult result2 = listener.processWikiContent(content,
                 "/nuxeo/site/sites", "/nuxeo/site/sites/mypage");
-
         String content2 = result2.getWikiContent().trim();
-
-        log.debug(content2);
-
         assertEquals(content, content2);
-
     }
 
 }
