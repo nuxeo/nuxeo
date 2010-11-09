8e6b570921506ba5165afc3a56a325caa7c647b2
Florent Guillaume <fg@nuxeo.com>
2010-11-09 16:33:32 +0100

    Fix bugs, better output

--- clone.py
+++ clone.py
@@ -4,7 +4,7 @@ import re, os, sys
 
 BRANCH = "5.4"
 
-if (sys.argv) == 2:
+if len(sys.argv) == 2:
     branch = sys.argv[1]
 else:
     branch = BRANCH
@@ -15,15 +15,14 @@ def system(cmd):
 
 for line in os.popen("mvn -N help:effective-pom"):
     line = line.strip()
-    print line
     m = re.match("<module>(.*?)</module>", line)
     if not m:
         continue
     addon = m.group(1)
 
-    if not os.path.isdir(addon):
+    if os.path.isdir(addon):
+        print "Existing " + addon
+    else:
         print "Cloning " + addon
         system("hg clone https://hg.nuxeo.org/addons/%s" % addon)
         system("cd %s ; hg up %s" % (addon, branch))
-
-
