f699d9b3cf7db88d640dce6bec38423661b17161
Thierry Martins <tmartins@nuxeo.com>
2010-11-24 17:46:06 +0100

    NXP-6015: Display new lines at <br/> tags in the textarea widget using the TextareaConverter.

--- nuxeo-platform-forms-layout-client/src/main/java/org/nuxeo/ecm/platform/forms/layout/facelets/plugins/TextareaWidgetTypeHandler.java
+++ nuxeo-platform-forms-layout-client/src/main/java/org/nuxeo/ecm/platform/forms/layout/facelets/plugins/TextareaWidgetTypeHandler.java
@@ -1,5 +1,5 @@
 /*
- * (C) Copyright 2006-2007 Nuxeo SAS (http://nuxeo.com/) and contributors.
+ * (C) Copyright 2006-2010 Nuxeo SAS (http://nuxeo.com/) and contributors.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the GNU Lesser General Public License
@@ -13,6 +13,7 @@
  *
  * Contributors:
  *     <a href="mailto:at@nuxeo.com">Anahide Tchertchian</a>
+ *     <a href="mailto:tm@nuxeo.com">Thierry Martins</a>
  *
  * $Id: TextareaWidgetTypeHandler.java 30416 2008-02-21 19:10:37Z atchertchian $
  */
@@ -27,18 +28,25 @@ import org.nuxeo.ecm.platform.forms.layout.api.Widget;
 import org.nuxeo.ecm.platform.forms.layout.api.exceptions.WidgetException;
 import org.nuxeo.ecm.platform.forms.layout.facelets.FaceletHandlerHelper;
 import org.nuxeo.ecm.platform.forms.layout.facelets.LeafFaceletHandler;
+import org.nuxeo.ecm.platform.forms.layout.facelets.TagConfigFactory;
+import org.nuxeo.ecm.platform.forms.layout.facelets.converter.ConvertTextareaHandler;
+import org.nuxeo.ecm.platform.ui.web.converter.TextareaConverter;
 
 import com.sun.facelets.FaceletContext;
 import com.sun.facelets.FaceletHandler;
 import com.sun.facelets.tag.CompositeFaceletHandler;
+import com.sun.facelets.tag.TagAttribute;
 import com.sun.facelets.tag.TagAttributes;
 import com.sun.facelets.tag.TagConfig;
 import com.sun.facelets.tag.jsf.ComponentHandler;
+import com.sun.facelets.tag.jsf.ConvertHandler;
+import com.sun.facelets.tag.jsf.ConverterConfig;
 
 /**
  * Textarea widget.
  *
  * @author <a href="mailto:at@nuxeo.com">Anahide Tchertchian</a>
+ * @author <a href="mailto:tm@nuxeo.com">Thierry Martins</a>
  */
 public class TextareaWidgetTypeHandler extends AbstractWidgetTypeHandler {
 
@@ -63,9 +71,16 @@ public class TextareaWidgetTypeHandler extends AbstractWidgetTypeHandler {
             FaceletHandler[] handlers = { input, message };
             return new CompositeFaceletHandler(handlers);
         } else {
-            // default on text for other modes
+            // default on text with nl2br converter for other modes
+            ConverterConfig convertConfig = TagConfigFactory.createConverterConfig(
+                    tagConfig, new TagAttributes(new TagAttribute[0]), leaf,
+                    TextareaConverter.CONVERTER_ID);
+            TagAttribute escape = helper.createAttribute("escape", "false");
+            attributes = FaceletHandlerHelper.addTagAttribute(attributes,
+                    escape);
+            ConvertHandler convert = new ConvertTextareaHandler(convertConfig);
             ComponentHandler output = helper.getHtmlComponentHandler(
-                    attributes, leaf, HtmlOutputText.COMPONENT_TYPE, null);
+                    attributes, convert, HtmlOutputText.COMPONENT_TYPE, null);
             return output;
         }
     }
