d8273eec213d76ebd3e77af4db5e2eef415cb15e
Florent Guillaume <fg@nuxeo.com>
2010-11-25 15:22:06 +0100

    Fix audit-related test cases following NXP-5997

--- nuxeo-opencmis-tests/src/test/java/org/nuxeo/ecm/core/opencmis/impl/NuxeoSessionTestCase.java
+++ nuxeo-opencmis-tests/src/test/java/org/nuxeo/ecm/core/opencmis/impl/NuxeoSessionTestCase.java
@@ -100,7 +100,8 @@ public abstract class NuxeoSessionTestCase extends SQLRepositoryTestCase {
         deployBundle("org.nuxeo.ecm.core.persistence");
         deployBundle("org.nuxeo.ecm.platform.audit.api");
         deployBundle("org.nuxeo.ecm.platform.audit");
-        deployTestContrib("org.nuxeo.ecm.platform.audit", "nxaudit-tests.xml");
+        deployContrib("org.nuxeo.ecm.core.opencmis.tests.tests",
+                "OSGI-INF/audit-persistence-config.xml");
 
         openSession(); // nuxeo
 
