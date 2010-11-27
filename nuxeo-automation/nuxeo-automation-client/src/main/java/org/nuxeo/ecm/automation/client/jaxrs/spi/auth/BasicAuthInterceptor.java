2edd622e11f463c91e028e7e868d13e94b28e66b
Stephane Lacoin <slacoin@nuxeo.com>
2010-11-27 21:20:03 +0100

    NXP-6041: updated javadoc and license headers

--- nuxeo-automation/nuxeo-automation-client/src/main/java/org/nuxeo/ecm/automation/client/jaxrs/spi/auth/BasicAuthInterceptor.java
+++ nuxeo-automation/nuxeo-automation-client/src/main/java/org/nuxeo/ecm/automation/client/jaxrs/spi/auth/BasicAuthInterceptor.java
@@ -1,3 +1,19 @@
+/*
+ * (C) Copyright 2006-2008 Nuxeo SAS (http://nuxeo.com/) and contributors.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser General Public License
+ * (LGPL) version 2.1 which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl.html
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * Contributors:
+ *     bstefanescu
+ */
 package org.nuxeo.ecm.automation.client.jaxrs.spi.auth;
 
 import org.nuxeo.ecm.automation.client.jaxrs.RequestInterceptor;
@@ -5,6 +21,12 @@ import org.nuxeo.ecm.automation.client.jaxrs.spi.Connector;
 import org.nuxeo.ecm.automation.client.jaxrs.spi.Request;
 import org.nuxeo.ecm.automation.client.jaxrs.util.Base64;
 
+/**
+ * Inject the basic authentication header in the request
+ *
+ * @author matic
+ *
+ */
 public class BasicAuthInterceptor implements RequestInterceptor {
 
     protected String token;
--- nuxeo-automation/nuxeo-automation-client/src/main/java/org/nuxeo/ecm/automation/client/jaxrs/spi/auth/PortalSSOAuthInterceptor.java
+++ nuxeo-automation/nuxeo-automation-client/src/main/java/org/nuxeo/ecm/automation/client/jaxrs/spi/auth/PortalSSOAuthInterceptor.java
@@ -1,3 +1,19 @@
+/*
+ * (C) Copyright 2006-2008 Nuxeo SAS (http://nuxeo.com/) and contributors.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser General Public License
+ * (LGPL) version 2.1 which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl.html
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * Contributors:
+ *     slacoin
+ */
 package org.nuxeo.ecm.automation.client.jaxrs.spi.auth;
 
 import java.security.MessageDigest;
@@ -10,6 +26,11 @@ import org.nuxeo.ecm.automation.client.jaxrs.spi.Connector;
 import org.nuxeo.ecm.automation.client.jaxrs.spi.Request;
 import org.nuxeo.ecm.automation.client.jaxrs.util.Base64;
 
+/**
+ *
+ * @author matic
+ *
+ */
 public class PortalSSOAuthInterceptor implements RequestInterceptor {
 
     protected final String secret;
