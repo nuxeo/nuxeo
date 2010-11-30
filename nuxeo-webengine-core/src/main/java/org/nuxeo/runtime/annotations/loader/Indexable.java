7cc64b818ebea51cd1208f93d97dbb223d4cbcb6
Bogdan Stefanescu <bstefanescu@nuxeo.com>
2010-11-30 12:27:36 +0100

    NXP-6035 - temporary fix for random problems at startup because of webengine activator starting before runtime

--- nuxeo-webengine-core/src/main/java/org/nuxeo/runtime/annotations/loader/Indexable.java
+++ nuxeo-webengine-core/src/main/java/org/nuxeo/runtime/annotations/loader/Indexable.java
@@ -27,7 +27,8 @@ import java.lang.annotation.Target;
 
 /**
  * @author <a href="mailto:bs@nuxeo.com">Bogdan Stefanescu</a>
- *
+ * 
+ * @deprecated not used
  */
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
