4e14d29249e968c7c077452d5bdd9f7b8adcf1ab
Hudson gimli <jcarsique@nuxeo.com>
2010-12-08 16:06:04 +0100

    nx-builder: Release 5.4.0.2-I20101208

--- clone.sh
+++ clone.sh
@@ -1,6 +1,6 @@
 # This script will check out the needed sources from mercurial.
 
-VERSION=5.4.0
+VERSION=release-5.4.0.2-I20101208
 
 for x in nuxeo-common nuxeo-runtime nuxeo-core \
     nuxeo-services nuxeo-theme nuxeo-webengine nuxeo-jsf \
