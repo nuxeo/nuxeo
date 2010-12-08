ce4a839353029cc77a74b895b93bc9fd85da7dcc
Stephane Lacoin <slacoin@nuxeo.com>
2010-12-08 14:41:51 +0100

    missing headers

--- nuxeo-runtime-test/src/test/java/org/nuxeo/runtime/test/runner/CanStreamBlobsTest.java
+++ nuxeo-runtime-test/src/test/java/org/nuxeo/runtime/test/runner/CanStreamBlobsTest.java
@@ -1,3 +1,19 @@
+/*
+ * (C) Copyright 2006-2010 Nuxeo SAS (http://nuxeo.com/) and contributors.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser General Public License
+ * (LGPL) version 2.1 which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl.html
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * Contributors:
+ *     slacoin
+ */
 package org.nuxeo.runtime.test.runner;
 
 import static org.hamcrest.Matchers.is;
@@ -12,7 +28,6 @@ import org.nuxeo.common.utils.FileUtils;
 import org.nuxeo.runtime.services.streaming.RemoteInputStream;
 import org.nuxeo.runtime.services.streaming.StreamManagerClient;
 import org.nuxeo.runtime.services.streaming.StreamManagerServer;
-import org.nuxeo.runtime.services.streaming.StreamSource;
 import org.nuxeo.runtime.services.streaming.StringSource;
 
 import com.google.inject.Inject;
