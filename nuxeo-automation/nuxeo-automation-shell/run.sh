c60b1077943eec7f1ec43569e6c04e5f2f6e9c6e
Bogdan Stefanescu <bstefanescu@nuxeo.com>
2010-12-28 17:14:18 +0100

    NXP-6160 - moved shell to addons

--- nuxeo-automation/nuxeo-automation-shell/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-cd /Users/bstefanescu/work/nuxeo/nuxeo-features/nuxeo-automation/nuxeo-automation-shell
-
-java -Xdebug -Xrunjdwp:transport=dt_socket,address=8000,server=y,suspend=n -cp target/nuxeo-automation-shell-1.0-SNAPSHOT.jar:target/nuxeo-automation-shell-1.0-SNAPSHOT-all.jar org.nuxeo.ecm.shell.Main http://localhost:8080/nuxeo/site/automation
-
-
-#sleep 10
