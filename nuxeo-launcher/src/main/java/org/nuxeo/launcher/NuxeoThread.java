42400e56308cf833f6c4ae4d3e3ee99a4b9dd9b8
Julien Carsique <jcarsique@nuxeo.com>
2011-01-03 11:45:20 +0100

    NXP-5847 - Nuxeo java launcher; work in progress; updated nuxeoctl to use ConfigurationGenerator from its new package; use org.apache.directory.daemon:daemon-bootstrappers for process wrapping

--- nuxeo-launcher/src/main/java/org/nuxeo/launcher/NuxeoThread.java
+++ nuxeo-launcher/src/main/java/org/nuxeo/launcher/NuxeoThread.java
@@ -21,7 +21,6 @@ package org.nuxeo.launcher;
 
 import java.io.File;
 import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 import java.net.MalformedURLException;
 
 import org.apache.commons.logging.Log;
@@ -32,26 +31,16 @@ import org.nuxeo.launcher.config.ConfigurationGenerator;
 import org.nuxeo.launcher.config.Environment;
 
 /**
- * Main Nuxeo server thread
- *
  * @author jcarsique
- * @since 5.4.1
+ *
  */
-public class NuxeoThread extends Thread {
-    private static final Log log = LogFactory.getLog(NuxeoThread.class);
+public abstract class NuxeoThread extends Thread {
+    static final Log log = LogFactory.getLog(NuxeoThread.class);
 
-    private static final String TOMCAT_STARTUP_CLASS = "org.apache.catalina.startup.Bootstrap";
-
-    private static final String JBOSS_STARTUP_CLASS = "org.jboss.Main";
-
-    private static final String JETTY_STARTUP_CLASS = "org.nuxeo.osgi.application.Main";
-
-    private ConfigurationGenerator configurationGenerator;
-
-    // private MutableClassLoader loader;
+    protected ConfigurationGenerator configurationGenerator;
 
     @SuppressWarnings("rawtypes")
-    private Class startupClass;
+    protected Class startupClass;
 
     public NuxeoThread(ConfigurationGenerator configurationGenerator) {
         super("Nuxeo");
@@ -60,36 +49,22 @@ public class NuxeoThread extends Thread {
 
     @Override
     public void run() {
-        if (configurationGenerator.isJBoss) {
-            throw new UnsupportedOperationException();
-        } else if (configurationGenerator.isJetty) {
-            throw new UnsupportedOperationException();
-        } else if (configurationGenerator.isTomcat) {
-            // setTomcatLogs();
-            startTomcat();
-        }
-    }
-
-    private void startTomcat() {
         MutableClassLoader loader = new MutableClassLoaderDelegate(
                 getClass().getClassLoader());
         // setJavaProperties();
-        setTomcatClassPath(loader);
-        setTomcatSystemProperties();
+        setClassPath(loader);
+        setSystemProperties();
         setNuxeoSystemProperties();
+        log.debug("1");
         configurationGenerator.initLogs();
+        log.debug("2");
+        System.out.println("2");
         try {
-            startupClass = loader.getClassLoader().loadClass(
-                    TOMCAT_STARTUP_CLASS);
-            Object server = startupClass.newInstance();
-            @SuppressWarnings("unchecked")
-            Method method = startupClass.getMethod("main", String[].class);
-            method.invoke(server, new Object[] { new String[] { "start" } });
+            startServer(loader);
         } catch (ClassNotFoundException e) {
-            throw new RuntimeException("Could not find Tomcat startup class", e);
+            throw new RuntimeException("Could not find startup class", e);
         } catch (InstantiationException e) {
-            throw new RuntimeException(
-                    "Could not instanciate Tomcat startup class", e);
+            throw new RuntimeException("Could not instanciate startup class", e);
         } catch (IllegalAccessException e) {
             throw new RuntimeException(e);
         } catch (SecurityException e) {
@@ -103,12 +78,16 @@ public class NuxeoThread extends Thread {
         }
     }
 
-    // private void setJavaProperties() {
-    // String javaOpts = configurationGenerator.getUserConfig().getProperty(
-    // "JAVA_OPTS");
-    // }
+    protected abstract void setSystemProperties();
 
-    private void setNuxeoSystemProperties() {
+    protected abstract void setClassPath(MutableClassLoader loader);
+
+    protected abstract void startServer(MutableClassLoader loader)
+            throws ClassNotFoundException, InstantiationException,
+            IllegalAccessException, SecurityException, NoSuchMethodException,
+            IllegalArgumentException, InvocationTargetException;
+
+    protected void setNuxeoSystemProperties() {
         System.setProperty("nuxeo.home",
                 configurationGenerator.getNuxeoHome().getPath());
         System.setProperty("nuxeo.conf",
@@ -125,34 +104,16 @@ public class NuxeoThread extends Thread {
                 configurationGenerator.getUserConfig().getProperty(property));
     }
 
-    private void setTomcatSystemProperties() {
-        System.setProperty("java.util.logging.manager",
-                "org.apache.juli.ClassLoaderLogManager");
-        System.setProperty("catalina.base",
-                configurationGenerator.getNuxeoHome().getPath());
-        System.setProperty("catalina.home",
-                configurationGenerator.getNuxeoHome().getPath());
-    }
-
-    private void setTomcatClassPath(MutableClassLoader loader) {
-        try {
-            addToClassPath(loader, "nxserver" + File.separator + "lib");
-            addToClassPath(loader, "bin" + File.separator + "bootstrap.jar");
-        } catch (MalformedURLException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    private void addToClassPath(MutableClassLoader loader, String filename)
+    protected void addToClassPath(MutableClassLoader loader, String filename)
             throws MalformedURLException {
-        File classPathEntry = new File(configurationGenerator.getNuxeoHome(),
-                filename);
-        if (!classPathEntry.exists()) {
-            throw new RuntimeException(
-                    "Tried to add inexistant classpath entry: "
-                            + classPathEntry);
-        }
-        loader.addURL(classPathEntry.toURI().toURL());
-    }
+                File classPathEntry = new File(configurationGenerator.getNuxeoHome(),
+                        filename);
+                if (!classPathEntry.exists()) {
+                    throw new RuntimeException(
+                            "Tried to add inexistant classpath entry: "
+                                    + classPathEntry);
+                }
+                loader.addURL(classPathEntry.toURI().toURL());
+            }
 
 }
