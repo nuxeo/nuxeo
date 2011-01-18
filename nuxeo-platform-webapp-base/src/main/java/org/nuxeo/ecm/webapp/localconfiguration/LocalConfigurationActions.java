67c25ef69bbaf0faa85922715def816c5c18292f
Thomas Roger <troger@nuxeo.com>
2011-01-18 14:17:26 +0100

    NXP-6221: send an event when the local configuration is activated / deactivated to recompute the document types Map

--- nuxeo-platform-webapp-base/src/main/java/org/nuxeo/ecm/webapp/localconfiguration/LocalConfigurationActions.java
+++ nuxeo-platform-webapp-base/src/main/java/org/nuxeo/ecm/webapp/localconfiguration/LocalConfigurationActions.java
@@ -69,6 +69,9 @@ public class LocalConfigurationActions implements Serializable {
         documentManager.saveDocument(currentDocument);
         navigationContext.invalidateCurrentDocument();
         documentManager.save();
+
+        Events.instance().raiseEvent(EventNames.LOCAL_CONFIGURATION_CHANGED,
+                navigationContext.getCurrentDocument());
     }
 
     public void saveLocalConfiguration() throws ClientException {
