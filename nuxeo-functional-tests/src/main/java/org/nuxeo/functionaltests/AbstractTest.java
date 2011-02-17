eb2f2e10f20b4fc032ee825316595717f8791243
Florent Guillaume <fg@nuxeo.com>
2011-02-17 17:31:52 +0100

    Add firebug extension inside selenium FireFox for easier debug

--- nuxeo-functional-tests/src/main/java/org/nuxeo/functionaltests/AbstractTest.java
+++ nuxeo-functional-tests/src/main/java/org/nuxeo/functionaltests/AbstractTest.java
@@ -17,6 +17,12 @@
  */
 package org.nuxeo.functionaltests;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.net.URL;
+
+import org.apache.commons.io.IOUtils;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -27,10 +33,16 @@ import org.openqa.selenium.firefox.FirefoxProfile;
  */
 public abstract class AbstractTest {
 
+    private static final String FIREBUG_XPI = "firebug-1.6.2-fx.xpi";
+
+    private static final String FIREBUG_VERSION = "1.6.2";
+
     protected static FirefoxDriver driver;
 
+    protected static File tmp_firebug_xpi;
+
     @BeforeClass
-    public static void initDriver() {
+    public static void initDriver() throws Exception {
         FirefoxProfile profile = new FirefoxProfile();
 
         // Set english as default language
@@ -41,13 +53,53 @@ public abstract class AbstractTest {
         profile.setPreference("general.useragent.extra.nuxeo",
                 "Nuxeo-Selenium-Tester");
 
+        addFireBug(profile);
+
         driver = new FirefoxDriver(profile);
     }
 
     @AfterClass
     public static void quitDriver() {
-        driver.close();
-        driver = null;
+        if (driver != null) {
+            driver.close();
+            driver = null;
+        }
+        removeFireBug();
+    }
+
+    protected static void addFireBug(FirefoxProfile profile) throws Exception {
+        ClassLoader cl = Thread.currentThread().getContextClassLoader();
+        URL xpi_url = cl.getResource(FIREBUG_XPI);
+        File xpi;
+        if (xpi_url.getProtocol().equals("file")) {
+            xpi = new File(xpi_url.getPath());
+        } else {
+            // copy to a file
+            InputStream firebug = cl.getResourceAsStream(FIREBUG_XPI);
+            if (firebug == null) {
+                throw new RuntimeException(FIREBUG_XPI + " resource not found");
+            }
+            File tmp = File.createTempFile("nxfirebug", null);
+            tmp.delete();
+            tmp.mkdir();
+            xpi = new File(tmp, FIREBUG_XPI);
+            FileOutputStream out = new FileOutputStream(xpi);
+            IOUtils.copy(firebug, out);
+            firebug.close();
+            out.close();
+            tmp_firebug_xpi = xpi;
+        }
+        profile.addExtension(xpi);
+        // avoid "first run" page
+        profile.setPreference("extensions.firebug.currentVersion",
+                FIREBUG_VERSION);
+    }
+
+    protected static void removeFireBug() {
+        if (tmp_firebug_xpi != null) {
+            tmp_firebug_xpi.delete();
+            tmp_firebug_xpi.getParentFile().delete();
+        }
     }
 
 }
