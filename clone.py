47a843e87ed2c2c0f6810e977d35019b4e20cbaa
Julien Carsique <jcarsique@nuxeo.com>
2011-03-08 14:46:21 +0100

    improvement: update sub repositories if already cloned

--- clone.py
+++ clone.py
@@ -4,7 +4,7 @@ import re, os, sys
 
 BRANCH = "5.4.0"
 
-if (sys.argv) == 2:
+if len(sys.argv) == 2:
     branch = sys.argv[1]
 else:
     branch = BRANCH
@@ -13,17 +13,18 @@ def system(cmd):
     print cmd
     os.system(cmd)
 
+system("hg pull; hg up %s" % branch)
 for line in os.popen("mvn -N help:effective-pom"):
     line = line.strip()
-    print line
     m = re.match("<module>(.*?)</module>", line)
     if not m:
         continue
     addon = m.group(1)
 
-    if not os.path.isdir(addon):
+    if os.path.isdir(addon):
+        print "Updating " + addon
+        system("hg pull -R %s" % addon)
+    else:
         print "Cloning " + addon
         system("hg clone https://hg.nuxeo.org/addons/%s" % addon)
-        system("cd %s ; hg up %s" % (addon, branch))
-
-
+    system("cd %s ; hg up %s" % (addon, branch))
