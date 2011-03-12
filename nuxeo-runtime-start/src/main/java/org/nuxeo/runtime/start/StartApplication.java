076453df2b42d737066621a39258a270499b106b
Stephane Lacoin <slacoin@nuxeo.com>
2011-03-12 20:12:59 +0100

    NXP-6273 configure logging if needed

--- nuxeo-runtime-start/src/main/java/org/nuxeo/runtime/start/StartApplication.java
+++ nuxeo-runtime-start/src/main/java/org/nuxeo/runtime/start/StartApplication.java
@@ -18,6 +18,12 @@ package org.nuxeo.runtime.start;
 
 import javax.naming.NamingException;
 
+import org.slf4j.LoggerFactory;
+
+import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.classic.joran.JoranConfigurator;
+import ch.qos.logback.core.joran.spi.JoranException;
+
 import org.nuxeo.common.jndi.NamingContextFactory;
 import org.nuxeo.runtime.api.Framework;
 import org.nuxeo.runtime.jtajca.NuxeoContainer;
@@ -30,45 +36,68 @@ import org.osgi.framework.ServiceReference;
 import org.osgi.service.packageadmin.PackageAdmin;
 
 /**
- * This bundle should be put in a startlevel superior than the one used to start nuxeo bundles.
- * When the bundle is started it will send an application started notification.
- *
+ * This bundle should be put in a startlevel superior than the one used to start
+ * nuxeo bundles. When the bundle is started it will send an application started
+ * notification.
+ * 
  * @author <a href="mailto:bs@nuxeo.com">Bogdan Stefanescu</a>
- *
+ * 
  */
 public class StartApplication implements BundleActivator {
 
     @Override
     public void start(BundleContext context) throws Exception {
+       configureLogging();
         startWebServices(context);
         startRuntime(context);
         startContainer();
-        ((OSGiRuntimeService)Framework.getRuntime()).fireApplicationStarted();
+        ((OSGiRuntimeService) Framework.getRuntime()).fireApplicationStarted();
     }
 
-    private void startRuntime(BundleContext context) throws BundleException {
+    protected void startRuntime(BundleContext context) throws BundleException {
         try {
             context.getBundle().loadClass("org.nuxeo.runtime.api.Framework");
         } catch (Throwable t) {
-            t.printStackTrace();
+            throw new BundleException("Cannot load framework", t);
         }
         // not in osgi.core r4
-        //FrameworkUtil.getBundle(Framework.class).start();
+        // FrameworkUtil.getBundle(Framework.class).start();
     }
 
-    private void startContainer() throws NamingException {
+    protected void startContainer() throws NamingException {
         NamingContextFactory.install();
         NuxeoContainer.install();
     }
 
-    private void startWebServices(BundleContext context) throws BundleException {
+    protected void configureLogging() throws BundleException {
+        if (System.getProperty("logback.configurationFile") != null) {
+            return;
+        }
+        String home = System.getProperty("nuxeo.home");
+        if (home == null) {
+            return;
+        }
+        final String config = home.concat("/config/logback.xml");
+        LoggerContext lc = (LoggerContext) LoggerFactory.getILoggerFactory();
+        JoranConfigurator configurator = new JoranConfigurator();
+        configurator.setContext(lc);
+        lc.reset();
+        try {
+            configurator.doConfigure(config);
+        } catch (JoranException e) {
+            throw new BundleException("Cannot configure logging from " + config, e);
+        }
+    }
+
+    protected void startWebServices(BundleContext context)
+            throws BundleException {
         ServiceReference pkgAdmin = context.getServiceReference(PackageAdmin.class.getName());
-        PackageAdmin pa = (PackageAdmin)context.getService(pkgAdmin);
+        PackageAdmin pa = (PackageAdmin) context.getService(pkgAdmin);
         tryStartJetty(pa);
         context.ungetService(pkgAdmin);
     }
 
-    private boolean tryStartJetty(PackageAdmin pa) {
+    protected boolean tryStartJetty(PackageAdmin pa) {
         Bundle[] bundles = pa.getBundles("org.eclipse.equinox.http.jetty", null);
         if (bundles != null && bundles.length > 0) {
             try {
