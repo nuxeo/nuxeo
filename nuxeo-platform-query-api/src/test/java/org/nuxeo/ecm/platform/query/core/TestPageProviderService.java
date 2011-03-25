2e20653dafe3e77249b1aacbb73fb0ba5ee9eb2e
Stephane Lacoin <slacoin@nuxeo.com>
2011-03-25 14:19:59 +0100

    added query provider test beside core storage

--- nuxeo-platform-query-api/src/test/java/org/nuxeo/ecm/platform/query/core/TestPageProviderService.java
+++ nuxeo-platform-query-api/src/test/java/org/nuxeo/ecm/platform/query/core/TestPageProviderService.java
@@ -16,21 +16,37 @@
  */
 package org.nuxeo.ecm.platform.query.core;
 
+import java.io.Serializable;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+
+import org.nuxeo.ecm.core.api.AbstractSession;
+import org.nuxeo.ecm.core.storage.sql.SQLRepositoryTestCase;
+import org.nuxeo.ecm.platform.query.api.PageProvider;
 import org.nuxeo.ecm.platform.query.api.PageProviderDefinition;
 import org.nuxeo.ecm.platform.query.api.PageProviderService;
+import org.nuxeo.ecm.platform.query.nxql.CoreQueryDocumentPageProvider;
 import org.nuxeo.runtime.api.Framework;
-import org.nuxeo.runtime.test.NXRuntimeTestCase;
 
 /**
  * @author Anahide Tchertchian
  * @since 5.4
  */
-public class TestPageProviderService extends NXRuntimeTestCase {
+public class TestPageProviderService extends SQLRepositoryTestCase {
+
+    private static final String CURRENT_DOCUMENT_CHILDREN = "CURRENT_DOCUMENT_CHILDREN";
+    private static final String FOO = "foo";
 
     @Override
     public void setUp() throws Exception {
         super.setUp();
-
+        openSession();
+    }
+    
+    @Override
+    protected void deployRepositoryContrib() throws Exception {
+        super.deployRepositoryContrib();
         deployContrib("org.nuxeo.ecm.platform.query.api",
                 "OSGI-INF/pageprovider-framework.xml");
         deployContrib("org.nuxeo.ecm.platform.query.api.test",
@@ -41,12 +57,27 @@ public class TestPageProviderService extends NXRuntimeTestCase {
         PageProviderService service = Framework.getService(PageProviderService.class);
         assertNotNull(service);
 
-        assertNull(service.getPageProviderDefinition("foo"));
+        assertNull(service.getPageProviderDefinition(FOO));
 
-        PageProviderDefinition def = service.getPageProviderDefinition("CURRENT_DOCUMENT_CHILDREN");
+        PageProviderDefinition def = service.getPageProviderDefinition(CURRENT_DOCUMENT_CHILDREN);
         assertNotNull(def);
-        assertEquals("CURRENT_DOCUMENT_CHILDREN", def.getName());
+        assertEquals(CURRENT_DOCUMENT_CHILDREN, def.getName());
         // TODO: test given provider information
     }
 
+    public void testQuery() throws Exception {
+        PageProviderService pps = Framework.getService(PageProviderService.class);
+        assertNotNull(pps);
+
+        PageProviderDefinition ppd = pps.getPageProviderDefinition(CURRENT_DOCUMENT_CHILDREN);
+        ppd.setPattern("SELECT * FROM Document");
+        HashMap<String,Serializable> props = new HashMap<String,Serializable>();
+        props.put(CoreQueryDocumentPageProvider.CORE_SESSION_PROPERTY, (AbstractSession)session);
+        PageProvider<?> pp = pps.getPageProvider(CURRENT_DOCUMENT_CHILDREN, ppd, Collections.EMPTY_LIST, 1L, 0L, props);
+        
+        assertNotNull(pp);
+        List<?> p = pp.getCurrentPage();
+        assertNotNull(p);
+        assertEquals(p.size(), 0);
+    }
 }
