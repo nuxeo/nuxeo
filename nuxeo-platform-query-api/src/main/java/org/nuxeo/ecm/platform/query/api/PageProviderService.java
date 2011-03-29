cdc4dd268304ccb3eeec8a93646b0c4c18d5426b
Anahide Tchertchian <at@nuxeo.com>
2011-03-29 17:14:03 +0200

    better javadoc for the PageProviderService

--- nuxeo-platform-query-api/src/main/java/org/nuxeo/ecm/platform/query/api/PageProviderService.java
+++ nuxeo-platform-query-api/src/main/java/org/nuxeo/ecm/platform/query/api/PageProviderService.java
@@ -29,8 +29,38 @@ import org.nuxeo.ecm.core.api.SortInfo;
  */
 public interface PageProviderService extends Serializable {
 
+    /**
+     * Returns a named page provider definition.
+     * <p>
+     * Useful to share the definition between the page provider service, and
+     * the content view service (as content views can reference a named page
+     * provider that is already registered instead of redefining it).
+     *
+     * @since 5.4
+     * @param name the page provider name
+     * @return the page provider definition or null, if no page provider with
+     *         this name was found.
+     */
     PageProviderDefinition getPageProviderDefinition(String name);
 
+    /**
+     * Returns an instance of page provider with given name and definition.
+     * <p>
+     * Useful to share the definition between the page provider service, and
+     * the content view service (as content views can reference a named page
+     * provider that is already registered instead of redefining it).
+     *
+     * @since 5.4
+     * @param name the name that will be set on the provider.
+     * @param desc the definition used to build the provider instance.
+     * @param sortInfos sort information to set on the provider instance.
+     * @param pageSize the provider page size.
+     * @param currentPage the provider current page index.
+     * @param properties the provider properties
+     * @param parameters the provider parameters.
+     * @return the page provider instance.
+     * @throws ClientException if the page provider instantiation fails.
+     */
     PageProvider<?> getPageProvider(String name, PageProviderDefinition desc,
             List<SortInfo> sortInfos, Long pageSize, Long currentPage,
             Map<String, Serializable> properties, Object... parameters)
