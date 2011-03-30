53855bbf1b852571c95f5d71df8f242a2f320287
Wojciech Sulejman <wsulejman@nuxeo.com>
2011-03-30 17:01:56 -0400

    changing selenium-server version to 2.0a4

--- build-and-test.sh
+++ build-and-test.sh
@@ -3,16 +3,17 @@
 HERE=$(cd $(dirname $0); pwd -P)
 . $HERE/integration-lib.sh
 TMP_DIR=/tmp
+TEST_PDF_FILE_NAME=original.pdf
 
 # check no one is running on this server
 check_ports_and_kill_ghost_process || exit 1
 
 # Build Nuxeo Platform Signature distribution 
 mvn -f nuxeo-platform-signature-distribution/pom.xml clean package || exit 1
-TEST_PDF=nuxeo-platform-signature-distribution/ftest/selenium/samples/original.pdf
-[ -f $TEST_PDF ] && echo "original pdf found"
-cp $TEST_PDF $TMP_DIR
-echo "$TEST_PDF copied to $TMP_DIR"
+TEST_PDF_ORIG=nuxeo-platform-signature-distribution/ftest/selenium/samples/$TEST_PDF_FILE_NAME
+[ -f $TEST_PDF_ORIG ] && echo "original pdf found"
+cp $TEST_PDF_ORIG $TMP_DIR
+echo "$TEST_PDF_ORIG copied to $TMP_DIR"
 
 # start the nuxeo server
 chmod +x nuxeo-platform-signature-distribution/target/stage/nuxeo-dm-server/bin/nuxeoctl || exit 1
@@ -25,7 +26,7 @@ ret1=$?
 # Stop the application server
 nuxeo-platform-signature-distribution/target/stage/nuxeo-dm-server/bin/nuxeoctl stop || exit 1
 
-rm $TMP_DIR/origital.pdf
+rm $TMP_DIR/$TEST_PDF_FILE_NAME
 
 # Exit if some tests failed
 [ $ret1 -eq 0 ] || exit 9
