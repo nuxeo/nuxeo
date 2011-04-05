dc1de1df83190716f2bd42913fac78847e7bed81
Wojciech Sulejman <wsulejman@nuxeo.com>
2011-04-05 15:17:52 -0400

    https://jira.nuxeo.com/browse/NXP-6295 platform independent file upload in the selenium test

--- build-and-test.sh
+++ build-and-test.sh
@@ -2,18 +2,12 @@
 
 HERE=$(cd $(dirname $0); pwd -P)
 . $HERE/integration-lib.sh
-TMP_DIR=/tmp
-TEST_PDF_FILE_NAME=original.pdf
 
 # check no one is running on this server
 check_ports_and_kill_ghost_process || exit 1
 
 # Build Nuxeo Platform Signature distribution 
 mvn -f nuxeo-platform-signature-distribution/pom.xml clean package || exit 1
-TEST_PDF_ORIG=nuxeo-platform-signature-distribution/ftest/selenium/samples/$TEST_PDF_FILE_NAME
-[ -f $TEST_PDF_ORIG ] && echo "original pdf found"
-cp $TEST_PDF_ORIG $TMP_DIR
-echo "$TEST_PDF_ORIG copied to $TMP_DIR"
 
 # start the nuxeo server
 chmod +x nuxeo-platform-signature-distribution/target/stage/nuxeo-dm-server/bin/nuxeoctl || exit 1
@@ -26,7 +20,6 @@ ret1=$?
 # Stop the application server
 nuxeo-platform-signature-distribution/target/stage/nuxeo-dm-server/bin/nuxeoctl stop || exit 1
 
-rm $TMP_DIR/$TEST_PDF_FILE_NAME
 
 # Exit if some tests failed
 [ $ret1 -eq 0 ] || exit 9
