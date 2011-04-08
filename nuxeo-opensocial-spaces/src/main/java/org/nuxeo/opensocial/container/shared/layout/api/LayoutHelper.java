967470770973865b45dd8a1f1e887a52f0071c5a
Vincent Dutat <vincent.dutat@ext.leroymerlin.fr>
2011-04-08 11:04:52 +0200

    NXP-6667: fix typo in name of layout presets X_4_FOOTER_3COLS, X_3_HEADER_2COLS and X_4_HEADER_3COLS

--- nuxeo-opensocial-spaces/src/main/java/org/nuxeo/opensocial/container/shared/layout/api/LayoutHelper.java
+++ nuxeo-opensocial-spaces/src/main/java/org/nuxeo/opensocial/container/shared/layout/api/LayoutHelper.java
@@ -16,8 +16,8 @@ public class LayoutHelper {
     public enum Preset {
         X_1_DEFAULT("x-1-default"), X_2_DEFAULT("x-2-default"), X_2_66_33(
                 "x-2-66-33"), X_2_33_66("x-2-33-66"), X_3_DEFAULT("x-3-default"), X_3_HEADER_2COLS(
-                "x-3-header-2cols"), X_4_FOOTER_3COLS("x-4-footer_3cols"), X_4_HEADER_3COLS(
-                "x-4-header-3cols"), X_4_66_33_50_50("x-4-66-33-50-50"), X_4_50_50_66_33(
+                "x-3-header2cols"), X_4_FOOTER_3COLS("x-4-footer3cols"), X_4_HEADER_3COLS(
+                "x-4-header3cols"), X_4_66_33_50_50("x-4-66-33-50-50"), X_4_50_50_66_33(
                 "x-4-50-50-66-33"), X_4_100_66_33_100("x-4-100-66-33-100"), X_4_100_33_66_100(
                 "x-4-100-33-66-100");
 
