6010d736381661a0eab74874c92b6bbf4d988712
Thierry Delprat <tdelprat@nuxeo.com>
2011-04-27 01:41:52 +0200

    NXP-6689 Externalize DnD UI for translations          Use only Automation          Provide JQuery wrapper for Automation          Improve UI

---
+++ nuxeo-platform-webapp/src/main/resources/web/nuxeo.war/scripts/automation.js
@@ -0,0 +1,109 @@
+function AutomationWrapper(operationId,opts) {
+
+  this.operationId=operationId;
+  this.opts=opts;
+
+  AutomationWrapper.prototype.addParameter = function(name, value){
+    this.opts.automationParams.params[name]=value;
+    return this;
+  }
+
+  AutomationWrapper.prototype.context = function(name, value){
+    this.opts.automationParams.context[name]=value;
+    return this;
+  }
+
+  AutomationWrapper.prototype.setContext = function(ctxParams){
+    jQuery.extend(this.opts.automationParams.context, ctxParams);
+    return this;
+  }
+
+  AutomationWrapper.prototype.execute = function(successCB, failureCB){
+    var targetUrl = this.opts.url + '/' + this.operationId;
+    jQuery.ajax({
+        type: 'POST',
+        contentType : 'application/json+nxrequest',
+        data: JSON.stringify(this.opts.automationParams),
+        url: targetUrl,
+        timeout: 10000,
+        error: function(xhr, status, e) {
+          if (failureCB) {
+              failureCB(xhr,status,"No Data");
+            } else {
+              console.log("Failed to execute");
+              console.log("Error, Status =" + status);
+            }
+        },
+        success: function(data, status,xhr) {
+          console.log("Executed OK");
+          if (status=="success") {
+            successCB(data,status,xhr);
+          } else {
+            if (failureCB) {
+              failureCB(xhr,status,"No Data");
+            } else {
+              console.log("Error, Status =" + status);
+            }
+          }
+        }
+      })
+  }
+
+  AutomationWrapper.prototype.batchExecute = function(batchId, successCB, failureCB){
+
+    console.log("batchExec");
+
+    this.addParameter("operationId", this.operationId);
+    this.addParameter("batchId", batchId);
+    console.log(opts);
+
+    var targetUrl = this.opts.url + '/batch/execute';
+    console.log(targetUrl);
+    jQuery.ajax({
+        type: 'POST',
+        contentType : 'application/json+nxrequest',
+        data: JSON.stringify(this.opts.automationParams),
+        url: targetUrl,
+        timeout: 10000,
+        error: function(xhr, status, e) {
+          console.log("Failed to execute");
+          if (failureCB) {
+              failureCB(xhr,status,"No Data");
+            } else {
+              console.log("Error, Status =" + status);
+            }
+        },
+        success: function(data, status,xhr) {
+          console.log("Executed OK");
+          if (status=="success") {
+            successCB(JSON.parse(data),status,xhr);
+          } else {
+              if (failureCB) {
+                  failureCB(xhr,status,"No Data");
+                } else {
+                  console.log("Error, Status =" + status);
+                }
+          }
+        }
+      })
+    }
+
+
+}
+
+(function($) {
+
+   $.fn.automation = function ( operationId ) {
+      var opts = jQuery.extend( {}, $.fn.automation.defaults, {});
+      return new AutomationWrapper(operationId, opts);
+   }
+
+   $.fn.automation.defaults = {
+        url : "/nuxeo/site/automation",
+        automationParams : {
+           params : {},
+           context : {}
+       }
+   }
+
+ })(jQuery);
--- nuxeo-platform-webapp/src/main/resources/web/nuxeo.war/scripts/dnd/nxdropzone.js
+++ nuxeo-platform-webapp/src/main/resources/web/nuxeo.war/scripts/dnd/nxdropzone.js
@@ -2,18 +2,24 @@
 //****************************
 // UI Handler
 // contains UI related methods and CallBacks
-function DropZoneUIHandler(idx, dropZoneId, url) {
+function DropZoneUIHandler(idx, dropZoneId, options,targetSelectedCB) {
 
   this.idx=idx;
   this.dropZoneId = dropZoneId;
   this.batchId=null;
   this.nxUploaded=0;
   this.nxUploadStarted=0;
-  this.url=url;
+  this.url=options.url;
+  this.ctx=options.dropContext;
   this.operationsDef=null;
+  this.uploadedFiles = new Array();
+  this.targetSelectedCB = targetSelectedCB;
 
   DropZoneUIHandler.prototype.uploadStarted = function(fileIndex, file){
       this.nxUploadStarted++;
+
+      jQuery("#dndMsgUploadInProgress").css("display","block");
+      jQuery("#dndMsgUploadCompleted").css("display","none");
 
       // UI Display
       var infoDiv = jQuery("<div></div>");
@@ -27,22 +33,22 @@ function DropZoneUIHandler(idx, dropZoneId, url) {
 
       var fileDiv = jQuery("<div></div>");
       fileDiv.addClass("dropzone-info-item");
+      fileDiv.attr("id", "dropzone-info-item-" + this.idx + "-" + fileIndex);
       fileDiv.append(infoDiv);
       fileDiv.append(progressDiv);
 
-      jQuery("#dropzone-info-" + this.idx).after(fileDiv);
+      jQuery("#dropzone-info").after(fileDiv);
   };
 
   DropZoneUIHandler.prototype.uploadFinished = function(fileIndex, file, duration){
-      jQuery("#dropzone-info-" +this.idx + "-" + fileIndex).html(file.fileName + " ("+getReadableFileSizeString(file.fileSize)+") in " + (getReadableDurationString(duration)));
-      jQuery("#dropzone-speed-" + this.idx + "-" + fileIndex).css({
-                'width': '100%',
-                'background-color': 'green'
-              });
-
+      jQuery("#dropzone-info-item-" +this.idx + "-" + fileIndex).css("display","none");
+      var fileDiv = jQuery("<div></div>");
+      fileDiv.addClass("dropzone-info-summary-item");
+      fileDiv.html(file.fileName + " ("+getReadableFileSizeString(file.fileSize)+") in " + (getReadableDurationString(duration)));
+      jQuery("#dropzone-info-summary").append(fileDiv);
       this.nxUploaded++;
-      jQuery("#dropzone-bar-msg-" + this.idx).html("import in progress :" + this.nxUploaded + "/" + this.nxUploadStarted);
-
+      this.uploadedFiles.push(file);
+      jQuery("#dropzone-bar-msg").html(this.nxUploaded + "/" + this.nxUploadStarted);
   };
 
   DropZoneUIHandler.prototype.fileUploadProgressUpdated = function(fileIndex, file, newProgress){
@@ -56,150 +62,113 @@ function DropZoneUIHandler(idx, dropZoneId, url) {
 
   DropZoneUIHandler.prototype.buildUI = function () {
       var dzone = jQuery("#"+this.dropZoneId); // XXX
-
-      var panel = jQuery("<div></div>");
+      dzone.addClass("dropzoneTarget");
+      this.targetSelectedCB();
+/*      var panel = jQuery("<div></div>");
       panel.addClass("dropPanel");
       panel.attr("id", "dropPanel-" + this.idx);
 
       dzone.before(panel);
-      panel.append(dzone);
-
-      var infoBar = jQuery("<div></div>");
-      infoBar.addClass("dropzone-bar");
-      infoBar.attr("id", "dropzone-bar-" + this.idx);
-      infoBar.html("<span id=\"dropzone-bar-msg-" + this.idx + "\" class=\"dropzone-bar-msg\"></span><input id=\"dropzone-bar-btn-" + this.idx + "\" class=\"dropzone-bar-btn\" type=\"button\" value=\"Continue\">");
-
-      var infoBlock = jQuery("<div></div>");
-      infoBlock.addClass("dropzone-info-block");
-      infoBlock.attr("id", "dropzone-info-block-" + this.idx);
-
-      var info = jQuery("<div></div>");
-      info.addClass("dropzone-info");
-      info.attr("id", "dropzone-info-" + this.idx);
-
-      infoBlock.append(info);
-      dzone.after(infoBlock);
-      dzone.after(infoBar);
-
+      panel.append(dzone);*/
     }
 
 
   DropZoneUIHandler.prototype.batchStarted = function(){
-
       if (this.batchId==null) {
         this.buildUI(this.dropZoneId);
         this.batchId = "batch-" + new Date().getTime();
-        // Fetch the operation definition
         var o=this; // deRef object !
-        console.log("url=" + url);
-        var targetUrl = url + "chooseOperation/" + this.batchId;
         var context = jQuery("#" + this.dropZoneId).attr("context");
-        targetUrl=targetUrl + "?context=" + context;
-        jQuery.get(targetUrl, function(data, textStatus) {
-          //console.log(data);
-          o.operationsDef=JSON.parse(data);
-          console.log(o);
-        });
+        // Fetch the import options
+        var getOptions = jQuery().automation('ImportOptions.GET');
+        getOptions.addParameter("category",context);
+        getOptions.execute(function(data, textStatus,xhr) {o.operationsDef=data;});
       }
-      jQuery("#dropzone-bar-msg-" + this.idx).html("import in progress ...");
-      jQuery("#dropzone-bar-btn-" + this.idx).css("visibility","hidden");
+      var panel=jQuery("#dropzone-info-panel").remove();
+      jQuery("body").prepend(panel);
+      panel.css("display","block");
+      jQuery("#dndMsgUploadInProgress").css("display","block");
+      jQuery("#dndMsgUploadCompleted").css("display","none");
+      jQuery("#dropzone-bar-msg").html("...");
+      jQuery("#dropzone-bar-btn").css("visibility","hidden");
       return this.batchId;
   }
 
 
   DropZoneUIHandler.prototype.batchFinished = function(batchId) {
-    jQuery("#dropzone-bar-msg-" + this.idx).html("upload completed, click to start the import");
-    jQuery("#dropzone-bar-btn-" + this.idx).css("visibility","visible");
-    jQuery("#dropzone-bar-btn-" + this.idx).attr("value","Continue");
+    jQuery("#dndMsgUploadInProgress").css("display","none");
+    jQuery("#dndMsgUploadCompleted").css("display","block");
+    jQuery("#dropzone-bar-btn").css("visibility","visible");
+    jQuery("#dropzone-bar-btn").attr("value","Continue");
     var o=this; // deRef object !
-    jQuery("#dropzone-bar-btn-" + this.idx).bind("click",function(event) {o.selectOperation(batchId, o.dropZoneId, o.url)});
+    jQuery("#dropzone-bar-btn").bind("click",function(event) {o.selectOperation(batchId, o.dropZoneId, o.url)});
   }
 
   DropZoneUIHandler.prototype.updateForm  = function (event, value) {
     console.log("updateForm : " + value);
     for (i=0; i< this.operationsDef.length; i++) {
-      if(this.operationsDef[i].id==value) {
+      if(this.operationsDef[i].operationId==value) {
        var desc = jQuery("<div></div>");
-       desc.html(this.operationsDef[i].label + "<br/>");
+       desc.html(this.operationsDef[i].description + "<br/>");
        jQuery("#dndSubForm").html(desc);
-       for (j=0; j< this.operationsDef[i].params.length; j++) {
-          var line = jQuery("<div></div>");
-          line.text(this.operationsDef[i].params[j].name);
-          var input = jQuery("<input></input>");
-          input.attr("name", this.operationsDef[i].params[j].name);
-          line.append(input);
-          jQuery("#dndSubForm").append(line);
-        }
         break;
       }
     }
     var panel = jQuery("#dndFormPanel");
-    var dZone = jQuery("#" + this.dropZoneId);
-    var panelPosition =dZone.position();
+    var body = jQuery("body");
+    var panelPosition =body.position();
     jQuery("body").append(panel);
-    if (panel.width() > 0.8*dZone.width()) {
-      panel.css("width",0.8*dZone.width());
+    if (panel.width() > 0.8*body.width()) {
+      panel.css("width",0.8*body.width());
     }
-    panelPosition.top = panelPosition.top + dZone.height()/2 - panel.height()/2;
-    panelPosition.left = panelPosition.left + dZone.width()/2 - panel.width()/2;
+    panelPosition.top = panelPosition.top + body.height()/2 - panel.height()/2;
+    panelPosition.left = panelPosition.left + body.width()/2 - panel.width()/2;
     panel.css(panelPosition);
-
-    console.log("panel width=" + panel.width());
   }
 
   DropZoneUIHandler.prototype.selectOperation  = function (batchId, dropId, url) {
 
     var o=this; // deRef object !
-    console.log("url=" + url);
-    //var targetUrl = url + "chooseOperation/" + batchId;
-    //var context = jQuery("#" + dropId).attr("context");
-    //targetUrl=targetUrl + "?context=" + context;
-    //jQuery.get(targetUrl, function(data, textStatus) {
-    //  jQuery("#" + dropId).html(data);
-    //});
+    console.log(this.operationsDef);
     if (this.operationsDef==null) {
       console.log("No OpDEf found !!!");
     } else {
-      if (this.operationsDef.length==1 && this.operationsDef[0].params.length==0) {
+      if (this.operationsDef.length==1 && this.operationsDef[0].formUrl=='') {
         // XXX start operation right now
+      console.log("Only one operation");
       }
     }
 
     // Build the form
     console.log("build form");
-    var panel = jQuery("<div></div>");
-    var form = jQuery("<form name=\"operationForm\"></form>");
-    var selector = jQuery("<select name=\"operationId\"></select>");
-    selector.attr("id","operationId");
+    var panel = jQuery("#dndFormPanel");
+
+    // update the file list
+    var fileList = jQuery("#uploadedFileList");
+    fileList.html("");
+    for (i=0;i< this.uploadedFiles.length; i++) {
+        var fileItem = jQuery("<div></div>");
+        file = this.uploadedFiles[i];
+        fileItem.html(file.fileName + " ("+getReadableFileSizeString(file.fileSize) + ")");
+        fileList.append(fileItem);
+    }
+
+    // fill the selector
+    var selector = jQuery("#operationIdSelector");
+    selector.html("");
     for (i=0; i< this.operationsDef.length; i++) {
       var optionEntry = jQuery("<option></option>")
-      optionEntry.attr("value",this.operationsDef[i].id);
-      optionEntry.text(this.operationsDef[i].id); // XXX need a short label
+      optionEntry.attr("value",this.operationsDef[i].operationId);
+      optionEntry.text(this.operationsDef[i].label);
       selector.append(optionEntry);
     }
     selector.bind("change", function(event) {o.updateForm(event, selector.val())});
-    form.append(selector);
-    var subForm = jQuery("<div></div>");
-    subForm.attr("id","dndSubForm");
-    form.append(subForm);
-    var buttonForm = jQuery("<input type='button'/>");
-    buttonForm.attr("value","Import");
-    form.append(buttonForm);
-    panel.append(form);
+    var buttonForm = jQuery("#dndFormSubmitButton");
 
-    panel.attr("id", "dndFormPanel");
-    panel.css("position","absolute");
-    panel.css("border","solid 1px black");
-    panel.css("background-color","rgba(255,255,255,0.8");
-    panel.css("padding","4px");
-    panel.css("margin","4px");
-    var panelPosition =jQuery("#" + dropId).position();
-    jQuery("body").append(panel);
-    this.updateForm(null,this.operationsDef[0].id);
+    panel.css("display","block");
+    this.updateForm(null,this.operationsDef[0].operationId);
 
-    jQuery("#dropzone-bar-msg-" + this.idx).html("Select the import operation");
-    //jQuery("#dropzone-bar-btn-" + this.idx).attr("value","Finish");
-    jQuery("#dropzone-bar-btn-" + this.idx).css("display","none");
+    jQuery("#dropzone-info-panel").css("display","none");
 
     buttonForm.bind("click", function(event) {
         var formParams = {};
@@ -207,54 +176,21 @@ function DropZoneUIHandler(idx, dropZoneId, url) {
         jQuery("#dndSubForm>div>:input").each(function(index,element) {
            formParams[element.name] = escape(element.value);
         });
-
-        formParams['operationId'] = jQuery("#operationId").val();
-        formParams['batchId'] = o.batchId;
-
-        var automationParams = {};
-        //automationParams.input = {};
-        automationParams.params = formParams;
-        automationParams.context = {};
-
-        var targetUrl = o.url + "execute"
-
-        jQuery.ajax({
-            type: 'POST',
-            contentType : 'application/json+nxrequest',
-            data: JSON.stringify(automationParams),
-            url: targetUrl,
-            timeout: 2000,
-            error: function() {
-              console.log("Failed to submit");
+        var operationId = jQuery("#operationIdSelector").val();
+        console.log("exec form on operation " + operationId + ", batchId=" + o.batchId);
+        var batchExec=jQuery().automation(operationId);
+        console.log(o.ctx);
+        batchExec.setContext(o.ctx);
+        console.log(batchExec);
+        batchExec.batchExecute(o.batchId,
+            function(data, status,xhr) {
+                console.log("Executed OK");
             },
-            success: function(r) {
-              console.log("Executed OK");
-              //o.removeDropPanel(dropId,batchId);
+            function(xhr,status,e) {
+                console.log("Error while executing batch");
             }
-          })
-
+        );
     });
-
-    jQuery("#dropzone-bar-btn-" + this.idx).bind("click",function(event) {
-       var inputs = [];
-       // XXX
-       jQuery("form[name='operationForm']>:input").each(function(index,element) {
-          inputs.push(element.name + '=' + escape(element.value));
-       });
-       jQuery.ajax({
-           type: 'POST',
-           data: inputs.join('&'),
-           url: jQuery("form[name='operationForm']").attr('action'),
-           timeout: 2000,
-           error: function() {
-             //console.log("Failed to submit");
-           },
-           success: function(r) {
-             o.removeDropPanel(dropId,batchId);
-           }
-         })
-    });
-
   }
 
   DropZoneUIHandler.prototype.removeDropPanel = function(dropId, batchId) {
@@ -267,7 +203,6 @@ function DropZoneUIHandler(idx, dropZoneId, url) {
   }
 
 };
-
 // ******************************
 
 
@@ -277,6 +212,7 @@ function DropZoneUIHandler(idx, dropZoneId, url) {
 var NxDropZoneHandlerIdx=0;
 var ids = new Array();
 var highLightOn=false;
+var targetSelected=false;
 
 (function($) {
 
@@ -285,14 +221,15 @@ var highLightOn=false;
      this.each(function(){
        var dropId = jQuery(this).attr("id");
        ids.push(dropId);
-       //console.log("Init handler for " + dropId)
+       console.log("Init handler for " + dropId)
        // create UI handler
-       var uiHandler = new DropZoneUIHandler(NxDropZoneHandlerIdx++, dropId, options.url);
+       var uiHandler = new DropZoneUIHandler(NxDropZoneHandlerIdx++, dropId, options, function(){targetSelected=true;});
        // copy optionMap
        var instanceOptions = jQuery.extend({},options);
        // register callback Handler
        instanceOptions.handler = uiHandler;
        jQuery("#" + dropId).dropzone(instanceOptions);
+       console.log("Init " + dropId + " done!")
      })
 
       // bind events on body to show the drop box
@@ -303,7 +240,7 @@ var highLightOn=false;
 
      function highlightDropZones(event) {
 
-         if (highLightOn) {
+         if (highLightOn || targetSelected ) {
            return;
          }
 
@@ -331,7 +268,7 @@ var highLightOn=false;
       };
 
       function removeHighlights(event) {
-          if (!highLightOn) {
+          if (!highLightOn || targetSelected ) {
               return;
           }
           //console.log(event);
