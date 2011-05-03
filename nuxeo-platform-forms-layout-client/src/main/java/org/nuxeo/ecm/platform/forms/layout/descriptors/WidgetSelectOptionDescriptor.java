c2b84c1e412732ac695b7b2c473ecac5b4ab0e60
Arnaud Kervern <akervern@nuxeo.com>
2011-05-03 16:05:50 +0200

    NXP-5725: Remove redundants toString call and useless boxing

--- nuxeo-platform-forms-layout-client/src/main/java/org/nuxeo/ecm/platform/forms/layout/descriptors/WidgetSelectOptionDescriptor.java
+++ nuxeo-platform-forms-layout-client/src/main/java/org/nuxeo/ecm/platform/forms/layout/descriptors/WidgetSelectOptionDescriptor.java
@@ -81,15 +81,15 @@ public class WidgetSelectOptionDescriptor implements WidgetSelectOption {
         builder.append(itemLabel).append(";");
         builder.append(itemValue).append(";");
         if (itemDisabled != null) {
-            builder.append(itemDisabled.toString());
+            builder.append(itemDisabled);
         }
         builder.append(";");
         if (itemRendered != null) {
-            builder.append(itemRendered.toString());
+            builder.append(itemRendered);
         }
         builder.append(";");
 
-        Integer intValue = new Integer(builder.toString().hashCode());
+        Integer intValue = builder.toString().hashCode();
         return intValue.toString();
     }
 
