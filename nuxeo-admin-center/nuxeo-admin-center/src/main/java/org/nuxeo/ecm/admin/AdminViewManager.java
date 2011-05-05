b5f6ee5b9b0815cf7b1f367dd4e8ce3ce5411231
Thomas Roger <troger@nuxeo.com>
2011-05-05 21:32:30 +0200

    format

--- nuxeo-admin-center/nuxeo-admin-center/src/main/java/org/nuxeo/ecm/admin/AdminViewManager.java
+++ nuxeo-admin-center/nuxeo-admin-center/src/main/java/org/nuxeo/ecm/admin/AdminViewManager.java
@@ -104,7 +104,9 @@ public class AdminViewManager implements Serializable {
     }
 
     public void setCurrentSubView(Action currentSubView) {
-        webActions.setCurrentTabAction(TabActionsSelection.getSubTabCategory(getCurrentViewId()), currentSubView);
+        webActions.setCurrentTabAction(
+                TabActionsSelection.getSubTabCategory(getCurrentViewId()),
+                currentSubView);
     }
 
     @Factory(value = "currentAdminSubViewId", scope = ScopeType.EVENT)
@@ -113,7 +115,9 @@ public class AdminViewManager implements Serializable {
     }
 
     public void setCurrentSubViewId(String currentSubViewId) {
-        webActions.setCurrentTabId(TabActionsSelection.getSubTabCategory(getCurrentViewId()), currentSubViewId);
+        webActions.setCurrentTabId(
+                TabActionsSelection.getSubTabCategory(getCurrentViewId()),
+                currentSubViewId);
     }
 
     public List<Action> getAvailableActions() {
