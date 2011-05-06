3c1cda2e23d8701a4a6c08d3e63f680e91d91f33
Benoit Delbosc <bdelbosc@nuxeo.com>
2011-05-06 11:48:01 +0200

    Fixing webdav put, removing 2 litmus warning

--- nuxeo-webdav/src/main/java/org/nuxeo/ecm/webdav/resource/UnknownResource.java
+++ nuxeo-webdav/src/main/java/org/nuxeo/ecm/webdav/resource/UnknownResource.java
@@ -75,14 +75,15 @@ public class UnknownResource extends AbstractResource {
 
         ensureParentExists();
         Blob content = new StreamingBlob(new InputStreamSource(request.getInputStream()));
-        if (content.getLength() > 0) {
+        // note that content.getLentgh from an input stream source always return -1
+        if (request.getContentLength() > 0) {
             String contentType = request.getContentType();
             if (contentType == null) {
                 contentType = "application/octet-stream";
             }
             content.setMimeType(contentType);
-        content.setFilename(name);
-        backend.createFile(parentPath, name, content);
+            content.setFilename(name);
+            backend.createFile(parentPath, name, content);
         } else {
             backend.createFile(parentPath, name);
         }
