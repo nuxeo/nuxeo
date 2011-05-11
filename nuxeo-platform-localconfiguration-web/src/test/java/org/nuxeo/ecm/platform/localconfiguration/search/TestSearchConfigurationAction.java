698f938cdb2cb2382e908910c3bc5a930045af35
Ronan Le Gall <rlegall@nuxeo.com>
2011-05-11 14:15:32 +0200

    Java indentation correction

--- nuxeo-platform-localconfiguration-web/src/test/java/org/nuxeo/ecm/platform/localconfiguration/search/TestSearchConfigurationAction.java
+++ nuxeo-platform-localconfiguration-web/src/test/java/org/nuxeo/ecm/platform/localconfiguration/search/TestSearchConfigurationAction.java
@@ -8,32 +8,32 @@ import static org.mockito.Mockito.*;
 
 public class TestSearchConfigurationAction {
 
-  SearchConfigurationActions searchConfigurationActions = new SearchConfigurationActions();
+    SearchConfigurationActions searchConfigurationActions = new SearchConfigurationActions();
 
-  @Test
-  public void testConditionToReturnAChosenAdvancedSearch() {
-    SearchLocalConfiguration configuration = null;
-    assertFalse(
-        "The condition should be False when the SearchLocalConfiguration is null",
-        searchConfigurationActions
-            .isLocalConfigurationExistsAndSearchViewAvailable(configuration));
-    configuration = mock(SearchLocalConfiguration.class);
-    when(configuration.getAdvancedSearchView()).thenReturn(null);
-    assertFalse(
-        "The condition should be False when the SearchLocalConfiguration is not null and an AdvancedSearchView is null",
-        searchConfigurationActions
-            .isLocalConfigurationExistsAndSearchViewAvailable(configuration));
-    when(configuration.getAdvancedSearchView()).thenReturn(
-        "Choosen Advanced Search View");
-    assertTrue(
-        "The condition should be True when the SearchLocalConfiguration exists and an AdvancedSearchView exists",
-        searchConfigurationActions
-            .isLocalConfigurationExistsAndSearchViewAvailable(configuration));
-    configuration = null;
-    assertFalse(
-        "The condition should be False when the SearchLocalConfiguration is null and an AdvancedSearchView exists",
-        searchConfigurationActions
-            .isLocalConfigurationExistsAndSearchViewAvailable(configuration));
-  }
+    @Test
+    public void testConditionToReturnAChosenAdvancedSearch() {
+        SearchLocalConfiguration configuration = null;
+        assertFalse(
+                "The condition should be False when the SearchLocalConfiguration is null",
+                searchConfigurationActions
+                        .isLocalConfigurationExistsAndSearchViewAvailable(configuration));
+        configuration = mock(SearchLocalConfiguration.class);
+        when(configuration.getAdvancedSearchView()).thenReturn(null);
+        assertFalse(
+                "The condition should be False when the SearchLocalConfiguration is not null and an AdvancedSearchView is null",
+                searchConfigurationActions
+                        .isLocalConfigurationExistsAndSearchViewAvailable(configuration));
+        when(configuration.getAdvancedSearchView()).thenReturn(
+                "Choosen Advanced Search View");
+        assertTrue(
+                "The condition should be True when the SearchLocalConfiguration exists and an AdvancedSearchView exists",
+                searchConfigurationActions
+                        .isLocalConfigurationExistsAndSearchViewAvailable(configuration));
+        configuration = null;
+        assertFalse(
+                "The condition should be False when the SearchLocalConfiguration is null and an AdvancedSearchView exists",
+                searchConfigurationActions
+                        .isLocalConfigurationExistsAndSearchViewAvailable(configuration));
+    }
 
 }
