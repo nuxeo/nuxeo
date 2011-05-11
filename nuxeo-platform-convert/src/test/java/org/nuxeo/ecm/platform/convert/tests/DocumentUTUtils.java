d6ba36c60180841ce1c3d0c30f0da60517b6aaaa
Stephane Lacoin <slacoin@nuxeo.com>
2011-05-11 10:29:52 +0200

    NXP-6815 pdfbox upgrade (test dependencies)

--- nuxeo-platform-convert/src/test/java/org/nuxeo/ecm/platform/convert/tests/DocumentUTUtils.java
+++ nuxeo-platform-convert/src/test/java/org/nuxeo/ecm/platform/convert/tests/DocumentUTUtils.java
@@ -21,8 +21,8 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.StringWriter;
 
-import org.pdfbox.pdmodel.PDDocument;
-import org.pdfbox.util.PDFTextStripper;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.util.PDFTextStripper;
 
 public final class DocumentUTUtils {
 
