49b39ea461ca15b463b6adca124573aff62df2a5
Hudson gimli <jcarsique@nuxeo.com>
2011-05-17 00:27:10 +0200

    nx-builder: Release 5.4.2-RC1

--- clone.bat
+++ clone.bat
@@ -1,7 +1,7 @@
 @echo off
 rem This script will checkout the needed sources
 
-set VERSION=5.4
+set VERSION=release-5.4.2-RC1
 
 hg clone http://hg.nuxeo.org/nuxeo/nuxeo-common
 hg clone http://hg.nuxeo.org/nuxeo/nuxeo-runtime
