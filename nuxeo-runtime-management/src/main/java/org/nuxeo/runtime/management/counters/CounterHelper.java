8288a8b13f5db3aff3d20062c7f94f11d711f15e
Thierry Delprat <tdelprat@nuxeo.com>
2011-05-17 16:24:35 +0200

    NXP-6877 Merge runtime-management-metrics + add APi to wrapp Simon Counters

---
+++ nuxeo-runtime-management/src/main/java/org/nuxeo/runtime/management/counters/CounterHelper.java
@@ -0,0 +1,46 @@
+/*
+ * (C) Copyright 2006-2009 Nuxeo SAS (http://nuxeo.com/) and contributors.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser General Public License
+ * (LGPL) version 2.1 which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl.html
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * Contributors:
+ *     Nuxeo - initial API and implementation
+ *
+ * $Id$
+ */
+package org.nuxeo.runtime.management.counters;
+
+import org.nuxeo.runtime.api.Framework;
+
+/**
+ * Dummy helper class to be used by code that updates the counters to avoid
+ * having to do the service lookup
+ *
+ * @author Tiry (tdelprat@nuxeo.com)
+ *
+ */
+public class CounterHelper {
+
+    protected static CounterManager cm = Framework.getLocalService(CounterManager.class);
+
+    public static void increaseCounter(String counterName) {
+        cm.increaseCounter(counterName);
+    }
+
+    public static void setCounterValue(String counterName, long value) {
+        cm.setCounterValue(counterName, value);
+    }
+
+    public static void decreaseCounter(String counterName) {
+        cm.decreaseCounter(counterName);
+    }
+
+}
