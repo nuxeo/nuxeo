2cb52348eed5825e83f24e254a73e0940f0446ce
Thierry Delprat <tdelprat@nuxeo.com>
2011-05-24 11:31:37 +0200

    NXP-6859 Make GetCounters operation restricted to administrators

--- nuxeo-automation/nuxeo-automation-features/src/main/java/org/nuxeo/ecm/automation/core/operations/management/GetCounters.java
+++ nuxeo-automation/nuxeo-automation-features/src/main/java/org/nuxeo/ecm/automation/core/operations/management/GetCounters.java
@@ -17,6 +17,7 @@
  */
 package org.nuxeo.ecm.automation.core.operations.management;
 
+import java.security.Principal;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -24,12 +25,15 @@ import java.util.List;
 import net.sf.json.JSONArray;
 import net.sf.json.JSONObject;
 
+import org.nuxeo.ecm.automation.OperationContext;
 import org.nuxeo.ecm.automation.core.Constants;
+import org.nuxeo.ecm.automation.core.annotations.Context;
 import org.nuxeo.ecm.automation.core.annotations.Operation;
 import org.nuxeo.ecm.automation.core.annotations.OperationMethod;
 import org.nuxeo.ecm.automation.core.annotations.Param;
 import org.nuxeo.ecm.automation.core.util.StringList;
 import org.nuxeo.ecm.core.api.Blob;
+import org.nuxeo.ecm.core.api.NuxeoPrincipal;
 import org.nuxeo.ecm.core.api.impl.blob.ByteArrayBlob;
 import org.nuxeo.runtime.api.Framework;
 import org.nuxeo.runtime.management.counters.CounterHistoryStack;
@@ -48,6 +52,9 @@ public class GetCounters {
 
     public static final String ID = "Counters.GET";
 
+    @Context
+    protected OperationContext ctx;
+
     @Param(name = "counterNames", required = true)
     protected StringList counterNames;
 
@@ -58,66 +65,74 @@ public class GetCounters {
 
         JSONObject collection = new JSONObject();
 
-        for (String counterName : counterNames) {
-            CounterHistoryStack stack = cm.getCounterHistory(counterName);
+        Principal principal = ctx.getPrincipal();
+        if (principal instanceof NuxeoPrincipal) {
+            NuxeoPrincipal nuxeoUser = (NuxeoPrincipal) principal;
+            // Only Administrators can access the counters
+            if (nuxeoUser.isAdministrator()) {
+                for (String counterName : counterNames) {
+                    CounterHistoryStack stack = cm.getCounterHistory(counterName);
 
-            // copy and reverse the list
-            List<long[]> valueList = new ArrayList<long[]>(stack.getAsList());
-            Collections.reverse(valueList);
+                    // copy and reverse the list
+                    List<long[]> valueList = new ArrayList<long[]>(stack.getAsList());
+                    Collections.reverse(valueList);
 
-            JSONObject counter = new JSONObject();
+                    JSONObject counter = new JSONObject();
 
-            // bare values [ [t0,v0], [t1,v1] ...]
-            JSONArray valueSerie = new JSONArray();
-            // delta values [ [t1,v1-v0], [t2,v2-v3] ...]
-            JSONArray deltaSerie = new JSONArray();
-            // speed values [ [t1,v1-v0/t1-t0], ...]
-            JSONArray speedSerie = new JSONArray();
+                    // bare values [ [t0,v0], [t1,v1] ...]
+                    JSONArray valueSerie = new JSONArray();
+                    // delta values [ [t1,v1-v0], [t2,v2-v3] ...]
+                    JSONArray deltaSerie = new JSONArray();
+                    // speed values [ [t1,v1-v0/t1-t0], ...]
+                    JSONArray speedSerie = new JSONArray();
 
-            float lastTS = 0;
-            float lastValue = 0;
-            long now = System.currentTimeMillis();
-            for (long[] values : valueList) {
+                    float lastTS = 0;
+                    float lastValue = 0;
+                    long now = System.currentTimeMillis();
+                    for (long[] values : valueList) {
 
-                // use seconds
-                long ts =values[0];
-                float t = (now - ts)/1000;
-                float value = values[1];
+                        // use seconds
+                        long ts =values[0];
+                        float t = (now - ts)/1000;
+                        float value = values[1];
 
-                JSONArray valueArray = new JSONArray();
-                JSONArray deltaArray = new JSONArray();
-                JSONArray speedArray = new JSONArray();
+                        JSONArray valueArray = new JSONArray();
+                        JSONArray deltaArray = new JSONArray();
+                        JSONArray speedArray = new JSONArray();
 
-                // bare values
-                valueArray.add(ts);
-                valueArray.add(value);
-                valueSerie.add(valueArray);
+                        // bare values
+                        valueArray.add(ts);
+                        valueArray.add(value);
+                        valueSerie.add(valueArray);
 
-                // delta values
-                deltaArray.add(ts);
-                deltaArray.add(value - lastValue);
-                deltaSerie.add(deltaArray);
+                        // delta values
+                        deltaArray.add(ts);
+                        deltaArray.add(value - lastValue);
+                        deltaSerie.add(deltaArray);
 
-                if (lastTS>0) {
-                    // speed values
-                    speedArray.add(ts);
-                    float tdelta = lastTS-t;
-                    if (tdelta==0) {
-                        tdelta=1;
+                        if (lastTS>0) {
+                            // speed values
+                            speedArray.add(ts);
+                            float tdelta = lastTS-t;
+                            if (tdelta==0) {
+                                tdelta=1;
+                            }
+                            speedArray.add(60*(value - lastValue) / (tdelta));
+                            speedSerie.add(speedArray);
+                        }
+                        lastTS = t;
+                        lastValue = value;
                     }
-                    speedArray.add(60*(value - lastValue) / (tdelta));
-                    speedSerie.add(speedArray);
+
+                    counter.put("values", valueSerie);
+                    counter.put("deltas", deltaSerie);
+                    counter.put("speed", speedSerie);
+
+                    collection.put(counterName, counter);
                 }
-                lastTS = t;
-                lastValue = value;
             }
-
-            counter.put("values", valueSerie);
-            counter.put("deltas", deltaSerie);
-            counter.put("speed", speedSerie);
-
-            collection.put(counterName, counter);
         }
+
 
         return new ByteArrayBlob(collection.toString().getBytes("UTF-8"),
         "application/json");
