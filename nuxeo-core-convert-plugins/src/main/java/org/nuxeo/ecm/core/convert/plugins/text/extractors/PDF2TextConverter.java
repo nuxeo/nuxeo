b0faa7d5528f80040df73cbafcf34a0c04310efc
Stephane Lacoin <slacoin@nuxeo.com>
2011-05-26 18:20:18 +0200

    NXP-6815 - log pdfbox warn only once

--- nuxeo-core-convert-plugins/src/main/java/org/nuxeo/ecm/core/convert/plugins/text/extractors/PDF2TextConverter.java
+++ nuxeo-core-convert-plugins/src/main/java/org/nuxeo/ecm/core/convert/plugins/text/extractors/PDF2TextConverter.java
@@ -23,10 +23,22 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.Serializable;
+import java.lang.reflect.Field;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.apache.pdfbox.cos.COSBase;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.encryption.PDEncryptionDictionary;
+import org.apache.pdfbox.pdmodel.encryption.PDStandardEncryption;
+import org.apache.pdfbox.util.PDFOperator;
+import org.apache.pdfbox.util.PDFStreamEngine;
+import org.apache.pdfbox.util.PDFTextStripper;
+import org.apache.pdfbox.util.operator.OperatorProcessor;
 import org.nuxeo.ecm.core.api.blobholder.BlobHolder;
 import org.nuxeo.ecm.core.api.impl.blob.FileBlob;
 import org.nuxeo.ecm.core.api.impl.blob.StringBlob;
@@ -34,13 +46,70 @@ import org.nuxeo.ecm.core.convert.api.ConversionException;
 import org.nuxeo.ecm.core.convert.cache.SimpleCachableBlobHolder;
 import org.nuxeo.ecm.core.convert.extension.Converter;
 import org.nuxeo.ecm.core.convert.extension.ConverterDescriptor;
-import org.apache.pdfbox.pdmodel.PDDocument;
-import org.apache.pdfbox.pdmodel.encryption.PDEncryptionDictionary;
-import org.apache.pdfbox.pdmodel.encryption.PDStandardEncryption;
-import org.apache.pdfbox.util.PDFTextStripper;
 
 @SuppressWarnings("deprecation")
 public class PDF2TextConverter implements Converter {
+
+    public static class PatchedPDFTextStripper extends PDFTextStripper {
+
+        public PatchedPDFTextStripper() throws IOException {
+            super();
+        }
+
+        protected Object unrestrictedAccess(String name) {
+            try {
+                Field f = PDFStreamEngine.class.getDeclaredField(name);
+                f.setAccessible(true);
+                return f.get(this);
+            } catch (Exception e) {
+                throw new Error("Cannot get access to PDFStreamEngine fields",
+                        e);
+            }
+        }
+
+        @SuppressWarnings("unchecked")
+        protected Set<String> unsupportedOperators() {
+            return (Set<String>) unrestrictedAccess("unsupportedOperators");
+        }
+
+        @SuppressWarnings("unchecked")
+        protected Map<String, OperatorProcessor> operators() {
+            return (Map<String, OperatorProcessor>) unrestrictedAccess("operators");
+        }
+
+        final static Set<StackTraceElement> loggedStacks = new HashSet<StackTraceElement>();
+
+        @Override
+        protected void processOperator(PDFOperator operator,
+                List<COSBase> arguments) throws IOException {
+            try {
+
+                String operation = operator.getOperation();
+                OperatorProcessor processor = operators().get(operation);
+                if (processor != null) {
+                    processor.setContext(this);
+                    processor.process(operator, arguments);
+                } else {
+                    if (!unsupportedOperators().contains(operation)) {
+                        log.info("unsupported/disabled operation: " + operation);
+                        unsupportedOperators().add(operation);
+                    }
+                }
+            } catch (Exception e) {
+                StackTraceElement root = e.getStackTrace()[0];
+                synchronized (loggedStacks) {
+                    if (loggedStacks.contains(root)) {
+                        return;
+                    }
+                    loggedStacks.add(root);
+                }
+                log.warn(
+                        "Caught error in pdfbox during extraction (stack logged only once)",
+                        e);
+            }
+        }
+
+    }
 
     private static final Log log = LogFactory.getLog(PDF2TextConverter.class);
 
@@ -53,15 +122,13 @@ public class PDF2TextConverter implements Converter {
         OutputStream fas = null;
         try {
             document = PDDocument.load(blobHolder.getBlob().getStream());
-            PDFTextStripper textStripper = new PDFTextStripper();
 
             // NXP-1556: if document is protected an IOException will be raised
             // Instead of catching the exception based on its message string
             // lets avoid sending messages that will generate this error
             // code taken from PDFTextStripper.writeText source.
             Boolean isReadable = true;
-            PDEncryptionDictionary encDictionary = document
-                    .getEncryptionDictionary();
+            PDEncryptionDictionary encDictionary = document.getEncryptionDictionary();
             // only care about standard encryption and if it was decrypted with
             // the
             // user password
@@ -71,7 +138,7 @@ public class PDF2TextConverter implements Converter {
                 isReadable = stdEncryption.canExtractContent();
             }
             if (isReadable) {
-                String text = textStripper.getText(document);
+                String text = new PatchedPDFTextStripper().getText(document);
                 text = text.replace("\u00a0", " ");
                 f = File.createTempFile("pdfboplugin", ".txt");
                 fas = new FileOutputStream(f);
