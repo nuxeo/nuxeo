10ee39e70951384e762c980a9d6781f22c5a876a
Florent Guillaume <fg@nuxeo.com>
2011-06-09 18:34:04 +0200

    NXP-7093: fix connection leak in JDBCConnectionPropagator

--- nuxeo-core-storage-sql/nuxeo-core-storage-sql-test/src/test/java/org/nuxeo/ecm/core/storage/sql/TestSQLBackend.java
+++ nuxeo-core-storage-sql/nuxeo-core-storage-sql-test/src/test/java/org/nuxeo/ecm/core/storage/sql/TestSQLBackend.java
@@ -22,6 +22,7 @@ import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.Serializable;
+import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Calendar;
@@ -45,6 +46,8 @@ import org.nuxeo.ecm.core.query.QueryFilter;
 import org.nuxeo.ecm.core.storage.PartialList;
 import org.nuxeo.ecm.core.storage.StorageException;
 import org.nuxeo.ecm.core.storage.sql.RepositoryDescriptor.FulltextIndexDescriptor;
+import org.nuxeo.ecm.core.storage.sql.jdbc.JDBCBackend;
+import org.nuxeo.ecm.core.storage.sql.jdbc.JDBCConnectionPropagator;
 import org.nuxeo.ecm.core.storage.sql.jdbc.JDBCMapper;
 
 /**
@@ -1680,6 +1683,31 @@ public class TestSQLBackend extends SQLBackendTestCase {
         } finally {
             JDBCMapper.testProps.clear();
         }
+    }
+
+    public void testJDBCConnectionPropagatorLeak() throws Exception {
+        assertEquals(0, getJDBCConnectionPropagatorSize());
+        repository.getConnection().close();
+        // 1 connection remains for the lock manager
+        assertEquals(1, getJDBCConnectionPropagatorSize());
+        Session s1 = repository.getConnection();
+        Session s2 = repository.getConnection();
+        Session s3 = repository.getConnection();
+        assertEquals(1 + 3, getJDBCConnectionPropagatorSize());
+        s1.close();
+        s2.close();
+        s3.close();
+        assertEquals(1, getJDBCConnectionPropagatorSize());
+    }
+
+    protected int getJDBCConnectionPropagatorSize() throws Exception {
+        Field backendField = RepositoryImpl.class.getDeclaredField("backend");
+        backendField.setAccessible(true);
+        JDBCBackend backend = (JDBCBackend) backendField.get(repository);
+        Field propagatorField = JDBCBackend.class.getDeclaredField("connectionPropagator");
+        propagatorField.setAccessible(true);
+        JDBCConnectionPropagator propagator = (JDBCConnectionPropagator) propagatorField.get(backend);
+        return propagator.connections.size();
     }
 
 }
