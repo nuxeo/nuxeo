672af6f809d8dd9e10e963fd4f1f5a6af6cf0f8c
Stephane Lacoin <slacoin@nuxeo.com>
2011-06-29 18:35:33 +0200

    NXP-7170 inject transactions for  application started handlers

--- nuxeo-runtime/src/main/java/org/nuxeo/runtime/model/impl/RegistrationInfoImpl.java
+++ nuxeo-runtime/src/main/java/org/nuxeo/runtime/model/impl/RegistrationInfoImpl.java
@@ -35,6 +35,7 @@ import org.nuxeo.common.xmap.annotation.XObject;
 import org.nuxeo.runtime.ComponentEvent;
 import org.nuxeo.runtime.Version;
 import org.nuxeo.runtime.api.Framework;
+import org.nuxeo.runtime.api.J2EEContainerDescriptor;
 import org.nuxeo.runtime.model.Component;
 import org.nuxeo.runtime.model.ComponentInstance;
 import org.nuxeo.runtime.model.ComponentManager;
@@ -45,10 +46,11 @@ import org.nuxeo.runtime.model.ExtensionPoint;
 import org.nuxeo.runtime.model.Property;
 import org.nuxeo.runtime.model.RegistrationInfo;
 import org.nuxeo.runtime.model.RuntimeContext;
+import org.nuxeo.runtime.transaction.TransactionHelper;
 
 /**
  * @author <a href="mailto:bs@nuxeo.com">Bogdan Stefanescu</a>
- *
+ * 
  */
 @XObject("component")
 public class RegistrationInfoImpl implements RegistrationInfo {
@@ -106,7 +108,7 @@ public class RegistrationInfoImpl implements RegistrationInfo {
     /**
      * To be set when deploying configuration components that are not in a
      * bundle (e.g. from config. dir).
-     *
+     * 
      * Represent the bundle that will be assumed to be the owner of the
      * component.
      */
@@ -134,7 +136,7 @@ public class RegistrationInfoImpl implements RegistrationInfo {
 
     /**
      * Useful when dynamically registering components
-     *
+     * 
      * @param name the component name
      */
     public RegistrationInfoImpl(ComponentName name) {
@@ -246,6 +248,7 @@ public class RegistrationInfoImpl implements RegistrationInfo {
 
     /**
      * Reload the underlying component if reload is supported
+     * 
      * @throws Exception
      */
     public synchronized void reload() throws Exception {
@@ -307,7 +310,19 @@ public class RegistrationInfoImpl implements RegistrationInfo {
         if (component != null) {
             Object ci = component.getInstance();
             if (ci instanceof Component) {
-                ((Component) ci).applicationStarted(component);
+                if (J2EEContainerDescriptor.getSelected() != null) {
+                    TransactionHelper.startTransaction();
+                    try {
+                        ((Component) ci).applicationStarted(component);
+                    } catch (Throwable e) {
+                        TransactionHelper.setTransactionRollbackOnly();
+                        log.error("Rollbacking transaction, got error from " + ci, e);
+                    } finally {
+                        TransactionHelper.commitOrRollbackTransaction();
+                    }
+                } else {
+                    ((Component) ci).applicationStarted(component);
+                }
             }
         }
     }
