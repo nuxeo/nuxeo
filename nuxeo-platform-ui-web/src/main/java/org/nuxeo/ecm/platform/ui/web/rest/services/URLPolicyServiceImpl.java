eb2932098cf53f0b59331a13e40a819bc810ef75
Anahide Tchertchian <at@nuxeo.com>
2011-07-07 11:39:40 +0200

    NXP-7215: rename new element in URL pattern descriptor + make sur new doc view binding is called even if doc view binding is not filled

--- nuxeo-platform-ui-web/src/main/java/org/nuxeo/ecm/platform/ui/web/rest/services/URLPolicyServiceImpl.java
+++ nuxeo-platform-ui-web/src/main/java/org/nuxeo/ecm/platform/ui/web/rest/services/URLPolicyServiceImpl.java
@@ -31,6 +31,7 @@ import javax.el.ValueExpression;
 import javax.faces.context.FacesContext;
 import javax.servlet.http.HttpServletRequest;
 
+import org.apache.commons.lang.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.nuxeo.common.utils.URIUtils;
@@ -418,9 +419,8 @@ public class URLPolicyServiceImpl implements URLPolicyService {
             List<URLPatternDescriptor> descs = getURLPatternDescriptors();
             boolean applies = false;
             for (URLPatternDescriptor desc : descs) {
-                String documentViewAppliesExpr = desc.getDocumentViewApplies();
-                if (documentViewAppliesExpr != null
-                        && !"".equals(documentViewAppliesExpr)) {
+                String documentViewAppliesExpr = desc.getDocumentViewBindingApplies();
+                if (!StringUtils.isBlank(documentViewAppliesExpr)) {
                     // TODO: maybe put view id to the request to help writing
                     // the EL expression
                     ValueExpression ve = ef.createValueExpression(context,
@@ -443,18 +443,17 @@ public class URLPolicyServiceImpl implements URLPolicyService {
             // resolved doc view values thanks to bindings
             Object docViewValue = null;
             String documentViewBinding = patternDesc.getDocumentViewBinding();
-            if (documentViewBinding != null && !"".equals(documentViewBinding)) {
+            if (!StringUtils.isBlank(documentViewBinding)) {
                 ValueExpression ve = ef.createValueExpression(context,
                         documentViewBinding, Object.class);
                 docViewValue = ve.getValue(context);
-                if (docViewValue == null) {
-                    documentViewBinding = patternDesc.getNewDocumentViewBinding();
-                    if (documentViewBinding != null
-                            && !"".equals(documentViewBinding)) {
-                        ve = ef.createValueExpression(context,
-                                documentViewBinding, Object.class);
-                        docViewValue = ve.getValue(context);
-                    }
+            }
+            if (docViewValue == null) {
+                documentViewBinding = patternDesc.getNewDocumentViewBinding();
+                if (!StringUtils.isBlank(documentViewBinding)) {
+                    ValueExpression ve = ef.createValueExpression(context,
+                            documentViewBinding, Object.class);
+                    docViewValue = ve.getValue(context);
                 }
             }
             if (docViewValue instanceof DocumentView) {
