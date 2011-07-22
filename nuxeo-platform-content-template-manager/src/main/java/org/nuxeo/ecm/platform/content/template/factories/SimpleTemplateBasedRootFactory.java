a7a11115d87a5e40b22e72aa4c61cc3a5b510b20
Stephane Lacoin <slacoin@nuxeo.com>
2011-07-22 10:02:39 +0200

    NXP-7293 -  register root factory execution using 'dc:issued' property on root document

--- nuxeo-platform-content-template-manager/src/main/java/org/nuxeo/ecm/platform/content/template/factories/SimpleTemplateBasedRootFactory.java
+++ nuxeo-platform-content-template-manager/src/main/java/org/nuxeo/ecm/platform/content/template/factories/SimpleTemplateBasedRootFactory.java
@@ -17,18 +17,18 @@
 
 package org.nuxeo.ecm.platform.content.template.factories;
 
+import java.util.Calendar;
+
 import org.nuxeo.ecm.core.api.ClientException;
 import org.nuxeo.ecm.core.api.DocumentModel;
-import org.nuxeo.ecm.core.api.DocumentModelList;
 import org.nuxeo.ecm.platform.content.template.listener.RepositoryInitializationListener;
 import org.nuxeo.ecm.platform.content.template.service.TemplateItemDescriptor;
 
 /**
- * Specific factory for Root.
- * Since some other {@link RepositoryInitializationListener}
- * have run before, root won't be empty
+ * Specific factory for Root. Since some other
+ * {@link RepositoryInitializationListener} have run before, root won't be empty
  * but we may still have to run this initializer.
- *
+ * 
  * @author Thierry Delprat
  */
 public class SimpleTemplateBasedRootFactory extends SimpleTemplateBasedFactory {
@@ -38,24 +38,28 @@ public class SimpleTemplateBasedRootFactory extends SimpleTemplateBasedFactory {
             throws ClientException {
         initSession(eventDoc);
 
-        if (shouldCreateContent(eventDoc)) {
-            for (TemplateItemDescriptor item : template) {
-                String itemPath = eventDoc.getPathAsString();
-                if (item.getPath() != null) {
-                    itemPath += "/" + item.getPath();
-                }
-                DocumentModel newChild = session.createDocumentModel(itemPath,
-                        item.getId(), item.getTypeName());
-                newChild.setProperty("dublincore", "title", item.getTitle());
-                newChild.setProperty("dublincore", "description",
-                        item.getDescription());
-                setProperties(item.getProperties(), newChild);
-                newChild = session.createDocument(newChild);
-                setAcl(item.getAcl(), newChild.getRef());
-            }
-            // init root ACL if really empty
-            setAcl(acl, eventDoc.getRef());
+        if (!shouldCreateContent(eventDoc)) {
+            return;
         }
+
+        for (TemplateItemDescriptor item : template) {
+            String itemPath = eventDoc.getPathAsString();
+            if (item.getPath() != null) {
+                itemPath += "/" + item.getPath();
+            }
+            DocumentModel newChild = session.createDocumentModel(itemPath,
+                    item.getId(), item.getTypeName());
+            newChild.setProperty("dublincore", "title", item.getTitle());
+            newChild.setProperty("dublincore", "description",
+                    item.getDescription());
+            setProperties(item.getProperties(), newChild);
+            newChild = session.createDocument(newChild);
+            setAcl(item.getAcl(), newChild.getRef());
+        }
+        // init root ACL if really empty
+        setAcl(acl, eventDoc.getRef());
+
+        createdContent();
     }
 
     /**
@@ -67,14 +71,15 @@ public class SimpleTemplateBasedRootFactory extends SimpleTemplateBasedFactory {
      */
     protected boolean shouldCreateContent(DocumentModel eventDoc)
             throws ClientException {
-        for (TemplateItemDescriptor item : template) {
-            DocumentModelList existingDocsOfTheSameType = session.getChildren(
-                    eventDoc.getRef(), item.getTypeName());
-            if (!existingDocsOfTheSameType.isEmpty()) {
-                return false;
-            }
-        }
-        return true;
+        Boolean issued = (Boolean)session.getRootDocument().getProperty("dc","issued");
+        return issued == null;
     }
 
+    /**
+     * Handles  completion, update processing status
+     * @throws ClientException
+     */
+    protected void createdContent() throws ClientException {
+        session.getRootDocument().setProperty("dc", "issued", Calendar.getInstance().getTime());
+    }
 }
