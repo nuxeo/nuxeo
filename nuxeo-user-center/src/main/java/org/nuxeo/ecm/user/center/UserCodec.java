cdc15a6742cf71c90250b74c0f2339e5d9b33d5f
Thomas Roger <troger@nuxeo.com>
2011-08-19 00:19:08 +0200

    NXP-7376: add codec to handle permlink on user profile

---
+++ nuxeo-user-center/src/main/java/org/nuxeo/ecm/user/center/UserCodec.java
@@ -0,0 +1,128 @@
+/*
+ * (C) Copyright 2006-2011 Nuxeo SA (http://nuxeo.com/) and others.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser General Public License
+ * (LGPL) version 2.1 which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl-2.1.html
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * Contributors:
+ *     Thomas Roger <troger@nuxeo.com>
+ */
+
+package org.nuxeo.ecm.user.center;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.nuxeo.common.utils.StringUtils;
+import org.nuxeo.common.utils.URIUtils;
+import org.nuxeo.ecm.core.api.ClientException;
+import org.nuxeo.ecm.core.api.DocumentLocation;
+import org.nuxeo.ecm.core.api.impl.DocumentLocationImpl;
+import org.nuxeo.ecm.core.api.repository.RepositoryManager;
+import org.nuxeo.ecm.platform.url.DocumentViewImpl;
+import org.nuxeo.ecm.platform.url.api.DocumentView;
+import org.nuxeo.ecm.platform.url.service.AbstractDocumentViewCodec;
+import org.nuxeo.runtime.api.Framework;
+
+/**
+ * Codec handling a username, an optional view and additional request
+ * parameters.
+ *
+ * @author <a href="mailto:troger@nuxeo.com">Thomas Roger</a>
+ * @since 5.4.3
+ */
+public class UserCodec extends AbstractDocumentViewCodec {
+
+    public static final String PREFIX = "user";
+
+    public static final String DEFAULT_VIEW_ID = "view_home";
+
+    public static final String DEFAULT_USERS_TAB = "USER_CENTER:UsersGroupsHome:UsersHome";
+
+    // prefix/username/view_id?requestParams
+    public static final String GET_URL_PATTERN = "/" // slash
+            + "([a-zA-Z_0-9\\-\\.]*)?" // username (group 1)
+            + "(/[a-zA-Z_0-9\\-\\.]*)?" // view id (group 2) (optional)
+            + "/?" // final slash (optional)
+            + "(\\?(.*)?)?"; // query (group 3) (optional)
+
+    @Override
+    public String getPrefix() {
+        if (prefix != null) {
+            return prefix;
+        }
+        return PREFIX;
+    }
+
+    @Override
+    public DocumentView getDocumentViewFromUrl(String url) {
+        Pattern pattern = Pattern.compile(getPrefix() + GET_URL_PATTERN);
+        Matcher m = pattern.matcher(url);
+        if (m.matches()) {
+            if (m.groupCount() >= 1) {
+                String username = m.group(1);
+
+                String viewId = m.group(2);
+                if (viewId == null || "".equals(viewId)) {
+                    viewId = DEFAULT_VIEW_ID;
+                }
+
+                String query = m.group(3);
+                Map<String, String> params = URIUtils.getRequestParameters(query);
+                if (params == null) {
+                    params = new HashMap<String, String>();
+                }
+
+                params.put("username", username);
+                params.put("showUser", "true");
+
+                if (!params.containsKey("tabIds")) {
+                    params.put("tabIds", DEFAULT_USERS_TAB);
+                }
+
+                final DocumentLocation docLoc = new DocumentLocationImpl(getDefaultRepositoryName(), null);
+                return new DocumentViewImpl(docLoc, viewId, params);
+            }
+        }
+        return null;
+    }
+
+    private String getDefaultRepositoryName() {
+        try {
+            return Framework.getService(RepositoryManager.class).getDefaultRepository().getName();
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    @Override
+    public String getUrlFromDocumentView(DocumentView docView) {
+        String username = docView.getParameter("username");
+        if (username != null) {
+            docView.removeParameter("username");
+            List<String> items = new ArrayList<String>();
+            items.add(getPrefix());
+            items.add(username);
+            String viewId = docView.getViewId();
+            if (viewId != null) {
+                items.add(viewId);
+            }
+            String uri = StringUtils.join(items, "/");
+            return URIUtils.addParametersToURIQuery(uri,
+                    docView.getParameters());
+        }
+        return null;
+    }
+
+}
---
+++ nuxeo-user-center/src/test/java/org/nuxeo/ecm/user/center/TestUserCodec.java
@@ -0,0 +1,110 @@
+/*
+ * (C) Copyright 2006-2011 Nuxeo SA (http://nuxeo.com/) and others.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser General Public License
+ * (LGPL) version 2.1 which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl-2.1.html
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * Contributors:
+ *     Thomas Roger <troger@nuxeo.com>
+ */
+
+package org.nuxeo.ecm.user.center;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.nuxeo.ecm.user.center.UserCodec.DEFAULT_USERS_TAB;
+import static org.nuxeo.ecm.user.center.UserCodec.DEFAULT_VIEW_ID;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Test;
+import org.nuxeo.ecm.core.api.DocumentLocation;
+import org.nuxeo.ecm.core.api.IdRef;
+import org.nuxeo.ecm.core.api.PathRef;
+import org.nuxeo.ecm.core.api.impl.DocumentLocationImpl;
+import org.nuxeo.ecm.platform.url.DocumentViewImpl;
+import org.nuxeo.ecm.platform.url.api.DocumentView;
+
+/**
+ * @author <a href="mailto:troger@nuxeo.com">Thomas Roger</a>
+ * @since 5.4.3
+ */
+public class TestUserCodec {
+
+    private DocumentView getDocumentView(String username, String view) {
+        DocumentLocation docLoc = new DocumentLocationImpl("demo", null);
+        Map<String, String> params = new HashMap<String, String>();
+        params.put("username", username);
+        return new DocumentViewImpl(docLoc, view, params);
+    }
+
+    @Test
+    public void shouldGenerateAnURL() {
+        UserCodec codec = new UserCodec();
+
+        DocumentView docView = getDocumentView("bender", DEFAULT_VIEW_ID);
+        String url = "user/bender/" + DEFAULT_VIEW_ID;
+        assertEquals(url, codec.getUrlFromDocumentView(docView));
+
+        docView = getDocumentView("leela", "view_user");
+        url = "user/leela/" + "view_user";
+        assertEquals(url, codec.getUrlFromDocumentView(docView));
+
+        docView = getDocumentView(null, null);
+        assertNull(codec.getUrlFromDocumentView(docView));
+
+        docView = getDocumentView("fry", null);
+        url = "user/fry";
+        assertEquals(url, codec.getUrlFromDocumentView(docView));
+
+        docView = getDocumentView("bender", null);
+        docView.addParameter("tabIds", "FakeTab");
+        url = "user/bender?tabIds=FakeTab";
+        assertEquals(url, codec.getUrlFromDocumentView(docView));
+    }
+
+    public void shouldGetDocumentView() {
+        UserCodec codec = new UserCodec();
+        String url = "user/bender";
+        DocumentView docView = codec.getDocumentViewFromUrl(url);
+        assertNotNull(docView);
+        assertEquals(DEFAULT_VIEW_ID, docView.getViewId());
+        assertEquals("true", docView.getParameter("showUser"));
+        assertEquals(DEFAULT_USERS_TAB, docView.getParameter("tabIds"));
+        assertEquals("bender", docView.getParameter("username"));
+
+        url = "user/bender/view_home";
+        docView = codec.getDocumentViewFromUrl(url);
+        assertNotNull(docView);
+        assertEquals(DEFAULT_VIEW_ID, docView.getViewId());
+        assertEquals("true", docView.getParameter("showUser"));
+        assertEquals(DEFAULT_USERS_TAB, docView.getParameter("tabIds"));
+        assertEquals("bender", docView.getParameter("username"));
+
+        url = "user/leela?tabIds=USER_CENTER:UsersGroupsHome:UsersHome";
+        docView = codec.getDocumentViewFromUrl(url);
+        assertNotNull(docView);
+        assertEquals(DEFAULT_VIEW_ID, docView.getViewId());
+        assertEquals("true", docView.getParameter("showUser"));
+        assertEquals(DEFAULT_USERS_TAB, docView.getParameter("tabIds"));
+        assertEquals("leela", docView.getParameter("username"));
+
+        url = "user/fry/view_user?tabIds=FakeTab";
+        docView = codec.getDocumentViewFromUrl(url);
+        assertNotNull(docView);
+        assertEquals("view_user", docView.getViewId());
+        assertEquals("true", docView.getParameter("showUser"));
+        assertEquals("FakeTab", docView.getParameter("tabIds"));
+        assertEquals("fry", docView.getParameter("username"));
+    }
+
+}
