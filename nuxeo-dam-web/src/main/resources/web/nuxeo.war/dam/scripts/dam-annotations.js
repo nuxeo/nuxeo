5e1a253f3640cf77690f13f77cbdb558e0f0ee30
Thomas Roger <troger@nuxeo.com>
2011-09-14 23:15:08 +0200

    DAM-618: use fancybox popup for annotations

---
+++ nuxeo-dam-web/src/main/resources/web/nuxeo.war/dam/scripts/dam-annotations.js
@@ -0,0 +1,12 @@
+jQuery(document).ready(function() {
+  jQuery("#showAnnotationsButton").fancybox({
+    'width'				: '95%',
+    'height'			: '95%',
+    'autoScale'			: true,
+    'transitionIn'		: 'none',
+    'transitionOut'		: 'none',
+    'type'				: 'iframe',
+    'enableEscapeButton': true,
+    'centerOnScroll': true
+  });
+});
