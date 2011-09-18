da598db8c3a1b1db08096861f850f677ee4c19b4
Mathieu Guillaume <mg@nuxeo.com>
2011-09-18 15:58:30 +0200

    NXP-5254: Don't throw a freemarker rendering exception when connection is closed

--- nuxeo-platform-rendering/src/main/java/org/nuxeo/ecm/platform/rendering/fm/FreemarkerEngine.java
+++ nuxeo-platform-rendering/src/main/java/org/nuxeo/ecm/platform/rendering/fm/FreemarkerEngine.java
@@ -16,6 +16,7 @@ package org.nuxeo.ecm.platform.rendering.fm;
 
 import java.io.Writer;
 import java.util.ResourceBundle;
+import java.net.SocketException;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -181,6 +182,8 @@ public class FreemarkerEngine implements RenderingEngine {
                     wrapper);
             env.process();
             bw.copyTo(writer);
+        } catch (SocketException e) {
+            // Connection was closed - not a rendering error
         } catch (Exception e) {
             throw new RenderingException(e);
         }
