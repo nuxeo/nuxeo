16ad7fdda60455daf6a299d18cc89ead114e4050
Julien Carsique <jcarsique@nuxeo.com>
2011-10-07 18:28:23 +0200

    NXP-7666 - Manage both Mercurial and GitHub source repositories

--- clone.py
+++ clone.py
@@ -1,31 +1,83 @@
 #!/usr/bin/env python
+##
+## (C) Copyright 2011 Nuxeo SAS (http://nuxeo.com/) and contributors.
+##
+## All rights reserved. This program and the accompanying materials
+## are made available under the terms of the GNU Lesser General Public License
+## (LGPL) version 2.1 which accompanies this distribution, and is available at
+## http://www.gnu.org/licenses/lgpl.html
+##
+## This library is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+## Lesser General Public License for more details.
+##
+## Contributors:
+##     Stefane Fermigier
+##     Julien Carsique
+##
+## This script clone or update Nuxeo addons source code from Mercurial and GitHub
+## repositories.
+##
 
-import re, os, sys
+import re, os, sys, commands, urllib
+#from pprint import pprint
 
-BRANCH = "5.4"
+git_url = "https://github.com/nuxeo"
+
+def system(cmd):
+    print "$> " + cmd
+    retcode = os.system(cmd)
+
+def hg_fetch(module, root_url=None):
+    if root_url is None:
+        hg_fetch(module, "https://hg.nuxeo.org/addons")
+        return
+
+    if os.path.isdir(module):
+        print "Updating " + module + "..."
+        system("hg pull -R %s" % module)
+    else:
+        print "Cloning " + module + "..."
+        system("hg clone %s/%s %s" % (root_url, module, module))
+    system("hg up -R %s %s" % (module, branch))
+    print
+
+def git_fetch(module):
+    if os.path.isdir(module):
+        print "Updating " + module + "..."
+        retcode = system("cd %s ; git pull %s/%s.git %s" % (module, git_url, module, branch))
+        if retcode != 0:
+            system("cd %s ; git pull %s/%s.git %s" % (module, git_url, module, "master"))
+    else:
+        print "Cloning " + module
+        system("git clone -b %s %s/%s.git %s" % (branch, git_url, module, module))
+    print
 
 if len(sys.argv) == 2:
     branch = sys.argv[1]
 else:
-    branch = BRANCH
-
-def system(cmd):
-    print cmd
-    os.system(cmd)
+    branch = commands.getoutput("hg id -b")
 
 system("hg pull")
 system("hg up %s" % branch)
+print
+lines = urllib.urlopen("https://hg.nuxeo.org/?sort=name").readlines()
+hg_addons = []
+for line in lines:
+    if not line.startswith("<b>addons/"):
+        continue
+    hg_addon = line[len("<b>addons/"):-len("</b>\n")]
+    hg_addons.append(hg_addon)
+#print hg_addons
+
 for line in os.popen("mvn -N help:effective-pom"):
     line = line.strip()
     m = re.match("<module>(.*?)</module>", line)
     if not m:
         continue
     addon = m.group(1)
-
-    if os.path.isdir(addon):
-        print "Updating " + addon
-        system("hg pull -R %s" % addon)
+    if addon in hg_addons:
+        hg_fetch(addon)
     else:
-        print "Cloning " + addon
-        system("hg clone https://hg.nuxeo.org/addons/%s %s" % (addon,addon))
-    system("hg up -R %s %s" % (addon,branch))
+        git_fetch(addon)
