235b729d128d2dfc6b5320dfd54ce060ae569988
Julien Carsique <jcarsique@nuxeo.com>
2011-10-10 18:05:56 +0200

    NXP-7681 - reactivate dead code when logging in debug level

--- nuxeo-webengine-core/src/test/java/org/nuxeo/runtime/annotations/AnnotationTest.java
+++ nuxeo-webengine-core/src/test/java/org/nuxeo/runtime/annotations/AnnotationTest.java
@@ -1,5 +1,5 @@
 /*
- * (C) Copyright 2006-2008 Nuxeo SAS (http://nuxeo.com/) and contributors.
+ * (C) Copyright 2006-2011 Nuxeo SAS (http://nuxeo.com/) and contributors.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the GNU Lesser General Public License
@@ -25,11 +25,16 @@ import java.util.List;
 
 import junit.framework.TestCase;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 /**
  * @author <a href="mailto:bs@nuxeo.com">Bogdan Stefanescu</a>
  *
  */
 public class AnnotationTest extends TestCase {
+
+    static final Log log = LogFactory.getLog(AnnotationTest.class);
 
     public void testAnnotations() {
         AnnotationManager mgr = new AnnotationManager();
@@ -44,24 +49,27 @@ public class AnnotationTest extends TestCase {
         assertEquals("I_class_anno", acC.getAnnotation(Anno2.class).value());
         assertEquals("I_class_anno", acI.getAnnotation(Anno2.class).value());
 
-        assertEquals("[@org.nuxeo.runtime.annotations.Anno1(value=A_class_anno)]",
+        assertEquals(
+                "[@org.nuxeo.runtime.annotations.Anno1(value=A_class_anno)]",
                 Arrays.asList(acA.getAnnotations()).toString());
-        assertEquals("[@org.nuxeo.runtime.annotations.Anno1(value=B_class_anno)]",
+        assertEquals(
+                "[@org.nuxeo.runtime.annotations.Anno1(value=B_class_anno)]",
                 Arrays.asList(acB.getAnnotations()).toString());
 
         List<Annotation> actual = Arrays.asList(acC.getAnnotations());
         assertEquals(2, actual.size());
         String a1 = "@org.nuxeo.runtime.annotations.Anno2(value=I_class_anno)";
         String a2 = "@org.nuxeo.runtime.annotations.Anno1(value=B_class_anno)";
-        assertTrue(("[" + a1 + ", " + a2 + "]").equals(actual.toString()) ||
-                ("[" + a2 + ", " + a1 + "]").equals(actual.toString()));
+        assertTrue(("[" + a1 + ", " + a2 + "]").equals(actual.toString())
+                || ("[" + a2 + ", " + a1 + "]").equals(actual.toString()));
 
-        assertEquals("[@org.nuxeo.runtime.annotations.Anno2(value=I_class_anno)]",
+        assertEquals(
+                "[@org.nuxeo.runtime.annotations.Anno2(value=I_class_anno)]",
                 Arrays.asList(acI.getAnnotations()).toString());
 
         // methods
-        if (false) {
-            System.out.println("#################methods##########");
+        if (log.isDebugEnabled()) {
+            log.debug("#################methods##########");
             printMethodAnnos(acA, Anno1.class);
             printMethodAnnos(acA, Anno2.class);
             printMethodAnnos(acB, Anno1.class);
@@ -73,39 +81,50 @@ public class AnnotationTest extends TestCase {
         }
     }
 
-    static void printMethodAnnos(AnnotatedClass<?> ac, Class<? extends Annotation> annoClass) {
+    static void printMethodAnnos(AnnotatedClass<?> ac,
+            Class<? extends Annotation> annoClass) {
         AnnotatedMethod[] ams = ac.getAnnotatedMethods(annoClass);
-        System.out.println("Annotation "+annoClass+" on class "+ac.getAnnotatedClass()+" is present on methods:");
+        log.debug("Annotation " + annoClass + " on class "
+                + ac.getAnnotatedClass() + " is present on methods:");
         if (ams.length == 0) {
-            System.out.println("N/A");
+            log.debug("N/A");
         } else {
             for (AnnotatedMethod am : ams) {
-                System.out.println(am.getMethod() +" => "+ am.getAnnotation(annoClass));
+                log.debug(am.getMethod() + " => " + am.getAnnotation(annoClass));
             }
         }
     }
-
 }
 
 @Anno1("A_class_anno")
 class A {
     @Anno1("A_test1_anno")
-    public void test1() {}
+    public void test1() {
+    }
+
     @Anno1("A_test2_anno")
-    public void test2() {}
-    public void test3() {}
+    public void test2() {
+    }
+
+    public void test3() {
+    }
 }
 
 @Anno1("B_class_anno")
 class B extends A {
     @Override
     @Anno1("B_test1_anno")
-    public void test1() {}
+    public void test1() {
+    }
+
     @Override
     @Anno2("B_test2_anno2")
-    public void test2() {} // inherit annotation
+    public void test2() {
+    } // inherit annotation
+
     @Override
-    public void test3() {}
+    public void test3() {
+    }
 }
 
 @Anno2("I_class_anno")
@@ -116,10 +135,15 @@ interface I {
 
 class C extends B implements I {
     @Override
-    public void test1() {}
+    public void test1() {
+    }
+
     @Override
-    public void test2() {}
+    public void test2() {
+    }
+
     @Override
     @Anno1("C_test3_anno")
-    public void test3() {}
+    public void test3() {
+    }
 }
