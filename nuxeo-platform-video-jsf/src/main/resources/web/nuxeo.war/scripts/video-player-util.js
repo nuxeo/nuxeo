21a3316770b6b0f0a8872a757047cb36637db8dc
Thomas Roger <troger@nuxeo.com>
2011-11-02 17:00:16 +0100

    NXP-7581: make the soryboard not clickable when the Flash video player is used

--- nuxeo-platform-video-jsf/src/main/resources/web/nuxeo.war/scripts/video-player-util.js
+++ nuxeo-platform-video-jsf/src/main/resources/web/nuxeo.war/scripts/video-player-util.js
@@ -1,11 +1,13 @@
 jQuery(document).ready(function() {
   jQuery(".video-js").VideoJS();
-  var videoPlayer = jQuery(".video-js")[0].player;
-
-  jQuery(".videoStoryboardItem").click(function() {
-    videoPlayer.currentTime(parseFloat(jQuery(this).attr('timecode')));
-    videoPlayer.play();
-    return false;
-  })
+  var videoJsElement = jQuery(".video-js");
+  if (videoJsElement.length > 0 && videoJsElement[0].player !== 'undefined') {
+    var videoPlayer = videoJsElement[0].player;
+    jQuery(".videoStoryboardItem").css('cursor', 'pointer');
+    jQuery(".videoStoryboardItem").click(function() {
+      videoPlayer.currentTime(parseFloat(jQuery(this).attr('timecode')));
+      videoPlayer.play();
+      return false;
+    });
+  }
 });
-
