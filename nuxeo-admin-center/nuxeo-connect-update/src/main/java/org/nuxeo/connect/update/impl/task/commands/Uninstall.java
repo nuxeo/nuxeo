aa5efef8c5501096810d285c557eafa81a3c812d
Julien Carsique <jcarsique@nuxeo.com>
2011-11-16 18:46:01 +0100

    typo

--- nuxeo-admin-center/nuxeo-connect-update/src/main/java/org/nuxeo/connect/update/impl/task/commands/Uninstall.java
+++ nuxeo-admin-center/nuxeo-connect-update/src/main/java/org/nuxeo/connect/update/impl/task/commands/Uninstall.java
@@ -1,5 +1,5 @@
 /*
- * (C) Copyright 2006-2010 Nuxeo SAS (http://nuxeo.com/) and contributors.
+ * (C) Copyright 2006-2011 Nuxeo SA (http://nuxeo.com/) and contributors.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the GNU Lesser General Public License
@@ -79,7 +79,7 @@ public class Uninstall extends AbstractCommand {
                                 bundle.uninstall();
                             }
                         } catch (Throwable t) {
-                            // ingnore uninstall -> this may break the entire
+                            // ignore uninstall -> this may break the entire
                             // chain. Usually uninstall is done only when
                             // rollbacking or uninstalling - force restart
                             // required
