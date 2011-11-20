ce873995e7480e44124335dff2d985553184dacc
Anahide Tchertchian <at@nuxeo.com>
2011-11-20 15:34:28 +0100

    NXP-7676: handle local configuration of theme flavor, as well as logo now attached to it. Renamed seam component following ThemeActions interface from "WebcontainerThemeActions" to "themeActions" and changed its API. Removed deprecated unused widget template site_theme_widget.xhtml referencing it.

--- nuxeo-platform-webapp-core/src/main/java/org/nuxeo/ecm/webapp/webcontainer/ThemeActionsBean.java
+++ nuxeo-platform-webapp-core/src/main/java/org/nuxeo/ecm/webapp/webcontainer/ThemeActionsBean.java
@@ -17,65 +17,90 @@
 
 package org.nuxeo.ecm.webapp.webcontainer;
 
-import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
-
 import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
-import javax.faces.model.SelectItem;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.jboss.seam.ScopeType;
-import org.jboss.seam.annotations.In;
 import org.jboss.seam.annotations.Name;
 import org.jboss.seam.annotations.Scope;
-import org.nuxeo.ecm.core.api.CoreSession;
+import org.nuxeo.runtime.api.Framework;
 import org.nuxeo.theme.ApplicationType;
 import org.nuxeo.theme.Manager;
 import org.nuxeo.theme.NegotiationDef;
+import org.nuxeo.theme.negotiation.Negotiator;
+import org.nuxeo.theme.styling.service.ThemeStylingService;
+import org.nuxeo.theme.styling.service.descriptors.Flavor;
+import org.nuxeo.theme.styling.service.descriptors.Logo;
 import org.nuxeo.theme.types.TypeFamily;
 import org.nuxeo.theme.types.TypeRegistry;
 
-@Name("WebcontainerThemeActions")
+@Name("themeActions")
 @Scope(ScopeType.PAGE)
-public class ThemeActionsBean implements ThemeActions, Serializable {
+public class ThemeActionsBean implements ThemeActions {
 
     private static final long serialVersionUID = 1L;
 
     private static final Log log = LogFactory.getLog(ThemeActionsBean.class);
 
-    @In(create = true, required = false)
-    protected transient CoreSession documentManager;
-
-    public List<SelectItem> getAvailableThemes() {
-        List<SelectItem> themes = new ArrayList<SelectItem>();
-        for (String theme : Manager.getThemeManager().getThemeNames()) {
-            themes.add(new SelectItem(theme, theme));
-        }
-        return themes;
-    }
-
-    public List<SelectItem> getAvailablePages(String theme) {
-        List<SelectItem> pages = new ArrayList<SelectItem>();
-        if (theme != null && !theme.equals("")) {
-            for (String pageName : Manager.getThemeManager().getPageNames(theme)) {
-                pages.add(new SelectItem(pageName, pageName));
-            }
-        }
-        return pages;
-    }
-
     public String getDefaultTheme() {
-        FacesContext fContext = FacesContext.getCurrentInstance();
+        FacesContext faces = FacesContext.getCurrentInstance();
+        return getDefaultTheme(faces.getExternalContext());
+    }
+
+    protected String getDefaultTheme(ExternalContext ec) {
         // get the negotiation strategy
         TypeRegistry typeRegistry = Manager.getTypeRegistry();
-        final ExternalContext external = fContext.getExternalContext();
         ApplicationType app = (ApplicationType) typeRegistry.lookup(
-                TypeFamily.APPLICATION, external.getRequestContextPath());
+                TypeFamily.APPLICATION, ec.getRequestContextPath());
         NegotiationDef negotiation = app.getNegotiation();
         return negotiation.getDefaultTheme();
+    }
+
+    public Logo getLogo() {
+        FacesContext faces = FacesContext.getCurrentInstance();
+        final ExternalContext ec = faces.getExternalContext();
+        String flavor = (String) ec.getRequestMap().get(
+                Negotiator.NEGOTIATION_RESULT_PREFIX
+                        + Negotiator.NEGOTIATION_OBJECT.collection.name());
+        return getLogo(flavor);
+    }
+
+    public Logo getLogo(String flavorName) {
+        if (flavorName == null) {
+            return null;
+        }
+        ThemeStylingService service = getStylingService();
+        if (service == null) {
+            return null;
+        }
+        return service.getLogo(flavorName);
+    }
+
+    public Flavor getFlavor(String flavorName) {
+        if (flavorName == null) {
+            return null;
+        }
+        ThemeStylingService service = getStylingService();
+        if (service == null) {
+            return null;
+        }
+        return service.getFlavor(flavorName);
+    }
+
+    protected ThemeStylingService getStylingService() {
+        try {
+            ThemeStylingService service = Framework.getService(ThemeStylingService.class);
+            if (service == null) {
+                log.error("Missing ThemeStylingService");
+                return null;
+            }
+            return service;
+        } catch (Exception e) {
+            log.error(e);
+            return null;
+        }
     }
 
 }
