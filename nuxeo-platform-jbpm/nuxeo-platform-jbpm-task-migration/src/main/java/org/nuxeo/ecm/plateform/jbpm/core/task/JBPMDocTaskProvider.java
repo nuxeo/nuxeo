110065361c7c768148869fc4f27d290e9070f8c2
Thierry Delprat <tdelprat@nuxeo.com>
2011-11-30 23:06:30 +0100

    NXP-7852 test Jbpm => Document migration + clean up

--- nuxeo-platform-jbpm/nuxeo-platform-jbpm-task-migration/src/main/java/org/nuxeo/ecm/plateform/jbpm/core/task/JBPMDocTaskProvider.java
+++ nuxeo-platform-jbpm/nuxeo-platform-jbpm-task-migration/src/main/java/org/nuxeo/ecm/plateform/jbpm/core/task/JBPMDocTaskProvider.java
@@ -1,36 +1,47 @@
+/*
+ * (C) Copyright 2011 Nuxeo SA (http://nuxeo.com/) and contributors.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser General Public License
+ * (LGPL) version 2.1 which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl.html
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * Contributors:
+ *    Nuxeo
+ */
 package org.nuxeo.ecm.plateform.jbpm.core.task;
 
 import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.Set;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.jbpm.JbpmContext;
-import org.jbpm.taskmgmt.exe.PooledActor;
 import org.jbpm.taskmgmt.exe.TaskInstance;
-import org.nuxeo.ecm.automation.task.CreateTask;
 import org.nuxeo.ecm.core.api.ClientException;
 import org.nuxeo.ecm.core.api.CoreSession;
 import org.nuxeo.ecm.core.api.DocumentModel;
-import org.nuxeo.ecm.core.api.DocumentRef;
-import org.nuxeo.ecm.core.api.IdRef;
 import org.nuxeo.ecm.core.api.NuxeoPrincipal;
-import org.nuxeo.ecm.platform.comment.workflow.utils.CommentsConstants;
 import org.nuxeo.ecm.platform.jbpm.JbpmOperation;
 import org.nuxeo.ecm.platform.jbpm.JbpmService;
 import org.nuxeo.ecm.platform.jbpm.NuxeoJbpmException;
 import org.nuxeo.ecm.platform.task.Task;
 import org.nuxeo.ecm.platform.task.TaskProvider;
 import org.nuxeo.ecm.platform.task.TaskService;
-import org.nuxeo.ecm.platform.task.core.service.CreateTaskUnrestricted;
 import org.nuxeo.ecm.platform.usermanager.UserManager;
 import org.nuxeo.runtime.api.Framework;
 
 public class JBPMDocTaskProvider implements TaskProvider {
 
     private static final long serialVersionUID = 1L;
+
+    protected final static Log log = LogFactory.getLog(JBPMDocTaskProvider.class);
 
     public static final String PUBLISHER_JBPMTASK_NAME = "org.nuxeo.ecm.platform.publisher.jbpm.CoreProxyWithWorkflowFactory";
 
@@ -57,80 +68,6 @@ public class JBPMDocTaskProvider implements TaskProvider {
         }
     }
 
-    public Task migrateJBPMtoDocTask(TaskInstance ti, JbpmContext context,
-            CoreSession coreSession) throws ClientException {
-        ti = context.getTaskInstance(ti.getId());
-        eagerLoadTaskInstance(ti);
-        String commentId = (String) ti.getVariable("commentId");
-        String taskName = ti.getName();
-        if (commentId != null) {
-            return migrateCommentTask(ti, context, coreSession).get(0);
-        } else if (PUBLISHER_JBPMTASK_NAME.equals(taskName)) {
-            return migratePublisherTask(ti, context, coreSession).get(0);
-        } else {
-            return new JBPMTaskWrapper(ti);
-        }
-    }
-
-    private List<Task> migrateCommentTask(TaskInstance ti, JbpmContext context,
-            CoreSession coreSession) throws ClientException {
-        Map<String, String> vars = new HashMap<String, String>();
-        vars.put(
-                CreateTask.OperationTaskVariableName.createdFromCreateTaskOperation.name(),
-                "true");
-        vars.put(
-                CreateTask.OperationTaskVariableName.acceptOperationChain.name(),
-                CommentsConstants.ACCEPT_CHAIN_NAME);
-        vars.put(
-                CreateTask.OperationTaskVariableName.rejectOperationChain.name(),
-                CommentsConstants.REJECT_CHAIN_NAME);
-        vars.putAll(ti.getVariables());
-        Set<PooledActor> pooledActors = ti.getPooledActors();
-        List<String> actors = new ArrayList<String>();
-        for (PooledActor pooledActor : pooledActors) {
-            actors.add(pooledActor.getActorId());
-        }
-        String docId = (String) ti.getVariable(JbpmService.VariableName.documentId.name());
-        DocumentRef idRef = new IdRef(docId);
-        DocumentModel doc = coreSession.getDocument(idRef);
-        String prefixedInitiator = (String) ti.getVariable("initiator");
-        String userId = prefixedInitiator.split(":")[1];
-        NuxeoPrincipal user = getUserManager().getPrincipal(userId);
-        String parentPath = getTaskService().getTaskRootParentPath(coreSession);
-
-        CreateTaskUnrestricted runner = new CreateTaskUnrestricted(coreSession, user, doc,
-                CommentsConstants.MODERATION_DIRECTIVE_NAME, actors, false,
-                null, null, ti.getDueDate(), vars, parentPath);
-        runner.runUnrestricted();
-        List<Task> tasks = runner.getTasks();
-        ti.suspend();
-        ti.getProcessInstance().suspend();
-        return tasks;
-    }
-
-    private List<Task> migratePublisherTask(TaskInstance ti,
-            JbpmContext context, CoreSession coreSession)
-            throws ClientException {
-        String docId = (String) ti.getVariable(JbpmService.VariableName.documentId.name());
-        DocumentRef idRef = new IdRef(docId);
-        DocumentModel doc = coreSession.getDocument(idRef);
-        String userId = (String) ti.getVariable("initiator");
-        NuxeoPrincipal user = getUserManager().getPrincipal(userId);
-        Set<PooledActor> pooledActors = ti.getPooledActors();
-        List<String> actors = new ArrayList<String>();
-        for (PooledActor pooledActor : pooledActors) {
-            actors.add(pooledActor.getActorId());
-        }
-
-        String parentPath = getTaskService().getTaskRootParentPath(coreSession);
-        CreateTaskUnrestricted runner = new CreateTaskUnrestricted(coreSession,
-                user, doc, PUBLISHER_TASK_NAME, actors, false,
-                null, null, ti.getDueDate(), ti.getVariables(), parentPath);
-        runner.runUnrestricted();
-        List<Task> tasks = runner.getTasks();
-        ti.suspend();
-        return tasks;
-    }
 
     @Override
     public List<Task> getCurrentTaskInstances(final CoreSession coreSession)
@@ -147,16 +84,13 @@ public class JBPMDocTaskProvider implements TaskProvider {
                         List<TaskInstance> tis = getJbpmService().getCurrentTaskInstances(
                                 (NuxeoPrincipal) coreSession.getPrincipal(),
                                 null);
-                        List<Task> tasks = new ArrayList<Task>(tis.size());
-                        for (TaskInstance taskInstance : tis) {
-                            try {
-                                tasks.add(migrateJBPMtoDocTask(taskInstance,
-                                        context, coreSession));
-                            } catch (ClientException e) {
-                                throw new NuxeoJbpmException(e);
-                            }
+                        TaskMigrationRunner migrationRunner = new TaskMigrationRunner(tis, context, coreSession);
+                        try {
+                            return (Serializable) migrationRunner.migrate();
+                        } catch (ClientException e) {
+                            log.error("Unable to migrate task", e);
+                            return new ArrayList<Task>();
                         }
-                        return (Serializable) tasks;
                     }
                 });
         return migratedTasks;
@@ -176,16 +110,14 @@ public class JBPMDocTaskProvider implements TaskProvider {
 
                         List<TaskInstance> tis = getJbpmService().getCurrentTaskInstances(
                                 actors, null);
-                        List<Task> tasks = new ArrayList<Task>(tis.size());
-                        for (TaskInstance taskInstance : tis) {
-                            try {
-                                tasks.add(migrateJBPMtoDocTask(taskInstance,
-                                        context, coreSession));
-                            } catch (ClientException e) {
-                                throw new NuxeoJbpmException(e);
-                            }
+
+                        TaskMigrationRunner migrationRunner = new TaskMigrationRunner(tis, context, coreSession);
+                        try {
+                            return (Serializable) migrationRunner.migrate();
+                        } catch (ClientException e) {
+                            log.error("Unable to migrate task", e);
+                            return new ArrayList<Task>();
                         }
-                        return (Serializable) tasks;
                     }
                 });
         return migratedTasks;
@@ -201,21 +133,18 @@ public class JBPMDocTaskProvider implements TaskProvider {
                     private static final long serialVersionUID = 1L;
 
                     @Override
-                    public Serializable run(JbpmContext context)
+                    public Serializable run(final JbpmContext context)
                             throws NuxeoJbpmException {
 
-                        List<TaskInstance> tis = getJbpmService().getTaskInstances(
+                        final List<TaskInstance> tis = getJbpmService().getTaskInstances(
                                 dm, user, null);
-                        List<Task> tasks = new ArrayList<Task>(tis.size());
-                        for (TaskInstance taskInstance : tis) {
-                            try {
-                                tasks.add(migrateJBPMtoDocTask(taskInstance,
-                                        context, coreSession));
-                            } catch (ClientException e) {
-                                throw new NuxeoJbpmException(e);
-                            }
+                        TaskMigrationRunner migrationRunner = new TaskMigrationRunner(tis, context, coreSession);
+                        try {
+                            return (Serializable) migrationRunner.migrate();
+                        } catch (ClientException e) {
+                            log.error("Unable to migrate task", e);
+                            return new ArrayList<Task>();
                         }
-                        return (Serializable) tasks;
                     }
                 });
         return migratedTasks;
@@ -233,19 +162,15 @@ public class JBPMDocTaskProvider implements TaskProvider {
                     @Override
                     public Serializable run(JbpmContext context)
                             throws NuxeoJbpmException {
-
                         List<TaskInstance> tis = getJbpmService().getTaskInstances(
                                 dm, actors, null);
-                        List<Task> tasks = new ArrayList<Task>(tis.size());
-                        for (TaskInstance taskInstance : tis) {
-                            try {
-                                tasks.add(migrateJBPMtoDocTask(taskInstance,
-                                        context, coreSession));
-                            } catch (ClientException e) {
-                                throw new NuxeoJbpmException(e);
-                            }
+                        TaskMigrationRunner migrationRunner = new TaskMigrationRunner(tis, context, coreSession);
+                        try {
+                            return (Serializable) migrationRunner.migrate();
+                        } catch (ClientException e) {
+                            log.error("Unable to migrate task", e);
+                            return new ArrayList<Task>();
                         }
-                        return (Serializable) tasks;
                     }
                 });
         return migratedTasks;
--- nuxeo-platform-jbpm/nuxeo-platform-jbpm-task-migration/src/main/java/org/nuxeo/ecm/plateform/jbpm/core/task/JBPMTaskWrapper.java
+++ nuxeo-platform-jbpm/nuxeo-platform-jbpm-task-migration/src/main/java/org/nuxeo/ecm/plateform/jbpm/core/task/JBPMTaskWrapper.java
@@ -1,3 +1,19 @@
+/*
+ * (C) Copyright 2011 Nuxeo SA (http://nuxeo.com/) and contributors.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser General Public License
+ * (LGPL) version 2.1 which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl.html
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * Contributors:
+ *    Nuxeo
+ */
 package org.nuxeo.ecm.plateform.jbpm.core.task;
 
 import java.util.ArrayList;
@@ -16,6 +32,11 @@ import org.nuxeo.ecm.platform.jbpm.JbpmService;
 import org.nuxeo.ecm.platform.task.Task;
 import org.nuxeo.ecm.platform.task.TaskComment;
 
+/**
+ * Simple wrapper around a Jbpm {@link TaskInstance}
+ *
+ *
+ */
 public class JBPMTaskWrapper implements Task {
 
     private static final long serialVersionUID = 1L;
