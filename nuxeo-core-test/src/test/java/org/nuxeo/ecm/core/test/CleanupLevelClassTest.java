c669e2f50bf6f3829848263be5ca93ee8d20d8cf
Florent Guillaume <fg@nuxeo.com>
2011-12-01 15:24:21 +0100

    make sure test order does not matter

--- nuxeo-core-test/src/test/java/org/nuxeo/ecm/core/test/CleanupLevelClassTest.java
+++ nuxeo-core-test/src/test/java/org/nuxeo/ecm/core/test/CleanupLevelClassTest.java
@@ -13,7 +13,7 @@ package org.nuxeo.ecm.core.test;
 
 import static org.junit.Assert.assertTrue;
 
-import org.junit.Ignore;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.nuxeo.ecm.core.api.CoreSession;
@@ -31,7 +31,37 @@ public class CleanupLevelClassTest {
     @Inject
     CoreSession session;
 
+    // the order of execution of the test methods isn't guaranteed by JUnit and
+    // changed between Java 6 and Java 7, so the test decides order on its own
+
+    public static int phase;
+
+    @BeforeClass
+    public static void beforeClass() {
+        phase = 0;
+    }
+
     @Test
+    public void testA() throws Exception {
+        runTest();
+    }
+
+    @Test
+    public void testB() throws Exception {
+        runTest();
+    }
+
+    public void runTest() throws Exception {
+        switch(++phase) {
+        case 1:
+            firstTestToCreateADoc();
+            break;
+        case 2:
+            docStillExists();
+            break;
+        }
+    }
+
     public void firstTestToCreateADoc() throws Exception {
         DocumentModel doc = session.createDocumentModel("/", "default-domain",
                 "Domain");
@@ -41,10 +71,6 @@ public class CleanupLevelClassTest {
         assertTrue(session.exists(new PathRef("/default-domain")));
     }
 
-    // test order isn't guaranteed (and changed under Java 7)
-    // so this cannot work
-    @Ignore
-    @Test
     public void docStillExists() throws Exception {
         assertTrue(session.exists(new PathRef("/default-domain")));
     }
