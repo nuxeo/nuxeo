2cadc8deae78321388609b53aa1a2a93998ed21d
Anahide Tchertchian <at@nuxeo.com>
2011-12-01 05:43:43 +0100

    NXP-5879: add forgotten jQuery library for focus

---
+++ nuxeo-platform-webapp/src/main/resources/web/nuxeo.war/scripts/jquery/jquery.tools.focusfirst.js
@@ -0,0 +1,25 @@
+(function($) {
+
+  $.fn.focusFirst = function() {
+    var elem = $('input:visible', this).get(0);
+    var select = $('select:visible', this).get(0);
+    if (select && elem) {
+      if (select.offsetTop < elem.offsetTop) {
+        elem = select;
+      }
+    }
+
+    var textarea = $('textarea:visible', this).get(0);
+    if (textarea && elem) {
+      if (textarea.offsetTop < elem.offsetTop) {
+        elem = textarea;
+      }
+    } 
+  
+    if (elem) {
+      elem.focus();
+    }
+    return this;
+  };
+
+})(jQuery);
