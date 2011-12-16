9c56e0988166a723ede07b4f1fffaaac12398087
Julien Carsique <jcarsique@nuxeo.com>
2011-12-16 17:05:53 +0100

    NXP-8190 - update clone.py

--- clone.py
+++ clone.py
@@ -13,18 +13,21 @@
 ## Lesser General Public License for more details.
 ##
 ## Contributors:
+##     Stefane Fermigier
 ##     Julien Carsique
 ##
-## This script clones or updates Nuxeo source code from Mercurial repositories.
+## This script clones or updates Nuxeo source code from Git repositories.
 ##
 
-import re, os, sys, shlex, subprocess, urlparse, posixpath
+import re, os, sys, shlex, subprocess, platform, urllib, urlparse, posixpath, time, argparse
+
+driveletter = "G"
 
 def log(message):
     sys.stdout.write(message + os.linesep)
     sys.stdout.flush()
 
-def system(cmd):
+def system(cmd, failonerror=True):
     log("$> " + cmd)
     args = shlex.split(cmd)
     p = subprocess.Popen(args, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
@@ -34,7 +37,38 @@ def system(cmd):
     retcode = p.returncode
     if retcode != 0:
         log("[ERROR]: command returned non-zero exit code: %s" % cmd)
-        sys.exit(retcode)
+        if failonerror:
+            long_path_workaround_cleanup()
+            sys.exit(retcode)
+    return retcode
+
+def system_with_retries(cmd, failonerror=True):
+    retries = 0
+    while True:
+        retries += 1
+        retcode = system(cmd, False)
+        if retcode == 0:
+            return 0
+        elif retries > 10:
+            return system(cmd, failonerror)
+        else:
+            log("Error executing %s - retrying in 10 seconds..." % cmd)
+            time.sleep(10)
+
+def long_path_workaround_init():
+    # On Windows, try to map the current directory to an unused drive letter to shorten path names
+    if platform.system() != "Windows": return
+    for letter in "GHIJKLMNOPQRSTUVWXYZ":
+        if not os.path.isdir("%s:\\" % (letter,)):
+            driveletter = letter
+            cwd = os.getcwd()
+            system("SUBST %s: \"%s\"" % (driveletter, cwd))
+            os.chdir("%s:\\" % (driveletter,))
+            break
+
+def long_path_workaround_cleanup():
+    if platform.system() != "Windows": return
+    system("SUBST %s: /D" % (driveletter,), False)
 
 def check_output(cmd):
     p = subprocess.Popen(cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
@@ -44,29 +78,29 @@ def check_output(cmd):
         log(err)
     return out.strip()
 
-def fetch(module):
+def git_fetch(module):
     cwd = os.getcwd()
     if os.path.isdir(module):
         log("Updating " + module + "...")
-        os.chdir(module)
-        system("hg pull")
     else:
         log("Cloning " + module + "...")
-        system("hg clone %s/%s %s" % (root_url, module, module))
-        os.chdir(module)
-    system("hg up %s" % (branch))
+        repo_url = url_pattern.replace("module", module)
+        retcode = system("git clone %s" % (repo_url))
+    os.chdir(module)
+
+    if version in check_output(["git", "tag"]).split():
+        # the version is a tag name
+        system("git checkout %s" % version)
+    elif version not in check_output(["git", "branch"]).split():
+        # create the local branch if missing
+        system("git checkout -b %s %s/%s" % (version, alias, version))
+    else:
+        # reuse local branch
+        system("git checkout %s" % version)
+        log("Updating branch")
+        system("git pull %s %s" % (alias, version))
     os.chdir(cwd)
     log("")
-
-if len(sys.argv) > 1:
-    branch = sys.argv[1]
-else:
-    t = check_output(["hg", "id", "-bt"]).split()
-    branch = t[0]
-    if (len(t) > 1):
-        tag = t[1]
-    if 'tag' in globals() and tag != "tip":
-        branch = tag
 
 def url_normpath(url):
     parsed = urlparse.urlparse(url)
@@ -76,13 +110,39 @@ def url_normpath(url):
         path = posixpath.normpath(parsed.path)
     return urlparse.urlunparse(parsed[:2] + (path,) + parsed[3:])
 
+def get_current_version():
+    t = check_output(["git", "describe", "--all"]).split("/")
+    return t[1]
+
+
+long_path_workaround_init()
+
+parser = argparse.ArgumentParser(description='Clone or update Nuxeo source code from Git repositories.')
+parser.add_argument('version', nargs='?', default=get_current_version(), help='The version (branch or tag) to checkout')
+parser.add_argument('-r', dest='remote_alias', nargs='?', default='origin', help='The Git alias of remote URL (default: %(default)s)')
+
+args = parser.parse_args()
+version = args.version
+alias = args.remote_alias
 
 log("Cloning/updating parent pom")
-system("hg pull")
-system("hg up %s" % branch)
+system("git fetch %s" % alias)
+system("git checkout %s" % version)
 log("")
 
-root_url = url_normpath(check_output(["hg", "path", "default"]))
+# find the remote URL
+remote_lines = check_output(["git", "remote", "-v"]).split("\n")
+for remote_line in remote_lines:
+    remote_alias, remote_url, _ = remote_line.split()
+    if alias == remote_alias:
+        break
+
+#root_url = url_normpath(remote_url)
+is_online = remote_url.endswith("/nuxeo.git")
+if is_online:
+    url_pattern = remote_url.replace("nuxeo", "module", -1)
+else:
+    url_pattern = remote_url + "/module"
 
 log("Using maven introspection of the pom.xml files"
     " to find the list of sub-repositories")
@@ -92,21 +152,15 @@ for line in os.popen("mvn -N help:effective-pom"):
     if not m:
         continue
     module = m.group(1)
-    if module == "addons":
-        continue
-    fetch(module)
-
-fetch("nuxeo-distribution")
-
-if root_url.startswith("http"):
-    root_url = url_normpath(root_url.replace("/nuxeo", ""))
-fetch("addons")
+    git_fetch(module)
 
 cwd = os.getcwd()
+log("$> cd addons; clone.py -r %s %s" % (alias, version))
 os.chdir("addons")
-log("$> cd addons; clone.py %s" % branch)
-retcode = os.system("python clone.py %s" % branch)
+retcode = os.system("python clone.py -r %s %s" % (alias, version))
+os.chdir(cwd)
 if retcode != 0:
     log("[ERROR]: cloning addons failed.")
     sys.exit(retcode)
-os.chdir(cwd)
+
+long_path_workaround_cleanup()
