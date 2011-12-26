f428a1e2d8b91c7183af74ccce5eeaaf6a7d0786
Stephane Lacoin <slacoin@nuxeo.com>
2011-12-26 20:47:35 +0100

    NXP-8194 better tx fixture setup(cherry picked from commit 4d72331fc0429453167e3480c3f1f37e7b95fa29)

--- nuxeo-core-test/src/main/java/org/nuxeo/ecm/core/test/TransactionalFeature.java
+++ nuxeo-core-test/src/main/java/org/nuxeo/ecm/core/test/TransactionalFeature.java
@@ -11,37 +11,80 @@
  */
 package org.nuxeo.ecm.core.test;
 
+import java.lang.annotation.Annotation;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.naming.NoInitialContextException;
+
 import org.nuxeo.common.Environment;
 import org.nuxeo.common.jndi.NamingContextFactory;
+import org.nuxeo.ecm.core.test.annotations.TransactionalConfig;
 import org.nuxeo.runtime.jtajca.NuxeoContainer;
 import org.nuxeo.runtime.test.runner.Deploy;
 import org.nuxeo.runtime.test.runner.FeaturesRunner;
 import org.nuxeo.runtime.test.runner.SimpleFeature;
 import org.nuxeo.runtime.transaction.TransactionHelper;
 
-@Deploy({
-    "org.nuxeo.runtime.jtajca"
-})
+@Deploy({ "org.nuxeo.runtime.jtajca" })
+@TransactionalConfig()
 public class TransactionalFeature extends SimpleFeature {
+
+    protected TransactionalConfig config;
 
     @Override
     public void initialize(FeaturesRunner runner) throws Exception {
-        NamingContextFactory.setAsInitial();
-        NuxeoContainer.install();
+        config = runner.getDescription().getAnnotation(
+                TransactionalConfig.class);
+        if (config == null) {
+            config = new TransactionalConfig() {
+
+                @Override
+                public Class<? extends Annotation> annotationType() {
+                    return TransactionalConfig.class;
+                }
+
+                @Override
+                public boolean autoStart() {
+                    return true;
+                }
+            };
+        }
+        try {
+            Context comp = (Context) new InitialContext().lookup("java:comp/");
+            if (comp == null) {
+                NamingContextFactory.setAsInitial();
+            }
+        } catch (NoInitialContextException e) {
+            NuxeoContainer.install();
+        }
     }
 
     @Override
     public void start(FeaturesRunner runner) throws Exception {
-         Environment.getDefault().setHostApplicationName(Environment.NXSERVER_HOST);
+        if (config.autoStart() == false) {
+            return;
+        }
+        Environment.getDefault().setHostApplicationName(
+                Environment.NXSERVER_HOST);
     }
-    
+
+    boolean txStarted;
+
     @Override
     public void beforeRun(FeaturesRunner runner) throws Exception {
-        TransactionHelper.startTransaction();
+        if (config.autoStart() == false) {
+            return;
+        }
+        txStarted = TransactionHelper.startTransaction();
     }
 
     @Override
     public void afterRun(FeaturesRunner runner) throws Exception {
+        if (txStarted == false) {
+            return;
+        }
         TransactionHelper.commitOrRollbackTransaction();
     }
 }
