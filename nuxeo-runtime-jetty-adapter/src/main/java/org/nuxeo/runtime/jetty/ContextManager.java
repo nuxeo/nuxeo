af0934d32bd25541d180c5ceca29942bbc380d4d
Stephane Lacoin <slacoin@nuxeo.com>
2011-12-28 18:01:58 +0100

    NXP-8194: hook servlet context listener in jetty by contribution (cherry picked from commit 9fc95a2f2d3aab12bb9843410f94dc8eb63c0e32)

--- nuxeo-runtime-jetty-adapter/src/main/java/org/nuxeo/runtime/jetty/ContextManager.java
+++ nuxeo-runtime-jetty-adapter/src/main/java/org/nuxeo/runtime/jetty/ContextManager.java
@@ -19,10 +19,17 @@ package org.nuxeo.runtime.jetty;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.servlet.ServletContextListener;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.mortbay.jetty.Handler;
 import org.mortbay.jetty.Server;
+import org.mortbay.jetty.handler.HandlerCollection;
 import org.mortbay.jetty.servlet.Context;
 import org.mortbay.jetty.servlet.FilterHolder;
 import org.mortbay.jetty.servlet.ServletHolder;
+import org.mortbay.jetty.webapp.WebAppContext;
 
 /**
  * @author <a href="mailto:bs@nuxeo.com">Bogdan Stefanescu</a>
@@ -30,12 +37,18 @@ import org.mortbay.jetty.servlet.ServletHolder;
  */
 public class ContextManager {
 
+    protected Log log = LogFactory.getLog(ContextManager.class);
+
     protected Server server;
+
     protected Map<String, Context> contexts;
+
+    protected Map<String, ServletContextListenerDescriptor> listeners;
 
     public ContextManager(Server server) {
         this.server = server;
         this.contexts = new HashMap<String, Context>();
+        this.listeners = new HashMap<String, ServletContextListenerDescriptor>();
     }
 
     public Server getServer() {
@@ -46,7 +59,8 @@ public class ContextManager {
         String path = descriptor.getContext();
         Context ctx = contexts.get(path);
         if (ctx == null) {
-            ctx = new Context(server, path, Context.SESSIONS | Context.NO_SECURITY);
+            ctx = new Context(server, path, Context.SESSIONS
+                    | Context.NO_SECURITY);
             contexts.put(path, ctx);
         }
         FilterHolder holder = new FilterHolder(descriptor.getClazz());
@@ -58,18 +72,20 @@ public class ContextManager {
         if (desc != null) {
             holder.setDisplayName(desc);
         }
-        Map<String,String> params = descriptor.getInitParams();
+        Map<String, String> params = descriptor.getInitParams();
         if (params != null) {
             holder.setInitParameters(params);
         }
-        ctx.addFilter(holder, descriptor.getPath(), org.mortbay.jetty.Handler.DEFAULT);
+        ctx.addFilter(holder, descriptor.getPath(),
+                org.mortbay.jetty.Handler.DEFAULT);
     }
 
     public synchronized void addServlet(ServletDescriptor descriptor) {
         String path = descriptor.getContext();
         Context ctx = contexts.get(path);
         if (ctx == null) {
-            ctx = new Context(server, path, Context.SESSIONS | Context.NO_SECURITY);
+            ctx = new Context(server, path, Context.SESSIONS
+                    | Context.NO_SECURITY);
             contexts.put(path, ctx);
         }
         ServletHolder holder = new ServletHolder(descriptor.getClazz());
@@ -81,20 +97,46 @@ public class ContextManager {
         if (desc != null) {
             holder.setDisplayName(desc);
         }
-        Map<String,String> params = descriptor.getInitParams();
+        Map<String, String> params = descriptor.getInitParams();
         if (params != null) {
             holder.setInitParameters(params);
         }
         ctx.addServlet(holder, descriptor.getPath());
     }
 
+    public synchronized void addLifecycleListener(ServletContextListenerDescriptor descriptor) {
+        listeners.put(descriptor.name, descriptor);
+    }
+
+    public synchronized void removeLifecycleListener(ServletContextListenerDescriptor descriptor) {
+        listeners.remove(descriptor.name);
+    }
+
     public synchronized void removeFilter(FilterDescriptor descriptor) {
-        //TODO not yet implemented
+        // TODO not yet implemented
     }
 
     public synchronized void removeServlet(ServletDescriptor descriptor) {
-        //TODO not yet implemented
+        // TODO not yet implemented
     }
 
+    public void applyLifecycleListeners() {
+        HandlerCollection hc = (HandlerCollection) server.getHandler();
+        Handler[] handlers =  hc.getChildHandlersByClass(WebAppContext.class);
+        for (ServletContextListenerDescriptor desc : listeners.values()) {
+            ServletContextListener listener;
+            try {
+                listener = desc.clazz.newInstance();
+            } catch (Exception e) {
+                log.error("Cannot add life cycle listener " + desc.name, e);
+                continue;
+            }
+            for (Handler handler : handlers) {
+                WebAppContext context = (WebAppContext)handler;
+                if (context.getContextPath().matches(desc.context)) {
+                    context.addEventListener(listener);
+                }
+            }
+        }
+    }
 }
-
