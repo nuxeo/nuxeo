f0887461897f4ecd5088cc14b60fdbd02f5955be
Julien Carsique <jcarsique@nuxeo.com>
2012-01-03 19:16:37 +0100

    NXP-8196 - release script; factorized repository functions into nxutils.py

--- clone.py
+++ clone.py
@@ -18,6 +18,8 @@
 ##
 ## This script clones or updates Nuxeo source code from Git repositories.
 ##
+from scripts.nxutils import ExitException
+from scripts.nxutils import Repository
 from scripts.nxutils import assert_git_config
 from scripts.nxutils import check_output
 from scripts.nxutils import get_current_version
@@ -25,7 +27,6 @@ from scripts.nxutils import log
 from scripts.nxutils import long_path_workaround_cleanup
 from scripts.nxutils import long_path_workaround_init
 from scripts.nxutils import system
-from scripts.nxutils import Repository
 import optparse
 import os
 import platform
@@ -36,33 +37,35 @@ import sys
 import time
 
 
-basedir = os.getcwd()
-assert_git_config()
-driveletter = long_path_workaround_init()
+def main():
+    usage = "usage: %prog [options] version"
+    parser = optparse.OptionParser(
+        usage=usage,
+        description='Clone or update Nuxeo source code from Git repositories.')
+    parser.add_option(
+        '-r', action="store", type="string", dest='remote_alias',
+        default='origin',
+        help='The Git alias of remote URL (default: %default)')
+    parser.add_option(
+        "-a", "--all", action="store_true", dest="with_optionals",
+        default=False, help="Include 'optional' addons (default: %default)")
 
-usage = "usage: %prog [options] version"
-parser = optparse.OptionParser(
-    usage=usage,
-    description='Clone or update Nuxeo source code from Git repositories.')
-parser.add_option(
-    '-r', action="store", type="string", dest='remote_alias',
-    default='origin', help='The Git alias of remote URL (default: %default)')
-parser.add_option(
-    "-a", "--all", action="store_true", dest="with_optionals", default=False,
-    help="Include 'optional' addons (default: %default)")
+    (options, args) = parser.parse_args()
+    if len(args) == 0:
+        version = get_current_version()
+    elif len(args) == 1:
+        version = args[0]
+    else:
+        log("[ERROR] Version must be a single argument", sys.stderr)
+        sys.exit(1)
 
-(options, args) = parser.parse_args()
-alias = options.remote_alias
-with_optionals = options.with_optionals
-if len(args) == 0:
-    version = get_current_version()
-elif len(args) == 1:
-    version = args[0]
-else:
-    log("[ERROR]: version must be a single argument", sys.stderr)
-    sys.exit(1)
+    repo = Repository(os.getcwd(), options.remote_alias)
+    try:
+        repo.clone(version, options.with_optionals)
+    except ExitException, e:
+        sys.exit (e.return_code)
+    finally:
+        repo.cleanup()
 
-repo = Repository(alias)
-repo.clone(version, with_optionals)
-
-long_path_workaround_cleanup(driveletter)
+if __name__ == '__main__':
+    main()
--- scripts/nxutils.py
+++ scripts/nxutils.py
@@ -17,18 +17,26 @@
 ##
 ## Utilities for Python scripts.
 ##
-import sys
+import optparse
 import os
+import platform
 import re
 import shlex
 import subprocess
-import platform
+import sys
 import time
-import optparse
+
+
+class ExitException(Exception):
+    def __init__(self, return_code):
+        self.return_code = return_code
 
 
 class Repository(object):
-    def __init__(self, alias):
+    def __init__(self, basedir, alias):
+        assert_git_config()
+        self.driveletter = long_path_workaround_init()
+        self.basedir = basedir
         self.alias = alias
         # find the remote URL
         remote_lines = check_output(["git", "remote", "-v"]).split("\n")
@@ -37,54 +45,21 @@ class Repository(object):
             if alias == remote_alias:
                 break
 
-        is_online = remote_url.endswith("/nuxeo.git")
-        if is_online:
+        self.is_online = remote_url.endswith("/nuxeo.git")
+        if self.is_online:
             self.url_pattern = re.sub("(.*)nuxeo", r"\1module", remote_url)
         else:
             self.url_pattern = remote_url + "/module"
+        self.modules = []
+        self.addons = []
 
-    def git_fetch(self, module, version):
-        repo_url = self.url_pattern.replace("module", module)
-        cwd = os.getcwd()
-        if os.path.isdir(module):
-            log("Updating " + module + "...")
-            os.chdir(module)
-            system("git fetch %s" % (self.alias))
-        else:
-            log("Cloning " + module + "...")
-            system("git clone %s" % (repo_url))
-            os.chdir(module)
+    def cleanup(self):
+        if hasattr(self, "driveletter"):
+            long_path_workaround_cleanup(self.driveletter, self.basedir)
 
-        if version in check_output(["git", "tag"]).split():
-            # the version is a tag name
-            system("git checkout %s" % version)
-        elif version not in check_output(["git", "branch"]).split():
-            # create the local branch if missing
-            system("git checkout --track -b %s %s/%s" % (version, self.alias, version))
-        else:
-            # reuse local branch
-            system("git checkout %s" % version)
-            log("Updating branch")
-            system("git rebase %s/%s" % (self.alias, version))
-        os.chdir(cwd)
-        log("")
-
-    def clone(self, version, with_optionals=False):
-        log("Cloning/updating parent pom")
-        system("git fetch %s" % (self.alias))
-        if version in check_output(["git", "tag"]).split():
-            # the version is a tag name
-            system("git checkout %s" % version)
-        elif version not in check_output(["git", "branch"]).split():
-            # create the local branch if missing
-            system("git checkout --track -b %s %s/%s" % (version, self.alias, version))
-        else:
-            # reuse local branch
-            system("git checkout %s" % version)
-            log("Updating branch")
-            system("git rebase %s/%s" % (self.alias, version))
-        log("")
-
+    def eval_modules(self):
+        os.chdir(self.basedir)
+        self.modules = []
         log("Using maven introspection of the pom.xml files"
             " to find the list of sub-repositories")
         for line in os.popen("mvn -N help:effective-pom"):
@@ -92,12 +67,11 @@ class Repository(object):
             m = re.match("<module>(.*?)</module>", line)
             if not m:
                 continue
-            module = m.group(1)
-            self.git_fetch(module, version)
+            self.modules.append(m.group(1))
 
-        cwd = os.getcwd()
-        # Addons
-        os.chdir("addons")
+    def eval_addons(self, with_optionals=False):
+        os.chdir(os.path.join(self.basedir, "addons"))
+        self.addons = []
         log("Using maven introspection of the pom.xml files"
             " to find the list of addons")
         all_lines = os.popen("mvn -N help:effective-pom").readlines()
@@ -108,8 +82,73 @@ class Repository(object):
             m = re.match("<module>(.*?)</module>", line)
             if not m:
                 continue
-            addon = m.group(1)
-            self.git_fetch(addon, version)
+            self.addons.append(m.group(1))
+
+    def git_pull(self, module, version):
+        repo_url = self.url_pattern.replace("module", module)
+        cwd = os.getcwd()
+        if os.path.isdir(module):
+            log("Updating " + module + "...")
+            os.chdir(module)
+            system("git fetch %s" % (self.alias))
+        else:
+            log("Cloning " + module + "...")
+            system("git clone %s" % (repo_url))
+            os.chdir(module)
+        self.git_update(version)
+        os.chdir(cwd)
+
+    def system_recurse(self, command):
+        """Execute the given command on all repositories"""
+        cwd = os.getcwd()
+        os.chdir(self.basedir)
+        system(command)
+        if not self.modules:
+            self.eval_modules()
+        for module in self.modules:
+            os.chdir(os.path.join(self.basedir, module))
+            system(command)
+        if not self.addons:
+            self.eval_addons()
+        for addon in self.addons:
+            os.chdir(os.path.join(self.basedir, "addons", addon))
+            system(command)
+        os.chdir(cwd)
+
+    def git_update(self, version):
+        if version in check_output(["git", "tag"]).split():
+            # the version is a tag name
+            system("git checkout %s" % version)
+        elif version not in check_output(["git", "branch"]).split():
+            # create the local branch if missing
+            system("git checkout --track -b %s %s/%s" % (version, self.alias, version))
+        else:
+            # reuse local branch
+            system("git checkout %s" % version)
+            log("Updating branch")
+            system("git rebase %s/%s" % (self.alias, version))
+        log("")
+
+    def clone(self, version, with_optionals=False):
+        log("Cloning/updating parent pom")
+        system("git fetch %s" % (self.alias))
+        self.git_update(version)
+
+        # Main modules
+        self.eval_modules()
+        for module in self.modules:
+            self.git_pull(module, version)
+
+        # Addons
+        cwd = os.getcwd()
+        os.chdir(os.path.join(self.basedir, "addons"))
+        self.eval_addons(with_optionals)
+        if not self.is_online:
+            self.url_pattern = self.url_pattern.replace("module", "addons/module")
+        for addon in self.addons:
+            self.git_pull(addon, version)
+        if not self.is_online:
+            self.url_pattern = self.url_pattern.replace("addons/module", "module")
         os.chdir(cwd)
 
 
@@ -128,10 +167,9 @@ def system(cmd, failonerror=True):
     sys.stdout.flush()
     retcode = p.returncode
     if retcode != 0:
-        log("[ERROR]: command returned non-zero exit code: %s" % cmd, sys.stderr)
+        log("[ERROR] Command returned non-zero exit code: %s" % cmd, sys.stderr)
         if failonerror:
-            long_path_workaround_cleanup()
-            sys.exit(retcode)
+            raise ExitException(retcode)
     return retcode
 
 
@@ -165,7 +203,7 @@ def long_path_workaround_init():
     return driveletter
 
 
-def long_path_workaround_cleanup(driveletter):
+def long_path_workaround_cleanup(driveletter, basedir):
     if driveletter != None:
         os.chdir(basedir)
         system("SUBST %s: /D" % (driveletter,), False)
@@ -175,7 +213,7 @@ def check_output(cmd):
     p = subprocess.Popen(cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
     out, err = p.communicate()
     if err != None:
-        log("[ERROR]: command", str(cmd), " returned an error:", sys.stderr)
+        log("[ERROR] Command", str(cmd), " returned an error:", sys.stderr)
         log(err, sys.stderr)
     return out.strip()
 
@@ -188,8 +226,8 @@ def get_current_version():
 def assert_git_config():
     t = check_output(["git", "config", "--get", "color.branch"])
     if "always" in t:
-        log("[ERROR]: The git color mode should be auto not always, try:", sys.stderr)
+        log("[ERROR] The git color mode should be auto not always, try:", sys.stderr)
         log(" git config --global color.branch auto", sys.stderr)
         log(" git config --global color.status auto", sys.stderr)
-        sys.exit(1)
+        raise ExitException(1)
 
