1cbe60bbf1678acf6631ef93f49418c097b7eeed
Thierry Martins <tmartins@nuxeo.com>
2012-01-05 15:29:08 +0100

    NXP-8561: use the character '|' to split the sort infos

--- nuxeo-automation/nuxeo-automation-features/src/main/java/org/nuxeo/ecm/automation/core/operations/services/DocumentPageProviderOperation.java
+++ nuxeo-automation/nuxeo-automation-features/src/main/java/org/nuxeo/ecm/automation/core/operations/services/DocumentPageProviderOperation.java
@@ -87,8 +87,8 @@ public class DocumentPageProviderOperation {
         if (sortInfoAsStringList != null) {
             for (String sortInfoDesc : sortInfoAsStringList) {
                 SortInfo sortInfo;
-                if (sortInfoDesc.contains(":")) {
-                    String[] parts = sortInfoDesc.split(":");
+                if (sortInfoDesc.contains("|")) {
+                    String[] parts = sortInfoDesc.split("|");
                     sortInfo = new SortInfo(parts[0],
                             Boolean.parseBoolean(parts[1]));
                 } else {
