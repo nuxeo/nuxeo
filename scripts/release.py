91f30081c12837d1ec69167141d8157661112251
Julien Carsique <jcarsique@nuxeo.com>
2012-01-05 18:34:23 +0100

    NXP-8196 - work also with nuxeo.version property

--- scripts/release.py
+++ scripts/release.py
@@ -173,7 +173,7 @@ class Release(object):
         if elem is not None and elem.text == self.snapshot:
             elem.text = self.tag
         # Properties
-        prop_pattern = re.compile("{" + namespaces.get("pom") + "}nuxeo\..*\.version")
+        prop_pattern = re.compile("{" + namespaces.get("pom") + "}nuxeo\..*version")
         properties = tree.getroot().find("pom:properties", namespaces)
         if properties is not None:
             for property in properties.getchildren():
