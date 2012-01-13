3e82b233c33909c4afec3c5b943b9066f3e6f095
Arnaud Kervern <akervern@nuxeo.com>
2012-01-13 10:40:03 +0100

    NXP-8557: Change task sorting to ensure to have the same order even in Windows ...

--- nuxeo-platform-jbpm/nuxeo-platform-jbpm-core/src/test/java/org/nuxeo/ecm/platform/jbpm/core/JbpmTaskServiceTest.java
+++ nuxeo-platform-jbpm/nuxeo-platform-jbpm-core/src/test/java/org/nuxeo/ecm/platform/jbpm/core/JbpmTaskServiceTest.java
@@ -264,8 +264,13 @@ public class JbpmTaskServiceTest extends SQLRepositoryTestCase {
                 (NuxeoPrincipal) null, null);
         Collections.sort(tasks, new Comparator<TaskInstance>() {
 
+            //Arbitrary invert sort tasks through pooled actor id to ensure to have EVERYTIME the same order
+            //it looks that sorting with createDate field is not Windows proof.
             public int compare(TaskInstance o1, TaskInstance o2) {
-                return o1.getCreate().compareTo(o2.getCreate());
+                String actor1 = ((PooledActor)o1.getPooledActors().toArray()[0]).getActorId();
+                String actor2 = ((PooledActor)o2.getPooledActors().toArray()[0]).getActorId();
+
+                return actor1.compareTo(actor2) * -1;
             }
 
         });
