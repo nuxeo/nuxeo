248ff2d63785980bfd798d50632beeae4c891d87
Stephane Lacoin <slacoin@nuxeo.com>
2012-02-16 11:49:05 +0100

    NXP-8863 repo init depends on application started in tests
    Conflicts:
    
    	nuxeo-core-storage-sql/nuxeo-core-storage-sql-test/src/main/java/org/nuxeo/ecm/core/storage/sql/TXSQLRepositoryTestCase.java

--- nuxeo-core-storage-sql/nuxeo-core-storage-sql-test/src/main/java/org/nuxeo/ecm/core/storage/sql/TXSQLRepositoryTestCase.java
+++ nuxeo-core-storage-sql/nuxeo-core-storage-sql-test/src/main/java/org/nuxeo/ecm/core/storage/sql/TXSQLRepositoryTestCase.java
@@ -12,6 +12,8 @@
 
 package org.nuxeo.ecm.core.storage.sql;
 
+import org.nuxeo.common.Environment;
+import org.nuxeo.runtime.api.Framework;
 import org.nuxeo.runtime.jtajca.NuxeoContainer;
 import org.nuxeo.runtime.transaction.TransactionHelper;
 
@@ -26,6 +28,8 @@ public class TXSQLRepositoryTestCase extends SQLRepositoryTestCase {
     public void setUp() throws Exception {
         setUpContainer();
         super.setUp(); // calls deployRepositoryConfig()
+        Environment.getDefault().setHostApplicationName(Environment.NXSERVER_HOST);
+        fireFrameworkStarted();
         TransactionHelper.startTransaction();
         openSession();
     }
