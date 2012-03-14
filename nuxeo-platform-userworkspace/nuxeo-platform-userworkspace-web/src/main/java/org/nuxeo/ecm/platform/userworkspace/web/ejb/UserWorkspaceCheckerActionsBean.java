500c494a234d32bbfc27f35a536de114788fe090
Thierry Martins <tmartins@nuxeo.com>
2012-03-14 15:53:37 +0100

    NXP-9034: add a new factory to get current personal workspace path

--- nuxeo-platform-userworkspace/nuxeo-platform-userworkspace-web/src/main/java/org/nuxeo/ecm/platform/userworkspace/web/ejb/UserWorkspaceCheckerActionsBean.java
+++ nuxeo-platform-userworkspace/nuxeo-platform-userworkspace-web/src/main/java/org/nuxeo/ecm/platform/userworkspace/web/ejb/UserWorkspaceCheckerActionsBean.java
@@ -25,12 +25,19 @@ import java.io.Serializable;
 import org.jboss.seam.annotations.Factory;
 import org.jboss.seam.annotations.In;
 import org.jboss.seam.annotations.Name;
+import org.jboss.seam.annotations.Observer;
 import org.jboss.seam.annotations.Scope;
+import org.jboss.seam.annotations.intercept.BypassInterceptors;
+import org.nuxeo.common.utils.Path;
+import org.nuxeo.ecm.core.api.ClientException;
+import org.nuxeo.ecm.core.api.CoreSession;
 import org.nuxeo.ecm.platform.ui.web.api.NavigationContext;
 import org.nuxeo.ecm.platform.userworkspace.constants.UserWorkspaceConstants;
+import org.nuxeo.ecm.webapp.helpers.EventNames;
 
 /**
  * Standalone class to check if navigation is in the user workspaces
+ * and if needed get the current personal workspace path
  *
  * @since 5.6
  * @author Thierry Martins <tm@nuxeo.com>
@@ -43,9 +50,51 @@ public class UserWorkspaceCheckerActionsBean implements Serializable {
     @In(create = true)
     protected transient NavigationContext navigationContext;
 
+    @In(required = false, create = true)
+    protected transient CoreSession documentManager;
+
+    protected Boolean isUserWorkspace;
+
+    protected String currentPersonalWorkspacePath;
+
     @Factory(value = "isUserWorkspace", scope = EVENT)
     public Boolean computeIsUserWorkspace() {
-        return navigationContext.getCurrentDocument().getPathAsString().contains(
-                UserWorkspaceConstants.USERS_PERSONAL_WORKSPACES_ROOT);
+        if (isUserWorkspace == null) {
+            isUserWorkspace = navigationContext.getCurrentDocument().getPathAsString().contains(
+                    UserWorkspaceConstants.USERS_PERSONAL_WORKSPACES_ROOT);
+        }
+        return isUserWorkspace;
+    }
+
+    @Factory(value = "currentPersonalWorkspacePath", scope = EVENT)
+    public String getCurrentPersonalWorkspace() throws ClientException {
+        if (currentPersonalWorkspacePath == null
+                && Boolean.TRUE.equals(isUserWorkspace)) {
+            // Do not compute path if not necessary
+            Path path = navigationContext.getCurrentDocument().getPath();
+            String lastSegment = "";
+            while (!path.isRoot() || !path.isEmpty()) {
+                if (UserWorkspaceConstants.USERS_PERSONAL_WORKSPACES_ROOT.equals(path.lastSegment())) {
+                    if (lastSegment.isEmpty()) {
+                        currentPersonalWorkspacePath = "";
+                    } else {
+                        currentPersonalWorkspacePath = path.append(lastSegment).toString();
+                    }
+                    return currentPersonalWorkspacePath;
+                } else {
+                    lastSegment = path.lastSegment();
+                }
+                path = path.removeLastSegments(1);
+            }
+        }
+        return currentPersonalWorkspacePath;
+    }
+
+    @Observer(value = { EventNames.USER_ALL_DOCUMENT_TYPES_SELECTION_CHANGED,
+            EventNames.LOCATION_SELECTION_CHANGED }, create = false)
+    @BypassInterceptors
+    public void reset() {
+        isUserWorkspace = null;
+        currentPersonalWorkspacePath = null;
     }
 }
