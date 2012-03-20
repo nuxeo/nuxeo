1f53c65d994704cef0e702b1d118e29cbc6be809
Stephane Lacoin <slacoin@nuxeo.com>
2012-03-20 09:59:14 +0100

    NXP-9012 removed wi session cache

--- nuxeo-platform-wi-backend/src/main/java/org/nuxeo/ecm/platform/wi/filter/WIRequestFilter.java
+++ nuxeo-platform-wi-backend/src/main/java/org/nuxeo/ecm/platform/wi/filter/WIRequestFilter.java
@@ -37,6 +37,7 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.nuxeo.ecm.platform.web.common.requestcontroller.filter.BufferingHttpServletResponse;
 import org.nuxeo.ecm.platform.web.common.requestcontroller.filter.RemoteHostGuessExtractor;
+import org.nuxeo.ecm.platform.wi.backend.Backend;
 import org.nuxeo.runtime.transaction.TransactionHelper;
 
 public class WIRequestFilter implements Filter {
@@ -48,12 +49,6 @@ public class WIRequestFilter implements Filter {
     public static final String SESSION_KEY = "org.nuxeo.ecm.platform.wi.session";
 
     public static final String BACKEND_KEY = "org.nuxeo.ecm.platform.wi.backend";
-
-    public static final String SESSION_LOCK_KEY = "SessionLockKey";
-
-    public static final String SESSION_LOCK_TIME = "SessionLockTime";
-
-    public static final String SYNCED_REQUEST_FLAG = "NuxeoSessionAlreadySync";
 
     public static final int LOCK_TIMOUT_S = 120;
 
@@ -70,13 +65,11 @@ public class WIRequestFilter implements Filter {
         HttpServletRequest httpRequest = (HttpServletRequest) request;
         HttpServletResponse httpResponse = (HttpServletResponse) response;
 
-        if (isWIRequest(httpRequest)) {
-            WISession session = SessionCacheHolder.getInstance().getCache().get(
-                    httpRequest);
+        if (isWIRequest(httpRequest) && httpRequest.getAttribute(SESSION_KEY) != null) {
+
+            WISession session = new WISession(null);
             httpRequest.setAttribute(SESSION_KEY, session);
 
-            boolean sessionSynched = false;
-            sessionSynched = simpleSyncOnSession(httpRequest);
             boolean txStarted = false;
             try {
                 txStarted = TransactionHelper.startTransaction();
@@ -108,8 +101,9 @@ public class WIRequestFilter implements Filter {
                         ((BufferingHttpServletResponse) response).stopBuffering();
                     }
                 }
-                if (sessionSynched) {
-                    simpleReleaseSyncOnSession(httpRequest);
+                Backend backend = (Backend)session.getAttribute(BACKEND_KEY);
+                if (backend != null) {
+                    backend.destroy();
                 }
                 if (log.isDebugEnabled()) {
                     log.debug(doFormatLogMessage(httpRequest,
@@ -132,80 +126,6 @@ public class WIRequestFilter implements Filter {
                 && (ua.contains(WEBDAV_USERAGENT) || ua.contains(MSOFFICE_USERAGENT));
     }
 
-    protected boolean simpleSyncOnSession(HttpServletRequest request) {
-        if (log.isDebugEnabled()) {
-            log.debug(doFormatLogMessage(request, "Trying to sync on session "));
-        }
-
-        if (request.getAttribute(SYNCED_REQUEST_FLAG) != null) {
-            if (log.isWarnEnabled()) {
-                log.warn(doFormatLogMessage(request,
-                        "Request has already be synced, filter is reentrant, exiting without locking"));
-            }
-            return false;
-        }
-
-        WISession session = (WISession) request.getAttribute(SESSION_KEY);
-
-        Lock lock = (Lock) session.getAttribute(SESSION_LOCK_KEY);
-        if (lock == null) {
-            lock = new ReentrantLock();
-            session.setAttribute(SESSION_LOCK_KEY, lock);
-        }
-
-        boolean locked = false;
-        try {
-            locked = lock.tryLock(LOCK_TIMOUT_S, TimeUnit.SECONDS);
-        } catch (InterruptedException e) {
-            log.error(
-                    doFormatLogMessage(request,
-                            "Unable to acuire lock for Session sync"), e);
-            return false;
-        }
-
-        if (locked) {
-            request.setAttribute(SYNCED_REQUEST_FLAG, Boolean.TRUE);
-            request.setAttribute(SESSION_LOCK_TIME,
-                    Long.valueOf(System.currentTimeMillis()));
-            if (log.isDebugEnabled()) {
-                log.debug(doFormatLogMessage(request,
-                        "Request synced on session"));
-            }
-        } else {
-            if (log.isDebugEnabled()) {
-                log.debug(doFormatLogMessage(request, "Sync timeout"));
-            }
-        }
-
-        return locked;
-    }
-
-    protected void simpleReleaseSyncOnSession(HttpServletRequest request) {
-        /*
-         * HttpSession httpSession = request.getSession(false); if (httpSession
-         * == null) { if (log.isDebugEnabled()) { log.debug(doFormatLogMessage(
-         * request,
-         * "No more HttpSession : can not unlock !, HttpSession must have been invalidated"
-         * )); } return; }
-         */
-
-        WISession session = (WISession) request.getAttribute(SESSION_KEY);
-
-        log.debug("Trying to unlock on httpSession key " + session.getKey()
-                + " WISession:" + session.getKey() + " on Thread "
-                + Thread.currentThread().getId());
-
-        Lock lock = (Lock) session.getAttribute(SESSION_LOCK_KEY);
-        if (lock == null) {
-            log.error("Unable to find session lock, HttpSession may have been invalidated");
-        } else {
-            lock.unlock();
-            if (log.isDebugEnabled()) {
-                log.debug("session unlocked on Thread ");
-                log.debug(doExecutionRequestLogMessage(request));
-            }
-        }
-    }
 
     protected String doFormatLogMessage(HttpServletRequest request, String info) {
         String remoteHost = RemoteHostGuessExtractor.getRemoteHost(request);
@@ -219,18 +139,6 @@ public class WIRequestFilter implements Filter {
         return "remote=" + remoteHost + ",principal=" + principalName + ",uri="
                 + uri + ", method=" + method + ",session=" + sessionId
                 + ",thread=" + threadName + ",info=" + info;
-    }
-
-    protected String doExecutionRequestLogMessage(HttpServletRequest request) {
-        Object lockTime = request.getAttribute(SESSION_LOCK_TIME);
-        if (lockTime != null) {
-            long time = ((Long) lockTime).longValue();
-            long executionTime = System.currentTimeMillis() - time;
-            return doFormatLogMessage(request, "Execution time:"
-                    + executionTime + " ms.");
-        } else {
-            return doFormatLogMessage(request, "Unknown time of execution");
-        }
     }
 
 }
