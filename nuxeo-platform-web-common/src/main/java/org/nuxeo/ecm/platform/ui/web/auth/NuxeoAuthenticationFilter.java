5e1d526d80638b18e480f01d97ec3eda18ae935c
Stephane Lacoin <slacoin@nuxeo.com>
2012-03-22 09:05:32 +0100

    NXP-9090 bypass auth for open URLs

--- nuxeo-platform-web-common/src/main/java/org/nuxeo/ecm/platform/ui/web/auth/NuxeoAuthenticationFilter.java
+++ nuxeo-platform-web-common/src/main/java/org/nuxeo/ecm/platform/ui/web/auth/NuxeoAuthenticationFilter.java
@@ -353,6 +353,11 @@ public class NuxeoAuthenticationFilter implements Filter {
             ServletResponse response, FilterChain chain) throws IOException,
             ServletException {
 
+        if (bypassAuth((HttpServletRequest)request)) {
+            chain.doFilter(request, response);
+            return;
+        }
+
         String tokenPage = getRequestedPage(request);
         if (tokenPage.equals(SWITCH_USER_PAGE)) {
             boolean result = switchUser(request, response, chain);
