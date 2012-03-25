ef285e5c00982272500834eabc77c2259cd8b87f
Mathieu Guillaume <mg@nuxeo.com>
2012-03-25 10:19:48 +0200

    Revert "NXP-9052 Adding a new property to reset the default maxPageSize value used in PageProviders"
    This reverts commit 9233f61f6bdf9ccc9157724dc81f7d7ba73dbeed.

--- nuxeo-platform-query-api/src/main/java/org/nuxeo/ecm/platform/query/api/AbstractPageProvider.java
+++ nuxeo-platform-query-api/src/main/java/org/nuxeo/ecm/platform/query/api/AbstractPageProvider.java
@@ -28,7 +28,6 @@ import org.apache.commons.logging.LogFactory;
 import org.nuxeo.ecm.core.api.ClientException;
 import org.nuxeo.ecm.core.api.DocumentModel;
 import org.nuxeo.ecm.core.api.SortInfo;
-import org.nuxeo.runtime.api.Framework;
 
 /**
  * Basic implementation for a {@link PageProvider}
@@ -47,7 +46,7 @@ public abstract class AbstractPageProvider<T> implements PageProvider<T> {
 
     protected long pageSize = 0;
 
-    protected long maxPageSize = getDefaultMaxPageSize();
+    protected long maxPageSize = DEFAULT_MAX_PAGE_SIZE;
 
     protected long resultsCount = UNKNOWN_SIZE;
 
@@ -580,7 +579,7 @@ public abstract class AbstractPageProvider<T> implements PageProvider<T> {
         long pageSize = getPageSize();
         long maxPageSize = getMaxPageSize();
         if (maxPageSize < 0) {
-            maxPageSize = getDefaultMaxPageSize();
+            maxPageSize = DEFAULT_MAX_PAGE_SIZE;
         }
         if (pageSize <= 0) {
             return maxPageSize;
@@ -589,19 +588,6 @@ public abstract class AbstractPageProvider<T> implements PageProvider<T> {
             return maxPageSize;
         }
         return pageSize;
-    }
-
-    protected long getDefaultMaxPageSize() {
-        String customDefaultMaxPageSize = Framework.getProperty("org.nuxeo.ecm.platform.query.api.PageProvider.customDefaultMaxPageSize");
-        if (customDefaultMaxPageSize == null) {
-            return DEFAULT_MAX_PAGE_SIZE;
-        }
-        try {
-            return Long.parseLong(customDefaultMaxPageSize);
-        } catch (NumberFormatException e) {
-            log.warn("No valid custom max page size defined (\"org.nuxeo.ecm.platform.query.api.PageProvider.customDefaultMaxPageSize\"): Should be a long value.");
-        }
-        return DEFAULT_MAX_PAGE_SIZE;
     }
 
 }
