fb245aebcba1481a363da5f99830400f4717405f
Antoine Taillefer <ataillefer@nuxeo.com>
2012-03-29 17:38:23 +0200

    NXP-9118: Allow version selection and action contribution to the Archived versions sub-tab

---
+++ deploy_diff.sh
@@ -0,0 +1,7 @@
+
+cp nuxeo-jsf/nuxeo-platform-webapp-base/target/nuxeo-platform-webapp-base-5.6-SNAPSHOT.jar ~/nuxeo/nuxeo-cap-5.6-SNAPSHOT-tomcat/nxserver/bundles/
+cp nuxeo-dm/nuxeo-platform-webapp-core/target/nuxeo-platform-webapp-core-5.6-SNAPSHOT.jar ~/nuxeo/nuxeo-cap-5.6-SNAPSHOT-tomcat/nxserver/bundles/
+cp nuxeo-features/nuxeo-platform-lang/target/nuxeo-platform-lang-5.6-SNAPSHOT.jar ~/nuxeo/nuxeo-cap-5.6-SNAPSHOT-tomcat/nxserver/bundles/
+cp nuxeo-features/nuxeo-platform-audit/nuxeo-platform-audit-client/target/nuxeo-platform-audit-client-5.6-SNAPSHOT.jar ~/nuxeo/nuxeo-cap-5.6-SNAPSHOT-tomcat/nxserver/bundles/
+cp addons/nuxeo-diff/target/nuxeo-diff-5.6-SNAPSHOT.jar ~/nuxeo/nuxeo-cap-5.6-SNAPSHOT-tomcat/nxserver/plugins/
+
