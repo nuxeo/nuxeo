ff873fababb1bcf4c3895f349acf06aa54d81b3d
Thierry Delprat <tdelprat@nuxeo.com>
2012-04-06 15:53:25 +0200

    NXP-9180 use new TOC feature from JODConverter

--- src/test/java/org/nuxeo/ecm/platform/template/tests/TestOOoConvert.java
+++ src/test/java/org/nuxeo/ecm/platform/template/tests/TestOOoConvert.java
@@ -12,7 +12,7 @@ public class TestOOoConvert extends BaseConverterTest {
     protected static final String ODT_MT = "application/vnd.oasis.opendocument.text";
 
     @Test
-    public void testOfficeConvertert() throws Exception {
+    public void testOfficeConverter() throws Exception {
         BlobHolder bh = getBlobFromPath("data/testMe.html", "text/html");
 
         ConversionService cs = Framework.getLocalService(ConversionService.class);
@@ -22,17 +22,51 @@ public class TestOOoConvert extends BaseConverterTest {
         assertEquals("any2odt", converterName);
 
         if (cs.isConverterAvailable(converterName).isAvailable()) {
-        
+
             BlobHolder result = cs.convert(converterName, bh, null);
-    
-            result.getBlob().transferTo(new File("/tmp/html.odt"));
-    
+
+            File odtFile = File.createTempFile("htmlfile", "odt");
+
+            result.getBlob().transferTo(odtFile);
+
             bh = getBlobFromPath("data/testMe.md", "text/x-web-markdown");
             assertEquals("any2odt", converterName);
-    
+
+            File odtFile2 = File.createTempFile("mdfile", "odt");
+
             result = cs.convert(converterName, bh, null);
-            result.getBlob().transferTo(new File("/tmp/md.odt"));
+            result.getBlob().transferTo(odtFile2);
+
+            odtFile.delete();
+            odtFile2.delete();
         }
 
     }
+
+    @Test
+    public void testOfficeConverter2() throws Exception {
+
+        BlobHolder bh = getBlobFromPath("data/Spec_ModelNux.odt",
+                "application/vnd.oasis.opendocument.text");
+
+        ConversionService cs = Framework.getLocalService(ConversionService.class);
+
+        String converterName = cs.getConverterName(bh.getBlob().getMimeType(),
+                "application/pdf");
+        assertEquals("any2pdf", converterName);
+
+        if (cs.isConverterAvailable(converterName).isAvailable()) {
+
+            BlobHolder result = cs.convert(converterName, bh, null);
+
+            File pdfFile = File.createTempFile("testfile", "pdf");
+
+            result.getBlob().transferTo(pdfFile);
+
+            pdfFile.delete();
+
+        }
+
+    }
+
 }
