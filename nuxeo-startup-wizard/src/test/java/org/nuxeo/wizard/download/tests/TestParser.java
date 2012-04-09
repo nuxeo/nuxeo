53e69bb4948a225edbdf7984c86fc222590e5dab
Mathieu Guillaume <mg@nuxeo.com>
2012-04-09 16:13:38 +0200

    NXP-9139: fix more typo

--- nuxeo-startup-wizard/src/test/java/org/nuxeo/wizard/download/tests/TestParser.java
+++ nuxeo-startup-wizard/src/test/java/org/nuxeo/wizard/download/tests/TestParser.java
@@ -22,7 +22,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.Test;
-import satic org.junit.Assert.*;
+import static org.junit.Assert.*;
 
 import org.nuxeo.wizard.download.DownloadDescriptorParser;
 import org.nuxeo.wizard.download.DownloadPackage;
