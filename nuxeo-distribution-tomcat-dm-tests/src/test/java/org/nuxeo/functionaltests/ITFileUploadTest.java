ea41f637ae42b8798c835039719d5c38f4dac0b0
Antoine Taillefer <ataillefer@nuxeo.com>
2012-04-09 13:37:55 +0200

    NXP-9186: Fix file upload and refactor to take advantage of AbstractTest generic methods

--- nuxeo-distribution-tomcat-dm-tests/src/test/java/org/nuxeo/functionaltests/ITFileUploadTest.java
+++ nuxeo-distribution-tomcat-dm-tests/src/test/java/org/nuxeo/functionaltests/ITFileUploadTest.java
@@ -18,16 +18,11 @@
  */
 package org.nuxeo.functionaltests;
 
-import static org.junit.Assert.assertEquals;
-
-import java.net.URISyntaxException;
-import java.net.URL;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 import org.nuxeo.functionaltests.pages.DocumentBasePage;
 import org.nuxeo.functionaltests.pages.FileDocumentBasePage;
-import org.nuxeo.functionaltests.pages.forms.FileCreationFormPage;
-import org.nuxeo.functionaltests.pages.forms.WorkspaceFormPage;
 
 /**
  * Test file upload in Nuxeo DM.
@@ -36,63 +31,29 @@ public class ITFileUploadTest extends AbstractTest {
 
     @Test
     public void testFileUpload() throws Exception {
-        // Get the the content tab and go to the existing Workspace root folder
-        DocumentBasePage documentBasePage = login().getContentTab().goToDocument(
-                "Workspaces");
 
-        // create a new workspace in there named workspace1
-        WorkspaceFormPage workspaceCreationFormPage = documentBasePage.getWorkspacesContentTab().getWorkspaceCreatePage();
-        DocumentBasePage workspacePage = workspaceCreationFormPage.createNewWorkspace(
-                "workspace1", "a workspace description");
+        // Login as Administrator
+        DocumentBasePage defaultDomainPage = login();
 
-        // create a new File document by clicking on New and filling file
-        FileCreationFormPage fileFormPage = workspacePage.getContentTab().getDocumentCreatePage(
-                "File", FileCreationFormPage.class);
+        // Init repository with a test Workspace
+        DocumentBasePage testWorkspacePage = initRepository(defaultDomainPage);
 
-        // Fill the form and upload the file
-        // get a file location from resources
-        // String fileToUpload = getFileFromResource("filetoupload.txt");
-        // String fileName =
-        // fileToUpload.substring(fileToUpload.lastIndexOf(File.separator) + 1);
+        // Create a File with an uploaded blob
+        String filePrefix = "NX-Webdriver-test-";
+        FileDocumentBasePage fileDocumentBasePage = createFile(
+                testWorkspacePage, "File title", "File description", true,
+                filePrefix, ".txt", "Webdriver test file content.");
 
-        // TODO: fix file upload when test ran by maven (jar instead of file
-        // issue)
-        // FileDocumentBasePage fileDocumentBasePage =
-        // fileFormPage.createFileDocument(
-        // "file title", "file description", fileToUpload);
-        FileDocumentBasePage fileDocumentBasePage = fileFormPage.createFileDocument(
-                "file title", "file description", null);
+        // Check uploaded file name
+        String uploadedFileName = fileDocumentBasePage.getFileSummaryTab().getMainContentFileText();
+        assertTrue("Wrong uploaded file name '" + uploadedFileName
+                + "', expected it to contain '" + filePrefix + "'",
+                uploadedFileName.contains(filePrefix));
 
-        // String uploadedFileName =
-        // fileDocumentBasePage.getFileSummaryTab().getMainContentFileText();
-        // assertTrue("The uploaded file name " + uploadedFileName
-        // + " didn't match the updated file name",
-        // uploadedFileName.contains(fileName));
+        // Clean up repository
+        cleanRepository(fileDocumentBasePage);
 
-        // cleaning
-        documentBasePage = fileDocumentBasePage.getNavigationSubPage().goToDocument(
-                "Workspaces");
-        documentBasePage = documentBasePage.getContentTab().removeDocument(
-                "workspace1");
-        // disconnect
+        // Logout
         logout();
     }
-
-    protected String getFileFromResource(String filePath)
-            throws URISyntaxException {
-
-        // TODO: fix file upload when coming from a jar
-        // File file = new File(
-        // Thread.currentThread().getContextClassLoader().getResource(
-        // filePath).toURI());
-        // assertTrue(file.exists());
-        //
-        // return file.getAbsolutePath();
-
-        ClassLoader cl = Thread.currentThread().getContextClassLoader();
-        URL fileUrl = cl.getResource(filePath);
-        assertEquals("file", fileUrl.getProtocol());
-        return fileUrl.getPath();
-    }
-
 }
