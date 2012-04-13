16dab0b975a68ae403a9788d33391e1c83849d4a
Mariana Cedica <mcedica@nuxeo.com>
2012-04-13 13:13:39 +0200

    NXP-9215: Fix overriding the permissions visibility for a document type, the new visibility for a permission should override the default one on the given document type

--- nuxeo-core/src/test/java/org/nuxeo/ecm/core/security/TestSecurityService.java
+++ nuxeo-core/src/test/java/org/nuxeo/ecm/core/security/TestSecurityService.java
@@ -135,8 +135,9 @@ public class TestSecurityService extends NXRuntimeTestCase {
         // test how previous permissions where affected by the override
         String[] groups = pp.getPermissionGroups("Read");
         assertNotNull(groups);
-        assertEquals(Arrays.asList("CustomCompoundPerm", "ReadRemove",
-                "ReadWrite"), Arrays.asList(groups));
+        assertEquals(
+                Arrays.asList("CustomCompoundPerm", "ReadRemove", "ReadWrite"),
+                Arrays.asList(groups));
 
         groups = pp.getPermissionGroups("ReadProperties");
         assertNotNull(groups);
@@ -232,6 +233,34 @@ public class TestSecurityService extends NXRuntimeTestCase {
         assertEquals(Arrays.asList("Write", "Read", "ReadRemove", "Version",
                 "Everything"), permStrings(orderedVisiblePermissions));
 
+        // Workspace falls back to default thus is overridden too
+        orderedVisiblePermissions = pp.getUserVisiblePermissionDescriptors("Workspace");
+        assertNotNull(orderedVisiblePermissions);
+
+        assertEquals(Arrays.asList("Write", "Read", "ReadRemove", "Version",
+                "Everything"), permStrings(orderedVisiblePermissions));
+    }
+
+    @Test
+    public void testOverridedVisiblePermission3() throws Exception {
+        deployContrib(CoreUTConstants.CORE_TESTS_BUNDLE,
+                "permissions-override3-contrib.xml");
+
+        PermissionProvider pp = service.getPermissionProvider();
+        List<UserVisiblePermission> orderedVisiblePermissions = pp.getUserVisiblePermissionDescriptors();
+        assertNotNull(orderedVisiblePermissions);
+
+        assertEquals(Arrays.asList("Write", "Read", "ReadRemove", "Version",
+                "Everything"), permStrings(orderedVisiblePermissions));
+
+        // custom settings for the Section type
+        orderedVisiblePermissions = pp.getUserVisiblePermissionDescriptors("Section");
+        assertNotNull(orderedVisiblePermissions);
+
+        assertEquals(
+                Arrays.asList("Write", "ReadRemove", "Everything", "Read"),
+                permStrings(orderedVisiblePermissions));
+        
         // Workspace falls back to default thus is overridden too
         orderedVisiblePermissions = pp.getUserVisiblePermissionDescriptors("Workspace");
         assertNotNull(orderedVisiblePermissions);
