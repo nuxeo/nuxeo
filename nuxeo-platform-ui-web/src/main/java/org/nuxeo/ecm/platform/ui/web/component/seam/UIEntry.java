054d2817fa16b85c53c63e09ea2b8b588977a7ad
Thierry Martins <tmartins@nuxeo.com>
2012-04-16 15:19:06 +0200

    NXP-9202: add missing copyright

--- nuxeo-platform-ui-web/src/main/java/org/nuxeo/ecm/platform/ui/web/component/seam/UIEntry.java
+++ nuxeo-platform-ui-web/src/main/java/org/nuxeo/ecm/platform/ui/web/component/seam/UIEntry.java
@@ -1,3 +1,18 @@
+/*
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser General Public License
+ * (LGPL) version 2.1 which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl.html
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * Contributors:
+ *     Original file from org.jboss.seam.rss.ui.UIEntry in jboss-seam-rss
+ *     Thierry Martins
+ */
 package org.nuxeo.ecm.platform.ui.web.component.seam;
 
 import java.io.IOException;
