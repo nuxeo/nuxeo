25b6dab81ac3233582ff5ba443e2e5a066593c82
Mathieu Guillaume <mg@nuxeo.com>
2012-04-16 16:35:33 +0200

    NXBT-423: Use the nightly-releases repository for the IT chain's artifacts

--- scripts/release.py
+++ scripts/release.py
@@ -349,7 +349,7 @@ class Release(object):
         sources_archive_name = "nuxeo-%s-sources.zip" % version
         self.repo.archive(os.path.join(self.archive_dir, sources_archive_name))
 
-    def prepare(self):
+    def prepare(self, dodeploy):
         """ Prepare the release: build, change versions, tag and package source
         and distributions."""
         cwd = os.getcwd()
@@ -386,7 +386,11 @@ class Release(object):
 
         # Build and package
         self.repo.system_recurse("git checkout release-%s" % self.tag)
-        self.repo.mvn("clean install", skip_tests=True,
+        if dodeploy == True:
+            self.repo.mvn("clean deploy", skip_tests=True,
+                        profiles="release,-qa,nightly")
+        else:
+            self.repo.mvn("clean install", skip_tests=True,
                         profiles="release,-qa")
         self.package_all()
         # TODO NXP-8571 package sources
@@ -468,6 +472,9 @@ maintenance branch is deleted after release)""")
                           dest='interactive', default=False,
                           help="""Not implemented (TODO NXP-8573). Interactive
 mode.""")
+        parser.add_option('-d', '--deploy', action="store",
+                          dest='deploy', default=False,
+                          help="""deploy artifacts to nightly repository""")
         (options, args) = parser.parse_args()
         if len(args) == 1:
             command = args[0]
@@ -501,7 +508,7 @@ mode.""")
         if "command" not in locals():
             raise ExitException(1, "Missing command. See usage with '-h'.")
         elif command == "prepare":
-            release.prepare()
+            release.prepare(options.deploy)
         elif command == "perform":
             release.perform()
         elif command == "package":
