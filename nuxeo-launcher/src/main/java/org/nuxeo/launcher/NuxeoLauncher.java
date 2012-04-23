e0024498739c998770590593037fdd152f08963c
Mathieu Guillaume <mg@nuxeo.com>
2012-04-23 17:09:35 +0200

    NXP-9251: factorize templates

--- nuxeo-launcher/src/main/java/org/nuxeo/launcher/NuxeoLauncher.java
+++ nuxeo-launcher/src/main/java/org/nuxeo/launcher/NuxeoLauncher.java
@@ -56,7 +56,6 @@ import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
 import org.apache.commons.cli.PosixParser;
 import org.apache.commons.cli.UnrecognizedOptionException;
-import org.apache.commons.io.FileUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.commons.logging.impl.SimpleLog;
