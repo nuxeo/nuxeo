74d8e1031475350db3556fedc56cc2dccfc0c1dd
Thierry Delprat <tdelprat@nuxeo.com>
2012-05-02 15:51:17 +0200

    Fix build after change in XDocReport APi

--- nuxeo-template-rendering-xdocreport/src/main/java/org/nuxeo/template/jaxrs/BinaryDataWrapper.java
+++ nuxeo-template-rendering-xdocreport/src/main/java/org/nuxeo/template/jaxrs/BinaryDataWrapper.java
@@ -12,7 +12,9 @@ import fr.opensagres.xdocreport.remoting.resources.domain.BinaryData;
 public class BinaryDataWrapper {
 
     public static BinaryData wrap(Blob blob) throws Exception {
-        return new BinaryData(blob.getStream(), blob.getFilename(),
+        // fall back to dumb byte[] constructor because of change 93648fa3e82b
+        // in XDocReport API
+        return new BinaryData(blob.getByteArray(), blob.getFilename(),
                 blob.getMimeType());
     }
 
