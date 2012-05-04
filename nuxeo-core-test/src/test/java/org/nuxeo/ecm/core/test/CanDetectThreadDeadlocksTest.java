3ef8d82c422fb4679095d69446280e06c565b690
Florent Guillaume <fg@nuxeo.com>
2012-05-04 15:18:32 +0200

    NXP-9203: don't hide skipped tests

--- nuxeo-core-test/src/test/java/org/nuxeo/ecm/core/test/CanDetectThreadDeadlocksTest.java
+++ nuxeo-core-test/src/test/java/org/nuxeo/ecm/core/test/CanDetectThreadDeadlocksTest.java
@@ -1,7 +1,6 @@
 package org.nuxeo.ecm.core.test;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.nuxeo.runtime.test.runner.Features;
@@ -11,8 +10,7 @@ import org.nuxeo.runtime.test.runner.FeaturesRunner;
 @Features(DetectThreadDeadlocksFeature.class)
 public class CanDetectThreadDeadlocksTest {
 
-    protected Log log = LogFactory.getLog(CanDetectThreadDeadlocksTest.class);
-
+    @Ignore("does not stop")
     @Test
     public void runIntoDeadlocks()  {
 
