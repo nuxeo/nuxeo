e444e93b736e806021e1a36934a732162012948d
Julien Carsique <jcarsique@nuxeo.com>
2012-05-09 19:37:30 +0200

    NXP-9268 - use constants

--- nuxeo-launcher-commons/src/main/java/org/nuxeo/launcher/info/DistributionInfo.java
+++ nuxeo-launcher-commons/src/main/java/org/nuxeo/launcher/info/DistributionInfo.java
@@ -21,25 +21,32 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
+
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
+import org.nuxeo.common.Environment;
+
 @XmlAccessorType(XmlAccessType.NONE)
 @XmlRootElement(name = "distribution")
 public class DistributionInfo {
 
-    public DistributionInfo() {}
+    public DistributionInfo() {
+    }
 
     public DistributionInfo(File distFile) throws IOException {
         Properties distProps = new Properties();
         distProps.load(new FileInputStream(distFile));
-        name = distProps.getProperty("org.nuxeo.distribution.name", "unknown");
-        server = distProps.getProperty("org.nuxeo.distribution.server", "unknown");
-        version = distProps.getProperty("org.nuxeo.distribution.version", "unknown");
+        name = distProps.getProperty(Environment.DISTRIBUTION_NAME, "unknown");
+        server = distProps.getProperty("org.nuxeo.distribution.server",
+                "unknown");
+        version = distProps.getProperty(Environment.DISTRIBUTION_VERSION,
+                "unknown");
         date = distProps.getProperty("org.nuxeo.distribution.date", "unknown");
-        packaging = distProps.getProperty("org.nuxeo.distribution.package", "unknown");
+        packaging = distProps.getProperty("org.nuxeo.distribution.package",
+                "unknown");
     }
 
     @XmlElement()
