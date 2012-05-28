31276ecf0264d736e17825b02d6f65c96056d021
Stephane Lacoin <slacoin@nuxeo.com>
2012-05-28 19:43:31 +0200

    added missing environment setup files

---
+++ scripts/gitfunctions.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+#
+# Convenient functions for use on Nuxeo projects version controlled under Git.
+#
+# (C) Copyright 2009-2012 Nuxeo SAS (http://nuxeo.com/) and contributors.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the GNU Lesser General Public License
+# (LGPL) version 2.1 which accompanies this distribution, and is available at
+# http://www.gnu.org/licenses/lgpl.html
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# Lesser General Public License for more details.
+#
+# Contributors:
+#   Julien Carsique
+#
+
+# usage: "gitf [git instructions]"
+gitf() {
+  for dir in . nuxeo-*; do
+    if [ -d "$dir"/.git ]; then
+      echo "[$dir]"
+      (cd "$dir" ; git "$@")
+      echo
+    fi
+  done
+}
+
+gitfa() {
+  if [ -d "addons" ]; then
+    from_root=true
+    gitf "$@"
+    cd addons
+  fi
+  ADDONS=$(mvn help:effective-pom -N|grep '<module>' |cut -d ">" -f 2 |cut -d "<" -f 1)
+  for dir in . $ADDONS; do
+    if [ -d "$dir"/.git ]; then
+      echo "[$dir]"
+      (cd "$dir" ; git "$@")
+      echo
+    fi
+  done
+  [ -z $from_root ] || cd ..
+}
