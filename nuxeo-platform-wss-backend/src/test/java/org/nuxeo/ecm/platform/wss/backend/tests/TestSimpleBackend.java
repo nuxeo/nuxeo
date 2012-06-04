a9a956621a77769a5ca46ce397865c44a3516074
Stephane Lacoin <slacoin@nuxeo.com>
2012-06-04 10:09:35 +0200

    NXP-8863 repo init in test

--- nuxeo-platform-wss-backend/src/test/java/org/nuxeo/ecm/platform/wss/backend/tests/TestSimpleBackend.java
+++ nuxeo-platform-wss-backend/src/test/java/org/nuxeo/ecm/platform/wss/backend/tests/TestSimpleBackend.java
@@ -49,6 +49,7 @@ public class TestSimpleBackend extends SQLRepositoryTestCase {
         deployBundle("org.nuxeo.ecm.core.api");
         deployBundle("org.nuxeo.ecm.platform.content.template");
         deployContrib("org.nuxeo.ecm.platform.wss.backend","OSGI-INF/wssbackendmanager-framework.xml");
+        fireFrameworkStarted();
         openSession();
 
         DocumentModel ws1 = session.createDocumentModel(
