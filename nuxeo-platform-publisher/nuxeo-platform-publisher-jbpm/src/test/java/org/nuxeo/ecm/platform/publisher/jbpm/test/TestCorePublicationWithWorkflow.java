9d4bce5fa4899b1ff39433c54c06d42c1ba9be71
Stephane Lacoin <slacoin@nuxeo.com>
2012-06-04 16:43:40 +0200

    NXP-8863 repo init in test

--- nuxeo-platform-publisher/nuxeo-platform-publisher-jbpm/src/test/java/org/nuxeo/ecm/platform/publisher/jbpm/test/TestCorePublicationWithWorkflow.java
+++ nuxeo-platform-publisher/nuxeo-platform-publisher-jbpm/src/test/java/org/nuxeo/ecm/platform/publisher/jbpm/test/TestCorePublicationWithWorkflow.java
@@ -76,6 +76,7 @@ public class TestCorePublicationWithWorkflow extends SQLRepositoryTestCase {
         deployBundle("org.nuxeo.ecm.platform.publisher.jbpm.test");
         deployBundle(JbpmUTConstants.CORE_BUNDLE_NAME);
         deployBundle(JbpmUTConstants.TESTING_BUNDLE_NAME);
+        fireFrameworkStarted();
         openSession();
 
         directoryService = Framework.getService(DirectoryService.class);
