7484ddbbdfa976b5db019c2cc93cce2b02b976cb
Thierry Delprat <tdelprat@nuxeo.com>
2012-06-11 14:15:34 +0200

    NXP-9089 Make FancyBox script more flexible in parameters parsing

--- nuxeo-platform-webapp/src/main/resources/web/nuxeo.war/scripts/fancybox-popup-utils.js
+++ nuxeo-platform-webapp/src/main/resources/web/nuxeo.war/scripts/fancybox-popup-utils.js
@@ -1,9 +1,19 @@
+function paramIsNumber(value) {
+  return ! isNaN (value-0) && value != null;  
+}
 // helper function to show content inside a FancyBox popup
 function showFancyBox(ele, width, height, scrolling) {
   width = width || "90%";
   height = height || "90%";
   scrolling = scrolling || "auto";
 
+  if (paramIsNumber(width)) {
+    width = parseInt(width);
+  }
+  if (paramIsNumber(height)) {
+    height = parseInt(height);
+  }
+  
   var popupType = 'iframe';
   if(ele.indexOf("#") == 0) {
     popupType = 'inline';
