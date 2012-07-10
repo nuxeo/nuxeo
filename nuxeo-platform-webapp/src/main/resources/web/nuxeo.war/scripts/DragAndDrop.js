e3d8494d8d93873e4a42093f8043f4fb8b9e3285
Antoine Taillefer <ataillefer@nuxeo.com>
2012-07-10 20:38:28 +0200

    NXP-9656: Cosmit

--- nuxeo-platform-webapp/src/main/resources/web/nuxeo.war/scripts/DragAndDrop.js
+++ nuxeo-platform-webapp/src/main/resources/web/nuxeo.war/scripts/DragAndDrop.js
@@ -13,7 +13,7 @@ function copyElement(element){
 function copyElementIfIdNotStartsWith(element, idPrefix){
     //Seam.Remoting.contextPath = "/nuxeo";
     if (element && element.id.indexOf(idPrefix) <= -1) {
-    	Seam.Remoting.getContext().setConversationId(currentConversationId);
+        Seam.Remoting.getContext().setConversationId(currentConversationId);
         Seam.Component.getInstance("FileManageActions").copyWithId(element.id,copyCallback);    	
     }
 }
