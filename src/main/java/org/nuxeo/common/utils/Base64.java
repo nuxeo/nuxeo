c27eeeedd016e74b78bf7963ddf38fd0c8e8915a
Julien Carsique <jcarsique@nuxeo.com>
2012-07-13 17:26:36 +0200

    NXP-9676 - avoid Base64 encoded String being chunked; set custom Base64 as deprecated

--- src/main/java/org/nuxeo/common/utils/Base64.java
+++ src/main/java/org/nuxeo/common/utils/Base64.java
@@ -11,16 +11,33 @@
  */
 package org.nuxeo.common.utils;
 
+import java.io.UnsupportedEncodingException;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
 /**
  * Simple wrapper around codec library to preserve backward compatibility
+ *
  * @author bstefanescu
+ * @deprecated Since 5.6. Use {@link org.apache.commons.codec.binary.Base64}
+ *             instead.
  *
  */
+@Deprecated
 public class Base64 {
 
+    private static final Log log = LogFactory.getLog(Base64.class);
+
     public final static String encodeBytes(byte[] source) {
-        return org.apache.commons.codec.binary.Base64.encodeBase64String(source).trim();
+        try {
+            return new String(
+                    org.apache.commons.codec.binary.Base64.encodeBase64(source),
+                    "US-ASCII").trim();
+        } catch (UnsupportedEncodingException e) {
+            log.error(e);
+            return "";
+        }
     }
 
     public final static byte[] decode(String s) {
