6781aa3266d7a725bfbb8bf536fc0e2cac61c440
Thierry Martins <tmartins@nuxeo.com>
2012-07-17 17:38:09 +0200

    NXP-9093: do not add default groups to anonymous users

--- nuxeo-platform-usermanager-core/src/main/java/org/nuxeo/ecm/platform/usermanager/UserManagerImpl.java
+++ nuxeo-platform-usermanager-core/src/main/java/org/nuxeo/ecm/platform/usermanager/UserManagerImpl.java
@@ -476,8 +476,8 @@ public class UserManagerImpl implements UserManager {
 
         // XXX why not set groups to anonymous user?
         List<String> virtualGroups = new LinkedList<String>();
-        // Add preconfigured groups: useful for LDAP
-        if (defaultGroup != null) {
+        // Add preconfigured groups: useful for LDAP, not for anonymous users
+        if (defaultGroup != null && !anonymous) {
             virtualGroups.add(defaultGroup);
         }
         // Add additional groups: useful for virtual users
