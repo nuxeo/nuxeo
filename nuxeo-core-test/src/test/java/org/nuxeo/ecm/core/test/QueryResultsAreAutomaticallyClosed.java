0da718e634dc271e0e434275b6a30c9b34848581
Stephane Lacoin <slacoin@nuxeo.com>
2012-08-21 18:00:53 +0200

    NXP-9662 automatically close and warn iterable query results

---
+++ nuxeo-core-test/src/test/java/org/nuxeo/ecm/core/test/QueryResultsAreAutomaticallyClosed.java
@@ -0,0 +1,73 @@
+package org.nuxeo.ecm.core.test;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.nuxeo.ecm.core.api.ClientException;
+import org.nuxeo.ecm.core.api.CoreSession;
+import org.nuxeo.ecm.core.api.IterableQueryResult;
+import org.nuxeo.ecm.core.api.UnrestrictedSessionRunner;
+import org.nuxeo.ecm.core.test.annotations.RepositoryConfig;
+import org.nuxeo.ecm.core.test.annotations.TransactionalConfig;
+import org.nuxeo.runtime.test.runner.Features;
+import org.nuxeo.runtime.test.runner.FeaturesRunner;
+import org.nuxeo.runtime.transaction.TransactionHelper;
+
+import com.google.inject.Inject;
+
+@RunWith(FeaturesRunner.class)
+@Features({TransactionalFeature.class, CoreFeature.class})
+@TransactionalConfig(autoStart=false)
+@RepositoryConfig(init=DefaultRepositoryInit.class)
+public class QueryResultsAreAutomaticallyClosed {
+
+    
+    @Inject RepositorySettings settings;
+    
+    @Test
+    public void testAutoCommit() throws ClientException {
+        CoreSession session = settings.openSessionAsSystemUser();
+        IterableQueryResult results = session.queryAndFetch(
+                "SELECT * from Document", "NXQL");
+        settings.getRepositoryHandler().releaseSession(session);
+        Assert.assertFalse(results.isLife());
+    }
+    
+    @Test
+    public void testTransactional() throws ClientException {
+        TransactionHelper.startTransaction();
+        CoreSession session = settings.openSessionAsSystemUser();
+        IterableQueryResult results = session.queryAndFetch(
+                "SELECT * from Document", "NXQL");
+        TransactionHelper.commitOrRollbackTransaction();
+        Assert.assertFalse(results.isLife());
+    }
+    
+    protected static class NestedQueryRunner extends UnrestrictedSessionRunner {
+
+        public NestedQueryRunner(String reponame) {
+            super(reponame);
+        }
+        
+        protected IterableQueryResult result;
+        
+        @Override
+        public void run() throws ClientException {
+             result = session.queryAndFetch(
+                    "SELECT * from Document", "NXQL");
+        }
+        
+    }
+    @Test
+    public void testNested() throws ClientException {
+        TransactionHelper.startTransaction();
+        CoreSession main = settings.openSessionAs("toto");
+        IterableQueryResult mainResults = main.queryAndFetch(
+                "SELECT * from Document", "NXQL");
+        IterableQueryResult subResults;
+        NestedQueryRunner runner = new NestedQueryRunner(settings.repositoryName);
+        runner.runUnrestricted();
+        Assert.assertFalse(runner.result.isLife());
+    }
+}
