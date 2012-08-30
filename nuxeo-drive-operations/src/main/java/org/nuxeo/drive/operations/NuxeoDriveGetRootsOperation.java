8c19073c577f88b1d982ec0dc34cc82cdbf6a7de
Olivier Grisel <olivier.grisel@ensta.org>
2012-08-30 15:06:09 +0200

    import cleanup

--- nuxeo-drive-operations/src/main/java/org/nuxeo/drive/operations/NuxeoDriveGetRootsOperation.java
+++ nuxeo-drive-operations/src/main/java/org/nuxeo/drive/operations/NuxeoDriveGetRootsOperation.java
@@ -11,7 +11,6 @@ import org.nuxeo.ecm.automation.core.Constants;
 import org.nuxeo.ecm.automation.core.annotations.Context;
 import org.nuxeo.ecm.automation.core.annotations.Operation;
 import org.nuxeo.ecm.automation.core.annotations.OperationMethod;
-import org.nuxeo.ecm.automation.core.annotations.Param;
 import org.nuxeo.ecm.core.api.ClientException;
 import org.nuxeo.ecm.core.api.CoreSession;
 import org.nuxeo.ecm.core.api.DocumentModelList;
