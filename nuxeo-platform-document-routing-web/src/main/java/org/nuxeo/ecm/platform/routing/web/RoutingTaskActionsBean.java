8175dd3103247644be348661ff83481f31f0f378
mcedica <mcedica@nuxeo.com>
2012-08-30 15:41:55 +0200

    NXP-9989: log task processing events

--- nuxeo-platform-document-routing-web/src/main/java/org/nuxeo/ecm/platform/routing/web/RoutingTaskActionsBean.java
+++ nuxeo-platform-document-routing-web/src/main/java/org/nuxeo/ecm/platform/routing/web/RoutingTaskActionsBean.java
@@ -414,7 +414,11 @@ public class RoutingTaskActionsBean implements Serializable {
             data.put("WorkflowVariables", formVariables);
             data.put("NodeVariables", formVariables);
         }
-
+        //if there is a comment on the submitted form, pass it to be logged by audit
+        if (formVariables.containsKey("comment")) {
+            data.put("comment", formVariables.get("comment"));
+        }
+        
         // get task documents
         boolean hasErrors = false;
         DocumentRoutingService routing = Framework.getLocalService(DocumentRoutingService.class);
