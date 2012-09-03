90e08219e4778330c1bf79efda4bcf3d85b77bbd
Stephane Lacoin <slacoin@nuxeo.com>
2012-09-03 16:33:07 +0200

    NXP-10006 integrated with path segment service

--- nuxeo-importer-core/src/main/java/org/nuxeo/ecm/platform/importer/factories/AbstractDocumentModelFactory.java
+++ nuxeo-importer-core/src/main/java/org/nuxeo/ecm/platform/importer/factories/AbstractDocumentModelFactory.java
@@ -26,8 +26,12 @@ import org.nuxeo.common.utils.IdUtils;
 import org.nuxeo.ecm.core.api.ClientException;
 import org.nuxeo.ecm.core.api.CoreSession;
 import org.nuxeo.ecm.core.api.DocumentModel;
+import org.nuxeo.ecm.core.api.impl.DocumentModelImpl;
+import org.nuxeo.ecm.core.api.model.PropertyException;
 import org.nuxeo.ecm.core.api.model.PropertyNotFoundException;
+import org.nuxeo.ecm.core.api.pathsegment.PathSegmentService;
 import org.nuxeo.ecm.platform.importer.source.SourceNode;
+import org.nuxeo.runtime.api.Framework;
 
 /**
  * Base class for classes implementing {@code ImporterDocumentModelFactory}.
@@ -51,16 +55,48 @@ public abstract class AbstractDocumentModelFactory implements
         return node.isFolderish();
     }
 
+    protected final FilenameNormalizer filenameNormalizer = 
+       "true".equals(Framework.getProperty("nuxeo.importer.compatFilenames"))
+            ? new CompatFilenameNormalizer()
+            : new DefaultFilenameNormalizer();
+     
+    protected interface FilenameNormalizer {
+        String normalize(String name) throws ClientException;
+    }
+    
+    protected static class CompatFilenameNormalizer implements FilenameNormalizer {
+
+        @Override
+        public String normalize(String name) {
+            name = IdUtils.generateId(name, "-", true, 100);
+            name = name.replace("'", "");
+            name = name.replace("(", "");
+            name = name.replace(")", "");
+            name = name.replace("+", "");
+            return name;
+        }
+        
+    }
+    
+    protected static class DefaultFilenameNormalizer implements FilenameNormalizer {
+
+        @Override
+        public String normalize(String name) throws ClientException {
+            DocumentModel fake = new DocumentModelImpl("/", "none", "File");
+            fake.setPropertyValue("dc:title", name);
+            Framework.getLocalService(PathSegmentService.class).generatePathSegment(
+                    fake);
+           return name;
+        }
+    }
+    
     /**
      * Returns a valid Nuxeo name from the given {@code fileName}.
+     * @throws ClientException 
+     * @throws PropertyException 
      */
-    protected String getValidNameFromFileName(String fileName) {
-        String name = IdUtils.generateId(fileName, "-", true, 100);
-        name = name.replace("'", "");
-        name = name.replace("(", "");
-        name = name.replace(")", "");
-        name = name.replace("+", "");
-        return name;
+    protected String getValidNameFromFileName(String fileName) throws ClientException {
+        return filenameNormalizer.normalize(fileName);
     }
 
     /**
