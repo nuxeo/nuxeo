f35f9ea8e811b624c2bd36481772c6716f2ebd83
Antoine Taillefer <ataillefer@nuxeo.com>
2012-10-11 00:42:36 +0200

    NXP-10268: Use specific TokenAuthenticationException

---
+++ nuxeo-platform-login-token/src/main/java/org/nuxeo/ecm/tokenauth/TokenAuthenticationException.java
@@ -0,0 +1,45 @@
+/*
+ * (C) Copyright 2006-20012 Nuxeo SA (http://nuxeo.com/) and contributors.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser General Public License
+ * (LGPL) version 2.1 which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl.html
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * Contributors:
+ *     Antoine Taillefer
+ */
+package org.nuxeo.ecm.tokenauth;
+
+import org.nuxeo.ecm.core.api.ClientException;
+import org.nuxeo.ecm.tokenauth.service.TokenAuthenticationService;
+
+/**
+ * Exception thrown when an error occurs in the
+ * {@link TokenAuthenticationService}.
+ *
+ * @author Antoine Taillefer (ataillefer@nuxeo.com)
+ * @since 5.7
+ */
+public class TokenAuthenticationException extends ClientException {
+
+    private static final long serialVersionUID = 2074869903479308929L;
+
+    public TokenAuthenticationException(String message) {
+        super(message);
+    }
+
+    public TokenAuthenticationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public TokenAuthenticationException(Throwable cause) {
+        super(cause);
+    }
+
+}
