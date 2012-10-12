13a8ffd368a63aceefb95641905d144b388de056
Antoine Taillefer <ataillefer@nuxeo.com>
2012-10-12 19:01:02 +0200

    NXP-10268: Remove useless test helper

--- nuxeo-platform-login-token/src/test/java/org/nuxeo/ecm/tokenauth/TestTokenAuthenticationServlet.java
+++ nuxeo-platform-login-token/src/test/java/org/nuxeo/ecm/tokenauth/TestTokenAuthenticationServlet.java
@@ -19,9 +19,12 @@ package org.nuxeo.ecm.tokenauth;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
+import java.io.IOException;
 import java.net.URI;
 
+import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.HttpException;
 import org.apache.commons.httpclient.HttpMethod;
 import org.apache.commons.httpclient.methods.GetMethod;
 import org.apache.commons.httpclient.util.URIUtil;
@@ -51,26 +54,26 @@ public class TestTokenAuthenticationServlet {
             // ------------ Test bad authentication ----------------
             getMethod = new GetMethod(
                     "http://localhost:18080/authentication/token?userName=joe&applicationName=myFavoriteApp&deviceName=Ubuntu&permission=rw");
-            int status = TokenAuthenticationTestHelper.executeGetMethod(
-                    httpClient, getMethod, "Administrator", "badPassword");
+            int status = executeGetMethod(httpClient, getMethod,
+                    "Administrator", "badPassword");
             // Should receive 401 (?)
             assertEquals(404, status);
 
             // ------------ Test omitting required parameters ----------------
             getMethod = new GetMethod(
                     "http://localhost:18080/authentication/token?userName=joe");
-            status = TokenAuthenticationTestHelper.executeGetMethod(httpClient,
-                    getMethod, "Administrator", "Administrator");
+            status = executeGetMethod(httpClient, getMethod, "Administrator",
+                    "Administrator");
             assertEquals(404, status);
 
             // ------------ Test acquiring token ----------------
-            String queryParams = URIUtil.encodeQuery("userName=joe&applicationName=myFavoriteApp&deviceName=Ubuntu box 64 bits&permission=rw");
+            String queryParams = URIUtil.encodeQuery("userName=joe&applicationName=Nuxeo Drive&deviceName=Ubuntu box 64 bits&permission=rw");
             URI uri = new URI("http", null, "localhost", 18080,
                     "/authentication/token", queryParams, null);
             getMethod = new GetMethod(uri.toString());
             // Acquire new token
-            status = TokenAuthenticationTestHelper.executeGetMethod(httpClient,
-                    getMethod, "Administrator", "Administrator");
+            status = executeGetMethod(httpClient, getMethod, "Administrator",
+                    "Administrator");
             assertEquals(200, status);
             String token = getMethod.getResponseBodyAsString();
             assertNotNull(token);
@@ -83,5 +86,20 @@ public class TestTokenAuthenticationServlet {
         } finally {
             getMethod.releaseConnection();
         }
+    }
+
+    /**
+     * Executes the specified HTTP method on the specified HTTP client with a
+     * basic authentication header given the specified credentials.
+     */
+    protected static final int executeGetMethod(HttpClient httpClient,
+            HttpMethod httpMethod, String userName, String password)
+            throws HttpException, IOException {
+
+        String authString = userName + ":" + password;
+        String basicAuthHeader = "Basic "
+                + new String(Base64.encodeBase64(authString.getBytes()));
+        httpMethod.setRequestHeader("Authorization", basicAuthHeader);
+        return httpClient.executeMethod(httpMethod);
     }
 }
