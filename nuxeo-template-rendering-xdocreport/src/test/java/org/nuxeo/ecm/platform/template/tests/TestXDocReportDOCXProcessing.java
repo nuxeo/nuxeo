185ce24f107906fe84f8ffd5eb4d5eb71361c479
Thierry Delprat <tdelprat@nuxeo.com>
2012-10-18 16:15:22 +0200

    NXP-10339 Better handling of null Picture proterty

--- nuxeo-template-rendering-xdocreport/src/test/java/org/nuxeo/ecm/platform/template/tests/TestXDocReportDOCXProcessing.java
+++ nuxeo-template-rendering-xdocreport/src/test/java/org/nuxeo/ecm/platform/template/tests/TestXDocReportDOCXProcessing.java
@@ -12,6 +12,7 @@ import org.nuxeo.common.utils.FileUtils;
 import org.nuxeo.ecm.core.api.Blob;
 import org.nuxeo.ecm.core.api.DocumentModel;
 import org.nuxeo.ecm.core.api.impl.blob.FileBlob;
+import org.nuxeo.template.api.InputType;
 import org.nuxeo.template.api.TemplateInput;
 import org.nuxeo.template.api.adapters.TemplateBasedDocument;
 import org.nuxeo.template.processors.xdocreport.ZipXmlHelper;
@@ -82,11 +83,85 @@ public class TestXDocReportDOCXProcessing extends SimpleTemplateDocTestCase {
 
     }
 
+    @Test
+    public void testWithMissingPicture2() throws Exception {
+
+        TemplateBasedDocument adapter = setupTestDocs();
+        DocumentModel testDoc = adapter.getAdaptedDoc();
+        assertNotNull(testDoc);
+
+        // remove picture
+        List<Map<String, Serializable>> blobs = new ArrayList<Map<String, Serializable>>();
+        testDoc.setPropertyValue("files:files", (Serializable) null);
+        testDoc = session.saveDocument(testDoc);
+
+        adapter = testDoc.getAdapter(TemplateBasedDocument.class);
+
+        List<TemplateInput> params = getTestParams();
+
+        testDoc = adapter.saveParams(TEMPLATE_NAME, params, true);
+        session.save();
+
+        Blob newBlob = adapter.renderAndStoreAsAttachment(TEMPLATE_NAME, true);
+
+        System.out.println(((FileBlob) newBlob).getFile().getAbsolutePath());
+
+        String xmlContent = ZipXmlHelper.readXMLContent(newBlob,
+                ZipXmlHelper.DOCX_MAIN_FILE);
+
+        // System.out.println(xmlContent);
+
+        assertTrue(xmlContent.contains("John Smith"));
+        assertTrue(xmlContent.contains("some description"));
+        assertTrue(xmlContent.contains("The Boolean value is false"));
+        assertTrue(xmlContent.contains("r:embed=\"xdocreport_0\""));
+
+    }
+
+    @Test
+    public void testWithMissingPicture3() throws Exception {
+
+        TemplateBasedDocument adapter = setupTestDocs();
+        DocumentModel testDoc = adapter.getAdaptedDoc();
+        assertNotNull(testDoc);
+
+        adapter = testDoc.getAdapter(TemplateBasedDocument.class);
+
+        List<TemplateInput> params = getTestParams();
+
+        // set wrong input
+        TemplateInput input5 = new TemplateInput("picture");
+        input5.setType(InputType.PictureProperty);
+        input5.setSource("dc:source");
+        params.remove(4);
+        params.add(input5);
+
+        testDoc = adapter.saveParams(TEMPLATE_NAME, params, true);
+        session.save();
+
+        adapter = testDoc.getAdapter(TemplateBasedDocument.class);
+
+        Blob newBlob = adapter.renderAndStoreAsAttachment(TEMPLATE_NAME, true);
+
+        System.out.println(((FileBlob) newBlob).getFile().getAbsolutePath());
+
+        String xmlContent = ZipXmlHelper.readXMLContent(newBlob,
+                ZipXmlHelper.DOCX_MAIN_FILE);
+
+        // System.out.println(xmlContent);
+
+        assertTrue(xmlContent.contains("John Smith"));
+        assertTrue(xmlContent.contains("some description"));
+        assertTrue(xmlContent.contains("The Boolean value is false"));
+        assertTrue(xmlContent.contains("r:embed=\"xdocreport_0\""));
+
+    }
+
     @Override
     protected Blob getTemplateBlob() {
         File file = FileUtils.getResourceFileFromContext("data/testDoc.docx");
         Blob fileBlob = new FileBlob(file);
-        fileBlob.setFilename("testDoc.odt");
+        fileBlob.setFilename("testDoc.docx");
         return fileBlob;
     }
 
