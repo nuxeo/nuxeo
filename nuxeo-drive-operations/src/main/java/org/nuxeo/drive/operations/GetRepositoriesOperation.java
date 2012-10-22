a61fc2ddb40a5b2219a76851c25992306e1662f3
Antoine Taillefer <ataillefer@nuxeo.com>
2012-10-22 14:17:32 +0200

    Fix bad Arrays import

--- nuxeo-drive-operations/src/main/java/org/nuxeo/drive/operations/GetRepositoriesOperation.java
+++ nuxeo-drive-operations/src/main/java/org/nuxeo/drive/operations/GetRepositoriesOperation.java
@@ -1,10 +1,11 @@
 /**
- * 
+ *
  */
 
 package org.nuxeo.drive.operations;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import org.nuxeo.ecm.automation.core.Constants;
@@ -15,11 +16,9 @@ import org.nuxeo.ecm.core.api.ClientException;
 import org.nuxeo.ecm.core.api.repository.Repository;
 import org.nuxeo.ecm.core.api.repository.RepositoryManager;
 
-import edu.emory.mathcs.backport.java.util.Collections;
-
 /**
  * Fetch the list of the repositories registered on the server.
- * 
+ *
  * TODO: move this to the list of default operations in 5.7.
  */
 @Operation(id = GetRepositoriesOperation.ID, category = Constants.CAT_FETCH, label = "List repository names on the server")
