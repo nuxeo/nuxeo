ea50405ab0e1f38ea27e86b7696bcec41103a545
Anahide Tchertchian <at@nuxeo.com>
2012-10-24 11:39:59 +0200

    NXP-5578: format tiny mce init script

--- nuxeo-platform-ui-web/src/main/resources/tiny_mce/tiny_mce_init.js
+++ nuxeo-platform-ui-web/src/main/resources/tiny_mce/tiny_mce_init.js
@@ -1,33 +1,35 @@
 //Language configuration
 if(lang!= 'en' && lang !='fr'){
-	lang = 'en';
+  lang = 'en';
 }
+
 tinyMCE.init({
-		width : width,
-        height : height,
-        mode : "specific_textareas",
-        theme : "advanced",
-		editor_selector : editorSelector,
-		editor_deselector : "disableMCEInit",
-        plugins : plugins,
-		language : lang,
-		theme_advanced_resizing : true,
-		
-		//Img insertion fixes
-		relative_urls : false,
-		remove_script_host : false,
-        skin : "o2k7",
-        skin_variant : "silver",
-    theme_advanced_disable : "styleselect",
-		theme_advanced_buttons3 : "hr,removeformat,visualaid,|,sub,sup,|,charmap,|",
-		theme_advanced_buttons3_add : toolbar
+  width : width,
+  height : height,
+  mode : "specific_textareas",
+  theme : "advanced",
+  editor_selector : editorSelector,
+  editor_deselector : "disableMCEInit",
+  plugins : plugins,
+  language : lang,
+  theme_advanced_resizing : true,
+
+  //Img insertion fixes
+  relative_urls : false,
+  remove_script_host : false,
+  skin : "o2k7",
+  skin_variant : "silver",
+  theme_advanced_disable : "styleselect",
+  theme_advanced_buttons3 : "hr,removeformat,visualaid,|,sub,sup,|,charmap,|",
+  theme_advanced_buttons3_add : toolbar
 });
 
 function toggleTinyMCE(id) {
-      if (!tinyMCE.getInstanceById(id))
-        addTinyMCE(id);
-       else
-        removeTinyMCE(id);
+  if (!tinyMCE.getInstanceById(id)) {
+    addTinyMCE(id);
+  } else {
+    removeTinyMCE(id);
+  }
 }
 
 function removeTinyMCE(id) {
