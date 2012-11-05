ba448a4b18939e9882920bff9ca31ed3dd653d73
Thomas Roger <troger@nuxeo.com>
2012-11-05 23:54:56 +0100

    Add copyright

--- src/main/java/org/nuxeo/ecm/csv/CSVImportId.java
+++ src/main/java/org/nuxeo/ecm/csv/CSVImportId.java
@@ -1,3 +1,20 @@
+/*
+ * (C) Copyright 2012 Nuxeo SA (http://nuxeo.com/) and contributors.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser General Public License
+ * (LGPL) version 2.1 which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl.html
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * Contributors:
+ *     Thomas Roger
+ */
+
 package org.nuxeo.ecm.csv;
 
 import java.security.DigestInputStream;
