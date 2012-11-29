cb42dbcda84dc6e816d05e5c6817bb7f4713491c
Olivier Grisel <olivier.grisel@ensta.org>
2012-11-29 15:02:48 +0100

    NXP-10567: fix $ sign crash in suggestbox search

--- nuxeo-platform-suggestbox-core/src/main/java/org/nuxeo/ecm/platform/suggestbox/service/suggesters/I18nHelper.java
+++ nuxeo-platform-suggestbox-core/src/main/java/org/nuxeo/ecm/platform/suggestbox/service/suggesters/I18nHelper.java
@@ -32,11 +32,9 @@ public class I18nHelper {
         while (matcher.find()) {
             int paramId = Integer.valueOf(matcher.group(1));
             if (paramId >= 0 && paramId < params.length) {
-                // ugly hack to workaround the fact that for some reason
-                // backslash chars make the appendReplacement crash...
-                String replacement = params[paramId].toString().replaceAll(
-                        "\\\\", "\\\\\\\\");
-                matcher.appendReplacement(sb, replacement);
+                String replacement = params[paramId].toString();
+                matcher.appendReplacement(sb,
+                        Matcher.quoteReplacement(replacement));
             } else {
                 throw new IllegalArgumentException(String.format(
                         "Invalid placeholder %d in message '%s': %d "
