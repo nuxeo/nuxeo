ab61e8cca215727039bccb9298b765ed6af53cd1
Antoine Taillefer <ataillefer@nuxeo.com>
2012-12-01 15:14:30 +0100

    NXP-10039: Deploy DublinCoreListener in test to have dc:created and dc:creator set

--- nuxeo-drive-core/src/test/java/org/nuxeo/drive/service/adapter/TestDefaultFileSystemItemFactory.java
+++ nuxeo-drive-core/src/test/java/org/nuxeo/drive/service/adapter/TestDefaultFileSystemItemFactory.java
@@ -59,7 +59,8 @@ import com.google.inject.Inject;
 @RunWith(FeaturesRunner.class)
 @Features(CoreFeature.class)
 @RepositoryConfig(cleanup = Granularity.METHOD)
-@Deploy({ "org.nuxeo.drive.core", "org.nuxeo.ecm.platform.filemanager.core",
+@Deploy({ "org.nuxeo.drive.core", "org.nuxeo.ecm.platform.dublincore",
+        "org.nuxeo.ecm.platform.filemanager.core",
         "org.nuxeo.ecm.platform.types.core",
         "org.nuxeo.ecm.webapp.base:OSGI-INF/ecm-types-contrib.xml" })
 @LocalDeploy("org.nuxeo.drive.core:OSGI-INF/test-nuxeodrive-types-contrib.xml")
@@ -88,7 +89,6 @@ public class TestDefaultFileSystemItemFactory {
 
         // File
         file = session.createDocumentModel("/", "aFile", "File");
-        file.setPropertyValue("dc:creator", "Joe");
         Blob blob = new StringBlob("Content of Joe's file.");
         blob.setFilename("Joe.odt");
         file.setPropertyValue("file:content", (Serializable) blob);
@@ -96,13 +96,11 @@ public class TestDefaultFileSystemItemFactory {
 
         // Note
         note = session.createDocumentModel("/", "aNote", "Note");
-        note.setPropertyValue("dc:creator", "Bob");
         note.setPropertyValue("note:note", "Content of Bob's note.");
         note = session.createDocument(note);
 
         // Custom doc type with the "file" schema
         custom = session.createDocumentModel("/", "aCustomDoc", "Custom");
-        custom.setPropertyValue("dc:creator", "Bonnie");
         blob = new StringBlob("Content of Bonnie's file.");
         blob.setFilename("Bonnie's file.odt");
         custom.setPropertyValue("file:content", (Serializable) blob);
@@ -111,7 +109,6 @@ public class TestDefaultFileSystemItemFactory {
         // Folder
         folder = session.createDocumentModel("/", "aFolder", "Folder");
         folder.setPropertyValue("dc:title", "Jack's folder");
-        folder.setPropertyValue("dc:creator", "Jack");
         folder = session.createDocument(folder);
 
         // FolderishFile: doc type with the "file" schema and the "Folderish"
@@ -119,7 +116,6 @@ public class TestDefaultFileSystemItemFactory {
         folderishFile = session.createDocumentModel("/", "aFolderishFile",
                 "FolderishFile");
         folderishFile.setPropertyValue("dc:title", "Sarah's folderish file");
-        folderishFile.setPropertyValue("dc:creator", "Sarah");
         folderishFile = session.createDocument(folderishFile);
 
         // Doc not adaptable as a FileSystemItem (not Folderish nor a
@@ -144,7 +140,7 @@ public class TestDefaultFileSystemItemFactory {
         assertEquals("test/" + file.getId(), fsItem.getId());
         assertEquals("Joe.odt", fsItem.getName());
         assertFalse(fsItem.isFolder());
-        assertEquals("Joe", fsItem.getCreator());
+        assertEquals("Administrator", fsItem.getCreator());
         Blob fileItemBlob = ((FileItem) fsItem).getBlob();
         assertEquals("Joe.odt", fileItemBlob.getFilename());
         assertEquals("Content of Joe's file.", fileItemBlob.getString());
@@ -156,7 +152,7 @@ public class TestDefaultFileSystemItemFactory {
         assertEquals("test/" + note.getId(), fsItem.getId());
         assertEquals("aNote.txt", fsItem.getName());
         assertFalse(fsItem.isFolder());
-        assertEquals("Bob", fsItem.getCreator());
+        assertEquals("Administrator", fsItem.getCreator());
         fileItemBlob = ((FileItem) fsItem).getBlob();
         assertEquals("aNote.txt", fileItemBlob.getFilename());
         assertEquals("Content of Bob's note.", fileItemBlob.getString());
@@ -168,7 +164,7 @@ public class TestDefaultFileSystemItemFactory {
         assertEquals("test/" + custom.getId(), fsItem.getId());
         assertEquals("Bonnie's file.odt", fsItem.getName());
         assertFalse(fsItem.isFolder());
-        assertEquals("Bonnie", fsItem.getCreator());
+        assertEquals("Administrator", fsItem.getCreator());
         fileItemBlob = ((FileItem) fsItem).getBlob();
         assertEquals("Bonnie's file.odt", fileItemBlob.getFilename());
         assertEquals("Content of Bonnie's file.", fileItemBlob.getString());
@@ -189,7 +185,7 @@ public class TestDefaultFileSystemItemFactory {
         assertEquals("test/" + folder.getId(), fsItem.getId());
         assertEquals("Jack's folder", fsItem.getName());
         assertTrue(fsItem.isFolder());
-        assertEquals("Jack", fsItem.getCreator());
+        assertEquals("Administrator", fsItem.getCreator());
         List<FileSystemItem> children = ((FolderItem) fsItem).getChildren();
         assertNotNull(children);
         assertEquals(0, children.size());
@@ -202,7 +198,7 @@ public class TestDefaultFileSystemItemFactory {
         assertEquals("test/" + folderishFile.getId(), fsItem.getId());
         assertEquals("Sarah's folderish file", fsItem.getName());
         assertTrue(fsItem.isFolder());
-        assertEquals("Sarah", fsItem.getCreator());
+        assertEquals("Administrator", fsItem.getCreator());
 
         // ------------------------------------------------------
         // Check not adaptable as a FileSystemItem
