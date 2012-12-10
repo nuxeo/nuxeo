4ddd60d46540cf83db041a18e91bdc4675cc06f4
Thierry Delprat <tdelprat@nuxeo.com>
2012-12-10 19:21:50 +0100

    NXP-10591 Automatically remove spaces on group names

--- nuxeo-core-api/src/main/java/org/nuxeo/ecm/core/api/impl/NuxeoGroupImpl.java
+++ nuxeo-core-api/src/main/java/org/nuxeo/ecm/core/api/impl/NuxeoGroupImpl.java
@@ -40,8 +40,8 @@ public class NuxeoGroupImpl implements NuxeoGroup {
         if (name == null) {
             throw new IllegalArgumentException("group name cannot be null");
         }
-        this.name = name;
-        label = name;
+        this.name = name.trim();
+        label = name.trim();
         users = new ArrayList<String>();
         groups = new ArrayList<String>();
         parentGroups = new ArrayList<String>();
@@ -70,7 +70,8 @@ public class NuxeoGroupImpl implements NuxeoGroup {
     @Override
     public void setMemberUsers(List<String> users) {
         if (users == null) {
-            throw new IllegalArgumentException("member users list cannot be null");
+            throw new IllegalArgumentException(
+                    "member users list cannot be null");
         }
         this.users.clear();
         this.users.addAll(users);
@@ -79,7 +80,8 @@ public class NuxeoGroupImpl implements NuxeoGroup {
     @Override
     public void setMemberGroups(List<String> groups) {
         if (groups == null) {
-            throw new IllegalArgumentException("member groups list cannot be null");
+            throw new IllegalArgumentException(
+                    "member groups list cannot be null");
         }
         this.groups.clear();
         this.groups.addAll(groups);
@@ -88,7 +90,8 @@ public class NuxeoGroupImpl implements NuxeoGroup {
     @Override
     public void setParentGroups(List<String> groups) {
         if (groups == null) {
-            throw new IllegalArgumentException("parent groups list cannot be null");
+            throw new IllegalArgumentException(
+                    "parent groups list cannot be null");
         }
         parentGroups.clear();
         parentGroups.addAll(groups);
