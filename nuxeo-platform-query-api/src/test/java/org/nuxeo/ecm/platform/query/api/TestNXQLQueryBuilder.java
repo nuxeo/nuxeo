716b5c8096f8ca0226128d008bfe26833498f88d
Florent Guillaume <fg@nuxeo.com>
2012-12-26 13:15:16 +0100

    cleanup

--- nuxeo-platform-query-api/src/test/java/org/nuxeo/ecm/platform/query/api/TestNXQLQueryBuilder.java
+++ nuxeo-platform-query-api/src/test/java/org/nuxeo/ecm/platform/query/api/TestNXQLQueryBuilder.java
@@ -1,3 +1,21 @@
+/*
+ * (C) Copyright 2011-2012 Nuxeo SA (http://nuxeo.com/) and contributors.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser General Public License
+ * (LGPL) version 2.1 which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl-2.1.html
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * Contributors:
+ *     Thierry Martins
+ *     Florent Guillaume
+ *     Marwane Kalam-Alami
+ */
 package org.nuxeo.ecm.platform.query.api;
 
 import static org.junit.Assert.assertEquals;
@@ -14,16 +32,10 @@ import org.nuxeo.runtime.api.Framework;
 
 public class TestNXQLQueryBuilder extends SQLRepositoryTestCase {
 
-    String query;
-
+    @Override
     @Before
     public void setUp() throws Exception {
         super.setUp();
-    }
-
-    @Override
-    protected void deployRepositoryContrib() throws Exception {
-        super.deployRepositoryContrib();
         deployContrib("org.nuxeo.ecm.platform.query.api",
                 "OSGI-INF/pageprovider-framework.xml");
         deployContrib("org.nuxeo.ecm.platform.query.api.test",
@@ -44,19 +56,19 @@ public class TestNXQLQueryBuilder extends SQLRepositoryTestCase {
                 "AdvancedSearch");
         model.setPropertyValue("search:title", "bar");
 
-        query = NXQLQueryBuilder.getQuery(model, whereClause, params, sortInfos);
+        String query = NXQLQueryBuilder.getQuery(model, whereClause, params, sortInfos);
         assertEquals(
                 "SELECT * FROM Document WHERE dc:title LIKE 'bar' AND (ecm:parentId = 'foo') ORDER BY dc:title",
                 query);
 
-        model.setPropertyValue("search:isPresent", true);
+        model.setPropertyValue("search:isPresent", Boolean.TRUE);
         query = NXQLQueryBuilder.getQuery(model, whereClause, params, sortInfos);
         assertEquals(
                 "SELECT * FROM Document WHERE dc:title LIKE 'bar' AND dc:modified IS NULL AND (ecm:parentId = 'foo') ORDER BY dc:title",
                 query);
 
         // only boolean available in schema without default value
-        model.setPropertyValue("search:isPresent", false);
+        model.setPropertyValue("search:isPresent", Boolean.FALSE);
         query = NXQLQueryBuilder.getQuery(model, whereClause, params, sortInfos);
         assertEquals(
                 "SELECT * FROM Document WHERE dc:title LIKE 'bar' AND dc:modified IS NOT NULL AND (ecm:parentId = 'foo') ORDER BY dc:title",
@@ -74,7 +86,7 @@ public class TestNXQLQueryBuilder extends SQLRepositoryTestCase {
                 "TEST_IN").getWhereClause();
         DocumentModel model = new DocumentModelImpl("/", "doc", "File");
         model.setPropertyValue("dc:subjects", new String[] { "foo", "bar" });
-        query = NXQLQueryBuilder.getQuery(model, whereClause, null);
+        String query = NXQLQueryBuilder.getQuery(model, whereClause, null);
         assertEquals("SELECT * FROM Document WHERE dc:title IN ('foo', 'bar')",
                 query);
 
