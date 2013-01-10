6c38f0f7f76338c7638ef5347f32c06349836b75
Florent Guillaume <fg@nuxeo.com>
2013-01-10 12:59:42 +0100

    NXP-10701: refactored filemanager plugin implementation

--- nuxeo-platform-video-core/src/main/java/org/nuxeo/ecm/platform/video/importer/VideoImporter.java
+++ nuxeo-platform-video-core/src/main/java/org/nuxeo/ecm/platform/video/importer/VideoImporter.java
@@ -1,10 +1,10 @@
 /*
- * (C) Copyright 2009 Nuxeo SA (http://nuxeo.com/) and contributors.
+ * (C) Copyright 2009-2013 Nuxeo SA (http://nuxeo.com/) and contributors.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the GNU Lesser General Public License
  * (LGPL) version 2.1 which accompanies this distribution, and is available at
- * http://www.gnu.org/licenses/lgpl.html
+ * http://www.gnu.org/licenses/lgpl-2.1.html
  *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -13,22 +13,14 @@
  *
  * Contributors:
  *     Peter Di Lorenzo
+ *     Florent Guillaume
  */
-
 package org.nuxeo.ecm.platform.video.importer;
 
-import java.io.IOException;
-import java.io.Serializable;
-
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.nuxeo.ecm.core.api.Blob;
 import org.nuxeo.ecm.core.api.ClientException;
-import org.nuxeo.ecm.core.api.CoreSession;
 import org.nuxeo.ecm.core.api.DocumentModel;
-import org.nuxeo.ecm.core.api.pathsegment.PathSegmentService;
 import org.nuxeo.ecm.platform.filemanager.service.extension.AbstractFileImporter;
-import org.nuxeo.ecm.platform.filemanager.utils.FileManagerUtils;
 import org.nuxeo.ecm.platform.types.Type;
 import org.nuxeo.ecm.platform.types.TypeManager;
 import org.nuxeo.ecm.platform.video.VideoConstants;
@@ -48,54 +40,26 @@ public class VideoImporter extends AbstractFileImporter {
 
     private static final long serialVersionUID = 1L;
 
-    @SuppressWarnings("unused")
-    private static final Log log = LogFactory.getLog(VideoImporter.class);
+    @Override
+    public String getDefaultDocType() {
+        return VideoConstants.VIDEO_TYPE;
+    }
 
     @Override
-    public DocumentModel create(CoreSession documentManager, Blob content,
-            String path, boolean overwrite, String fullname,
-            TypeManager typeService) throws ClientException, IOException {
+    public boolean isOverwriteByTitle() {
+        return true;
+    }
 
-        String filename = FileManagerUtils.fetchFileName(fullname);
-
-        String title = FileManagerUtils.fetchTitle(filename);
-
-        // Check to see if an existing Document with the same title exists.
-        DocumentModel docModel = FileManagerUtils.getExistingDocByTitle(
-                documentManager, path, title);
-
-        // if overwrite flag is true and the file already exists, overwrite it
-        if (overwrite && (docModel != null)) {
-            docModel.setPropertyValue("file:content", (Serializable) content);
-            docModel.setPropertyValue("file:filename", filename);
-        } else {
-            PathSegmentService pss;
-            try {
-                pss = Framework.getService(PathSegmentService.class);
-            } catch (Exception e) {
-                throw new ClientException(e);
-            }
-
-            String docType = getDocType();
-            if (docType == null) {
-                docType = VideoConstants.VIDEO_TYPE;
-            }
-
-            docModel = documentManager.createDocumentModel(docType);
-            docModel.setProperty("dublincore", "title", title);
-            docModel.setPropertyValue("file:content", (Serializable) content);
-            docModel.setPropertyValue("file:filename", filename);
-
-            // updating icon
-            Type type = typeService.getType(docType);
-            if (type != null) {
-                String iconPath = type.getIcon();
-                docModel.setProperty("common", "icon", iconPath);
-            }
-            docModel.setPathInfo(path, pss.generatePathSegment(docModel));
-            docModel = documentManager.createDocument(docModel);
+    @Override
+    public void updateDocument(DocumentModel doc, Blob content)
+            throws ClientException {
+        super.updateDocument(doc, content);
+        // update the icon
+        Type type = Framework.getLocalService(TypeManager.class).getType(
+                doc.getType());
+        if (type != null) {
+            doc.setProperty("common", "icon", type.getIcon());
         }
-        return docModel;
     }
 
 }
