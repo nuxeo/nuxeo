2240a122a9d873bdb5ef7dc10dd72bd3e6b5a9cf
Thierry Delprat <tdelprat@nuxeo.com>
2013-01-14 11:29:15 +0100

    NXP-10787 Avoid breaking Automation Rest interface in case of bad Automation Chain definition

--- nuxeo-automation/nuxeo-automation-server/src/main/java/org/nuxeo/ecm/automation/server/jaxrs/AutomationInfo.java
+++ nuxeo-automation/nuxeo-automation-server/src/main/java/org/nuxeo/ecm/automation/server/jaxrs/AutomationInfo.java
@@ -17,6 +17,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.nuxeo.ecm.automation.AutomationService;
 import org.nuxeo.ecm.automation.OperationChain;
 import org.nuxeo.ecm.automation.OperationDocumentation;
@@ -26,6 +28,8 @@ import org.nuxeo.ecm.automation.OperationParameters;
  * @author <a href="mailto:bs@nuxeo.com">Bogdan Stefanescu</a>
  */
 public class AutomationInfo {
+
+    protected static final Log log = LogFactory.getLog(AutomationInfo.class);
 
     protected final List<OperationDocumentation> ops;
 
@@ -40,39 +44,59 @@ public class AutomationInfo {
         }
         chains = new ArrayList<OperationDocumentation>();
         for (OperationChain chain : service.getOperationChains()) {
-            OperationDocumentation doc = new OperationDocumentation(
-                    chain.getId());
-            doc.description = chain.getDescription();
-            doc.category = "Chain";
-            doc.label = doc.id;
-            doc.params = Collections.emptyList();
-            // compute chain signature
-            List<OperationParameters> ops = chain.getOperations();
-            if (ops.isEmpty()) {
-                doc.signature = new String[] { "void", "void" };
-            } else if (ops.size() == 1) {
-                OperationDocumentation opdoc = map.get(ops.get(0).id());
-                doc.signature = opdoc.signature;
-            } else {
-                ArrayList<String[]> sigs = new ArrayList<String[]>();
-                for (OperationParameters o : ops) {
-                    sigs.add(map.get(o.id()).signature);
-                }
-                String[] head = sigs.get(0);
-                ArrayList<String> rs = new ArrayList<String>();
-                for (int i = 0; i < head.length; i += 2) {
-                    String in = head[i];
-                    String out = head[i + 1];
-                    List<String> result = new ArrayList<String>();
-                    checkPath(out, sigs, 1, result);
-                    for (String r : result) {
-                        rs.add(in);
-                        rs.add(r);
+            try {
+                OperationDocumentation doc = new OperationDocumentation(
+                        chain.getId());
+                doc.description = chain.getDescription();
+                doc.category = "Chain";
+                doc.label = doc.id;
+                doc.params = Collections.emptyList();
+                // compute chain signature
+                List<OperationParameters> ops = chain.getOperations();
+                if (ops.isEmpty()) {
+                    doc.signature = new String[] { "void", "void" };
+                } else if (ops.size() == 1) {
+                    OperationDocumentation opdoc = map.get(ops.get(0).id());
+                    if (opdoc == null) {
+                        log.error("Unable to find Operation " + ops.get(0).id()
+                                + " that is used by chain " + chain.getId()
+                                + ", chain will be discarded");
+                        continue;
                     }
+                    doc.signature = opdoc.signature;
+                } else {
+                    ArrayList<String[]> sigs = new ArrayList<String[]>();
+                    for (OperationParameters o : ops) {
+                        OperationDocumentation opdoc = map.get(o.id());
+                        if (opdoc == null) {
+                            log.error("Unable to find Operation " + o.id()
+                                    + " that is used by chain " + chain.getId()
+                                    + ", chain will be discarded");
+                            continue;
+                        }
+                        sigs.add(opdoc.signature);
+                    }
+                    String[] head = sigs.get(0);
+                    ArrayList<String> rs = new ArrayList<String>();
+                    for (int i = 0; i < head.length; i += 2) {
+                        String in = head[i];
+                        String out = head[i + 1];
+                        List<String> result = new ArrayList<String>();
+                        checkPath(out, sigs, 1, result);
+                        for (String r : result) {
+                            rs.add(in);
+                            rs.add(r);
+                        }
+                    }
+                    doc.signature = rs.toArray(new String[rs.size()]);
                 }
-                doc.signature = rs.toArray(new String[rs.size()]);
+                chains.add(doc);
+
+            } catch (Throwable e) {
+                log.error(
+                        "Unable to build definition of Chain " + chain.getId(),
+                        e);
             }
-            chains.add(doc);
         }
     }
 
