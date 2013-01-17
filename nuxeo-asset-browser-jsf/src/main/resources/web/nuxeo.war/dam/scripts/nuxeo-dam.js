5e1ee5121a035f0fea85808fee3065af3e8443bb
Thomas Roger <throger@gmail.com>
2013-01-17 19:15:07 +0100

    NXP-10806 NXP-10817: add box layout listing + various fixes on the AJAX selection in default listing layout / box listing

---
+++ nuxeo-asset-browser-jsf/src/main/resources/web/nuxeo.war/dam/scripts/nuxeo-dam.js
@@ -0,0 +1,17 @@
+var nuxeo = nuxeo || {}
+
+nuxeo.dam = (function(m) {
+
+  m.selectDocument = function(event, docRef) {
+    if (event && event.srcElement) {
+      var ele = jQuery(event.srcElement)
+      if (ele.is("input") && ele.attr("type").match(/checkbox/i)) {
+        return
+      }
+    }
+    // trigger the a4j:jsFunction actually selecting the document
+    //noinspection JSUnresolvedFunction
+    selectDocument(docRef)
+  }
+
+}(nuxeo.dam || {}))
