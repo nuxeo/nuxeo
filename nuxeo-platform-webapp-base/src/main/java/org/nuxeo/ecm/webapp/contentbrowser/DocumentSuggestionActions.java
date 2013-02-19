e69adcad59d71c86c89d92d9b139273b7c715f6e
Anahide Tchertchian <at@nuxeo.com>
2013-02-19 16:41:49 +0100

    NXP-11014: Allow using additional parameters in page provider for document suggestion widgets

--- nuxeo-platform-webapp-base/src/main/java/org/nuxeo/ecm/webapp/contentbrowser/DocumentSuggestionActions.java
+++ nuxeo-platform-webapp-base/src/main/java/org/nuxeo/ecm/webapp/contentbrowser/DocumentSuggestionActions.java
@@ -21,6 +21,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import javax.faces.context.FacesContext;
+
 import org.apache.commons.lang.StringUtils;
 import org.jboss.seam.ScopeType;
 import org.jboss.seam.annotations.In;
@@ -34,8 +36,10 @@ import org.nuxeo.ecm.core.api.IdRef;
 import org.nuxeo.ecm.core.api.PathRef;
 import org.nuxeo.ecm.core.api.security.SecurityConstants;
 import org.nuxeo.ecm.platform.query.api.PageProvider;
+import org.nuxeo.ecm.platform.query.api.PageProviderDefinition;
 import org.nuxeo.ecm.platform.query.api.PageProviderService;
 import org.nuxeo.ecm.platform.query.nxql.CoreQueryDocumentPageProvider;
+import org.nuxeo.ecm.platform.ui.web.util.ComponentTagUtils;
 import org.nuxeo.runtime.api.Framework;
 
 /**
@@ -78,6 +82,18 @@ public class DocumentSuggestionActions implements Serializable {
         return documents;
     }
 
+    /**
+     * Returns suggestion documents matching given pattern.
+     * <p>
+     * Search is performed using the page provider referenced in the widget
+     * definition (defaults to {@link #DEFAULT_PAGE_PROVIDER_NAME}).
+     * <p>
+     * Since 5.7, if the page provider defines parameters, they're added to the
+     * implicit parameter (the pattern) that stays the first one to be
+     * referenced in the search query.
+     *
+     * @param pattern: the input as typed by user in suggestion box
+     */
     @SuppressWarnings("unchecked")
     protected PageProvider<DocumentModel> getPageProvider(String pattern)
             throws ClientException {
@@ -90,8 +106,21 @@ public class DocumentSuggestionActions implements Serializable {
             Map<String, Serializable> props = new HashMap<String, Serializable>();
             props.put(CoreQueryDocumentPageProvider.CORE_SESSION_PROPERTY,
                     (Serializable) documentManager);
+            // resolve additional parameters
+            PageProviderDefinition ppDef = ppService.getPageProviderDefinition(ppName);
+            String[] params = ppDef.getQueryParameters();
+            if (params == null) {
+                params = new String[0];
+            }
+            FacesContext context = FacesContext.getCurrentInstance();
+            Object[] resolvedParams = new Object[params.length + 1];
+            resolvedParams[0] = pattern;
+            for (int i = 0; i < params.length; i++) {
+                resolvedParams[i + 1] = ComponentTagUtils.resolveElExpression(
+                        context, params[i]);
+            }
             PageProvider<DocumentModel> pp = (PageProvider<DocumentModel>) ppService.getPageProvider(
-                    ppName, null, null, null, props, new Object[] { pattern });
+                    ppName, null, null, null, props, resolvedParams);
             if (pp == null) {
                 throw new ClientException("Page provider not found: " + ppName);
             }
