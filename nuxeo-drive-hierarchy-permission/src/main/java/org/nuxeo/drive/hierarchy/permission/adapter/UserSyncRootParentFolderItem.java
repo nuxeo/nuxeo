961051b7a7237fa77f11109cbfd8cbbb4331abd5
Antoine Taillefer <ataillefer@nuxeo.com>
2013-02-26 20:48:36 +0100

    NXP-11074: Add TODO

--- nuxeo-drive-hierarchy-permission/src/main/java/org/nuxeo/drive/hierarchy/permission/adapter/UserSyncRootParentFolderItem.java
+++ nuxeo-drive-hierarchy-permission/src/main/java/org/nuxeo/drive/hierarchy/permission/adapter/UserSyncRootParentFolderItem.java
@@ -93,6 +93,7 @@ public class UserSyncRootParentFolderItem extends DocumentBackedFolderItem {
                 Iterator<IdRef> syncRootRefsIt = syncRootRefs.iterator();
                 while (syncRootRefsIt.hasNext()) {
                     IdRef idRef = syncRootRefsIt.next();
+                    // TODO: handle DocumentSecurityException
                     DocumentModel doc = session.getDocument(idRef);
                     // Filter by creator
                     // TODO: allow filtering by dc:creator in
@@ -100,6 +101,7 @@ public class UserSyncRootParentFolderItem extends DocumentBackedFolderItem {
                     // principal)
                     if (session.getPrincipal().getName().equals(
                             doc.getPropertyValue("dc:creator"))) {
+                        // TODO: handle null FileSystemItem
                         children.add(getFileSystemItemAdapterService().getFileSystemItem(
                                 doc, getId()));
                     }
