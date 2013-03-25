029f2ad9bd4c7a8062f6d3b07415d3132a50b2c2
Thierry Delprat <tdelprat@nuxeo.com>
2013-03-26 00:56:56 +0100

    NXP-11305 Add Identity processor as default when no other processor is found

--- nuxeo-template-rendering-core/src/main/java/org/nuxeo/template/service/TemplateProcessorComponent.java
+++ nuxeo-template-rendering-core/src/main/java/org/nuxeo/template/service/TemplateProcessorComponent.java
@@ -30,6 +30,7 @@ import org.nuxeo.template.api.descriptor.ContextExtensionFactoryDescriptor;
 import org.nuxeo.template.api.descriptor.OutputFormatDescriptor;
 import org.nuxeo.template.api.descriptor.TemplateProcessorDescriptor;
 import org.nuxeo.template.context.AbstractContextBuilder;
+import org.nuxeo.template.processors.IdentityProcessor;
 
 /**
  * Runtime Component used to handle Extension Points and expose the
@@ -212,8 +213,8 @@ public class TemplateProcessorComponent extends DefaultComponent implements
     @Override
     public TemplateProcessor getProcessor(String name) {
         if (name == null) {
-            log.warn("Can not get a TemplateProcessor with null name !!!");
-            return null;
+            log.info("no defined processor name, using Identity as default");
+            name = IdentityProcessor.NAME;
         }
         TemplateProcessorDescriptor desc = processorRegistry.getProcessorByName(name);
         if (desc != null) {
@@ -394,7 +395,8 @@ public class TemplateProcessorComponent extends DefaultComponent implements
     }
 
     @Override
-    public OutputFormatDescriptor getOutputFormatDescriptor(String outputFormatId) {
+    public OutputFormatDescriptor getOutputFormatDescriptor(
+            String outputFormatId) {
         return outputFormatRegistry.getOutputFormatById(outputFormatId);
     }
 }
