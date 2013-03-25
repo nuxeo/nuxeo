80ef2195c2fd2d0f69c3a40e4ba7872bbf3de809
Antoine Taillefer <ataillefer@nuxeo.com>
2013-03-25 02:24:38 +0100

    NXP-11299: Organize imports

--- nuxeo-drive-core/src/test/java/org/nuxeo/drive/service/adapter/TestFileSystemItemAdapterService.java
+++ nuxeo-drive-core/src/test/java/org/nuxeo/drive/service/adapter/TestFileSystemItemAdapterService.java
@@ -28,7 +28,6 @@ import java.util.List;
 import java.util.Map;
 
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.nuxeo.drive.adapter.FileItem;
