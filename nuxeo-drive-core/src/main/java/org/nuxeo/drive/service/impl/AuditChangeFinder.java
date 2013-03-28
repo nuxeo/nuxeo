ce910fe6a142d6234a317831e33a37d990b57cbb
Olivier Grisel <olivier.grisel@ensta.org>
2013-03-28 17:22:25 +0100

    Cosmit: remove deprecation warning

--- nuxeo-drive-core/src/main/java/org/nuxeo/drive/service/impl/AuditChangeFinder.java
+++ nuxeo-drive-core/src/main/java/org/nuxeo/drive/service/impl/AuditChangeFinder.java
@@ -23,9 +23,9 @@ import java.util.Date;
 import java.util.List;
 import java.util.Set;
 
+import org.apache.commons.lang.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.nuxeo.common.utils.StringUtils;
 import org.nuxeo.drive.adapter.FileSystemItem;
 import org.nuxeo.drive.service.FileSystemChangeFinder;
 import org.nuxeo.drive.service.NuxeoDriveEvents;
