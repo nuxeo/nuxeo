1e089ae53db8ae880687c38e1d08c6439ef8aef1
Antoine Taillefer <ataillefer@nuxeo.com>
2013-04-04 16:06:27 +0200

    NXP-11074: Make PermissionTopLevelFactory extend abstract class instead of default implementation

--- nuxeo-drive-hierarchy-permission/src/main/java/org/nuxeo/drive/hierarchy/permission/factory/PermissionTopLevelFactory.java
+++ nuxeo-drive-hierarchy-permission/src/main/java/org/nuxeo/drive/hierarchy/permission/factory/PermissionTopLevelFactory.java
@@ -28,7 +28,7 @@ import org.apache.commons.logging.LogFactory;
 import org.nuxeo.drive.adapter.FolderItem;
 import org.nuxeo.drive.hierarchy.permission.adapter.PermissionTopLevelFolderItem;
 import org.nuxeo.drive.service.TopLevelFolderItemFactory;
-import org.nuxeo.drive.service.impl.DefaultTopLevelFolderItemFactory;
+import org.nuxeo.drive.service.impl.AbstractVirtualFolderItemFactory;
 import org.nuxeo.ecm.core.api.ClientException;
 
 /**
@@ -37,7 +37,8 @@ import org.nuxeo.ecm.core.api.ClientException;
  *
  * @author Antoine Taillefer
  */
-public class PermissionTopLevelFactory extends DefaultTopLevelFolderItemFactory {
+public class PermissionTopLevelFactory extends AbstractVirtualFolderItemFactory
+        implements TopLevelFolderItemFactory {
 
     private static final Log log = LogFactory.getLog(PermissionTopLevelFactory.class);
 
@@ -45,6 +46,7 @@ public class PermissionTopLevelFactory extends DefaultTopLevelFolderItemFactory 
 
     protected List<String> childrenFactoryNames = new ArrayList<String>();
 
+    /*---------------------- FileSystemItemFactory ---------------*/
     @Override
     public void handleParameters(Map<String, String> parameters)
             throws ClientException {
@@ -60,6 +62,14 @@ public class PermissionTopLevelFactory extends DefaultTopLevelFolderItemFactory 
         }
     }
 
+    /*---------------------- VirtualFolderItemFactory ---------------*/
+    @Override
+    public FolderItem getVirtualFolderItem(Principal principal)
+            throws ClientException {
+        return getTopLevelFolderItem(principal);
+    }
+
+    /*----------------------- TopLevelFolderItemFactory ---------------------*/
     @Override
     public FolderItem getTopLevelFolderItem(Principal principal)
             throws ClientException {
