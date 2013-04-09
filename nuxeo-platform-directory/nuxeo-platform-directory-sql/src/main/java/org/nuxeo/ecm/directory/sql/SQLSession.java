f4935e7d1b57aaae2512894a79c1920471a6721a
Florent Guillaume <fg@nuxeo.com>
2013-04-09 15:17:40 +0200

    NXP-11030: fix auto-generated key return for Oracle

--- nuxeo-platform-directory/nuxeo-platform-directory-sql/src/main/java/org/nuxeo/ecm/directory/sql/SQLSession.java
+++ nuxeo-platform-directory/nuxeo-platform-directory-sql/src/main/java/org/nuxeo/ecm/directory/sql/SQLSession.java
@@ -204,7 +204,7 @@ public class SQLSession extends BaseSession implements EntrySource {
         PreparedStatement ps = null;
         Statement st = null;
         try {
-            if (autoincrementIdField) {
+            if (autoincrementIdField && dialect.hasIdentityGeneratedKey()) {
                 ps = sqlConnection.prepareStatement(sql,
                         new String[] { idField });
             } else {
