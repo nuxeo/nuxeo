cd4a4eefe12973099b486e73cf3ceb7ce9e7fb67
Florent Guillaume <fg@nuxeo.com>
2013-04-11 15:08:44 +0200

    NXP-11397: optimize document write

--- nuxeo-core/src/main/java/org/nuxeo/ecm/core/api/DocumentModelFactory.java
+++ nuxeo-core/src/main/java/org/nuxeo/ecm/core/api/DocumentModelFactory.java
@@ -41,7 +41,6 @@ import org.nuxeo.ecm.core.schema.Prefetch;
 import org.nuxeo.ecm.core.schema.PrefetchInfo;
 import org.nuxeo.ecm.core.schema.SchemaManager;
 import org.nuxeo.ecm.core.schema.TypeProvider;
-import org.nuxeo.ecm.core.schema.TypeService;
 import org.nuxeo.ecm.core.schema.types.Field;
 import org.nuxeo.ecm.core.schema.types.ListType;
 import org.nuxeo.ecm.core.schema.types.Schema;
@@ -294,9 +293,10 @@ public class DocumentModelFactory {
         }
 
         // write data models
-        DocumentPart[] parts = docModel.getParts(); // TODO only loaded ones
-        for (DocumentPart part : parts) {
-            if (part.isDirty()) {
+        // check only the loaded ones to find the dirty ones
+        for (DataModel dm : docModel.getDataModelsCollection()) { // only loaded
+            if (dm.isDirty()) {
+                DocumentPart part = ((DataModelImpl) dm).getDocumentPart();
                 try {
                     doc.writeDocumentPart(part);
                 } catch (ClientException e) {
