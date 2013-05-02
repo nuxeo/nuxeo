87e04527e91d9034a36822af3da2a123fcf71b77
Florent Guillaume <fg@nuxeo.com>
2013-05-02 15:16:08 +0200

    cleanup test output

--- nuxeo-core-event/src/test/resources/script/postCommitListener.groovy
+++ nuxeo-core-event/src/test/resources/script/postCommitListener.groovy
@@ -1,6 +1,1 @@
-
-bundle.each
-{
-    org.nuxeo.ecm.core.event.test.PostCommitEventListenerTest.SCRIPT_CNT++;
-    System.out.println("Hello from a post commit Groovy listener. Event name: "+ it.name + ". CNT: "+org.nuxeo.ecm.core.event.test.PostCommitEventListenerTest.SCRIPT_CNT);
-}
+// dummy listener, not executed
--- nuxeo-core-test/src/test/resources/script/postCommitListener.groovy
+++ nuxeo-core-test/src/test/resources/script/postCommitListener.groovy
@@ -2,5 +2,5 @@
 bundle.each
 {
     org.nuxeo.ecm.core.event.test.PostCommitEventListenerTest.SCRIPT_CNT++;
-    System.out.println("Hello from a post commit Groovy listener. Event name: "+ it.name + ". CNT: "+org.nuxeo.ecm.core.event.test.PostCommitEventListenerTest.SCRIPT_CNT);
+    // System.out.println("Hello from a post commit Groovy listener. Event name: "+ it.name + ". CNT: "+org.nuxeo.ecm.core.event.test.PostCommitEventListenerTest.SCRIPT_CNT);
 }
