6d2ea4d7006a18210f120ee4ad5d863895ce22e9
Benjamin JALON <bjalon@nuxeo.com>
2013-05-02 18:26:08 +0200

    NXP-11505: Improve Class mapper in XMAP to manage Array

--- src/main/java/org/nuxeo/common/xmap/Context.java
+++ src/main/java/org/nuxeo/common/xmap/Context.java
@@ -1,5 +1,5 @@
 /*
- * (C) Copyright 2006-2011 Nuxeo SA (http://nuxeo.com/) and contributors.
+ * (C) Copyright 2006-2013 Nuxeo SA (http://nuxeo.com/) and contributors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -12,11 +12,12 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * 
+ *
  * Contributors:
  *     Nuxeo - initial API and implementation
+ *     Bogdan Stefanescu
+ *     Benjamin JALON
  *
- * $Id$
  */
 
 package org.nuxeo.common.xmap;
@@ -33,12 +34,13 @@ public class Context extends ArrayList<Object> {
 
     private static final long serialVersionUID = 1L;
 
-    // Declare as a HashMap so that it is considered serializable
-    @SuppressWarnings({"CollectionDeclaredAsConcreteClass"})
     private final HashMap<String, Object> properties = new HashMap<String, Object>();
 
 
-    public Class loadClass(String className) throws ClassNotFoundException {
+    public Class<?> loadClass(String className) throws ClassNotFoundException {
+        if (className.startsWith("[")) {
+            return Class.forName(className, true, Thread.currentThread().getContextClassLoader());
+        }
         return Thread.currentThread().getContextClassLoader().loadClass(className);
     }
 
