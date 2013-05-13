c16f33d953a20f8ae3b0e3a25780ea15863f5b73
Olivier Grisel <olivier.grisel@ensta.org>
2013-05-13 18:32:31 +0200

    NXP-10080: add backward compat for the RestFeature renamed class by using inheritance

--- nuxeo-automation/nuxeo-automation-test/src/main/java/org/nuxeo/ecm/automation/test/RestFeature.java
+++ nuxeo-automation/nuxeo-automation-test/src/main/java/org/nuxeo/ecm/automation/test/RestFeature.java
@@ -1,10 +1,10 @@
 /*
- * (C) Copyright 2013 Nuxeo SAS (http://nuxeo.com/) and contributors.
+ * (C) Copyright 2013 Nuxeo SA (http://nuxeo.com/) and contributors.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the GNU Lesser General Public License
  * (LGPL) version 2.1 which accompanies this distribution, and is available at
- * http://www.gnu.org/licenses/lgpl.html
+ * http://www.gnu.org/licenses/lgpl-2.1.html
  *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -16,7 +16,6 @@
  */
 package org.nuxeo.ecm.automation.test;
 
-import org.nuxeo.runtime.test.runner.Features;
 
 /**
  * Shortcut to deploy bundles required by automation in your test.
@@ -24,7 +23,6 @@ import org.nuxeo.runtime.test.runner.Features;
  * @deprecated in 5.7: use EmbeddedAutomationServerFeature directly instead.
  */
 @Deprecated
-@Features({ EmbeddedAutomationServerFeature.class })
-public class RestFeature {
+public class RestFeature extends EmbeddedAutomationServerFeature {
 
 }
