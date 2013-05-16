f193f0f391d19552ac51c4bab0f7d33ffff8af00
Thomas Roger <throger@gmail.com>
2013-05-16 22:01:18 +0200

    NXP-11557: add Filter to redirect old '/dam/icons/*' resources to '/icons/' to keep backward compatibility

---
+++ nuxeo-dam-jsf/src/main/java/org/nuxeo/dam/filter/DamOldResourcesFilter.java
@@ -0,0 +1,59 @@
+/*
+ * (C) Copyright 2013 Nuxeo SA (http://nuxeo.com/) and contributors.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser General Public License
+ * (LGPL) version 2.1 which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl-2.1.html
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * Contributors:
+ *     Thomas Roger
+ */
+
+package org.nuxeo.dam.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Filter to redirect "/dam/icons/*" resources to "/icons/" for backward
+ * compatibility.
+ *
+ * @since 5.7
+ */
+public class DamOldResourcesFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response,
+            FilterChain chain) throws IOException, ServletException {
+        if (request instanceof HttpServletRequest) {
+            HttpServletRequest httpServletRequest = ((HttpServletRequest) request);
+            String contextPath = httpServletRequest.getContextPath();
+            String requestURI = httpServletRequest.getRequestURI();
+            requestURI = requestURI.replaceFirst(contextPath, "");
+            requestURI = requestURI.replace("/dam/icons", "/icons");
+            httpServletRequest.getRequestDispatcher(requestURI).forward(
+                    request, response);
+        }
+    }
+
+    @Override
+    public void destroy() {
+    }
+}
