2e0b66f5e39e6ea65dcf55ae9a5080535eca6c31
Damien Metzler <dmetzler@gmail.com>
2013-05-23 16:30:53 +0200

    NXP-11558 Refactor test infra

---
+++ nuxeo-quota-core/src/test/java/org/nuxeo/ecm/quota/count/QuotaFeature.java
@@ -0,0 +1,38 @@
+/*
+ * (C) Copyright 2011 Nuxeo SA (http://nuxeo.com/) and contributors.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser General Public License
+ * (LGPL) version 2.1 which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl.html
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * Contributors:
+ *     dmetzler
+ */
+package org.nuxeo.ecm.quota.count;
+
+import org.nuxeo.ecm.core.test.CoreFeature;
+import org.nuxeo.ecm.core.test.TransactionalFeature;
+import org.nuxeo.ecm.core.test.annotations.TransactionalConfig;
+import org.nuxeo.runtime.test.runner.Deploy;
+import org.nuxeo.runtime.test.runner.Features;
+import org.nuxeo.runtime.test.runner.SimpleFeature;
+
+/**
+ * @author dmetzler
+ *
+ */
+@Features({ TransactionalFeature.class, CoreFeature.class })
+@TransactionalConfig(autoStart = false)
+@Deploy({ "org.nuxeo.ecm.platform.userworkspace.api",
+        "org.nuxeo.ecm.platform.userworkspace.core",
+        "org.nuxeo.ecm.platform.userworkspace.types",
+        "org.nuxeo.ecm.quota.core" })
+public class QuotaFeature extends SimpleFeature{
+
+}
