39e042989e6850452f03047f852a825203be7a96
Vladimir Pasquier <vpasquier@nuxeo.com>
2013-06-11 20:29:34 +0200

    NXP-11260: add pojo example into automation tests

---
+++ nuxeo-automation/nuxeo-automation-test/src/test/java/org/nuxeo/ecm/automation/server/test/business/pojo/BusinessBean.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c) 2006-2011 Nuxeo SA (http://nuxeo.com/) and others.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Vladimir Pasquier <vpasquier@nuxeo.com>
+ */
+package org.nuxeo.ecm.automation.server.test.business.pojo;
+
+/**
+ * Automation client File pojo example
+ */
+public class BusinessBean {
+
+    protected String title;
+
+    protected String description;
+
+    protected String id;
+
+    public BusinessBean() {
+    }
+
+    public BusinessBean(String title, String description) {
+        this.title = title;
+        this.description = description;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+}
