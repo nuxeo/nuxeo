f8c976bfbf9a660484b2982f2970db5936ae657d
Vladimir Pasquier <vpasquier@nuxeo.com>
2013-06-17 18:32:20 +0200

    NXP-11260: add automation client side pojo as an example for adapter mapping test

---
+++ nuxeo-automation/nuxeo-automation-test/src/test/java/org/nuxeo/ecm/automation/server/test/business/client/BusinessBean.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright (c) 2006-2013 Nuxeo SA (http://nuxeo.com/) and others.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Vladimir Pasquier <vpasquier@nuxeo.com>
+ */
+package org.nuxeo.ecm.automation.server.test.business.client;
+
+import org.nuxeo.ecm.automation.client.annotations.EntityType;
+
+/**
+ * Automation client File pojo example - Annotated by EntityType setting the
+ * document model adapter simple name to map server side
+ */
+@EntityType("BusinessBeanAdapter")
+public class BusinessBean {
+
+    protected String title;
+
+    protected String description;
+
+    protected String id;
+
+    public BusinessBean() {
+    }
+
+    public BusinessBean(String title, String description) {
+        this.title = title;
+        this.description = description;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+}
