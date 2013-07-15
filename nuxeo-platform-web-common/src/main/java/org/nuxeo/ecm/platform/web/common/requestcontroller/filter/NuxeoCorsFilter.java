f6263816943c905bb37562186c7dfb3c65ad6457
Arnaud Kervern <akervern@nuxeo.com>
2013-07-15 12:29:27 +0200

    NXP-11686: Add missing copyrights

--- nuxeo-platform-web-common/src/main/java/org/nuxeo/ecm/platform/web/common/requestcontroller/filter/NuxeoCorsFilter.java
+++ nuxeo-platform-web-common/src/main/java/org/nuxeo/ecm/platform/web/common/requestcontroller/filter/NuxeoCorsFilter.java
@@ -1,3 +1,20 @@
+/*
+ * (C) Copyright 2013 Nuxeo SA (http://nuxeo.com/) and contributors.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser General Public License
+ * (LGPL) version 2.1 which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl-2.1.html
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * Contributors:
+ *     Arnaud Kervern
+ */
+
 package org.nuxeo.ecm.platform.web.common.requestcontroller.filter;
 
 import java.io.IOException;
@@ -14,10 +31,10 @@ import com.thetransactioncompany.cors.CORSFilter;
 /**
  * Nuxeo CORS filter wrapper to com.thetransactioncompany.cors.CORSFilter
  * allowing to configure cors filter depending of the request url.
- *
+ * 
  * Each time a request matchs a contribution is found, CORSFilter had to be
  * re-initialized to change his configurations.
- *
+ * 
  * @author <a href="mailto:ak@nuxeo.com">Arnaud Kervern</a>
  * @since 5.7.2
  */
