7a2dfae73d9ce8217ee274bdd24c33b74314757b
Stephane Lacoin <slacoin@nuxeo.com>
2013-07-17 09:27:51 +0200

    NXP-11995 metrics upgrade 3.0.0

--- nuxeo-runtime-metrics/src/main/java/org/nuxeo/runtime/metrics/MetricsServiceImpl.java
+++ nuxeo-runtime-metrics/src/main/java/org/nuxeo/runtime/metrics/MetricsServiceImpl.java
@@ -16,38 +16,43 @@
  */
 package org.nuxeo.runtime.metrics;
 
-import java.io.File;
-import java.util.concurrent.TimeUnit;
+import javax.management.MalformedObjectNameException;
+import javax.management.ObjectName;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.apache.log4j.LogManager;
-import org.nuxeo.runtime.api.Framework;
 import org.nuxeo.runtime.model.ComponentContext;
 import org.nuxeo.runtime.model.ComponentInstance;
 import org.nuxeo.runtime.model.DefaultComponent;
 
-import com.yammer.metrics.Metrics;
-import com.yammer.metrics.core.Counter;
-import com.yammer.metrics.graphite.GraphiteReporter;
-import com.yammer.metrics.log4j.InstrumentedAppender;
-import com.yammer.metrics.reporting.CsvReporter;
-import com.yammer.metrics.util.JmxGauge;
+import com.codahale.metrics.Counter;
+import com.codahale.metrics.Gauge;
+import com.codahale.metrics.JmxAttributeGauge;
+import com.codahale.metrics.MetricRegistry;
+import com.codahale.metrics.SharedMetricRegistries;
+import com.codahale.metrics.Timer;
 
 public class MetricsServiceImpl extends DefaultComponent implements
         MetricsService {
 
-    private static final Log log = LogFactory.getLog(MetricsServiceImpl.class);
+    protected static final Log log = LogFactory.getLog(MetricsServiceImpl.class);
 
-    private final Counter instanceUp = Metrics.defaultRegistry().newCounter(
-            MetricsServiceImpl.class, "instance-up");
+    protected MetricRegistry registry = SharedMetricRegistries.getOrCreate(MetricsService.class.getName());
 
-    public static final String CONFIGURATION_EP = "configuration";
+    private final Counter instanceUp = registry.counter(MetricRegistry.name(
+            MetricsServiceImpl.class, "instance-up"));
+
+    protected static final String CONFIGURATION_EP = "configuration";
 
     public static MetricsDescriptor config;
 
     public MetricsServiceImpl() {
         super();
+    }
+
+    @Override
+    public MetricRegistry getRegistry() {
+        return registry;
     }
 
     @Override
@@ -64,13 +69,47 @@ public class MetricsServiceImpl extends DefaultComponent implements
     }
 
     @Override
+    public Gauge<?> newGauge(String mbean, String attribute, Class<?> clazz,
+            String... names) {
+        try {
+            JmxAttributeGauge gauge = new JmxAttributeGauge(new ObjectName(
+                    mbean), attribute);
+            return newGauge(gauge, clazz, names);
+        } catch (MalformedObjectNameException | IllegalArgumentException e) {
+            throw new UnsupportedOperationException(
+                    "Cannot compute object name of " + mbean);
+        }
+    }
+
+    @Override
+    public Gauge<?> newGauge(Gauge<?> gauge, Class<?> clazz, String... names) {
+        registry.register(MetricRegistry.name(clazz, names), gauge);
+        return gauge;
+    }
+
+    @Override
+    public Counter newCounter(Class<?> clazz, String... names) {
+        return registry.counter(MetricRegistry.name(clazz, names));
+    }
+
+
+    @Override
+    public Timer newTimer(Class<?> clazz, String... names) {
+        return registry.timer(MetricRegistry.name(clazz, names));
+    }
+
+    @Override
     public void activate(ComponentContext context) {
         log.debug("Activate component.");
     }
 
     @Override
     public void deactivate(ComponentContext context) {
-        instanceUp.dec();
+        try {
+            config.disable(registry);
+        } finally {
+            instanceUp.dec();
+        }
         log.debug("Deactivate component.");
     }
 
@@ -81,88 +120,8 @@ public class MetricsServiceImpl extends DefaultComponent implements
             config = new MetricsDescriptor();
         }
         log.info("Setting up metrics configuration");
-        if (config.graphiteReporter.isEnabled()) {
-            log.info(config.graphiteReporter);
-            GraphiteReporter.enable(config.graphiteReporter.getPeriod(),
-                    TimeUnit.SECONDS, config.graphiteReporter.getHost(),
-                    config.graphiteReporter.getPort(),
-                    config.graphiteReporter.getPrefix());
-        }
-        if (config.csvReporter.isEnabled()) {
-            log.info(config.csvReporter);
-            File outputDir = config.csvReporter.getOutput();
-            if (outputDir.getParentFile().exists() && outputDir.getParentFile().isDirectory()) {
-                outputDir.mkdir();
-                CsvReporter.enable(outputDir, config.csvReporter.period,
-                        TimeUnit.SECONDS);
-            } else {
-                config.csvReporter.enabled = false;
-                log.error("Invalid output directory, disabling: " + config.csvReporter);
-            }
-        }
-        if (config.log4jInstrunmentation.isEnabled()) {
-            log.info(config.log4jInstrunmentation);
-            LogManager.getRootLogger().addAppender(new InstrumentedAppender());
-        }
+        config.enable(registry);
         instanceUp.inc();
-
-        if (config.tomcatInstrunmentation.isEnabled()) {
-            log.info(config.tomcatInstrunmentation);
-            // TODO: do not hard code the common datasource name
-            String commonDs = "jdbc/nuxeo";
-            Metrics.defaultRegistry().newGauge(
-                    getClass(),
-                    "jdbc-numActive",
-                    new JmxGauge(
-                            "Catalina:type=DataSource,class=javax.sql.DataSource,name=\""
-                                    + commonDs + "\"", "numActive"));
-            Metrics.defaultRegistry().newGauge(
-                    getClass(),
-                    "jdbc-numIdle",
-                    new JmxGauge(
-                            "Catalina:type=DataSource,class=javax.sql.DataSource,name=\""
-                                    + commonDs + "\"", "numIdle"));
-
-            String connector = String.format("http-%s-%s",
-                    Framework.getProperty("nuxeo.bind.address", "0.0.0.0"),
-                    Framework.getProperty("nuxeo.bind.port", "8080"));
-            Metrics.defaultRegistry().newGauge(
-                    getClass(),
-                    "tomcat-currentThreadCount",
-                    new JmxGauge("Catalina:type=ThreadPool,name=" + connector,
-                            "currentThreadCount"));
-            Metrics.defaultRegistry().newGauge(
-                    getClass(),
-                    "tomcat-currentThreadsBusy",
-                    new JmxGauge("Catalina:type=ThreadPool,name=" + connector,
-                            "currentThreadsBusy"));
-            Metrics.defaultRegistry().newGauge(
-                    getClass(),
-                    "tomcat-errorCount",
-                    new JmxGauge("Catalina:type=GlobalRequestProcessor,name="
-                            + connector, "errorCount"));
-            Metrics.defaultRegistry().newGauge(
-                    getClass(),
-                    "tomcat-errorCount",
-                    new JmxGauge("Catalina:type=GlobalRequestProcessor,name="
-                            + connector, "errorCount"));
-            Metrics.defaultRegistry().newGauge(
-                    getClass(),
-                    "tomcat-requestCount",
-                    new JmxGauge("Catalina:type=GlobalRequestProcessor,name="
-                            + connector, "requestCount"));
-            Metrics.defaultRegistry().newGauge(
-                    getClass(),
-                    "tomcat-processingTime",
-                    new JmxGauge("Catalina:type=GlobalRequestProcessor,name="
-                            + connector, "processingTime"));
-            Metrics.defaultRegistry().newGauge(
-                    getClass(),
-                    "tomcat-activeSessions",
-                    new JmxGauge(
-                            "Catalina:type=Manager,path=/nuxeo,host=localhost",
-                            "activeSessions"));
-        }
     }
 
 }
