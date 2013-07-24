ff67350ca1136cae8a177904282a76fc68e764b8
Lise Kemen <lkemen@nuxeo.com>
2013-07-24 22:49:12 +0200

    NXP-12062: Replace specific icon by the default delete icon

--- nuxeo-platform-webapp/src/main/resources/web/nuxeo.war/scripts/hideableAdminMessage.js
+++ nuxeo-platform-webapp/src/main/resources/web/nuxeo.war/scripts/hideableAdminMessage.js
@@ -3,7 +3,7 @@ jQuery(function() {
  jQuery('.header_admin_messages .actionHide').each(function() {
   if (jQuery.cookie(buildCookieName(jQuery(this)))) { jQuery(this).remove(); return; }
 
-  var hide = jQuery('<a><img src="icons/action_clipboard_remove.gif" alt="hide" /></a>').attr({href: '#'}).click(function() {
+  var hide = jQuery('<a><img src="icons/delete.png" alt="hide" /></a>').attr({href: '#'}).click(function() {
     var parent = jQuery(this).parent();
     parent.fadeOut();
     jQuery.cookie(buildCookieName(parent), true);
