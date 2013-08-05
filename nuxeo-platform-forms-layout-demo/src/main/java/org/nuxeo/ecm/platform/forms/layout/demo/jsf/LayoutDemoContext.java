374cd6e3b95d705ea2ab8d0b195ecdaeb20d64f9
Guillaume Renard <grenard@nuxeo.com>
2013-08-05 12:43:32 +0200

    NXP-12018: create specific fields in layout demo schema for Select2

--- nuxeo-platform-forms-layout-demo/src/main/java/org/nuxeo/ecm/platform/forms/layout/demo/jsf/LayoutDemoContext.java
+++ nuxeo-platform-forms-layout-demo/src/main/java/org/nuxeo/ecm/platform/forms/layout/demo/jsf/LayoutDemoContext.java
@@ -184,6 +184,9 @@ public class LayoutDemoContext implements Serializable {
             doc.setPropertyValue("lds:selectVocabularyField", "cartman");
             doc.setPropertyValue("lds:selectMultiVocabularyField",
                     new String[] { "cartman", "marsh" });
+            doc.setPropertyValue("selectSelect2DirectoryField", "africa/Botswana");
+            doc.setPropertyValue("lds:selectSelect2DirectoryMultiField",
+                    new String[] { "art/art history","art/culture","sciences/logic" });
             doc.setPropertyValue("lds:dateField", Calendar.getInstance());
             doc.setPropertyValue("lds:intField", new Integer(666));
             doc.setPropertyValue("lds:booleanField", Boolean.FALSE);
@@ -224,6 +227,9 @@ public class LayoutDemoContext implements Serializable {
             doc.setPropertyValue("lds:selectVocabularyField", "marsh");
             doc.setPropertyValue("lds:selectMultiVocabularyField",
                     new String[] { "cartman" });
+            doc.setPropertyValue("selectSelect2DirectoryField", "africa/Botswana");
+            doc.setPropertyValue("lds:selectSelect2DirectoryMultiField",
+                    new String[] { "art/art history","art/culture","sciences/logic" });
             doc.setPropertyValue("lds:dateField", Calendar.getInstance());
             doc.setPropertyValue("lds:intField", new Integer(667));
             doc.setPropertyValue("lds:booleanField", Boolean.TRUE);
