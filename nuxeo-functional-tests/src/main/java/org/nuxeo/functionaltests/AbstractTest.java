baea0623132a9ab8e0aadfec0bedfb2025861e63
Anahide Tchertchian <at@nuxeo.com>
2013-08-14 14:51:06 +0200

    NXP-12250/NXBT-609: Improve Firefox profile for Webdriver tests

--- nuxeo-functional-tests/src/main/java/org/nuxeo/functionaltests/AbstractTest.java
+++ nuxeo-functional-tests/src/main/java/org/nuxeo/functionaltests/AbstractTest.java
@@ -272,6 +272,73 @@ public abstract class AbstractTest {
         // Set English as default language
         profile.setPreference("general.useragent.locale", "en");
         profile.setPreference("intl.accept_languages", "en");
+        // Set other confs to speed up FF
+
+        // Speed up firefox by pipelining requests on a single connection
+        profile.setPreference("network.http.keep-alive", true);
+        profile.setPreference("network.http.pipelining", true);
+        profile.setPreference("network.http.proxy.pipelining", true);
+        profile.setPreference("network.http.pipelining.maxrequests", 8);
+
+        // Try to use less memory
+        profile.setPreference("browser.sessionhistory.max_entries", 10);
+        profile.setPreference("browser.sessionhistory.max_total_viewers", 4);
+        profile.setPreference("browser.sessionstore.max_tabs_undo", 4);
+        profile.setPreference("browser.sessionstore.interval", 1800000);
+
+        // do not load images
+        profile.setPreference("permissions.default.image", 2);
+
+        // disable unresponsive script alerts
+        profile.setPreference("dom.max_script_run_time", 0);
+        profile.setPreference("dom.max_chrome_script_run_time", 0);
+
+        // don't skip proxy for localhost
+        profile.setPreference("network.proxy.no_proxies_on", "");
+
+        // prevent different kinds of popups/alerts
+        profile.setPreference("browser.tabs.warnOnClose", false);
+        profile.setPreference("browser.tabs.warnOnOpen", false);
+        profile.setPreference("extensions.newAddons", false);
+        profile.setPreference("extensions.update.notifyUser", false);
+
+        // disable autoscrolling
+        profile.setPreference("browser.urlbar.autocomplete.enabled", false);
+
+        // downloads conf
+        profile.setPreference("browser.download.useDownloadDir", false);
+
+        // prevent FF from running in offline mode when there's no network
+        // connection
+        profile.setPreference("toolkit.networkmanager.disable", true);
+
+        // prevent FF from giving health reports
+        profile.setPreference("datareporting.policy.dataSubmissionEnabled",
+                false);
+        profile.setPreference("datareporting.healthreport.uploadEnabled", false);
+        profile.setPreference("datareporting.healthreport.service.firstRun",
+                false);
+        profile.setPreference("datareporting.healthreport.service.enabled",
+                false);
+        profile.setPreference(
+                "datareporting.healthreport.logging.consoleEnabled", false);
+
+        // start page conf to speed up FF
+        profile.setPreference("browser.startup.homepage", "about:blank");
+        profile.setPreference(
+                "pref.browser.homepage.disable_button.bookmark_page", false);
+        profile.setPreference(
+                "pref.browser.homepage.disable_button.restore_default", false);
+
+        // misc confs to avoid useless updates
+        profile.setPreference("browser.search.update", false);
+        profile.setPreference("browser.bookmarks.restore_default_bookmarks",
+                false);
+
+        // misc confs to speed up FF
+        profile.setPreference("extensions.ui.dictionary.hidden", true);
+        profile.setPreference("layout.spellcheckDefault", 0);
+
         addFireBug(profile);
         Proxy proxy = startProxy();
         if (proxy != null) {
