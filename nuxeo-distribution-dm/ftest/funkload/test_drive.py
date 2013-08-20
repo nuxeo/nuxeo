7b7f112df1a5310e2d43c5b35b8f6135836f1886
bdelbosc <bdelbosc@nuxeo.com>
2013-08-20 18:06:05 +0200

    NXP-12065: Use a new folder for each test so the number of request is constant

--- nuxeo-distribution-dm/ftest/funkload/test_drive.py
+++ nuxeo-distribution-dm/ftest/funkload/test_drive.py
@@ -30,8 +30,7 @@ from nuxeo.testcase import NuxeoTestCase
 
 class Drive(NuxeoTestCase):
     ws_title = "FLNXTEST Drive workspace"
-    dir_title = "FLNXTEST Drive folder"
-    dir_path = "workspaces/FLNXTEST Drive workspace/FLNXTEST Drive folder"
+    ws_path = "workspaces/FLNXTEST Drive workspace"
     tag = "FLNXTEST"
 
     def setUp(self):
@@ -42,48 +41,54 @@ class Drive(NuxeoTestCase):
                       for item in os.listdir(import_path)]
 
     def testInit(self):
-        """Create a folder for drive document."""
+        """Create a workspace for drive document."""
         p = LoginPage(self).login(*self.cred_admin)
-        ret = p.viewDocumentPath(self.dir_path, raiseOn404=False)
+        ret = p.viewDocumentPath(self.ws_path, raiseOn404=False)
         if ret is None:
             p = (p.getRootWorkspaces()
                  .createWorkspace(self.ws_title, 'A description')
                  .rights().grant('ReadWrite', 'Members group')
-                 .view()
-                 .createFolder(self.dir_title, 'A description'))
-        self.createServerFile()
+                 .view())
         p.logout()
 
-    def createServerFile(self):
-        self.setHeader('Accept-Language', 'en-us')
-        p = FolderPage(self).viewDocumentPath(self.dir_path)
+    def createFolder(self, name):
+        return (FolderPage(self).viewDocumentPath(self.ws_path)
+                .createFolder(name, 'folder for drive test by ' +
+                              self.cred_member[0]))
+
+    def createServerFile(self, folder):
         file_path = random.choice(self.files)
         title = self._lipsum.getSubject(uniq=True, prefix=self.tag)
         description = self.tag + ' ' + self._lipsum.getParagraph(1)
-        p.createFile(title, description, file_path)
-        return p.getDocUid()
+        folder.createFile(title, description, file_path)
+        return folder.getDocUid()
 
     def testDrive(self):
+        self.setHeader('Accept-Language', 'en-us')
         d = (DriveClient(self)
              .bind_server(*self.cred_member))
-        # Use the token auth
-        (FolderPage(self)
-         .viewDocumentPath(self.dir_path)
-         .driveSynchronizeCurrentDocument())
+        # Use the token auth, create a folder and a file
+        folder_name = "Folder " + self._lipsum.getUniqWord()
+        folder = self.createFolder(folder_name)
+        fuid = folder.getDocUid()
+        folder.driveSynchronizeCurrentDocument()
+        self.createServerFile(folder)
+        # start drive and sync
         d.start_drive()
         parent_id = d.root_ids[0]
         for i in range(self.nb_write):
             path = random.choice(self.files)
             name = self._lipsum.getUniqWord() + '-' + os.path.basename(path)
             d.upload_file(parent_id, path, name)
-
-            uid = self.createServerFile()
+            folder.viewDocumentUid(fuid)
+            uid = self.createServerFile(folder)
             sleep(1)
             d.get_update(" update " + str(i))
             self.assert_(uid in self.getBody(),
                          "expecting to find the new doc uid: " +
                          uid)   # + " in " + self.getBody())
-
+        folder.viewDocumentUid(fuid)
+        folder.driveUnsynchronizeCurrentDocument()
 
 if __name__ in ('main', '__main__'):
     unittest.main()
