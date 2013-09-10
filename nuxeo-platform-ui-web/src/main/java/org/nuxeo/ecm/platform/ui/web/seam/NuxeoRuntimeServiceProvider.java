45d5366d56b2189f406a447a5329681e86313f8a
Thierry Delprat <tdelprat@nuxeo.com>
2013-09-10 12:02:54 +0200

    NXP-12207 Allow to lookup Nuxeo Service by name so that we have a real equivalent of the BusinessDelegates

--- nuxeo-platform-ui-web/src/main/java/org/nuxeo/ecm/platform/ui/web/seam/NuxeoRuntimeServiceProvider.java
+++ nuxeo-platform-ui-web/src/main/java/org/nuxeo/ecm/platform/ui/web/seam/NuxeoRuntimeServiceProvider.java
@@ -20,6 +20,10 @@ package org.nuxeo.ecm.platform.ui.web.seam;
 
 import static org.jboss.seam.annotations.Install.FRAMEWORK;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.locks.ReentrantLock;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.jboss.seam.ScopeType;
@@ -27,6 +31,7 @@ import org.jboss.seam.annotations.Install;
 import org.jboss.seam.annotations.Name;
 import org.jboss.seam.annotations.Scope;
 import org.jboss.seam.annotations.intercept.BypassInterceptors;
+import org.jboss.seam.contexts.Contexts;
 import org.jboss.seam.core.providers.ServiceProvider;
 import org.nuxeo.ecm.core.api.CoreSession;
 import org.nuxeo.runtime.api.Framework;
@@ -49,33 +54,93 @@ public class NuxeoRuntimeServiceProvider implements ServiceProvider {
 
     private static final Log log = LogFactory.getLog(NuxeoRuntimeServiceProvider.class);
 
+    @SuppressWarnings("rawtypes")
+    protected Map<String, Class> name2ServiceClassCache = new HashMap<String, Class>();
+
+    protected ReentrantLock name2ServiceClassLock = new ReentrantLock();
+
     @Override
+    @SuppressWarnings({ "rawtypes", "unchecked" })
     public Object lookup(String name, Class type, boolean create) {
 
         if (Framework.getRuntime() == null) {
             return null;
         }
 
-        if (log.isDebugEnabled()) {
-            log.debug("Nuxeo Lookup for " + name + " class " + type);
-        }
-
         if (type != null && type.isAssignableFrom(CoreSession.class)) {
             // XXX return a CoreSession on the default repository ?
             return null;
         }
+
+        Object result = null;
+
         // service loopkup
-        Object result = Framework.getLocalService(type);
-        if (result == null) {
-            // fallback on component lookup
-            result = Framework.getRuntime().getComponent(name);
+        if (type != null) {
+            result = Framework.getLocalService(type);
         }
 
+        // fallback on component lookup
+        if (result == null && name != null) {
+            if (!name.startsWith("org.jboss")) {
+                result = Framework.getRuntime().getComponent(name);
+                // lookup service by short name
+                if (result == null) {
+                    result = findServiceByShortCut(name);
+                }
+            }
+        }
         if (log.isDebugEnabled()) {
             log.debug("Nuxeo Lookup => return " + result);
         }
         return result;
 
+    }
+
+    @SuppressWarnings({ "rawtypes", "unchecked" })
+    protected Object findServiceByShortCut(String name) {
+
+        if (!name2ServiceClassCache.containsKey(name)) {
+            Class klass = null;
+            name2ServiceClassLock.lock();
+            try {
+                for (String serviceClassName : Framework.getRuntime().getComponentManager().getServices()) {
+                    int p = serviceClassName.lastIndexOf('.');
+                    String fullClassName = serviceClassName;
+                    if (p > -1) {
+                        serviceClassName = serviceClassName.substring(p + 1);
+                    }
+                    if (name.equalsIgnoreCase(serviceClassName)) {
+                        try {
+                            klass = Thread.currentThread().getContextClassLoader().loadClass(
+                                    fullClassName);
+                            if (log.isDebugEnabled()) {
+                                log.debug("Lookup for " + name
+                                        + " resolved to service "
+                                        + fullClassName);
+                            }
+                            break;
+                        } catch (ClassNotFoundException e) {
+                            log.error("Unable to load class for service "
+                                    + fullClassName, e);
+                        }
+                    }
+                }
+                // NB : puts null if not found to avoid multiple lookups
+                name2ServiceClassCache.put(name, klass);
+            } finally {
+                name2ServiceClassLock.unlock();
+            }
+        }
+        Class serviceClass = name2ServiceClassCache.get(name);
+        Object result = null;
+        if (serviceClass != null) {
+            result = Framework.getLocalService(serviceClass);
+            if (result != null && Contexts.isEventContextActive()) {
+                // cache in Event scope
+                Contexts.getEventContext().set(name, result);
+            }
+        }
+        return result;
     }
 
 }
