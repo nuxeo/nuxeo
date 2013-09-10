a0e68a0b588717e9b98ccb5924ce2222245c3f93
Benjamin JALON <bjalon@nuxeo.com>
2013-09-10 18:41:59 +0200

    NXP-12420: Add bundles that replace default Scan importer XML Parser by the new advanced XML Parser.

---
+++ nuxeo-importer-scan-xml-parser/src/main/java/org/nuxeo/ecm/platform/importer/xml/parser/AdvancedScannedFileFactory.java
@@ -0,0 +1,77 @@
+/*
+ * (C) Copyright 2006-2012 Nuxeo SAS (http://nuxeo.com/) and contributors.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser General Public License
+ * (LGPL) version 2.1 which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl.html
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * Contributors:
+ *     Nuxeo - initial API and implementation
+ *
+ * $Id$
+ */package org.nuxeo.ecm.platform.importer.xml.parser;
+
+import java.util.List;
+
+import org.nuxeo.ecm.core.api.ClientException;
+import org.nuxeo.ecm.core.api.CoreSession;
+import org.nuxeo.ecm.core.api.DocumentModel;
+import org.nuxeo.ecm.platform.importer.base.GenericMultiThreadedImporter;
+import org.nuxeo.ecm.platform.importer.factories.ImporterDocumentModelFactory;
+import org.nuxeo.ecm.platform.importer.source.FileSourceNode;
+import org.nuxeo.ecm.platform.importer.source.SourceNode;
+import org.nuxeo.ecm.platform.scanimporter.processor.ScanedFileFactory;
+import org.nuxeo.ecm.platform.scanimporter.processor.ScannedFileImporter;
+import org.nuxeo.ecm.platform.scanimporter.service.ImporterConfig;
+import org.nuxeo.runtime.api.Framework;
+
+/**
+ *
+ * Custom implementation of the {@link ImporterDocumentModelFactory}. Provides :
+ * - container doc type configuration from service - leaf doc type configuration
+ * given by the XML Parser service.
+ *
+ * @author Benjamin JALON
+ *
+ */
+public class AdvancedScannedFileFactory extends ScanedFileFactory implements
+        ImporterDocumentModelFactory {
+
+    public AdvancedScannedFileFactory(ImporterConfig config) {
+        super(config);
+    }
+
+    protected static String targetContainerType = null;
+
+    protected ImporterConfig config;
+
+    @Override
+    public DocumentModel createLeafNode(CoreSession session,
+            DocumentModel parent, SourceNode node) throws Exception {
+
+        XMLImporterService importer = Framework.getLocalService(XMLImporterService.class);
+        if (!(node instanceof FileSourceNode)) {
+            throw new ClientException("Waiting a FileSourceNode object not: "
+                    + node.getClass().getName());
+        }
+        FileSourceNode fileNode = (FileSourceNode) node;
+        List<DocumentModel> docCreated = importer.importDocuments(parent,
+                fileNode.getFile());
+
+        if (docCreated == null || docCreated.size() < 1) {
+            return null;
+        }
+
+        ScannedFileImporter.addProcessedDescriptor(fileNode.getSourcePath());
+
+        GenericMultiThreadedImporter.addCreatedDoc("_XMLimporter",
+                docCreated.size() - 1);
+        return docCreated.get(0);
+    }
+}
---
+++ nuxeo-importer-scan-xml-parser/src/main/java/org/nuxeo/ecm/platform/importer/xml/parser/XMLFileSourceNode.java
@@ -0,0 +1,55 @@
+/*
+ * (C) Copyright 2013 Nuxeo SA (http://nuxeo.com/) and contributors.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser General Public License
+ * (LGPL) version 2.1 which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl-2.1.html
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * Contributors:
+ *     Benjamin JALON<bjalon@nuxeo.com>
+ */
+package org.nuxeo.ecm.platform.importer.xml.parser;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.nuxeo.ecm.platform.importer.source.FileSourceNode;
+import org.nuxeo.ecm.platform.importer.source.SourceNode;
+
+/**
+ * @since 5.7.3
+ *
+ */
+public class XMLFileSourceNode extends FileSourceNode {
+
+    public static final Log log = LogFactory.getLog(XMLFileSourceNode.class);
+
+    public XMLFileSourceNode(File file) {
+        super(file);
+    }
+
+    @Override
+    public List<SourceNode> getChildren() {
+        List<SourceNode> children = new ArrayList<SourceNode>();
+
+        File[] childrenFile = file.listFiles();
+        for (File child : childrenFile) {
+            if (child.getName().endsWith(".xml") || child.getName().endsWith(".zip") || child.isDirectory()) {
+                children.add(new XMLFileSourceNode(child));
+            } else {
+                log.info("File ignored as not xml or zip or directory file " + child.getAbsolutePath());
+            }
+        }
+        return children;
+    }
+
+}
