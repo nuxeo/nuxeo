d6f26e6b8ab4995a2759f6fe033b4011293b4267
Guillaume Renard <grenard@nuxeo.com>
2013-09-11 12:55:35 +0200

    NXP-12263: tiny mce tells SafeEdit when it is ready and what post restore processing it needs

--- nuxeo-platform-ui-web/src/main/resources/tiny_mce/tiny_mce_init.js
+++ nuxeo-platform-ui-web/src/main/resources/tiny_mce/tiny_mce_init.js
@@ -1,8 +1,30 @@
+var nbTinyMceEditor = 0;
+
 function initTinyMCE(width, height, editorSelector, plugins, lang, toolbar) {
   // force English by default since there are no translations for other
   // languages than en and fr
   if (lang != 'en' && lang != 'fr'){
     lang = 'en';
+  }
+
+  nbTinyMceEditor++;
+
+  // if SafeEdit present
+  if (typeof registerSafeEditWait == 'function') {
+    // tell him to wait until all eleditors are loaded
+    registerSafeEditWait(function() {
+      return nbTinyMceEditor <= 0;
+    });
+  }
+
+  // if SafeEdit present
+  if (typeof registerPostRestoreCallBacks == 'function') {
+     // tell him to run a restore again the following elts
+     registerPostRestoreCallBacks(function(formSelector, data) {
+        processRestore(jQuery(formSelector).find(
+        "textarea.mceEditor,td.mceIframeContainer>iframe"), data);
+     }
+      );
   }
 
   tinyMCE.init({
@@ -16,14 +38,20 @@ function initTinyMCE(width, height, editorSelector, plugins, lang, toolbar) {
     language : lang,
     theme_advanced_resizing : true,
 
-    //Img insertion fixes
+    // Img insertion fixes
     relative_urls : false,
     remove_script_host : false,
     skin : "o2k7",
     skin_variant : "silver",
     theme_advanced_disable : "styleselect",
     theme_advanced_buttons3 : "hr,removeformat,visualaid,|,sub,sup,|,charmap,|",
-    theme_advanced_buttons3_add : toolbar
+    theme_advanced_buttons3_add : toolbar,
+    setup : function(ed) {
+      ed.onInit.add(function(ed) {
+          nbTinyMceEditor--;
+          // XXX Make sure this work when we have several editors
+      });
+   }
   });
 }
 
