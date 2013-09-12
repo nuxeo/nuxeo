6bad0ad4b6ac29acc105fed33b585d202dcfded2
Antoine Taillefer <ataillefer@nuxeo.com>
2013-09-12 16:19:28 +0200

    NXP-12523: Add Document.GetVersions and Document.RestoreVersion operations

---
+++ nuxeo-automation/nuxeo-automation-core/src/main/java/org/nuxeo/ecm/automation/core/operations/document/GetDocumentVersions.java
@@ -0,0 +1,47 @@
+/*
+ * (C) Copyright 2013 Nuxeo SA (http://nuxeo.com/) and contributors.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser General Public License
+ * (LGPL) version 2.1 which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl-2.1.html
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * Contributors:
+ *     Antoine Taillefer <ataillefer@nuxeo.com>
+ */
+package org.nuxeo.ecm.automation.core.operations.document;
+
+import org.nuxeo.ecm.automation.core.Constants;
+import org.nuxeo.ecm.automation.core.annotations.Context;
+import org.nuxeo.ecm.automation.core.annotations.Operation;
+import org.nuxeo.ecm.automation.core.annotations.OperationMethod;
+import org.nuxeo.ecm.core.api.CoreSession;
+import org.nuxeo.ecm.core.api.DocumentModel;
+import org.nuxeo.ecm.core.api.DocumentModelList;
+import org.nuxeo.ecm.core.api.impl.DocumentModelListImpl;
+
+/**
+ * Gets the versions of the input document.
+ *
+ * @since 5.7.3
+ * @author Antoine Taillefer
+ */
+@Operation(id = GetDocumentVersions.ID, category = Constants.CAT_DOCUMENT, label = "Get Versions", description = "Gets the versions of the input document.")
+public class GetDocumentVersions {
+
+    public static final String ID = "Document.GetVersions";
+
+    @Context
+    protected CoreSession session;
+
+    @OperationMethod
+    public DocumentModelList run(DocumentModel doc) throws Exception {
+        return new DocumentModelListImpl(session.getVersions(doc.getRef()));
+    }
+
+}
