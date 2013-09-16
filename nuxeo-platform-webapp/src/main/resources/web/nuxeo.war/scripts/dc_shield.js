d203033c2fe647a788c514f830244a921e2c4470
Guillaume Renard <grenard@nuxeo.com>
2013-09-17 01:46:07 +0200

    NXP-12541: add a double click shield on all forms of the page

---
+++ nuxeo-platform-webapp/src/main/resources/web/nuxeo.war/scripts/dc_shield.js
@@ -0,0 +1,28 @@
+jQuery.fn.preventDoubleSubmission = function() {
+  jQuery(this).on('submit', function(e) {
+
+    var $form = jQuery(this);
+    if ($form.data('submitted') === true) {
+      // Previously submitted - don't submit again
+      e.preventDefault();
+      jQuery("#doubleClickMsg").css({
+        "display" : "inline"
+      });
+      window.setTimeout(function() {
+        jQuery("#doubleClickMsg").css({
+          "display" : "none"
+        });
+      }, 1500);
+    } else {
+      // Mark it so that the next submit can be ignored
+      $form.data('submitted', true);
+    }
+  });
+
+  // Keep chainability
+  return this;
+};
+
+jQuery(document).ready(function() {
+  jQuery('form').preventDoubleSubmission();
+});
