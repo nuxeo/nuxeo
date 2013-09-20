a3b8f398faf51988eb52605e631ec17704777b7e
Thomas Roger <troger@nuxeo.com>
2013-09-20 19:48:27 +0200

    NXP-12390: make the admin message closable

--- nuxeo-platform-webapp/src/main/resources/web/nuxeo.war/scripts/hideableAdminMessage.js
+++ nuxeo-platform-webapp/src/main/resources/web/nuxeo.war/scripts/hideableAdminMessage.js
@@ -1,4 +1,20 @@
 (function() {
+  var cookieName = "nuxeo.adminMesage.cookie";
+  jQuery(document).ready(function() {
+    jQuery(".adminMessage").each(function() {
+      var adminMessageEle = jQuery(this);
+      adminMessageEle.find(".close").click(function() {
+        adminMessageEle.addClass("displayN");
+        jQuery.cookie(cookieName, adminMessageEle.data('timestamp'));
+      });
 
-
+      var timestamp = parseInt(jQuery.cookie(cookieName));
+      if (!timestamp) {
+        adminMessageEle.removeClass("displayN");
+      } else if (timestamp !== adminMessageEle.data("timestamp")) {
+        // admin message modified
+        adminMessageEle.removeClass("displayN");
+      }
+    });
+  });
 })();
