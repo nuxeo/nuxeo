a927526acad60bb7a11d4ca4104168922f60eaa7
Anahide Tchertchian <at@nuxeo.com>
2013-10-03 15:54:12 +0200

    NXP-12435: remove from Seam component deprecated methods linked to query models, keep the other methods needed to handle orderable folder actions

--- nuxeo-platform-webapp-core/src/main/java/org/nuxeo/ecm/webapp/contentbrowser/OrderableDocumentActions.java
+++ nuxeo-platform-webapp-core/src/main/java/org/nuxeo/ecm/webapp/contentbrowser/OrderableDocumentActions.java
@@ -1,29 +1,25 @@
 /*
-* (C) Copyright 2010 Nuxeo SA (http://nuxeo.com/) and contributors.
-*
-* All rights reserved. This program and the accompanying materials
-* are made available under the terms of the GNU Lesser General Public License
-* (LGPL) version 2.1 which accompanies this distribution, and is available at
-* http://www.gnu.org/licenses/lgpl.html
-*
-* This library is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-* Lesser General Public License for more details.
-*
-* Contributors:
-* Thomas Roger
-*/
+ * (C) Copyright 2010 Nuxeo SA (http://nuxeo.com/) and contributors.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser General Public License
+ * (LGPL) version 2.1 which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl.html
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * Contributors:
+ * Thomas Roger
+ */
 
 package org.nuxeo.ecm.webapp.contentbrowser;
 
-import static org.jboss.seam.ScopeType.EVENT;
 import static org.nuxeo.ecm.webapp.documentsLists.DocumentsListsManager.CURRENT_DOCUMENT_SECTION_SELECTION;
 import static org.nuxeo.ecm.webapp.documentsLists.DocumentsListsManager.CURRENT_DOCUMENT_SELECTION;
-import static org.nuxeo.ecm.webapp.helpers.EventNames.CONTENT_ROOT_SELECTION_CHANGED;
 import static org.nuxeo.ecm.webapp.helpers.EventNames.DOCUMENT_CHILDREN_CHANGED;
-import static org.nuxeo.ecm.webapp.helpers.EventNames.DOCUMENT_SELECTION_CHANGED;
-import static org.nuxeo.ecm.webapp.helpers.EventNames.DOMAIN_SELECTION_CHANGED;
 
 import java.io.Serializable;
 import java.util.List;
@@ -31,10 +27,8 @@ import java.util.List;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.jboss.seam.ScopeType;
-import org.jboss.seam.annotations.Factory;
 import org.jboss.seam.annotations.In;
 import org.jboss.seam.annotations.Name;
-import org.jboss.seam.annotations.Observer;
 import org.jboss.seam.annotations.Scope;
 import org.jboss.seam.core.Events;
 import org.jboss.seam.faces.FacesMessages;
@@ -42,40 +36,23 @@ import org.jboss.seam.international.StatusMessage;
 import org.nuxeo.ecm.core.api.ClientException;
 import org.nuxeo.ecm.core.api.CoreSession;
 import org.nuxeo.ecm.core.api.DocumentModel;
-import org.nuxeo.ecm.core.api.DocumentModelList;
 import org.nuxeo.ecm.core.api.DocumentRef;
 import org.nuxeo.ecm.core.api.IdRef;
-import org.nuxeo.ecm.core.api.PagedDocumentsProvider;
-import org.nuxeo.ecm.core.api.impl.DocumentModelListImpl;
 import org.nuxeo.ecm.core.schema.FacetNames;
-import org.nuxeo.ecm.core.search.api.client.querymodel.QueryModel;
-import org.nuxeo.ecm.core.search.api.client.querymodel.QueryModelService;
-import org.nuxeo.ecm.core.search.api.client.querymodel.descriptor.QueryModelDescriptor;
 import org.nuxeo.ecm.platform.ui.web.api.NavigationContext;
-import org.nuxeo.ecm.platform.ui.web.model.SelectDataModel;
-import org.nuxeo.ecm.platform.ui.web.model.SelectDataModelListener;
-import org.nuxeo.ecm.platform.ui.web.model.impl.SelectDataModelImpl;
-import org.nuxeo.ecm.platform.ui.web.model.impl.SelectDataModelRowEvent;
 import org.nuxeo.ecm.webapp.documentsLists.DocumentsListsManager;
 import org.nuxeo.ecm.webapp.helpers.ResourcesAccessor;
-import org.nuxeo.ecm.webapp.pagination.ResultsProvidersCache;
-import org.nuxeo.runtime.api.Framework;
 
 /**
-* Seam bean used for Orderable documents.
-*
-* @author <a href="mailto:troger@nuxeo.com">Thomas Roger</a>
-*/
+ * Seam bean used for Orderable documents.
+ *
+ * @author <a href="mailto:troger@nuxeo.com">Thomas Roger</a>
+ */
 @Name("orderableDocumentActions")
 @Scope(ScopeType.CONVERSATION)
-public class OrderableDocumentActions implements SelectDataModelListener,
-        Serializable {
+public class OrderableDocumentActions implements Serializable {
 
     private static final Log log = LogFactory.getLog(OrderableDocumentActions.class);
-
-    public static final String CURRENT_DOC_ORDERED_CHILDREN_QM = "CURRENT_DOC_ORDERED_CHILDREN";
-
-    public static final String CHILDREN_DOCUMENT_LIST = "CHILDREN_DOCUMENT_LIST";
 
     public static final String SECTION_TYPE = "Section";
 
@@ -88,68 +65,15 @@ public class OrderableDocumentActions implements SelectDataModelListener,
     @In(required = false, create = true)
     protected transient DocumentsListsManager documentsListsManager;
 
-    @In(required = false, create = true)
-    protected transient ResultsProvidersCache resultsProvidersCache;
-
     @In(create = true, required = false)
     protected transient FacesMessages facesMessages;
 
     @In(create = true)
     protected transient ResourcesAccessor resourcesAccessor;
 
-    @Factory(value = "currentOrderedChildrenSelectModel", scope = EVENT)
-    public SelectDataModel getCurrentOrderedChildrenSelectModel()
-            throws ClientException {
-        DocumentModelList documents = getCurrentDocumentChildrenPage();
-        List<DocumentModel> selectedDocuments = documentsListsManager.getWorkingList(CURRENT_DOCUMENT_SELECTION);
-        SelectDataModel model = new SelectDataModelImpl(CHILDREN_DOCUMENT_LIST,
-                documents, selectedDocuments);
-        model.addSelectModelListener(this);
-        return model;
-    }
-
-    protected DocumentModelList getCurrentDocumentChildrenPage()
-            throws ClientException {
-        if (documentManager == null) {
-            log.error("documentManager not initialized");
-            return new DocumentModelListImpl();
-        }
-
-        PagedDocumentsProvider resultsProvider = getOrderedChildrenProvider();
-        return resultsProvider.getCurrentPage();
-    }
-
-    protected PagedDocumentsProvider getOrderedChildrenProvider()
-            throws ClientException {
-        return resultsProvidersCache.get(CURRENT_DOC_ORDERED_CHILDREN_QM);
-    }
-
-    @Factory(value = "sectionOrderedChildrenSelectModel", scope = EVENT)
-    public SelectDataModel getSectionOrderedChildrenSelectModel()
-            throws ClientException {
-        DocumentModelList documents = getCurrentDocumentChildrenPage();
-        List<DocumentModel> selectedDocuments = documentsListsManager.getWorkingList(CURRENT_DOCUMENT_SECTION_SELECTION);
-        SelectDataModel model = new SelectDataModelImpl(CHILDREN_DOCUMENT_LIST,
-                documents, selectedDocuments);
-        model.addSelectModelListener(this);
-        return model;
-    }
-
-    public void processSelectRowEvent(SelectDataModelRowEvent event) {
-        Boolean selection = event.getSelected();
-        DocumentModel data = (DocumentModel) event.getRowData();
-        if (selection) {
-            documentsListsManager.addToWorkingList(CURRENT_DOCUMENT_SELECTION,
-                    data);
-        } else {
-            documentsListsManager.removeFromWorkingList(
-                    CURRENT_DOCUMENT_SELECTION, data);
-        }
-    }
-
     /*
-* -------- Web Actions --------
-*/
+     * -------- Web Actions --------
+     */
 
     public boolean getCanMoveDown() throws ClientException {
         DocumentModel currentDocument = navigationContext.getCurrentDocument();
@@ -174,7 +98,8 @@ public class OrderableDocumentActions implements SelectDataModelListener,
         DocumentModel selectedDocument = docs.get(0);
         List<DocumentRef> children = documentManager.getChildrenRefs(
                 container.getRef(), null);
-        int selectedDocumentIndex = children.indexOf(new IdRef(selectedDocument.getId()));
+        int selectedDocumentIndex = children.indexOf(new IdRef(
+                selectedDocument.getId()));
         int nextIndex = selectedDocumentIndex + 1;
         if (nextIndex == children.size()) {
             // can't move down the last document
@@ -202,7 +127,8 @@ public class OrderableDocumentActions implements SelectDataModelListener,
 
         List<DocumentRef> children = documentManager.getChildrenRefs(
                 container.getRef(), null);
-        int selectedDocumentIndex = children.indexOf(new IdRef(selectedDocument.getId()));
+        int selectedDocumentIndex = children.indexOf(new IdRef(
+                selectedDocument.getId()));
         int nextIndex = selectedDocumentIndex + 1;
         DocumentRef nextDocumentRef = children.get(nextIndex);
 
@@ -245,8 +171,10 @@ public class OrderableDocumentActions implements SelectDataModelListener,
         }
 
         DocumentModel selectedDocument = docs.get(0);
-        List<DocumentRef> children = documentManager.getChildrenRefs(container.getRef(), null);
-        int selectedDocumentIndex = children.indexOf(new IdRef(selectedDocument.getId()));
+        List<DocumentRef> children = documentManager.getChildrenRefs(
+                container.getRef(), null);
+        int selectedDocumentIndex = children.indexOf(new IdRef(
+                selectedDocument.getId()));
         int previousIndex = selectedDocumentIndex - 1;
         if (previousIndex < 0) {
             // can't move up the first document
@@ -274,7 +202,8 @@ public class OrderableDocumentActions implements SelectDataModelListener,
 
         List<DocumentRef> children = documentManager.getChildrenRefs(
                 container.getRef(), null);
-        int selectedDocumentIndex = children.indexOf(new IdRef(selectedDocument.getId()));
+        int selectedDocumentIndex = children.indexOf(new IdRef(
+                selectedDocument.getId()));
         int previousIndex = selectedDocumentIndex - 1;
         DocumentRef previousDocumentRef = children.get(previousIndex);
 
@@ -311,7 +240,8 @@ public class OrderableDocumentActions implements SelectDataModelListener,
         DocumentModel selectedDocument = docs.get(0);
         List<DocumentRef> children = documentManager.getChildrenRefs(
                 container.getRef(), null);
-        int selectedDocumentIndex = children.indexOf(new IdRef(selectedDocument.getId()));
+        int selectedDocumentIndex = children.indexOf(new IdRef(
+                selectedDocument.getId()));
         if (selectedDocumentIndex <= 0) {
             // can't move to top the first document
             return false;
@@ -374,7 +304,8 @@ public class OrderableDocumentActions implements SelectDataModelListener,
         DocumentModel selectedDocument = docs.get(0);
         List<DocumentRef> children = documentManager.getChildrenRefs(
                 container.getRef(), null);
-        int selectedDocumentIndex = children.indexOf(new IdRef(selectedDocument.getId()));
+        int selectedDocumentIndex = children.indexOf(new IdRef(
+                selectedDocument.getId()));
         if (selectedDocumentIndex >= children.size() - 1) {
             // can't move to bottom the last document
             return false;
@@ -416,12 +347,6 @@ public class OrderableDocumentActions implements SelectDataModelListener,
     protected void addFacesMessage(String messageLabel) {
         facesMessages.add(StatusMessage.Severity.INFO,
                 resourcesAccessor.getMessages().get(messageLabel));
-    }
-
-    @Observer(value = { DOCUMENT_SELECTION_CHANGED, DOMAIN_SELECTION_CHANGED,
-            CONTENT_ROOT_SELECTION_CHANGED, DOCUMENT_CHILDREN_CHANGED }, create = false)
-    public void invalidateProviderOnDocumentChanged() {
-        resultsProvidersCache.invalidate(CURRENT_DOC_ORDERED_CHILDREN_QM);
     }
 
 }
