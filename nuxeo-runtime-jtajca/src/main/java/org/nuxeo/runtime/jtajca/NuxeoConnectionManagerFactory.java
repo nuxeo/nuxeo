adde540aeb851f6579655570cecb2b47fbdb4d57
Stephane Lacoin <slacoin@nuxeo.com>
2013-10-14 20:28:18 +0200

    NXP-12398 fixing copyrights

--- nuxeo-runtime-jtajca/src/main/java/org/nuxeo/runtime/jtajca/NuxeoConnectionManagerFactory.java
+++ nuxeo-runtime-jtajca/src/main/java/org/nuxeo/runtime/jtajca/NuxeoConnectionManagerFactory.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2006-2011 Nuxeo SA (http://nuxeo.com/) and others.
+ * Copyright (c) 2006-2012 Nuxeo SA (http://nuxeo.com/) and others.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
