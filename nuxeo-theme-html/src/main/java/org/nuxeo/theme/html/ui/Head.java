0e8e22497909249f5caec8f0ed01ee620d1a6827
Guillaume Renard <grenard@nuxeo.com>
2013-10-16 18:23:58 +0200

    NXP-12862: remove meta since it is in the header of the response

--- nuxeo-theme-html/src/main/java/org/nuxeo/theme/html/ui/Head.java
+++ nuxeo-theme-html/src/main/java/org/nuxeo/theme/html/ui/Head.java
@@ -63,7 +63,6 @@ public class Head {
         if (baseUrl != null) {
             sb.append(String.format("<base href=\"%s\" />", baseUrl));
         }
-        sb.append("<meta http-equiv=\"X-UA-Compatible\" content=\"chrome=1\">");
 
         return sb.toString();
     }
