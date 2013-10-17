0542b0920faf70d018ea20afb435974b5c06bf93
Guillaume Renard <grenard@nuxeo.com>
2013-10-17 09:28:39 +0200

    NXP-12742: add copyright header + change since version

--- nuxeo-functional-tests/src/main/java/org/nuxeo/functionaltests/LogTestWatchman.java
+++ nuxeo-functional-tests/src/main/java/org/nuxeo/functionaltests/LogTestWatchman.java
@@ -1,3 +1,20 @@
+/*
+ * (C) Copyright 2013 Nuxeo SA (http://nuxeo.com/) and contributors.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser General Public License
+ * (LGPL) version 2.1 which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl.html
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * Contributors:
+ *     <a href="mailto:grenard@nuxeo.com">Guillaume</a>
+ */
+
 package org.nuxeo.functionaltests;
 
 import java.io.File;
@@ -22,7 +39,7 @@ import org.openqa.selenium.remote.RemoteWebDriver;
 /**
  * Watchman to log info about the test and create snapshot on failure.
  *
- * @since 5.7
+ * @since 5.8
  */
 public class LogTestWatchman extends TestWatchman {
 
