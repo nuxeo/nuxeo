627190e8d57f528bde3ed00d8d1858175f681fb8
Mariana Cedica <mcedica@nuxeo.com>
2013-10-21 18:26:49 +0200

    NXP-12908: TasksInfoWrapper object exposed in the automation context should be a iterable collection

--- nuxeo-platform-document-routing-core/src/main/java/org/nuxeo/ecm/platform/routing/core/api/TasksInfoWrapper.java
+++ nuxeo-platform-document-routing-core/src/main/java/org/nuxeo/ecm/platform/routing/core/api/TasksInfoWrapper.java
@@ -17,9 +17,14 @@
 package org.nuxeo.ecm.platform.routing.core.api;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
 import java.util.List;
+import java.util.ListIterator;
 
 import org.nuxeo.ecm.platform.routing.core.impl.GraphNode;
+import org.nuxeo.ecm.platform.routing.core.impl.GraphNode.TaskInfo;
 
 /**
  * Wraps the list of {@link GraphNode.TaskInfo} on a {@link GraphNode} to expose
@@ -27,11 +32,15 @@ import org.nuxeo.ecm.platform.routing.core.impl.GraphNode;
  *
  * @since 5.7.3
  */
-public class TasksInfoWrapper implements Serializable {
+public class TasksInfoWrapper implements List<GraphNode.TaskInfo>, Serializable {
 
     private static final long serialVersionUID = 1L;
 
     protected List<GraphNode.TaskInfo> tasks;
+
+    public TasksInfoWrapper() {
+        tasks = new ArrayList<GraphNode.TaskInfo>();
+    }
 
     public TasksInfoWrapper(List<GraphNode.TaskInfo> tasks) {
         this.tasks = tasks;
@@ -46,5 +55,122 @@ public class TasksInfoWrapper implements Serializable {
             }
         }
         return noEndedWithStatus;
+    }
+
+    @Override
+    public int size() {
+        return tasks.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return tasks.isEmpty();
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return tasks.contains(o);
+    }
+
+    @Override
+    public Iterator<TaskInfo> iterator() {
+        return tasks.iterator();
+    }
+
+    @Override
+    public Object[] toArray() {
+        return tasks.toArray();
+    }
+
+    @Override
+    public <T> T[] toArray(T[] a) {
+        return tasks.toArray(a);
+    }
+
+    @Override
+    public boolean add(TaskInfo e) {
+        return tasks.add(e);
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        return tasks.remove(o);
+    }
+
+    @Override
+    public boolean containsAll(Collection<?> c) {
+        return tasks.containsAll(c);
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends TaskInfo> c) {
+        return tasks.addAll(c);
+    }
+
+    @Override
+    public boolean addAll(int index, Collection<? extends TaskInfo> c) {
+        return tasks.addAll(index, c);
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> c) {
+        return tasks.removeAll(c);
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> c) {
+        return tasks.retainAll(c);
+    }
+
+    @Override
+    public void clear() {
+        tasks.clear();
+
+    }
+
+    @Override
+    public TaskInfo get(int index) {
+        return tasks.get(index);
+    }
+
+    @Override
+    public TaskInfo set(int index, TaskInfo element) {
+        return tasks.set(index, element);
+    }
+
+    @Override
+    public void add(int index, TaskInfo element) {
+        tasks.add(index, element);
+
+    }
+
+    @Override
+    public TaskInfo remove(int index) {
+        return tasks.remove(index);
+    }
+
+    @Override
+    public int indexOf(Object o) {
+        return tasks.indexOf(o);
+    }
+
+    @Override
+    public int lastIndexOf(Object o) {
+        return tasks.lastIndexOf(o);
+    }
+
+    @Override
+    public ListIterator<TaskInfo> listIterator() {
+        return tasks.listIterator();
+    }
+
+    @Override
+    public ListIterator<TaskInfo> listIterator(int index) {
+        return tasks.listIterator(index);
+    }
+
+    @Override
+    public List<TaskInfo> subList(int fromIndex, int toIndex) {
+        return tasks.subList(fromIndex, toIndex);
     }
 }
