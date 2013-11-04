832fa23e2a0dede4253637e1f64a05ec5b5b0fe4
Damien Metzler <dmetzler@gmail.com>
2013-11-04 12:32:00 +0100

    NXP-13076 Reencode URI before dispatching request

--- nuxeo-rest-api-server/src/main/java/org/nuxeo/ecm/restapi/server/APIServlet.java
+++ nuxeo-rest-api-server/src/main/java/org/nuxeo/ecm/restapi/server/APIServlet.java
@@ -17,12 +17,13 @@
 package org.nuxeo.ecm.restapi.server;
 
 import java.io.IOException;
-
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.nuxeo.common.utils.URIUtils;
 
 /**
  * This servlet is bound to /api and dispatch calls to /site/api in oder to have
@@ -40,8 +41,9 @@ public class APIServlet extends HttpServlet {
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
+
         RequestDispatcher rd = req.getRequestDispatcher("/site/api"
-                + req.getPathInfo());
+                + URIUtils.quoteURIPathComponent(req.getPathInfo(), false, false));
         rd.forward(req, resp);
     }
 
