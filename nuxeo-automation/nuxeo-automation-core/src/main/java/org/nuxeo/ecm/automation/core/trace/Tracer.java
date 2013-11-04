49117c7343b3f11d0aa89259f62217fb5d7675f6
Vladimir Pasquier <vpasquier@nuxeo.com>
2013-11-04 12:38:02 +0100

    NXP-13072: fix automation trace when mode is not activated - refactor tracer structure

--- nuxeo-automation/nuxeo-automation-core/src/main/java/org/nuxeo/ecm/automation/core/trace/Tracer.java
+++ nuxeo-automation/nuxeo-automation-core/src/main/java/org/nuxeo/ecm/automation/core/trace/Tracer.java
@@ -18,13 +18,9 @@
 
 package org.nuxeo.ecm.automation.core.trace;
 
-import java.util.LinkedList;
 import java.util.Map;
-import java.util.Stack;
 
-import org.nuxeo.ecm.automation.OperationCallback;
 import org.nuxeo.ecm.automation.OperationContext;
-import org.nuxeo.ecm.automation.OperationException;
 import org.nuxeo.ecm.automation.OperationType;
 import org.nuxeo.ecm.automation.core.impl.ChainTypeImpl;
 import org.nuxeo.ecm.automation.core.impl.InvokableMethod;
@@ -35,70 +31,10 @@ import org.nuxeo.ecm.automation.core.impl.InvokableMethod;
  *
  * @since 5.7.3
  */
-public class Tracer implements OperationCallback {
-
-    protected final TracerFactory factory;
-
-    protected final LinkedList<Call> calls = new LinkedList<Call>();
-
-    protected Stack<Trace> callingStacks = new Stack<Trace>();
-
-    protected Call parent;
-
-    protected OperationType chain;
-
-    protected Trace trace;
-
-    protected String factoryIndex;
-
-    protected Boolean printable;
+public class Tracer extends BasedTracer {
 
     protected Tracer(TracerFactory factory, Boolean printable) {
-        this.factory = factory;
-        this.printable = printable;
-    }
-
-    protected void pushContext(OperationType newChain) {
-        if (chain != null) {
-            callingStacks.push(new Trace(parent, chain, calls));
-            parent = calls.getLast();
-            calls.clear();
-        }
-        chain = newChain;
-    }
-
-    protected void popContext() {
-        calls.clear();
-        if (callingStacks.isEmpty()) {
-            parent = null;
-            chain = null;
-            return;
-        }
-        Trace trace = callingStacks.pop();
-        parent = trace.parent;
-        chain = trace.chain;
-        calls.addAll(trace.operations);
-    }
-
-    protected void saveTrace(Trace popped) {
-        if (parent == null) {
-            trace = popped;
-            chain = null;
-            calls.clear();
-            factory.onTrace(popped);
-        } else {
-            parent.nested.add(popped);
-            popContext();
-        }
-    }
-
-    public String getFactoryIndex() {
-        return factoryIndex;
-    }
-
-    @Override
-    public void onChain(OperationType chain) {
-        pushContext(chain);
+        super(factory, printable);
     }
 
     @Override
@@ -112,23 +48,4 @@ public class Tracer implements OperationCallback {
         calls.add(call);
     }
 
-    @Override
-    public void onOutput(Object output) {
-        saveTrace(new Trace(parent, chain, calls, output));
-    }
-
-    @Override
-    public void onError(OperationException error) {
-        saveTrace(new Trace(parent, chain, calls, error));
-    }
-
-    @Override
-    public Trace getTrace() {
-        return trace;
-    }
-
-    @Override
-    public String getFormattedText() {
-        return printable ? trace.getFormattedText() : "";
-    }
 }
