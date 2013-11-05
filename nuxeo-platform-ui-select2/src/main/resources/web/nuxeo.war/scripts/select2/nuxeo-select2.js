7aa1d6d3b292c0e7b238a754a1de63b520e2a129
Guillaume Renard <grenard@nuxeo.com>
2013-11-05 14:45:25 +0100

    NXP-12787: change way of registering safeEdit postRestore callback for select2

--- nuxeo-platform-ui-select2/src/main/resources/web/nuxeo.war/scripts/select2/nuxeo-select2.js
+++ nuxeo-platform-ui-select2/src/main/resources/web/nuxeo.war/scripts/select2/nuxeo-select2.js
@@ -376,15 +376,13 @@
   window.initSelect2Widgets = function initSelect2Widgets() {
     jQuery("input[type='hidden'][id$=select2]").each(function(idx, el) {
       initSelect2Widget(jQuery(el));
+      if (jQuery().initSafeEdit) {
+        // Register postRestore function for safeEdit
+        jQuery(el).closest("form").registerPostRestoreCallBacks(function(data) {
+          initSelect2Widget(jQuery(el));
+        });
+      }
     });
-    if (typeof registerPostRestoreCallBacks == 'function') {
-      registerPostRestoreCallBacks(function(formSelector, data) {
-        jQuery(formSelector).find("input[type='hidden'][id$=select2]").each(
-            function(idx, el) {
-              initSelect2Widget(jQuery(el));
-            });
-      });
-    }
   }
 
 })();
