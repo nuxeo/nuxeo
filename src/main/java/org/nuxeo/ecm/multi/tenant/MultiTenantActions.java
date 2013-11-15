03bb66ad9a8b086b9cfd4bf8da0bc863c68aec7e
Thomas Roger <troger@nuxeo.com>
2013-11-15 18:39:39 +0100

    NXP-13029: add validator to 'tenant_administrators_widget.xhtml' template to avoid the current user to remove himself from the tenant administrators; use the new template for the 'tenant_administrators' widget

--- src/main/java/org/nuxeo/ecm/multi/tenant/MultiTenantActions.java
+++ src/main/java/org/nuxeo/ecm/multi/tenant/MultiTenantActions.java
@@ -19,10 +19,12 @@ package org.nuxeo.ecm.multi.tenant;
 
 import static org.jboss.seam.ScopeType.STATELESS;
 import static org.jboss.seam.annotations.Install.FRAMEWORK;
+import static org.nuxeo.ecm.multi.tenant.Constants.TENANT_ID_PROPERTY;
 
 import java.io.Serializable;
 import java.util.List;
 
+import org.apache.commons.lang.StringUtils;
 import org.jboss.seam.annotations.In;
 import org.jboss.seam.annotations.Install;
 import org.jboss.seam.annotations.Name;
@@ -30,10 +32,18 @@ import org.jboss.seam.annotations.Scope;
 import org.nuxeo.ecm.core.api.ClientException;
 import org.nuxeo.ecm.core.api.CoreSession;
 import org.nuxeo.ecm.core.api.DocumentModel;
+import org.nuxeo.ecm.core.api.NuxeoPrincipal;
 import org.nuxeo.ecm.directory.DirectoryException;
 import org.nuxeo.ecm.directory.api.DirectoryService;
+import org.nuxeo.ecm.platform.ui.web.api.NavigationContext;
+import org.nuxeo.ecm.platform.ui.web.util.ComponentUtils;
 import org.nuxeo.ecm.webapp.directory.DirectoryUIActionsBean;
 import org.nuxeo.runtime.api.Framework;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.ValidatorException;
 
 /**
  * @author <a href="mailto:troger@nuxeo.com">Thomas Roger</a>
@@ -46,8 +56,13 @@ public class MultiTenantActions implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
+    public static final String TENANT_ADMINISTRATORS_VALIDATION_ERROR = "label.tenant.administrators.validation.error";
+
     @In(create = true)
     protected transient CoreSession documentManager;
+
+    @In(create = true)
+    protected NavigationContext navigationContext;
 
     @In(create = true)
     protected DirectoryUIActionsBean directoryUIActions;
@@ -82,6 +97,30 @@ public class MultiTenantActions implements Serializable {
             }
         }
         return directoryUIActions.isReadOnly(directoryName);
+    }
+
+    @SuppressWarnings("unchecked")
+    public void validateTenantAdministrators(FacesContext context,
+            UIComponent component, Object value) throws ClientException {
+        DocumentModel currentDocument = navigationContext.getCurrentDocument();
+        String currentDocumentTenantId = (String) currentDocument.getPropertyValue(TENANT_ID_PROPERTY);
+        NuxeoPrincipal currentUser = (NuxeoPrincipal) documentManager.getPrincipal();
+        String currentUserTenantId = currentUser.getTenantId();
+        if (!StringUtils.isBlank(currentDocumentTenantId)
+                && !StringUtils.isBlank(currentUserTenantId)
+                && currentUserTenantId.equals(currentDocumentTenantId)) {
+            String administratorGroup = MultiTenantHelper.computeTenantAdministratorsGroup(currentDocumentTenantId);
+            if (currentUser.isMemberOf(administratorGroup)) {
+                List<String> users = (List<String>) value;
+                if (!users.contains(currentUser.getName())) {
+                    FacesMessage message = new FacesMessage(
+                            FacesMessage.SEVERITY_ERROR,
+                            ComponentUtils.translate(context,
+                                    TENANT_ADMINISTRATORS_VALIDATION_ERROR), null);
+                    throw new ValidatorException(message);
+                }
+            }
+        }
     }
 
 }
