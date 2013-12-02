ddee97fc618131dbd6007fc481bfc39c8dbd95ef
Julien Carsique <jcarsique@nuxeo.com>
2013-12-02 14:40:40 +0100

    NXP-13313: cleanup and format

--- clone.py
+++ clone.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 ##
-## (C) Copyright 2011 Nuxeo SA (http://nuxeo.com/) and contributors.
+## (C) Copyright 2011-2013 Nuxeo SA (http://nuxeo.com/) and contributors.
 ##
 ## All rights reserved. This program and the accompanying materials
 ## are made available under the terms of the GNU Lesser General Public License
 ## (LGPL) version 2.1 which accompanies this distribution, and is available at
-## http://www.gnu.org/licenses/lgpl.html
+## http://www.gnu.org/licenses/lgpl-2.1.html
 ##
 ## This library is distributed in the hope that it will be useful,
 ## but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -16,7 +16,7 @@
 ##     Stefane Fermigier
 ##     Julien Carsique
 ##
-## This script clones or updates Nuxeo addons source code from Git repositories.
+## This script clones or updates Nuxeo addons source code from Git repositories
 ##
 import sys
 import os
@@ -27,6 +27,7 @@ import platform
 import time
 import optparse
 
+#pylint: disable=C0103
 driveletter = None
 basedir = os.getcwd()
 
@@ -36,16 +37,19 @@ def log(message, out=sys.stdout):
     out.flush()
 
 
+#pylint: disable=C0103
 def system(cmd, failonerror=True):
     log("$> " + cmd)
-    args = shlex.split(cmd)
-    p = subprocess.Popen(args, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-    out, err = p.communicate()
+    cmdargs = shlex.split(cmd)
+    p = subprocess.Popen(cmdargs, stdin=subprocess.PIPE,
+                         stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    out, err = p.communicate()  # @UnusedVariable
     sys.stdout.write(out)
     sys.stdout.flush()
     retcode = p.returncode
     if retcode != 0:
-        log("[ERROR]: command returned non-zero exit code: %s" % cmd, sys.stderr)
+        log("[ERROR]: command returned non-zero exit code: %s" % cmd,
+            sys.stderr)
         if failonerror:
             long_path_workaround_cleanup()
             sys.exit(retcode)
@@ -62,14 +66,15 @@ def system_with_retries(cmd, failonerror=True):
         elif retries > 10:
             return system(cmd, failonerror)
         else:
-            log("Error executing %s - retrying in 10 seconds..." % cmd, sys.stderr)
+            log("Error executing %s - retrying in 10 seconds..." % cmd,
+                sys.stderr)
             time.sleep(10)
 
 
 def long_path_workaround_init():
     global driveletter
     global basedir
-    # On Windows, try to map the current directory to an unused drive letter to shorten path names
+    # map the current directory to an unused drive letter to shorten path names
     if platform.system() != "Windows" or len(basedir) < 20:
         return
     for letter in "GHIJKLMNOPQRSTUVWXYZ":
@@ -133,7 +138,8 @@ def get_current_version():
 def assert_git_config():
     t = check_output(["git", "config", "--get", "color.branch"])
     if "always" in t:
-        log("[ERROR]: The git color mode should be auto not always, try:", sys.stderr)
+        log("[ERROR]: The git color mode should be auto not always, try:",
+            sys.stderr)
         log(" git config --global color.branch auto", sys.stderr)
         log(" git config --global color.status auto", sys.stderr)
         sys.exit(1)
@@ -196,7 +202,8 @@ log("Using maven introspection of the pom.xml files"
     " to find the list of addons")
 all_lines = os.popen("mvn -N help:effective-pom").readlines()
 if with_optionals:
-    all_lines += os.popen("mvn -N help:effective-pom -f pom-optionals.xml").readlines()
+    all_lines += os.popen("mvn -N help:effective-pom -f pom-optionals.xml").\
+        readlines()
 
 for line in all_lines:
     line = line.strip()
