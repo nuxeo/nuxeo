7fc0c88ee3bf184d13bbcf4de0e2488ede83ad1e
Stephane Lacoin aka nxmatic <slacoin@nuxeo.com>
2013-12-16 11:21:32 +0100

    NXP-13409 regression in duplicate name detection
    This reverts commit 31be9fa1b90b3d5dbb4bed6597a4c7e6332e9402.

--- nuxeo-core/src/main/java/org/nuxeo/ecm/core/model/DuplicatedNameFixer.java
+++ nuxeo-core/src/main/java/org/nuxeo/ecm/core/model/DuplicatedNameFixer.java
@@ -13,10 +13,6 @@
 package org.nuxeo.ecm.core.model;
 
 import org.nuxeo.ecm.core.api.ClientException;
-import org.nuxeo.ecm.core.api.CoreSession;
-import org.nuxeo.ecm.core.api.DocumentModel;
-import org.nuxeo.ecm.core.api.DocumentRef;
-import org.nuxeo.ecm.core.api.PathRef;
 import org.nuxeo.ecm.core.api.event.CoreEventConstants;
 import org.nuxeo.ecm.core.event.Event;
 import org.nuxeo.ecm.core.event.EventListener;
@@ -27,22 +23,16 @@ import org.nuxeo.ecm.core.event.impl.DocumentEventContext;
  */
 public class DuplicatedNameFixer implements EventListener {
 
+
     @Override
     public void handleEvent(Event event) throws ClientException {
         DocumentEventContext context = (DocumentEventContext) event.getContext();
-        CoreSession session = context.getCoreSession();
-        DocumentRef parentRef = (DocumentRef) context.getProperty(
-                CoreEventConstants.DESTINATION_REF);
-        if (parentRef == null) {
+        Boolean destinationExists = (Boolean)context.getProperty(CoreEventConstants.DESTINATION_EXISTS);
+        if (!destinationExists) {
             return;
         }
         String name = (String)context.getProperty(CoreEventConstants.DESTINATION_NAME);
-        DocumentModel parent = session.getDocument(parentRef);
-        String parentPath = parent.getPathAsString();
-        PathRef path = new PathRef(parentPath, name);
-        if (session.exists(path)) {
-            name += '.' + String.valueOf(System.currentTimeMillis());
-        }
+        name += '.' + String.valueOf(System.currentTimeMillis());
         context.setProperty(CoreEventConstants.DESTINATION_NAME, name);
     }
 
