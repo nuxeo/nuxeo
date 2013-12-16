ca491c1cf76a64a917cdadaacec78792c2c4e7fd
Julien Carsique <jcarsique@nuxeo.com>
2013-12-16 12:06:16 +0100

    NXP-13426: Apply Git Shell scripts on any directory

--- scripts/gitf.bat
+++ scripts/gitf.bat
@@ -1,12 +1,12 @@
 @echo off
 REM  Convenient functions for use on Nuxeo projects version controlled under Git
 REM
-REM  (C) Copyright 2010 Nuxeo SAS (http://nuxeo.com/) and contributors.
+REM  (C) Copyright 2010-2013 Nuxeo SA (http://nuxeo.com/) and contributors.
 REM
 REM  All rights reserved. This program and the accompanying materials
 REM  are made available under the terms of the GNU Lesser General Public License
 REM  (LGPL) version 2.1 which accompanies this distribution, and is available at
-REM  http://www.gnu.org/licenses/lgpl.html
+REM  http://www.gnu.org/licenses/lgpl-2.1.html
 REM
 REM  This library is distributed in the hope that it will be useful,
 REM  but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -16,13 +16,12 @@ REM
 REM  Contributors:
 REM    Julien Carsique
 REM
-REM  $Id$
 
 set PWD=%CD%
 echo [.]
 git %*
 
-for /d %%D in (nuxeo-*) do (
+for /d %%D in (*) do (
 echo [%%D]
 cd %PWD%\%%D
 git %*
--- scripts/gitfunctions.sh
+++ scripts/gitfunctions.sh
@@ -2,12 +2,12 @@
 #
 # Convenient functions for use on Nuxeo projects version controlled under Git.
 #
-# (C) Copyright 2009-2012 Nuxeo SAS (http://nuxeo.com/) and contributors.
+# (C) Copyright 2009-2013 Nuxeo SA (http://nuxeo.com/) and contributors.
 #
 # All rights reserved. This program and the accompanying materials
 # are made available under the terms of the GNU Lesser General Public License
 # (LGPL) version 2.1 which accompanies this distribution, and is available at
-# http://www.gnu.org/licenses/lgpl.html
+# http://www.gnu.org/licenses/lgpl-2.1.html
 #
 # This library is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -20,7 +20,7 @@
 
 # usage: "gitf [git instructions]"
 gitf() {
-  for dir in . nuxeo-*; do
+  for dir in . *; do
     if [ -d "$dir"/.git ]; then
       echo "[$dir]"
       (cd "$dir" ; git "$@")
