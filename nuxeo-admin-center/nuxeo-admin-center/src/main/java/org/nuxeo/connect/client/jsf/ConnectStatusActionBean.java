e054ced503c590df6b173c03d840263bb27a236d
Guillaume Renard <grenard@nuxeo.com>
2013-12-19 15:24:56 +0100

    NXP-13444: correct @since

--- nuxeo-admin-center/nuxeo-admin-center/src/main/java/org/nuxeo/connect/client/jsf/ConnectStatusActionBean.java
+++ nuxeo-admin-center/nuxeo-admin-center/src/main/java/org/nuxeo/connect/client/jsf/ConnectStatusActionBean.java
@@ -385,7 +385,7 @@ public class ConnectStatusActionBean implements Serializable {
     }
 
     /**
-     * @since 5.9.1
+     * @since 5.9.2
      */
     @Factory(scope = ScopeType.APPLICATION, value = "connectBannerEnabled")
     public boolean isConnectBannerEnabled() {
