125510a40984d0f4f3c3f97c7419e2cf89981262
Florent Guillaume <fg@nuxeo.com>
2013-12-26 23:06:20 +0100

    NXP-13403: split upgrade tests into separate class

--- nuxeo-core-storage-sql/nuxeo-core-storage-sql-test/src/test/java/org/nuxeo/ecm/core/storage/sql/TestSQLBackend.java
+++ nuxeo-core-storage-sql/nuxeo-core-storage-sql-test/src/test/java/org/nuxeo/ecm/core/storage/sql/TestSQLBackend.java
@@ -70,7 +70,6 @@ import org.nuxeo.ecm.core.storage.sql.jdbc.ClusterNodeHandler;
 import org.nuxeo.ecm.core.storage.sql.jdbc.JDBCBackend;
 import org.nuxeo.ecm.core.storage.sql.jdbc.JDBCConnection;
 import org.nuxeo.ecm.core.storage.sql.jdbc.JDBCConnectionPropagator;
-import org.nuxeo.ecm.core.storage.sql.jdbc.JDBCMapper;
 import org.nuxeo.ecm.core.storage.sql.jdbc.JDBCRowMapper;
 import org.nuxeo.ecm.core.storage.sql.jdbc.dialect.Dialect;
 import org.nuxeo.runtime.api.Framework;
@@ -379,24 +378,6 @@ public class TestSQLBackend extends SQLBackendTestCase {
         // delete the node
         // session.removeNode(nodea);
         // session.save();
-    }
-
-    @Test
-    public void testBasicsUpgrade() throws Exception {
-        if ("sequence".equals(DatabaseHelper.DEF_ID_TYPE)) {
-            return;
-        }
-        JDBCMapper.testProps.put(JDBCMapper.TEST_UPGRADE, Boolean.TRUE);
-        JDBCMapper.testProps.put(JDBCMapper.TEST_UPGRADE_VERSIONS,
-                Boolean.FALSE);
-        JDBCMapper.testProps.put(JDBCMapper.TEST_UPGRADE_LAST_CONTRIBUTOR,
-                Boolean.FALSE);
-        JDBCMapper.testProps.put(JDBCMapper.TEST_UPGRADE_LOCKS, Boolean.FALSE);
-        try {
-            testBasics();
-        } finally {
-            JDBCMapper.testProps.clear();
-        }
     }
 
     @Test
@@ -2747,109 +2728,6 @@ public class TestSQLBackend extends SQLBackendTestCase {
         assertEquals(1, res.list.size());
     }
 
-    protected static boolean isLatestVersion(Node node) throws Exception {
-        Boolean b = (Boolean) node.getSimpleProperty(
-                Model.VERSION_IS_LATEST_PROP).getValue();
-        return b.booleanValue();
-    }
-
-    protected static boolean isLatestMajorVersion(Node node) throws Exception {
-        Boolean b = (Boolean) node.getSimpleProperty(
-                Model.VERSION_IS_LATEST_MAJOR_PROP).getValue();
-        return b.booleanValue();
-    }
-
-    protected static String getVersionLabel(Node node) throws Exception {
-        return (String) node.getSimpleProperty(Model.VERSION_LABEL_PROP).getValue();
-    }
-
-    @Test
-    public void testVersionsUpgrade() throws Exception {
-        if ("sequence".equals(DatabaseHelper.DEF_ID_TYPE)) {
-            return;
-        }
-        JDBCMapper.testProps.put(JDBCMapper.TEST_UPGRADE, Boolean.TRUE);
-        JDBCMapper.testProps.put(JDBCMapper.TEST_UPGRADE_VERSIONS, Boolean.TRUE);
-        JDBCMapper.testProps.put(JDBCMapper.TEST_UPGRADE_LAST_CONTRIBUTOR,
-                Boolean.FALSE);
-        JDBCMapper.testProps.put(JDBCMapper.TEST_UPGRADE_LOCKS, Boolean.FALSE);
-        try {
-            Node ver;
-            Session session = repository.getConnection();
-
-            // check normal doc is not a version
-            Node doc = session.getNodeById("dddddddd-dddd-dddd-dddd-dddddddddddd");
-            assertFalse(doc.isVersion());
-
-            // v 1
-            ver = session.getNodeById("11111111-0000-0000-2222-000000000000");
-            assertTrue(ver.isVersion());
-            assertFalse(isLatestVersion(ver));
-            assertFalse(isLatestMajorVersion(ver));
-            assertEquals("0.1", getVersionLabel(ver));
-
-            // v 2
-            ver = session.getNodeById("11111111-0000-0000-2222-000000000001");
-            assertTrue(ver.isVersion());
-            assertFalse(isLatestVersion(ver));
-            assertTrue(isLatestMajorVersion(ver));
-            assertEquals("1.0", getVersionLabel(ver));
-
-            // v 3
-            ver = session.getNodeById("11111111-0000-0000-2222-000000000002");
-            assertTrue(ver.isVersion());
-            assertTrue(isLatestVersion(ver));
-            assertFalse(isLatestMajorVersion(ver));
-            assertEquals("1.1", getVersionLabel(ver));
-
-            // v 4 other doc
-            ver = session.getNodeById("11111111-0000-0000-3333-000000000001");
-            assertTrue(ver.isVersion());
-            assertTrue(isLatestVersion(ver));
-            assertTrue(isLatestMajorVersion(ver));
-            assertEquals("1.0", getVersionLabel(ver));
-
-        } finally {
-            JDBCMapper.testProps.clear();
-        }
-    }
-
-    @Test
-    public void testLastContributorUpgrade() throws StorageException {
-        if ("sequence".equals(DatabaseHelper.DEF_ID_TYPE)) {
-            return;
-        }
-        JDBCMapper.testProps.put(JDBCMapper.TEST_UPGRADE, Boolean.TRUE);
-        JDBCMapper.testProps.put(JDBCMapper.TEST_UPGRADE_LAST_CONTRIBUTOR,
-                Boolean.TRUE);
-        JDBCMapper.testProps.put(JDBCMapper.TEST_UPGRADE_VERSIONS,
-                Boolean.FALSE);
-        JDBCMapper.testProps.put(JDBCMapper.TEST_UPGRADE_LOCKS, Boolean.FALSE);
-
-        try {
-            Node ver;
-            Session session = repository.getConnection();
-
-            ver = session.getNodeById("12121212-dddd-dddd-dddd-000000000000");
-            assertNotNull(ver);
-            assertEquals("mynddoc", ver.getName());
-            assertEquals("Administrator",
-                    ver.getSimpleProperty("dc:creator").getString());
-            assertEquals("Administrator",
-                    ver.getSimpleProperty("dc:lastContributor").getString());
-
-            ver = session.getNodeById("12121212-dddd-dddd-dddd-000000000001");
-            assertNotNull(ver);
-            assertEquals("myrddoc", ver.getName());
-            assertEquals("Administrator",
-                    ver.getSimpleProperty("dc:creator").getString());
-            assertEquals("FakeOne",
-                    ver.getSimpleProperty("dc:lastContributor").getString());
-        } finally {
-            JDBCMapper.testProps.clear();
-        }
-    }
-
     @Test
     public void testMixinAPI() throws Exception {
         PartialList<Serializable> res;
@@ -3293,44 +3171,6 @@ public class TestSQLBackend extends SQLBackendTestCase {
             } while (System.currentTimeMillis() - start < waitMillis);
             session.close();
         }
-    }
-
-    @Test
-    public void testLocksUpgrade() throws Exception {
-        if ("sequence".equals(DatabaseHelper.DEF_ID_TYPE)) {
-            return;
-        }
-        JDBCMapper.testProps.put(JDBCMapper.TEST_UPGRADE, Boolean.TRUE);
-        JDBCMapper.testProps.put(JDBCMapper.TEST_UPGRADE_LOCKS, Boolean.TRUE);
-        JDBCMapper.testProps.put(JDBCMapper.TEST_UPGRADE_VERSIONS,
-                Boolean.FALSE);
-        JDBCMapper.testProps.put(JDBCMapper.TEST_UPGRADE_LAST_CONTRIBUTOR,
-                Boolean.FALSE);
-        try {
-            doTestLocksUpgrade();
-        } finally {
-            JDBCMapper.testProps.clear();
-        }
-    }
-
-    protected void doTestLocksUpgrade() throws Exception {
-        Session session = repository.getConnection();
-        String id;
-        Lock lock;
-
-        // check lock has been upgraded from 'bob:Jan 26, 2011'
-        id = "dddddddd-dddd-dddd-dddd-dddddddddddd";
-        lock = session.getLock(id);
-        assertNotNull(lock);
-        assertEquals("bob", lock.getOwner());
-        Calendar expected = new GregorianCalendar(2011, Calendar.JANUARY, 26,
-                0, 0, 0);
-        assertEquals(expected, lock.getCreated());
-
-        // old lock was nulled after unlock
-        id = "11111111-2222-3333-4444-555555555555";
-        lock = session.getLock(id);
-        assertNull(lock);
     }
 
     @Test
