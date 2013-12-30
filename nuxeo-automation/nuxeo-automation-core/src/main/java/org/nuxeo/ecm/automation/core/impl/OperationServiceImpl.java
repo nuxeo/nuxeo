58f057d475aed26ba655c13fe7bd204741d8a4ab
Stephane Lacoin aka nxmatic <slacoin@nuxeo.com>
2013-12-30 16:04:18 +0100

    NXP-13463 add remove operation from type onto service API

--- nuxeo-automation/nuxeo-automation-core/src/main/java/org/nuxeo/ecm/automation/core/impl/OperationServiceImpl.java
+++ nuxeo-automation/nuxeo-automation-core/src/main/java/org/nuxeo/ecm/automation/core/impl/OperationServiceImpl.java
@@ -346,6 +346,7 @@ public class OperationServiceImpl implements AutomationService {
         removeOperation(type);
     }
 
+    @Override
     public synchronized void removeOperation(OperationType type) {
         operations.removeContribution(type);
     }
