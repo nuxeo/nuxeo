62fbe40c1dc150b9ea3d94354f9666adb127bae8
Florent Guillaume <fg@nuxeo.com>
2014-02-03 15:35:26 +0100

    NXP-12947: cleanup unrelated test (avoid spurious error output)

--- nuxeo-automation/nuxeo-automation-core/src/test/java/org/nuxeo/ecm/automation/core/test/OperationsEncodingTest.java
+++ nuxeo-automation/nuxeo-automation-core/src/test/java/org/nuxeo/ecm/automation/core/test/OperationsEncodingTest.java
@@ -14,18 +14,15 @@ package org.nuxeo.ecm.automation.core.test;
 import static org.junit.Assert.assertEquals;
 
 import java.util.List;
-import org.junit.Before;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.nuxeo.ecm.automation.AutomationService;
 import org.nuxeo.ecm.automation.core.events.EventHandler;
 import org.nuxeo.ecm.automation.core.events.EventHandlerRegistry;
 import org.nuxeo.ecm.core.api.CoreSession;
-import org.nuxeo.ecm.core.api.DocumentModel;
 import org.nuxeo.ecm.core.test.CoreFeature;
 import org.nuxeo.ecm.core.test.annotations.Granularity;
 import org.nuxeo.ecm.core.test.annotations.RepositoryConfig;
-import org.nuxeo.runtime.api.Framework;
 import org.nuxeo.runtime.test.runner.Deploy;
 import org.nuxeo.runtime.test.runner.Features;
 import org.nuxeo.runtime.test.runner.FeaturesRunner;
@@ -33,9 +30,6 @@ import org.nuxeo.runtime.test.runner.LocalDeploy;
 
 import com.google.inject.Inject;
 
-/**
- * @author <a href="mailto:bs@nuxeo.com">Bogdan Stefanescu</a>
- */
 @RunWith(FeaturesRunner.class)
 @Features(CoreFeature.class)
 @Deploy({ "org.nuxeo.ecm.automation.core" })
@@ -43,40 +37,13 @@ import com.google.inject.Inject;
 @RepositoryConfig(cleanup = Granularity.METHOD)
 public class OperationsEncodingTest {
 
-    protected DocumentModel src;
-
-    protected DocumentModel dst;
-
-    @Inject
-    AutomationService service;
-
     @Inject
     EventHandlerRegistry reg;
 
-    @Inject
-    CoreSession session;
-
-    @Before
-    public void initRepo() throws Exception {
-        src = session.createDocumentModel("/", "src", "Workspace");
-        src.setPropertyValue("dc:title", "Source");
-        src = session.createDocument(src);
-        session.save();
-        src = session.getDocument(src.getRef());
-
-        dst = session.createDocumentModel("/", "dst", "Workspace");
-        dst.setPropertyValue("dc:title", "Destination");
-        dst = session.createDocument(dst);
-        session.save();
-        dst = session.getDocument(dst.getRef());
-    }
-
     @Test
     public void testXmlEncoding() {
-
-        EventHandlerRegistry reg = Framework.getLocalService(EventHandlerRegistry.class);
         List<EventHandler> eh = reg.getEventHandlers("aboutToCreate");
-        assertEquals("a < b & b > c", eh.get(0).getExpression());
+        assertEquals("a < b & b > c", eh.get(0).getCondition());
     }
 
 }
