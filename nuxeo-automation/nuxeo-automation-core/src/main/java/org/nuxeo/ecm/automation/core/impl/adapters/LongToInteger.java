98d500f5cc43a9f9ab9b33b69e51ba285f28fc20
Anahide Tchertchian <at@nuxeo.com>
2014-02-07 12:30:50 +0100

    NXP-13722: Add missing long to integer adapter on automation

---
+++ nuxeo-automation/nuxeo-automation-core/src/main/java/org/nuxeo/ecm/automation/core/impl/adapters/LongToInteger.java
@@ -0,0 +1,35 @@
+/*
+ * (C) Copyright 2014 Nuxeo SA (http://nuxeo.com/) and contributors.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser General Public License
+ * (LGPL) version 2.1 which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl-2.1.html
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * Contributors:
+ *     Anahide Tchertchian
+ */
+package org.nuxeo.ecm.automation.core.impl.adapters;
+
+import org.nuxeo.ecm.automation.OperationContext;
+import org.nuxeo.ecm.automation.TypeAdaptException;
+import org.nuxeo.ecm.automation.TypeAdapter;
+
+/**
+ * @since 5.9.2
+ */
+public class LongToInteger implements TypeAdapter {
+
+    @Override
+    public Object getAdaptedValue(OperationContext ctx, Object objectToAdapt)
+            throws TypeAdaptException {
+        Long content = (Long) objectToAdapt;
+        return Integer.valueOf(content.toString());
+    }
+
+}
