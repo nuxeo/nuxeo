6e903fc8ce2db9d251ea7def5aba000c6d18c475
Guillaume Renard <grenard@nuxeo.com>
2014-02-27 14:39:14 +0100

    NXP-13855: default delayIn at 500ms and then 0ms at first show up

--- nuxeo-platform-webapp/src/main/resources/web/nuxeo.war/scripts/jquery.nuxeo.tipsy.js
+++ nuxeo-platform-webapp/src/main/resources/web/nuxeo.war/scripts/jquery.nuxeo.tipsy.js
@@ -2,23 +2,42 @@
 
   var check = "tipsyGravity";
 
-  $.fn.initTipsy = function() {
+  $.fn.initTipsy = function(delayIn) {
 
-    var cls = jQuery(this).attr('class').split(' ');
-    var gravity = jQuery.fn.tipsy.autoNS;
-    for (var i = 0; i < cls.length; i++) {
-      if (cls[i].indexOf(check) > -1) {
-        gravity = cls[i].slice(check.length, cls[i].length).toLowerCase();
-      }
-    }
+    $(this).doInit(delayIn);
 
-    jQuery(this).tipsy({
-      title : 'title',
-      gravity : gravity
+    $(document).on("tipsy-show", function() {
+      $(document).unbind("tipsy-show");
+      $('.tipsyShow').each(function() {
+        $(this).doInit(0);
+      });
     });
 
     return this;
   };
+
+  $.fn.doInit = function(delayIn) {
+
+    // default 500ms
+    delayIn = typeof delayIn !== 'undefined' ? delayIn : 500;
+
+    $(this).each(function() {
+      var cls = $(this).attr('class').split(' ');
+      var gravity = $.fn.tipsy.auto;
+      for ( var i = 0; i < cls.length; i++) {
+        if (cls[i].indexOf(check) > -1) {
+          gravity = cls[i].slice(check.length, cls[i].length).toLowerCase();
+        }
+      }
+
+      $(this).tipsy({
+        title : 'title',
+        gravity : gravity,
+        live : true,
+        delayIn : delayIn
+      });
+    });
+  }
 
   $.initTipsy = $.fn.initTipsy; // Rename for easier calling.
 
