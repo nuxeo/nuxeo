78feb273c7c7d1bf171f377a6fc5d3fea8753995
Benoit Delbosc <bdelbosc@nuxeo.com>
2014-03-04 14:56:06 +0100

    NXP-13655: Add unit test on ES type and index param

--- nuxeo-rest-api-test/src/test/java/org/nuxeo/ecm/restapi/test/ESDocumentsTest.java
+++ nuxeo-rest-api-test/src/test/java/org/nuxeo/ecm/restapi/test/ESDocumentsTest.java
@@ -1,5 +1,5 @@
 /*
- * (C) Copyright 2013 Nuxeo SA (http://nuxeo.com/) and contributors.
+ * (C) Copyright 2014 Nuxeo SA (http://nuxeo.com/) and contributors.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the GNU Lesser General Public License
@@ -12,47 +12,37 @@
  * Lesser General Public License for more details.
  *
  * Contributors:
- *     dmetzler
+ *     bdelbosc
  */
 package org.nuxeo.ecm.restapi.test;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.core.Response;
 
 import org.codehaus.jackson.JsonNode;
-import org.codehaus.jackson.node.ArrayNode;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.nuxeo.ecm.automation.jaxrs.io.documents.JsonESDocumentListWriter;
 import org.nuxeo.ecm.core.api.DocumentModel;
-import org.nuxeo.ecm.core.event.EventService;
-import org.nuxeo.ecm.core.storage.sql.DatabaseHelper;
 import org.nuxeo.ecm.core.test.annotations.Granularity;
 import org.nuxeo.ecm.core.test.annotations.RepositoryConfig;
 import org.nuxeo.ecm.restapi.server.jaxrs.adapters.ChildrenAdapter;
 import org.nuxeo.ecm.restapi.server.jaxrs.adapters.PageProviderAdapter;
-import org.nuxeo.ecm.restapi.server.jaxrs.adapters.SearchAdapter;
-import org.nuxeo.runtime.api.Framework;
 import org.nuxeo.runtime.test.runner.Features;
 import org.nuxeo.runtime.test.runner.FeaturesRunner;
 import org.nuxeo.runtime.test.runner.Jetty;
 import org.nuxeo.runtime.test.runner.LocalDeploy;
 
-import com.google.common.base.Joiner;
 import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.core.util.MultivaluedMapImpl;
 
-import edu.emory.mathcs.backport.java.util.Arrays;
-
-import org.nuxeo.ecm.automation.jaxrs.io.documents.JsonESDocumentListWriter;
-
 /**
- * Test the various ways to query for document lists.
+ * Test the various ways to get elasticsearch Json output.
  * 
- * @since 5.7.2
+ * @since 5.9.3
  */
 @RunWith(FeaturesRunner.class)
 @Features({ RestServerFeature.class })
@@ -67,8 +57,7 @@ public class ESDocumentsTest extends BaseTest {
         DocumentModel doc = RestServerInit.getNote(0, session);
 
         // When i do a GET Request
-        ClientResponse response = getResponse(RequestType.GETES,
-                "id/" + doc.getId());
+        ClientResponse response = getResponse(RequestType.GETES, "id/" + doc.getId());
 
         // Then I get the document as Json will all the properties
         assertEquals(Response.Status.OK.getStatusCode(), response.getStatus());
@@ -76,7 +65,6 @@ public class ESDocumentsTest extends BaseTest {
         System.err.println(node.toString());
         assertEquals("Note 0", node.get("note:note").getTextValue());
     }
-
 
     @Test
     public void iCanGetTheChildrenOfADocument() throws Exception {
@@ -97,6 +85,26 @@ public class ESDocumentsTest extends BaseTest {
         // {"index":{"_index":"nuxeo","_type":"doc","_id":"c0941844-7729-431f-9d07-57c6a6580716"}}
         JsonNode node = mapper.readTree(response.getEntityInputStream());
         assertTrue(node.get("index").isObject());
+    }
+
+    @Test
+    public void iCanSetTheIndexAndTypeOfBulkOutput() throws Exception {
+        // Given a folder
+        DocumentModel folder = RestServerInit.getFolder(1, session);
+
+        // When I query for it children
+        MultivaluedMap<String, String> queryParams = new MultivaluedMapImpl();
+        queryParams.putSingle("esIndex", "myIndex");
+        queryParams.putSingle("esType", "myType");
+        ClientResponse response = getResponse(RequestType.GETES, "id/" + folder.getId()
+                + "/@" + ChildrenAdapter.NAME, null, queryParams, null);
+        // Then I get elasticsearch bulk output for the two document
+        assertEquals(Response.Status.OK.getStatusCode(), response.getStatus());
+        // The first node is the an index action it looks like
+        // {"index":{"_index":"nuxeo","_type":"doc","_id":"c0941844-7729-431f-9d07-57c6a6580716"}}
+        JsonNode node = mapper.readTree(response.getEntityInputStream());
+        assertEquals("myindex", node.get("index").get("_index").getTextValue());
+        assertEquals("mytype", node.get("index").get("_type").getTextValue());
     }
 
     @Test
