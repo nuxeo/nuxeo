527a53976e68aae71121fd9dc2e586454afd1f96
Julien Carsique <jcarsique@nuxeo.com>
2014-03-07 16:33:09 +0100

    NXP-13826: no noisy error message "tput: No value for xterm and no -T specified"
    Revert "NXP-13826: disable buggy terminal size detection ("tput: No value for xterm and no -T specified")"
    This reverts commit 50e663f4067a2651e4bdeaf2d2c49d2aa6dcf9b6.

--- scripts/release.py
+++ scripts/release.py
@@ -66,7 +66,7 @@ import sys
 import tempfile
 
 from IndentedHelpFormatterWithNL import IndentedHelpFormatterWithNL
-from nxutils import ExitException, Repository, assert_git_config, extract_zip, \
+from nxutils import ExitException, Repository, assert_git_config, extract_zip,\
      log, make_zip, system
 from terminalsize import get_terminal_size
 
@@ -662,7 +662,9 @@ def main():
         description = """Release Nuxeo from a given branch, tag the release, then
 set the next SNAPSHOT version. If a maintenance version was provided, then a
 maintenance branch is kept, else it is deleted after release."""
-        help_formatter = IndentedHelpFormatterWithNL(width=80)
+        help_formatter = IndentedHelpFormatterWithNL(
+#                 max_help_position=6,
+                 width=get_terminal_size()[0])
         parser = optparse.OptionParser(usage=usage, description=description,
                                        formatter=help_formatter)
         parser.add_option('-r', action="store", type="string",
