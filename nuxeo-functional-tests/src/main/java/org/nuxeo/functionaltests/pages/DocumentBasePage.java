988b2cc25bbb02f72114a24af5fb3b79f0cf68f3
Guillaume Renard <grenard@nuxeo.com>
2014-03-13 15:19:33 +0100

    NXP-13922: format and sort

--- nuxeo-functional-tests/src/main/java/org/nuxeo/functionaltests/pages/DocumentBasePage.java
+++ nuxeo-functional-tests/src/main/java/org/nuxeo/functionaltests/pages/DocumentBasePage.java
@@ -45,163 +45,6 @@ import org.openqa.selenium.support.FindBy;
  */
 public class DocumentBasePage extends AbstractPage {
 
-    @Required
-    @FindBy(xpath = "//div[@id='nxw_documentTabs_panel']")
-    public WebElement tabsBar;
-
-    @FindBy(xpath = "//div[@id='nxw_documentTabs_panel']//a/span[text()='Content']")
-    public WebElement contentTabLink;
-
-    @FindBy(xpath = "//div[@id='nxw_documentTabs_panel']//a/span[text()='Summary']")
-    public WebElement summaryTabLink;
-
-    @FindBy(xpath = "//div[@id='nxw_documentTabs_panel']//a/span[text()='Edit']")
-    public WebElement editTabLink;
-
-    @FindBy(xpath = "//div[@id='nxw_documentTabs_panel']//a/span[text()='History']")
-    public WebElement historyTabLink;
-
-    @FindBy(xpath = "//div[@id='nxw_documentTabs_panel']//a/span[text()='Relations']")
-    public WebElement relationTabLink;
-
-    @FindBy(xpath = "//div[@id='nxw_documentTabs_panel']//a/span[text()='Manage']")
-    public WebElement manageTabLink;
-
-    @FindBy(className = "documentDescription")
-    public WebElement currentDocumentDescription;
-
-    @FindBy(className = "currentDocumentDescription")
-    public WebElement currentFolderishDescription;
-
-    @FindBy(xpath = "/html/body/table[2]/tbody/tr/td[2]/div[2]//h1")
-    public WebElement currentDocumentTitle;
-
-    @FindBy(xpath = "//form[@id='breadcrumbForm']")
-    public WebElement breadcrumbForm;
-
-    @FindBy(className = "creator")
-    public WebElement currentDocumentContributor;
-
-    @FindBy(linkText = "DOCUMENT MANAGEMENT")
-    public WebElement documentManagementLink;
-
-    @FindBy(linkText = "Workflow")
-    public WebElement workflowLink;
-
-    public ContextualActions contextualActions;
-
-    public DocumentBasePage(WebDriver driver) {
-        super(driver);
-    }
-
-    /**
-     * Click on the content tab and return the subpage of this page.
-     *
-     */
-    public ContentTabSubPage getContentTab() {
-        clickOnLinkIfNotSelected(contentTabLink);
-        return asPage(ContentTabSubPage.class);
-    }
-
-    public EditTabSubPage getEditTab() {
-        clickOnLinkIfNotSelected(editTabLink);
-        return asPage(EditTabSubPage.class);
-    }
-
-    public SummaryTabSubPage getSummaryTab() {
-        clickOnLinkIfNotSelected(summaryTabLink);
-        return asPage(SummaryTabSubPage.class);
-    }
-
-    public HistoryTabSubPage getHistoryTab() {
-        clickOnLinkIfNotSelected(historyTabLink);
-        return asPage(HistoryTabSubPage.class);
-    }
-
-    public ManageTabSubPage getManageTab() {
-        clickOnLinkIfNotSelected(manageTabLink);
-        return asPage(ManageTabSubPage.class);
-    }
-
-    public RelationTabSubPage getRelationTab() {
-        clickOnLinkIfNotSelected(relationTabLink);
-        return asPage(RelationTabSubPage.class);
-    }
-
-    public NavigationSubPage getNavigationSubPage() {
-        return asPage(NavigationSubPage.class);
-    }
-
-    protected void clickOnLinkIfNotSelected(WebElement tabLink) {
-        WebElement selectedTab = findElementWithTimeout(By.xpath("//div[@id='nxw_documentTabs_panel']//li[@class='selected']/a/span"));
-        if (!selectedTab.equals(tabLink)) {
-            tabLink.click();
-        }
-    }
-
-    /**
-     * For workspace type, the content tab is a bit different.
-     *
-     */
-    public WorkspacesContentTabSubPage getWorkspacesContentTab() {
-        clickOnLinkIfNotSelected(contentTabLink);
-        return asPage(WorkspacesContentTabSubPage.class);
-    }
-
-    /**
-     * Check if the user is connected by looking for the text: You are logged as
-     * Username
-     *
-     * @param username
-     * @throws UserNotConnectedException
-     */
-    public void checkUserConnected(String username)
-            throws UserNotConnectedException {
-        if (!(getHeaderLinks().getText().contains(username))) {
-            throw new UserNotConnectedException(username);
-        }
-    }
-
-    /**
-     * Check if the title of the current document page is equal to the
-     * {@code expectedTitle}.
-     *
-     * @param expectedTitle the expected title
-     */
-    public void checkDocTitle(String expectedTitle) {
-        assertEquals(expectedTitle, getCurrentDocumentTitle());
-    }
-
-    public String getCurrentDocumentDescription() {
-        return currentDocumentDescription.getText();
-    }
-
-    public String getCurrentFolderishDescription() {
-        return currentFolderishDescription.getText();
-    }
-
-    public String getCurrentDocumentTitle() {
-        return currentDocumentTitle.getText();
-    }
-
-    public List<String> getCurrentStates() {
-        List<WebElement> states = findElementsWithTimeout(By.className("sticker"));
-        List<String> stateLabels = new ArrayList<String>();
-        for (WebElement state : states) {
-            stateLabels.add(state.getText());
-        }
-        return stateLabels;
-    }
-
-    public String getCurrentContributors() {
-        return currentDocumentContributor.getText();
-    }
-
-    public DocumentBasePage goToDocumentByBreadcrumb(String documentTitle) {
-        breadcrumbForm.findElement(By.linkText(documentTitle)).click();
-        return asPage(DocumentBasePage.class);
-    }
-
     /**
      * Exception occurred a user is expected to be connected but it isn't.
      *
@@ -218,9 +61,161 @@ public class DocumentBasePage extends AbstractPage {
         }
     }
 
+    @FindBy(xpath = "//form[@id='breadcrumbForm']")
+    public WebElement breadcrumbForm;
+
+    @FindBy(xpath = "//div[@id='nxw_documentTabs_panel']//a/span[text()='Content']")
+    public WebElement contentTabLink;
+
+    public ContextualActions contextualActions;
+
+    @FindBy(className = "creator")
+    public WebElement currentDocumentContributor;
+
+    @FindBy(className = "documentDescription")
+    public WebElement currentDocumentDescription;
+
+    @FindBy(xpath = "/html/body/table[2]/tbody/tr/td[2]/div[2]//h1")
+    public WebElement currentDocumentTitle;
+
+    @FindBy(className = "currentDocumentDescription")
+    public WebElement currentFolderishDescription;
+
+    @FindBy(linkText = "DOCUMENT MANAGEMENT")
+    public WebElement documentManagementLink;
+
+    @FindBy(xpath = "//div[@id='nxw_documentTabs_panel']//a/span[text()='Edit']")
+    public WebElement editTabLink;
+
+    @FindBy(xpath = "//div[@id='nxw_documentTabs_panel']//a/span[text()='History']")
+    public WebElement historyTabLink;
+
+    @FindBy(xpath = "//div[@id='nxw_documentTabs_panel']//a/span[text()='Manage']")
+    public WebElement manageTabLink;
+
+    @FindBy(xpath = "//div[@id='nxw_documentTabs_panel']//a/span[text()='Relations']")
+    public WebElement relationTabLink;
+
+    @FindBy(xpath = "//div[@id='nxw_documentTabs_panel']//a/span[text()='Summary']")
+    public WebElement summaryTabLink;
+
+    @Required
+    @FindBy(xpath = "//div[@id='nxw_documentTabs_panel']")
+    public WebElement tabsBar;
+
+    @FindBy(linkText = "Workflow")
+    public WebElement workflowLink;
+
+    public DocumentBasePage(WebDriver driver) {
+        super(driver);
+    }
+
+    /**
+     * Check if the title of the current document page is equal to the
+     * {@code expectedTitle}.
+     *
+     * @param expectedTitle the expected title
+     */
+    public void checkDocTitle(String expectedTitle) {
+        assertEquals(expectedTitle, getCurrentDocumentTitle());
+    }
+
+    /**
+     * Check if the user is connected by looking for the text: You are logged as
+     * Username
+     *
+     * @param username
+     * @throws UserNotConnectedException
+     */
+    public void checkUserConnected(String username)
+            throws UserNotConnectedException {
+        if (!(getHeaderLinks().getText().contains(username))) {
+            throw new UserNotConnectedException(username);
+        }
+    }
+
+    protected void clickOnLinkIfNotSelected(WebElement tabLink) {
+        WebElement selectedTab = findElementWithTimeout(By.xpath("//div[@id='nxw_documentTabs_panel']//li[@class='selected']/a/span"));
+        if (!selectedTab.equals(tabLink)) {
+            tabLink.click();
+        }
+    }
+
     public AdminCenterBasePage getAdminCenter() {
         findElementWithTimeout(By.linkText("ADMIN CENTER")).click();
         return asPage(AdminCenterBasePage.class);
+    }
+
+    /**
+     * Click on the content tab and return the subpage of this page.
+     *
+     */
+    public ContentTabSubPage getContentTab() {
+        clickOnLinkIfNotSelected(contentTabLink);
+        return asPage(ContentTabSubPage.class);
+    }
+
+    public ContextualActions getContextualActions() {
+        return asPage(ContextualActions.class);
+    }
+
+    public String getCurrentContributors() {
+        return currentDocumentContributor.getText();
+    }
+
+    public String getCurrentDocumentDescription() {
+        return currentDocumentDescription.getText();
+    }
+
+    public String getCurrentDocumentTitle() {
+        return currentDocumentTitle.getText();
+    }
+
+    public String getCurrentFolderishDescription() {
+        return currentFolderishDescription.getText();
+    }
+
+    public List<String> getCurrentStates() {
+        List<WebElement> states = findElementsWithTimeout(By.className("sticker"));
+        List<String> stateLabels = new ArrayList<String>();
+        for (WebElement state : states) {
+            stateLabels.add(state.getText());
+        }
+        return stateLabels;
+    }
+
+    public DocumentBasePage getDocumentManagement() {
+        documentManagementLink.click();
+        return asPage(DocumentBasePage.class);
+    }
+
+    public EditTabSubPage getEditTab() {
+        clickOnLinkIfNotSelected(editTabLink);
+        return asPage(EditTabSubPage.class);
+    }
+
+    public HistoryTabSubPage getHistoryTab() {
+        clickOnLinkIfNotSelected(historyTabLink);
+        return asPage(HistoryTabSubPage.class);
+    }
+
+    public ManageTabSubPage getManageTab() {
+        clickOnLinkIfNotSelected(manageTabLink);
+        return asPage(ManageTabSubPage.class);
+    }
+
+    public NavigationSubPage getNavigationSubPage() {
+        return asPage(NavigationSubPage.class);
+    }
+
+    public RelationTabSubPage getRelationTab() {
+        clickOnLinkIfNotSelected(relationTabLink);
+        return asPage(RelationTabSubPage.class);
+    }
+
+    public SummaryTabSubPage getSummaryTab() {
+        clickOnLinkIfNotSelected(summaryTabLink);
+        return asPage(SummaryTabSubPage.class);
     }
 
     /**
@@ -233,17 +228,22 @@ public class DocumentBasePage extends AbstractPage {
         return page.goToDashboard();
     }
 
-    public ContextualActions getContextualActions() {
-        return asPage(ContextualActions.class);
-    }
-
-    public DocumentBasePage getDocumentManagement() {
-        documentManagementLink.click();
-        return asPage(DocumentBasePage.class);
-    }
-
     public WorkflowTabSubPage getWorkflow() {
         workflowLink.click();
         return asPage(WorkflowTabSubPage.class);
+    }
+
+    /**
+     * For workspace type, the content tab is a bit different.
+     *
+     */
+    public WorkspacesContentTabSubPage getWorkspacesContentTab() {
+        clickOnLinkIfNotSelected(contentTabLink);
+        return asPage(WorkspacesContentTabSubPage.class);
+    }
+
+    public DocumentBasePage goToDocumentByBreadcrumb(String documentTitle) {
+        breadcrumbForm.findElement(By.linkText(documentTitle)).click();
+        return asPage(DocumentBasePage.class);
     }
 }
