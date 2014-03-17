911921a9fb118de86e14a0b6991942d7746b0db7
Guillaume Renard <grenard@nuxeo.com>
2014-03-17 15:34:28 +0100

    NXP-13922: test user rights on collections

--- nuxeo-distribution-dm/ftest/webdriver/src/test/java/org/nuxeo/functionaltests/ITCollectionsTest.java
+++ nuxeo-distribution-dm/ftest/webdriver/src/test/java/org/nuxeo/functionaltests/ITCollectionsTest.java
@@ -31,6 +31,9 @@ import org.nuxeo.functionaltests.pages.CollectionsPage;
 import org.nuxeo.functionaltests.pages.DocumentBasePage;
 import org.nuxeo.functionaltests.pages.DocumentBasePage.UserNotConnectedException;
 import org.nuxeo.functionaltests.pages.FileDocumentBasePage;
+import org.nuxeo.functionaltests.pages.admincenter.usermanagement.UsersGroupsBasePage;
+import org.nuxeo.functionaltests.pages.admincenter.usermanagement.UsersTabSubPage;
+import org.nuxeo.functionaltests.pages.tabs.AccessRightsSubPage;
 import org.nuxeo.functionaltests.pages.tabs.CollectionContentTabSubPage;
 import org.nuxeo.functionaltests.pages.tabs.ContentTabSubPage;
 import org.nuxeo.functionaltests.pages.tabs.ManageTabSubPage;
@@ -59,10 +62,19 @@ public class ITCollectionsTest extends AbstractTest {
 
     private static final String COLLECTION_DESSCRIPTION_2 = "My second collection";
 
+    private static final String CAN_COLLECT_RIGHT = "Can Collect";
+
     @After
     public void tearDown() throws UserNotConnectedException {
         DocumentBasePage documentBasePage = login();
-        ContentTabSubPage contentTabSubPage  = documentBasePage.swithToPersonalWorkspace().getContentTab();
+        UsersTabSubPage usersTab = login().getAdminCenter().getUsersGroupsHomePage().getUsersTab();
+        usersTab = usersTab.searchUser(TEST_USERNAME);
+        if (usersTab.isUserFound(TEST_USERNAME)) {
+            usersTab = usersTab.viewUser(TEST_USERNAME).deleteUser();
+            documentBasePage = usersTab.exitAdminCenter().getHeaderLinks().getNavigationSubPage().goToDocument(
+                    "Workspaces");
+        }
+        ContentTabSubPage contentTabSubPage = documentBasePage.swithToPersonalWorkspace().getContentTab();
         contentTabSubPage = contentTabSubPage.removeAllDocuments();
         ManageTabSubPage manageTabSubPage = contentTabSubPage.getManageTab();
         manageTabSubPage.getTrashSubTab().purgeAllDocuments();
@@ -72,7 +84,8 @@ public class ITCollectionsTest extends AbstractTest {
     }
 
     @Test
-    public void testAddDocumentToCollectionAndRemove() throws UserNotConnectedException, IOException {
+    public void testAddDocumentToCollectionAndRemove()
+            throws UserNotConnectedException, IOException {
         DocumentBasePage documentBasePage = login();
         // Check we can not add Domain to a collection
         assertFalse(documentBasePage.isAddToCollectionUpperActionAvailable());
@@ -80,8 +93,9 @@ public class ITCollectionsTest extends AbstractTest {
         DocumentBasePage workspacePage = createWorkspace(documentBasePage,
                 WORKSPACE_TITLE, null);
 
-        FileDocumentBasePage fileDocumentBasePage = createFile(workspacePage, TEST_FILE_NAME, "Test File description",
-                false, null, null, null);
+        FileDocumentBasePage fileDocumentBasePage = createFile(workspacePage,
+                TEST_FILE_NAME, "Test File description", false, null, null,
+                null);
 
         // Simple add to collection
         AddToCollectionForm addToCollectionForm = fileDocumentBasePage.getAddToCollectionPopup();
@@ -95,8 +109,8 @@ public class ITCollectionsTest extends AbstractTest {
         workspacePage = fileDocumentBasePage.getNavigationSubPage().goToDocument(
                 WORKSPACE_TITLE);
 
-        fileDocumentBasePage = createFile(workspacePage, TEST_FILE_NAME2, "Test File description",
-                false, null, null, null);
+        fileDocumentBasePage = createFile(workspacePage, TEST_FILE_NAME2,
+                "Test File description", false, null, null, null);
 
         addToCollectionForm = fileDocumentBasePage.getAddToCollectionPopup();
 
@@ -106,7 +120,8 @@ public class ITCollectionsTest extends AbstractTest {
 
         assertTrue(addToCollectionForm.isExistingDescriptionVisible());
 
-        assertEquals(COLLECTION_DESSCRIPTION_1, addToCollectionForm.getExistingDescription());
+        assertEquals(COLLECTION_DESSCRIPTION_1,
+                addToCollectionForm.getExistingDescription());
 
         fileDocumentBasePage = addToCollectionForm.add(FileDocumentBasePage.class);
 
@@ -135,23 +150,29 @@ public class ITCollectionsTest extends AbstractTest {
         assertEquals(COLLECTION_NAME_1, collectionNames.get(0));
         assertEquals(COLLECTION_NAME_2, collectionNames.get(1));
 
-        ContentTabSubPage contentTabSubPage  = workspaceContentTab.swithToPersonalWorkspace().getContentTab();
+        ContentTabSubPage contentTabSubPage = workspaceContentTab.swithToPersonalWorkspace().getContentTab();
 
         // Check Collection are stored in Personal Workspace
         List<WebElement> personalWorkspaceRootDocs = contentTabSubPage.getChildDocumentRows();
 
         assertEquals(1, personalWorkspaceRootDocs.size());
-        assertEquals("Collections", personalWorkspaceRootDocs.get(0).findElement(By.xpath("td[3]")).getText());
+        assertEquals(
+                "Collections",
+                personalWorkspaceRootDocs.get(0).findElement(By.xpath("td[3]")).getText());
 
         contentTabSubPage.swithToDocumentBase();
 
-        CollectionContentTabSubPage collectionContentTabSubPage = contentTabSubPage.goToHomePage().goToCollections().gotToCollection(COLLECTION_NAME_1);
+        CollectionContentTabSubPage collectionContentTabSubPage = contentTabSubPage.goToHomePage().goToCollections().gotToCollection(
+                COLLECTION_NAME_1);
 
-        assertEquals(2, collectionContentTabSubPage.getChildDocumentRows().size());
+        assertEquals(2,
+                collectionContentTabSubPage.getChildDocumentRows().size());
 
-        collectionContentTabSubPage = collectionContentTabSubPage.goToHomePage().goToCollections().gotToCollection(COLLECTION_NAME_2);
+        collectionContentTabSubPage = collectionContentTabSubPage.goToHomePage().goToCollections().gotToCollection(
+                COLLECTION_NAME_2);
 
-        assertEquals(2, collectionContentTabSubPage.getChildDocumentRows().size());
+        assertEquals(2,
+                collectionContentTabSubPage.getChildDocumentRows().size());
 
         // Check copy/paste collection
         contentTabSubPage = collectionContentTabSubPage.swithToPersonalWorkspace().getContentTab();
@@ -164,7 +185,7 @@ public class ITCollectionsTest extends AbstractTest {
 
         assertEquals(3, contentTabSubPage.getChildDocumentRows().size());
 
-        contentTabSubPage = contentTabSubPage.goToDocument(1).getContentTab();
+        contentTabSubPage = contentTabSubPage.goToDocument(1).getCollectionContentTab();
 
         assertEquals(2, contentTabSubPage.getChildDocumentRows().size());
 
@@ -174,9 +195,87 @@ public class ITCollectionsTest extends AbstractTest {
     }
 
     @Test
-    public void testRightsOnCollection() throws UserNotConnectedException {
+    public void testRightsOnCollection() throws UserNotConnectedException, IOException {
         DocumentBasePage documentBasePage = login();
-        // TODO
+        // Create test user if not exist
+        UsersGroupsBasePage page;
+        UsersTabSubPage usersTab = documentBasePage.getAdminCenter().getUsersGroupsHomePage().getUsersTab();
+        usersTab = usersTab.searchUser(TEST_USERNAME);
+        if (!usersTab.isUserFound(TEST_USERNAME)) {
+            page = usersTab.getUserCreatePage().createUser(TEST_USERNAME,
+                    TEST_USERNAME, "lastname1", "company1", "email1",
+                    TEST_PASSWORD, "members");
+            usersTab = page.getUsersTab(true);
+        }
+        usersTab.searchUser(TEST_USERNAME);
+        assertTrue(usersTab.isUserFound(TEST_USERNAME));
+
+        documentBasePage = usersTab.exitAdminCenter().getHeaderLinks().getNavigationSubPage().goToDocument(
+                "Workspaces");
+
+        // Create workspace and deny all to test user
+        DocumentBasePage workspacePage = createWorkspace(documentBasePage,
+                WORKSPACE_TITLE, null);
+        AccessRightsSubPage accessRightSubTab = workspacePage.getManageTab().getAccessRightsSubTab();
+        if (!accessRightSubTab.hasPermissionForUser("Manage everything",
+                TEST_USERNAME)) {
+            accessRightSubTab.addPermissionForUser(TEST_USERNAME, "Read", false);
+        }
+
+        // Create 2 collections in "My Collections" container
+        documentBasePage = createCollections(workspacePage, "My Collections",
+                null);
+        documentBasePage = createCollection(workspacePage, COLLECTION_NAME_1,
+                COLLECTION_DESSCRIPTION_1);
+        documentBasePage = documentBasePage.getCollectionContentTab().getNavigationSubPage().goToDocument(
+                "My Collections");
+        documentBasePage = createCollection(workspacePage, COLLECTION_NAME_2,
+                COLLECTION_DESSCRIPTION_2);
+
+        // Give "Can Collect" to test user to the second one
+        documentBasePage.getManageTab().getAccessRightsSubTab().addPermissionForUser(
+                TEST_USERNAME, CAN_COLLECT_RIGHT, true);
+        documentBasePage = documentBasePage.swithToPersonalWorkspace();
+        workspacePage = documentBasePage.getNavigationSubPage().goToDocument(
+                "Administrator");
+        // Add a file to this collection that test user can't see
+        FileDocumentBasePage fileDocumentBasePage = createFile(workspacePage,
+                TEST_FILE_NAME, "Test File description", false, null, null,
+                null);
+        AddToCollectionForm addToCollectionForm = fileDocumentBasePage.getAddToCollectionPopup();
+        addToCollectionForm.setCollection(COLLECTION_NAME_2);
+        fileDocumentBasePage = addToCollectionForm.add(FileDocumentBasePage.class);
+        logout();
+
+        // Login as test user
+        documentBasePage = loginAsTestUser();
+        CollectionsPage collectionsPage = documentBasePage.goToHomePage().goToCollections();
+        List<String> collections = collectionsPage.getCollectionNames();
+        // Check that I can see the second collection but not the first one
+        assertEquals(1, collections.size());
+        assertTrue(COLLECTION_NAME_2.equals(collections.get(0)));
+
+        // Check that I can't see the file Admin added
+        CollectionContentTabSubPage collectionContentTabSubPage = collectionsPage.gotToCollection(COLLECTION_NAME_2);
+        assertEquals(0,
+                collectionContentTabSubPage.getChildDocumentRows().size());
+
+        // Create a file in test user workspace and add it to the collection
+        documentBasePage = collectionContentTabSubPage.swithToPersonalWorkspace();
+        workspacePage = documentBasePage.getNavigationSubPage().goToDocument(
+                "jdoe lastname1");
+        fileDocumentBasePage = createFile(workspacePage, TEST_FILE_NAME,
+                "Test File description", false, null, null, null);
+        addToCollectionForm = fileDocumentBasePage.getAddToCollectionPopup();
+        addToCollectionForm.setCollection(COLLECTION_NAME_2);
+        fileDocumentBasePage = addToCollectionForm.add(FileDocumentBasePage.class);
+
+        // Check now the collection has one file
+        collectionsPage = documentBasePage.goToHomePage().goToCollections();
+        collectionContentTabSubPage = collectionsPage.gotToCollection(COLLECTION_NAME_2);
+        assertEquals(1,
+                collectionContentTabSubPage.getChildDocumentRows().size());
+
         logout();
     }
 
