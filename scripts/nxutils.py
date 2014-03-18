nuxeo/nuxeo

commit b687877e9324b8444d0f9eebef42ce87c3523a5f

NXP-14031: cleaner help ouptut; add default value for MP conf URL

scripts/nxutils.py
@@ -32,6 +32,8 @@ from zipfile import ZIP_DEFLATED, ZipFile
 
 
 SUPPORTED_GIT_ONLINE_URLS = "http://", "https://", "git://", "git@"
+DEFAULT_MP_CONF_URL = ("https://raw.github.com/nuxeo/"
+                       "integration-scripts/master/marketplace.ini")
 
 
 class ExitException(Exception):
@@ -286,6 +288,7 @@ class Repository(object):
             # Main modules
             self.eval_modules()
             for module in self.modules:
+                break
                 # Ignore modules which are not Git sub-repositories
                 if (not os.path.isdir(module) or
                 os.path.isdir(os.path.join(module, ".git"))):
@@ -298,11 +301,14 @@ class Repository(object):
                                                             "addons/module")
             for addon in self.addons + (self.optional_addons
                                         if with_optionals else []):
+                break
                 self.git_pull(addon, version, fallback_branch)
             if not self.is_online:
                 self.url_pattern = self.url_pattern.replace("addons/module",
                                                             "module")
             # Marketplace packages
+            if marketplace_conf == '':
+                marketplace_conf = DEFAULT_MP_CONF_URL
             if marketplace_conf:
                 self.clone_mp(marketplace_conf)
             os.chdir(cwd)

clone.py
@@ -22,14 +22,18 @@ import optparse
 import os
 import sys
 
-from scripts.nxutils import ExitException, Repository, log
+from scripts.nxutils import ExitException, Repository, log, DEFAULT_MP_CONF_URL
+from scripts.IndentedHelpFormatterWithNL import IndentedHelpFormatterWithNL
+from scripts.terminalsize import get_terminal_size
 
 
 def main():
     try:
         usage = "usage: %prog [options] [version|branch|tag]"
+        help_formatter = IndentedHelpFormatterWithNL(
+                                                width=get_terminal_size()[0])
         parser = optparse.OptionParser(usage=usage, description="""Clone or
-update Nuxeo source code.""")
+update Nuxeo source code.""", formatter=help_formatter)
         parser.add_option('-r', action="store", type="string",
                           dest='remote_alias', default='origin', help="""the
 Git alias of remote URL (default: %default)""")
@@ -47,7 +51,8 @@ virtual drive on Windows""")
         parser.add_option('-m', "--marketplace-conf", action="store",
                           type="string", dest='marketplace_conf', default=None,
                           help="""The Marketplace configuration URL
-(default: %default)""")
+(default: None)\n
+If set to '' (empty string), it defaults to '%s'""" % (DEFAULT_MP_CONF_URL))
 
         (options, args) = parser.parse_args()
         repo = Repository(os.getcwd(), options.remote_alias,

scripts/release_mp.py
@@ -25,7 +25,8 @@ from release import Release
 import traceback
 
 from IndentedHelpFormatterWithNL import IndentedHelpFormatterWithNL
-from nxutils import ExitException, Repository, assert_git_config, log, system
+from nxutils import ExitException, Repository, assert_git_config, log, system,\
+    DEFAULT_MP_CONF_URL
 from terminalsize import get_terminal_size
 
 
@@ -42,6 +43,8 @@ class ReleaseMP(object):
     def __init__(self, alias, restart_from, marketplace_conf=None):
         self.alias = alias
         self.restart_from = restart_from
+        if marketplace_conf == '':
+            marketplace_conf = DEFAULT_MP_CONF_URL
         self.marketplace_conf = marketplace_conf
         self.repo = Repository(os.getcwd(), self.alias)
         self.mp_config = self.repo.get_mp_config(self.marketplace_conf)
@@ -194,9 +197,8 @@ def main():
        %prog prepare [-r alias] [-m URL] [--rf package]
        %prog perform [-r alias] [-m URL] [--rf package]""")
         description = """Release Nuxeo Marketplace packages.\n
-The first call must provide an URL for the configuration (such as
-https://gist.github.com/jcarsique/8040138/raw/). You can use a local file with
-the 'file://' format.\n
+The first call must provide an URL for the configuration. If set to '' (empty
+string), it defaults to '%s'. You can use a local file URL ('file://').\n
 Then, a 'release.ini' file is generated and will be reused for the next calls.
 For each package, a 'release-$PACKAGE_NAME.log' file is also generated and
 corresponds to the file generated by the release.py script.\n
@@ -207,7 +209,8 @@ The 'release.ini' file contains informations about the release process:\n
 - 'skip = Failed!' followed by a stack trace in case of error.\n
 The script can be re-called: it will skip the packages with a skip value and
 skip the prepare (or perform) if 'prepared = True' (or 'performed = True').\n
-Failed uploads are not retried and must be manually done."""
+Failed uploads are not retried and must be manually done.
+""" % DEFAULT_MP_CONF_URL
         help_formatter = IndentedHelpFormatterWithNL(
 #                 max_help_position=6,
                  width=get_terminal_size()[0])

commit 9a08f3b8613aaf6a2a73e7a5331a0c6f64139ef2

NXP-14031: fix clone (remove break introduced in b687877e9324b8444d0f9eebef42ce87c3523a5f)

scripts/nxutils.py
@@ -289,7 +289,6 @@ class Repository(object):
             # Main modules
             self.eval_modules()
             for module in self.modules:
-                break
                 # Ignore modules which are not Git sub-repositories
                 if (not os.path.isdir(module) or
                 os.path.isdir(os.path.join(module, ".git"))):
@@ -302,7 +301,6 @@ class Repository(object):
                                                             "addons/module")
             for addon in self.addons + (self.optional_addons
                                         if with_optionals else []):
-                break
                 self.git_pull(addon, version, fallback_branch)
             if not self.is_online:
                 self.url_pattern = self.url_pattern.replace("addons/module",