8a8f36d1b263d65ba00c7bacdd316604c59cec1f
Stephane Lacoin aka nxmatic <slacoin@nuxeo.com>
2014-03-27 10:13:47 +0100

    NXP-14119: enable datasource pool monitoring

--- nuxeo-runtime-jtajca/src/main/java/org/nuxeo/runtime/jtajca/NuxeoContainerListener.java
+++ nuxeo-runtime-jtajca/src/main/java/org/nuxeo/runtime/jtajca/NuxeoContainerListener.java
@@ -12,7 +12,7 @@ import org.apache.geronimo.connector.outbound.AbstractConnectionManager;
 
 public interface NuxeoContainerListener {
 
-    void handleNewConnectionManager(String repositoryName, AbstractConnectionManager mgr);
+    void handleNewConnectionManager(String name, AbstractConnectionManager mgr);
 
-    void handleConnectionManagerReset(String repositoryName, AbstractConnectionManager mgr);
+    void handleConnectionManagerReset(String name, AbstractConnectionManager mgr);
 }
