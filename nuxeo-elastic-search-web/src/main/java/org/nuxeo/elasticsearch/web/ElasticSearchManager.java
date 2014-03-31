0f878946f009a61203fcad02c4d6a4f52ddd7db5
Thierry Delprat <tdelprat@nuxeo.com>
2014-03-31 14:06:02 +0200

    NXP-14128 Improve ContentView/PageProvider view

--- nuxeo-elastic-search-web/src/main/java/org/nuxeo/elasticsearch/web/ElasticSearchManager.java
+++ nuxeo-elastic-search-web/src/main/java/org/nuxeo/elasticsearch/web/ElasticSearchManager.java
@@ -1,9 +1,25 @@
+/*
+ * (C) Copyright 2014 Nuxeo SA (http://nuxeo.com/) and contributors.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser General Public License
+ * (LGPL) version 2.1 which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl.html
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * Contributors:
+ *     Nuxeo
+ */
 package org.nuxeo.elasticsearch.web;
 
 import static org.jboss.seam.ScopeType.EVENT;
 
 import java.util.ArrayList;
-import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 import org.elasticsearch.action.admin.cluster.health.ClusterHealthResponse;
@@ -14,6 +30,8 @@ import org.jboss.seam.annotations.In;
 import org.jboss.seam.annotations.Name;
 import org.jboss.seam.annotations.Scope;
 import org.nuxeo.ecm.core.api.CoreSession;
+import org.nuxeo.ecm.platform.contentview.jsf.ContentView;
+import org.nuxeo.ecm.platform.contentview.jsf.ContentViewService;
 import org.nuxeo.ecm.platform.query.api.PageProviderDefinition;
 import org.nuxeo.ecm.platform.query.core.CoreQueryPageProviderDescriptor;
 import org.nuxeo.ecm.platform.query.core.GenericPageProviderDescriptor;
@@ -21,10 +39,12 @@ import org.nuxeo.elasticsearch.api.ElasticSearchAdmin;
 import org.nuxeo.elasticsearch.api.ElasticSearchIndexing;
 import org.nuxeo.elasticsearch.commands.IndexingCommand;
 import org.nuxeo.runtime.api.Framework;
-import org.nuxeo.runtime.model.Extension;
-import org.nuxeo.runtime.model.RegistrationInfo;
-import org.nuxeo.ecm.platform.contentview.jsf.ContentViewDescriptor;
 
+/**
+ *
+ * @author <a href="mailto:tdelprat@nuxeo.com">Tiry</a>
+ *
+ */
 @Name("esAdmin")
 @Scope(EVENT)
 public class ElasticSearchManager {
@@ -38,11 +58,7 @@ public class ElasticSearchManager {
     @In(create = true, required = false)
     protected transient CoreSession documentManager;
 
-    protected List<PageProviderDefinition> elasticSearchPageProviders = null;
-
-    protected List<PageProviderDefinition> corePageProviders = null;
-
-    protected List<PageProviderDefinition> otherProviders = null;
+    protected List<ContentViewStatus> cvStatuses = null;
 
     public String getNodesInfo() {
         NodesInfoResponse nodesInfo = esa.getClient().admin().cluster().prepareNodesInfo().execute().actionGet();
@@ -77,83 +93,37 @@ public class ElasticSearchManager {
 
     protected void introspectPageProviders() throws Exception {
 
-        elasticSearchPageProviders = new ArrayList<>();
-        corePageProviders = new ArrayList<>();
-        otherProviders = new ArrayList<>();
+        cvStatuses = new ArrayList<>();
 
-        Collection<RegistrationInfo> infos = Framework.getRuntime().getComponentManager().getRegistrations();
-        for (RegistrationInfo info : infos) {
-            if (info.getExtensions()==null) {
-                continue;
-            }
-            for (Extension extension : info.getExtensions()) {
-                if (extension.getExtensionPoint().equals("providers")) {
-                    if (extension.getContributions()==null) {
-                        continue;
-                    }
-                    for (Object contrib : extension.getContributions()) {
-                        if (contrib instanceof GenericPageProviderDescriptor) {
-                            GenericPageProviderDescriptor gppd = (GenericPageProviderDescriptor) contrib;
-                            if (gppd.getPageProviderClass().getName().contains(
-                                    "elasticsearch")) {
-                                elasticSearchPageProviders.add(gppd);
-                            } else {
-                                otherProviders.add(gppd);
-                            }
-                        } else if (contrib instanceof CoreQueryPageProviderDescriptor) {
-                            corePageProviders.add((CoreQueryPageProviderDescriptor) contrib);
-                        }
-                    }
-                }
-                else if (extension.getExtensionPoint().equals("contentViews")) {
-                    if (extension.getContributions()==null) {
-                        continue;
-                    }
-                    for (Object contrib : extension.getContributions()) {
-                        if (contrib instanceof ContentViewDescriptor) {
-                            ContentViewDescriptor cv = (ContentViewDescriptor) contrib;
-                            if (cv.getCoreQueryPageProvider()!=null) {
-                                corePageProviders.add(cv.getCoreQueryPageProvider());
-                            } else {
-                                GenericPageProviderDescriptor gppd = cv.getGenericPageProvider();
-                                if (gppd==null) {
-                                    continue;
-                                }
-                                if (gppd.getPageProviderClass().getName().contains(
-                                        "elasticsearch")) {
-                                    elasticSearchPageProviders.add(gppd);
-                                } else {
-                                    otherProviders.add(gppd);
-                                }
-                            }
-                        }
-                    }
+        ContentViewService cvs = Framework.getLocalService(ContentViewService.class);
 
+        for (String cvName : cvs.getContentViewNames()) {
+            ContentView cv = cvs.getContentView(cvName);
+            PageProviderDefinition def = cv.getPageProvider().getDefinition();
+            if (def instanceof GenericPageProviderDescriptor) {
+                GenericPageProviderDescriptor gppd = (GenericPageProviderDescriptor) def;
+                if (gppd.getPageProviderClass().getName().contains(
+                        "elasticsearch")) {
+                    cvStatuses.add(new ContentViewStatus(cvName,
+                            gppd.getName(), "elasticsearch"));
+                } else {
+                    cvStatuses.add(new ContentViewStatus(cvName,
+                            gppd.getName(),
+                            gppd.getPageProviderClass().getName()));
                 }
+            } else if (def instanceof CoreQueryPageProviderDescriptor) {
+                cvStatuses.add(new ContentViewStatus(cvName, def.getName(),
+                        "core"));
             }
         }
+
+        Collections.sort(cvStatuses);
     }
 
-    public List<PageProviderDefinition> getElasticSearchPageProviders()
-            throws Exception {
-        if (elasticSearchPageProviders == null) {
+    public List<ContentViewStatus> getContentViewStatus() throws Exception {
+        if (cvStatuses == null) {
             introspectPageProviders();
         }
-        return elasticSearchPageProviders;
+        return cvStatuses;
     }
-
-    public List<PageProviderDefinition> getCorePageProviders() throws Exception {
-        if (corePageProviders == null) {
-            introspectPageProviders();
-        }
-        return corePageProviders;
-    }
-
-    public List<PageProviderDefinition> getOtherProviders() throws Exception {
-        if (otherProviders == null) {
-            introspectPageProviders();
-        }
-        return otherProviders;
-    }
-
 }
