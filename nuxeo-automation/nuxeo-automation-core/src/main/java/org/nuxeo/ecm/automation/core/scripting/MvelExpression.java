96a509b80014c8f6914e458236dedc34cb06a452
Mariana Cedica <mcedica@nuxeo.com>
2014-03-31 15:24:23 +0200

    NXP-13543: add possibility to add custom bindings in scripting context

--- nuxeo-automation/nuxeo-automation-core/src/main/java/org/nuxeo/ecm/automation/core/scripting/MvelExpression.java
+++ nuxeo-automation/nuxeo-automation-core/src/main/java/org/nuxeo/ecm/automation/core/scripting/MvelExpression.java
@@ -12,6 +12,7 @@
 package org.nuxeo.ecm.automation.core.scripting;
 
 import java.io.Serializable;
+import java.util.Map;
 
 import org.mvel2.MVEL;
 import org.mvel2.compiler.BlankLiteral;
@@ -36,11 +37,16 @@ public class MvelExpression implements Expression {
         if (compiled == null) {
             compiled = MVEL.compileExpression(expr);
         }
-        Object result = MVEL.executeExpression(compiled,
-                Scripting.initBindings(ctx));
+        Object result = MVEL.executeExpression(compiled, getBindings(ctx));
         return result != null
                 && result.getClass().isAssignableFrom(BlankLiteral.class) ? ""
                 : result;
     }
 
+    /**
+     * @since 5.9.3
+     */
+    protected Map<String, Object> getBindings(OperationContext ctx) {
+        return Scripting.initBindings(ctx);
+    }
 }
