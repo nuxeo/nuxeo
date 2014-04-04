252923fdcaf7484ac41194d6495ba9551b488762
Benoit Delbosc <bdelbosc@nuxeo.com>
2014-04-04 15:52:01 +0200

    NXP-14007: cosmetic

--- nuxeo-elastic-search-core/src/main/java/org/nuxeo/elasticsearch/listener/ElasticSearchPostCommitSyncListener.java
+++ nuxeo-elastic-search-core/src/main/java/org/nuxeo/elasticsearch/listener/ElasticSearchPostCommitSyncListener.java
@@ -1,3 +1,19 @@
+/*
+ * (C) Copyright 2014 Nuxeo SA (http://nuxeo.com/) and contributors.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser General Public License
+ * (LGPL) version 2.1 which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl.html
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * Contributors:
+ *     tiry
+ */
 package org.nuxeo.elasticsearch.listener;
 
 import java.io.Serializable;
@@ -21,22 +37,23 @@ public class ElasticSearchPostCommitSyncListener implements
 
         List<IndexingCommand> cmds = new ArrayList<>();
         for (Event event : bundle) {
-            if (event.getName().equals(EventConstants.ES_INDEX_EVENT_SYNC)) {
-                Map<String, Serializable> props = event.getContext().getProperties();
+            if (EventConstants.ES_INDEX_EVENT_SYNC.equals(event.getName())) {
+                Map<String, Serializable> props = event.getContext()
+                        .getProperties();
                 for (String key : props.keySet()) {
                     if (key.startsWith(IndexingCommand.PREFIX)) {
-                        IndexingCommand cmd = IndexingCommand.fromJSON(
-                                event.getContext().getCoreSession(),
-                                (String) props.get(key));
+                        IndexingCommand cmd = IndexingCommand.fromJSON(event
+                                .getContext().getCoreSession(), (String) props
+                                .get(key));
                         cmds.add(cmd);
                     }
                 }
             }
         }
-        if (cmds.size() > 0) {
-            ElasticSearchIndexing esi = Framework.getLocalService(ElasticSearchIndexing.class);
+        if (!cmds.isEmpty()) {
+            ElasticSearchIndexing esi = Framework
+                    .getLocalService(ElasticSearchIndexing.class);
             esi.indexNow(cmds);
-            // flush ES index
             esi.flush();
         }
     }
