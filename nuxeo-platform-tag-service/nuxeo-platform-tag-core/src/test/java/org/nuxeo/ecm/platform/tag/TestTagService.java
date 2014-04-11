bd411c7ab590372b620f7e969a2a5f3f0c96a0a5
Vladimir Pasquier <vpasquier@nuxeo.com>
2014-04-11 13:46:56 +0200

    Revert "NXP-12247: trying to fix random mssql failure on tag test"
    This reverts commit 1f43281b3369ded6fee7006e21b31dc36079a399.

--- nuxeo-platform-tag-service/nuxeo-platform-tag-core/src/test/java/org/nuxeo/ecm/platform/tag/TestTagService.java
+++ nuxeo-platform-tag-service/nuxeo-platform-tag-core/src/test/java/org/nuxeo/ecm/platform/tag/TestTagService.java
@@ -128,7 +128,7 @@ public class TestTagService {
         tags = tagService.getDocumentTags(session, file2Id, "bob");
         assertTrue(tags.isEmpty());
 
-        DatabaseHelper.DATABASE.sleepForFulltext();
+        maybeSleep();
 
         // find docs for tag
         List<String> docIds;
@@ -148,7 +148,7 @@ public class TestTagService {
         docIds = tagService.getTagDocumentIds(session, "othertag", "bob");
         assertTrue(docIds.isEmpty());
 
-        DatabaseHelper.DATABASE.sleepForFulltext();
+        maybeSleep();
 
         // global cloud
         List<Tag> cloud = tagService.getTagCloud(session, null, null, null);
@@ -190,7 +190,7 @@ public class TestTagService {
         suggestions = tagService.getSuggestions(session, "%tag", null);
         assertEquals(twotags, labels(suggestions));
 
-        DatabaseHelper.DATABASE.sleepForFulltext();
+        maybeSleep();
 
         // ws loader
 
