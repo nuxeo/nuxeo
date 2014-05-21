81fe47db0d5e965dfeb814f45d37f5fa6db93243
Vladimir Pasquier <vpasquier@nuxeo.com>
2014-05-21 17:44:31 +0200

    NXP-14377: restrict access to connect pkg manager

--- nuxeo-admin-center/nuxeo-admin-center/src/main/java/org/nuxeo/connect/client/we/ConnectClientRoot.java
+++ nuxeo-admin-center/nuxeo-admin-center/src/main/java/org/nuxeo/connect/client/we/ConnectClientRoot.java
@@ -1,5 +1,5 @@
 /*
- * (C) Copyright 2006-2012 Nuxeo SA (http://nuxeo.com/) and contributors.
+ * (C) Copyright 2014 Nuxeo SA (http://nuxeo.com/) and contributors.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the GNU Lesser General Public License
@@ -13,21 +13,21 @@
  *
  * Contributors:
  *     Nuxeo - initial API and implementation
- *
+ *     Vladimir Pasquier <vpasquier@nuxeo.com>
  */
 
 package org.nuxeo.connect.client.we;
+
+import org.nuxeo.ecm.admin.NuxeoCtlManager;
+import org.nuxeo.ecm.core.api.NuxeoPrincipal;
+import org.nuxeo.ecm.webengine.model.WebObject;
+import org.nuxeo.ecm.webengine.model.impl.ModuleRoot;
+import org.nuxeo.runtime.api.Framework;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
-
-import org.nuxeo.ecm.admin.NuxeoCtlManager;
-import org.nuxeo.ecm.core.api.NuxeoPrincipal;
-import org.nuxeo.ecm.webengine.model.Resource;
-import org.nuxeo.ecm.webengine.model.WebObject;
-import org.nuxeo.ecm.webengine.model.impl.ModuleRoot;
 
 /**
  * Root object: mainly acts as a router.
@@ -38,40 +38,49 @@ import org.nuxeo.ecm.webengine.model.impl.ModuleRoot;
 @WebObject(type = "connectClientRoot")
 public class ConnectClientRoot extends ModuleRoot {
 
+    private boolean isAdmin;
+
+    @Override
+    public void initialize(Object... args) {
+        this.isAdmin = ((NuxeoPrincipal) getContext().getPrincipal())
+                .isAdministrator();
+    }
+
     @Path(value = "packages")
-    public Resource listPackages() {
-        return ctx.newObject("packageListingProvider");
+    public Object listPackages() {
+        return isAdmin ? ctx.newObject("packageListingProvider") :
+                unauthorized();
     }
 
     @Path(value = "download")
-    public Resource download() {
-        return ctx.newObject("downloadHandler");
+    public Object download() {
+        return isAdmin ? ctx.newObject("downloadHandler") : unauthorized();
     }
 
     @Path(value = "install")
-    public Resource install() {
-        return ctx.newObject("installHandler");
+    public Object install() {
+        return isAdmin ? ctx.newObject("installHandler") : unauthorized();
     }
 
     @Path(value = "uninstall")
-    public Resource uninstall() {
-        return ctx.newObject("uninstallHandler");
+    public Object uninstall() {
+        return isAdmin ? ctx.newObject("uninstallHandler") : unauthorized();
     }
 
     @Path(value = "remove")
-    public Resource remove() {
-        return ctx.newObject("removeHandler");
+    public Object remove() {
+        return isAdmin ? ctx.newObject("removeHandler") : unauthorized();
     }
 
     @GET
     @Produces("text/html")
     @Path(value = "restartView")
     public Object restartServerView() {
-        if (((NuxeoPrincipal) getContext().getPrincipal()).isAdministrator()) {
-            return getView("serverRestart").arg("nuxeoctl",
-                    new NuxeoCtlManager());
-        } else {
-            return Response.status(Response.Status.UNAUTHORIZED).build();
-        }
+        return isAdmin ? getView("serverRestart").arg("nuxeoctl",
+                new NuxeoCtlManager()) : unauthorized();
+    }
+
+    protected Response unauthorized() {
+        return Response.status(Response.Status.UNAUTHORIZED).build();
     }
 }
