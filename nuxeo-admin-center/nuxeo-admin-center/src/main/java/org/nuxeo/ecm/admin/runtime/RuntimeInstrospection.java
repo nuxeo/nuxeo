867009689b8e50642e08f9981768bde82f742db9
Julien Carsique <jcarsique@nuxeo.com>
2014-05-22 15:01:17 +0200

    NXP-14404: cleanup

--- nuxeo-admin-center/nuxeo-admin-center/src/main/java/org/nuxeo/ecm/admin/runtime/RuntimeInstrospection.java
+++ nuxeo-admin-center/nuxeo-admin-center/src/main/java/org/nuxeo/ecm/admin/runtime/RuntimeInstrospection.java
@@ -1,10 +1,10 @@
 /*
- * (C) Copyright 2006-2009 Nuxeo SAS (http://nuxeo.com/) and contributors.
+ * (C) Copyright 2006-2014 Nuxeo SA (http://nuxeo.com/) and contributors.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the GNU Lesser General Public License
  * (LGPL) version 2.1 which accompanies this distribution, and is available at
- * http://www.gnu.org/licenses/lgpl.html
+ * http://www.gnu.org/licenses/lgpl-2.1.html
  *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -13,8 +13,7 @@
  *
  * Contributors:
  *     Nuxeo - initial API and implementation
- *
- * $Id$
+ *     tdelprat, jcarsique
  */
 
 package org.nuxeo.ecm.admin.runtime;
@@ -54,11 +53,8 @@ public class RuntimeInstrospection {
         if (info == null) {
             RuntimeService runtime = Framework.getRuntime();
             Collection<RegistrationInfo> registrations = runtime.getComponentManager().getRegistrations();
-
-            bundleIds = new ArrayList<String>();
-
-            List<SimplifiedBundleInfo> bundles = new ArrayList<SimplifiedBundleInfo>();
-
+            bundleIds = new ArrayList<>();
+            List<SimplifiedBundleInfo> bundles = new ArrayList<>();
             for (RegistrationInfo ri : registrations) {
                 Bundle bundle = ri.getContext().getBundle();
                 if (bundle != null
@@ -70,7 +66,6 @@ public class RuntimeInstrospection {
                     }
                 }
             }
-
             Collections.sort(bundles);
             info = new SimplifiedServerInfo();
             info.setBundleInfos(bundles);
@@ -87,7 +82,6 @@ public class RuntimeInstrospection {
         return bundleIds;
     }
 
-    @SuppressWarnings("unchecked")
     protected static SimplifiedBundleInfo getBundleSimplifiedInfo(Bundle bundle) {
         SimplifiedBundleInfo result = null;
         if (bundle instanceof BundleImpl) {
