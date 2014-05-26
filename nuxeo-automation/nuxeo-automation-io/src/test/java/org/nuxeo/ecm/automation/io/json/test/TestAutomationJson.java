8b39296969bb09574d0c2f95760e9c4459bfa84c
Anahide Tchertchian <at@nuxeo.com>
2014-05-26 15:27:06 +0200

    NXP-14502: fix @since

--- nuxeo-automation/nuxeo-automation-io/src/test/java/org/nuxeo/ecm/automation/io/json/test/TestAutomationJson.java
+++ nuxeo-automation/nuxeo-automation-io/src/test/java/org/nuxeo/ecm/automation/io/json/test/TestAutomationJson.java
@@ -34,7 +34,7 @@ import org.nuxeo.runtime.test.runner.LocalDeploy;
 import com.google.inject.Inject;
 
 /**
- * @since 19
+ * @since 5.9.4
  */
 @RunWith(FeaturesRunner.class)
 @Features(PlatformFeature.class)
