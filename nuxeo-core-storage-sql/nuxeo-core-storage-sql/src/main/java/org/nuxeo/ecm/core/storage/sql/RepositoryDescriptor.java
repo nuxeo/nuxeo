nuxeo-archives/nuxeo-core

NXP-14142 adapt fixture logic for running tests in branch

nuxeo-core-storage-sql/nuxeo-core-storage-sql/src/main/java/org/nuxeo/ecm/core/storage/sql/RepositoryDescriptor.java
@@ -183,8 +183,13 @@ public class RepositoryDescriptor {
         return !Boolean.FALSE.equals(bool);
     }
 
-    @XNode("@name")
     public String name;
+
+    @XNode("@name")
+    public void setName(String name) {
+        this.name = name;
+        pool.setName("NuxeoConnectionManager/"+name);
+    }
 
     @XNode("@label")
     public String label;
@@ -212,11 +217,11 @@ public class RepositoryDescriptor {
     @XNode("repository")
     public RepositoryDescriptor repositoryDescriptor;
 
-    public NuxeoConnectionManagerConfiguration pool;
+    public NuxeoConnectionManagerConfiguration pool = new NuxeoConnectionManagerConfiguration();
 
     @XNode("pool")
     public void setPool(NuxeoConnectionManagerConfiguration pool) {
-        pool.setName(pool.getName() + "/" + name);
+        pool.setName("NuxeoConnectionManager/"+name);
         this.pool = pool;
     }
 

nuxeo/nuxeo

nuxeo-core-storage-sql/nuxeo-core-storage-sql/src/main/java/org/nuxeo/ecm/core/storage/sql/RepositoryDescriptor.java
@@ -184,8 +184,13 @@ public class RepositoryDescriptor {
         return !Boolean.FALSE.equals(bool);
     }
 
-    @XNode("@name")
     public String name;
+
+    @XNode("@name")
+    public void setName(String name) {
+        this.name = name;
+        pool.setName("repository/"+name);
+    }
 
     @XNode("@label")
     public String label;
@@ -213,7 +218,7 @@ public class RepositoryDescriptor {
     @XNode("repository")
     public RepositoryDescriptor repositoryDescriptor;
 
-    public NuxeoConnectionManagerConfiguration pool;
+    public NuxeoConnectionManagerConfiguration pool = new NuxeoConnectionManagerConfiguration();
 
     @XNode("pool")
     public void setPool(NuxeoConnectionManagerConfiguration pool) {