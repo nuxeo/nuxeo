b1931dca1409ba9f4f72d86b47090ea7d405185b
Thierry Delprat <tdelprat@nuxeo.com>
2014-07-25 17:17:34 +0200

    NXP-14785 Use Servlet EndPoint when Seam.Download is used for big files

--- nuxeo-automation/nuxeo-automation-jsf/src/main/java/org/nuxeo/ecm/automation/jsf/operations/DownloadFile.java
+++ nuxeo-automation/nuxeo-automation-jsf/src/main/java/org/nuxeo/ecm/automation/jsf/operations/DownloadFile.java
@@ -11,8 +11,15 @@
  */
 package org.nuxeo.ecm.automation.jsf.operations;
 
-import javax.faces.context.FacesContext;
+import java.io.IOException;
+import java.util.UUID;
 
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.nuxeo.ecm.automation.OperationContext;
 import org.nuxeo.ecm.automation.OperationException;
 import org.nuxeo.ecm.automation.core.Constants;
@@ -20,6 +27,9 @@ import org.nuxeo.ecm.automation.core.annotations.Context;
 import org.nuxeo.ecm.automation.core.annotations.Operation;
 import org.nuxeo.ecm.automation.core.annotations.OperationMethod;
 import org.nuxeo.ecm.core.api.Blob;
+import org.nuxeo.ecm.platform.ui.web.auth.NXAuthConstants;
+import org.nuxeo.ecm.platform.ui.web.tag.fn.Functions;
+import org.nuxeo.ecm.platform.ui.web.util.BaseURL;
 import org.nuxeo.ecm.platform.ui.web.util.ComponentUtils;
 
 /**
@@ -27,6 +37,8 @@ import org.nuxeo.ecm.platform.ui.web.util.ComponentUtils;
  */
 @Operation(id = DownloadFile.ID, category = Constants.CAT_UI, requires = Constants.SEAM_CONTEXT, label = "Download file", description = "Download a file")
 public class DownloadFile {
+
+    protected static Log log = LogFactory.getLog(DownloadFile.class);
 
     public static final String ID = "Seam.DownloadFile";
 
@@ -41,7 +53,41 @@ public class DownloadFile {
 
         FacesContext faces = FacesContext.getCurrentInstance();
         String filename = blob.getFilename();
-        ComponentUtils.download(faces, blob, filename);
+        if (blob.getLength() > Functions.getBigFileSizeLimit()) {
+
+            HttpServletResponse response = (HttpServletResponse) FacesContext.getCurrentInstance().getExternalContext().getResponse();
+            HttpServletRequest request = (HttpServletRequest) FacesContext.getCurrentInstance().getExternalContext().getRequest();
+
+            if (!blob.isPersistent()) {
+                blob.persist();
+            }
+
+            String sid = UUID.randomUUID().toString();
+            request.getSession(true).setAttribute(sid, blob);
+
+            String bigDownloadURL = BaseURL.getBaseURL(request);
+            bigDownloadURL += "nxbigblob" + "/" + sid;
+
+            try {
+                // Operation was probably triggered by a POST
+                // so we need to de-activate the ResponseWrapper that would
+                // rewrite the URL
+                request.setAttribute(
+                        NXAuthConstants.DISABLE_REDIRECT_REQUEST_KEY,
+                        new Boolean(true));
+                // send the redirect
+                response.sendRedirect(bigDownloadURL);
+                // mark all JSF processing as completed
+                response.flushBuffer();
+                FacesContext.getCurrentInstance().responseComplete();
+                // set Outcome to null (just in case)
+                ctx.getVars().put("Outcome", null);
+            } catch (IOException e) {
+                log.error("Error while redirecting for big blob downloader", e);
+            }
+        } else {
+            ComponentUtils.download(faces, blob, filename);
+        }
     }
 
 }
