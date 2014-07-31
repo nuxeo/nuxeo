f6314180389780bae15f961086f6a8dd49211b94
Gildas Lefevre <glefevre@nuxeo.com>
2014-07-31 11:34:48 +0200

    NXP-14713: Creation of a Favorites gadget

---
+++ nuxeo-opensocial-gadgets/src/main/resources/web/nuxeo.war/scripts/opensocial/gadgets/favorites.js
@@ -0,0 +1,26 @@
+var prefs = new gadgets.Prefs();
+
+// configure Automation REST call
+var NXRequestParams = {
+  operationId: 'Collection.GetElementsInFavorite',
+  operationParams: {},
+  operationContext: {},
+  operationInput: '/default-domain/UserWorkspaces/Administrator/',
+  operationDocumentProperties: "common,dublincore",
+  entityType: 'documents',
+  usePagination: true,
+  displayMethod: displayDocumentList,
+  displayColumns: [
+    {type: 'builtin', field: 'icon'},
+    {type: 'builtin', field: 'titleWithLink', label: prefs.getMsg('label.dublincore.title')},
+    {type: 'date', field: 'dc:modified', label: prefs.getMsg('label.dublincore.modified')},
+    {type: 'text', field: 'dc:creator', label: prefs.getMsg('label.dublincore.creator')}
+  ],
+  noEntryLabel: prefs.getMsg('label.gadget.no.document')
+};
+
+// execute automation request onload
+gadgets.util.registerOnLoadHandler(function() {
+  initContextPathSettingsButton();
+  doAutomationRequest(NXRequestParams);
+});
