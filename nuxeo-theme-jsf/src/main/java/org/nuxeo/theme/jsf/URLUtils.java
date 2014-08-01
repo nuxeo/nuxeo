c3fcae3c9dcbfd4df5fe021d063323dd06de0bfb
Damien Metzler <dmetzler@nuxeo.com>
2014-08-01 16:56:38 +0200

    NXP-14904 Use vhosthelper to compute serverUrl, remove some duplicated code

--- nuxeo-theme-jsf/src/main/java/org/nuxeo/theme/jsf/URLUtils.java
+++ nuxeo-theme-jsf/src/main/java/org/nuxeo/theme/jsf/URLUtils.java
@@ -21,19 +21,14 @@ package org.nuxeo.theme.jsf;
 
 import javax.faces.context.FacesContext;
 import javax.servlet.ServletRequest;
-import javax.servlet.http.HttpServletRequest;
-
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.nuxeo.ecm.platform.ui.web.util.BaseURL;
+import org.nuxeo.ecm.platform.web.common.vh.VirtualHostHelper;
 
 public final class URLUtils {
 
     private static final Log log = LogFactory.getLog(URLUtils.class);
-
-    private static final String X_FORWARDED_HOST = "x-forwarded-host";
-
-    private static final String VH_HEADER = "nuxeo-virtual-host";
 
     private URLUtils() {
     }
@@ -46,64 +41,11 @@ public final class URLUtils {
      * @return Server URL as : protocol://serverName:port/
      */
     public static String getServerURL(ServletRequest request) {
-        String baseURL = null;
-        final FacesContext facesContext = FacesContext.getCurrentInstance();
-        if (facesContext != null || request != null) {
-
-            StringBuilder sbaseURL = new StringBuilder();
-
-            if (request == null) {
-                request = (ServletRequest) facesContext.getExternalContext().getRequest();
-            }
-
-            if (request instanceof HttpServletRequest) {
-                HttpServletRequest httpRequest = (HttpServletRequest) request;
-
-                String serverName = httpRequest.getServerName();
-                int serverPort = httpRequest.getServerPort();
-
-                // Detect Nuxeo specific header for VH
-                String nuxeoVH = httpRequest.getHeader(VH_HEADER);
-                if (nuxeoVH != null && nuxeoVH.contains("http")) {
-                    return nuxeoVH;
-                }
-
-                // Detect virtual hosting based in standard header
-                String forwardedHost = httpRequest.getHeader(X_FORWARDED_HOST);
-
-                // :XXX: Need to test out with different proxy configurations
-                // especially with several proxies routing the request.
-                if (forwardedHost != null) {
-                    if (forwardedHost.contains(":")) {
-                        serverName = forwardedHost.split(":")[0];
-                        serverPort = Integer.valueOf(forwardedHost.split(":")[1]);
-                    } else {
-                        serverPort = 80; // fallback
-                        serverName = forwardedHost;
-                    }
-                }
-
-                // :XXX: To be tested in case of virtual hosting...
-                String protocol = httpRequest.getScheme();
-
-                sbaseURL.append(protocol);
-                sbaseURL.append("://");
-                sbaseURL.append(serverName);
-                if (serverPort != 0) {
-                    if ("http".equals(protocol) && serverPort != 80
-                            || "https".equals(protocol) && serverPort != 443) {
-                        sbaseURL.append(':');
-                        sbaseURL.append(serverPort);
-                    }
-                }
-                sbaseURL.append('/');
-                baseURL = sbaseURL.toString();
-            }
+        if (request == null) {
+            final FacesContext facesContext = FacesContext.getCurrentInstance();
+            request = (ServletRequest) facesContext.getExternalContext().getRequest();
         }
-        if (baseURL == null) {
-            log.error("Could not retrieve base url correctly");
-        }
-        return baseURL;
+        return VirtualHostHelper.getServerURL(request);
     }
 
     /**
@@ -117,15 +59,7 @@ public final class URLUtils {
      * @return base URL as protocol://serverName:port/webappName/
      */
     public static String getBaseURL() {
-        String baseURL = null;
-        String serverUrl = getServerURL();
-        if (serverUrl != null) {
-            baseURL = serverUrl + getWebAppName() + '/';
-        }
-        if (baseURL == null) {
-            log.error("Could not retrieve base url correctly");
-        }
-        return baseURL;
+        return getBaseURL(null);
     }
 
     public static String getBaseURL(ServletRequest request) {
