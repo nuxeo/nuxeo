7bcfd5e586bba4904b54e4d8825f0b1414d308bf
Julien Carsique <jcarsique@nuxeo.com>
2014-08-05 16:26:08 +0200

    NXP-13231: cleanup and format

--- nuxeo-template-rendering-core/src/main/java/org/nuxeo/template/processors/AbstractTemplateProcessor.java
+++ nuxeo-template-rendering-core/src/main/java/org/nuxeo/template/processors/AbstractTemplateProcessor.java
@@ -1,10 +1,10 @@
 /*
- * (C) Copyright 2006-20012 Nuxeo SAS (http://nuxeo.com/) and contributors.
+ * (C) Copyright 2006-2014 Nuxeo SA (http://nuxeo.com/) and contributors.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the GNU Lesser General Public License
  * (LGPL) version 2.1 which accompanies this distribution, and is available at
- * http://www.gnu.org/licenses/lgpl.html
+ * http://www.gnu.org/licenses/lgpl-2.1.html
  *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -21,6 +21,7 @@ import java.io.File;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+
 import org.nuxeo.common.utils.FileUtils;
 import org.nuxeo.ecm.core.api.Blob;
 import org.nuxeo.template.api.TemplateProcessor;
@@ -28,9 +29,9 @@ import org.nuxeo.template.api.adapters.TemplateBasedDocument;
 
 /**
  * Common code between the implementations of {@link TemplateProcessor}
- * 
+ *
  * @author Tiry (tdelprat@nuxeo.com)
- * 
+ *
  */
 public abstract class AbstractTemplateProcessor implements TemplateProcessor {
 
