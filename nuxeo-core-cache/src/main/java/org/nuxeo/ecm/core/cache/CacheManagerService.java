27f89dc7df8fc294434efb6a86d3bf724bef53e4
Maxime HILAIRE <mhilaire@nuxeo.com>
2014-08-19 18:06:35 +0200

    NXP-14929 Provide a unified cache service
    Rename CacheManager to Cache

--- nuxeo-core-cache/src/main/java/org/nuxeo/ecm/core/cache/CacheManagerService.java
+++
@@ -1,7 +0,0 @@
-package org.nuxeo.ecm.core.cache;
-
-public interface CacheManagerService {
-    public static final String INVALIDATE_ALL = "invalidateAll";
-    
-    public CacheManager<?,?> getCacheManager(String name);
-}
