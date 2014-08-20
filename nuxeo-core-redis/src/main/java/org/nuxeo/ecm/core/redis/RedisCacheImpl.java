1844a110b461777195f1f11faa38e28f359587ae
Maxime HILAIRE <mhilaire@nuxeo.com>
2014-08-20 16:09:30 +0200

    NXP-14929 Provide a unified cache service
    Impl a redis cache and impl unit test

---
+++ nuxeo-core-redis/src/main/java/org/nuxeo/ecm/core/redis/RedisCacheImpl.java
@@ -0,0 +1,183 @@
+/*
+ * (C) Copyright 2014 Nuxeo SAS (http://nuxeo.com/) and contributors.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser General Public License
+ * (LGPL) version 2.1 which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl.html
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * Contributors:
+ *     Maxime Hilaire
+ *
+ */
+package org.nuxeo.ecm.core.redis;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.nuxeo.ecm.core.cache.AbstractCache;
+import org.nuxeo.runtime.api.Framework;
+import org.nuxeo.runtime.services.event.Event;
+
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+
+/**
+ * Default in memory implementation for cache management based on guava
+ * 
+ * @since 5.9.6
+ */
+public class RedisCacheImpl extends AbstractCache {
+
+    protected static final String UTF_8 = "UTF-8";
+
+    private static final Log log = LogFactory.getLog(RedisCacheImpl.class);
+
+    private RedisService redisService = null;
+
+
+    protected String prefix;
+
+    /**
+     * Prefix for keys, added after the globally configured prefix of the
+     * {@link RedisService}.
+     */
+    public static final String PREFIX = "cache:";
+
+    public RedisCacheImpl() {
+        redisService = getRedisService();
+    }
+
+    protected RedisService getRedisService() {
+        if (redisService == null) {
+            redisService = Framework.getLocalService(RedisService.class);
+            prefix = redisService.getPrefix() + PREFIX + name + ":";
+        }
+        return redisService;
+    }
+
+    protected Jedis getJedis() {
+        RedisService redisService = getRedisService();
+        if (redisService == null) {
+            return null;
+        }
+        JedisPool jedisPool = redisService.getJedisPool();
+        if (jedisPool == null) {
+            return null;
+        }
+        return jedisPool.getResource();
+    }
+
+    protected void closeJedis(Jedis jedis) {
+        if(getRedisService().getJedisPool() != null)
+        {
+            getRedisService().getJedisPool().returnResource(jedis);
+        }
+    }
+    
+    protected Serializable deserializeValue(byte[] workBytes) throws IOException {
+        if (workBytes == null) {
+            return null;
+        }
+        InputStream bain = new ByteArrayInputStream(workBytes);
+        ObjectInputStream in = new ObjectInputStream(bain);
+        try {
+            return (Serializable) in.readObject();
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    protected static byte[] bytes(String string) {
+        try {
+            return string.getBytes(UTF_8);
+        } catch (IOException e) {
+            // cannot happen for UTF-8
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public Serializable get(String key) {
+        Jedis jedis = getJedis();
+        try {
+            return deserializeValue(jedis.get(bytes(prefix + key)));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        finally
+        {
+            closeJedis(jedis);
+        }
+    }
+
+    protected byte[] serializeValue(Serializable value) throws IOException {
+        ByteArrayOutputStream baout = new ByteArrayOutputStream();
+        ObjectOutputStream out = new ObjectOutputStream(baout);
+        out.writeObject(value);
+        out.flush();
+        out.close();
+        return baout.toByteArray();
+    }
+
+    @Override
+    public void invalidate(String key) {
+        Jedis jedis = getJedis();
+        try {
+            jedis.del(key);
+        }
+        finally
+        {
+            closeJedis(jedis);
+        }
+    }
+
+    @Override
+    public void invalidateAll() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void put(String key, Serializable value) {
+        Jedis jedis = getJedis();
+        try {
+            byte[] bkey = bytes(prefix + key);
+            jedis.set(bkey, serializeValue(value));
+            jedis.expire(bkey, ttl);
+        } catch (IOException e) {
+            log.error(
+                    String.format("Could not set value in the cache %s", name),
+                    e);
+            throw new RuntimeException(e);
+        } finally {
+            closeJedis(jedis);
+        }
+    }
+
+    @Override
+    public boolean aboutToHandleEvent(Event event) {
+        // TODO Auto-generated method stub
+        // return false;
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void handleEvent(Event event) {
+        // TODO Auto-generated method stub
+        //
+        throw new UnsupportedOperationException();
+    }
+
+}
---
+++ nuxeo-core-redis/src/test/java/org/nuxeo/ecm/core/cache/redis/test/TestRedisCacheService.java
@@ -0,0 +1,111 @@
+package org.nuxeo.ecm.core.cache.redis.test;
+
+/*
+ * (C) Copyright 2014 Nuxeo SAS (http://nuxeo.com/) and contributors.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser General Public License
+ * (LGPL) version 2.1 which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl.html
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * Contributors:
+ *     mhilaire
+ *
+ * $Id: TestMultiDirectory.java 30378 2008-02-20 17:37:26Z gracinet $
+ */
+
+import static org.junit.Assume.assumeTrue;
+import junit.framework.Assert;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.nuxeo.ecm.core.cache.Cache;
+import org.nuxeo.ecm.core.cache.CacheService;
+import org.nuxeo.ecm.core.redis.RedisCacheImpl;
+import org.nuxeo.ecm.core.redis.RedisConfigurationDescriptor;
+import org.nuxeo.ecm.core.redis.RedisService;
+import org.nuxeo.ecm.core.redis.RedisServiceImpl;
+import org.nuxeo.ecm.core.test.CoreFeature;
+import org.nuxeo.ecm.core.test.DefaultRepositoryInit;
+import org.nuxeo.ecm.core.test.annotations.RepositoryConfig;
+import org.nuxeo.runtime.api.Framework;
+import org.nuxeo.runtime.test.NXRuntimeTestCase;
+import org.nuxeo.runtime.test.runner.Deploy;
+import org.nuxeo.runtime.test.runner.Features;
+import org.nuxeo.runtime.test.runner.FeaturesRunner;
+import org.nuxeo.runtime.test.runner.RuntimeHarness;
+
+import com.google.inject.Inject;
+
+/**
+ * @author Maxime Hilaire
+ *
+ */
+
+@RunWith(FeaturesRunner.class)
+@Features(CoreFeature.class)
+@RepositoryConfig(init = DefaultRepositoryInit.class)
+@Deploy({ "org.nuxeo.ecm.core.schema", "org.nuxeo.ecm.core.cache",
+        "org.nuxeo.ecm.core.redis" })
+public class TestRedisCacheService extends NXRuntimeTestCase {
+
+    private static final String TEST_BUNDLE = "org.nuxeo.ecm.core.redis.tests";
+
+    protected Cache redisCache;
+
+    @Inject
+    protected CacheService cacheService;
+
+    @Inject
+    protected RuntimeHarness harness;
+
+    private RedisConfigurationDescriptor redisConfigurationDescriptor;
+
+    protected static String CACHE_NAME = "redis-test-cache";
+
+    @Before
+    public void setUp() throws Exception {
+
+        
+        
+        redisConfigurationDescriptor = RedisTestHelper.getRedisConfigurationDescriptor();
+        boolean enabled = redisConfigurationDescriptor != null;
+        assumeTrue(enabled);
+        RedisServiceImpl redisService = (RedisServiceImpl) Framework.getLocalService(RedisService.class);
+        redisService.registerConfiguration(redisConfigurationDescriptor);
+        RedisTestHelper.clearRedis(redisService);
+        
+
+        // Config for the tested bundle
+        harness.deployContrib(TEST_BUNDLE, "OSGI-INF/redis-cache-config.xml");
+        
+        cacheService = Framework.getLocalService(CacheService.class);
+    }
+
+    @Test
+    public void getCache() {
+        redisCache = (RedisCacheImpl) cacheService.getCache(CACHE_NAME);
+
+        Assert.assertNotNull(redisCache);
+        
+         redisCache.put("key1", "val1");
+        
+         String testGet = (String)redisCache.get("key1");
+         Assert.assertNotNull(testGet);
+         Assert.assertEquals(testGet, "val1");
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        // cacheManagerService.unregisterExtension();
+    }
+
+}
