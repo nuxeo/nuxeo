32341a0bfcebf065874949b2efdf689936f9c9db
Thierry Martins <tmartins@nuxeo.com>
2014-08-21 14:00:13 +0200

    NXP-14999: make usage of setDocuments method more consistent by only setting docIds

--- nuxeo-core-event/src/main/java/org/nuxeo/ecm/core/work/AbstractWork.java
+++ nuxeo-core-event/src/main/java/org/nuxeo/ecm/core/work/AbstractWork.java
@@ -161,13 +161,8 @@ public abstract class AbstractWork implements Work {
 
     public void setDocuments(String repositoryName, List<String> docIds) {
         this.repositoryName = repositoryName;
-        if (docIds.size() == 1) {
-            docId = docIds.get(0);
-            this.docIds = null;
-        } else {
-            docId = null;
-            this.docIds = new ArrayList<String>(docIds);
-        }
+        docId = null;
+        this.docIds = new ArrayList<String>(docIds);
     }
 
     @Override
