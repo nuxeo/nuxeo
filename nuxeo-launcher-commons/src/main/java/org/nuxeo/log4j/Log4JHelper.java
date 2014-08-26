d5f0a85d0bb269f352a00d3527abdd5ec09b58b5
Julien Carsique <jcarsique@nuxeo.com>
2014-08-26 16:53:35 +0200

    NXP-15046: get all Log4J appenders

--- nuxeo-launcher-commons/src/main/java/org/nuxeo/log4j/Log4JHelper.java
+++ nuxeo-launcher-commons/src/main/java/org/nuxeo/log4j/Log4JHelper.java
@@ -1,10 +1,10 @@
 /*
- * (C) Copyright 2011-2012 Nuxeo SA (http://nuxeo.com/) and contributors.
+ * (C) Copyright 2011-2014 Nuxeo SA (http://nuxeo.com/) and contributors.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the GNU Lesser General Public License
  * (LGPL) version 2.1 which accompanies this distribution, and is available at
- * http://www.gnu.org/licenses/lgpl.html
+ * http://www.gnu.org/licenses/lgpl-2.1.html
  *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -72,6 +72,17 @@ public class Log4JHelper {
             if (appender instanceof FileAppender) {
                 FileAppender fileAppender = (FileAppender) appender;
                 logFiles.add(fileAppender.getFile());
+            }
+        }
+        Enumeration<Logger> currentLoggers = loggerRepository.getCurrentLoggers();
+        while (currentLoggers.hasMoreElements()) {
+            Logger logger = (currentLoggers.nextElement());
+            for (Enumeration<Appender> appenders = logger.getAllAppenders(); appenders.hasMoreElements();) {
+                Appender appender = appenders.nextElement();
+                if (appender instanceof FileAppender) {
+                    FileAppender fileAppender = (FileAppender) appender;
+                    logFiles.add(fileAppender.getFile());
+                }
             }
         }
         return logFiles;
