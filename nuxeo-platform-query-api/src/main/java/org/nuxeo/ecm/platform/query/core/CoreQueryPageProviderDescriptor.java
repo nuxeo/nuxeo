560d4437cc0345c5e82c234847c6cfc749c8d688
Benoit Delbosc <bdelbosc@nuxeo.com>
2014-08-26 15:13:57 +0200

    NXP-14879: Remove code duplication on descriptor since xmap can be extended now

--- nuxeo-platform-query-api/src/main/java/org/nuxeo/ecm/platform/query/core/CoreQueryPageProviderDescriptor.java
+++ nuxeo-platform-query-api/src/main/java/org/nuxeo/ecm/platform/query/core/CoreQueryPageProviderDescriptor.java
@@ -18,16 +18,10 @@ package org.nuxeo.ecm.platform.query.core;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
-import org.nuxeo.common.xmap.annotation.XNode;
-import org.nuxeo.common.xmap.annotation.XNodeList;
-import org.nuxeo.common.xmap.annotation.XNodeMap;
 import org.nuxeo.common.xmap.annotation.XObject;
-import org.nuxeo.ecm.core.api.SortInfo;
 import org.nuxeo.ecm.platform.query.api.PageProviderDefinition;
-import org.nuxeo.ecm.platform.query.api.WhereClauseDefinition;
 
 /**
  * Core Query page provider descriptor.
@@ -36,126 +30,10 @@ import org.nuxeo.ecm.platform.query.api.WhereClauseDefinition;
  * @since 5.4
  */
 @XObject("coreQueryPageProvider")
-public class CoreQueryPageProviderDescriptor implements PageProviderDefinition {
+public class CoreQueryPageProviderDescriptor extends BasePageProviderDescriptor
+        implements PageProviderDefinition {
 
     private static final long serialVersionUID = 1L;
-
-    @XNode("@name")
-    protected String name;
-
-    @XNode("@enabled")
-    protected boolean enabled = true;
-
-    @XNodeMap(value = "property", key = "@name", type = HashMap.class, componentType = String.class)
-    protected Map<String, String> properties = new HashMap<String, String>();
-
-    @XNodeList(value = "parameter", type = String[].class, componentType = String.class)
-    protected String[] queryParameters;
-
-    @XNode("pageSize")
-    protected long pageSize = 0;
-
-    @XNode("pageSizeBinding")
-    protected String pageSizeBinding;
-
-    @XNode("maxPageSize")
-    protected Long maxPageSize;
-
-    @XNode("sortable")
-    protected boolean sortable = true;
-
-    @XNodeList(value = "sort", type = ArrayList.class, componentType = SortInfoDescriptor.class)
-    protected List<SortInfoDescriptor> sortInfos;
-
-    @XNode("sortInfosBinding")
-    protected String sortInfosBinding;
-
-    protected String pattern;
-
-    @XNode("pattern@quoteParameters")
-    protected boolean quotePatternParameters = true;
-
-    @XNode("pattern@escapeParameters")
-    protected boolean escapePatternParameters = true;
-
-    @XNode("whereClause")
-    protected WhereClauseDescriptor whereClause;
-
-    @XNode("pattern")
-    public void setPattern(String pattern) {
-        // remove new lines and following spaces
-        if (pattern != null) {
-            this.pattern = pattern.replaceAll("\r?\n\\s*", " ");
-        }
-    }
-
-    public boolean getQuotePatternParameters() {
-        return quotePatternParameters;
-    }
-
-    public boolean getEscapePatternParameters() {
-        return escapePatternParameters;
-    }
-
-    public Map<String, String> getProperties() {
-        return properties;
-    }
-
-    public String[] getQueryParameters() {
-        return queryParameters;
-    }
-
-    public String getPattern() {
-        return pattern;
-    }
-
-    public WhereClauseDefinition getWhereClause() {
-        return whereClause;
-    }
-
-    public boolean isSortable() {
-        return sortable;
-    }
-
-    public List<SortInfo> getSortInfos() {
-        List<SortInfo> res = new ArrayList<SortInfo>();
-        if (sortInfos != null) {
-            for (SortInfoDescriptor sortInfo : sortInfos) {
-                res.add(sortInfo.getSortInfo());
-            }
-        }
-        return res;
-    }
-
-    public long getPageSize() {
-        return pageSize;
-    }
-
-    public boolean isEnabled() {
-        return enabled;
-    }
-
-    @Override
-    public void setEnabled(boolean enabled) {
-        this.enabled = enabled;
-    }
-
-    public String getPageSizeBinding() {
-        return pageSizeBinding;
-    }
-
-    public String getSortInfosBinding() {
-        return sortInfosBinding;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    @Override
-    public Long getMaxPageSize() {
-        return maxPageSize;
-    }
 
     /**
      * @since 5.6
