74a6517efadbecd2fa7a243109895214ae49f0a4
Maxime HILAIRE <mhilaire@nuxeo.com>
2014-08-27 14:58:27 +0200

    NXP-14930 setup unit test to switch on redis

--- nuxeo-platform-directory/nuxeo-platform-directory-ldap/src/test/java/org/nuxeo/ecm/directory/ldap/TestCachedLDAPSession.java
+++ nuxeo-platform-directory/nuxeo-platform-directory-ldap/src/test/java/org/nuxeo/ecm/directory/ldap/TestCachedLDAPSession.java
@@ -19,28 +19,60 @@
 
 package org.nuxeo.ecm.directory.ldap;
 
+import static org.junit.Assume.assumeTrue;
+
 import java.util.Arrays;
 import java.util.List;
 
+import org.nuxeo.ecm.core.redis.RedisConfigurationDescriptor;
+import org.nuxeo.ecm.core.redis.RedisService;
+import org.nuxeo.ecm.core.redis.RedisServiceImpl;
+import org.nuxeo.ecm.core.redis.RedisTestHelper;
 import org.nuxeo.ecm.directory.DirectoryCache;
+import org.nuxeo.runtime.api.Framework;
 
 /**
- * @author <a href="mailto:ogrisel@nuxeo.com">Olivier Grisel</a>
+ * Test class for LDAP directory that use cache
  */
 public class TestCachedLDAPSession extends TestLDAPSession {
 
-    protected final static String CACHE_CONTRIB = "ldap-directory-cache-config.xml";
+    protected final static String CACHE_CONFIG = "ldap-directory-cache-config.xml";
+
+    protected final static String REDIS_CACHE_CONFIG = "ldap-directory-cache-config.xml";
 
     protected final static String ENTRY_CACHE_NAME = "ldap-entry-cache";
 
     protected final static String ENTRY_CACHE_WITHOUT_REFERENCES_NAME = "ldap-entry-cache-without-references";
+
+    // Set to true to use redis cache implementation for unit test
+    // Make sure you have started your redis server and that you compile the
+    // RedisTestHelper class
+    // with the server parameters pointing to your configuration
+    protected final static boolean USE_REDIS = false;
+
+    private RedisConfigurationDescriptor redisConfigurationDescriptor;
 
     @Override
     public void setUp() throws Exception {
         super.setUp();
 
         deployBundle("org.nuxeo.ecm.core.cache");
-        deployTestContrib(TEST_BUNDLE, CACHE_CONTRIB);
+
+        if (USE_REDIS) {
+            
+            //deployBundle("org.nuxeo.ecm.core.redis");
+            deployTestContrib(TEST_BUNDLE, "OSGI-INF/redis-service.xml");
+            redisConfigurationDescriptor = RedisTestHelper.getRedisConfigurationDescriptor();
+            boolean enabled = redisConfigurationDescriptor != null;
+            assumeTrue(enabled);
+            RedisServiceImpl redisService = (RedisServiceImpl) Framework.getLocalService(RedisService.class);
+            redisService.registerConfiguration(redisConfigurationDescriptor);
+            RedisTestHelper.clearRedis(redisService);
+            deployTestContrib(TEST_BUNDLE, REDIS_CACHE_CONFIG);
+
+        } else {
+            deployTestContrib(TEST_BUNDLE, CACHE_CONFIG);
+        }
 
         List<String> directories = Arrays.asList("userDirectory",
                 "groupDirectory");
