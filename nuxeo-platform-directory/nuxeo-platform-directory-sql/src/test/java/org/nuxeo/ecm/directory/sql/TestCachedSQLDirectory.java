bf844f96d471fdb5473a39995a625bfcb8b67c80
Maxime HILAIRE <mhilaire@nuxeo.com>
2014-08-27 17:00:01 +0200

    NXP-14930 update sql directory unit test to switch on redis cache

--- nuxeo-platform-directory/nuxeo-platform-directory-sql/src/test/java/org/nuxeo/ecm/directory/sql/TestCachedSQLDirectory.java
+++ nuxeo-platform-directory/nuxeo-platform-directory-sql/src/test/java/org/nuxeo/ecm/directory/sql/TestCachedSQLDirectory.java
@@ -19,27 +19,49 @@
 
 package org.nuxeo.ecm.directory.sql;
 
-import java.util.Arrays;
-import java.util.List;
-
+import org.nuxeo.ecm.core.redis.RedisConfigurationDescriptor;
+import org.nuxeo.ecm.core.redis.RedisService;
+import org.nuxeo.ecm.core.redis.RedisServiceImpl;
+import org.nuxeo.ecm.core.redis.RedisTestHelper;
 import org.nuxeo.ecm.directory.AbstractDirectory;
 import org.nuxeo.ecm.directory.DirectoryCache;
+import org.nuxeo.runtime.api.Framework;
 
 public class TestCachedSQLDirectory extends TestSQLDirectory {
 
     protected final static String CACHE_CONTRIB = "sql-directory-cache-config.xml";
 
+    protected final static String REDIS_CACHE_CONFIG = "sql-directory-redis-cache-config.xml";
+
     protected final static String ENTRY_CACHE_NAME = "sql-entry-cache";
 
     protected final static String ENTRY_CACHE_WITHOUT_REFERENCES_NAME = "sql-entry-cache-without-references";
+
+    protected static final boolean USE_REDIS = false;
 
     @Override
     public void setUp() throws Exception {
         super.setUp();
 
         deployBundle("org.nuxeo.ecm.core.cache");
-        deployTestContrib("org.nuxeo.ecm.directory.sql.tests", CACHE_CONTRIB);
 
+        if (USE_REDIS) {
+
+            deployBundle("org.nuxeo.ecm.core.redis");
+            RedisConfigurationDescriptor redisConfigurationDescriptor = RedisTestHelper.getRedisConfigurationDescriptor();
+            boolean enabled = redisConfigurationDescriptor != null;
+            if (enabled) {
+                RedisServiceImpl redisService = (RedisServiceImpl) Framework.getLocalService(RedisService.class);
+                redisService.registerConfiguration(redisConfigurationDescriptor);
+                RedisTestHelper.clearRedis(redisService);
+                deployTestContrib("org.nuxeo.ecm.directory.sql.tests",
+                        REDIS_CACHE_CONFIG);
+            }
+
+        } else {
+            deployTestContrib("org.nuxeo.ecm.directory.sql.tests",
+                    CACHE_CONTRIB);
+        }
         AbstractDirectory dir = getSQLDirectory();
 
         DirectoryCache cache = dir.getCache();
