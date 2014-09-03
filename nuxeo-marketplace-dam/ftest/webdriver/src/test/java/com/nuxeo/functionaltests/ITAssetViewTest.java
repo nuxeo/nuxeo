d2927d63b299dd7da61c7fbbb89c71c8aa15ddcf
Anahide Tchertchian <at@nuxeo.com>
2014-09-03 18:09:36 +0200

    NXP-15004: avoid triggering the 'save' popup on DAM ajaxified toggleable layout

--- nuxeo-marketplace-dam/ftest/webdriver/src/test/java/com/nuxeo/functionaltests/ITAssetViewTest.java
+++ nuxeo-marketplace-dam/ftest/webdriver/src/test/java/com/nuxeo/functionaltests/ITAssetViewTest.java
@@ -29,6 +29,7 @@ import org.nuxeo.functionaltests.dam.SearchResultsFragment;
 import org.nuxeo.functionaltests.forms.LayoutElement;
 import org.nuxeo.functionaltests.fragment.WebFragment;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
@@ -158,6 +159,10 @@ public class ITAssetViewTest extends AbstractDAMTest {
         ipBox.open();
         ipBox.waitForTextToBePresent("New holder");
 
+        // avoid triggering the "save" popup (FIXME: it should not be triggered
+        // here anyway)
+        ((JavascriptExecutor) driver).executeScript("window.onbeforeunload = function(e){};");
+        ((JavascriptExecutor) driver).executeScript("jQuery(window).unbind('unload');");
         logout();
     }
 }
