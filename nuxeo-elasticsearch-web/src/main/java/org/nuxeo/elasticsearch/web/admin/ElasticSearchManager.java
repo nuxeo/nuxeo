0136f3277e7425f95eb287c3ff1504e1d5f956c5
Benoit Delbosc <bdelbosc@nuxeo.com>
2014-09-03 10:56:18 +0200

    NXP-15110: Add a way to reindex from a root docid

--- nuxeo-elasticsearch-web/src/main/java/org/nuxeo/elasticsearch/web/admin/ElasticSearchManager.java
+++ nuxeo-elasticsearch-web/src/main/java/org/nuxeo/elasticsearch/web/admin/ElasticSearchManager.java
@@ -27,11 +27,11 @@ import org.apache.commons.logging.LogFactory;
 import org.elasticsearch.action.admin.cluster.health.ClusterHealthResponse;
 import org.elasticsearch.action.admin.cluster.node.info.NodesInfoResponse;
 import org.elasticsearch.action.admin.cluster.node.stats.NodesStatsResponse;
-import org.elasticsearch.action.admin.cluster.tasks.PendingClusterTasksResponse;
 import org.jboss.seam.annotations.In;
 import org.jboss.seam.annotations.Name;
 import org.jboss.seam.annotations.Scope;
 import org.nuxeo.ecm.core.api.CoreSession;
+import org.nuxeo.ecm.core.api.IdRef;
 import org.nuxeo.ecm.platform.contentview.jsf.ContentView;
 import org.nuxeo.ecm.platform.contentview.jsf.ContentViewService;
 import org.nuxeo.ecm.platform.query.api.PageProviderDefinition;
@@ -72,6 +72,8 @@ public class ElasticSearchManager {
 
     protected Timer indexTimer;
 
+    private String rootId;
+
     public String getNodesInfo() {
         NodesInfoResponse nodesInfo = esa.getClient().admin().cluster()
                 .prepareNodesInfo().execute().actionGet();
@@ -94,6 +96,17 @@ public class ElasticSearchManager {
         log.warn("Start re-indexing repository");
         IndexingCommand cmd = new IndexingCommand(
                 documentManager.getRootDocument(), false, true);
+        esi.scheduleIndexing(cmd);
+    }
+
+    public void startReindexFrom() throws Exception {
+        if (rootId == null) {
+            startReindex();
+            return;
+        }
+        log.warn(String.format("Start re-indexing from %s repository", rootId));
+        IndexingCommand cmd = new IndexingCommand(
+                documentManager.getDocument(new IdRef(rootId)), false, true);
         esi.scheduleIndexing(cmd);
     }
 
@@ -181,5 +194,13 @@ public class ElasticSearchManager {
         return String.format("%.2f, %.2f, %.2f", indexTimer.getOneMinuteRate(),
                 indexTimer.getFiveMinuteRate(),
                 indexTimer.getFifteenMinuteRate());
+    }
+
+    public String getRootId() {
+        return rootId;
+    }
+
+    public void setRootId(String rootId) {
+        this.rootId = rootId;
     }
 }
