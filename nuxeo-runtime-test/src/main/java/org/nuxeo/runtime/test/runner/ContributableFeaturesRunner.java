3c97e1caa827b31d7181b47547603a1974b00f42
Stephane Lacoin aka nxmatic <slacoin@nuxeo.com>
2014-09-03 12:00:42 +0200

    NXP-15102 fixing copyrights

--- nuxeo-runtime-test/src/main/java/org/nuxeo/runtime/test/runner/ContributableFeaturesRunner.java
+++ nuxeo-runtime-test/src/main/java/org/nuxeo/runtime/test/runner/ContributableFeaturesRunner.java
@@ -1,3 +1,11 @@
+/*******************************************************************************
+ * Copyright (c) 2006-2014 Nuxeo SA (http://nuxeo.com/) and others.
+ * 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ ******************************************************************************/
 package org.nuxeo.runtime.test.runner;
 
 import org.junit.runner.Runner;
