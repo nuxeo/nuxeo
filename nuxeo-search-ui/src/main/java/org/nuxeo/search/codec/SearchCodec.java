327548102912cc9a25abbc5ec45c1e1c3f2913c1
Thomas Roger <troger@nuxeo.com>
2014-09-08 12:12:33 +0200

    NXP-15053: add new Search view

---
+++ nuxeo-search-ui/src/main/java/org/nuxeo/search/codec/SearchCodec.java
@@ -0,0 +1,52 @@
+/*
+ * (C) Copyright 2014 Nuxeo SA (http://nuxeo.com/) and others.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser General Public License
+ * (LGPL) version 2.1 which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl-2.1.html
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * Contributors:
+ *     Thomas Roger
+ */
+
+package org.nuxeo.search.codec;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.nuxeo.ecm.platform.url.api.DocumentView;
+import org.nuxeo.ecm.platform.url.codec.DocumentPathCodec;
+
+/**
+ * Codec used for 'nxsearch' URLs.
+ *
+ * @since 5.9.6
+ */
+public class SearchCodec extends DocumentPathCodec {
+
+    private static final Log log = LogFactory.getLog(SearchCodec.class);
+
+    public static final String PREFIX = "nxsearch";
+
+    @Override
+    public String getPrefix() {
+        if (prefix != null) {
+            return prefix;
+        }
+        return PREFIX;
+    }
+
+    /**
+     * Never handle document views: this codec is useless on post requests.
+     */
+    @Override
+    public boolean handleDocumentView(DocumentView docView) {
+        return false;
+    }
+
+}
---
+++ nuxeo-search-ui/src/main/java/org/nuxeo/search/seam/SearchActions.java
@@ -0,0 +1,485 @@
+/*
+ * (C) Copyright 2014 Nuxeo SA (http://nuxeo.com/) and others.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser General Public License
+ * (LGPL) version 2.1 which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl-2.1.html
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * Contributors:
+ *     Thomas Roger
+ */
+
+package org.nuxeo.search.seam;
+
+import static org.apache.commons.logging.LogFactory.getLog;
+import static org.jboss.seam.ScopeType.CONVERSATION;
+import static org.jboss.seam.annotations.Install.FRAMEWORK;
+
+import org.apache.commons.lang.StringUtils;
+import org.apache.commons.logging.Log;
+import org.jboss.seam.annotations.Begin;
+import org.jboss.seam.annotations.In;
+import org.jboss.seam.annotations.Install;
+import org.jboss.seam.annotations.Name;
+import org.jboss.seam.annotations.Observer;
+import org.jboss.seam.annotations.Scope;
+import org.jboss.seam.annotations.intercept.BypassInterceptors;
+import org.nuxeo.ecm.core.api.ClientException;
+import org.nuxeo.ecm.core.api.CoreSession;
+import org.nuxeo.ecm.core.api.DocumentModel;
+import org.nuxeo.ecm.core.api.IdRef;
+import org.nuxeo.ecm.core.api.PathRef;
+import org.nuxeo.ecm.core.api.impl.DocumentLocationImpl;
+import org.nuxeo.ecm.platform.contentview.jsf.ContentView;
+import org.nuxeo.ecm.platform.contentview.jsf.ContentViewHeader;
+import org.nuxeo.ecm.platform.contentview.jsf.ContentViewService;
+import org.nuxeo.ecm.platform.contentview.jsf.ContentViewState;
+import org.nuxeo.ecm.platform.contentview.json.JSONContentViewState;
+import org.nuxeo.ecm.platform.contentview.seam.ContentViewActions;
+import org.nuxeo.ecm.platform.query.api.PageProvider;
+import org.nuxeo.ecm.platform.query.api.PageProviderService;
+import org.nuxeo.ecm.platform.ui.web.api.WebActions;
+import org.nuxeo.ecm.platform.ui.web.rest.RestHelper;
+import org.nuxeo.ecm.platform.ui.web.util.BaseURL;
+import org.nuxeo.ecm.platform.url.DocumentViewImpl;
+import org.nuxeo.ecm.platform.url.api.DocumentView;
+import org.nuxeo.ecm.platform.url.api.DocumentViewCodecManager;
+import org.nuxeo.ecm.webapp.action.MainTabsActions;
+import org.nuxeo.ecm.webapp.helpers.EventNames;
+import org.nuxeo.runtime.api.Framework;
+
+import java.io.Serializable;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import javax.faces.component.UIViewRoot;
+import javax.faces.context.FacesContext;
+import javax.faces.model.SelectItem;
+import javax.faces.model.SelectItemGroup;
+
+/**
+ * Seam bean handling SEARCH main tab actions.
+ *
+ * @since 5.9.6
+ */
+@Name("searchActions")
+@Scope(CONVERSATION)
+@Install(precedence = FRAMEWORK)
+public class SearchActions implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private static final Log log = getLog(SearchActions.class);
+
+    public static final String SAVED_SEARCHES_LABEL = "label.saved.searches";
+
+    public static final String SHARED_SEARCHES_LABEL = "label.shared.searches";
+
+    public static final String SEARCH_FILTERS_LABEL = "label.search.filters";
+
+    public static final String SEARCH_SAVED_LABEL = "label.search.saved";
+
+    public static final String MAIN_TABS_SEARCH = "MAIN_TABS:search";
+
+    public static final String SEARCH_VIEW_ID = "/search/search.xhtml";
+
+    public static final String SEARCH_MAIN_TAB_ACTION_ID = "search";
+
+    public static final String SEARCH_CODEC = "docpathsearch";
+
+    public static final String SEARCH_CONTENT_VIEW_FLAG = "SEARCH";
+
+    public static final String SAVED_SEARCHES_PROVIDER_NAME = "SAVED_SEARCHES";
+
+    public static final String SHARED_SEARCHES_PROVIDER_NAME = "SHARED_SAVED_SEARCHES";
+
+    public static final String CONTENT_VIEW_NAME_PARAMETER = "contentViewName";
+
+    public static final String CURRENT_PAGE_PARAMETER = "currentPage";
+
+    public static final String PAGE_SIZE_PARAMETER = "pageSize";
+
+    public static final String CONTENT_VIEW_STATE_PARAMETER = "state";
+
+    @In(create = true, required = false)
+    protected transient CoreSession documentManager;
+
+    @In(create = true)
+    protected transient WebActions webActions;
+
+    @In(create = true)
+    protected MainTabsActions mainTabsActions;
+
+    @In(create = true)
+    protected RestHelper restHelper;
+
+    @In(create = true)
+    protected ContentViewActions contentViewActions;
+
+    @In(create = true)
+    protected Map<String, String> messages;
+
+    protected List<String> contentViewNames;
+
+    protected Set<ContentViewHeader> contentViewHeaders;
+
+    protected String currentContentViewName;
+
+    protected String currentSelectedSavedSearchId;
+
+    protected String currentPage;
+
+    protected String pageSize;
+
+    protected String savedSearchTitle;
+
+    public String getSearchMainTab() {
+        return MAIN_TABS_SEARCH;
+    }
+
+    public void setSearchMainTab(String tabs) {
+        webActions.setCurrentTabIds(!StringUtils.isBlank(tabs) ? tabs
+                : MAIN_TABS_SEARCH);
+    }
+
+    /**
+     * Returns true if the user is viewing SEARCH.
+     */
+    public boolean isOnSearchView() {
+        if (FacesContext.getCurrentInstance() == null) {
+            return false;
+        }
+
+        UIViewRoot viewRoot = FacesContext.getCurrentInstance().getViewRoot();
+        if (viewRoot != null) {
+            String viewId = viewRoot.getViewId();
+            // FIXME find a better way to update the current document only
+            // if we are on SEARCH
+            if (SEARCH_VIEW_ID.equals(viewId)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public String getJSONContentViewState() throws ClientException,
+            UnsupportedEncodingException {
+        ContentView contentView = contentViewActions.getContentView(currentContentViewName);
+        ContentViewService contentViewService = Framework.getLocalService(ContentViewService.class);
+        ContentViewState state = contentViewService.saveContentView(contentView);
+        return JSONContentViewState.toJSON(state, true);
+    }
+
+    public String getCurrentContentViewName() {
+        if (currentContentViewName == null) {
+            List<String> contentViewNames = getContentViewNames();
+            if (!contentViewNames.isEmpty()) {
+                currentContentViewName = contentViewNames.get(0);
+            }
+        }
+        return currentContentViewName;
+    }
+
+    public void setCurrentContentViewName(String contentViewName)
+            throws ClientException {
+        this.currentContentViewName = contentViewName;
+    }
+
+    public String getCurrentSelectedSavedSearchId() {
+        return currentSelectedSavedSearchId != null ? currentSelectedSavedSearchId
+                : currentContentViewName;
+    }
+
+    public void setCurrentSelectedSavedSearchId(String selectedSavedSearchId)
+            throws ClientException {
+        if (contentViewNames.contains(selectedSavedSearchId)) {
+            contentViewActions.reset(currentContentViewName);
+            currentContentViewName = selectedSavedSearchId;
+            currentSelectedSavedSearchId = null;
+        } else {
+            DocumentModel savedSearch = documentManager.getDocument(new IdRef(
+                    selectedSavedSearchId));
+            String contentViewName = (String) savedSearch.getPropertyValue("cvd:contentViewName");
+            loadSavedSearch(contentViewName, savedSearch);
+        }
+    }
+
+    public void loadSavedSearch(String contentViewName,
+                                DocumentModel searchDocument) throws ClientException {
+        ContentView contentView = contentViewActions.getContentView(
+                contentViewName, searchDocument);
+        if (contentView != null) {
+            currentContentViewName = contentViewName;
+            currentSelectedSavedSearchId = searchDocument.getId();
+        }
+    }
+
+    public List<String> getContentViewNames() {
+        if (contentViewNames == null) {
+            ContentViewService contentViewService = Framework.getLocalService(ContentViewService.class);
+            contentViewNames = new ArrayList<>(
+                    contentViewService.getContentViewNames(SEARCH_CONTENT_VIEW_FLAG));
+        }
+        return contentViewNames;
+    }
+
+    public Set<ContentViewHeader> getContentViewHeaders()
+            throws ClientException {
+        if (contentViewHeaders == null) {
+            contentViewHeaders = new HashSet<>();
+            ContentViewService contentViewService = Framework.getLocalService(ContentViewService.class);
+            for (String name : getContentViewNames()) {
+                ContentViewHeader header = contentViewService.getContentViewHeader(name);
+                if (header != null) {
+                    contentViewHeaders.add(header);
+                }
+            }
+        }
+        return contentViewHeaders;
+    }
+
+    public void clearSearch() throws ClientException {
+        contentViewActions.reset(getCurrentContentViewName());
+    }
+
+//    @Observer(value = { REFRESH_DAM_SEARCH }, create = true)
+    public void refreshAndRewind() throws ClientException {
+        contentViewActions.refreshAndRewind(getCurrentContentViewName());
+    }
+
+
+    /*
+     * ----- Load / Save searches -----
+     */
+
+    public List<SelectItem> getAllSavedSearchesSelectItems()
+            throws ClientException {
+        List<SelectItem> items = new ArrayList<>();
+        // Add saved searches
+        SelectItemGroup userGroup = new SelectItemGroup(
+                messages.get(SAVED_SEARCHES_LABEL));
+        List<DocumentModel> userSavedSearches = getSavedSearches();
+        List<SelectItem> userSavedSearchesItems = convertToSelectItems(userSavedSearches);
+        userGroup.setSelectItems(userSavedSearchesItems.toArray(new SelectItem[userSavedSearchesItems.size()]));
+        items.add(userGroup);
+        // Add shared searches
+        List<DocumentModel> otherUsersSavedFacetedSearches = getSharedSearches();
+        List<SelectItem> otherUsersSavedSearchesItems = convertToSelectItems(otherUsersSavedFacetedSearches);
+        SelectItemGroup allGroup = new SelectItemGroup(
+                messages.get(SHARED_SEARCHES_LABEL));
+        allGroup.setSelectItems(otherUsersSavedSearchesItems.toArray(new SelectItem[otherUsersSavedSearchesItems.size()]));
+        items.add(allGroup);
+        SelectItemGroup flaggedGroup = new SelectItemGroup(
+                messages.get(SEARCH_FILTERS_LABEL));
+        // Add flagged content views
+        Set<ContentViewHeader> flaggedSavedSearches = getContentViewHeaders();
+        List<SelectItem> flaggedSavedSearchesItems = convertCVToSelectItems(flaggedSavedSearches);
+        flaggedGroup.setSelectItems(flaggedSavedSearchesItems.toArray(new SelectItem[flaggedSavedSearchesItems.size()]));
+        items.add(flaggedGroup);
+        return items;
+    }
+
+    protected List<DocumentModel> getSavedSearches() throws ClientException {
+        return getDocuments(SAVED_SEARCHES_PROVIDER_NAME,
+                documentManager.getPrincipal().getName());
+    }
+
+    protected List<DocumentModel> getSharedSearches() throws ClientException {
+        return getDocuments(SHARED_SEARCHES_PROVIDER_NAME,
+                documentManager.getPrincipal().getName());
+    }
+
+    @SuppressWarnings("unchecked")
+    protected List<DocumentModel> getDocuments(String pageProviderName,
+                                               Object... parameters) throws ClientException {
+        PageProviderService pageProviderService = Framework.getLocalService(PageProviderService.class);
+        Map<String, Serializable> properties = new HashMap<String, Serializable>();
+        properties.put("coreSession", (Serializable) documentManager);
+        return ((PageProvider<DocumentModel>) pageProviderService.getPageProvider(
+                pageProviderName, null, null, null, properties, parameters)).getCurrentPage();
+    }
+
+    protected List<SelectItem> convertToSelectItems(List<DocumentModel> docs)
+            throws ClientException {
+        List<SelectItem> items = new ArrayList<>();
+        for (DocumentModel doc : docs) {
+            items.add(new SelectItem(doc.getId(), doc.getTitle(), ""));
+        }
+        return items;
+    }
+
+    protected List<SelectItem> convertCVToSelectItems(
+            Set<ContentViewHeader> contentViewHeaders) {
+        List<SelectItem> items = new ArrayList<>();
+        for (ContentViewHeader contentViewHeader : contentViewHeaders) {
+            items.add(new SelectItem(contentViewHeader.getName(),
+                    messages.get(contentViewHeader.getTitle()), ""));
+        }
+        return items;
+    }
+
+    public String getSavedSearchTitle() {
+        return savedSearchTitle;
+    }
+
+    public void setSavedSearchTitle(String savedSearchTitle) {
+        this.savedSearchTitle = savedSearchTitle;
+    }
+
+    public String saveSearch() throws ClientException {
+//        ContentView contentView = contentViewActions.getContentView(getCurrentContentViewName());
+//        if (contentView != null) {
+//            UserWorkspaceService userWorkspaceService = Framework.getLocalService(UserWorkspaceService.class);
+//            DocumentModel uws = userWorkspaceService.getCurrentUserPersonalWorkspace(
+//                    documentManager, null);
+//
+//            DocumentModel searchDoc = contentView.getSearchDocumentModel();
+//            searchDoc.setPropertyValue("cvd:contentViewName",
+//                    contentView.getName());
+//            searchDoc.setPropertyValue("dc:title", savedSearchTitle);
+//            PathSegmentService pathService = Framework.getLocalService(PathSegmentService.class);
+//            searchDoc.setPathInfo(uws.getPathAsString(),
+//                    pathService.generatePathSegment(searchDoc));
+//            searchDoc = documentManager.createDocument(searchDoc);
+//            documentManager.save();
+//
+//            facesMessages.add(StatusMessage.Severity.INFO,
+//                    messages.get(SEARCH_SAVED_LABEL));
+//
+//            Events.instance().raiseEvent(EventNames.DOCUMENT_CHILDREN_CHANGED,
+//                    uws);
+//
+//            savedSearchTitle = null;
+//            currentSelectedSavedSearchId = searchDoc.getId();
+//        }
+
+        return null;
+    }
+
+    /*
+     * ----- Permanent links -----
+     */
+
+    public void setState(String state) throws ClientException,
+            UnsupportedEncodingException {
+        if (StringUtils.isNotBlank(state)) {
+            Long finalPageSize = null;
+            if (!StringUtils.isBlank(pageSize)) {
+                try {
+                    finalPageSize = Long.valueOf(pageSize);
+                } catch (NumberFormatException e) {
+                    log.warn(String.format(
+                            "Unable to parse '%s' parameter with value '%s'",
+                            PAGE_SIZE_PARAMETER, pageSize));
+                }
+            }
+
+            Long finalCurrentPage = null;
+            if (!StringUtils.isBlank(currentPage)) {
+                try {
+                    finalCurrentPage = Long.valueOf(currentPage);
+                } catch (NumberFormatException e) {
+                    log.warn(String.format(
+                            "Unable to parse '%s' parameter with value '%s'",
+                            CURRENT_PAGE_PARAMETER, currentPage));
+                }
+            }
+
+            contentViewActions.restoreContentView(getCurrentContentViewName(),
+                    finalCurrentPage, finalPageSize, null, state);
+        }
+    }
+
+    public String getCurrentPage() {
+        return currentPage;
+    }
+
+    public void setCurrentPage(String currentPage) {
+        this.currentPage = currentPage;
+    }
+
+    public String getPageSize() {
+        return pageSize;
+    }
+
+    public void setPageSize(String pageSize) {
+        this.pageSize = pageSize;
+    }
+
+    /**
+     * Compute a permanent link for the current search.
+     */
+    @SuppressWarnings("unchecked")
+    public String getSearchPermanentLinkUrl() throws ClientException,
+            UnsupportedEncodingException {
+        // do not try to compute an URL if we don't have any CoreSession
+        if (documentManager == null) {
+            return null;
+        }
+
+        String currentContentViewName = getCurrentContentViewName();
+        DocumentModel currentDocument = mainTabsActions.getDocumentFor(SEARCH_MAIN_TAB_ACTION_ID);
+        DocumentView docView = computeDocumentView(currentDocument);
+        docView.setViewId("search");
+        docView.addParameter(CONTENT_VIEW_NAME_PARAMETER,
+                currentContentViewName);
+        docView.addParameter(CONTENT_VIEW_STATE_PARAMETER,
+                getJSONContentViewState());
+        DocumentViewCodecManager documentViewCodecManager = Framework.getLocalService(DocumentViewCodecManager.class);
+        String url = documentViewCodecManager.getUrlFromDocumentView(SEARCH_CODEC,
+                docView, true, BaseURL.getBaseURL());
+        return RestHelper.addCurrentConversationParameters(url);
+    }
+
+    protected DocumentView computeDocumentView(DocumentModel doc) {
+        if (doc != null) {
+            return new DocumentViewImpl(new DocumentLocationImpl(
+                    documentManager.getRepositoryName(), new PathRef(
+                    doc.getPathAsString())));
+        } else {
+            return new DocumentViewImpl(new DocumentLocationImpl(
+                    documentManager.getRepositoryName(), null));
+        }
+    }
+
+    @Begin(id = "#{conversationIdGenerator.currentOrNewMainConversationId}", join = true)
+    public String loadPermanentLink(DocumentView docView)
+            throws ClientException {
+        restHelper.initContextFromRestRequest(docView);
+        return "search";
+    }
+
+//    @Observer(value = { CONTENT_VIEW_PAGE_CHANGED_EVENT,
+//            CONTENT_VIEW_PAGE_SIZE_CHANGED_EVENT, CONTENT_VIEW_REFRESH_EVENT }, create = true)
+//    public void onContentViewPageProviderChanged(String contentViewName)
+//            throws ClientException {
+//        String currentContentViewName = getCurrentContentViewName();
+//        if (currentContentViewName != null
+//                && currentContentViewName.equals(contentViewName)) {
+//            updateCurrentDocument();
+//        }
+//    }
+
+    /**
+     * Reset attributes.
+     */
+    @Observer(value = { EventNames.FLUSH_EVENT }, create = false)
+    @BypassInterceptors
+    public void resetOnFlush() {
+        contentViewHeaders = null;
+        contentViewNames = null;
+        currentSelectedSavedSearchId = null;
+        currentContentViewName = null;
+    }
+}
