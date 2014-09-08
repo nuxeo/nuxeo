f79d25f7ffa216d7623581740f3a556711e9e2cb
Anahide Tchertchian <at@nuxeo.com>
2014-09-08 18:22:15 +0200

    NXP-13080: format / replaces tabs by spaces

--- nuxeo-platform-tag-service/nuxeo-platform-tag-web/src/main/resources/web/nuxeo.war/scripts/tags2Formatter.js
+++ nuxeo-platform-tag-service/nuxeo-platform-tag-web/src/main/resources/web/nuxeo.war/scripts/tags2Formatter.js
@@ -1,23 +1,23 @@
 function createNewTag(term, data) {
-	return {
-		id : term,
-		displayLabel : term,
-		newTag : true
-	};
+  return {
+    id : term,
+    displayLabel : term,
+    newTag : true
+  };
 }
 
 function addTagHandler(tag) {
-	return addTagging(tag.id);
+  return addTagging(tag.id);
 }
 
 function removeTagHandler(tag) {
-	return removeTagging(tag.id);
+  return removeTagging(tag.id);
 }
 
 function formatSuggestedTags(tag) {
-	if (tag.newTag) {
-		return "<span class='s2newTag'>" + tag.displayLabel + "</span>"
-	} else {
-		return "<span class='s2existingTag'>" + tag.displayLabel + "</span>"
-	}
+  if (tag.newTag) {
+    return "<span class='s2newTag'>" + tag.displayLabel + "</span>"
+  } else {
+    return "<span class='s2existingTag'>" + tag.displayLabel + "</span>"
+  }
 }
