791ff103be4ea81ef9c29aa2beb6b2395c9433a1
Thierry Delprat <tdelprat@nuxeo.com>
2014-09-19 17:34:25 -0400

    NXP-15087 integrate basic Query Builder

--- nuxeo-elasticsearch-audit/src/main/java/org/nuxeo/elasticsearch/ESAuditBackend.java
+++ nuxeo-elasticsearch-audit/src/main/java/org/nuxeo/elasticsearch/ESAuditBackend.java
@@ -19,7 +19,6 @@ package org.nuxeo.elasticsearch;
 
 import static org.elasticsearch.common.xcontent.XContentFactory.jsonBuilder;
 
-import java.io.ByteArrayOutputStream;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -30,16 +29,26 @@ import org.codehaus.jackson.JsonGenerator;
 import org.elasticsearch.action.bulk.BulkItemResponse;
 import org.elasticsearch.action.bulk.BulkRequestBuilder;
 import org.elasticsearch.action.bulk.BulkResponse;
+import org.elasticsearch.action.count.CountResponse;
+import org.elasticsearch.action.search.SearchRequestBuilder;
 import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.action.search.SearchType;
 import org.elasticsearch.client.Client;
 import org.elasticsearch.common.xcontent.XContentBuilder;
+import org.elasticsearch.index.query.BoolFilterBuilder;
+import org.elasticsearch.index.query.BoolQueryBuilder;
+import org.elasticsearch.index.query.FilterBuilder;
 import org.elasticsearch.index.query.FilterBuilders;
+import org.elasticsearch.index.query.QueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.SearchHits;
 import org.nuxeo.ecm.core.api.ClientException;
+import org.nuxeo.ecm.platform.audit.api.AuditRuntimeException;
 import org.nuxeo.ecm.platform.audit.api.FilterMapEntry;
 import org.nuxeo.ecm.platform.audit.api.LogEntry;
+import org.nuxeo.ecm.platform.audit.api.query.AuditQueryException;
+import org.nuxeo.ecm.platform.audit.api.query.DateRangeParser;
 import org.nuxeo.ecm.platform.audit.service.AbstractAuditBackend;
 import org.nuxeo.ecm.platform.audit.service.AuditBackend;
 import org.nuxeo.ecm.platform.audit.service.BaseLogEntryProvider;
@@ -49,21 +58,24 @@ import org.nuxeo.elasticsearch.io.AuditEntryJSONWriter;
 import org.nuxeo.elasticsearch.seqgen.SequenceGenerator;
 import org.nuxeo.runtime.api.Framework;
 
+import com.sun.star.uno.RuntimeException;
+
 /**
- * Implementation of the {@link AuditBackend} interface using Elasticsearch persistence
+ * Implementation of the {@link AuditBackend} interface using Elasticsearch
+ * persistence
  * 
  * @author tiry
- *
+ * 
  */
 public class ESAuditBackend extends AbstractAuditBackend implements
         AuditBackend {
 
     public static final String IDX_NAME = "audit";
-    
+
     public static final String IDX_TYPE = "entry";
-        
+
     public static final String SEQ_NAME = "audit";
-    
+
     protected Client esClient = null;
 
     protected BaseLogEntryProvider provider = null;
@@ -83,15 +95,16 @@ public class ESAuditBackend extends AbstractAuditBackend implements
         }
     }
 
-   
     @Override
     public List<LogEntry> getLogEntriesFor(String uuid,
             Map<String, FilterMapEntry> filterMap, boolean doDefaultSort) {
-                
-        SearchResponse searchResponse = getClient().prepareSearch(IDX_NAME).setTypes(IDX_TYPE)        
-        .setSearchType(SearchType.DFS_QUERY_THEN_FETCH).setQuery(
+
+        SearchResponse searchResponse = getClient().prepareSearch(IDX_NAME).setTypes(
+                IDX_TYPE).setSearchType(SearchType.DFS_QUERY_THEN_FETCH).setQuery(
                 QueryBuilders.matchQuery("docUUID", uuid)) // Query
         .setFrom(0).setSize(60).execute().actionGet();
+
+        // filterMap
         
         List<LogEntry> entries = new ArrayList<>();
         for (SearchHit hit : searchResponse.getHits()) {
@@ -100,13 +113,28 @@ public class ESAuditBackend extends AbstractAuditBackend implements
             } catch (Exception e) {
                 log.error("Error while reading Audit Entry from ES", e);
             }
-        }        
+        }
         return entries;
     }
 
     @Override
     public LogEntry getLogEntryByID(long id) {
-        throw new UnsupportedOperationException("Not implemented yet!");
+
+        SearchResponse searchResponse = getClient().prepareSearch(IDX_NAME).setTypes(
+                IDX_TYPE).setSearchType(SearchType.DFS_QUERY_THEN_FETCH).setQuery(
+                QueryBuilders.idsQuery(String.valueOf(id))).setFrom(0).setSize(
+                10).execute().actionGet();
+
+        SearchHits hits = searchResponse.getHits();
+        if (hits.getTotalHits() > 1) {
+            throw new RuntimeException(
+                    "Found several match for the same ID : there is something wrong");
+        }
+        try {
+            return AuditEntryJSONReader.read(hits.getAt(0).getSourceAsString());
+        } catch (Exception e) {
+            throw new RuntimeException("Unable to read Entry for id " + id, e);
+        }
     }
 
     @Override
@@ -116,56 +144,146 @@ public class ESAuditBackend extends AbstractAuditBackend implements
     }
 
     @Override
-    public List<LogEntry> queryLogsByPage(String[] eventIds, String dateRange,
-            String[] categories, String path, int pageNb, int pageSize) {
-        throw new UnsupportedOperationException("Not implemented yet!");
-    }
-
-    @Override
     public List<LogEntry> queryLogsByPage(String[] eventIds, Date limit,
             String[] categories, String path, int pageNb, int pageSize) {
-        throw new UnsupportedOperationException("Not implemented yet!");
+        
+        SearchRequestBuilder builder = getClient().prepareSearch(IDX_NAME).setTypes(
+                IDX_TYPE).setSearchType(SearchType.DFS_QUERY_THEN_FETCH);
+       
+        BoolQueryBuilder queryBuilder = QueryBuilders.boolQuery();
+        BoolFilterBuilder filterBuilder = FilterBuilders.boolFilter();
+        int nbClauses = 0; 
+        int nbFilters = 0;
+                
+        if (eventIds !=null && eventIds.length>0) {
+            if (eventIds.length==1) {
+                queryBuilder.must(QueryBuilders.matchQuery("eventId", eventIds[0]));
+                nbClauses++;
+            } else {
+                filterBuilder.must(FilterBuilders.termsFilter("eventId", eventIds));
+                nbFilters++;
+            }            
+        }                
+        
+        if (categories !=null && categories.length>0) {
+            if (categories.length==1) {
+                queryBuilder.must(QueryBuilders.matchQuery("category", categories[0]));
+                nbClauses++;
+            } else {
+                filterBuilder.must(FilterBuilders.termsFilter("category", categories));
+                nbFilters++;
+            }            
+        }
+
+        if (path !=null ) {            
+           queryBuilder.must(QueryBuilders.matchQuery("docPath", path));
+           nbClauses++;
+        }
+
+        if (limit !=null ) {                        
+            queryBuilder.must(QueryBuilders.rangeQuery("eventDate").lt(limit));
+            nbClauses++;
+         }
+        
+        QueryBuilder targetBuilder = null;
+        FilterBuilder targetFilter = null;
+        
+        
+        if (nbClauses>0) {        
+            targetBuilder = queryBuilder;            
+        } else {
+            targetBuilder = QueryBuilders.matchAllQuery();            
+        }
+
+        if (nbFilters>0) {
+            targetFilter = filterBuilder;
+        } else {
+            targetFilter = FilterBuilders.matchAllFilter();
+        }
+        
+        builder.setQuery(QueryBuilders.filteredQuery(targetBuilder, targetFilter));
+        
+        if (pageNb>0) {
+            builder.setFrom(pageNb*pageSize);
+        }
+        
+        if (pageSize>0) {
+            builder.setSize(pageSize);
+        }
+        
+        SearchResponse searchResponse = builder.execute().actionGet();        
+        List<LogEntry> entries = new ArrayList<>();
+        for (SearchHit hit : searchResponse.getHits()) {
+            try {
+                entries.add(AuditEntryJSONReader.read(hit.getSourceAsString()));
+            } catch (Exception e) {
+                log.error("Error while reading Audit Entry from ES", e);
+            }
+        }
+        return entries;
     }
+
+    
+    @Override
+    public List<LogEntry> queryLogsByPage(String[] eventIds, String dateRange,
+            String[] categories, String path, int pageNb, int pageSize) {
+
+        Date limit = null;
+        if  (dateRange!=null) {
+            try {
+                limit = DateRangeParser.parseDateRangeQuery(new Date(), dateRange);
+            } catch (AuditQueryException aqe) {
+                throw new AuditRuntimeException(
+                        "Wrong date range query. Query was " + dateRange, aqe);
+            }
+        }
+        return queryLogsByPage(eventIds, limit, categories, path, pageNb,
+                pageSize);
+    }
+
 
     @Override
     public void addLogEntries(List<LogEntry> entries) {
 
-        BulkRequestBuilder bulkRequest = getClient().prepareBulk();            
+        BulkRequestBuilder bulkRequest = getClient().prepareBulk();
         JsonFactory factory = new JsonFactory();
-        
+
         SequenceGenerator sg = Framework.getService(SequenceGenerator.class);
-        
+
         try {
-            
-            for (LogEntry entry : entries) {                
+
+            for (LogEntry entry : entries) {
                 entry.setId(sg.getNextId(SEQ_NAME));
                 XContentBuilder builder = jsonBuilder();
-                JsonGenerator jsonGen = factory.createJsonGenerator(builder.stream());            
-                AuditEntryJSONWriter.asJSON(jsonGen, entry);            
-                bulkRequest.add(getClient().prepareIndex(IDX_NAME, IDX_TYPE, String.valueOf(entry.getId())).setSource(builder));                                                                
-            }                
-            
+                JsonGenerator jsonGen = factory.createJsonGenerator(builder.stream());
+                AuditEntryJSONWriter.asJSON(jsonGen, entry);
+                bulkRequest.add(getClient().prepareIndex(IDX_NAME, IDX_TYPE,
+                        String.valueOf(entry.getId())).setSource(builder));
+            }
+
             BulkResponse bulkResponse = bulkRequest.execute().actionGet();
             if (bulkResponse.hasFailures()) {
                 for (BulkItemResponse response : bulkResponse.getItems()) {
                     if (response.isFailed()) {
-                        log.error("Unable to index audit entry " + response.getItemId() + " :" + response.getFailureMessage());
+                        log.error("Unable to index audit entry "
+                                + response.getItemId() + " :"
+                                + response.getFailureMessage());
                     }
                 }
-            }       
+            }
         } catch (Exception e) {
-            throw new ClientException(
-                    "Error while indexing Audit entries", e);
+            throw new ClientException("Error while indexing Audit entries", e);
         }
 
     }
 
     @Override
     public Long getEventsCount(String eventId) {
-        throw new UnsupportedOperationException("Not implemented yet!");
+        CountResponse res = getClient().prepareCount(IDX_NAME).setTypes(
+                IDX_TYPE).setQuery(QueryBuilders.matchQuery("eventId", eventId)).execute().actionGet();        
+        return res.getCount();        
     }
 
-    
     protected BaseLogEntryProvider getProvider() {
 
         if (provider == null) {
--- nuxeo-elasticsearch-audit/src/test/java/org/nuxeo/elasticsearch/TestAuditWithElasticSearch.java
+++ nuxeo-elasticsearch-audit/src/test/java/org/nuxeo/elasticsearch/TestAuditWithElasticSearch.java
@@ -20,7 +20,11 @@ package org.nuxeo.elasticsearch;
 import static org.hamcrest.Matchers.notNullValue;
 import static org.junit.Assert.assertThat;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import junit.framework.Assert;
 
@@ -29,8 +33,13 @@ import org.junit.runner.RunWith;
 import org.nuxeo.ecm.core.api.CoreSession;
 import org.nuxeo.ecm.core.api.DocumentModel;
 import org.nuxeo.ecm.core.event.EventService;
+import org.nuxeo.ecm.platform.audit.api.AuditAdmin;
+import org.nuxeo.ecm.platform.audit.api.AuditLogger;
 import org.nuxeo.ecm.platform.audit.api.AuditReader;
+import org.nuxeo.ecm.platform.audit.api.ExtendedInfo;
 import org.nuxeo.ecm.platform.audit.api.LogEntry;
+import org.nuxeo.ecm.platform.audit.impl.ExtendedInfoImpl;
+import org.nuxeo.ecm.platform.audit.impl.LogEntryImpl;
 import org.nuxeo.ecm.platform.audit.service.AuditBackend;
 import org.nuxeo.ecm.platform.audit.service.NXAuditEventsService;
 import org.nuxeo.elasticsearch.api.ElasticSearchAdmin;
@@ -107,7 +116,8 @@ public class TestAuditWithElasticSearch {
 
         Assert.assertEquals(2, trail.size());
         
-        Assert.assertEquals(1, trail.get(0).getId());
+        
+        Long startId = trail.get(0).getId();
         Assert.assertEquals("documentCreated", trail.get(0).getEventId());
         Assert.assertEquals("eventDocumentCategory", trail.get(0).getCategory());
         Assert.assertEquals(
@@ -115,13 +125,83 @@ public class TestAuditWithElasticSearch {
                 trail.get(0).getExtendedInfos().get("title").getValue(
                         String.class));
         
-        Assert.assertEquals(2, trail.get(1).getId());
+        Assert.assertEquals(startId+1, trail.get(1).getId());
         Assert.assertEquals("documentModified", trail.get(1).getEventId());
         Assert.assertEquals("eventDocumentCategory", trail.get(1).getCategory());
         Assert.assertEquals(
                 "A modified File",
                 trail.get(1).getExtendedInfos().get("title").getValue(
-                        String.class));
+                        String.class));        
     }
 
+    
+    protected Map<String, ExtendedInfo> createExtendedInfos() {
+        Map<String, ExtendedInfo> infos = new HashMap<String, ExtendedInfo>();
+        ExtendedInfo info = ExtendedInfoImpl.createExtendedInfo(new Long(1));
+        infos.put("id", info);
+        return infos;
+    }
+    protected LogEntry doCreateEntry(String docId, String eventId, String category) {
+        LogEntry createdEntry = new LogEntryImpl();
+        createdEntry.setEventId(eventId);
+        createdEntry.setCategory(category);
+        createdEntry.setDocUUID(docId);
+        createdEntry.setEventDate(new Date());
+        createdEntry.setDocPath("/" + docId);
+        createdEntry.setRepositoryId("test");
+        createdEntry.setExtendedInfos(createExtendedInfos());
+        
+        return createdEntry;
+    }
+
+    @Test
+    public void shouldSupportMultiCriteriaQueries() throws Exception {
+        
+        List<LogEntry> entries = new ArrayList<>();
+        
+        AuditLogger logger = Framework.getLocalService(AuditLogger.class);
+        Assert.assertNotNull(logger);
+
+        for (int i = 0; i < 9; i++) {
+            entries.add(doCreateEntry("d"+i%5, "evt" + i, "cat" + i%2));            
+        }
+        logger.addLogEntries(entries);
+        flushAndSync();
+        
+        AuditReader reader = Framework.getLocalService(AuditReader.class);
+        
+        // simple Query
+        String[] evts = {"evt1","evt2"};        
+        List<LogEntry> res = reader.queryLogs(evts, null);
+        Assert.assertNotNull(res);
+        Assert.assertEquals(2, res.size());
+
+        evts = new String[] {"evt1",};        
+        res = reader.queryLogs(evts, null);
+        Assert.assertEquals(1, res.size());
+        
+        evts = new String[] {"evt",};        
+        res = reader.queryLogs(evts, null);
+        Assert.assertEquals(0, res.size());        
+       
+        // multi  Query
+        evts = new String[] {"evt1","evt2"};
+        String[] cats = {"cat1"};
+        res =  reader.queryLogsByPage(evts, (Date) null, cats, null, 0, 5);
+        Assert.assertEquals(1, res.size());
+        
+        evts = new String[] {"evt1","evt2"};
+        cats = new String[] {"cat1", "cat0"};
+        res =  reader.queryLogsByPage(evts, (Date) null, cats, null, 0, 5);
+        Assert.assertEquals(2, res.size());
+
+        // test page size
+        res =  reader.queryLogsByPage((String[]) null, (Date) null, (String[])null, null, 0, 5);
+        Assert.assertEquals(5, res.size());
+        
+        res =  reader.queryLogsByPage((String[]) null, (Date) null, (String[])null, null, 1, 5);
+        Assert.assertEquals(4, res.size());
+                
+    }
+    
 }
