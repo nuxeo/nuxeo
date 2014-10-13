41baf57bdf5b3d7bc99167f64d53b3a081b9d4e5
Mariana Cedica <mcedica@nuxeo.com>
2014-10-13 17:50:19 +0200

    NXP-15510: Fix NPE when serializing to json a document model without a parent

--- nuxeo-automation/nuxeo-automation-io/src/main/java/org/nuxeo/ecm/automation/jaxrs/io/documents/JsonDocumentWriter.java
+++ nuxeo-automation/nuxeo-automation-io/src/main/java/org/nuxeo/ecm/automation/jaxrs/io/documents/JsonDocumentWriter.java
@@ -174,7 +174,9 @@ public class JsonDocumentWriter implements MessageBodyWriter<DocumentModel> {
         jg.writeStringField("type", doc.getType());
         jg.writeStringField("state",
                 doc.getRef() != null ? doc.getCurrentLifeCycleState() : null);
-        jg.writeStringField("parentRef", doc.getParentRef().toString());
+        jg.writeStringField("parentRef",
+                doc.getParentRef() != null ? doc.getParentRef().toString()
+                        : null);
         jg.writeStringField("versionLabel", doc.getVersionLabel());
         jg.writeBooleanField("isCheckedOut", doc.isCheckedOut());
         Lock lock = doc.getLockInfo();
