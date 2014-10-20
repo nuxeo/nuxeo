367c7193115431d2c73672bb74fa0a305b2d9f56
Nelson Silva <nelson.silva@inevo.pt>
2014-10-20 11:42:12 +0100

    NXP-15330: add popup title and close button

--- src/main/js/app/app.js
+++ src/main/js/app/app.js
@@ -15,56 +15,81 @@
  *     Nelson Silva <nelson.silva@inevo.pt>
  */
 import {Connection} from './nuxeo/connection';
+import {Log} from './ui/log';
 import {Spreadsheet} from './ui/spreadsheet';
 import {parseNXQL} from './nuxeo/util/nxql';
 
 var {layout, query, columns} = parseParams();
 
-// Only parse queries entered in the queryArea
-var doParseQuery = !query;
+// Check if we're in standalone mode
+var isStandalone = !query;
+
+// Our Spreadsheet instance
+var sheet;
+
+function setupUI() {
+
+  var log = new Log($('#console'));
+
+  // Only display close button in popup
+  if (!isStandalone) {
+    $('#close').click(function() {
+      parent.jQuery.fancybox.close();
+    });
+    $('#close').toggle(true);
+  }
+
+  // Only display query area in standalone
+  $('#queryArea').toggle(isStandalone);
+
+  $('#query').val(query);
+
+  $('#execute').click(doQuery);
+
+  $('#save').click(() => {
+    log.info('Saving...');
+    sheet.save().then(() => log.default());
+  });
+
+  $('input[name=autosave]').click(function() {
+    sheet.autosave = $(this).is(':checked');
+    if (sheet.autosave) {
+      log.default('Each change is automatically saved.');
+    } else {
+      log.default('');
+    }
+  });
+}
+
+function doQuery() {
+  var q = $('#query').val();
+  // Only parse queries in standalone mode
+  sheet.query.nxql = (isStandalone) ? parseNXQL(q) : q;
+  sheet.update();
+}
 
 function run() {
-  var nx = new Connection();
+
+  // Setup our connection
+  var nx = new Connection("http://localhost:8080/nuxeo", "Administrator", "Administrator");
   nx.schemas(['*']);
 
-  $(() => nx.connect().then(() => {
+  $(() => {
 
-    $('#queryArea').toggle(!query);
-    $('#query').val(query);
+    setupUI();
 
-    var console = $('#console');
+    nx.connect().then(() => {
 
-    // Setup the SpreadSheet
-    var sheet = new Spreadsheet($('#grid'), nx, layout, (columns) ? columns.split(',') : null);
-    sheet.query.nxql = query;
+      // Setup the SpreadSheet
+      sheet = new Spreadsheet($('#grid'), nx, layout, (columns) ? columns.split(',') : null);
+      sheet.query.nxql = query;
 
-    var doQuery = () => {
-      var q = $('#query').val();
-      sheet.query.nxql = (doParseQuery) ? parseNXQL(q) : q;
-      sheet.update();
-    };
-
-    if (query) {
-      doQuery();
-    }
-
-    $('#execute').click(doQuery);
-
-    $('#save').click(() => {
-      console.text('Saving...');
-      sheet.save().then(() => console.text(''));
-    });
-
-    $('input[name=autosave]').click(function() {
-      sheet.autosave = $(this).is(':checked');
-      if (sheet.autosave) {
-        console.text('Changes will be automatically saved!');
-      } else {
-        console.text('');
+      if (query) {
+        doQuery();
       }
-    });
 
-  }));
+    })
+  });
 }
 
 // Utils
