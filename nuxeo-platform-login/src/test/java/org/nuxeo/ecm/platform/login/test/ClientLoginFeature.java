106c7e960e249a5153e2ee044d859b4d9c2a7315
Maxime HILAIRE <mhilaire@nuxeo.com>
2014-10-20 18:40:45 +0200

    NXP-13724 implement dummy login tests feature without using usermanager

---
+++ nuxeo-platform-login/src/test/java/org/nuxeo/ecm/platform/login/test/ClientLoginFeature.java
@@ -0,0 +1,49 @@
+/*
+ * (C) Copyright 2014 Nuxeo SA (http://nuxeo.com/) and contributors.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser General Public License
+ * (LGPL) version 2.1 which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl-2.1.html
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * Contributors:
+ *     mhilaire
+ */
+package org.nuxeo.ecm.platform.login.test;
+
+import java.security.Principal;
+
+import javax.security.auth.login.LoginContext;
+import javax.security.auth.login.LoginException;
+
+import org.nuxeo.ecm.core.test.CoreFeature;
+import org.nuxeo.ecm.core.test.TransactionalFeature;
+import org.nuxeo.runtime.api.Framework;
+import org.nuxeo.runtime.test.runner.Deploy;
+import org.nuxeo.runtime.test.runner.Features;
+import org.nuxeo.runtime.test.runner.SimpleFeature;
+
+@Features({ TransactionalFeature.class, CoreFeature.class })
+@Deploy({ "org.nuxeo.ecm.core.schema", "org.nuxeo.ecm.directory.types.contrib",
+        "org.nuxeo.ecm.platform.login",
+        "org.nuxeo.ecm.platform.login.test:dummy-client-login-config.xml" })
+public class ClientLoginFeature extends SimpleFeature {
+
+    protected LoginContext logContext = null;
+
+    public Principal loginAs(String username, String password)
+            throws LoginException {
+        this.logContext = Framework.login(username, password);
+        return logContext.getSubject().getPrincipals().iterator().next();
+    }
+
+    public void logout() throws LoginException {
+        this.logContext.logout();
+    }
+
+}
---
+++ nuxeo-platform-login/src/test/java/org/nuxeo/ecm/platform/login/test/TestDummyLogInClientModule.java
@@ -0,0 +1,58 @@
+/*
+ * (C) Copyright 2014 Nuxeo SA (http://nuxeo.com/) and contributors.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser General Public License
+ * (LGPL) version 2.1 which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl-2.1.html
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * Contributors:
+ *     mhilaire
+ */
+package org.nuxeo.ecm.platform.login.test;
+
+import java.security.Principal;
+
+import javax.inject.Inject;
+import javax.security.auth.login.LoginException;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.nuxeo.ecm.core.api.NuxeoPrincipal;
+import org.nuxeo.ecm.core.api.local.ClientLoginModule;
+import org.nuxeo.runtime.test.runner.Features;
+import org.nuxeo.runtime.test.runner.FeaturesRunner;
+
+/**
+ * Test on the {@link DummyNuxeoLoginModule}
+ *
+ * @since 5.9.6
+ */
+@RunWith(FeaturesRunner.class)
+@Features(ClientLoginFeature.class)
+public class TestDummyLogInClientModule {
+
+    @Inject
+    ClientLoginFeature login;
+
+    @Test
+    public void canBuildDummyPrincipal() throws LoginException {
+        Principal dummyPrincipal = login.loginAs("dummyName", "dummyPwd");
+        Assert.assertNotNull(dummyPrincipal);
+    }
+    
+    @Test
+    public void canGetCurrentDummyPrincipal() throws LoginException
+    {
+        Principal dummyPrincipal = login.loginAs("dummyName", "dummyPwd");
+        NuxeoPrincipal currentDummy = ClientLoginModule.getCurrentPrincipal();
+        Assert.assertEquals(dummyPrincipal.getName(), currentDummy.getName());
+    }
+
+}
