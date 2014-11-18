9186230f6ac2531ce447310d8d1d64e8fd050d68
Florent Guillaume <fg@nuxeo.com>
2014-11-18 11:17:15 +0100

    NXP-15936: don't log as WARN JAX-RS 404 errors

--- nuxeo-webengine/nuxeo-webengine-core/src/main/java/org/nuxeo/ecm/webengine/app/WebEngineExceptionMapper.java
+++ nuxeo-webengine/nuxeo-webengine-core/src/main/java/org/nuxeo/ecm/webengine/app/WebEngineExceptionMapper.java
@@ -17,9 +17,11 @@
 package org.nuxeo.ecm.webengine.app;
 
 import com.sun.jersey.api.NotFoundException;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.nuxeo.ecm.webengine.WebException;
+import org.nuxeo.ecm.webengine.model.exceptions.WebResourceNotFoundException;
 
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.HttpHeaders;
@@ -43,7 +45,10 @@ public class WebEngineExceptionMapper implements ExceptionMapper<Throwable> {
     public Response toResponse(Throwable cause) {
         if (cause instanceof NotFoundException) {
             NotFoundException nfe = (NotFoundException) cause;
-            log.warn("JAX-RS 404 Not Found: " + nfe.getNotFoundUri());
+            log.debug("JAX-RS 404 Not Found: " + nfe.getNotFoundUri());
+        } else if (cause instanceof WebResourceNotFoundException) {
+            WebResourceNotFoundException nfe = (WebResourceNotFoundException) cause;
+            log.debug("JAX-RS 404 Not Found: " + nfe.getMessage());
         } else {
             log.warn("Exception in JAX-RS processing", cause);
         }
