b1aead7c60d6e71b367344d6958976ed81210a79
Benoit Delbosc <bdelbosc@nuxeo.com>
2014-12-02 15:07:34 +0100

    NXP-15826: Improve the indexing actions, reindexing the entire repo performs a drop index, recursive reindexation try to clean the index first

--- nuxeo-features/nuxeo-elasticsearch/nuxeo-elasticsearch-core/src/main/java/org/nuxeo/elasticsearch/ElasticSearchComponent.java
+++ nuxeo-features/nuxeo-elasticsearch/nuxeo-elasticsearch-core/src/main/java/org/nuxeo/elasticsearch/ElasticSearchComponent.java
@@ -207,6 +207,11 @@ public class ElasticSearchComponent extends DefaultComponent implements
     }
 
     @Override
+    public void dropAndInitRepositoryIndex(String repositoryName) {
+        esa.dropAndInitRepositoryIndex(repositoryName);
+    }
+
+    @Override
     public List<String> getRepositoryNames() {
         return esa.getRepositoryNames();
     }
