c043d76f46f309798ee92ab70c8500e9500d47c6
Thomas Roger <throger@gmail.com>
2014-12-10 14:31:06 +0100

    NXP-16175: use UserAgentMatcher from nuxeo-common

--- nuxeo-platform-video-jsf/src/main/java/org/nuxeo/ecm/platform/video/VideoActions.java
+++ nuxeo-platform-video-jsf/src/main/java/org/nuxeo/ecm/platform/video/VideoActions.java
@@ -29,11 +29,11 @@ import org.jboss.seam.annotations.Install;
 import org.jboss.seam.annotations.Name;
 import org.jboss.seam.core.Interpolator;
 import org.jboss.seam.faces.FacesMessages;
+import org.nuxeo.common.utils.UserAgentMatcher;
 import org.nuxeo.ecm.core.api.ClientException;
 import org.nuxeo.ecm.core.api.DocumentModel;
 import org.nuxeo.ecm.platform.ui.web.tag.fn.DocumentModelFunctions;
 import org.nuxeo.ecm.platform.video.service.VideoService;
-import org.nuxeo.ecm.platform.web.common.UserAgentMatcher;
 
 /**
  * @author "<a href=\"mailto:bjalon@nuxeo.com\">Benjamin JALON</a>"
