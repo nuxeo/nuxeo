1160f41701aa1efa974dcd7c4b81f2a98ede2aba
Antoine Taillefer <ataillefer@nuxeo.com>
2014-12-11 13:38:21 +0100

    NXP-16175: Fix missing Assert.assertEquals static import

--- nuxeo-jsf/nuxeo-platform-webapp-base/src/test/java/org/nuxeo/ecm/webapp/bulkedit/TestBulkEditService.java
+++ nuxeo-jsf/nuxeo-platform-webapp-base/src/test/java/org/nuxeo/ecm/webapp/bulkedit/TestBulkEditService.java
@@ -17,6 +17,7 @@
 
 package org.nuxeo.ecm.webapp.bulkedit;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
