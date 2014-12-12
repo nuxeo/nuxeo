f4b70561a38d051239adccc3950f751c19d4bd3a
Vladimir Pasquier <vpasquier@nuxeo.com>
2014-12-12 15:40:34 +0100

    NXP-12452: init binary metadata testing

---
+++ src/test/java/org/nuxeo/binary/metadata/test/TestBinaryMetadataListener.java
@@ -0,0 +1,42 @@
+/*
+ * (C) Copyright 2014 Nuxeo SA (http://nuxeo.com/) and contributors.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser General Public License
+ * (LGPL) version 2.1 which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl-2.1.html
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * Contributors:
+ *      Vladimir Pasquier <vpasquier@nuxeo.com>
+ */
+package org.nuxeo.binary.metadata.test;
+
+import org.junit.runner.RunWith;
+import org.nuxeo.binary.metadata.api.service.BinaryMetadataService;
+import org.nuxeo.ecm.core.test.annotations.Granularity;
+import org.nuxeo.ecm.core.test.annotations.RepositoryConfig;
+import org.nuxeo.runtime.test.runner.Features;
+import org.nuxeo.runtime.test.runner.FeaturesRunner;
+import org.nuxeo.runtime.test.runner.LocalDeploy;
+
+import com.google.inject.Inject;
+
+/**
+ * @since 7.1
+ */
+@RunWith(FeaturesRunner.class)
+@Features({ BinaryMetadataFeature.class })
+@LocalDeploy({ "org.nuxeo.binary.metadata.test:binary-metadata-contrib-test.xml",
+        "org.nuxeo.binary.metadata.test:binary-metadata-file-contrib-test.xml" })
+@RepositoryConfig(cleanup = Granularity.METHOD)
+public class TestBinaryMetadataListener {
+
+    @Inject
+    BinaryMetadataService binaryMetadataService;
+
+}
---
+++ src/test/java/org/nuxeo/binary/metadata/test/TestBinaryMetadataOperation.java
@@ -0,0 +1,49 @@
+/*
+ * (C) Copyright 2014 Nuxeo SA (http://nuxeo.com/) and contributors.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser General Public License
+ * (LGPL) version 2.1 which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl-2.1.html
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * Contributors:
+ *      Vladimir Pasquier <vpasquier@nuxeo.com>
+ */
+package org.nuxeo.binary.metadata.test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.nuxeo.ecm.automation.AutomationService;
+import org.nuxeo.ecm.core.test.annotations.Granularity;
+import org.nuxeo.ecm.core.test.annotations.RepositoryConfig;
+import org.nuxeo.runtime.test.runner.Deploy;
+import org.nuxeo.runtime.test.runner.Features;
+import org.nuxeo.runtime.test.runner.FeaturesRunner;
+import org.nuxeo.runtime.test.runner.LocalDeploy;
+
+import com.google.inject.Inject;
+
+/**
+ * @since 7.1
+ */
+@RunWith(FeaturesRunner.class)
+@Features({ BinaryMetadataFeature.class })
+@Deploy({ "org.nuxeo.ecm.automation.core" })
+@LocalDeploy({ "org.nuxeo.binary.metadata.test:binary-metadata-contrib-test.xml",
+        "org.nuxeo.binary.metadata.test:binary-metadata-file-contrib-test.xml" })
+@RepositoryConfig(cleanup = Granularity.METHOD)
+public class TestBinaryMetadataOperation {
+
+    @Inject
+    AutomationService automationService;
+
+    @Test
+    public void itShouldExtractBinaryMetadata() {
+
+    }
+}
