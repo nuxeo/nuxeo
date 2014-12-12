b21032b91d7e05d7c7d98e9bb75a29ece72fa92f
Vladimir Pasquier <vpasquier@nuxeo.com>
2014-12-12 14:50:00 +0100

    NXP-12452: init binary metadata listener

---
+++ src/main/java/org/nuxeo/binary/metadata/api/listener/BinaryMetadataListener.java
@@ -0,0 +1,57 @@
+/*
+ * (C) Copyright 2014 Nuxeo SA (http://nuxeo.com/) and others.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser General Public License
+ * (LGPL) version 2.1 which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl-2.1.html
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * Contributors:
+ *     Vladimir Pasquier <vpasquier@nuxeo.com>
+ */
+
+package org.nuxeo.binary.metadata.api.listener;
+
+import static org.nuxeo.ecm.core.api.event.DocumentEventTypes.ABOUT_TO_CREATE;
+import static org.nuxeo.ecm.core.api.event.DocumentEventTypes.BEFORE_DOC_UPDATE;
+
+import org.nuxeo.binary.metadata.api.service.BinaryMetadataService;
+import org.nuxeo.ecm.core.api.ClientException;
+import org.nuxeo.ecm.core.api.CoreSession;
+import org.nuxeo.ecm.core.api.DocumentModel;
+import org.nuxeo.ecm.core.event.Event;
+import org.nuxeo.ecm.core.event.EventContext;
+import org.nuxeo.ecm.core.event.EventListener;
+import org.nuxeo.ecm.core.event.impl.DocumentEventContext;
+import org.nuxeo.runtime.api.Framework;
+
+/**
+ * @since 7.1
+ */
+public class BinaryMetadataListener implements EventListener {
+
+    @Override
+    public void handleEvent(Event event) throws ClientException {
+        EventContext ctx = event.getContext();
+        if (!(ctx instanceof DocumentEventContext)) {
+            return;
+        }
+
+        BinaryMetadataService binaryMetadataService = Framework.getLocalService(BinaryMetadataService.class);
+
+        DocumentEventContext docCtx = (DocumentEventContext) ctx;
+        if (ABOUT_TO_CREATE.equals(event.getName())) {
+            DocumentModel doc = docCtx.getSourceDocument();
+            CoreSession session = docCtx.getCoreSession();
+        } else if (BEFORE_DOC_UPDATE.equals(event.getName())) {
+            DocumentModel doc = docCtx.getSourceDocument();
+            CoreSession session = docCtx.getCoreSession();
+        }
+    }
+
+}
