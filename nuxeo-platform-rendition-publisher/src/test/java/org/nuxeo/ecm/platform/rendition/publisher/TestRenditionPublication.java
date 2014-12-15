1745375e6f5bea759c62be8d21996dc45a62bac7
Thierry Martins <tmartins@nuxeo.com>
2014-12-15 22:32:50 +0100

    NXP-16211: fix tests

--- nuxeo-platform-rendition-publisher/src/test/java/org/nuxeo/ecm/platform/rendition/publisher/TestRenditionPublication.java
+++ nuxeo-platform-rendition-publisher/src/test/java/org/nuxeo/ecm/platform/rendition/publisher/TestRenditionPublication.java
@@ -67,7 +67,7 @@ import com.google.inject.Inject;
         "org.nuxeo.ecm.relations.jena", "org.nuxeo.ecm.platform.publisher.core.contrib",
         "org.nuxeo.ecm.platform.publisher.core", "org.nuxeo.ecm.platform.publisher.task",
         TaskUTConstants.CORE_BUNDLE_NAME, TaskUTConstants.TESTING_BUNDLE_NAME,
-        "org.nuxeo.ecm.platform.rendition.publisher" })
+        "org.nuxeo.ecm.platform.rendition.publisher", "org.nuxeo.ecm.platform.mimetype.core" })
 @LocalDeploy("org.nuxeo.ecm.platform.rendition.publisher:relations-default-jena-contrib.xml")
 public class TestRenditionPublication {
 
