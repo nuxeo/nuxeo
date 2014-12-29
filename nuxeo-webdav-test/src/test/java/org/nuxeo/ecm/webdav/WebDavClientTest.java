211225b0ecfc264268506b9295effebfb7430e4f
Florent Guillaume <fg@nuxeo.com>
2014-12-29 17:35:27 +0100

    NXP-16266: don't strip semicolons in WebDAV URLs

--- nuxeo-webdav-test/src/test/java/org/nuxeo/ecm/webdav/WebDavClientTest.java
+++ nuxeo-webdav-test/src/test/java/org/nuxeo/ecm/webdav/WebDavClientTest.java
@@ -225,6 +225,15 @@ public class WebDavClientTest extends AbstractServerTest {
     }
 
     @Test
+    public void testCreateTextFileWithSemiColon() throws Exception {
+        String name = "newfile;;;.txt"; // name with semicolons
+        String mimeType = "text/plain";
+        byte[] bytes = "Hello, world!".getBytes("UTF-8");
+        String expectedType = "Note";
+        doTestPutFile(name, bytes, mimeType, expectedType);
+    }
+
+    @Test
     // NXP-12735: disabled because failing under windows + pgsql
     public void testOverwriteExistingFile() throws Exception {
         String name = "test.txt"; // this file already exists
