b43bd1a27628059444be196424585c9886623dc4
Vladimir Pasquier <vpasquier@nuxeo.com>
2014-12-30 13:58:45 +0100

    Revert "NXP-16237: handle parameter quotes in command line executor"
    This reverts commit 4a5d4353c279d579495e04a5a204871904703940.

--- nuxeo-services/nuxeo-platform-commandline-executor/src/main/java/org/nuxeo/ecm/platform/commandline/executor/service/CommandLineExecutorComponent.java
+++ nuxeo-services/nuxeo-platform-commandline-executor/src/main/java/org/nuxeo/ecm/platform/commandline/executor/service/CommandLineExecutorComponent.java
@@ -145,29 +145,14 @@ public class CommandLineExecutorComponent extends DefaultComponent implements Co
      */
     @Override
     public ExecResult execCommand(String commandName, CmdParameters params) throws CommandNotAvailable {
-        CommandLineDescriptor cmdDesc = getCommandLineDescriptor(commandName);
-        Executor executor = executors.get(cmdDesc.getExecutor());
-        return executor.exec(cmdDesc, params);
-    }
-
-    @Override
-    public ExecResult execCommand(String commandName, CmdParameters params,
-            boolean quoteParameter) throws CommandNotAvailable {
-        CommandLineDescriptor cmdDesc = getCommandLineDescriptor(commandName);
-        Executor executor = executors.get(cmdDesc.getExecutor());
-        return executor.exec(cmdDesc, params, quoteParameter);
-    }
-
-    /**
-     * @since 7.1
-     */
-    protected CommandLineDescriptor getCommandLineDescriptor(String
-            commandName) throws CommandNotAvailable {
         CommandAvailability availability = getCommandAvailability(commandName);
         if (!availability.isAvailable()) {
             throw new CommandNotAvailable(availability);
         }
-        return commandDescriptors.get(commandName);
+
+        CommandLineDescriptor cmdDesc = commandDescriptors.get(commandName);
+        Executor executor = executors.get(cmdDesc.getExecutor());
+        return executor.exec(cmdDesc, params);
     }
 
     @Override
