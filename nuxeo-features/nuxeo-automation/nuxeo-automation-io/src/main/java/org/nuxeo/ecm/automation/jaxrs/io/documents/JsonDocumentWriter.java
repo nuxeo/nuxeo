28d5d6ca70921e7437cd9765fdcbd1cfe0a15e7a
ron1 <ron1@localhost>
2014-12-31 23:57:33 -0500

    NXP-14474 CMIS JSON Complex Types

--- nuxeo-features/nuxeo-automation/nuxeo-automation-io/src/main/java/org/nuxeo/ecm/automation/jaxrs/io/documents/JsonDocumentWriter.java
+++ nuxeo-features/nuxeo-automation/nuxeo-automation-io/src/main/java/org/nuxeo/ecm/automation/jaxrs/io/documents/JsonDocumentWriter.java
@@ -41,6 +41,8 @@ import org.joda.time.DateTime;
 import org.joda.time.format.ISODateTimeFormat;
 import org.nuxeo.common.utils.StringUtils;
 import org.nuxeo.common.utils.URIUtils;
+import org.nuxeo.ecm.automation.core.util.DateTimeFormat;
+import org.nuxeo.ecm.automation.core.util.JSONPropertyWriter;
 import org.nuxeo.ecm.automation.io.services.enricher.HeaderDocEvaluationContext;
 import org.nuxeo.ecm.automation.io.services.enricher.ContentEnricherService;
 import org.nuxeo.ecm.automation.io.services.enricher.RestEvaluationContext;
@@ -259,138 +261,7 @@ public class JsonDocumentWriter implements MessageBodyWriter<DocumentModel> {
      */
     public static void writePropertyValue(JsonGenerator jg, Property prop, String filesBaseUrl)
             throws PropertyException, JsonGenerationException, IOException {
-        if (prop.isScalar()) {
-            writeScalarPropertyValue(jg, prop);
-        } else if (prop.isList()) {
-            writeListPropertyValue(jg, prop, filesBaseUrl);
-        } else {
-            if (prop.isPhantom()) {
-                jg.writeNull();
-            } else if (prop instanceof BlobProperty) { // a blob
-                writeBlobPropertyValue(jg, prop, filesBaseUrl);
-            } else { // a complex property
-                writeMapPropertyValue(jg, (ComplexProperty) prop, filesBaseUrl);
-            }
-        }
-    }
-
-    protected static void writeScalarPropertyValue(JsonGenerator jg, Property prop) throws PropertyException,
-            IOException {
-        org.nuxeo.ecm.core.schema.types.Type type = prop.getType();
-        Object v = prop.getValue();
-        if (v == null) {
-            jg.writeNull();
-        } else {
-            if (type instanceof BooleanType) {
-                jg.writeBoolean((Boolean) v);
-            } else if (type instanceof LongType) {
-                jg.writeNumber((Long) v);
-            } else if (type instanceof DoubleType) {
-                jg.writeNumber((Double) v);
-            } else if (type instanceof IntegerType) {
-                jg.writeNumber((Integer) v);
-            } else if (type instanceof BinaryType) {
-                jg.writeBinary((byte[]) v);
-            } else {
-                jg.writeString(type.encode(v));
-            }
-        }
-    }
-
-    protected static void writeListPropertyValue(JsonGenerator jg, Property prop, String filesBaseUrl)
-            throws PropertyException, JsonGenerationException, IOException {
-        jg.writeStartArray();
-        if (prop instanceof ArrayProperty) {
-            Object[] ar = (Object[]) prop.getValue();
-            if (ar == null) {
-                jg.writeEndArray();
-                return;
-            }
-            org.nuxeo.ecm.core.schema.types.Type type = ((ListType) prop.getType()).getFieldType();
-            for (Object o : ar) {
-                jg.writeString(type.encode(o));
-            }
-        } else {
-            ListProperty listp = (ListProperty) prop;
-            for (Property p : listp.getChildren()) {
-                writePropertyValue(jg, p, filesBaseUrl);
-            }
-        }
-        jg.writeEndArray();
-    }
-
-    protected static void writeMapPropertyValue(JsonGenerator jg, ComplexProperty prop, String filesBaseUrl)
-            throws JsonGenerationException, IOException, PropertyException {
-        jg.writeStartObject();
-        for (Property p : prop.getChildren()) {
-            jg.writeFieldName(p.getName());
-            writePropertyValue(jg, p, filesBaseUrl);
-        }
-        jg.writeEndObject();
-    }
-
-    protected static void writeBlobPropertyValue(JsonGenerator jg, Property prop, String filesBaseUrl)
-            throws PropertyException, JsonGenerationException, IOException {
-        Blob blob = (Blob) prop.getValue();
-        if (blob == null) {
-            jg.writeNull();
-            return;
-        }
-        jg.writeStartObject();
-        String v = blob.getFilename();
-        if (v == null) {
-            jg.writeNullField("name");
-        } else {
-            jg.writeStringField("name", v);
-        }
-        v = blob.getMimeType();
-        if (v == null) {
-            jg.writeNullField("mime-type");
-        } else {
-            jg.writeStringField("mime-type", v);
-        }
-        v = blob.getEncoding();
-        if (v == null) {
-            jg.writeNullField("encoding");
-        } else {
-            jg.writeStringField("encoding", v);
-        }
-        v = blob.getDigest();
-        if (v == null) {
-            jg.writeNullField("digest");
-        } else {
-            jg.writeStringField("digest", v);
-        }
-        jg.writeStringField("length", Long.toString(blob.getLength()));
-        if (filesBaseUrl != null) {
-            jg.writeStringField("data", getBlobUrl(prop, filesBaseUrl));
-        }
-        jg.writeEndObject();
-    }
-
-    /**
-     * Get the full URL of where a blob can be downloaded.
-     *
-     * @param prop
-     * @param filesBaseUrl
-     * @return
-     * @throws UnsupportedEncodingException
-     * @throws PropertyException
-     * @since 5.9.3
-     */
-    private static String getBlobUrl(Property prop, String filesBaseUrl) throws UnsupportedEncodingException,
-            PropertyException {
-        StringBuilder blobUrlBuilder = new StringBuilder(filesBaseUrl);
-        blobUrlBuilder.append(prop.getSchema().getName());
-        blobUrlBuilder.append(":");
-        String canonicalXPath = ComplexTypeImpl.canonicalXPath(prop.getPath().substring(1));
-        blobUrlBuilder.append(canonicalXPath);
-        blobUrlBuilder.append("/");
-        String filename = ((Blob) prop.getValue()).getFilename();
-        if (filename != null) {
-            blobUrlBuilder.append(URIUtils.quoteURIPathComponent(filename, true));
-        }
-        return blobUrlBuilder.toString();
+        JSONPropertyWriter.writePropertyValue(jg, prop, DateTimeFormat.W3C, filesBaseUrl);
     }
 
 }
