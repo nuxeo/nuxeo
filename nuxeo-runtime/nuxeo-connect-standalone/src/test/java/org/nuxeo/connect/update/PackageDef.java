18363ce9a06a5a717e22d45f4c82d6f1c62b5c93
Julien Carsique <jcarsique@nuxeo.com>
2015-01-06 11:56:10 +0100

    NXP-11734: format

--- nuxeo-runtime/nuxeo-connect-standalone/src/test/java/org/nuxeo/connect/update/PackageDef.java
+++ nuxeo-runtime/nuxeo-connect-standalone/src/test/java/org/nuxeo/connect/update/PackageDef.java
@@ -1,10 +1,10 @@
 /*
- * (C) Copyright 2006-2014 Nuxeo SA (http://nuxeo.com/) and contributors.
+ * (C) Copyright 2006-2015 Nuxeo SA (http://nuxeo.com/) and contributors.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the GNU Lesser General Public License
  * (LGPL) version 2.1 which accompanies this distribution, and is available at
- * http://www.gnu.org/licenses/lgpl.html
+ * http://www.gnu.org/licenses/lgpl-2.1.html
  *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -30,7 +30,6 @@ import org.nuxeo.connect.update.xml.XmlWriter;
 
 /**
  * @author <a href="mailto:bs@nuxeo.com">Bogdan Stefanescu</a>
- *
  */
 public abstract class PackageDef {
 
@@ -48,18 +47,15 @@ public abstract class PackageDef {
 
     private PackageUpdateService service;
 
-    public PackageDef(String name, String version, PackageUpdateService service)
-            throws Exception {
+    public PackageDef(String name, String version, PackageUpdateService service) {
         this(name, version, PackageType.ADDON, service);
     }
 
-    public PackageDef(String name, String version, PackageType type,
-            PackageUpdateService service) throws Exception {
+    public PackageDef(String name, String version, PackageType type, PackageUpdateService service) {
         this(name, version, type, "dm-" + version, service);
     }
 
-    public PackageDef(String name, String version, PackageType type,
-            String targetVersion, PackageUpdateService service) {
+    public PackageDef(String name, String version, PackageType type, String targetVersion, PackageUpdateService service) {
         builder = new PackageBuilder();
         builder.name(name).version(version).type(type);
         builder.platform(targetVersion);
@@ -148,8 +144,7 @@ public abstract class PackageDef {
 
     protected abstract void updatePackage() throws Exception;
 
-    protected abstract void writeInstallCommands(XmlWriter writer)
-            throws Exception;
+    protected abstract void writeInstallCommands(XmlWriter writer) throws Exception;
 
     public void addFile(String name, URL url) throws Exception {
         builder.addEntry(name, url.openStream());
