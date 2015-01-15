045bd8a057fc85bc1e7beb6865ddea00e2bc7916
Stephane Lacoin aka nxmatic <slacoin@nuxeo.com>
2015-01-15 17:31:08 +0100

    NXP-16295 rest api tests should rely on single datasource

--- nuxeo-features/nuxeo-automation/nuxeo-automation-test/src/test/java/org/nuxeo/ecm/automation/server/test/EmbeddedAutomationClientTest.java
+++ nuxeo-features/nuxeo-automation/nuxeo-automation-test/src/test/java/org/nuxeo/ecm/automation/server/test/EmbeddedAutomationClientTest.java
@@ -34,6 +34,8 @@ import junit.framework.Assert;
 import org.codehaus.jackson.JsonNode;
 import org.codehaus.jackson.map.ObjectMapper;
 import org.hamcrest.number.IsCloseTo;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -79,6 +81,7 @@ import org.nuxeo.ecm.core.test.annotations.RepositoryConfig;
 import org.nuxeo.ecm.platform.audit.AuditFeature;
 import org.nuxeo.ecm.platform.usermanager.UserManager;
 import org.nuxeo.ecm.platform.web.common.ServletHelper;
+import org.nuxeo.runtime.api.ConnectionHelper;
 import org.nuxeo.runtime.api.Framework;
 import org.nuxeo.runtime.model.ComponentInstance;
 import org.nuxeo.runtime.test.runner.Deploy;
@@ -86,6 +89,7 @@ import org.nuxeo.runtime.test.runner.Features;
 import org.nuxeo.runtime.test.runner.FeaturesRunner;
 import org.nuxeo.runtime.test.runner.Jetty;
 import org.nuxeo.runtime.test.runner.LocalDeploy;
+import org.nuxeo.runtime.transaction.TransactionHelper;
 
 import com.google.inject.Inject;
 
@@ -101,7 +105,8 @@ import com.google.inject.Inject;
         "org.nuxeo.ecm.platform.notification.core:OSGI-INF/NotificationService.xml",
         "org.nuxeo.ecm.automation.test" })
 @LocalDeploy({ "org.nuxeo.ecm.automation.test:test-bindings.xml",
-        "org.nuxeo.ecm.automation.test:test-mvalues.xml" })
+        "org.nuxeo.ecm.automation.test:test-mvalues.xml",
+        "org.nuxeo.runtime.datasource:ds-contrib.xml"})
 @Features({EmbeddedAutomationServerFeature.class, AuditFeature.class})
 @Jetty(port = 18080)
 @RepositoryConfig(cleanup = Granularity.METHOD)
@@ -109,6 +114,23 @@ public class EmbeddedAutomationClientTest extends AbstractAutomationClientTest {
 
     protected static String[] attachments = { "att1", "att2", "att3" };
 
+	protected String savedSingleDS;
+
+	@Before
+	public void setSingleDataSourceMode() {
+		savedSingleDS = System.getProperty(ConnectionHelper.SINGLE_DS);
+		System.setProperty(ConnectionHelper.SINGLE_DS, "jdbc/NuxeoTestDS");
+	}
+
+	@After
+	public void resetSingleDataSourceMode() {
+		if (savedSingleDS == null || savedSingleDS.isEmpty()) {
+			System.clearProperty(ConnectionHelper.SINGLE_DS);
+		} else {
+			System.setProperty(ConnectionHelper.SINGLE_DS, savedSingleDS);
+		}
+	}
+	
     @Inject
     UserManager userManager;
 
@@ -410,7 +432,8 @@ public class EmbeddedAutomationClientTest extends AbstractAutomationClientTest {
             user.setPropertyValue("user:username", testUserName);
             user.setPropertyValue("user:password", "secret");
             userManager.createUser(user);
-
+            TransactionHelper.commitOrRollbackTransaction();
+            TransactionHelper.startTransaction();
             // check invalid credentials
             try {
                 client.getSession(testUserName, "badpassword");
