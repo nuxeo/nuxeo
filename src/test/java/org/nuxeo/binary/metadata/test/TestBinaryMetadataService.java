8dea307d84a89a2b74b38ce07d2924f4c920a593
Julien Carsique <jcarsique@nuxeo.com>
2015-01-19 18:11:58 +0100

    Revert "NXP-12452: tmp ignoring unit tests"
    This reverts commit 3d069ff50769e7dc5da37da2d450ed76212ff0a4.

--- src/test/java/org/nuxeo/binary/metadata/test/TestBinaryMetadataService.java
+++ src/test/java/org/nuxeo/binary/metadata/test/TestBinaryMetadataService.java
@@ -25,7 +25,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.nuxeo.binary.metadata.api.BinaryMetadataService;
@@ -43,7 +42,6 @@ import com.google.inject.Inject;
 /**
  * @since 7.1
  */
-@Ignore("NXBT-876")
 @RunWith(FeaturesRunner.class)
 @Features(BinaryMetadataFeature.class)
 @LocalDeploy({ "org.nuxeo.binary.metadata:binary-metadata-contrib-test.xml",
