d4f038d2b478e47236ba3a35e60e1094a7cf8f33
Antoine Taillefer <ataillefer@nuxeo.com>
2015-01-20 16:25:59 +0100

    NXP-16383: Dump threads at test failure

--- nuxeo-core/nuxeo-core-test/src/main/java/org/nuxeo/ecm/core/test/DetectThreadDeadlocksFeature.java
+++ nuxeo-core/nuxeo-core-test/src/main/java/org/nuxeo/ecm/core/test/DetectThreadDeadlocksFeature.java
@@ -18,21 +18,44 @@
  */
 package org.nuxeo.ecm.core.test;
 
+import java.io.File;
+
+import org.apache.commons.logging.LogFactory;
+import org.junit.runner.notification.Failure;
+import org.junit.runner.notification.RunListener;
+import org.junit.runner.notification.RunNotifier;
 import org.nuxeo.runtime.management.jvm.ThreadDeadlocksDetector;
 import org.nuxeo.runtime.test.runner.FeaturesRunner;
 import org.nuxeo.runtime.test.runner.SimpleFeature;
 
+import com.google.inject.Inject;
+
 public class DetectThreadDeadlocksFeature extends SimpleFeature {
+
+    @Inject
+    protected RunNotifier notifier;
 
     protected ThreadDeadlocksDetector detector = new ThreadDeadlocksDetector();
 
+    protected final RunListener listener = new RunListener() {
+        @Override
+        public void testFailure(Failure failure) throws Exception {
+            long[] detectThreadLock = detector.detectThreadLock();
+            File dump = detector.dump(detectThreadLock);
+            LogFactory.getLog(DetectThreadDeadlocksFeature.class).error("Thread dump available at " + dump);
+        }
+    };
+
     @Override
     public void beforeRun(FeaturesRunner runner) throws Exception {
+        runner.getInjector().injectMembers(this);
+        notifier.addListener(listener);
         detector.schedule(30 * 1000, new ThreadDeadlocksDetector.KillListener());
     }
 
     @Override
     public void afterRun(FeaturesRunner runner) throws Exception {
+        notifier.removeListener(listener);
         detector.cancel();
     }
 }
