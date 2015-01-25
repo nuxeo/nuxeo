ec1cc33493796982b6ea358bed48154135987752
Florent Guillaume <fg@nuxeo.com>
2015-01-26 00:34:23 +0100

    NXP-16376: cleanup Blob implementations and usage

--- nuxeo-platform-suggestbox-core/src/main/java/org/nuxeo/ecm/platform/suggestbox/automation/SuggestOperation.java
+++ nuxeo-platform-suggestbox-core/src/main/java/org/nuxeo/ecm/platform/suggestbox/automation/SuggestOperation.java
@@ -27,8 +27,8 @@ import org.nuxeo.ecm.automation.core.annotations.Operation;
 import org.nuxeo.ecm.automation.core.annotations.OperationMethod;
 import org.nuxeo.ecm.automation.core.annotations.Param;
 import org.nuxeo.ecm.core.api.Blob;
+import org.nuxeo.ecm.core.api.Blobs;
 import org.nuxeo.ecm.core.api.CoreSession;
-import org.nuxeo.ecm.core.api.impl.blob.StringBlob;
 import org.nuxeo.ecm.platform.suggestbox.service.Suggestion;
 import org.nuxeo.ecm.platform.suggestbox.service.SuggestionContext;
 import org.nuxeo.ecm.platform.suggestbox.service.SuggestionService;
@@ -75,6 +75,6 @@ public class SuggestOperation {
             result.add(suggestionJSON);
         }
 
-        return new StringBlob(result.toString(), "application/json");
+        return Blobs.createBlob(result.toString(), "application/json");
     }
 }
