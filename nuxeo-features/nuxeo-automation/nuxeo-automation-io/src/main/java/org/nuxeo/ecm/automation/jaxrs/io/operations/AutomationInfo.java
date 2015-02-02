f2b4dbca3a62fde048964c74e37e17c516a31d01
Vladimir Pasquier <vpasquier@nuxeo.com>
2015-02-02 18:06:25 +0100

    NXP-16458: cleanup AutomationInfo

--- nuxeo-features/nuxeo-automation/nuxeo-automation-io/src/main/java/org/nuxeo/ecm/automation/jaxrs/io/operations/AutomationInfo.java
+++ nuxeo-features/nuxeo-automation/nuxeo-automation-io/src/main/java/org/nuxeo/ecm/automation/jaxrs/io/operations/AutomationInfo.java
@@ -12,17 +12,13 @@
 package org.nuxeo.ecm.automation.jaxrs.io.operations;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.nuxeo.ecm.automation.AutomationService;
-import org.nuxeo.ecm.automation.OperationChain;
 import org.nuxeo.ecm.automation.OperationDocumentation;
 import org.nuxeo.ecm.automation.OperationException;
-import org.nuxeo.ecm.automation.OperationParameters;
 
 /**
  * @author <a href="mailto:bs@nuxeo.com">Bogdan Stefanescu</a>
@@ -31,81 +27,17 @@ public class AutomationInfo {
 
     protected static final Log log = LogFactory.getLog(AutomationInfo.class);
 
-    protected final List<OperationDocumentation> ops;
+    public static final String CHAIN = "Chain";
 
-    protected final List<OperationDocumentation> chains;
+    protected List<OperationDocumentation> ops;
+
+    protected List<OperationDocumentation> chains = new ArrayList<>();
 
     public AutomationInfo(AutomationService service) throws OperationException {
         ops = service.getDocumentation();
-        // build a map for easy lookup
-        Map<String, OperationDocumentation> map = new HashMap<String, OperationDocumentation>();
-        for (OperationDocumentation doc : ops) {
-            map.put(doc.id, doc);
-            if (doc.getAliases() != null) {
-                for (String alias : doc.getAliases()) {
-                    map.put(alias, doc);
-                }
-            }
-        }
-        chains = new ArrayList<>();
-        NEXT_CHAIN:
-        for (OperationChain chain : service.getOperationChains()) {
-            OperationDocumentation doc = new OperationDocumentation(chain.getId());
-            doc.description = chain.getDescription();
-            doc.category = "Chain";
-            doc.label = doc.id;
-            doc.params = new OperationDocumentation.Param[0];
-            // compute chain signature
-            List<OperationParameters> ops = chain.getOperations();
-            if (ops.isEmpty()) {
-                doc.signature = new String[] { "void", "void" };
-            } else if (ops.size() == 1) {
-                OperationDocumentation opdoc = map.get(ops.get(0).id());
-                if (opdoc == null) {
-                    log.error("Unable to find Operation " + ops.get(0).id() + " that is used by chain " + chain.getId()
-                            + ", chain will be discarded");
-                    continue NEXT_CHAIN;
-                }
-                doc.signature = opdoc.signature;
-            } else {
-                ArrayList<String[]> sigs = new ArrayList<String[]>();
-                for (OperationParameters o : ops) {
-                    OperationDocumentation opdoc = map.get(o.id());
-                    if (opdoc == null) {
-                        log.error("Unable to find Operation " + o.id() + " that is used by chain " + chain.getId()
-                                + ", chain will be discarded");
-                        continue NEXT_CHAIN;
-                    }
-                    sigs.add(opdoc.signature);
-                }
-                String[] head = sigs.get(0);
-                ArrayList<String> rs = new ArrayList<String>();
-                for (int i = 0; i < head.length; i += 2) {
-                    String in = head[i];
-                    String out = head[i + 1];
-                    List<String> result = new ArrayList<String>();
-                    checkPath(out, sigs, 1, result);
-                    for (String r : result) {
-                        rs.add(in);
-                        rs.add(r);
-                    }
-                }
-                doc.signature = rs.toArray(new String[rs.size()]);
-            }
-            chains.add(doc);
-        }
-    }
-
-    protected void checkPath(String in, List<String[]> sigs, int offset, List<String> result) {
-        boolean last = sigs.size() - 1 == offset;
-        String[] sig = sigs.get(offset);
-        for (int i = 0; i < sig.length; i += 2) {
-            if ("void".equals(in) || "void".equals(sig[i]) || in.equals(sig[i])) {
-                if (last) {
-                    result.add(sig[i + 1]);
-                } else {
-                    checkPath(sig[i + 1], sigs, offset + 1, result);
-                }
+        for(OperationDocumentation op: ops){
+            if(CHAIN.equals(op.getCategory())){
+                chains.add(op);
             }
         }
     }
