786cf95d32a10e13b0e5bf007b03470d71408171
Vladimir Pasquier <vpasquier@nuxeo.com>
2015-02-03 17:32:34 +0100

    NXP-16433: cleanup tests

--- nuxeo-features/nuxeo-automation/nuxeo-automation-scripting/src/test/java/org/nuxeo/automation/scripting/test/TestCompileAndContext.java
+++ nuxeo-features/nuxeo-automation/nuxeo-automation-scripting/src/test/java/org/nuxeo/automation/scripting/test/TestCompileAndContext.java
@@ -22,12 +22,20 @@ import static org.junit.Assert.assertEquals;
 import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
 import java.io.PrintStream;
+import java.io.StringReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+import javax.script.Compilable;
+import javax.script.CompiledScript;
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
 
 import jdk.nashorn.api.scripting.ScriptObjectMirror;
 
+import junit.framework.Assert;
 import org.apache.commons.io.IOUtils;
 import org.junit.After;
 import org.junit.Before;
@@ -35,6 +43,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.nuxeo.automation.scripting.api.AutomationScriptingConstants;
 import org.nuxeo.automation.scripting.api.AutomationScriptingService;
+import org.nuxeo.automation.scripting.internals.MarshalingHelper;
 import org.nuxeo.automation.scripting.internals.ScriptRunner;
 import org.nuxeo.ecm.core.api.CoreSession;
 import org.nuxeo.ecm.core.test.CoreFeature;
@@ -92,6 +101,65 @@ public class TestCompileAndContext {
         assertNotNull(stream);
         engine.eval(IOUtils.toString(stream));
         assertEquals("Hello\n", outContent.toString());
+    }
+
+    @Test
+    public void testNashorn() throws Exception {
+        ScriptEngineManager engineManager = new ScriptEngineManager();
+        ScriptEngine engine = engineManager.getEngineByName(AutomationScriptingConstants.NASHORN_ENGINE);
+        assertNotNull(engine);
+
+        Compilable compiler = (Compilable) engine;
+        assertNotNull(compiler);
+
+        InputStream stream = this.getClass().getResourceAsStream("/testScript.js");
+        assertNotNull(stream);
+        String js = IOUtils.toString(stream);
+
+        CompiledScript compiled = compiler.compile(new StringReader(js));
+
+        engine.put("mapper", new Mapper());
+
+        compiled.eval(engine.getContext());
+
+    }
+
+    public class Mapper {
+
+        public Object callMe(ScriptObjectMirror params) {
+
+            Map<String, Object> map = (Map<String, Object>) MarshalingHelper
+                    .unwrap(params);
+
+            Integer p1 = (Integer) map.get("p1");
+            String p2 = (String) map.get("p2");
+            List<Object> p3 = (List<Object>) map.get("p3");
+
+            Assert.assertEquals(3, p3.size());
+            Map<String, Object> nested = (Map<String, Object>) p3.get(2);
+
+            System.out.println(p1);
+            System.out.println(p2);
+            System.out.println(p3);
+            System.out.println(nested);
+
+            Map<String, Object> data = new HashMap<>();
+            data.put("p1", "This is a string");
+            data.put("p2", 2);
+            List<String> l = new ArrayList<String>();
+            l.add("A");
+            l.add("B");
+            l.add("C");
+            data.put("p3", l);
+
+            Map<String, Object> nested2 = new HashMap<>();
+            nested2.put("a", "salut");
+            nested2.put("b", "from java");
+            data.put("p4", nested2);
+
+            return MarshalingHelper.wrap(data);
+        }
+
     }
 
 }
