2e3f1006e85150e2554f6927bf888579616276b1
Benoit Delbosc <bdelbosc@nuxeo.com>
2015-02-16 11:33:54 +0100

    NXP-16530: fix memory leak when clossing connection

--- nuxeo-core/nuxeo-core-storage-sql/nuxeo-core-storage-sql-ra/src/main/java/org/nuxeo/ecm/core/storage/sql/ra/ConnectionImpl.java
+++ nuxeo-core/nuxeo-core-storage-sql/nuxeo-core-storage-sql-ra/src/main/java/org/nuxeo/ecm/core/storage/sql/ra/ConnectionImpl.java
@@ -376,6 +376,7 @@ public class ConnectionImpl implements Session {
                         "Closing a query results for you, check stack trace for allocating point", context);
             }
         }
+        queryResults.clear();
     }
 
     @Override
