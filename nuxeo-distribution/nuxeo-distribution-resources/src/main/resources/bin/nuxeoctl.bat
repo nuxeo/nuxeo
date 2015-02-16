959b7a1be977e117ee058b6615da6ee413892fed
Julien Carsique <jcarsique@nuxeo.com>
2015-02-16 12:06:49 +0100

    NXP-16514: Java 8 is required

--- nuxeo-distribution/nuxeo-distribution-resources/src/main/resources/bin/nuxeoctl.bat
+++ nuxeo-distribution/nuxeo-distribution-resources/src/main/resources/bin/nuxeoctl.bat
@@ -183,7 +183,7 @@ goto END
 :HAS_JAVA
 echo Using JAVA = %JAVA%
 REM ***** Check Java version
-set REQUIRED_JAVA_VERSION=1.7
+set REQUIRED_JAVA_VERSION=1.8
 for /f "tokens=3" %%g in ('java -version 2^>^&1 ^| findstr /i "version"') do (
     set JAVA_VERSION=%%g
 )
