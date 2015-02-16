572a121e3c28f8ef7da7fb2be002821ae6d7852a
Florent Guillaume <fg@nuxeo.com>
2015-02-16 18:40:51 +0100

    NXP-16535: use annotations from javax.inject instead of guice

--- nuxeo-platform-user-registration-core/src/test/java/org/nuxeo/ecm/user/registration/AbstractUserRegistration.java
+++ nuxeo-platform-user-registration-core/src/test/java/org/nuxeo/ecm/user/registration/AbstractUserRegistration.java
@@ -15,6 +15,8 @@
  */
 package org.nuxeo.ecm.user.registration;
 
+import javax.inject.Inject;
+
 import org.junit.runner.RunWith;
 import org.nuxeo.ecm.core.api.ClientException;
 import org.nuxeo.ecm.core.api.CoreSession;
@@ -30,8 +32,6 @@ import org.nuxeo.runtime.test.runner.Deploy;
 import org.nuxeo.runtime.test.runner.Features;
 import org.nuxeo.runtime.test.runner.FeaturesRunner;
 import org.nuxeo.runtime.test.runner.LocalDeploy;
-
-import com.google.inject.Inject;
 
 /**
  * @author <a href="mailto:akervern@nuxeo.com">Arnaud Kervern</a>
