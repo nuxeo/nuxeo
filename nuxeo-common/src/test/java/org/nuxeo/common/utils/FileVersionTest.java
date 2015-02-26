f90d5894c91e331739a4d2ac0aee2c68b9af96df
Julien Carsique <jcarsique@nuxeo.com>
2015-02-26 12:26:11 +0100

    NXP-16611: add test on file versions ordering

--- nuxeo-common/src/test/java/org/nuxeo/common/utils/FileVersionTest.java
+++ nuxeo-common/src/test/java/org/nuxeo/common/utils/FileVersionTest.java
@@ -1,10 +1,10 @@
 /*
- * (C) Copyright 2011 Nuxeo SA (http://nuxeo.com/) and contributors.
+ * (C) Copyright 2011-2015 Nuxeo SA (http://nuxeo.com/) and contributors.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the GNU Lesser General Public License
  * (LGPL) version 2.1 which accompanies this distribution, and is available at
- * http://www.gnu.org/licenses/lgpl.html
+ * http://www.gnu.org/licenses/lgpl-2.1.html
  *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -22,13 +22,17 @@ import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 import org.junit.Test;
+import org.nuxeo.common.utils.FileVersion;
 
 public class FileVersionTest {
 
     @Test
     public void test() {
-
         // test version containing only major part
         assertArrayEquals(new FileVersion("1").getSplitVersion(), new Integer[] { 1 });
         FileVersion fv = new FileVersion("1-qualifier");
@@ -104,5 +108,23 @@ public class FileVersionTest {
                 new FileVersion("5.5.1-I20120101_0115").compareTo(new FileVersion("5.5")) > 0);
         assertTrue("5.5.1-I20120101_0115 must be greater than 5.5.1-I20110101_0115", new FileVersion(
                 "5.5.1-I20120101_0115").compareTo(new FileVersion("5.5.1-I20110101_0115")) > 0);
+    }
+
+    @Test
+    public void testOrdering() {
+        List<FileVersion> versions = new ArrayList<FileVersion>();
+        versions.add(new FileVersion("5.8"));
+        versions.add(new FileVersion("5.8-HF00-NXP-9999"));
+        versions.add(new FileVersion("5.8-NXP-9999"));
+        versions.add(new FileVersion("5.8-HF01"));
+        Collections.shuffle(versions);
+        Collections.sort(versions);
+
+        List<FileVersion> expectedOrder = new ArrayList<FileVersion>();
+        expectedOrder.add(new FileVersion("5.8"));
+        expectedOrder.add(new FileVersion("5.8-HF00-NXP-9999"));
+        expectedOrder.add(new FileVersion("5.8-HF01"));
+        expectedOrder.add(new FileVersion("5.8-NXP-9999"));
+        assertEquals(expectedOrder, versions);
     }
 }
