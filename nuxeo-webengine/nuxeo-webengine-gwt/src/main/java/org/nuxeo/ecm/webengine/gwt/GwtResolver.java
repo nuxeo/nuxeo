ff25e30c5e24e08c3b7a9d5f51dae28be9026bbf
Stephane Lacoin aka nxmatic <slacoin@nuxeo.com>
2015-04-09 16:33:28 +0200

    NXP-16922 allow in place deployment mode of gwt studio module (fixing jar deployment)

--- nuxeo-webengine/nuxeo-webengine-gwt/src/main/java/org/nuxeo/ecm/webengine/gwt/GwtResolver.java
+++ nuxeo-webengine/nuxeo-webengine-gwt/src/main/java/org/nuxeo/ecm/webengine/gwt/GwtResolver.java
@@ -1,14 +1,20 @@
 package org.nuxeo.ecm.webengine.gwt;
 
+import static java.nio.file.FileVisitResult.CONTINUE;
+import static java.nio.file.StandardCopyOption.COPY_ATTRIBUTES;
+import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
+
 import java.io.File;
 import java.io.IOException;
 import java.net.URI;
+import java.nio.file.FileSystems;
 import java.nio.file.FileVisitResult;
 import java.nio.file.FileVisitor;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.attribute.BasicFileAttributes;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -41,37 +47,16 @@ public class GwtResolver {
     }
 
     protected File install(URI location) throws IOException {
+        if ("jar".equals(location.getScheme())) {
+            FileSystems.newFileSystem(location, Collections.emptyMap());
+        }
         Path path = Paths.get(location);
         try {
             return path.toFile();
         } catch (UnsupportedOperationException cause) {
             ;
         }
-        Path rootPath = GWT_ROOT.toPath();
-        Files.walkFileTree(path.relativize(path), new FileVisitor<Path>() {
-
-            @Override
-            public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
-                Files.createDirectory(rootPath.resolve(dir));
-                return FileVisitResult.CONTINUE;
-            }
-
-            @Override
-            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-                Files.copy(file, rootPath.resolve(file));
-                return FileVisitResult.CONTINUE;
-            }
-
-            @Override
-            public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
-                throw new IOException("Cannot copy " + file, exc);
-            }
-
-            @Override
-            public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
-                return FileVisitResult.CONTINUE;
-            }
-        });
+        Files.walkFileTree(path, new TreeImporter(path, GWT_ROOT.toPath()));
         return GWT_ROOT;
     }
 
@@ -96,5 +81,58 @@ public class GwtResolver {
             return resolvers.get(name).resolve(path);
         }
         return ROOT_RESOLVER.resolve(path);
+    }
+
+    public class TreeImporter implements FileVisitor<Path> {
+        final Path source;
+
+        final Path sink;
+
+        public TreeImporter(Path source, Path sink) {
+            this.source = source;
+            this.sink = sink;
+        }
+
+        @Override
+        public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+            if (dir == source) {
+                return CONTINUE;
+            }
+            Files.copy(dir, toSinkPath(dir), COPY_ATTRIBUTES);
+            return CONTINUE;
+        }
+
+        Path toSinkPath(Path path) {
+            if (path == source) {
+                return sink;
+            }
+            path = source.relativize(path);
+            path = sink.resolve(path.toString());
+            return path;
+        }
+
+        @Override
+        public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+            Files.copy(file, toSinkPath(file), COPY_ATTRIBUTES, REPLACE_EXISTING);
+            return CONTINUE;
+        }
+
+        @Override
+        public FileVisitResult postVisitDirectory(Path dir, IOException error) {
+            if (error != null) {
+                return FileVisitResult.TERMINATE;
+            }
+            return CONTINUE;
+        }
+
+        @Override
+        public FileVisitResult visitFileFailed(Path file, IOException error) {
+            if (error != null) {
+                return FileVisitResult.TERMINATE;
+            }
+
+            return CONTINUE;
+        }
+
     }
 }
