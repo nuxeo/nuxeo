b9bf0e0d9f15f0313cc8ec9b9b08db1ad79ee5c0
Vincent Dutat <vdutat@nuxeo.com>
2015-04-13 13:45:21 -0700

    NXP-16890 HttpAutomationClient fails when 'Documents' object is passed to 'OperationRequest.setInput()'

--- nuxeo-features/nuxeo-automation/nuxeo-automation-client/src/main/java/org/nuxeo/ecm/automation/client/model/Documents.java
+++ nuxeo-features/nuxeo-automation/nuxeo-automation-client/src/main/java/org/nuxeo/ecm/automation/client/model/Documents.java
@@ -75,7 +75,7 @@ public class Documents implements Serializable, OperationInput, Iterable<Documen
     }
 
     public String toString() {
-        StringBuilder buf = new StringBuilder();
+        StringBuilder buf = new StringBuilder("docs:");
         int size = size();
         if (size == 0) {
             return buf.toString();
