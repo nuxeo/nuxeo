006cb60047c09ace8aa97d29b5c754c7d1610be3
Stephane Lacoin aka nxmatic <slacoin@nuxeo.com>
2015-04-14 14:32:18 +0200

    NXP-16922 allow gwt war contribution from bundles

--- nuxeo-webengine/nuxeo-webengine-gwt/src/main/java/org/nuxeo/ecm/webengine/gwt/GwtComponent.java
+++ nuxeo-webengine/nuxeo-webengine-gwt/src/main/java/org/nuxeo/ecm/webengine/gwt/GwtComponent.java
@@ -1,6 +1,8 @@
 package org.nuxeo.ecm.webengine.gwt;
 
 import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
 
 import org.nuxeo.ecm.core.api.NuxeoException;
 import org.nuxeo.runtime.model.ComponentInstance;
@@ -17,11 +19,19 @@ public class GwtComponent extends DefaultComponent {
             GwtAppResolver descriptor = (GwtAppResolver) contribution;
             resolver.install(descriptor.name, descriptor.strategy);
         } else if (contribution instanceof GwtAppLocation) {
-            GwtAppLocation location = (GwtAppLocation)contribution;
+            GwtAppLocation descriptor = (GwtAppLocation)contribution;
             try {
-                resolver.install(location.name, location.dir.toURI());
+                resolver.install(descriptor.name, descriptor.dir.toURI());
             } catch (IOException cause) {
-                throw new NuxeoException("Cannot install " + location, cause);
+                throw new NuxeoException("Cannot install " + descriptor, cause);
+            }
+        } else if (contribution instanceof GwtAppBundleLocation) {
+            GwtAppBundleLocation descriptor = (GwtAppBundleLocation)contribution;
+            URL location = contributor.getContext().getBundle().getEntry(descriptor.pathname);
+            try {
+                resolver.install(descriptor.name, location.toURI());
+            } catch (IOException | URISyntaxException cause) {
+                throw new NuxeoException("Cannot install " + descriptor, cause);
             }
         }
     }
