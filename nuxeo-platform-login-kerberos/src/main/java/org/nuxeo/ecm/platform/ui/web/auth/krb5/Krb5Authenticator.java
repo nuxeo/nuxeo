b8bf1fe7bc33920ca3077d141c06cd252c1452d9
Thierry Martins <tmartins@nuxeo.com>
2015-04-16 15:09:52 +0200

    NXP-16940: put the GSS context to the HTTP Session

--- nuxeo-platform-login-kerberos/src/main/java/org/nuxeo/ecm/platform/ui/web/auth/krb5/Krb5Authenticator.java
+++ nuxeo-platform-login-kerberos/src/main/java/org/nuxeo/ecm/platform/ui/web/auth/krb5/Krb5Authenticator.java
@@ -30,7 +30,7 @@ import org.nuxeo.ecm.platform.ui.web.auth.interfaces.NuxeoAuthenticationPlugin;
  */
 public class Krb5Authenticator implements NuxeoAuthenticationPlugin {
 
-    private static final String CONTEXT_ATTRIBUTE = "context";
+    private static final String CONTEXT_ATTRIBUTE = "Krb5Authenticator_context";
 
     private static final Log logger = LogFactory.getLog(Krb5Authenticator.class);
 
@@ -118,6 +118,7 @@ public class Krb5Authenticator implements NuxeoAuthenticationPlugin {
                 return info;
             } else {
                 // save context in the HTTP session to be reused after client response
+                req.getSession().setAttribute(CONTEXT_ATTRIBUTE, context);
                 // need another roundtrip
                 res.setHeader(WWW_AUTHENTICATE, NEGOTIATE + " " + Base64.encodeBase64String(respToken));
                 return null;
