17e79ba4c2e6fafc23766913980fbc8f929eb711
Stephane Lacoin aka nxmatic <slacoin@nuxeo.com>
2015-04-17 17:58:21 +0200

    NXP-16922 fixing filesystem access

--- nuxeo-webengine/nuxeo-webengine-gwt-codeserver/src/main/java/org/nuxeo/webengine/gwt/codeserver/CodeServerLoader.java
+++ nuxeo-webengine/nuxeo-webengine-gwt-codeserver/src/main/java/org/nuxeo/webengine/gwt/codeserver/CodeServerLoader.java
@@ -5,55 +5,71 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.net.URLClassLoader;
+import java.nio.file.FileSystemNotFoundException;
+import java.nio.file.FileSystems;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 public class CodeServerLoader extends URLClassLoader {
 
-    CodeServerLoader(URL[] jars)  {
-        super(jars , CodeServerLoader.class.getClassLoader());
-    }
+	CodeServerLoader(URL[] jars) {
+		super(jars, CodeServerLoader.class.getClassLoader());
+	}
 
-    @Override
-    protected java.lang.Class<?> loadClass(String name, boolean resolve) throws ClassNotFoundException {
-        Class<?> clazz = findLoadedClass(name);
-        if (clazz != null) {
-            return clazz;
-        }
+	@Override
+	protected java.lang.Class<?> loadClass(String name, boolean resolve) throws ClassNotFoundException {
+		Class<?> clazz = findLoadedClass(name);
+		if (clazz != null) {
+			return clazz;
+		}
 
-        if (name.equals(CodeServerLauncher.class.getName())) {
-            return getParent().loadClass(name);
-        }
+		if (name.equals(CodeServerLauncher.class.getName())) {
+			return getParent().loadClass(name);
+		}
 
-        if (name.equals(CodeServerWrapper.class.getName())) {
-            try {
-                return reloadClass(CodeServerWrapper.class);
-            } catch (URISyntaxException | IOException cause) {
-                throw new ClassNotFoundException("Cannot reload wrapper in gwt dev class loader", cause);
-            }
-        }
-        try {
-            return ClassLoader.getSystemClassLoader().loadClass(name);
-        } catch (ClassNotFoundException cause) {
-            ;
-        }
-        synchronized (getClassLoadingLock(name)) {
-            clazz = findClass(name);
-            if (clazz != null) {
-                return clazz;
-            }
-        }
-        throw new ClassNotFoundException("Cannot find " + name + " in gwt class loader");
-    };
+		if (name.equals(CodeServerWrapper.class.getName())) {
+			try {
+				return reloadClass(CodeServerWrapper.class);
+			} catch (URISyntaxException | IOException cause) {
+				throw new ClassNotFoundException("Cannot reload wrapper in gwt dev class loader", cause);
+			}
+		}
+		try {
+			return ClassLoader.getSystemClassLoader().loadClass(name);
+		} catch (ClassNotFoundException cause) {
+			;
+		}
+		synchronized (getClassLoadingLock(name)) {
+			clazz = findClass(name);
+			if (clazz != null) {
+				return clazz;
+			}
+		}
+		throw new ClassNotFoundException("Cannot find " + name + " in gwt class loader");
+	};
 
-    Class<?> reloadClass(Class<?> clazz) throws URISyntaxException, IOException {
-        URI location = clazz.getResource(clazz.getSimpleName().concat(".class")).toURI();
-        byte[] content = Files.readAllBytes(Paths.get(location));
-        return defineClass(clazz.getName(), content, 0, content.length);
-    }
+	Class<?> reloadClass(Class<?> clazz) throws URISyntaxException, IOException {
+		URI location = clazz.getResource(clazz.getSimpleName().concat(".class")).toURI();
+		byte[] content = Files.readAllBytes(getPath(location));
+		return defineClass(clazz.getName(), content, 0, content.length);
+	}
 
-    CodeServerLauncher load() throws ReflectiveOperationException {
-        return (CodeServerLauncher) loadClass(CodeServerWrapper.class.getName()).newInstance();
-    }
+	private Path getPath(URI location) throws IOException {
+		try {
+			return Paths.get(location);
+		} catch (FileSystemNotFoundException cause) {
+			Map<String, Object> env = Collections.emptyMap();
+			FileSystems.newFileSystem(location, env);
+			return Paths.get(location);
+		}
+	}
+
+	CodeServerLauncher load() throws ReflectiveOperationException {
+		return (CodeServerLauncher) loadClass(CodeServerWrapper.class.getName()).newInstance();
+	}
 
 }
