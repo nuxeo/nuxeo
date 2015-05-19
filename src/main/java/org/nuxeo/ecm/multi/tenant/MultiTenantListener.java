c8e7bbbe0650eb34cd37680e08c364eaa633f40b
Florent Guillaume <fg@nuxeo.com>
2015-05-19 14:57:11 +0200

    NXP-16532: don't suggest deleted domains as tenants (further fixes and tests)

--- src/main/java/org/nuxeo/ecm/multi/tenant/MultiTenantListener.java
+++ src/main/java/org/nuxeo/ecm/multi/tenant/MultiTenantListener.java
@@ -17,6 +17,10 @@
 
 package org.nuxeo.ecm.multi.tenant;
 
+import static org.nuxeo.ecm.core.api.LifeCycleConstants.DELETE_TRANSITION;
+import static org.nuxeo.ecm.core.api.LifeCycleConstants.TRANSITION_EVENT;
+import static org.nuxeo.ecm.core.api.LifeCycleConstants.TRANSTION_EVENT_OPTION_TRANSITION;
+import static org.nuxeo.ecm.core.api.LifeCycleConstants.UNDELETE_TRANSITION;
 import static org.nuxeo.ecm.core.api.event.DocumentEventTypes.DOCUMENT_CREATED;
 import static org.nuxeo.ecm.core.api.event.DocumentEventTypes.DOCUMENT_CREATED_BY_COPY;
 import static org.nuxeo.ecm.core.api.event.DocumentEventTypes.DOCUMENT_REMOVED;
@@ -45,28 +49,32 @@ public class MultiTenantListener implements EventListener {
 
         MultiTenantService multiTenantService = Framework.getLocalService(MultiTenantService.class);
         String tenantDocumentType = multiTenantService.getTenantDocumentType();
-        if (tenantDocumentType == null) {
+        DocumentModel doc = ((DocumentEventContext) ctx).getSourceDocument();
+        if (!doc.getType().equals(tenantDocumentType)) {
             return;
         }
 
-        DocumentEventContext docCtx = (DocumentEventContext) ctx;
-        if (DOCUMENT_CREATED.equals(event.getName()) || DOCUMENT_CREATED_BY_COPY.equals(event.getName())) {
-            DocumentModel doc = docCtx.getSourceDocument();
-            CoreSession session = docCtx.getCoreSession();
-            if (tenantDocumentType.equals(doc.getType())) {
-                if (multiTenantService.isTenantIsolationEnabled(session)) {
-                    multiTenantService.enableTenantIsolationFor(session, doc);
-                    session.save();
-                }
-            }
-        } else if (DOCUMENT_REMOVED.equals(event.getName())) {
-            DocumentModel doc = docCtx.getSourceDocument();
-            CoreSession session = docCtx.getCoreSession();
-            if (tenantDocumentType.equals(doc.getType())) {
-                if (multiTenantService.isTenantIsolationEnabled(session)) {
-                    multiTenantService.disableTenantIsolationFor(session, doc);
-                    session.save();
-                }
+        @SuppressWarnings("resource")
+        CoreSession session = ctx.getCoreSession();
+        if (!multiTenantService.isTenantIsolationEnabled(session)) {
+            return;
+        }
+
+        String eventName = event.getName();
+        if (DOCUMENT_CREATED.equals(eventName) || DOCUMENT_CREATED_BY_COPY.equals(eventName)) {
+            multiTenantService.enableTenantIsolationFor(session, doc);
+            session.save();
+        } else if (DOCUMENT_REMOVED.equals(eventName)) {
+            multiTenantService.disableTenantIsolationFor(session, doc);
+            session.save();
+        } else if (TRANSITION_EVENT.equals(eventName)) {
+            String transition = (String) ctx.getProperty(TRANSTION_EVENT_OPTION_TRANSITION);
+            if (DELETE_TRANSITION.equals(transition)) {
+                multiTenantService.disableTenantIsolationFor(session, doc);
+                session.save();
+            } else if (UNDELETE_TRANSITION.equals(transition)) {
+                multiTenantService.enableTenantIsolationFor(session, doc);
+                session.save();
             }
         }
     }
