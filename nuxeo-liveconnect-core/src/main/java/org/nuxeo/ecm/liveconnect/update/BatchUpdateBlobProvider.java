c5dbbefd84fc50a68897442491e5359c9e0503e8
Guillaume Renard <grenard@nuxeo.com>
2015-05-26 17:34:08 +0200

    NXP-16976: isolate nuxeo-liveconnect-core

--- nuxeo-liveconnect-core/src/main/java/org/nuxeo/ecm/liveconnect/update/BatchUpdateBlobProvider.java
+++ nuxeo-liveconnect-core/src/main/java/org/nuxeo/ecm/liveconnect/update/BatchUpdateBlobProvider.java
@@ -22,8 +22,21 @@ import java.io.IOException;
 import java.util.List;
 
 import org.nuxeo.ecm.core.api.DocumentModel;
+import org.nuxeo.ecm.liveconnect.update.listener.BlobProviderDocumentsUpdateListener;
 
 /**
+ * Interface to batch update documents provided by implementing provider. The method {@link #processDocumentsUpdate()}
+ * is called by {@link BlobProviderDocumentsUpdateListener}.
+ * <p>
+ * The implementation of {@link #processDocumentsUpdate()} must schedule a {@link BlobProviderDocumentsUpdateWork} with
+ * the document ids to be checked and updated if needed.
+ * <p>
+ * The @{link BlobProviderDocumentsUpdateWork} will then call the implementation of
+ * {@link #checkChangesAndUpdateBlob(List)}.
+ * <p>
+ * Note that it is recommended to schedule many workers dealing with a smaller amount of documents (using
+ * {@link #MAX_RESULT}) than a single one processing all document brought by the provider.
+ *
  * @since 7.3
  */
 public interface BatchUpdateBlobProvider {
@@ -31,12 +44,18 @@ public interface BatchUpdateBlobProvider {
     static final long MAX_RESULT = 50;
 
     /**
+     * Check the given list of document for change and update if needed. Note that session.save still needs to be called
+     * on changed documents.
+     *
      * @param documents to be checked for update
      * @return the list of DocumentModel that have changed
      * @throws IOException
      */
     List<DocumentModel> checkChangesAndUpdateBlob(List<DocumentModel> doc);
 
+    /**
+     * Trigger the documents update for the implementing providers.
+     */
     void processDocumentsUpdate();
 
 }
