69c694261a4e5f8760687cb84c2ec10db7d577d3
Guillaume Renard <grenard@nuxeo.com>
2015-06-01 18:18:33 +0200

    NXP-17126: add unit test

---
+++ nuxeo-features/nuxeo-platform-collections/nuxeo-platform-collections-core/src/test/java/org/nuxeo/ecm/collections/core/test/CollectionAsynchronousCheckinRestoreTest.java
@@ -0,0 +1,131 @@
+/*
+ * (C) Copyright 2015 Nuxeo SA (http://nuxeo.com/) and contributors.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser General Public License
+ * (LGPL) version 2.1 which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl-2.1.html
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * Contributors:
+ *     <a href="mailto:grenard@nuxeo.com">Guillaume Renard</a>
+ *
+ */
+
+package org.nuxeo.ecm.collections.core.test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+import org.nuxeo.ecm.collections.core.adapter.Collection;
+import org.nuxeo.ecm.collections.core.adapter.CollectionMember;
+import org.nuxeo.ecm.core.api.ClientException;
+import org.nuxeo.ecm.core.api.CoreSession;
+import org.nuxeo.ecm.core.api.DocumentModel;
+import org.nuxeo.ecm.core.api.DocumentRef;
+import org.nuxeo.ecm.core.api.PathRef;
+import org.nuxeo.ecm.core.api.VersioningOption;
+
+/**
+ * Test versioning on Collections.
+ *
+ * @since 7.3
+ */
+public class CollectionAsynchronousCheckinRestoreTest extends CollectionTestCase {
+
+    protected void testCheckingAndResotreCollection(final List<DocumentModel> docs) throws ClientException, InterruptedException {
+        List<DocumentModel> firstMembers = docs.subList(0, docs.size() / 2);
+        List<DocumentModel> secondMembers = docs.subList(docs.size() / 2, docs.size());
+
+        collectionManager.addToNewCollection(COLLECTION_NAME, COLLECTION_DESCRIPTION, firstMembers, session);
+
+        final String newlyCreatedCollectionPath = COLLECTION_FOLDER_PATH + "/" + COLLECTION_NAME;
+
+        final DocumentRef newCollectionRef = new PathRef(newlyCreatedCollectionPath);
+
+        assertTrue(session.exists(newCollectionRef));
+
+        DocumentModel newlyCreatedCollection = session.getDocument(newCollectionRef);
+
+        Collection collectionAdapter = newlyCreatedCollection.getAdapter(Collection.class);
+
+        final String newCollectionId = newlyCreatedCollection.getId();
+
+        for (DocumentModel file : firstMembers) {
+
+            file = session.getDocument(file.getRef());
+
+            assertTrue(collectionAdapter.getCollectedDocumentIds().contains(file.getId()));
+
+            CollectionMember collectionMemberAdapter = file.getAdapter(CollectionMember.class);
+
+            assertTrue(collectionMemberAdapter.getCollectionIds().contains(newCollectionId));
+        }
+
+        // Checkin collection
+        DocumentRef checkoutCollectionRef = null;
+        checkoutCollectionRef = newlyCreatedCollection.checkIn(VersioningOption.MAJOR, "a new version");
+        awaitCollectionWorks();
+
+        // Check that version's members are the same
+        DocumentModel checkoutCollection = session.getDocument(checkoutCollectionRef);
+        final String checkoutCollectionId = checkoutCollection.getId();
+        Collection checkoutCollectionAdapater = checkoutCollection.getAdapter(Collection.class);
+        for (DocumentModel file : firstMembers) {
+            assertTrue(checkoutCollectionAdapater.getCollectedDocumentIds().contains(file.getId()));
+            CollectionMember collectionMemberAdapter = session.getDocument(file.getRef()).getAdapter(
+                    CollectionMember.class);
+            assertTrue(collectionMemberAdapter.getCollectionIds().contains(checkoutCollectionId));
+        }
+        firstMembers = refresh(session, firstMembers);
+        secondMembers = refresh(session, secondMembers);
+
+        // Add new members to working copy of collection
+        collectionManager.addToCollection(newlyCreatedCollection, secondMembers, session);
+        // Remove old members from working copy of collection
+        collectionManager.removeAllFromCollection(newlyCreatedCollection, firstMembers, session);
+
+        // Restore collection
+        session.restoreToVersion(newCollectionRef, checkoutCollectionRef, true, true);
+        session.save();
+        awaitCollectionWorks();
+        newlyCreatedCollection = session.getDocument(newCollectionRef);
+
+        // Check members are correctly restored
+        collectionAdapter = newlyCreatedCollection.getAdapter(Collection.class);
+        for (DocumentModel file : firstMembers) {
+            assertTrue(collectionAdapter.getCollectedDocumentIds().contains(file.getId()));
+            CollectionMember collectionMemberAdapter = session.getDocument(file.getRef()).getAdapter(
+                    CollectionMember.class);
+            assertTrue(collectionMemberAdapter.getCollectionIds().contains(newCollectionId));
+        }
+        for (DocumentModel file : secondMembers) {
+            assertFalse(collectionAdapter.getCollectedDocumentIds().contains(file.getId()));
+            CollectionMember collectionMemberAdapter = session.getDocument(file.getRef()).getAdapter(
+                    CollectionMember.class);
+            assertFalse(collectionMemberAdapter.getCollectionIds().contains(newCollectionId));
+        }
+    }
+
+    protected List<DocumentModel> refresh(CoreSession session, List<DocumentModel> docs) {
+        List<DocumentModel> result = new ArrayList<DocumentModel>(docs.size());
+        for (DocumentModel doc : docs) {
+            result.add(session.getDocument(doc.getRef()));
+        }
+        return result;
+    }
+
+    @Test
+    public void testCheckinAndRestoreCollectionWithManyItems() throws ClientException, InterruptedException {
+        testCheckingAndResotreCollection(createTestFiles(session, MAX_CARDINALITY * 2));
+    }
+
+}
