c5364a92b2031470dffa79312cd078fadb551ec1
Anahide Tchertchian <at@nuxeo.com>
2015-06-09 11:47:41 +0200

    NXP-17103: remove handling of Nuxeo resources from JSF view handler after all (tags from nxr taglibs will handle resource resolution)

--- nuxeo-jsf/nuxeo-platform-ui-web/src/main/java/org/nuxeo/ecm/platform/ui/web/application/NuxeoResourceHandler.java
+++ nuxeo-jsf/nuxeo-platform-ui-web/src/main/java/org/nuxeo/ecm/platform/ui/web/application/NuxeoResourceHandler.java
@@ -21,9 +21,7 @@ import javax.faces.application.ResourceHandlerWrapper;
 import javax.faces.application.ViewResource;
 import javax.faces.context.FacesContext;
 
-import org.nuxeo.ecm.web.resources.api.Resource;
 import org.nuxeo.ecm.web.resources.api.service.WebResourceManager;
-import org.nuxeo.ecm.web.resources.wro.provider.NuxeoUriLocator;
 
 /**
  * Custom resource handler resolving resources thanks to {@link WebResourceManager} service, and handling error
@@ -43,13 +41,6 @@ public class NuxeoResourceHandler extends ResourceHandlerWrapper {
     public ViewResource createViewResource(FacesContext facesContext, String resourceName) {
         if (resourceName.startsWith(NuxeoUnknownResource.MARKER)) {
             return new NuxeoUnknownResource(resourceName.substring(NuxeoUnknownResource.MARKER.length()));
-        } else if (resourceName.startsWith(Resource.PREFIX)) {
-            Resource resource = NuxeoUriLocator.getResource(resourceName);
-            if (resource == null) {
-                return new NuxeoUnknownResource(resourceName);
-            } else {
-                return wrapped.createViewResource(facesContext, resource.getURI());
-            }
         }
         ViewResource res = wrapped.createViewResource(facesContext, resourceName);
         if (res == null) {
