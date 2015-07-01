be01d832056eae31aaf76fdf95eeac880f63309d
Anahide Tchertchian <at@nuxeo.com>
2015-07-01 17:56:45 +0200

    NXP-16425: move custom scripts using the scripts servlet out of includes.xhtml, remove empty placeholders

--- nuxeo-dm/nuxeo-platform-webapp/src/main/resources/web/nuxeo.war/scripts/confirmAlerts.js
+++
@@ -1,4 +0,0 @@
-// since 5.5, this resource is deprecated and kept empty here for
-// compatibility: confirm alerts and associated translation mechanism are
-// now handled by confirm-alerts.xhtml for default confirm alerts, and
-// custom-confirm-alerts.xhtml (both included by includes.xhtml)
