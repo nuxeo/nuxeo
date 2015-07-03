8b4ae2e9fffb515b1b992e4fece7daff10d9aa10
Florent Guillaume <fg@nuxeo.com>
2015-07-03 11:32:52 +0200

    NXP-17439: fix NPE when there is no selected user

--- nuxeo-jsf/nuxeo-platform-webapp-base/src/main/java/org/nuxeo/ecm/webapp/security/UserManagementActions.java
+++ nuxeo-jsf/nuxeo-platform-webapp-base/src/main/java/org/nuxeo/ecm/webapp/security/UserManagementActions.java
@@ -155,7 +155,7 @@ public class UserManagementActions extends AbstractUserGroupManagement implement
     }
 
     public boolean getAllowEditUser() throws ClientException {
-        return getCanEditUsers(true) && !BaseSession.isReadOnlyEntry(selectedUser);
+        return selectedUser != null && getCanEditUsers(true) && !BaseSession.isReadOnlyEntry(selectedUser);
     }
 
     protected boolean getCanEditUsers(boolean allowCurrentUser) throws ClientException {
@@ -193,7 +193,7 @@ public class UserManagementActions extends AbstractUserGroupManagement implement
     }
 
     public boolean getAllowChangePassword() throws ClientException {
-        return getCanEditUsers(true) && !BaseSession.isReadOnlyEntry(selectedUser);
+        return selectedUser != null && getCanEditUsers(true) && !BaseSession.isReadOnlyEntry(selectedUser);
     }
 
     public boolean getAllowCreateUser() throws ClientException {
@@ -201,7 +201,7 @@ public class UserManagementActions extends AbstractUserGroupManagement implement
     }
 
     public boolean getAllowDeleteUser() throws ClientException {
-        return getCanEditUsers(false) && !BaseSession.isReadOnlyEntry(selectedUser);
+        return selectedUser != null && getCanEditUsers(false) && !BaseSession.isReadOnlyEntry(selectedUser);
     }
 
     public void clearSearch() {
