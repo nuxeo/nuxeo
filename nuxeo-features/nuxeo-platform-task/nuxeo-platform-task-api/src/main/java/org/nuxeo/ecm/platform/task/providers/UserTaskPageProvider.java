ed7d26fa878720a9a9394367bc5a0beebac4978f
Vladimir Pasquier <vpasquier@nuxeo.com>
2015-07-06 22:59:42 +0200

    NXP-17452: add content view to list all tasks from current user as actor or delegated actor - update task dashboard content view

--- nuxeo-features/nuxeo-platform-task/nuxeo-platform-task-api/src/main/java/org/nuxeo/ecm/platform/task/providers/UserTaskPageProvider.java
+++ nuxeo-features/nuxeo-platform-task/nuxeo-platform-task-api/src/main/java/org/nuxeo/ecm/platform/task/providers/UserTaskPageProvider.java
@@ -114,7 +114,7 @@ public class UserTaskPageProvider extends
             boolean filterTrashDocs = getFilterDocumentsInTrash();
             NuxeoPrincipal pal = (NuxeoPrincipal) coreSession.getPrincipal();
             TaskService taskService = Framework.getService(TaskService.class);
-            List<Task> tasks = taskService.getCurrentTaskInstances(coreSession, getSortInfos());
+            List<Task> tasks = taskService.getAllCurrentTaskInstances(coreSession, getSortInfos());
             if (tasks != null) {
                 for (Task task : tasks) {
                     try {
