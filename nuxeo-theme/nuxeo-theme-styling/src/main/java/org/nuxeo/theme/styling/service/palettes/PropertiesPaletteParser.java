f9b073f09ce1f67401f0bcf456c58267ffe781fe
Anahide Tchertchian <at@nuxeo.com>
2015-07-07 11:05:46 +0200

    NXP-16425: fix copyright headers

--- nuxeo-theme/nuxeo-theme-styling/src/main/java/org/nuxeo/theme/styling/service/palettes/PropertiesPaletteParser.java
+++ nuxeo-theme/nuxeo-theme-styling/src/main/java/org/nuxeo/theme/styling/service/palettes/PropertiesPaletteParser.java
@@ -1,15 +1,19 @@
 /*
- * (C) Copyright 2006-2007 Nuxeo SAS <http://nuxeo.com> and others
+ * (C) Copyright 2006-2015 Nuxeo SA (http://nuxeo.com/) and contributors.
  *
  * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+ * are made available under the terms of the GNU Lesser General Public License
+ * (LGPL) version 2.1 which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl-2.1.html
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
  *
  * Contributors:
  *     Jean-Marc Orliaguet, Chalmers
- *
- * $Id$
+ *     Anahide Tchertchian
  */
 
 package org.nuxeo.theme.styling.service.palettes;
