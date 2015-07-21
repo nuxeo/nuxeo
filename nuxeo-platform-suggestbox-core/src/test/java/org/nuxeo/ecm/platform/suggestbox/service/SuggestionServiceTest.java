02952380d48fb573a3baf739fb1b76f2ac9ba664
Florent Guillaume <fg@nuxeo.com>
2015-07-21 22:39:10 +0200

    NXP-17539: remove leftover TransactionalFeature

--- nuxeo-platform-suggestbox-core/src/test/java/org/nuxeo/ecm/platform/suggestbox/service/SuggestionServiceTest.java
+++ nuxeo-platform-suggestbox-core/src/test/java/org/nuxeo/ecm/platform/suggestbox/service/SuggestionServiceTest.java
@@ -37,7 +37,6 @@ import org.nuxeo.ecm.core.api.CoreSession;
 import org.nuxeo.ecm.core.api.DocumentModel;
 import org.nuxeo.ecm.core.api.NuxeoPrincipal;
 import org.nuxeo.ecm.core.test.CoreFeature;
-import org.nuxeo.ecm.core.test.TransactionalFeature;
 import org.nuxeo.ecm.core.test.annotations.Granularity;
 import org.nuxeo.ecm.core.test.annotations.RepositoryConfig;
 import org.nuxeo.ecm.directory.Session;
@@ -52,7 +51,7 @@ import org.nuxeo.runtime.test.runner.FeaturesRunner;
 import org.nuxeo.runtime.transaction.TransactionHelper;
 
 @RunWith(FeaturesRunner.class)
-@Features({ TransactionalFeature.class, CoreFeature.class })
+@Features(CoreFeature.class)
 @RepositoryConfig(cleanup = Granularity.METHOD)
 @Deploy({ "org.nuxeo.ecm.platform.query.api", //
         "org.nuxeo.ecm.platform.types.api", //
