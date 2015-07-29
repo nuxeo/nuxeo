9e2e90caf81cc6744ad7bec06c8a986209d24124
Antoine Taillefer <ataillefer@nuxeo.com>
2015-07-29 13:01:42 +0200

    NXP-17602: Add missing throws ClientException

--- nuxeo-drive-operations/src/main/java/org/nuxeo/drive/operations/NuxeoDriveAttachBlob.java
+++ nuxeo-drive-operations/src/main/java/org/nuxeo/drive/operations/NuxeoDriveAttachBlob.java
@@ -27,6 +27,7 @@ import org.nuxeo.ecm.automation.core.annotations.Operation;
 import org.nuxeo.ecm.automation.core.annotations.OperationMethod;
 import org.nuxeo.ecm.automation.core.annotations.Param;
 import org.nuxeo.ecm.core.api.Blob;
+import org.nuxeo.ecm.core.api.ClientException;
 import org.nuxeo.ecm.core.api.CoreSession;
 import org.nuxeo.ecm.core.api.DocumentModel;
 import org.nuxeo.ecm.core.api.NuxeoException;
@@ -58,7 +59,7 @@ public class NuxeoDriveAttachBlob {
     protected String factoryName = "defaultFileSystemItemFactory";
 
     @OperationMethod
-    public Blob run(Blob blob) {
+    public Blob run(Blob blob) throws ClientException {
         if (applyVersioningPolicy) {
             FileSystemItemFactory factory = ((FileSystemItemAdapterServiceImpl) Framework.getService(FileSystemItemAdapterService.class)).getFileSystemItemFactory(factoryName);
             if (!(factory instanceof VersioningFileSystemItemFactory)) {
