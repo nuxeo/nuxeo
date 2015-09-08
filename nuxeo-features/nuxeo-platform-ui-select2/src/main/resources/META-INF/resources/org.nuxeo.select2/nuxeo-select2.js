d9ac8897d388b53667150f8dccc00581df07acab
Guillaume Renard <grenard@nuxeo.com>
2015-09-08 18:01:12 +0200

    NXP-17817: remove useless %

--- nuxeo-features/nuxeo-platform-ui-select2/src/main/resources/META-INF/resources/org.nuxeo.select2/nuxeo-select2.js
+++ nuxeo-features/nuxeo-platform-ui-select2/src/main/resources/META-INF/resources/org.nuxeo.select2/nuxeo-select2.js
@@ -113,7 +113,7 @@
       temp.searchType = params.userSuggestionSearchType;
     } else if (params.operationId == 'Document.PageProvider') {
       // build default operation for Document
-      temp.searchTerm = query.term + "%";
+      temp.searchTerm = query.term;
       temp.query = params.query;
       temp.providerName = params.pageProviderName;
       temp.page = "0";
