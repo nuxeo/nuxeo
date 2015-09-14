df7f4f36c3aa1bcce346c438b46dc4b9555e30b2
Florent Guillaume <fguillaume@nuxeo.com>
2015-09-14 18:52:19 +0200

    NXP-17827: get rid of ExtendedBlobProvider for cleaner interface

--- nuxeo-drive-core/src/main/java/org/nuxeo/drive/service/impl/DefaultFileSystemItemFactory.java
+++ nuxeo-drive-core/src/main/java/org/nuxeo/drive/service/impl/DefaultFileSystemItemFactory.java
@@ -38,7 +38,6 @@ import org.nuxeo.ecm.core.api.VersioningOption;
 import org.nuxeo.ecm.core.api.blobholder.BlobHolder;
 import org.nuxeo.ecm.core.blob.BlobManager;
 import org.nuxeo.ecm.core.blob.BlobProvider;
-import org.nuxeo.ecm.core.blob.ExtendedBlobProvider;
 import org.nuxeo.runtime.api.Framework;
 
 /**
@@ -155,7 +154,7 @@ public class DefaultFileSystemItemFactory extends AbstractFileSystemItemFactory 
             BlobManager blobManager = Framework.getService(BlobManager.class);
             BlobHolder bh = doc.getAdapter(BlobHolder.class);
             BlobProvider blobProvider = blobManager.getBlobProvider(bh.getBlob());
-            if (blobProvider != null && blobProvider instanceof ExtendedBlobProvider) {
+            if (blobProvider != null && !blobProvider.supportsWrite()) {
                 if (log.isDebugEnabled()) {
                     log.debug(String.format(
                         "Blob for Document %s is backed by an ExtendedBlobProvider, it cannot be adapted as a FileSystemItem.",
