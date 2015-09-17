f00e4a3806de136e77ad9bc342f663e189a1f55a
ron1 <ron1@localhost>
2015-09-17 09:00:31 -0400

    NXP-16959: Assign rendition blob filename if needed

--- nuxeo-features/nuxeo-platform-rendition/nuxeo-platform-rendition-core/src/main/java/org/nuxeo/ecm/platform/rendition/extension/AutomationRenderer.java
+++ nuxeo-features/nuxeo-platform-rendition/nuxeo-platform-rendition-core/src/main/java/org/nuxeo/ecm/platform/rendition/extension/AutomationRenderer.java
@@ -19,6 +19,8 @@ package org.nuxeo.ecm.platform.rendition.extension;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.commons.io.FilenameUtils;
+import org.apache.commons.lang.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.nuxeo.ecm.automation.AutomationService;
@@ -29,7 +31,9 @@ import org.nuxeo.ecm.core.api.CoreSession;
 import org.nuxeo.ecm.core.api.DocumentModel;
 import org.nuxeo.ecm.core.api.NuxeoException;
 import org.nuxeo.ecm.core.api.blobholder.BlobHolder;
+import org.nuxeo.ecm.platform.mimetype.interfaces.MimetypeRegistry;
 import org.nuxeo.ecm.platform.rendition.service.RenditionDefinition;
+import org.nuxeo.ecm.platform.mimetype.interfaces.MimetypeEntry;
 import org.nuxeo.runtime.api.Framework;
 
 /**
@@ -42,6 +46,13 @@ public class AutomationRenderer {
 
     protected static final Log log = LogFactory.getLog(AutomationRenderer.class);
 
+    /**
+     * Test if the Rendition is available on the given DocumentModel
+     *
+     * @param doc the target {@link DocumentModel}
+     * @param def the {@link RenditionDefinition} to use
+     * @return The test result
+     */
     public static boolean isRenditionAvailable(DocumentModel doc, RenditionDefinition def) {
         String chain = def.getOperationChain();
         if (chain == null) {
@@ -78,6 +89,15 @@ public class AutomationRenderer {
         return true;
     }
 
+    /**
+     * Generate the rendition Blobs for a given {@link RenditionDefinition}. Return is a List of Blob for bigger
+     * flexibility (typically HTML rendition with resources)
+     *
+     * @param doc the target {@link DocumentModel}
+     * @param definition the {@link RenditionDefinition} to use
+     * @param session the {@link CoreSession} to use
+     * @return The list of Blobs
+     */
     public static List<Blob> render(DocumentModel doc, RenditionDefinition definition, CoreSession session) {
 
         String chain = definition.getOperationChain();
@@ -112,6 +132,10 @@ public class AutomationRenderer {
             }
 
             Blob blob = (Blob) as.run(oc, definition.getOperationChain());
+            if (blob != null && StringUtils.isBlank(blob.getFilename())) {
+                String filename = getFilenameWithExtension(doc.getTitle(), blob.getMimeType(), "bin");
+                blob.setFilename(filename);
+            }
             List<Blob> blobs = new ArrayList<Blob>();
             blobs.add(blob);
             return blobs;
@@ -120,6 +144,30 @@ public class AutomationRenderer {
             throw new NuxeoException("Exception while running the operation chain: "
                     + definition.getOperationChain(), e);
         }
+    }
+
+    /**
+     * Generate a revised filename whose extension is either based on the supplied mimeType if applicable or the
+     * supplied default extension.
+     *
+     * @param filename  the filename to use
+     * @param mimeType  the mimeType from which the assigned extension is derived
+     * @param defaultExtension  the default extension to be assigned if the mimeType has no corresponding extension
+     * @return the filename with the revised extension
+     * @since 7.4
+     */
+    public static String getFilenameWithExtension(String filename, String mimeType, String defaultExtension) {
+        String baseName = FilenameUtils.getBaseName(filename);
+        MimetypeRegistry mimetypeRegistry = Framework.getLocalService(MimetypeRegistry.class);
+        MimetypeEntry mimeTypeEntry = mimetypeRegistry.getMimetypeEntryByMimeType(mimeType);
+        List<String> extensions = mimeTypeEntry.getExtensions();
+        String extension;
+        if (!extensions.isEmpty()) {
+            extension = extensions.get(0);
+        } else {
+            extension = defaultExtension;
+        }
+        return (extension == null) ? filename : baseName + "." + extension;
     }
 
 }
