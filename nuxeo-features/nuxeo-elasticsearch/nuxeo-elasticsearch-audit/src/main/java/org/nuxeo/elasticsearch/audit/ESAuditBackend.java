b47bd6045896cc6635a0510cc89eff6f709dbf96
Maxime HILAIRE <mhilaire@nuxeo.com>
2015-10-06 22:50:52 -0700

    NXP-18086: add more debug logs

--- nuxeo-features/nuxeo-elasticsearch/nuxeo-elasticsearch-audit/src/main/java/org/nuxeo/elasticsearch/audit/ESAuditBackend.java
+++ nuxeo-features/nuxeo-elasticsearch/nuxeo-elasticsearch-audit/src/main/java/org/nuxeo/elasticsearch/audit/ESAuditBackend.java
@@ -352,6 +352,10 @@ public class ESAuditBackend extends AbstractAuditBackend implements
 
             for (LogEntry entry : entries) {
                 entry.setId(sg.getNextId(SEQ_NAME));
+                if (log.isDebugEnabled()) {
+                    log.debug(String.format("Indexing log enry Id: %s, with logDate : %s, for docUUID: %s ",
+                            entry.getId(), entry.getLogDate(), entry.getDocUUID()));
+                }
                 XContentBuilder builder = jsonBuilder();
                 JsonGenerator jsonGen = factory.createJsonGenerator(builder.stream());
                 AuditEntryJSONWriter.asJSON(jsonGen, entry);
