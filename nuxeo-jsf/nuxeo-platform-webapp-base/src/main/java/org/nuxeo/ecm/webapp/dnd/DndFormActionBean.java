22fa1d912eb6079f49d8659dbe444f2aae3c7367
Thierry Martins <tmartins@nuxeo.com>
2015-10-14 13:59:57 +0200

    NXP-18128: use a DocumentModel as data collector + remove deprecated fields/methods

--- nuxeo-jsf/nuxeo-platform-webapp-base/src/main/java/org/nuxeo/ecm/webapp/dnd/DndFormActionBean.java
+++ nuxeo-jsf/nuxeo-platform-webapp-base/src/main/java/org/nuxeo/ecm/webapp/dnd/DndFormActionBean.java
@@ -1,31 +1,23 @@
 /*
- * (C) Copyright 2006-2007 Nuxeo SAS (http://nuxeo.com/) and contributors.
+ * Copyright (c) 2006-2015 Nuxeo SA (http://nuxeo.com/) and others.
  *
  * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the GNU Lesser General Public License
- * (LGPL) version 2.1 which accompanies this distribution, and is available at
- * http://www.gnu.org/licenses/lgpl.html
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Lesser General Public License for more details.
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *     Nuxeo - initial API and implementation
- *
+ *     Thierry Delprat
+ *     Thierry Martins
  */
 package org.nuxeo.ecm.webapp.dnd;
 
 import java.io.IOException;
 import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Date;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
@@ -39,7 +31,13 @@ import org.jboss.seam.annotations.Scope;
 import org.jboss.seam.annotations.web.RequestParameter;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.nuxeo.ecm.core.api.DataModel;
+import org.nuxeo.ecm.core.api.DocumentModel;
+import org.nuxeo.ecm.core.api.impl.SimpleDocumentModel;
+import org.nuxeo.ecm.core.schema.SchemaManager;
+import org.nuxeo.ecm.core.schema.types.Schema;
 import org.nuxeo.ecm.core.schema.utils.DateParser;
+import org.nuxeo.runtime.api.Framework;
 
 /**
  * Seam action bean that is used to handle the meta-data form for the Drag&Drop feature
@@ -52,33 +50,7 @@ public class DndFormActionBean implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
-    protected Map<String, Map<String, Serializable>> metadataCollector;
-
-    /**
-     * @deprecated since 5.7
-     */
-    @Deprecated
-    protected String currentSchema;
-
-    /**
-     * @deprecated since 5.7
-     */
-    @Deprecated
-    protected String currentLayout;
-
-    /**
-     * @deprecated since 5.7
-     */
-    @RequestParameter
-    @Deprecated
-    protected String schema;
-
-    /**
-     * @deprecated since 5.7
-     */
-    @RequestParameter
-    @Deprecated
-    protected String layout;
+    protected DocumentModel metadataCollector;
 
     /**
      * @since 5.7
@@ -102,34 +74,18 @@ public class DndFormActionBean implements Serializable {
     @RequestParameter
     protected String layouts;
 
-    /**
-     * @deprecated since 5.7. Use {@link #getSchemas()}.
-     */
-    @Deprecated
-    public String getSchema() {
-        if (schema != null && !schema.isEmpty()) {
-            currentSchema = schema;
-        }
-        return currentSchema;
-    }
-
-    /**
-     * @deprecated since 5.7. Use {@link #getLayouts()}.
-     */
-    @Deprecated
-    public String getLayout() {
-        if (layout != null && !layout.isEmpty()) {
-            currentLayout = layout;
-        }
-        return currentLayout;
-    }
-
     public List<String> getSchemas() {
         currentSchemas = new ArrayList<>();
         if (StringUtils.isNotBlank(schemas)) {
-            currentSchemas.addAll(Arrays.asList(schemas.split(",")));
-        } else if (StringUtils.isNotBlank(schema)) {
-            currentSchemas.add(schema);
+            SchemaManager sc = Framework.getLocalService(SchemaManager.class);
+            for (String schemaName : schemas.split(",")) {
+                Schema schema = sc.getSchemaFromPrefix(schemaName);
+                if (schema != null) {
+                    currentSchemas.add(schema.getName());
+                } else {
+                    currentSchemas.add(schemaName);
+                }
+            }
         }
         return currentSchemas;
     }
@@ -137,19 +93,14 @@ public class DndFormActionBean implements Serializable {
     public String getLayouts() {
         if (StringUtils.isNotBlank(layouts)) {
             currentLayouts = layouts;
-        } else if (StringUtils.isNotBlank(layout)) {
-            currentLayouts = layout;
         }
         return currentLayouts;
     }
 
     @Factory(value = "dataCollector", scope = ScopeType.PAGE)
-    public Map<String, Map<String, Serializable>> getCollector() {
+    public DocumentModel getCollector() {
         if (metadataCollector == null) {
-            metadataCollector = new HashMap<String, Map<String, Serializable>>();
-            for (String schema : getSchemas()) {
-                metadataCollector.put(schema, new HashMap<String, Serializable>());
-            }
+            metadataCollector = new SimpleDocumentModel(getSchemas());
         }
         return metadataCollector;
     }
@@ -181,18 +132,19 @@ public class DndFormActionBean implements Serializable {
         sb.append("var collectedData= ");
         JSONObject jsonObject = new JSONObject();
 
-        Map<String, Map<String, Serializable>> metadataCollector = getCollector();
+        DocumentModel collector = getCollector();
         // Collect meta-data
         JSONObject formData = new JSONObject();
-        for (String key : metadataCollector.keySet()) {
-            for (String field : metadataCollector.get(key).keySet()) {
-                Object data = metadataCollector.get(key).get(field);
+        for (String key : collector.getSchemas()) {
+            DataModel dataModel = collector.getDataModel(key);
+            for (String field : dataModel.getDirtyFields()) {
+                Object data = dataModel.getData(field);
                 if (data instanceof Date) {
                     data = DateParser.formatW3CDateTime((Date) data);
                 } else if (data instanceof Calendar) {
                     data = DateParser.formatW3CDateTime(((Calendar) data).getTime());
                 }
-                formData.put(key + ":" + field, data);
+                formData.put(field, data);
             }
         }
         jsonObject.put("docMetaData", formData);
