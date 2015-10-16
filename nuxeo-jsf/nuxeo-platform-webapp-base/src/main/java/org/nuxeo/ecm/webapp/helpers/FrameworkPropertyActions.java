c1945a456f14e4c6ce6646044738bf4a7587a8b1
Andre Justo <ajusto@nuxeo.com>
2015-10-16 09:52:57 +0100

    NXP-18018: use Framework instead of configuration service in Seam component 'frameworkPropertyActions'

--- nuxeo-jsf/nuxeo-platform-webapp-base/src/main/java/org/nuxeo/ecm/webapp/helpers/FrameworkPropertyActions.java
+++ nuxeo-jsf/nuxeo-platform-webapp-base/src/main/java/org/nuxeo/ecm/webapp/helpers/FrameworkPropertyActions.java
@@ -19,16 +19,13 @@ package org.nuxeo.ecm.webapp.helpers;
 import static org.jboss.seam.annotations.Install.FRAMEWORK;
 
 import org.jboss.seam.ScopeType;
-import org.jboss.seam.annotations.In;
 import org.jboss.seam.annotations.Install;
 import org.jboss.seam.annotations.Name;
 import org.jboss.seam.annotations.Scope;
 import org.nuxeo.runtime.api.Framework;
-import org.nuxeo.runtime.services.config.ConfigurationService;
 
 /**
- * Seam component that exposes getters for properties managed by the {@link ConfigurationService} or the runtime
- * {@link Framework}.
+ * Seam component that exposes getters for properties managed by the runtime {@link Framework}.
  *
  * @since 5.5
  */
@@ -37,49 +34,27 @@ import org.nuxeo.runtime.services.config.ConfigurationService;
 @Install(precedence = FRAMEWORK)
 public class FrameworkPropertyActions {
 
-    @In(create = true)
-    protected transient ConfigurationService configurationService;
-
     /**
-     * Returns the given property value from the {@link ConfigurationService} if any, otherwise null.
+     * Returns the given property value from the {@link Framework} if any, otherwise null.
      */
     public String getProperty(String propertyName) {
-        return configurationService.getProperty(propertyName);
+        return Framework.getProperty(propertyName);
     }
 
     /**
-     * Returns the given property value from the {@link ConfigurationService} if any, otherwise the given default value.
+     * Returns the given property value from the {@link Framework} if any, otherwise the given default value.
      */
     public String getProperty(String propertyName, String defaultValue) {
-        return configurationService.getProperty(propertyName, defaultValue);
+        return Framework.getProperty(propertyName, defaultValue);
     }
 
     /**
      * Returns true if given property has been setup to true (defaults to false if not set).
      *
      * @since 5.8
-     * @see {@link ConfigurationService#isBooleanPropertyTrue(String)}
+     * @see {@link Framework#isBooleanPropertyTrue(String)}
      */
     public boolean isBooleanPropertyTrue(String propertyName) {
-        return configurationService.isBooleanPropertyTrue(propertyName);
+        return Framework.isBooleanPropertyTrue(propertyName);
     }
-
-    /**
-     * Returns the given property value from the {@link Framework} if any, otherwise null.
-     *
-     * @since 7.4
-     */
-    public String getFrameworkProperty(String key) {
-        return Framework.getProperty(key);
-    }
-
-    /**
-     * Returns the given property value from the {@link Framework} if any, otherwise the given default value.
-     *
-     * @since 7.4
-     */
-    public String getFrameworkProperty(String key, String defValue) {
-        return Framework.getProperty(key, defValue);
-    }
-
 }
