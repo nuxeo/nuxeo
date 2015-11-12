7d2c5d3419961c3a817aad80b337e1694f6313a5
Vladimir Pasquier <vpasquier@nuxeo.com>
2015-11-12 15:28:36 -0500

    NXP-18141: set 'mail.transport.user' in global conf replacing jboss 'mail.transport.username' and handle compatibility

--- nuxeo-runtime/nuxeo-launcher-commons/src/main/java/org/nuxeo/launcher/config/ConfigurationGenerator.java
+++ nuxeo-runtime/nuxeo-launcher-commons/src/main/java/org/nuxeo/launcher/config/ConfigurationGenerator.java
@@ -324,6 +324,7 @@ public class ConfigurationGenerator {
             put("mail.smtp.host", "mail.transport.host");
             put("mail.smtp.port", "mail.transport.port");
             put("mail.smtp.username", "mail.transport.username");
+            put("mail.transport.username", "mail.transport.user");
             put("mail.smtp.password", "mail.transport.password");
             put("mail.smtp.usetls", "mail.transport.usetls");
             put("mail.smtp.auth", "mail.transport.auth");
