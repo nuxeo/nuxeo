2cae621464cb88e81e1a043ed15d7657a5409abf
Julien Carsique <julien.carsique@gmail.com>
2015-12-14 18:53:01 +0100

    NXP-13965: cleanup & format

--- nuxeo-platform-video-convert/src/test/java/org/nuxeo/ecm/platform/video/convert/VideoConversionTest.java
+++ nuxeo-platform-video-convert/src/test/java/org/nuxeo/ecm/platform/video/convert/VideoConversionTest.java
@@ -1,5 +1,5 @@
 /*
- * (C) Copyright 2006-2011 Nuxeo SA (http://nuxeo.com/) and others.
+ * (C) Copyright 2006-2015 Nuxeo SA (http://nuxeo.com/) and others.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the GNU Lesser General Public License
@@ -54,6 +54,7 @@ public class VideoConversionTest extends NXRuntimeTestCase {
 
     public static final String DELTA_OGV = "DELTA.ogv";
 
+    @Override
     @Before
     public void setUp() throws Exception {
         super.setUp();
