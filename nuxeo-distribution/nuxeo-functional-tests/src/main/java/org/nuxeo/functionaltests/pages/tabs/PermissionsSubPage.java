b9e42e5fc4d17e9bb479d509b22e4c6f62e12927
Thomas Roger <troger@nuxeo.com>
2015-12-21 16:54:16 +0100

    NXP-17225: improve labels

--- nuxeo-distribution/nuxeo-functional-tests/src/main/java/org/nuxeo/functionaltests/pages/tabs/PermissionsSubPage.java
+++ nuxeo-distribution/nuxeo-functional-tests/src/main/java/org/nuxeo/functionaltests/pages/tabs/PermissionsSubPage.java
@@ -36,7 +36,7 @@ import org.openqa.selenium.support.FindBy;
 public class PermissionsSubPage extends AbstractPage {
 
     @Required
-    @FindBy(xpath = "//paper-button[text()='New Permission']")
+    @FindBy(xpath = "//div[contains(@class, 'jsLocalPermissions')]/*/paper-button")
     WebElement newPermission;
 
     public PermissionsSubPage(WebDriver driver) {
