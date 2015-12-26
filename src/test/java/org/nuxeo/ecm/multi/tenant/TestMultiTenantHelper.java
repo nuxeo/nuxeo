ccadc3912e0c9150656a8d724bf40ad92be211fa
Florent Guillaume <fguillaume@nuxeo.com>
2015-12-26 03:04:39 +0100

    NXP-18664: add missing Nuxeo copyright headers

--- src/test/java/org/nuxeo/ecm/multi/tenant/TestMultiTenantHelper.java
+++ src/test/java/org/nuxeo/ecm/multi/tenant/TestMultiTenantHelper.java
@@ -1,3 +1,21 @@
+/*
+ * (C) Copyright 2015 Nuxeo SA (http://nuxeo.com/) and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Contributors:
+ *     Vincent Dutat
+ */
 package org.nuxeo.ecm.multi.tenant;
 
 import static org.junit.Assert.*;
@@ -31,7 +49,7 @@ public class TestMultiTenantHelper {
 
     /**
      * just to test it does not throw an exception.
-     * 
+     *
      * @throws Exception
      */
     @Test public void testGetCurrentTenantId() throws Exception {
@@ -40,7 +58,7 @@ public class TestMultiTenantHelper {
 
     /**
      * just to test it does not throw an exception.
-     * 
+     *
      * @throws Exception
      */
     @Test public void testGetTenantId() throws Exception {
