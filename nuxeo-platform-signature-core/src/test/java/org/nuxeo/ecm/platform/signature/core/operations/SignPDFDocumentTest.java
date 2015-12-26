a2459e8733a3e2bdf245701ce4429dafc5bca9a3
Florent Guillaume <fguillaume@nuxeo.com>
2015-12-26 03:09:52 +0100

    NXP-18664: add missing Nuxeo copyright headers

--- nuxeo-platform-signature-core/src/test/java/org/nuxeo/ecm/platform/signature/core/operations/SignPDFDocumentTest.java
+++ nuxeo-platform-signature-core/src/test/java/org/nuxeo/ecm/platform/signature/core/operations/SignPDFDocumentTest.java
@@ -1,3 +1,21 @@
+/*
+ * (C) Copyright 2014 Nuxeo SA (http://nuxeo.com/) and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Contributors:
+ *     Vladimir Pasquier
+ */
 package org.nuxeo.ecm.platform.signature.core.operations;
 
 import static org.junit.Assert.*;
@@ -33,7 +51,7 @@ import org.nuxeo.runtime.test.runner.FeaturesRunner;
 
 @RunWith(FeaturesRunner.class)
 @Features({ PlatformFeature.class, AutomationFeature.class })
-@Deploy({ 
+@Deploy({
     "org.nuxeo.ecm.platform.signature.core",
     "org.nuxeo.ecm.platform.signature.core.test",
     "org.nuxeo.ecm.core.convert.api", //
