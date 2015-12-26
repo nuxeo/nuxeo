c0976cb6b0935f3df8d858f786ba44683c3c2a86
Florent Guillaume <fguillaume@nuxeo.com>
2015-12-26 03:10:14 +0100

    NXP-18664: add missing Nuxeo copyright headers

--- nuxeo-template-rendering-api/src/main/java/org/nuxeo/template/api/context/ContextExtensionFactory.java
+++ nuxeo-template-rendering-api/src/main/java/org/nuxeo/template/api/context/ContextExtensionFactory.java
@@ -1,3 +1,21 @@
+/*
+ * (C) Copyright 2012 Nuxeo SA (http://nuxeo.com/) and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Contributors:
+ *     Thierry Delprat
+ */
 package org.nuxeo.template.api.context;
 
 import java.util.Map;
@@ -7,7 +25,7 @@ import org.nuxeo.ecm.core.api.DocumentModel;
 /**
  * Factory used to register new Context Extensions The resturned Object will be directly accessible inside the Rendering
  * context
- * 
+ *
  * @author <a href="mailto:tdelprat@nuxeo.com">Tiry</a>
  */
 public interface ContextExtensionFactory {
@@ -21,7 +39,7 @@ public interface ContextExtensionFactory {
      * <p>
      * you can also directly push in the provided ctx map
      * </p>
-     * 
+     *
      * @param currentDocument
      * @param wrapper
      * @param ctx
