3e679ed20bc96187c1285be6c43065c7eb45ffd3
Kevin Leturc <kleturc@nuxeo.com>
2015-12-30 11:56:51 +0100

    NXP-18659: Add unit tests.

--- nuxeo-liveconnect-box/src/test/java/org/nuxeo/ecm/liveconnect/box/TestBoxBlobProvider.java
+++ nuxeo-liveconnect-box/src/test/java/org/nuxeo/ecm/liveconnect/box/TestBoxBlobProvider.java
@@ -19,14 +19,24 @@
 package org.nuxeo.ecm.liveconnect.box;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.UUID;
 
 import javax.inject.Inject;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.nuxeo.ecm.core.api.Blob;
+import org.nuxeo.ecm.core.api.DocumentModel;
+import org.nuxeo.ecm.core.api.impl.DocumentModelImpl;
 import org.nuxeo.ecm.core.blob.BlobManager;
-import org.nuxeo.ecm.core.blob.BlobProvider;
+import org.nuxeo.ecm.core.blob.BlobManager.BlobInfo;
+import org.nuxeo.ecm.core.blob.SimpleManagedBlob;
 import org.nuxeo.runtime.test.runner.RuntimeHarness;
 
 public class TestBoxBlobProvider extends BoxTestCase {
@@ -40,25 +50,68 @@ public class TestBoxBlobProvider extends BoxTestCase {
 
     private static final String FILE_1_NAME = "tigers.jpeg";
 
+    @Inject
+    private RuntimeHarness harness;
 
     @Inject
-    protected RuntimeHarness harness;
+    private BlobManager blobManager;
 
-    @Inject
-    protected BlobManager blobManager;
+    private BoxBlobProvider blobProvider;
+
+    @Before
+    public void before() {
+        blobProvider = (BoxBlobProvider) blobManager.getBlobProvider(PREFIX);
+        assertNotNull(blobProvider);
+    }
 
     @Test
     public void testSupportsUserUpdate() throws Exception {
-        BlobProvider blobProvider = blobManager.getBlobProvider(PREFIX);
         assertTrue(blobProvider.supportsUserUpdate());
     }
 
     @Test
     public void testGetBlob() throws Exception {
         LiveConnectFileInfo fileInfo = new LiveConnectFileInfo(USERID, FILE_1_ID_JPEG);
-        Blob blob = ((BoxBlobProvider) blobManager.getBlobProvider(PREFIX)).toBlob(fileInfo);
+        Blob blob = blobProvider.toBlob(fileInfo);
         assertEquals(FILE_1_SIZE, blob.getLength());
         assertEquals(FILE_1_NAME, blob.getFilename());
+    }
+
+    @Test
+    public void testCheckChangesAndUpdateBlobWithUpdate() {
+        DocumentModel doc = new DocumentModelImpl("parent", "file-1", "File");
+        doc.setPropertyValue("content", createBlob(FILE_1_ID_JPEG, ""));
+        List<DocumentModel> docs = blobProvider.checkChangesAndUpdateBlob(Collections.singletonList(doc));
+        assertFalse(docs.isEmpty());
+
+        doc = new DocumentModelImpl("parent", "file-1", "File");
+        doc.setPropertyValue("content", createBlob(FILE_1_ID_JPEG));
+        docs = blobProvider.checkChangesAndUpdateBlob(Collections.singletonList(doc));
+        assertFalse(docs.isEmpty());
+
+    }
+
+    @Test
+    public void testCheckChangesAndUpdateBlobWithoutUpdate() {
+        DocumentModel doc = new DocumentModelImpl("parent", "file-1", "File");
+        List<DocumentModel> docs = blobProvider.checkChangesAndUpdateBlob(Collections.singletonList(doc));
+        assertTrue(docs.isEmpty());
+
+        doc = new DocumentModelImpl("parent", "file-1", "File");
+        doc.setPropertyValue("content", createBlob(FILE_1_ID_JPEG, "134b65991ed521fcfe4724b7d814ab8ded5185dc"));
+        docs = blobProvider.checkChangesAndUpdateBlob(Collections.singletonList(doc));
+        assertTrue(docs.isEmpty());
+    }
+
+    protected SimpleManagedBlob createBlob(String fileId) {
+        return createBlob(fileId, UUID.randomUUID().toString());
+    }
+
+    protected SimpleManagedBlob createBlob(String fileId, String digest) {
+        BlobInfo blobInfo = new BlobInfo();
+        blobInfo.key = PREFIX + ':' + USERID + ':' + fileId;
+        blobInfo.digest = digest;
+        return new SimpleManagedBlob(blobInfo);
     }
 
 }
