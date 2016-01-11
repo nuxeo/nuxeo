1e41965ce12f8ccfdd7289de3c3373beac970afd
Julien Carsique <jcarsique@nuxeo.com>
2016-01-11 16:49:37 +0100

    NXP-18748: license update

--- scripts/release_mp.py
+++ scripts/release_mp.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python
 """
-(C) Copyright 2013 Nuxeo SA (http://nuxeo.com/) and contributors.
+(C) Copyright 2013-2016 Nuxeo SA (http://nuxeo.com/) and others.
 
-All rights reserved. This program and the accompanying materials
-are made available under the terms of the GNU Lesser General Public License
-(LGPL) version 2.1 which accompanies this distribution, and is available at
-http://www.gnu.org/licenses/lgpl-2.1.html
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
 
-This library is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-Lesser General Public License for more details.
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
 
 Contributors:
     Julien Carsique
@@ -107,14 +109,14 @@ class ReleaseMP(object):
                 else:
                     log("Prepare release of %s..." % marketplace)
                 mp_release = Release(mp_repo,
-                        self.mp_config.get(marketplace, "branch"),
-                        self.mp_config.get(marketplace, "tag"),
-                        self.mp_config.get(marketplace, "next_snapshot"),
-                        self.mp_config.get(marketplace, "maintenance_version"),
-                        is_final=True, skipTests=False, skipITs=False,
-                        other_versions=self.mp_config.get(
-                                                marketplace, "other_versions",
-                                                None))
+                                     self.mp_config.get(marketplace, "branch"),
+                                     self.mp_config.get(marketplace, "tag"),
+                                     self.mp_config.get(marketplace, "next_snapshot"),
+                                     self.mp_config.get(marketplace, "maintenance_version"),
+                                     is_final=True,
+                                     skipTests=False,
+                                     skipITs=False,
+                                     other_versions=self.mp_config.get(marketplace, "other_versions", None))
                 mp_release.log_summary()
                 mp_release.prepare(dryrun=dryrun, upgrade_only=upgrade_only,
                                    dodeploy=True)
