886ccfd83e55a8294681d5c7da019b43cfb58d1d
Arnaud Kervern <akervern@nuxeo.com>
2016-01-11 15:19:18 +0100

    NXP-18747: Add User Profile Json Enricher

---
+++ nuxeo-features/nuxeo-user-center/nuxeo-user-profile/src/main/java/org/nuxeo/ecm/user/center/profile/rest/UserProfileEnricher.java
@@ -0,0 +1,83 @@
+/*
+ * (C) Copyright 2016 Nuxeo SA (http://nuxeo.com/) and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Contributors:
+ *     Nuxeo
+ */
+
+package org.nuxeo.ecm.user.center.profile.rest;
+
+import static org.nuxeo.ecm.core.io.registry.reflect.Instantiations.SINGLETON;
+import static org.nuxeo.ecm.core.io.registry.reflect.Priorities.REFERENCE;
+import static org.nuxeo.ecm.user.center.profile.UserProfileConstants.USER_PROFILE_AVATAR_FIELD;
+import static org.nuxeo.ecm.user.center.profile.UserProfileConstants.USER_PROFILE_BIRTHDATE_FIELD;
+import static org.nuxeo.ecm.user.center.profile.UserProfileConstants.USER_PROFILE_PHONENUMBER_FIELD;
+
+import java.io.IOException;
+
+import org.codehaus.jackson.JsonGenerator;
+import org.nuxeo.ecm.core.api.Blob;
+import org.nuxeo.ecm.core.api.DocumentModel;
+import org.nuxeo.ecm.core.api.NuxeoPrincipal;
+import org.nuxeo.ecm.core.io.download.DownloadService;
+import org.nuxeo.ecm.core.io.marshallers.json.enrichers.AbstractJsonEnricher;
+import org.nuxeo.ecm.core.io.registry.context.RenderingContext;
+import org.nuxeo.ecm.core.io.registry.reflect.Setup;
+import org.nuxeo.ecm.user.center.profile.UserProfileService;
+import org.nuxeo.runtime.api.Framework;
+
+/**
+ * @author <a href="mailto:ak@nuxeo.com">Arnaud Kervern</a>
+ * @since 8.1
+ */
+@Setup(mode = SINGLETON, priority = REFERENCE)
+public class UserProfileEnricher extends AbstractJsonEnricher<NuxeoPrincipal> {
+
+    public static final String NAME = "userprofile";
+
+    public UserProfileEnricher() {
+        super(NAME);
+    }
+
+    @Override
+    public void write(JsonGenerator jg, NuxeoPrincipal nuxeoPrincipal) throws IOException {
+
+        UserProfileService ups = Framework.getLocalService(UserProfileService.class);
+        try (RenderingContext.SessionWrapper sw = ctx.getSession(null)) {
+            DocumentModel up = ups.getUserProfileDocument(nuxeoPrincipal.getName(), sw.getSession());
+
+            jg.writeFieldName(NAME);
+            if (up == null) {
+                jg.writeNull();
+            } else {
+                jg.writeStartObject();
+
+                jg.writeStringField("birthdate", (String) up.getPropertyValue(USER_PROFILE_BIRTHDATE_FIELD));
+                jg.writeStringField("phonenumber", (String) up.getPropertyValue(USER_PROFILE_PHONENUMBER_FIELD));
+
+                Blob avatar = (Blob) up.getPropertyValue(USER_PROFILE_AVATAR_FIELD);
+                if (avatar != null) {
+                    DownloadService downloadService = Framework.getService(DownloadService.class);
+                    String url = downloadService.getDownloadUrl(up, USER_PROFILE_AVATAR_FIELD, avatar.getFilename());
+                    jg.writeStringField("avatar", ctx.getBaseUrl() + url);
+                } else {
+                    jg.writeNullField("avatar");
+                }
+
+                jg.writeEndObject();
+            }
+        }
+    }
+}
---
+++ nuxeo-features/nuxeo-user-center/nuxeo-user-profile/src/test/java/org/nuxeo/ecm/user/center/profile/rest/UserProfileEnricherTest.java
@@ -0,0 +1,78 @@
+/*
+ * (C) Copyright 2016 Nuxeo SA (http://nuxeo.com/) and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Contributors:
+ *     Nuxeo
+ */
+
+package org.nuxeo.ecm.user.center.profile.rest;
+
+import static org.nuxeo.ecm.user.center.profile.UserProfileConstants.USER_PROFILE_PHONENUMBER_FIELD;
+import static org.nuxeo.ecm.user.center.profile.rest.UserProfileEnricher.NAME;
+
+import java.io.IOException;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.nuxeo.ecm.core.api.CoreSession;
+import org.nuxeo.ecm.core.api.DocumentModel;
+import org.nuxeo.ecm.core.api.NuxeoPrincipal;
+import org.nuxeo.ecm.core.io.marshallers.json.AbstractJsonWriterTest;
+import org.nuxeo.ecm.core.io.marshallers.json.JsonAssert;
+import org.nuxeo.ecm.core.io.registry.context.RenderingContext;
+import org.nuxeo.ecm.core.io.registry.context.RenderingContext.CtxBuilder;
+import org.nuxeo.ecm.platform.test.PlatformFeature;
+import org.nuxeo.ecm.platform.usermanager.io.NuxeoPrincipalJsonWriter;
+import org.nuxeo.ecm.user.center.profile.UserProfileService;
+import org.nuxeo.runtime.api.Framework;
+import org.nuxeo.runtime.test.runner.Deploy;
+import org.nuxeo.runtime.test.runner.Features;
+
+import com.google.inject.Inject;
+
+/**
+ * @author <a href="mailto:ak@nuxeo.com">Arnaud Kervern</a>
+ * @since 8.1
+ */
+@Features(PlatformFeature.class)
+@Deploy({ "org.nuxeo.ecm.platform.userworkspace.types", "org.nuxeo.ecm.platform.userworkspace.api",
+        "org.nuxeo.ecm.platform.userworkspace.core", "org.nuxeo.ecm.user.center.profile" })
+public class UserProfileEnricherTest extends AbstractJsonWriterTest.External<NuxeoPrincipalJsonWriter, NuxeoPrincipal> {
+    public UserProfileEnricherTest() {
+        super(NuxeoPrincipalJsonWriter.class, NuxeoPrincipal.class);
+    }
+
+    @Inject
+    CoreSession session;
+
+    @Before
+    public void setUp() {
+        UserProfileService ups = Framework.getLocalService(UserProfileService.class);
+        DocumentModel up = ups.getUserProfileDocument(session);
+        up.setPropertyValue(USER_PROFILE_PHONENUMBER_FIELD, "mynumber");
+        session.saveDocument(up);
+        session.save();
+    }
+
+    @Test
+    public void testPrincipal() throws IOException {
+        RenderingContext ctx = CtxBuilder.session(session).enrich("user", NAME).get();
+        JsonAssert jsonAssert = jsonAssert((NuxeoPrincipal) session.getPrincipal(), ctx);
+        jsonAssert = jsonAssert.get(String.format("contextParameters.%s", NAME));
+        jsonAssert.has(String.format("avatar", NAME));
+        jsonAssert.has(String.format("birthdate", NAME));
+        jsonAssert.get(String.format("phonenumber", NAME)).isEquals("mynumber");
+    }
+}
