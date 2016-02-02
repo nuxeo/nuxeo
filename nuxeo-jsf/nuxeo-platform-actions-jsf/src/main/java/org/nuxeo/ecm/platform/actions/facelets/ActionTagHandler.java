beb5befcdb0bfe55f057ac9383f83ce8c6e66a68
Anahide Tchertchian <at@nuxeo.com>
2016-02-02 14:11:18 +0100

    action tag refinements

--- nuxeo-jsf/nuxeo-platform-actions-jsf/src/main/java/org/nuxeo/ecm/platform/actions/facelets/ActionTagHandler.java
+++ nuxeo-jsf/nuxeo-platform-actions-jsf/src/main/java/org/nuxeo/ecm/platform/actions/facelets/ActionTagHandler.java
@@ -27,8 +27,6 @@ import java.util.List;
 import java.util.Map;
 
 import javax.el.ELException;
-import javax.el.ExpressionFactory;
-import javax.el.ValueExpression;
 import javax.el.VariableMapper;
 import javax.faces.FacesException;
 import javax.faces.component.UIComponent;
@@ -54,6 +52,7 @@ import org.nuxeo.ecm.platform.forms.layout.service.WebLayoutManager;
 import org.nuxeo.ecm.platform.ui.web.binding.BlockingVariableMapper;
 import org.nuxeo.ecm.platform.ui.web.tag.handler.FormTagHandler;
 import org.nuxeo.ecm.platform.ui.web.tag.handler.TagConfigFactory;
+import org.nuxeo.ecm.platform.ui.web.util.ComponentTagUtils;
 import org.nuxeo.ecm.platform.web.common.debug.DebugTracer;
 import org.nuxeo.runtime.api.Framework;
 
@@ -69,6 +68,8 @@ public class ActionTagHandler extends MetaTagHandler {
     protected final TagConfig config;
 
     protected final TagAttribute action;
+
+    protected final TagAttribute value;
 
     protected final TagAttribute mode;
 
@@ -87,6 +88,7 @@ public class ActionTagHandler extends MetaTagHandler {
         this.config = config;
 
         action = getRequiredAttribute("action");
+        value = getRequiredAttribute("value");
         mode = getAttribute("mode");
         addForm = getAttribute("addForm");
         useAjaxForm = getAttribute("useAjaxForm");
@@ -115,14 +117,6 @@ public class ActionTagHandler extends MetaTagHandler {
                 BlockingVariableMapper vm = new BlockingVariableMapper(orig);
                 ctx.setVariableMapper(vm);
 
-                ExpressionFactory eFactory = ctx.getExpressionFactory();
-                FaceletHandlerHelper helper = new FaceletHandlerHelper(config);
-
-                // expose action variable
-                ValueExpression actionVe = eFactory.createValueExpression(actionInstance, Action.class);
-                vm.setVariable("action", actionVe);
-                vm.addBlockedPattern("action");
-
                 // build corresponding widget and adjust properties
                 String wtype = actionInstance.getType();
                 if (StringUtils.isBlank(wtype)) {
@@ -141,14 +135,41 @@ public class ActionTagHandler extends MetaTagHandler {
                 Map<String, Serializable> props = new HashMap<>();
                 // put all action properties
                 props.putAll(actionInstance.getProperties());
+                if ("template".equals(wtype)) {
+                    // avoid erasing template value from widget type configuration, and match template
+                    String templateName = "template";
+                    String modeTemplateName = "template" + "_" + modeValue;
+                    if (BuiltinWidgetModes.VIEW.equals(modeValue) && props.containsKey(templateName)) {
+                        props.put("action_template", props.get(templateName));
+                    } else if (props.containsKey(modeTemplateName)) {
+                        props.put("action_template", props.get(modeTemplateName));
+                        props.remove(modeTemplateName);
+                    }
+                    props.remove(templateName);
+                }
                 // add up more
-                props.put("action", actionInstance);
                 props.put("immediate", actionInstance.isImmediate());
                 props.put("icon", actionInstance.getIcon());
-                // TODO add confirm message args stuff
+                // handle onclick
+                StringBuilder fullOnclick = new StringBuilder();
+                if (props.containsKey("confirmMessage")) {
+                    String confirmMessage = (String) props.get("confirmMessage");
+                    if (!StringUtils.isEmpty(confirmMessage)) {
+                        fullOnclick.append(
+                                "var message = \"#{nxu:translate(widgetProperty_confirmMessage, widgetProperty_confirmMessageArgs)}\";if (message != \"\" && !confirm(message)) {return false;};");
+                    }
+                }
+                String confirm = actionInstance.getConfirm();
+                if (!StringUtils.isEmpty(confirm)) {
+                    fullOnclick.append(confirm).append(";");
+                }
+                String onclick = (String) actionInstance.getProperties().get("onclick");
+                if (!StringUtils.isEmpty(onclick)) {
+                    fullOnclick.append(onclick).append(";");
+                }
                 props.put("onclick", actionInstance.getConfirm());
                 props.put("accessKey", actionInstance.getAccessKey());
-                // TODO handle re-render
+                props.put("available", actionInstance.getAvailable());
 
                 // add all extra props passed to the tag
                 String widgetPropertyMarker = RenderVariables.widgetVariables.widgetProperty.name() + "_";
@@ -168,10 +189,14 @@ public class ActionTagHandler extends MetaTagHandler {
                 wDef.setTypeCategory(wcat);
                 wDef.setDynamic(true);
                 WebLayoutManager layoutService = Framework.getService(WebLayoutManager.class);
-                Widget widgetInstance = layoutService.createWidget(ctx, wDef, modeValue,
-                        RenderVariables.widgetVariables.fieldOrValue.name(), null);
+                String valueName = value.getValue();
+                if (ComponentTagUtils.isStrictValueReference(valueName)) {
+                    valueName = ComponentTagUtils.getBareValueName(valueName);
+                }
+                Widget widgetInstance = layoutService.createWidget(ctx, wDef, modeValue, valueName, null);
 
                 // set unique id on widget before exposing it to the context
+                FaceletHandlerHelper helper = new FaceletHandlerHelper(config);
                 WidgetTagHandler.generateWidgetId(ctx, helper, widgetInstance, false);
 
                 // expose widget variables
@@ -188,8 +213,8 @@ public class ActionTagHandler extends MetaTagHandler {
                 wattrs = FaceletHandlerHelper.addTagAttribute(wattrs,
                         helper.createAttribute(RenderVariables.widgetVariables.widget.name(),
                                 "#{" + RenderVariables.widgetVariables.widget.name() + "}"));
-                wattrs = FaceletHandlerHelper.addTagAttribute(wattrs, helper.createAttribute("value",
-                        "#{" + RenderVariables.widgetVariables.fieldOrValue.name() + "}"));
+                wattrs = FaceletHandlerHelper.addTagAttribute(wattrs,
+                        helper.createAttribute("value", value.getValue()));
                 TagConfig wconfig = TagConfigFactory.createTagConfig(config, config.getTagId(), wattrs, nextHandler);
                 FaceletHandler handler = new WidgetTagHandler(wconfig);
 
