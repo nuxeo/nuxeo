72f106d3f030b8f6ae80dc7f80f2f11c0e37714e
Florent Guillaume <fguillaume@nuxeo.com>
2016-02-02 15:40:56 +0100

    NXP-18942: proper tag escaping

--- nuxeo-features/nuxeo-platform-tag-service/nuxeo-platform-tag-web/src/main/resources/web/nuxeo.war/scripts/tags2Formatter.js
+++ nuxeo-features/nuxeo-platform-tag-service/nuxeo-platform-tag-web/src/main/resources/web/nuxeo.war/scripts/tags2Formatter.js
@@ -1,3 +1,25 @@
+var nuxeo = nuxeo || {};
+
+nuxeo.utils = (function(m) {
+
+    var entityMap = {
+        '&': '&amp;',
+        '<': '&lt;',
+        '>': '&gt;',
+        '"': '&quot;',
+        "'": '&#39;',
+        '/': '&#x2F;'
+    };
+
+    m.escapeHTML = function escapeHTML(string) {
+        return String(string).replace(/[&<>"'\/]/g, function fromEntityMap(s) { return entityMap[s]; });
+    };
+
+    return m
+
+}(nuxeo.utils || {}));
+
+
 function createNewTag(term, data) {
     return {
         id : term,
@@ -15,15 +37,17 @@ function removeTagHandler(tag) {
 }
 
 function formatSuggestedTags(tag) {
+    var escapeHTML = nuxeo.utils.escapeHTML;
     if (tag.newTag) {
-        return "<span class='s2newTag'>" + tag.displayLabel + "</span>"
+        return "<span class='s2newTag'>" + escapeHTML(tag.displayLabel) + "</span>"
     } else {
-        return "<span class='s2existingTag'>" + tag.displayLabel + "</span>"
+        return "<span class='s2existingTag'>" + escapeHTML(tag.displayLabel) + "</span>"
     }
 }
 
 function formatSelectedTags(tag) {
-    var jsFragment = "listDocumentsForTag('" + tag.displayLabel + "');";
+    var escapeHTML = nuxeo.utils.escapeHTML;
+    var jsFragment = "listDocumentsForTag('" + escapeHTML(tag.displayLabel) + "');";
     return '<span class="s2newTag"><a href="' + window.nxContextPath + '/search/tag_search_results.faces?conversationId=' + currentConversationId + '" onclick="' + jsFragment + '">'
-            + tag.displayLabel + '</a></span>'
+            + escapeHTML(tag.displayLabel) + '</a></span>'
 }
