2781a582089d95f54cf20fb67d6a2e8b600401c7
Antoine Taillefer <ataillefer@nuxeo.com>
2016-02-03 14:32:20 +0100

    NXP-18936: Fix randomly failing test because of bad CLASS granularity

--- nuxeo-drive-operations/src/test/java/org/nuxeo/drive/hierarchy/permission/TestPermissionHierarchyFileSystemChanges.java
+++ nuxeo-drive-operations/src/test/java/org/nuxeo/drive/hierarchy/permission/TestPermissionHierarchyFileSystemChanges.java
@@ -84,7 +84,7 @@ import org.nuxeo.runtime.transaction.TransactionHelper;
  */
 @RunWith(FeaturesRunner.class)
 @Features(AuditFeature.class)
-@RepositoryConfig(cleanup = Granularity.CLASS)
+@RepositoryConfig(cleanup = Granularity.METHOD)
 // We handle transaction start and commit manually to make it possible to have
 // several consecutive transactions in a test method
 @Deploy({ "org.nuxeo.ecm.platform.userworkspace.types", "org.nuxeo.ecm.platform.userworkspace.api",
@@ -165,8 +165,12 @@ public class TestPermissionHierarchyFileSystemChanges {
         TransactionHelper.startTransaction();
 
         // Close core sessions
-        session1.close();
-        session2.close();
+        if (session1 != null) {
+            session1.close();
+        }
+        if (session2 != null) {
+            session2.close();
+        }
 
         // Delete test users
         deleteUser("user1");
