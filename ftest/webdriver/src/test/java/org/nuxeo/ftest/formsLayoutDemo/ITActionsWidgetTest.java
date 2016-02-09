7307156af74dd3a618411da190643b8db6510444
Anahide Tchertchian <at@nuxeo.com>
2016-02-09 08:41:57 +0100

    NXP-17681: adapt webdriver tests to fancybox form id changes

--- ftest/webdriver/src/test/java/org/nuxeo/ftest/formsLayoutDemo/ITActionsWidgetTest.java
+++ ftest/webdriver/src/test/java/org/nuxeo/ftest/formsLayoutDemo/ITActionsWidgetTest.java
@@ -41,63 +41,49 @@ public class ITActionsWidgetTest extends AbstractWidgetPageTest {
         Locator.waitForTextNotPresent(driver.findElement(By.xpath("//html")), VALUE_REQUIRED);
 
         String idPrefix = "actionsWidgetLayout_view_form:nxl_actionsWidgetLayout_1:";
-        assertNotNull(driver.findElement(By.xpath("//a[@id='" + idPrefix + "nxw_actionsWidget_1_layoutDemoLink']/span")));
-        assertNotNull(driver.findElement(By.xpath("//div[@id='" + idPrefix + "nxw_actionsWidget_1_panel']/div/a/span")));
-        assertNotNull(driver.findElement(By.xpath("//a[@id='" + idPrefix
-                + "nxw_actionsWidget_1_layoutDemoFancyBox_subview:nxw_actionsWidget_1_layoutDemoFancyBox_link']/span")));
-        assertEquals(
-                "",
-                driver.findElement(
-                        By.id("actionsWidgetLayout_view_form:nxl_actionsWidgetLayout_1:nxw_actionTextWidget_1")).getText());
+        String linkXpath = "//a[@id='" + idPrefix + "nxw_actionsWidget_1_layoutDemoLink']/span";
+        String actionsXpath = "//div[@id='" + idPrefix + "nxw_actionsWidget_1_panel']/div/a/span";
+
+        String fancyXPath = "//a[@id='" + idPrefix + "nxw_actionsWidget_1_layoutDemoFancyBox_link']/span";
+        String widgetId = "actionsWidgetLayout_view_form:nxl_actionsWidgetLayout_1:nxw_actionTextWidget_1";
+        assertNotNull(driver.findElement(By.xpath(linkXpath)));
+        assertNotNull(driver.findElement(By.xpath(actionsXpath)));
+        assertNotNull(driver.findElement(By.xpath(fancyXPath)));
+        assertEquals("", driver.findElement(By.id(widgetId)).getText());
         doSubmitDemo();
 
-        assertEquals(
-                VALUE_REQUIRED,
+        assertEquals(VALUE_REQUIRED,
                 driver.findElement(
-                        By.id("actionsWidgetLayout_edit_form:nxl_actionsWidgetLayout:nxw_actionTextWidget_message")).getText());
-        driver.findElement(By.id("actionsWidgetLayout_edit_form:nxl_actionsWidgetLayout:nxw_actionTextWidget")).sendKeys(
-                "test");
+                        By.id("actionsWidgetLayout_edit_form:nxl_actionsWidgetLayout:nxw_actionTextWidget_message"))
+                      .getText());
+        driver.findElement(By.id("actionsWidgetLayout_edit_form:nxl_actionsWidgetLayout:nxw_actionTextWidget"))
+              .sendKeys("test");
         doSubmitDemo();
 
         Locator.waitForTextNotPresent(driver.findElement(By.xpath("//html")), VALUE_REQUIRED);
-        assertNotNull(driver.findElement(By.xpath("//a[@id='" + idPrefix + "nxw_actionsWidget_1_layoutDemoLink']/span")));
-        assertNotNull(driver.findElement(By.xpath("//div[@id='" + idPrefix + "nxw_actionsWidget_1_panel']/div/a/span")));
-        assertNotNull(driver.findElement(By.xpath("//a[@id='" + idPrefix
-                + "nxw_actionsWidget_1_layoutDemoFancyBox_subview:nxw_actionsWidget_1_layoutDemoFancyBox_link']/span")));
-        assertEquals(
-                "test",
-                driver.findElement(
-                        By.id("actionsWidgetLayout_view_form:nxl_actionsWidgetLayout_1:nxw_actionTextWidget_1")).getText());
+        assertNotNull(driver.findElement(By.xpath(linkXpath)));
+        assertNotNull(driver.findElement(By.xpath(actionsXpath)));
+        assertNotNull(driver.findElement(By.xpath(fancyXPath)));
+        assertEquals("test", driver.findElement(By.id(widgetId)).getText());
 
         get(HomePage.URL + pageId, Page.class);
-        assertNotNull(driver.findElement(By.xpath("//a[@id='" + idPrefix + "nxw_actionsWidget_1_layoutDemoLink']/span")));
-        assertNotNull(driver.findElement(By.xpath("//div[@id='" + idPrefix + "nxw_actionsWidget_1_panel']/div/a/span")));
-        assertNotNull(driver.findElement(By.xpath("//a[@id='" + idPrefix
-                + "nxw_actionsWidget_1_layoutDemoFancyBox_subview:nxw_actionsWidget_1_layoutDemoFancyBox_link']/span")));
-        driver.findElement(
-                By.xpath("//a[@id='"
-                        + idPrefix
-                        + "nxw_actionsWidget_1_layoutDemoFancyBox_subview:nxw_actionsWidget_1_layoutDemoFancyBox_link']/span")).click();
-        Locator.waitUntilElementPresent(By.cssSelector("#nxw_actionsWidget_1_layoutDemoFancyBox_box > div > h3"));
+        assertNotNull(driver.findElement(By.xpath(linkXpath)));
+        assertNotNull(driver.findElement(By.xpath(actionsXpath)));
+        assertNotNull(driver.findElement(By.xpath(fancyXPath)));
+        driver.findElement(By.xpath(fancyXPath)).click();
+        Locator.waitUntilElementPresent(By.cssSelector("#nxw_actionsWidget_1_layoutDemoFancyBox_after_view_box > div > h3"));
         assertEquals("Fancy box sample",
-                driver.findElement(By.cssSelector("#nxw_actionsWidget_1_layoutDemoFancyBox_box > div > h3")).getText());
+                driver.findElement(By.cssSelector("#nxw_actionsWidget_1_layoutDemoFancyBox_after_view_box > div > h3")).getText());
         assertNotNull(driver.findElement(By.id("fancybox-close")));
         driver.findElement(By.id("fancybox-close")).click();
         Locator.waitForTextNotPresent(driver.findElement(By.xpath("//html")), "Fancy box sample");
-        assertEquals(
-                "test",
-                driver.findElement(
-                        By.id("actionsWidgetLayout_view_form:nxl_actionsWidgetLayout_1:nxw_actionTextWidget_1")).getText());
+        assertEquals("test", driver.findElement(By.id(widgetId)).getText());
 
         navigateTo(pageId);
-        assertNotNull(driver.findElement(By.xpath("//a[@id='" + idPrefix + "nxw_actionsWidget_1_layoutDemoLink']/span")));
-        assertNotNull(driver.findElement(By.xpath("//div[@id='" + idPrefix + "nxw_actionsWidget_1_panel']/div/a/span")));
-        assertNotNull(driver.findElement(By.xpath("//a[@id='" + idPrefix
-                + "nxw_actionsWidget_1_layoutDemoFancyBox_subview:nxw_actionsWidget_1_layoutDemoFancyBox_link']/span")));
-        assertEquals(
-                "",
-                driver.findElement(
-                        By.id("actionsWidgetLayout_view_form:nxl_actionsWidgetLayout_1:nxw_actionTextWidget_1")).getText());
+        assertNotNull(driver.findElement(By.xpath(linkXpath)));
+        assertNotNull(driver.findElement(By.xpath(actionsXpath)));
+        assertNotNull(driver.findElement(By.xpath(fancyXPath)));
+        assertEquals("", driver.findElement(By.id(widgetId)).getText());
     }
 
 }
