f52b986863040f9f16e8918524e80ba54f310631
Anahide Tchertchian <at@nuxeo.com>
2016-02-16 14:33:03 +0100

    NXP-8549: fix ITSearchTabTest, expecting created user to be the author of one of the documents (and not especially the user workspace)

--- nuxeo-distribution/nuxeo-distribution-cap-webdriver-tests/src/test/java/org/nuxeo/ftest/cap/ITSearchTabTest.java
+++ nuxeo-distribution/nuxeo-distribution-cap-webdriver-tests/src/test/java/org/nuxeo/ftest/cap/ITSearchTabTest.java
@@ -85,7 +85,7 @@ public class ITSearchTabTest extends AbstractTest {
         RestHelper.createDocument(WORKSPACES_PATH, WORKSPACE_TYPE, WORKSPACE2_TITLE, null);
         RestHelper.addPermission(wsId, TEST_USERNAME, "Everything");
 
-        login();
+        loginAsTestUser();
         open(String.format(NXDOC_URL_FORMAT, wsId));
 
         FileDocumentBasePage filePage = createFile(asPage(DocumentBasePage.class), "Test file for ITSearchTabTest",
@@ -108,9 +108,9 @@ public class ITSearchTabTest extends AbstractTest {
 
     @After
     public void after() throws UserNotConnectedException {
-        RestHelper.cleanup();
+        RestHelper.cleanupUsers();
 
-        // test aggregate on deleted user
+        // test aggregate on deleted user, on user workspace
         DocumentBasePage documentBasePage = login();
         SearchPage searchPage = documentBasePage.goToSearchPage();
         DefaultSearchSubPage searchLayoutSubPage = searchPage.getDefaultSearch();
@@ -123,6 +123,8 @@ public class ITSearchTabTest extends AbstractTest {
             }
         }
         assertTrue(testUserFound);
+
+        RestHelper.cleanupDocuments();
     }
 
     @Test
