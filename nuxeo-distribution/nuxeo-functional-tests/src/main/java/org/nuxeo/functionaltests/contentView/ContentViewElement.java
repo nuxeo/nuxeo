184772a3f5317c07d6adcf9f742e6f3bce93bf8f
Anahide Tchertchian <at@nuxeo.com>
2016-02-17 09:38:31 +0100

    NXP-8549: improve ContentViewElement API from ContentTabSubPage logics, to make them reusable for other content views thatn the 'document_content' one

--- nuxeo-distribution/nuxeo-functional-tests/src/main/java/org/nuxeo/functionaltests/contentView/ContentViewElement.java
+++ nuxeo-distribution/nuxeo-functional-tests/src/main/java/org/nuxeo/functionaltests/contentView/ContentViewElement.java
@@ -18,13 +18,14 @@
  */
 package org.nuxeo.functionaltests.contentView;
 
+import java.util.List;
+
 import org.nuxeo.functionaltests.AjaxRequestManager;
-import org.nuxeo.functionaltests.Required;
 import org.nuxeo.functionaltests.fragment.WebFragmentImpl;
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
 
 /**
  * Represents a content view element.
@@ -32,6 +33,10 @@ import org.openqa.selenium.support.FindBy;
  * @since 7.1
  */
 public class ContentViewElement extends WebFragmentImpl {
+
+    private static final String SELECT_ALL_BUTTON_XPATH = "//input[@type=\"checkbox\" and @title=\"Select all / deselect all\"]";
+
+    private static final String CHECK_BOX_XPATH = "td/input[@type=\"checkbox\"]";
 
     public static enum ResultLayout {
         THUMBNAIL("Thumbnail view"), LISTING("List view");
@@ -43,22 +48,128 @@ public class ContentViewElement extends WebFragmentImpl {
         }
     }
 
-    @FindBy(className = "contentViewUpperActions")
-    @Required
-    private WebElement upperActions;
-
     public ContentViewElement(WebDriver driver, WebElement element) {
         super(driver, element);
     }
 
-    public WebElement getActionByTitle(String title) {
-        return upperActions.findElement(By.xpath("//img[@alt=\"" + title + "\"]"));
+    protected ContentViewElement reload() {
+        return getWebFragment(By.id(getId()), ContentViewElement.class);
     }
 
-    public void switchToResultLayout(ResultLayout layout) {
+    protected String getContentViewId() {
+        String id = getId();
+        if (id.endsWith("_panel")) {
+            return id.substring(0, id.length() - "_panel".length());
+        }
+        return id;
+    }
+
+    protected WebElement getResultsPanel() {
+        String id = getContentViewId() + "_resultsPanel";
+        return getElement().findElement(By.id(id));
+    }
+
+    public WebElement getActionByTitle(String title) {
+        return getElement().findElement(By.className("contentViewUpperActions"))
+                           .findElement(By.xpath("//img[@alt=\"" + title + "\"]"));
+    }
+
+    public ContentViewElement switchToResultLayout(ResultLayout layout) {
         AjaxRequestManager a = new AjaxRequestManager(driver);
         a.watchAjaxRequests();
         getActionByTitle(layout.title).click();
         a.waitForAjaxRequests();
+        return reload();
     }
+
+    /**
+     * @since 8.2
+     */
+    public List<WebElement> getItems() {
+        return getResultsPanel().findElements(By.xpath("(.//form)[1]//tbody//tr"));
+    }
+
+    /**
+     * @since 8.2
+     */
+    public void clickOnItemTitle(String title) {
+        getResultsPanel().findElement(By.linkText(title)).click();
+    }
+
+    /**
+     * @since 8.2
+     */
+    public boolean hasItem(String title) {
+        try {
+            WebElement element = getResultsPanel().findElement(By.linkText(title));
+            return element != null;
+        } catch (NoSuchElementException e) {
+            return false;
+        }
+    }
+
+    /**
+     * @since 8.2
+     */
+    public ContentViewElement checkByTitle(String... titles) {
+        List<WebElement> items = getItems();
+        for (WebElement item : items) {
+            for (String title : titles) {
+                try {
+                    item.findElement(By.linkText(title));
+                    WebElement checkBox = item.findElement(By.xpath(CHECK_BOX_XPATH));
+                    AjaxRequestManager arm = new AjaxRequestManager(driver);
+                    arm.begin();
+                    checkBox.click();
+                    arm.end();
+                    break;
+                } catch (NoSuchElementException e) {
+                    // next
+                }
+            }
+        }
+        return reload();
+    }
+
+    /**
+     * @since 8.2
+     */
+    public ContentViewElement checkByIndex(int... indexes) {
+        AjaxRequestManager a = new AjaxRequestManager(driver);
+        for (int i : indexes) {
+            a.watchAjaxRequests();
+            getItems().get(i).findElement(By.xpath(CHECK_BOX_XPATH)).click();
+            a.waitForAjaxRequests();
+        }
+        return reload();
+    }
+
+    /**
+     * @since 8.2
+     */
+    public ContentViewElement checkAllItems() {
+        WebElement selectAll = null;
+        try {
+            selectAll = getResultsPanel().findElement(By.xpath(SELECT_ALL_BUTTON_XPATH));
+        } catch (NoSuchElementException e) {
+            // no item
+        }
+        if (selectAll != null) {
+            AjaxRequestManager arm = new AjaxRequestManager(driver);
+            arm.begin();
+            selectAll.click();
+            arm.end();
+            return reload();
+        }
+        return this;
+    }
+
+    /**
+     * @since 8.2
+     */
+    public WebElement getSelectionActionByTitle(String title) {
+        return getResultsPanel().findElement(By.xpath("//div[contains(@id,'nxw_cvButton_panel')]"))
+                                .findElement(By.xpath("//input[@value=\"" + title + "\"]"));
+    }
+
 }
