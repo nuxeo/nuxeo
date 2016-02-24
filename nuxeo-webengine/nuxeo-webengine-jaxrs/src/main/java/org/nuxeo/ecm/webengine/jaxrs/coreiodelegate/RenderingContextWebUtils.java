9ebe4a287157690f004c20785103295eeb40a449
Nicolas Chapurlat <nchapurlat@nuxeo.com>
2016-02-24 10:12:15 +0100

    NXP-19017: Initialize the locale used to marshall objects in the REST API with the user's locale or the requests's locale

--- nuxeo-webengine/nuxeo-webengine-jaxrs/src/main/java/org/nuxeo/ecm/webengine/jaxrs/coreiodelegate/RenderingContextWebUtils.java
+++ nuxeo-webengine/nuxeo-webengine-jaxrs/src/main/java/org/nuxeo/ecm/webengine/jaxrs/coreiodelegate/RenderingContextWebUtils.java
@@ -20,6 +20,7 @@
 package org.nuxeo.ecm.webengine.jaxrs.coreiodelegate;
 
 import java.util.Enumeration;
+import java.util.Locale;
 import java.util.Map;
 
 import javax.servlet.ServletRequest;
@@ -29,7 +30,9 @@ import org.nuxeo.ecm.core.api.CoreSession;
 import org.nuxeo.ecm.core.io.registry.context.RenderingContext;
 import org.nuxeo.ecm.core.io.registry.context.RenderingContext.CtxBuilder;
 import org.nuxeo.ecm.core.io.registry.context.RenderingContextImpl.RenderingContextBuilder;
+import org.nuxeo.ecm.platform.web.common.locale.LocaleProvider;
 import org.nuxeo.ecm.webengine.jaxrs.session.SessionFactory;
+import org.nuxeo.runtime.api.Framework;
 
 /**
  * Utility class that get or create a {@link RenderingContext} from the current {@link HttpServletRequest}.
@@ -112,6 +115,18 @@ public final class RenderingContextWebUtils {
             // current session
             CoreSession session = SessionFactory.getSession(webRequest);
             builder.base(baseURL).session(session);
+            // get the locale, first try through the user's prefs, then through the request
+            Locale locale = null;
+            LocaleProvider localeProvider = Framework.getService(LocaleProvider.class);
+            if (localeProvider != null) {
+                locale = localeProvider.getLocale(session);
+            }
+            if (locale == null) {
+                locale = request.getLocale();
+            }
+            if (locale != null) {
+                builder.locale(locale);
+            }
             // headers
             Enumeration<String> headerNames = webRequest.getHeaderNames();
             while (headerNames.hasMoreElements()) {
