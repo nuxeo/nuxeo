3ec2fcd4c5d67517fe7ddbaacd389d4e48df7bc6
Anahide Tchertchian <at@nuxeo.com>
2016-02-26 14:32:45 +0100

    NXP-19002: fix group search buttons disablement on submit, fix ajaxified call in webdriver test and remove the "onsubmit" attribute on the form (not executed anymore with ajaxified actions)

--- nuxeo-distribution/nuxeo-functional-tests/src/main/java/org/nuxeo/functionaltests/pages/admincenter/usermanagement/GroupsTabSubPage.java
+++ nuxeo-distribution/nuxeo-functional-tests/src/main/java/org/nuxeo/functionaltests/pages/admincenter/usermanagement/GroupsTabSubPage.java
@@ -17,6 +17,7 @@
 package org.nuxeo.functionaltests.pages.admincenter.usermanagement;
 
 import org.nuxeo.functionaltests.AbstractTest;
+import org.nuxeo.functionaltests.AjaxRequestManager;
 import org.nuxeo.functionaltests.Required;
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
@@ -53,7 +54,10 @@ public class GroupsTabSubPage extends UsersGroupsBasePage {
     public GroupsTabSubPage searchGroup(String query) {
         searchInput.clear();
         searchInput.sendKeys(query);
-        searchButton.submit();
+        AjaxRequestManager arm = new AjaxRequestManager(driver);
+        arm.begin();
+        searchButton.click();
+        arm.end();
         return asPage(GroupsTabSubPage.class);
     }
 
