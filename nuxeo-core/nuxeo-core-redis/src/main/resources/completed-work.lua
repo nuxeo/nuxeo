5dad633432eec0009583308ff060da49e3ea0e81
Benoit Delbosc <bdelbosc@nuxeo.com>
2016-03-07 14:58:40 +0100

    NXP-19092: Use lua script instead of pipelined, this reduce round trip and make operation atomic, also pipelined command don't raise error creating inconsistent state

---
+++ nuxeo-core/nuxeo-core-redis/src/main/resources/completed-work.lua
@@ -0,0 +1,16 @@
+--
+-- Mark work as completed
+--
+local dataKey = KEYS[1]
+local stateKey = KEYS[2]
+local runningKey = KEYS[3]
+local completedKey = KEYS[4]
+
+local workId = ARGV[1]
+local workData = ARGV[2]
+local state = ARGV[3]
+
+redis.call('HSET', dataKey, workId, workData);
+redis.call('SREM', runningKey, workId);
+redis.call('SADD', completedKey, workId);
+redis.call('HSET', stateKey, workId, state);
---
+++ nuxeo-core/nuxeo-core-redis/src/main/resources/scheduling-work.lua
@@ -0,0 +1,16 @@
+--
+-- Schedule a work
+--
+local dataKey = KEYS[1]
+local stateKey = KEYS[2]
+local scheduledKey = KEYS[3]
+local queueKey = KEYS[4]
+
+local workId = ARGV[1]
+local workData = ARGV[2]
+local state = ARGV[3]
+
+redis.call('HSET', dataKey, workId, workData);
+redis.call('SADD', scheduledKey, workId);
+redis.call('HSET', stateKey, workId, state);
+redis.call('LPUSH', queueKey, workId);
