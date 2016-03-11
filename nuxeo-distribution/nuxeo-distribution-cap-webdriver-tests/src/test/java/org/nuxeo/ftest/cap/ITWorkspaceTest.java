f0ffe63b28eafa49265698b9738d61ae31c01175
Kevin Leturc <kleturc@nuxeo.com>
2016-03-11 16:31:17 +0100

    NXP-8549: Migrate deleteWorkspace selenium test to webdriver.

--- nuxeo-distribution/nuxeo-distribution-cap-webdriver-tests/src/test/java/org/nuxeo/ftest/cap/ITWorkspaceTest.java
+++ nuxeo-distribution/nuxeo-distribution-cap-webdriver-tests/src/test/java/org/nuxeo/ftest/cap/ITWorkspaceTest.java
@@ -18,6 +18,9 @@
  */
 package org.nuxeo.ftest.cap;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 import static org.nuxeo.functionaltests.Constants.WORKSPACES_PATH;
 
 import java.io.IOException;
@@ -30,6 +33,9 @@ import org.nuxeo.functionaltests.AbstractTest;
 import org.nuxeo.functionaltests.RestHelper;
 import org.nuxeo.functionaltests.pages.DocumentBasePage;
 import org.nuxeo.functionaltests.pages.DocumentBasePage.UserNotConnectedException;
+import org.nuxeo.functionaltests.pages.tabs.ContentTabSubPage;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.WebElement;
 
 /**
  * Test workspace structure creation.
@@ -39,6 +45,8 @@ import org.nuxeo.functionaltests.pages.DocumentBasePage.UserNotConnectedExceptio
 public class ITWorkspaceTest extends AbstractTest {
 
     protected final static String WORKSPACE_TITLE = "Test Workspace " + new Date().getTime();
+
+    protected static final String WORKSPACE_DESCRIPTION = "Workspace Description" + new Date().getTime();
 
     @Before
     public void before() {
@@ -55,7 +63,7 @@ public class ITWorkspaceTest extends AbstractTest {
     public void testWorkspaceStructure() throws UserNotConnectedException, IOException {
         try {
             DocumentBasePage documentBasePage = loginAsTestUser();
-            DocumentBasePage wsPage = documentBasePage.createWorkspace(WORKSPACE_TITLE, "my desc");
+            DocumentBasePage wsPage = documentBasePage.createWorkspace(WORKSPACE_TITLE, WORKSPACE_DESCRIPTION);
             checkAvailableTabs();
             wsPage.getContentTab().createFolder("My folder", "folder desc");
             checkAvailableTabs();
@@ -72,6 +80,37 @@ public class ITWorkspaceTest extends AbstractTest {
         DocumentBasePage page = asPage(DocumentBasePage.class);
         page.clickOnDocumentTabLink(page.permissionsTabLink, false);
         asPage(DocumentBasePage.class).getHistoryTab();
+    }
+
+    @Test
+    public void testDeleteWorkspace() throws Exception {
+        // First create workspace as Administrator
+        login().createWorkspace(WORKSPACE_TITLE, WORKSPACE_DESCRIPTION).createNote("Note to delete",
+                "Note description to delete", false, null);
+        logout();
+
+        // Delete it as Test User
+        DocumentBasePage workspacesPage = loginAsTestUser().goToWorkspaces().goToDocumentWorkspaces();
+        ContentTabSubPage contentTabPage = workspacesPage.getContentTab().selectByTitle(WORKSPACE_TITLE);
+        WebElement deleteLink = driver.findElementById("document_content_buttons:nxw_CURRENT_SELECTION_TRASH_form:nxw_CURRENT_SELECTION_TRASH");
+        assertTrue(deleteLink.isEnabled());
+        assertTrue(driver.findElementById(
+                "document_content_buttons:nxw_CURRENT_SELECTION_ADDTOLIST_form:nxw_CURRENT_SELECTION_ADDTOLIST")
+                         .isEnabled());
+        // Delete the workspace then cancel it on confirmation
+        deleteLink.click();
+        Alert alert = driver.switchTo().alert();
+        assertEquals("Delete selected document(s)?", alert.getText());
+        alert.dismiss();
+        // De-select workspace to delete as removeDocument() select it
+        contentTabPage = contentTabPage.getContentTab().selectByTitle(WORKSPACE_TITLE);
+        contentTabPage = contentTabPage.removeDocument(WORKSPACE_TITLE).asPage(ContentTabSubPage.class);
+        assertFalse(contentTabPage.getChildDocumentRows()
+                                  .stream()
+                                  .filter(element -> WORKSPACE_TITLE.equals(element.getText()))
+                                  .findAny()
+                                  .isPresent());
+
     }
 
 }
