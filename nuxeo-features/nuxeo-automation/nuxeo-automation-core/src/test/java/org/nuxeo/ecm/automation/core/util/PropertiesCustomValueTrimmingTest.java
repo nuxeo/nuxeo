3eb03a75b18bffc30b04c82fb3794c6bbd4b5661
Ronan DANIELLOU <rdaniellou@nuxeo.com>
2016-03-21 12:23:57 +0100

    NXP-19170: Add backward compatibility for behavior change on multi line handling in automation operation.
    Note that default value is the opposite than on master, for backward compatibility.
    Note that ConfigurationService being unavailable for version 6, we rely on a variable in nuxeo.conf.

---
+++ nuxeo-features/nuxeo-automation/nuxeo-automation-core/src/test/java/org/nuxeo/ecm/automation/core/util/PropertiesCustomValueTrimmingTest.java
@@ -0,0 +1,50 @@
+/*
+ * (C) Copyright 2016 Nuxeo SA (http://nuxeo.com/) and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Contributors:
+ *     Ronan DANIELLOU <rdaniellou@nuxeo.com>
+ */
+package org.nuxeo.ecm.automation.core.util;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.IOException;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.nuxeo.runtime.api.Framework;
+import org.nuxeo.runtime.test.runner.Features;
+import org.nuxeo.runtime.test.runner.FeaturesRunner;
+import org.nuxeo.runtime.test.runner.RuntimeFeature;
+
+/**
+ * Custom value.
+ *
+ * @since 8.2
+ */
+@RunWith(FeaturesRunner.class)
+@Features({ RuntimeFeature.class })
+public class PropertiesCustomValueTrimmingTest extends PropertiesTestAbstract {
+
+    @Test
+    public void customValueTrimming() throws IOException {
+        Boolean isTrimmedDefaultValue = Properties.isPropertyValueTrimmed();
+        Framework.getRuntime().setProperty(Properties.IS_PROPERTY_VALUE_TRIMMED_KEY, !isTrimmedDefaultValue);
+        Boolean isTrimmed = Properties.isPropertyValueTrimmed();
+        assertThat(isTrimmed).isNotEqualTo(isTrimmedDefaultValue);
+        loadProperties(isTrimmed);
+    }
+
+}
