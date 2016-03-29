5ba656bb4bb29f6b72a7a0aeceed77559e43e4d3
Stephane Lacoin aka nxmatic <slacoin@nuxeo.com>
2016-03-29 16:18:23 +0200

    NXP-19346 when serializing documents suspend committed tx and test existence in new tx

---
+++ nuxeo-core/nuxeo-core-redis/src/test/java/org/nuxeo/ecm/core/redis/TestRedisWorkSchedule.java
@@ -0,0 +1,92 @@
+package org.nuxeo.ecm.core.redis;
+
+import java.io.Serializable;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+import javax.inject.Inject;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.nuxeo.ecm.core.api.CoreSession;
+import org.nuxeo.ecm.core.api.DocumentModel;
+import org.nuxeo.ecm.core.event.EventBundle;
+import org.nuxeo.ecm.core.event.EventProducer;
+import org.nuxeo.ecm.core.event.PostCommitEventListener;
+import org.nuxeo.ecm.core.event.impl.DocumentEventContext;
+import org.nuxeo.ecm.core.test.CoreFeature;
+import org.nuxeo.runtime.api.Framework;
+import org.nuxeo.runtime.test.runner.Features;
+import org.nuxeo.runtime.test.runner.FeaturesRunner;
+import org.nuxeo.runtime.test.runner.LocalDeploy;
+import org.nuxeo.runtime.transaction.TransactionHelper;
+
+@RunWith(FeaturesRunner.class)
+@Features({ RedisFeature.class })
+public class TestRedisWorkSchedule {
+
+    @Inject
+    CoreSession session;
+
+    @Inject
+    CoreFeature core;
+
+    static TestRedisWorkSchedule self;
+
+    @Before
+    public void injectSelf() {
+        self = this;
+    }
+
+    @After
+    public void outjectSelf() {
+        self = null;
+    }
+
+    final CountDownLatch observed = new CountDownLatch(1);
+
+    public static class PfouhListener implements PostCommitEventListener {
+
+        public PfouhListener() {
+           super();
+        }
+
+        @Override
+        public void handleEvent(EventBundle events) {
+            self.observed.countDown();
+        }
+
+    }
+
+    static class DocWrapper implements Serializable {
+        /**
+         *
+         */
+        private static final long serialVersionUID = 1L;
+        final DocumentModel doc;
+
+        public DocWrapper(DocumentModel source) {
+            doc = source;
+        }
+
+    }
+
+    @Test
+    @LocalDeploy("org.nuxeo.ecm.core.event:test-redis-pfouh-listener.xml")
+    public void canSerializeEventBundles() throws InterruptedException {
+        Assert.assertTrue(TransactionHelper.isTransactionActive());
+        DocumentModel doc = session.createDocumentModel("/", "pfouh", "Document");
+        doc = session.createDocument(doc);
+        DocumentEventContext context = new DocumentEventContext(session, session.getPrincipal(), doc);
+        context.newEvent("pfouh");
+        context.getProperties().put("pfouh",new DocWrapper(doc));
+        Framework.getService(EventProducer.class)
+                .fireEvent(context.newEvent("pfouh"));
+        TransactionHelper.commitOrRollbackTransaction();
+        TransactionHelper.startTransaction();
+        Assert.assertTrue(observed.await(10, TimeUnit.SECONDS));
+    }
+}
