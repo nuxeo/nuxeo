67a2774935e66ae787804eaf44a259fe005c7e9d
Pierre-Gildas MILLON <pgmillon@nuxeo.com>
2016-04-04 10:31:07 +0200

    NXP-19392: Append LibreOffice 5 jPipe paths

--- nuxeo-services/nuxeo-platform-convert/src/main/java/org/nuxeo/ecm/platform/convert/ooomanager/ConfigBuilderHelper.java
+++ nuxeo-services/nuxeo-platform-convert/src/main/java/org/nuxeo/ecm/platform/convert/ooomanager/ConfigBuilderHelper.java
@@ -1,5 +1,5 @@
 /*
- * (C) Copyright 2006-2015 Nuxeo SA (http://nuxeo.com/) and contributors.
+ * (C) Copyright 2010-2016 Nuxeo SA (http://nuxeo.com/) and others.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the GNU Lesser General Public License
@@ -32,9 +32,12 @@ public class ConfigBuilderHelper {
 
     private static final String JPIPE_LIB_PATH_PROPERTY_KEY = "jod.jpipe.lib.path";
 
-    private static final String[] UNIX_JPIPE_PATHS = { "/usr/lib/ure/lib" };
+    private static final String[] UNIX_JPIPE_PATHS = { "/usr/lib/ure/lib", "/usr/lib/libreoffice/program" };
 
-    private static final String[] MAC_JPIPE_PATHS = { "/Applications/OpenOffice.org.app/Contents/basis-link/ure-link/lib" };
+    private static final String[] MAC_JPIPE_PATHS = {
+            "/Applications/OpenOffice.org.app/Contents/basis-link/ure-link/lib",
+            "/Applications/LibreOffice.app/Contents/Frameworks"
+    };
 
     private ConfigBuilderHelper() {
     }
