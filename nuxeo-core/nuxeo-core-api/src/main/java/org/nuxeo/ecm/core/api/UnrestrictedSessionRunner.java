b894459486e0b586c164c057baa02a0f113ae39f
Benoit Delbosc <bdelbosc@nuxeo.com>
2016-04-06 13:01:58 +0200

    NXP-19422: Test mode should not require extra session.save

--- nuxeo-core/nuxeo-core-api/src/main/java/org/nuxeo/ecm/core/api/UnrestrictedSessionRunner.java
+++ nuxeo-core/nuxeo-core-api/src/main/java/org/nuxeo/ecm/core/api/UnrestrictedSessionRunner.java
@@ -171,9 +171,6 @@ public abstract class UnrestrictedSessionRunner {
             }
         } finally {
             isUnrestricted = false;
-            if (Framework.isTestModeSet() && sessionIsAlreadyUnrestricted) {
-                session.save();
-            }
         }
     }
 
