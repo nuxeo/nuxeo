b109c75a696f3c728112dae8eb2c3d45d5f31da5
nuxeojenkins <jenkins@nuxeo.com>
2016-04-12 19:08:12 +0200

    NXP-19436: fix hot-reload with Redis WorkManager (again) (#60)

--- nuxeo-core/nuxeo-core-redis/src/main/java/org/nuxeo/ecm/core/redis/contribs/RedisWorkQueuing.java
+++ nuxeo-core/nuxeo-core-redis/src/main/java/org/nuxeo/ecm/core/redis/contribs/RedisWorkQueuing.java
@@ -173,6 +173,11 @@ public class RedisWorkQueuing implements WorkQueuing {
     }
 
     @Override
+    public void shutdownWorkQueue(String queueId) {
+        allQueued.remove(queueId);
+    }
+
+    @Override
     public boolean workSchedule(String queueId, Work work) {
         return getWorkQueue(queueId).offer(new WorkHolder(work));
     }
@@ -359,18 +364,6 @@ public class RedisWorkQueuing implements WorkQueuing {
     @Override
     public State getWorkState(String workId) {
         return getWorkStateInfo(workId);
-    }
-
-    @Override
-    public int setSuspending(String queueId) {
-        try {
-            int n = suspendScheduledWork(queueId);
-            log.info("Suspending " + n + " work instances from queue: " + queueId);
-            allQueued.remove(queueId);
-            return n;
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
     }
 
     @Override
