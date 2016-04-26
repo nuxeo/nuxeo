33f6e03f3dbdf4047da68811b1c244f675460411
Vladimir Pasquier <vpasquier@nuxeo.com>
2016-04-26 14:01:59 -0400

    cleanup

--- nuxeo-features/nuxeo-automation/nuxeo-automation-client/src/main/java/org/nuxeo/ecm/automation/client/jaxrs/spi/DefaultOperationRequest.java
+++ nuxeo-features/nuxeo-automation/nuxeo-automation-client/src/main/java/org/nuxeo/ecm/automation/client/jaxrs/spi/DefaultOperationRequest.java
@@ -158,7 +158,7 @@ public class DefaultOperationRequest implements OperationRequest {
     }
 
     public OperationRequest setContextProperty(String key, Object value) {
-        ctx.put(key, value != null ? value : null);
+        ctx.put(key, value);
         return this;
     }
 
