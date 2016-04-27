92500d23fca1ccea632e42dcaa9a339a05c300a7
Yannis JULIENNE <yjulienne@nuxeo.com>
2016-04-27 18:21:05 +0200

    NXP-8549: Migrate ITs on permissions from selenium to webdriver

--- nuxeo-distribution/nuxeo-distribution-cap-webdriver-tests/src/test/java/org/nuxeo/ftest/cap/ITPermissionsTest.java
+++ nuxeo-distribution/nuxeo-distribution-cap-webdriver-tests/src/test/java/org/nuxeo/ftest/cap/ITPermissionsTest.java
@@ -28,14 +28,12 @@ import org.nuxeo.functionaltests.RestHelper;
 import org.nuxeo.functionaltests.pages.DocumentBasePage;
 import org.nuxeo.functionaltests.pages.tabs.ContentTabSubPage;
 import org.nuxeo.functionaltests.pages.tabs.PermissionsSubPage;
+import org.nuxeo.functionaltests.pages.tabs.SectionContentTabSubPage;
 import org.openqa.selenium.NoSuchElementException;
 
-import static org.nuxeo.ftest.cap.TestConstants.TEST_WORKSPACE_TITLE;
-import static org.nuxeo.ftest.cap.TestConstants.TEST_WORKSPACE_URL;
+import static org.nuxeo.ftest.cap.TestConstants.*;
 
-import static org.nuxeo.functionaltests.Constants.WORKSPACES_PATH;
-import static org.nuxeo.functionaltests.Constants.WORKSPACES_URL;
-import static org.nuxeo.functionaltests.Constants.WORKSPACE_TYPE;
+import static org.nuxeo.functionaltests.Constants.*;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
@@ -50,8 +48,13 @@ public class ITPermissionsTest extends AbstractTest {
         RestHelper.createUser(TEST_USERNAME, TEST_PASSWORD, null, null, null, null, "members");
         RestHelper.createUser("bree", "bree1", null, null, null, null, "members");
         RestHelper.createUser("linnet", "linnet1", null, null, null, null, "members");
+        RestHelper.createUser("susan", "susan1", null, null, null, null, "members");
+        RestHelper.createGroup("housewives", "Housewives", new String[] { "linnet", "susan" }, null);
         RestHelper.createDocument(WORKSPACES_PATH, WORKSPACE_TYPE, TEST_WORKSPACE_TITLE, null);
         RestHelper.addPermission(WORKSPACES_PATH, TEST_USERNAME, "Everything");
+        RestHelper.createDocument(SECTIONS_PATH, SECTION_TYPE, TEST_SECTION_TITLE, null);
+        RestHelper.addPermission(SECTIONS_PATH, TEST_USERNAME, "Everything");
+        RestHelper.createDocument(TEST_WORKSPACE_PATH, FILE_TYPE, TEST_FILE_TITLE, null);
     }
 
     @After
@@ -125,105 +128,261 @@ public class ITPermissionsTest extends AbstractTest {
     }
 
     @Test
-    public void testWorkspacePermissionsByAdmin() throws DocumentBasePage.UserNotConnectedException {
-        login("bree", "bree1");
-        open(TEST_WORKSPACE_URL);
-
-        // check bree has only read permission
-        DocumentBasePage page = asPage(DocumentBasePage.class);
-        assertFalse(hasNewButton(page));
-        assertFalse(hasEditTab(page));
-        assertFalse(hasNewPermissionsButton(page));
-        assertFalse(hasManageTab(page));
-
-        login();
-        open(TEST_WORKSPACE_URL);
-
-        page = asPage(DocumentBasePage.class);
-        PermissionsSubPage permissionsTab = page.getPermissionsTab();
-
-        // grant manage everything to bree
-        PermissionsSubPage permissionsSubPage = permissionsTab.grantPermission("Manage everything", "bree");
-        assertTrue(permissionsSubPage.hasPermission("Manage everything", "bree"));
-
-        login("bree", "bree1");
-        open(TEST_WORKSPACE_URL);
-
-        // check result
-        page = asPage(DocumentBasePage.class);
-        assertTrue(hasNewButton(page));
-        assertTrue(hasEditTab(page));
-        assertTrue(hasNewPermissionsButton(page));
-        assertTrue(hasManageTab(page));
-
-        login();
-        open(TEST_WORKSPACE_URL);
-
-        page = asPage(DocumentBasePage.class);
-        permissionsTab = page.getPermissionsTab();
-
-        // revoke manage everything to bree
-        permissionsSubPage = permissionsTab.deletePermission("Manage everything", "bree");
-        assertFalse(permissionsSubPage.hasPermission("Manage everything", "bree"));
-
-        login("bree", "bree1");
-        open(TEST_WORKSPACE_URL);
-
-        // check result
-        page = asPage(DocumentBasePage.class);
-        assertFalse(hasNewButton(page));
-        assertFalse(hasEditTab(page));
-        assertFalse(hasNewPermissionsButton(page));
-        assertFalse(hasManageTab(page));
+    public void testWorkspacePermissions() throws DocumentBasePage.UserNotConnectedException {
+        testPermissionsOn(TEST_WORKSPACE_URL, false, false);
     }
 
-    @Test
-    public void testWorkspacePermissionsByManager() throws DocumentBasePage.UserNotConnectedException {
-        login("bree", "bree1");
-        open(TEST_WORKSPACE_URL);
-
-        DocumentBasePage page = asPage(DocumentBasePage.class);
-        assertFalse(hasEditTab(page));
-
-        login(TEST_USERNAME, TEST_PASSWORD);
-        open(TEST_WORKSPACE_URL);
-
-        page = asPage(DocumentBasePage.class);
-        PermissionsSubPage permissionsTab = page.getPermissionsTab();
-        PermissionsSubPage permissionsSubPage = permissionsTab.grantPermission("Edit", "bree");
-        assertTrue(permissionsSubPage.hasPermission("Edit", "bree"));
-
-        login("bree", "bree1");
-        open(TEST_WORKSPACE_URL);
-
-        page = asPage(DocumentBasePage.class);
-        assertTrue(hasEditTab(page));
-
-        login(TEST_USERNAME, TEST_PASSWORD);
-        open(TEST_WORKSPACE_URL);
-
-        page = asPage(DocumentBasePage.class);
-        permissionsTab = page.getPermissionsTab();
-        permissionsTab.deletePermission("Edit", "bree");
-
-        login("bree", "bree1");
-        open(TEST_WORKSPACE_URL);
-
-        page = asPage(DocumentBasePage.class);
-        assertFalse(hasEditTab(page));
-    }
 
     @Test
     public void testSectionPermissions() throws DocumentBasePage.UserNotConnectedException {
-
+        testPermissionsOn(TEST_SECTION_URL, true, false);
     }
 
     @Test
     public void testDocumentPermissions() throws DocumentBasePage.UserNotConnectedException {
-
+        testPermissionsOn(TEST_FILE_URL, false, true);
     }
 
-    private boolean hasNewButton(DocumentBasePage page) {
+    private void testPermissionsOn(String docUrl, boolean isSection, boolean isFile) throws DocumentBasePage.UserNotConnectedException {
+        // check bree has only read permission
+        login("bree", "bree1");
+        open(docUrl);
+
+        DocumentBasePage page = asPage(DocumentBasePage.class);
+        if(isFile){
+            assertFalse(hasFilesTab(page));
+            assertFalse(hasNewRelationLink(page));
+        }
+        else{
+            assertFalse(hasNewButton(page, isSection));
+        }
+        assertFalse(hasEditTab(page));
+        assertFalse(hasNewPermissionsButton(page));
+        assertFalse(hasManageTab(page));
+
+        // check linnet has only read permission
+        login("linnet", "linnet1");
+        open(docUrl);
+
+        page = asPage(DocumentBasePage.class);
+        if(isFile){
+            assertFalse(hasFilesTab(page));
+            assertFalse(hasNewRelationLink(page));
+        }
+        else{
+            assertFalse(hasNewButton(page, isSection));
+        }
+        assertFalse(hasEditTab(page));
+        assertFalse(hasNewPermissionsButton(page));
+        assertFalse(hasManageTab(page));
+
+        // grant manage everything to bree and housewives group as admin
+        login();
+        open(docUrl);
+
+        page = asPage(DocumentBasePage.class);
+        PermissionsSubPage permissionsTab = page.getPermissionsTab();
+        PermissionsSubPage permissionsSubPage = permissionsTab.grantPermission("Manage everything", "bree");
+        permissionsSubPage = permissionsTab.grantPermission("Manage everything", "housewives");
+        assertTrue(permissionsSubPage.hasPermission("Manage everything", "bree"));
+        assertTrue(permissionsSubPage.hasPermission("Manage everything", "housewives"));
+
+        // check result for bree
+        login("bree", "bree1");
+        open(docUrl);
+
+        page = asPage(DocumentBasePage.class);
+        if(isFile){
+            assertTrue(hasFilesTab(page));
+            assertTrue(hasNewRelationLink(page));
+        }
+        else{
+            assertTrue(hasNewButton(page, isSection));
+        }
+        assertTrue(hasEditTab(page));
+        assertTrue(hasNewPermissionsButton(page));
+        assertTrue(hasManageTab(page));
+
+        // check result for linnet
+        login("linnet", "linnet1");
+        open(docUrl);
+
+        page = asPage(DocumentBasePage.class);
+        if(isFile){
+            assertTrue(hasFilesTab(page));
+            assertTrue(hasNewRelationLink(page));
+        }
+        else{
+            assertTrue(hasNewButton(page, isSection));
+        }
+        assertTrue(hasEditTab(page));
+        assertTrue(hasNewPermissionsButton(page));
+        assertTrue(hasManageTab(page));
+
+        // revoke manage everything to bree and housewives as admin
+        login();
+        open(docUrl);
+
+        page = asPage(DocumentBasePage.class);
+        permissionsTab = page.getPermissionsTab();
+
+        permissionsSubPage = permissionsTab.deletePermission("Manage everything", "bree");
+        permissionsSubPage = permissionsTab.deletePermission("Manage everything", "housewives");
+        assertFalse(permissionsSubPage.hasPermission("Manage everything", "bree"));
+        assertFalse(permissionsSubPage.hasPermission("Manage everything", "housewives"));
+
+        // check result for bree
+        login("bree", "bree1");
+        open(docUrl);
+
+        page = asPage(DocumentBasePage.class);
+        if(isFile){
+            assertFalse(hasFilesTab(page));
+            assertFalse(hasNewRelationLink(page));
+        }
+        else{
+            assertFalse(hasNewButton(page, isSection));
+        }
+        assertFalse(hasEditTab(page));
+        assertFalse(hasNewPermissionsButton(page));
+        assertFalse(hasManageTab(page));
+
+        // check result for linnet
+        login("linnet", "linnet1");
+        open(docUrl);
+
+        page = asPage(DocumentBasePage.class);
+        if(isFile){
+            assertFalse(hasFilesTab(page));
+            assertFalse(hasNewRelationLink(page));
+        }
+        else{
+            assertFalse(hasNewButton(page, isSection));
+        }
+        assertFalse(hasEditTab(page));
+        assertFalse(hasNewPermissionsButton(page));
+        assertFalse(hasManageTab(page));
+
+        // check susan has only read premission
+        login("susan", "susan1");
+        open(docUrl);
+
+        page = asPage(DocumentBasePage.class);
+        if(isFile){
+            assertFalse(hasFilesTab(page));
+            assertFalse(hasNewRelationLink(page));
+        }
+        else{
+            assertFalse(hasNewButton(page, isSection));
+        }
+        assertFalse(hasEditTab(page));
+        assertFalse(hasNewPermissionsButton(page));
+        assertFalse(hasManageTab(page));
+
+        // grant edit to bree and housewives as manager
+        login(TEST_USERNAME, TEST_PASSWORD);
+        open(docUrl);
+
+        page = asPage(DocumentBasePage.class);
+        permissionsTab = page.getPermissionsTab();
+        permissionsSubPage = permissionsTab.grantPermission("Edit", "bree");
+        permissionsSubPage = permissionsTab.grantPermission("Edit", "housewives");
+        assertTrue(permissionsSubPage.hasPermission("Edit", "bree"));
+        assertTrue(permissionsSubPage.hasPermission("Edit", "housewives"));
+
+        // check result for bree
+        login("bree", "bree1");
+        open(docUrl);
+
+        page = asPage(DocumentBasePage.class);
+        if(isFile){
+            assertTrue(hasFilesTab(page));
+            assertTrue(hasNewRelationLink(page));
+        }
+        else{
+            assertTrue(hasNewButton(page, isSection));
+        }
+        assertTrue(hasEditTab(page));
+        assertFalse(hasNewPermissionsButton(page));
+        assertFalse(hasManageTab(page));
+
+        // check result for susan
+        login("susan", "susan1");
+        open(docUrl);
+
+        page = asPage(DocumentBasePage.class);
+        if(isFile){
+            assertTrue(hasFilesTab(page));
+            assertTrue(hasNewRelationLink(page));
+        }
+        else{
+            assertTrue(hasNewButton(page, isSection));
+        }
+        assertTrue(hasEditTab(page));
+        assertFalse(hasNewPermissionsButton(page));
+        assertFalse(hasManageTab(page));
+
+        // revoke edit to bree and housewives as manager
+        login(TEST_USERNAME, TEST_PASSWORD);
+        open(docUrl);
+
+        page = asPage(DocumentBasePage.class);
+        permissionsTab = page.getPermissionsTab();
+
+        permissionsSubPage = permissionsTab.deletePermission("Edit", "bree");
+        permissionsSubPage = permissionsTab.deletePermission("Edit", "housewives");
+        assertFalse(permissionsSubPage.hasPermission("Edit", "bree"));
+        assertFalse(permissionsSubPage.hasPermission("Edit", "housewives"));
+
+        // check result for bree
+        login("bree", "bree1");
+        open(docUrl);
+
+        page = asPage(DocumentBasePage.class);
+        if(isFile){
+            assertFalse(hasFilesTab(page));
+            assertFalse(hasNewRelationLink(page));
+        }
+        else{
+            assertFalse(hasNewButton(page, isSection));
+        }
+        assertFalse(hasEditTab(page));
+        assertFalse(hasNewPermissionsButton(page));
+        assertFalse(hasManageTab(page));
+
+        // check result for susan
+        login("susan", "susan1");
+        open(docUrl);
+
+        page = asPage(DocumentBasePage.class);
+        if(isFile){
+            assertFalse(hasFilesTab(page));
+            assertFalse(hasNewRelationLink(page));
+        }
+        else{
+            assertFalse(hasNewButton(page, isSection));
+        }
+        assertFalse(hasEditTab(page));
+        assertFalse(hasNewPermissionsButton(page));
+        assertFalse(hasManageTab(page));
+    }
+
+    private boolean hasNewRelationLink(DocumentBasePage page) {
+        return page.getRelationTab().hasNewRelationLink();
+    }
+
+    private boolean hasFilesTab(DocumentBasePage page) {
+        try {
+            return page.getFilesTab() != null;
+        } catch (NoSuchElementException e) {
+            return false;
+        }
+    }
+
+    private boolean hasNewButton(DocumentBasePage page, boolean isSection) {
+        if(isSection){
+            return page.getContentTab(SectionContentTabSubPage.class).hasNewButton();
+        }
         return page.getContentTab().hasNewButton();
     }
 
