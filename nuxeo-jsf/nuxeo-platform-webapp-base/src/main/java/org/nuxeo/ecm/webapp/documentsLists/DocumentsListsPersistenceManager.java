d1f62b2814f2eae466f388405267051c112c319b
Damien Metzler <dmetzler@nuxeo.com>
2016-05-31 17:03:17 +0200

    NXP-19682 return no doc in case of a DocumentSecurityException

--- nuxeo-jsf/nuxeo-platform-webapp-base/src/main/java/org/nuxeo/ecm/webapp/documentsLists/DocumentsListsPersistenceManager.java
+++ nuxeo-jsf/nuxeo-platform-webapp-base/src/main/java/org/nuxeo/ecm/webapp/documentsLists/DocumentsListsPersistenceManager.java
@@ -37,6 +37,7 @@ import org.nuxeo.ecm.core.api.DocumentModel;
 import org.nuxeo.ecm.core.api.DocumentModelList;
 import org.nuxeo.ecm.core.api.DocumentNotFoundException;
 import org.nuxeo.ecm.core.api.DocumentRef;
+import org.nuxeo.ecm.core.api.DocumentSecurityException;
 import org.nuxeo.ecm.core.api.IdRef;
 import org.nuxeo.ecm.core.api.PathRef;
 import org.nuxeo.ecm.directory.DirectoryException;
@@ -169,7 +170,7 @@ public class DocumentsListsPersistenceManager {
         DocumentModel doc = null;
         try {
             doc = session.getDocument(docRef);
-        } catch (DocumentNotFoundException e) {
+        } catch (DocumentSecurityException | DocumentNotFoundException e) {
             log.warn("document with ref " + ref + " was not found : " + e.getMessage());
             return null;
         }
