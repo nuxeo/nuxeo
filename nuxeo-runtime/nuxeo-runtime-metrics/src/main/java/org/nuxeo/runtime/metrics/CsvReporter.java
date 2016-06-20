482eee48e6afa5bd057a9b556114ac51aaf7807d
Benoit Delbosc <bdelbosc@nuxeo.com>
2016-06-20 16:13:55 +0200

    NXP-19764: Add license header

--- nuxeo-runtime/nuxeo-runtime-metrics/src/main/java/org/nuxeo/runtime/metrics/CsvReporter.java
+++ nuxeo-runtime/nuxeo-runtime-metrics/src/main/java/org/nuxeo/runtime/metrics/CsvReporter.java
@@ -1,5 +1,20 @@
-/**
- * This is a patch of Codahale CsvReporter, only the sanitizer method is overrided to enable
+/*
+ * (C) Copyright 2013 Coda Hale <coda.hale@gmail.com> and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ *
+ * This is a patched version of Coda Hale CsvReporter, the sanitizer method is overrided to enable
  * metric name with a '/' in the name.
  *
  * @since 8.3
