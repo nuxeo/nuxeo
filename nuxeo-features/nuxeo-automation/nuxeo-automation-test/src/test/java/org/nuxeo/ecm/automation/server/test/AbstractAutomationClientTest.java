07e9435ce203cc4b93a01c0b94357896d25d1611
Remi Cattiau <remi@cattiau.com>
2016-06-21 15:57:58 +0200

    NXP-19844: Use \u instead of UTF-8 caracters

--- nuxeo-features/nuxeo-automation/nuxeo-automation-test/src/test/java/org/nuxeo/ecm/automation/server/test/AbstractAutomationClientTest.java
+++ nuxeo-features/nuxeo-automation/nuxeo-automation-test/src/test/java/org/nuxeo/ecm/automation/server/test/AbstractAutomationClientTest.java
@@ -97,7 +97,7 @@ public abstract class AbstractAutomationClientTest {
     }
 
     protected File newFile(String content) throws IOException {
-        File file = File.createTempFile("automation-test-éåò-", ".xml");
+        File file = File.createTempFile("automation-test-\u00e9\u00e1\u00f2-", ".xml");
         FileUtils.writeFile(file, content);
         return file;
     }
