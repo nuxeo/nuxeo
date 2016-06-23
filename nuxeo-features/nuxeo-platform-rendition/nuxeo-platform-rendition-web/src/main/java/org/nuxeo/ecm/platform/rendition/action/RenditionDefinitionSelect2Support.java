2136db3109b88d937f3c260b3728ba4be1fbb0c4
Florent Guillaume <fguillaume@nuxeo.com>
2016-06-23 16:29:12 +0200

    NXP-19872: new stored renditions admin UI

---
+++ nuxeo-features/nuxeo-platform-rendition/nuxeo-platform-rendition-web/src/main/java/org/nuxeo/ecm/platform/rendition/action/RenditionDefinitionSelect2Support.java
@@ -0,0 +1,102 @@
+/*
+ * (C) Copyright 2016 Nuxeo SA (http://nuxeo.com/) and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Contributors:
+ */
+package org.nuxeo.ecm.platform.rendition.action;
+
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
+import org.jboss.seam.annotations.Name;
+import org.jboss.seam.annotations.Scope;
+import org.nuxeo.ecm.platform.rendition.automation.SuggestRenditionDefinitionEntry;
+import org.nuxeo.ecm.platform.ui.select2.common.Select2Common;
+
+import java.io.Serializable;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import static org.jboss.seam.ScopeType.EVENT;
+
+/**
+ * Helper component for rendition name widget relying on select2.
+ *
+ * @since 8.3
+ */
+@Name("renditionDefinitionSelect2Support")
+@Scope(EVENT)
+public class RenditionDefinitionSelect2Support {
+
+    protected String label;
+
+    public String resolveRenditionDefinitions(List<String> list) {
+        return Select2Common.resolveDefaultEntries(list);
+    }
+
+    public String resolveRenditionDefinitions(String[] array) {
+        if (array == null || array.length == 0) {
+            return Select2Common.resolveDefaultEntries(null);
+        }
+        return Select2Common.resolveDefaultEntries(Arrays.asList(array));
+    }
+
+    protected void reset() {
+        label = null;
+    }
+
+    public String encodeParameters(Map<String, Serializable> widgetProperties) {
+        return encodeCommonParameters(widgetProperties).toString();
+    }
+
+    protected JSONObject encodeCommonParameters(Map<String, Serializable> widgetProperties) {
+        return encodeCommonParameters(widgetProperties, null);
+    }
+
+    protected JSONObject encodeCommonParameters(Map<String, Serializable> widgetProperties,
+            Map<String, String> additionalParameters) {
+        JSONObject obj = new JSONObject();
+        obj.put("multiple", "true");
+        obj.put(Select2Common.MIN_CHARS, "1");
+        obj.put(Select2Common.READ_ONLY_PARAM, "false");
+        obj.put(Select2Common.OPERATION_ID, SuggestRenditionDefinitionEntry.ID);
+        obj.put(Select2Common.WIDTH, "300px");
+        obj.put(Select2Common.SELECTION_FORMATTER, "formatSelectedRenditionDefinitions");
+        obj.put(Select2Common.SUGGESTION_FORMATTER, "formatSuggestedRenditionDefinitions");
+        JSONArray tokenSeparator = new JSONArray();
+        tokenSeparator.add(",");
+        tokenSeparator.add(" ");
+        obj.put("tokenSeparators", tokenSeparator);
+        if (additionalParameters != null) {
+            for (Entry<String, String> entry : additionalParameters.entrySet()) {
+                obj.put(entry.getKey(), entry.getValue().toString());
+            }
+        }
+        for (Entry<String, Serializable> entry : widgetProperties.entrySet()) {
+            obj.put(entry.getKey(), entry.getValue().toString());
+        }
+        return obj;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
+
+}
---
+++ nuxeo-features/nuxeo-platform-rendition/nuxeo-platform-rendition-web/src/main/java/org/nuxeo/ecm/platform/rendition/automation/SuggestRenditionDefinitionEntry.java
@@ -0,0 +1,82 @@
+/*
+ * (C) Copyright 2016 Nuxeo SA (http://nuxeo.com/) and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Contributors:
+ */
+package org.nuxeo.ecm.platform.rendition.automation;
+
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
+import org.apache.commons.lang.StringUtils;
+import org.nuxeo.ecm.automation.core.Constants;
+import org.nuxeo.ecm.automation.core.annotations.Operation;
+import org.nuxeo.ecm.automation.core.annotations.OperationMethod;
+import org.nuxeo.ecm.automation.core.annotations.Param;
+import org.nuxeo.ecm.core.api.Blob;
+import org.nuxeo.ecm.core.api.Blobs;
+import org.nuxeo.ecm.platform.rendition.service.RenditionDefinition;
+import org.nuxeo.ecm.platform.rendition.service.RenditionService;
+import org.nuxeo.ecm.platform.ui.select2.common.Select2Common;
+import org.nuxeo.runtime.api.Framework;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * @since 8.3
+ */
+@Operation(id = SuggestRenditionDefinitionEntry.ID, category = Constants.CAT_SERVICES, label = "Get rendition definition suggestion", description = "Get rendition definition suggestion")
+public class SuggestRenditionDefinitionEntry {
+
+    public static final String ID = "RenditionDefinition.Suggestion";
+
+    public static final int MAX_SUGGESTIONS = 10;
+
+    public static final Comparator<RenditionDefinition> LABEL_COMPARATOR = new RenditionDefinitionComparator();
+
+    @Param(name = "searchTerm", required = false)
+    protected String searchTerm;
+
+    @OperationMethod
+    public Blob run() {
+        JSONArray result = new JSONArray();
+        if (!StringUtils.isBlank(searchTerm)) {
+            for (RenditionDefinition def : getSuggestions(searchTerm)) {
+                JSONObject obj = new JSONObject();
+                String name = def.getName();
+                obj.element(Select2Common.ID, name);
+                obj.element(Select2Common.LABEL, name);
+                result.add(obj);
+            }
+        }
+        return Blobs.createBlob(result.toString(), "application/json");
+    }
+
+    protected List<RenditionDefinition> getSuggestions(String searchTerm) {
+        RenditionService renditionService = Framework.getService(RenditionService.class);
+        List<RenditionDefinition> defs = renditionService.getDeclaredRenditionDefinitions();
+        return defs.stream().filter(def -> def.getName().startsWith(searchTerm)).sorted(LABEL_COMPARATOR).limit(
+                MAX_SUGGESTIONS).collect(Collectors.toList());
+    }
+
+    protected static class RenditionDefinitionComparator implements Comparator<RenditionDefinition> {
+        @Override
+        public int compare(RenditionDefinition o1, RenditionDefinition o2) {
+            return o1.getName().compareToIgnoreCase(o2.getName());
+        }
+    }
+
+}
