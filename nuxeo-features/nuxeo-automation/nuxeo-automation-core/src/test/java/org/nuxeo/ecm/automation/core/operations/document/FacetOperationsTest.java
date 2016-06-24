7a8f440c48e5068e6af861fc434405ea7492ba33
Ricardo Dias <rdias@nuxeo.com>
2016-06-24 16:30:46 +0100

    NXP-19617: Added the Document.AddItemToListProperty operation from nuxeo labs.

--- nuxeo-features/nuxeo-automation/nuxeo-automation-core/src/test/java/org/nuxeo/ecm/automation/core/operations/document/FacetOperationsTest.java
+++ nuxeo-features/nuxeo-automation/nuxeo-automation-core/src/test/java/org/nuxeo/ecm/automation/core/operations/document/FacetOperationsTest.java
@@ -1,7 +1,21 @@
-/**
+/*
+ * (C) Copyright 2016 Nuxeo SA (http://nuxeo.com/) and others.
  *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Contributors:
+ *     Ricardo Dias
  */
-
 package org.nuxeo.ecm.automation.core.operations.document;
 
 import static org.junit.Assert.*;
@@ -76,7 +90,7 @@ public class FacetOperationsTest {
     }
     
     @After
-    public void cleanup() {
+    public void clearRepo() {
         session.removeChildren(session.getRootDocument().getRef());
         session.save();
     }
