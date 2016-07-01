68bea741cf8dca7923594e27e5a7474781705ea0
Ricardo Dias <rdias@nuxeo.com>
2016-07-01 15:22:16 +0200

    NXP-19910: improve request API in HTTPHelper

--- nuxeo-features/nuxeo-automation/nuxeo-automation-features/src/main/java/org/nuxeo/ecm/automation/features/HTTPHelper.java
+++ nuxeo-features/nuxeo-automation/nuxeo-automation-features/src/main/java/org/nuxeo/ecm/automation/features/HTTPHelper.java
@@ -16,15 +16,24 @@
  * Contributors:
  *      Thibaud Arguillere <targuillere@nuxeo.com>
  *      Vladimir Pasquier <vpasquier@nuxeo.com>
+ *      Ricardo Dias <rdias@nuxeo.com>
  */
 package org.nuxeo.ecm.automation.features;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.Charset;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.MultivaluedMap;
 
+import com.sun.jersey.core.util.Base64;
+import com.sun.jersey.core.util.MultivaluedMapImpl;
+import org.apache.commons.lang.StringUtils;
 import org.codehaus.jackson.map.ObjectMapper;
 import org.nuxeo.ecm.automation.context.ContextHelper;
 import org.nuxeo.ecm.core.api.Blob;
@@ -49,12 +58,14 @@ public class HTTPHelper implements ContextHelper {
 
     private static final Integer TIMEOUT = 1000 * 60 * 5; // 5min
 
+    private static final String HTTP_CONTENT_DISPOSITION = "Content-Disposition";
+
     public Blob call(String username, String password, String requestType, String path) throws IOException {
         return call(username, password, requestType, path, null, null, null, null);
     }
 
-    public Blob call(String username, String password, String requestType, String path,
-            Map<String, String> headers) throws IOException {
+    public Blob call(String username, String password, String requestType, String path, Map<String, String> headers)
+            throws IOException {
         return call(username, password, requestType, path, null, null, null, headers);
     }
 
@@ -148,4 +159,211 @@ public class HTTPHelper implements ContextHelper {
         }
     }
 
+    /**
+     * @since 8.4
+     */
+    public Blob get(String url, Map<String, Object> options) throws IOException {
+        return invoke("GET", url, null, null, options);
+    }
+
+    /**
+     * @since 8.4
+     */
+    public Blob post(String url, Object data, Map<String, Object> options) throws IOException {
+        return invoke("POST", url, data, null, options);
+    }
+
+    /**
+     * @since 8.4
+     */
+    public Blob post(String url, MultiPart multiPart, Map<String, Object> options) throws IOException {
+        return invoke("POST", url, null, multiPart, options);
+    }
+
+    /**
+     * @since 8.4
+     */
+    public Blob put(String url, Object data, Map<String, Object> options) throws IOException {
+        return invoke("PUT", url, data, null, options);
+    }
+
+    /**
+     * @since 8.4
+     */
+    public Blob put(String url, MultiPart multiPart, Map<String, Object> options) throws IOException {
+        return invoke("PUT", url, null, multiPart, options);
+    }
+
+    /**
+     * @since 8.4
+     */
+    public Blob delete(String url, Object data, Map<String, Object> options) throws IOException {
+        return invoke("DELETE", url, data, null, options);
+    }
+
+    private Blob invoke(String requestType, String url, Object data, MultiPart multipart, Map<String, Object> options)
+            throws IOException {
+        MultivaluedMap<String, String> queryParams = getQueryParameters(options);
+        Map<String, String> headers = getHeaderParameters(options);
+
+        ClientConfig config = new DefaultClientConfig();
+        config.getClasses().add(MultiPartWriter.class);
+        Client client = Client.create(config);
+        client.setConnectTimeout(TIMEOUT);
+
+        WebResource wr = client.resource(url);
+
+        if (queryParams != null && !queryParams.isEmpty()) {
+            wr = wr.queryParams(queryParams);
+        }
+        WebResource.Builder builder;
+        builder = wr.accept(MediaType.APPLICATION_JSON);
+        if (multipart != null) {
+            builder = wr.type(MediaType.MULTIPART_FORM_DATA_TYPE);
+        }
+
+        // Adding some headers if needed
+        if (headers != null && !headers.isEmpty()) {
+            for (String headerKey : headers.keySet()) {
+                builder.header(headerKey, headers.get(headerKey));
+            }
+        }
+        ClientResponse response = null;
+        try {
+            switch (requestType) {
+            case "HEAD":
+            case "GET":
+                response = builder.get(ClientResponse.class);
+                break;
+            case "POST":
+                if (multipart != null) {
+                    response = builder.post(ClientResponse.class, multipart);
+                } else {
+                    response = builder.post(ClientResponse.class, data);
+                }
+                break;
+            case "PUT":
+                if (multipart != null) {
+                    response = builder.put(ClientResponse.class, multipart);
+                } else {
+                    response = builder.put(ClientResponse.class, data);
+                }
+                break;
+            case "DELETE":
+                response = builder.delete(ClientResponse.class, data);
+                break;
+            default:
+                break;
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        if (response != null && response.getStatus() >= 200 && response.getStatus() < 300) {
+            return setUpBlob(response, url);
+        } else {
+            return new StringBlob(response.getStatusInfo() != null ? response.getStatusInfo().toString() : "error");
+        }
+    }
+
+    private Map<String, String> getHeaderParameters(Map<String, Object> options) {
+        if (options != null) {
+            Map<String, String> headers = new HashMap<>();
+
+            Map<String, String> authorization = (Map<String, String>) options.get("auth");
+            if (authorization != null) {
+                String method = authorization.get("method");
+                switch (method) {
+                case "basic":
+                    Map<String, String> header = basicAuthentication(authorization.get("username"),
+                            authorization.get("password"));
+                    headers.putAll(header);
+                    break;
+                default:
+                    break;
+                }
+            }
+
+            Map<String, String> headersOptions = (Map<String, String>) options.get("headers");
+            if (headersOptions != null) {
+                headers.putAll(headersOptions);
+            }
+
+            return headers;
+        }
+        return null;
+    }
+
+    private MultivaluedMap<String, String> getQueryParameters(Map<String, Object> options) {
+        if (options != null) {
+            Map<String, List<String>> params = (Map<String, List<String>>) options.get("params");
+            if (params != null) {
+                MultivaluedMap<String, String> queryParams = new MultivaluedMapImpl();
+                for (String key : params.keySet()) {
+                    queryParams.put(key, params.get(key));
+                }
+                return queryParams;
+            }
+        }
+        return null;
+    }
+
+    private Blob setUpBlob(ClientResponse response, String url) throws IOException {
+        MultivaluedMap<String, String> headers = response.getHeaders();
+        String disposition = headers.getFirst(HTTP_CONTENT_DISPOSITION);
+
+        String filename = "";
+        if (disposition != null) {
+            // extracts file name from header field
+            int index = disposition.indexOf("filename=");
+            if (index > -1) {
+                filename = disposition.substring(index + 9);
+            }
+        } else {
+            // extracts file name from URL
+            filename = url.substring(url.lastIndexOf("/") + 1, url.length());
+        }
+
+        Blob resultBlob = Blobs.createBlob(response.getEntityInputStream());
+        if (!StringUtils.isEmpty(filename)) {
+            resultBlob.setFilename(filename);
+        }
+
+        String encoding = headers.getFirst(HttpHeaders.CONTENT_ENCODING);
+        if (encoding != null) {
+            resultBlob.setEncoding(encoding);
+        }
+
+        MediaType contentType = response.getType();
+        if (contentType != null) {
+            resultBlob.setMimeType(contentType.getType());
+        }
+
+        return resultBlob;
+    }
+
+    private Map<String, String> basicAuthentication(String username, String password) {
+
+        if (username == null || password == null) {
+            return null;
+        }
+
+        Map<String, String> authenticationHeader;
+        try {
+            final byte[] prefix = (username + ":").getBytes(Charset.forName("iso-8859-1"));
+            final byte[] usernamePassword = new byte[prefix.length + password.getBytes().length];
+
+            System.arraycopy(prefix, 0, usernamePassword, 0, prefix.length);
+            System.arraycopy(password.getBytes(), 0, usernamePassword, prefix.length, password.getBytes().length);
+
+            String authentication = "Basic " + new String(Base64.encode(usernamePassword), "ASCII");
+
+            authenticationHeader = new HashMap<>();
+            authenticationHeader.put(HttpHeaders.AUTHORIZATION, authentication);
+
+        } catch (UnsupportedEncodingException ex) {
+            // This should never occur
+            throw new RuntimeException(ex);
+        }
+        return authenticationHeader;
+    }
 }
---
+++ nuxeo-features/nuxeo-automation/nuxeo-automation-features/src/test/java/org/nuxeo/ecm/automation/core/test/HTTPHelperTest.java
@@ -0,0 +1,282 @@
+/*
+ * (C) Copyright 2016 Nuxeo SA (http://nuxeo.com/) and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Contributors:
+ *     Ricardo Dias
+ */
+package org.nuxeo.ecm.automation.core.test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import static org.junit.Assert.fail;
+import static org.mockserver.integration.ClientAndProxy.startClientAndProxy;
+import static org.mockserver.integration.ClientAndServer.startClientAndServer;
+import static org.mockserver.model.HttpRequest.request;
+import static org.mockserver.model.HttpResponse.response;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+import javax.inject.Inject;
+import javax.ws.rs.core.HttpHeaders;
+
+import org.apache.commons.io.IOUtils;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockserver.client.server.MockServerClient;
+import org.mockserver.integration.ClientAndProxy;
+import org.mockserver.integration.ClientAndServer;
+import org.mockserver.matchers.Times;
+import org.mockserver.model.BinaryBody;
+import org.mockserver.model.Body;
+import org.mockserver.model.Delay;
+import org.mockserver.model.Header;
+import org.mockserver.model.JsonBody;
+import org.nuxeo.common.utils.FileUtils;
+import org.nuxeo.ecm.automation.OperationContext;
+import org.nuxeo.ecm.automation.context.ContextHelper;
+import org.nuxeo.ecm.automation.context.ContextService;
+import org.nuxeo.ecm.automation.core.scripting.Scripting;
+import org.nuxeo.ecm.automation.features.HTTPHelper;
+import org.nuxeo.ecm.core.api.Blob;
+import org.nuxeo.ecm.core.api.CoreSession;
+import org.nuxeo.ecm.platform.test.PlatformFeature;
+import org.nuxeo.runtime.test.runner.Deploy;
+import org.nuxeo.runtime.test.runner.Features;
+import org.nuxeo.runtime.test.runner.FeaturesRunner;
+
+/**
+ * @since 8.4
+ */
+@RunWith(FeaturesRunner.class)
+@Features(PlatformFeature.class)
+@Deploy({ "org.nuxeo.ecm.automation.core", "org.nuxeo.ecm.automation.features" })
+public class HTTPHelperTest {
+
+    @Inject
+    CoreSession session;
+
+    @Inject
+    ContextService ctxService;
+
+    OperationContext ctx;
+
+    protected ClientAndProxy proxy;
+
+    protected ClientAndServer mockServer;
+
+    protected final static String SERVER_HOST = "localhost";
+
+    protected final static String SERVER_PATH = "/ws/path/";
+
+    protected final static int SERVER_PORT = 1080;
+
+    protected final static String SERVER_URL = "http://" + SERVER_HOST + ":" + String.valueOf(SERVER_PORT)
+            + SERVER_PATH;
+
+    protected final static String IMAGE_FILENAME = "sample.jpeg";
+
+    private final JsonBody DEFAULT_HTTP_RESPONSE = new JsonBody("{\"message\": \"Default answer to requests.\"}");
+
+    @Before
+    public void setUp() throws Exception {
+        mockServer = startClientAndServer(1080);
+        proxy = startClientAndProxy(1090);
+
+        ctx = new OperationContext(session);
+
+        // assert that the helper is available
+        Map<String, ContextHelper> contextHelperList = ctxService.getHelperFunctions();
+        Object httpHelper = contextHelperList.get("HTTP");
+        assertNotNull(httpHelper);
+        assertTrue(httpHelper instanceof HTTPHelper);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        proxy.stop();
+        mockServer.stop();
+    }
+
+    @Test
+    public void testHTTPHelperGet() {
+        createMockServer("GET", SERVER_PATH, DEFAULT_HTTP_RESPONSE);
+        try {
+            String expr = String.format(
+                    "HTTP.get(\'%s\', {'auth' : { 'method' : 'basic', 'username' : 'test', 'password' : 'test' }})",
+                    SERVER_URL);
+            Blob resultBlob = (Blob) Scripting.newExpression(expr).eval(ctx);
+            String result = IOUtils.toString(resultBlob.getStream(), "utf-8");
+            ObjectMapper mapper = new ObjectMapper();
+            Map<String, String> jsonResult = mapper.readValue(result, Map.class);
+
+            String message = jsonResult.get("message");
+            assertEquals("Default answer to requests.", message);
+        } catch (IOException exception) {
+            fail("Problem parsing the result. " + exception);
+        }
+    }
+
+    @Test
+    public void testHTTPHelperGetWithParams() {
+        createMockServer("GET", SERVER_PATH, DEFAULT_HTTP_RESPONSE);
+        try {
+            String expr = String.format(
+                    "HTTP.get(\'%s\', " + "{'auth' : { 'method' : 'basic', 'username' : 'test', 'password' : 'test' }, "
+                            + "'params' : { 'param1' : [ 'value1' ] , 'param2' : [ 'value2' ] }})",
+                    SERVER_URL);
+            Blob resultBlob = (Blob) Scripting.newExpression(expr).eval(ctx);
+            String result = IOUtils.toString(resultBlob.getStream(), "utf-8");
+            ObjectMapper mapper = new ObjectMapper();
+            Map<String, String> jsonResult = mapper.readValue(result, Map.class);
+
+            String message = jsonResult.get("message");
+            assertEquals("Default answer to requests.", message);
+        } catch (IOException exception) {
+            fail("Problem parsing the result. " + exception.getMessage());
+        }
+    }
+
+    @Test
+    public void testHTTPHelperGetWithHeaders() {
+        createMockServer("GET", SERVER_PATH, DEFAULT_HTTP_RESPONSE);
+        try {
+            String expr = String.format(
+                    "HTTP.get(\'%s\', " + "{'auth' : { 'method' : 'basic', 'username' : 'test', 'password' : 'test' }, "
+                            + "'headers' : { 'Accept' : 'application/json' , 'User-Agent' : 'Mozilla/5.0' }})",
+                    SERVER_URL);
+            Blob resultBlob = (Blob) Scripting.newExpression(expr).eval(ctx);
+            String result = IOUtils.toString(resultBlob.getStream(), "utf-8");
+            ObjectMapper mapper = new ObjectMapper();
+            Map<String, String> jsonResult = mapper.readValue(result, Map.class);
+
+            String message = jsonResult.get("message");
+            assertEquals("Default answer to requests.", message);
+        } catch (IOException exception) {
+            fail("Problem parsing the result. " + exception.getMessage());
+        }
+    }
+
+    @Test
+    public void testHTTPHelperPost() {
+        createMockServer("POST", SERVER_PATH, DEFAULT_HTTP_RESPONSE);
+        try {
+            String expr = String.format(
+                    "HTTP.post(\'%s\', 'Test', {'auth' : { 'method' : 'basic', 'username' : 'test', 'password' : 'test' }})",
+                    SERVER_URL);
+            Blob resultBlob = (Blob) Scripting.newExpression(expr).eval(ctx);
+            String result = IOUtils.toString(resultBlob.getStream(), "utf-8");
+            ObjectMapper mapper = new ObjectMapper();
+            Map<String, String> jsonResult = mapper.readValue(result, Map.class);
+
+            String message = jsonResult.get("message");
+            assertEquals("Default answer to requests.", message);
+        } catch (IOException exception) {
+            fail("Problem parsing the result. " + exception);
+        }
+    }
+
+    @Test
+    public void testHTTPHelperPut() {
+        createMockServer("PUT", SERVER_PATH, DEFAULT_HTTP_RESPONSE);
+        try {
+            String expr = String.format(
+                    "HTTP.put(\'%s\', 'Test', {'auth' : { 'method' : 'basic', 'username' : 'test', 'password' : 'test' }})",
+                    SERVER_URL);
+            Blob resultBlob = (Blob) Scripting.newExpression(expr).eval(ctx);
+            String result = IOUtils.toString(resultBlob.getStream(), "utf-8");
+            ObjectMapper mapper = new ObjectMapper();
+            Map<String, String> jsonResult = mapper.readValue(result, Map.class);
+
+            String message = jsonResult.get("message");
+            assertEquals("Default answer to requests.", message);
+        } catch (IOException exception) {
+            fail("Problem parsing the result. " + exception);
+        }
+    }
+
+    @Test
+    public void testHTTPHelperDelete() {
+        createMockServer("DELETE", SERVER_PATH, DEFAULT_HTTP_RESPONSE);
+        try {
+            String expr = String.format(
+                    "HTTP.delete(\'%s\', 'Test', {'auth' : { 'method' : 'basic', 'username' : 'test', 'password' : 'test' }})",
+                    SERVER_URL);
+            Blob resultBlob = (Blob) Scripting.newExpression(expr).eval(ctx);
+            String result = IOUtils.toString(resultBlob.getStream(), "utf-8");
+            ObjectMapper mapper = new ObjectMapper();
+            Map<String, String> jsonResult = mapper.readValue(result, Map.class);
+
+            String message = jsonResult.get("message");
+            assertEquals("Default answer to requests.", message);
+        } catch (IOException exception) {
+            fail("Problem parsing the result. " + exception);
+        }
+    }
+
+    @Test
+    public void testHTTPHelperGetDownloadFile() {
+        try {
+            File file = FileUtils.getResourceFileFromContext("test-data/sample.jpeg");
+            byte[] answer = FileUtils.readBytes(file);
+            Body responseBody = new BinaryBody(answer);
+            createMockServer("GET", SERVER_PATH + IMAGE_FILENAME, responseBody);
+        } catch (IOException e) {
+            fail("Error reading the image file." + e.getMessage());
+        }
+
+        String expr = String.format(
+                "HTTP.get(\'%s\', {'auth' : { 'method' : 'basic', 'username' : 'test', 'password' : 'test' }})",
+                SERVER_URL + IMAGE_FILENAME);
+        Blob httpResult = (Blob) Scripting.newExpression(expr).eval(ctx);
+        assertTrue(httpResult.getLength() > 0);
+        assertEquals(httpResult.getFilename(), IMAGE_FILENAME);
+    }
+
+    /**
+     * Create a mock server that answers http requests with a certain set of headers and a body answer.
+     * 
+     * @param method the HTTP request (GET, POST, PUT, DELETE)
+     * @param path the path of the requests
+     * @param answer the body of the http response
+     */
+    public void createMockServer(String method, String path, Body answer) {
+        List<Header> requestHeaders = new ArrayList<>();
+        requestHeaders.add(new Header(HttpHeaders.AUTHORIZATION, "Basic dGVzdDp0ZXN0"));
+
+        List<Header> responseHeaders = new ArrayList<>();
+        responseHeaders.add(new Header(HttpHeaders.CONTENT_TYPE, "application/json; charset=utf-8"));
+        responseHeaders.add(new Header(HttpHeaders.CACHE_CONTROL, "public, max-age=86400"));
+
+        new MockServerClient(SERVER_HOST, SERVER_PORT)
+                  .when(request().withHeaders(requestHeaders)
+                                 .withMethod(method)
+                                 .withPath(path),
+                          Times.exactly(2))
+                  .respond(response().withStatusCode(200)
+                                     .withHeaders(responseHeaders)
+                                     .withBody(answer)
+                                     .withDelay(new Delay(TimeUnit.SECONDS, 1)));
+    }
+}
