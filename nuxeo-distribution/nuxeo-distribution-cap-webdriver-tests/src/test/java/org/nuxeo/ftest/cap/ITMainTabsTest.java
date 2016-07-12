05f456468fbb08bc4b54eb07978950e709367436
Florent Guillaume <fguillaume@nuxeo.com>
2016-07-12 12:51:33 +0200

    NXP-19971: fix ITMainTabsTest by having an explicitly-created search result

--- nuxeo-distribution/nuxeo-distribution-cap-webdriver-tests/src/test/java/org/nuxeo/ftest/cap/ITMainTabsTest.java
+++ nuxeo-distribution/nuxeo-distribution-cap-webdriver-tests/src/test/java/org/nuxeo/ftest/cap/ITMainTabsTest.java
@@ -21,11 +21,17 @@ package org.nuxeo.ftest.cap;
 import org.junit.Test;
 import org.nuxeo.functionaltests.AbstractTest;
 import org.nuxeo.functionaltests.Locator;
+import org.nuxeo.functionaltests.RestHelper;
 import org.nuxeo.functionaltests.pages.DocumentBasePage;
 import org.openqa.selenium.By;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+import static org.nuxeo.functionaltests.Constants.WORKSPACES_PATH;
+import static org.nuxeo.functionaltests.Constants.WORKSPACE_TYPE;
+
+import org.junit.After;
+import org.junit.Before;
 
 /**
  * Test main tabs management.
@@ -34,8 +40,23 @@ import static org.junit.Assert.assertTrue;
  */
 public class ITMainTabsTest extends AbstractTest {
 
+    @Before
+    public void before() {
+        RestHelper.createDocument(WORKSPACES_PATH, WORKSPACE_TYPE, "ws", null);
+    }
+
+    @After
+    public void after() {
+        RestHelper.cleanup();
+    }
+
     /**
      * Non-regression test for NXP-17207
+     */
+    /*
+     * We explicitly create a workspace to find something in the search results. Just relying on the
+     * automatically-created user workspace is not enough as its creation does not come from a user action so its
+     * indexing is asynchronous and in some rare cases we won't find it in the search.
      */
     @Test
     public void testSearchTabSelection() throws Exception {
@@ -45,7 +66,7 @@ public class ITMainTabsTest extends AbstractTest {
         documentBasePage.goToSearchPage();
         assertFalse(documentBasePage.isMainTabSelected(documentBasePage.documentManagementLink));
         assertTrue(documentBasePage.isMainTabSelected(documentBasePage.searchPageLink));
-        // click on first search result, at least user workspace should be there
+        // click on first search result, at least the test workspace should be there
         Locator.findElementWaitUntilEnabledAndClick(By.cssSelector("span[class='documentTitle']"));
         documentBasePage = AbstractTest.asPage(DocumentBasePage.class);
         // check that workspace main tab is now selected
