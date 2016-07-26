c2227e10722450b9e8f37ce5630a094cbe9e82e8
Tiago Cardoso <tcardoso@nuxeo.com>
2016-07-26 09:58:07 +0100

    NXP-20143: add thumbnail factory contrib and empty factory

---
+++ nuxeo-platform-3d-core/src/main/java/org/nuxeo/ecm/platform/threed/adapter/ThumbnailThreeDFactory.java
@@ -0,0 +1,49 @@
+/*
+ * (C) Copyright 2006-2016 Nuxeo SA (http://nuxeo.com/) and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0 *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Contributors:
+ *     Tiago Cardoso <tcardoso@nuxeo.com>
+ */
+package org.nuxeo.ecm.platform.threed.adapter;
+
+import static org.nuxeo.ecm.platform.threed.ThreeDConstants.THREED_FACET;
+
+import org.nuxeo.ecm.core.api.Blob;
+import org.nuxeo.ecm.core.api.CoreSession;
+import org.nuxeo.ecm.core.api.DocumentModel;
+import org.nuxeo.ecm.core.api.NuxeoException;
+import org.nuxeo.ecm.core.api.thumbnail.ThumbnailFactory;
+
+/**
+ * 3D content thumbnail factory
+ *
+ * @since 8.4
+ */
+public class ThumbnailThreeDFactory implements ThumbnailFactory {
+
+    @Override
+    public Blob getThumbnail(DocumentModel documentModel, CoreSession coreSession) {
+        if (!documentModel.hasFacet(THREED_FACET)) {
+            throw new NuxeoException("Document is not 3D");
+        }
+        // XXX - implement thumbnail rendering
+        return null;
+    }
+
+    @Override
+    public Blob computeThumbnail(DocumentModel documentModel, CoreSession coreSession) {
+        return null;
+    }
+
+}
