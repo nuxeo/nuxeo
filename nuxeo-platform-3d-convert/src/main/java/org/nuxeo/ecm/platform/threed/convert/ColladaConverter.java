a9b567390f091a4c6874c7bc06076fbc48abf1e1
Tiago Cardoso <tcardoso@nuxeo.com>
2016-07-27 11:03:04 +0100

    NXP-20149: add Collada conversion with Blender command

---
+++ nuxeo-platform-3d-convert/src/main/java/org/nuxeo/ecm/platform/threed/convert/ColladaConverter.java
@@ -0,0 +1,81 @@
+/*
+ * (C) Copyright 2006-2016 Nuxeo SA (http://nuxeo.com/) and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Contributors:
+ *     Tiago Cardoso <tcardoso@nuxeo.com>
+ */
+package org.nuxeo.ecm.platform.threed.convert;
+
+import org.nuxeo.common.utils.FileUtils;
+import org.nuxeo.ecm.core.api.Blob;
+import org.nuxeo.ecm.core.api.blobholder.BlobHolder;
+import org.nuxeo.ecm.core.api.blobholder.SimpleBlobHolderWithProperties;
+import org.nuxeo.ecm.core.api.impl.blob.FileBlob;
+import org.nuxeo.ecm.core.commandline.executor.api.CmdParameters;
+import org.nuxeo.ecm.core.convert.api.ConversionException;
+import org.nuxeo.ecm.core.convert.extension.ConverterDescriptor;
+
+import java.io.File;
+import java.io.Serializable;
+import java.util.*;
+
+import static org.nuxeo.ecm.platform.threed.convert.Constants.LODS_PARAMETER;
+import static org.nuxeo.ecm.platform.threed.convert.Constants.OUT_DIR_PARAMETER;
+
+/**
+ * 3D conversion constants
+ *
+ * @since 8.4
+ */
+public class ColladaConverter extends BaseBlenderConverter {
+    @Override
+    public void init(ConverterDescriptor converterDescriptor) {
+
+    }
+
+    @Override
+    protected Map<String, Blob> getCmdBlobParameters(BlobHolder blobHolder, Map<String, Serializable> parameters)
+            throws ConversionException {
+        return null;
+    }
+
+    @Override
+    protected Map<String, String> getCmdStringParameters(BlobHolder blobHolder, Map<String, Serializable> parameters)
+            throws ConversionException {
+        return null;
+    }
+
+    @Override
+    protected BlobHolder buildResult(List<String> cmdOutput, CmdParameters cmdParams) throws ConversionException {
+        String outDir = cmdParams.getParameter(OUT_DIR_PARAMETER);
+        List<String> conversions = getConversions(outDir);
+        if (conversions.isEmpty() || conversions.size() != 1) {
+            throw new ConversionException("Unable to get Collada conversion result file.");
+        }
+        File collada = new File(conversions.get(0));
+        if (!"dae".equals(FileUtils.getFileExtension(conversions.get(0)))) {
+            throw new ConversionException("Wrong result file type (not Collada).");
+        }
+
+        Blob blob = new FileBlob(collada);
+        blob.setFilename(collada.getName());
+        List<Blob> blobs = new ArrayList<>();
+        blobs.add(blob);
+
+        Map<String, Serializable> properties = new HashMap<>();
+        properties.put("cmdOutput", (Serializable) cmdOutput);
+        return new SimpleBlobHolderWithProperties(blobs, properties);
+    }
+}
