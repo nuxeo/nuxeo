be7f7b97b3167895b21ed2945f251a2438b1cb1e
Tiago Cardoso <tcardoso@nuxeo.com>
2016-08-13 00:30:43 +0100

    NXP-20158: add render view beans, layout and action contributions

---
+++ nuxeo-platform-3d-jsf/src/main/java/org/nuxeo/ecm/platform/threed/renderView/RenderViewItem.java
@@ -0,0 +1,61 @@
+/*
+ * (C) Copyright 2006-2016 Nuxeo SA (http://nuxeo.com/) and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Contributors:
+ *     Tiago Cardoso <tcardoso@nuxeo.com>
+ */
+package org.nuxeo.ecm.platform.threed.renderView;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.nuxeo.ecm.core.api.Blob;
+import org.nuxeo.ecm.core.api.DocumentModel;
+import org.nuxeo.ecm.core.api.PropertyException;
+import org.nuxeo.ecm.platform.ui.web.tag.fn.DocumentModelFunctions;
+
+/**
+ * Small DTO to precompute the thumbnail URLs for JSF
+ *
+ * @since 8.4
+ */
+public class RenderViewItem {
+
+    public static final Log log = LogFactory.getLog(RenderViewItem.class);
+
+    protected final DocumentModel doc;
+
+    protected final int position;
+
+    protected final String blobPropertyName;
+
+    protected String filename;
+
+    public RenderViewItem(DocumentModel doc, String basePropertyPath, int position) {
+        this.doc = doc;
+        this.position = position;
+        String propertyPath = basePropertyPath + "/" + position;
+        blobPropertyName = propertyPath + "/content";
+        try {
+            filename = ((Blob) doc.getPropertyValue(blobPropertyName)).getFilename();
+        } catch (PropertyException e) {
+            log.warn(e);
+        }
+    }
+
+    public String getUrl() {
+        return DocumentModelFunctions.bigFileUrl(doc, blobPropertyName, filename);
+    }
+
+}
