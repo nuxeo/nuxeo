cc9b04c9aac71f375dc4b100fb74068977cde9b5
Thomas Roger <troger@nuxeo.com>
2016-09-08 10:48:55 +0200

    NXP-20397: update tests

--- nuxeo-opencmis-tests/src/test/java/org/nuxeo/ecm/core/opencmis/impl/CmisSuiteSession.java
+++ nuxeo-opencmis-tests/src/test/java/org/nuxeo/ecm/core/opencmis/impl/CmisSuiteSession.java
@@ -593,7 +593,7 @@ public class CmisSuiteSession {
         oc.setRenditionFilterString("*");
         ob = session.getObject(session.createObjectId(ob.getId()), oc);
         renditions = ob.getRenditions();
-        assertEquals(5, renditions.size());
+        assertEquals(4, renditions.size());
         Collections.sort(renditions, RENDITION_CMP);
         check(renditions.get(0), checkStream);
 
@@ -603,7 +603,7 @@ public class CmisSuiteSession {
         ItemIterable<QueryResult> results = session.query(q, true, oc);
         assertEquals(1, results.getTotalNumItems());
         renditions = results.iterator().next().getRenditions();
-        assertEquals(5, renditions.size());
+        assertEquals(4, renditions.size());
         Collections.sort(renditions, RENDITION_CMP);
         check(renditions.get(0), false);
         // no rendition stream, Chemistry deficiency (QueryResultImpl
