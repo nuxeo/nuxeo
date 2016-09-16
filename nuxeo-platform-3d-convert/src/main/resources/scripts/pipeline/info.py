74f7d6a4d60fffa0802f05c7a9df5f4e85b70ed8
Miguel Nixo <mnixo@nuxeo.com>
2016-09-16 10:28:50 +0100

    NXP-20452: fix typo

--- nuxeo-platform-3d-convert/src/main/resources/scripts/pipeline/info.py
+++ nuxeo-platform-3d-convert/src/main/resources/scripts/pipeline/info.py
@@ -23,7 +23,7 @@ for obj in bpy.context.scene.objects:
             'dimension_x': obj.dimensions.x,
             'dimension_y': obj.dimensions.y,
             'dimension_z': obj.dimensions.z,
-            'lod_succes': lod_success
+            'lod_success': lod_success
         }
         bpy.ops.mesh.select_all(action='DESELECT')
         bpy.ops.object.mode_set(mode='OBJECT')
