62c287f06752fe96ae3b6045f9d26ebbf643bfa5
Arnaud Kervern <akervern@nuxeo.com>
2016-09-19 14:48:20 -0400

    NXP-20494: Use basic Restlet to make an a target platform endpoint

--- nuxeo-target-platforms-jaxrs/src/main/java/org/nuxeo/ecm/restapi/server/jaxrs/targetplatforms/TargetPlatformsRestlet.java
+++ nuxeo-target-platforms-jaxrs/src/main/java/org/nuxeo/ecm/restapi/server/jaxrs/targetplatforms/TargetPlatformsRestlet.java
@@ -3,29 +3,28 @@ package org.nuxeo.ecm.restapi.server.jaxrs.targetplatforms;
 import java.io.ByteArrayOutputStream;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
+import javax.servlet.http.HttpServletResponse;
 
-import org.nuxeo.ecm.webengine.model.WebObject;
-import org.nuxeo.ecm.webengine.model.impl.DefaultObject;
+import org.nuxeo.ecm.platform.ui.web.restAPI.BaseNuxeoRestlet;
 import org.nuxeo.runtime.api.Framework;
 import org.nuxeo.targetplatforms.api.TargetPlatform;
 import org.nuxeo.targetplatforms.api.impl.TargetPlatformFilterImpl;
 import org.nuxeo.targetplatforms.api.service.TargetPlatformService;
 import org.nuxeo.targetplatforms.io.JSONExporter;
+import org.restlet.data.MediaType;
+import org.restlet.data.Request;
+import org.restlet.data.Response;
+import org.restlet.data.Status;
 
 import com.google.common.cache.CacheBuilder;
 import com.google.common.cache.CacheLoader;
 import com.google.common.cache.LoadingCache;
 
-@WebObject(type = "target-platforms")
-@Produces(MediaType.APPLICATION_JSON)
-public class TargetPlatformObject extends DefaultObject {
+public class TargetPlatformsRestlet extends BaseNuxeoRestlet {
     private static final String PUBLIC_TP_CACHE_KEY = "PUBLIC_TP";
 
     private static final LoadingCache<String, String> PUBLIC_CACHE = CacheBuilder //
@@ -40,15 +39,28 @@ public class TargetPlatformObject extends DefaultObject {
                 }
             });
 
-    @GET
-    public Object doGet() {
-        return Response.status(Response.Status.NOT_FOUND).build();
+    @Override
+    public void handle(Request request, Response response) {
+        HttpServletResponse res = getHttpResponse(response);
+        if (res == null || res.isCommitted()) {
+            return;
+        }
+
+        try {
+            String targetPlatforms = getTargetPlatforms();
+            response.setEntity(targetPlatforms, MediaType.APPLICATION_JSON);
+        } catch (ExecutionException e) {
+            response.setStatus(Status.CLIENT_ERROR_BAD_REQUEST);
+        }
     }
 
-    @GET
-    @Path("public")
-    public Object doGetPublic() throws Exception {
-        String platforms = PUBLIC_CACHE.get(PUBLIC_TP_CACHE_KEY, () -> {
+    private String getTargetPlatforms() throws ExecutionException {
+        return PUBLIC_CACHE.get(PUBLIC_TP_CACHE_KEY, new TargetPlatformCallable());
+    }
+
+    private static class TargetPlatformCallable implements Callable<String> {
+        @Override
+        public String call() throws Exception {
             try (ByteArrayOutputStream baos = new ByteArrayOutputStream()) {
                 TargetPlatformService tps = Framework.getService(TargetPlatformService.class);
                 List<TargetPlatform> res = tps.getAvailableTargetPlatforms(
@@ -59,8 +71,6 @@ public class TargetPlatformObject extends DefaultObject {
                 JSONExporter.exportToJson(res, baos, false);
                 return new String(baos.toByteArray());
             }
-        });
-
-        return Response.status(Response.Status.OK).entity(platforms).build();
+        }
     }
 }
