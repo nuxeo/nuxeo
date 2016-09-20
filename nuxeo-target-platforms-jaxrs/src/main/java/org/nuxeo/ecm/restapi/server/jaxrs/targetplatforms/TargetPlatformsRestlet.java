8716fd22dd64e019496cd53af5ac1d18b47e369e
Arnaud Kervern <akervern@nuxeo.com>
2016-09-20 16:44:31 -0400

    NXP-20494: Add Copyright

--- nuxeo-target-platforms-jaxrs/src/main/java/org/nuxeo/ecm/restapi/server/jaxrs/targetplatforms/TargetPlatformsRestlet.java
+++ nuxeo-target-platforms-jaxrs/src/main/java/org/nuxeo/ecm/restapi/server/jaxrs/targetplatforms/TargetPlatformsRestlet.java
@@ -1,3 +1,21 @@
+/*
+ * (C) Copyright 2016 Nuxeo SA (http://nuxeo.com/) and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Contributors:
+ *     Nuxeo
+ */
 package org.nuxeo.ecm.restapi.server.jaxrs.targetplatforms;
 
 import java.io.ByteArrayOutputStream;
@@ -24,6 +42,9 @@ import com.google.common.cache.CacheBuilder;
 import com.google.common.cache.CacheLoader;
 import com.google.common.cache.LoadingCache;
 
+/**
+ * @since 1.7.6032
+ */
 public class TargetPlatformsRestlet extends BaseNuxeoRestlet {
     private static final String PUBLIC_TP_CACHE_KEY = "PUBLIC_TP";
 
