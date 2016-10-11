d705edc72d35e6acc497fe92e2a58de86801680b
Anahide Tchertchian <at@nuxeo.com>
2016-10-11 08:32:53 +0200

    NXP-20624: do no cache locale provider service in a static variable, to avoid retrieving an invalid service in tests

--- src/main/java/org/nuxeo/ecm/platform/groups/audit/service/acl/utils/MessageAccessor.java
+++ src/main/java/org/nuxeo/ecm/platform/groups/audit/service/acl/utils/MessageAccessor.java
@@ -27,14 +27,16 @@ import org.nuxeo.ecm.platform.web.common.locale.LocaleProvider;
 import org.nuxeo.runtime.api.Framework;
 
 public class MessageAccessor {
-    protected static LocaleProvider localeProvider = Framework.getLocalService(LocaleProvider.class);
 
     public static String get(CoreSession session, String key) {
         Locale locale = null;
-        if (localeProvider != null)
+        LocaleProvider localeProvider = Framework.getLocalService(LocaleProvider.class);
+        if (localeProvider != null) {
             locale = localeProvider.getLocale(session);
-        if (locale == null)
+        }
+        if (locale == null) {
             locale = Locale.ENGLISH;
+        }
 
         try {
             return I18NUtils.getMessageString("messages", key, null, locale);
@@ -42,4 +44,5 @@ public class MessageAccessor {
             return key;
         }
     }
+
 }
