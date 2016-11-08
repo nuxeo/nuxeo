57e5c222419e034c83f29d170a72708fcf705691
Funsho David <fdavid@nuxeo.com>
2016-11-08 10:05:40 +0100

    NXP-16650 : add property to allow zip preview

--- nuxeo-features/nuxeo-platform-preview/src/main/java/org/nuxeo/ecm/platform/preview/adapter/base/ConverterBasedHtmlPreviewAdapter.java
+++ nuxeo-features/nuxeo-platform-preview/src/main/java/org/nuxeo/ecm/platform/preview/adapter/base/ConverterBasedHtmlPreviewAdapter.java
@@ -37,6 +37,7 @@ import org.nuxeo.ecm.platform.preview.adapter.PreviewAdapterManager;
 import org.nuxeo.ecm.platform.preview.api.NothingToPreviewException;
 import org.nuxeo.ecm.platform.preview.api.PreviewException;
 import org.nuxeo.runtime.api.Framework;
+import org.nuxeo.runtime.services.config.ConfigurationService;
 
 /**
  * Base class for preview based on "on the fly" HTML transformers
@@ -50,6 +51,11 @@ public class ConverterBasedHtmlPreviewAdapter extends AbstractHtmlPreviewAdapter
     protected String defaultFieldXPath;
 
     protected MimetypeRegistry mimeTypeService;
+
+    /**
+     * @since 8.10
+     */
+    protected static final String ALLOW_ZIP_PREVIEW = "nuxeo.preview.zip.enabled";
 
     public ConversionService getConversionService() {
         return Framework.getService(ConversionService.class);
@@ -219,7 +225,8 @@ public class ConverterBasedHtmlPreviewAdapter extends AbstractHtmlPreviewAdapter
         } catch (NothingToPreviewException e) {
             return false;
         }
-        if (blob2Preview == null) {
+        if (blob2Preview == null || ("application/zip".equals(getMimeType(blob2Preview))
+                && !Framework.getService(ConfigurationService.class).isBooleanPropertyTrue(ALLOW_ZIP_PREVIEW))) {
             return false;
         }
         return true;
