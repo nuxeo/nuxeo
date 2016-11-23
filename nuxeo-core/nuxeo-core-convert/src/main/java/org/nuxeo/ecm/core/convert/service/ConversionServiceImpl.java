6389cd23e7e57727901c7a7696d8a5d4a8c8047c
Florent Guillaume <fguillaume@nuxeo.com>
2016-11-23 18:39:29 +0100

    NXP-21126: allow proper contrib override

--- nuxeo-core/nuxeo-core-convert/src/main/java/org/nuxeo/ecm/core/convert/service/ConversionServiceImpl.java
+++ nuxeo-core/nuxeo-core-convert/src/main/java/org/nuxeo/ecm/core/convert/service/ConversionServiceImpl.java
@@ -110,6 +110,7 @@ public class ConversionServiceImpl extends DefaultComponent implements Conversio
         converterDescriptors.clear();
         translationHelper.clear();
         self = this;
+        config.clearCachingDirectory();
         Framework.addListener(new RuntimeServiceListener() {
 
             @Override
@@ -148,6 +149,7 @@ public class ConversionServiceImpl extends DefaultComponent implements Conversio
         } else if (CONFIG_EP.equals(extensionPoint)) {
             GlobalConfigDescriptor desc = (GlobalConfigDescriptor) contribution;
             config.update(desc);
+            config.clearCachingDirectory();
         } else {
             log.error("Unable to handle unknown extensionPoint " + extensionPoint);
         }
