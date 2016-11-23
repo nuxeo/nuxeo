5ab038210fc637c12880f648f9432084e3b1c1d5
Antoine Taillefer <ataillefer@nuxeo.com>
2016-11-23 17:18:40 +0100

    NXP-20468: Temporarily add @RandomBug.Repeat on TestVersioningFileSystemItemFactory

--- nuxeo-drive-core/src/test/java/org/nuxeo/drive/service/adapter/TestVersioningFileSystemItemFactory.java
+++ nuxeo-drive-core/src/test/java/org/nuxeo/drive/service/adapter/TestVersioningFileSystemItemFactory.java
@@ -53,6 +53,7 @@ import org.nuxeo.runtime.services.config.ConfigurationService;
 import org.nuxeo.runtime.test.runner.Features;
 import org.nuxeo.runtime.test.runner.FeaturesRunner;
 import org.nuxeo.runtime.test.runner.LocalDeploy;
+import org.nuxeo.runtime.test.runner.RandomBug;
 import org.nuxeo.runtime.transaction.TransactionHelper;
 
 /**
@@ -130,6 +131,7 @@ import org.nuxeo.runtime.transaction.TransactionHelper;
  */
 @RunWith(FeaturesRunner.class)
 @Features(NuxeoDriveFeature.class)
+@RandomBug.Repeat(issue = "NXP-20468")
 public class TestVersioningFileSystemItemFactory {
 
     // needs to be bigger than 1s for MySQL
