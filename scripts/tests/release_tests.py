f82c7c803e0f828f8135d5031bb30d15eab7cd8f
Pierre-Gildas MILLON <pgmillon@nuxeo.com>
2016-11-30 18:08:22 +0100

    NXP-13846: Add regression test for prepare, use revert -n instead of reset, fix update_version, update the maintenance() method

--- scripts/tests/release_tests.py
+++ scripts/tests/release_tests.py
@@ -15,12 +15,14 @@ Contributors:
     Pierre-Gildas MILLON <pgmillon@nuxeo.com>
 """
 import os
+import shutil
 import unittest
+from tempfile import mkdtemp
 
 from mock.mock import Mock, patch
 
-from nxutils import Repository
-from release import Release
+from nxutils import Repository, system, system_with_retries, check_output
+from release import Release, ReleaseInfo
 
 
 class TestMocks(object):
@@ -42,39 +44,80 @@ class ReleaseTestCase(unittest.TestCase):
     def setUp(self):
         super(ReleaseTestCase, self).setUp()
 
+        self.workdir = mkdtemp()
         self.mocks = TestMocks()
-        patches = [
-            patch('release.etree_parse', side_effect=self.mocks.etree_parse)
-        ]
 
-        for patcher in patches:
-            patcher.start()
-            self.addCleanup(patcher.stop)
+        os.chdir(self.workdir)
+        system('git init')
+        system('git remote add origin file:///dev/null')
+
+    def tearDown(self):
+        super(ReleaseTestCase, self).tearDown()
+
+        shutil.rmtree(self.workdir)
 
     def assertNextSnapshot(self, current, expected, policy='auto_last'):
         self.mocks.etree_parse.return_value.getroot.return_value.find.return_value.text = current
 
         release_info = ReleaseInfo(branch='some_branch', tag='auto', maintenance_version='auto', is_final=True,
-                                   auto_increment_policy=policy)
-        self.assertEqual(expected,
-                         Release(Repository(os.getcwd(), 'nuxeo_scripts'), release_info).next_snapshot)
+                                   auto_increment_policy=policy, next_snapshot='auto')
+        self.assertEqual(expected, Release(Repository(os.getcwd(), 'nuxeo_scripts'), release_info).next_snapshot)
 
     def testSetNextSnapshot(self):
-        self.assertNextSnapshot('1.0.19-SNAPSHOT', '1.0.20-SNAPSHOT')
+        with(patch('release.etree_parse', side_effect=self.mocks.etree_parse)):
+            self.assertNextSnapshot('1.0.19-SNAPSHOT', '1.0.20-SNAPSHOT')
 
-        self.assertNextSnapshot('1.0.2-SNAPSHOT', '1.0.3-SNAPSHOT', policy='auto_last')
-        self.assertNextSnapshot('1.2-SNAPSHOT', '1.3-SNAPSHOT', policy='auto_last')
-        self.assertNextSnapshot('29-SNAPSHOT', '30-SNAPSHOT', policy='auto_last')
+            self.assertNextSnapshot('1.0.2-SNAPSHOT', '1.0.3-SNAPSHOT', policy='auto_last')
+            self.assertNextSnapshot('1.2-SNAPSHOT', '1.3-SNAPSHOT', policy='auto_last')
+            self.assertNextSnapshot('29-SNAPSHOT', '30-SNAPSHOT', policy='auto_last')
 
-        self.assertNextSnapshot('1.0.2-SNAPSHOT', '1.0.3-SNAPSHOT', policy='auto_patch')
-        self.assertNextSnapshot('1.2-SNAPSHOT', '1.2.1-SNAPSHOT', policy='auto_patch')
-        self.assertNextSnapshot('29-SNAPSHOT', '29.0.1-SNAPSHOT', policy='auto_patch')
+            self.assertNextSnapshot('1.0.2-SNAPSHOT', '1.0.3-SNAPSHOT', policy='auto_patch')
+            self.assertNextSnapshot('1.2-SNAPSHOT', '1.2.1-SNAPSHOT', policy='auto_patch')
+            self.assertNextSnapshot('29-SNAPSHOT', '29.0.1-SNAPSHOT', policy='auto_patch')
 
-        self.assertNextSnapshot('1.0.2-SNAPSHOT', '1.1.0-SNAPSHOT', policy='auto_minor')
-        self.assertNextSnapshot('1.2-SNAPSHOT', '1.3-SNAPSHOT', policy='auto_minor')
-        self.assertNextSnapshot('29-SNAPSHOT', '29.1-SNAPSHOT', policy='auto_minor')
+            self.assertNextSnapshot('1.0.2-SNAPSHOT', '1.1.0-SNAPSHOT', policy='auto_minor')
+            self.assertNextSnapshot('1.2-SNAPSHOT', '1.3-SNAPSHOT', policy='auto_minor')
+            self.assertNextSnapshot('29-SNAPSHOT', '29.1-SNAPSHOT', policy='auto_minor')
 
-        self.assertNextSnapshot('1.0.2-SNAPSHOT', '2.0.0-SNAPSHOT', policy='auto_major')
-        self.assertNextSnapshot('1.2-SNAPSHOT', '2.0-SNAPSHOT', policy='auto_major')
-        self.assertNextSnapshot('29-SNAPSHOT', '30-SNAPSHOT', policy='auto_major')
+            self.assertNextSnapshot('1.0.2-SNAPSHOT', '2.0.0-SNAPSHOT', policy='auto_major')
+            self.assertNextSnapshot('1.2-SNAPSHOT', '2.0-SNAPSHOT', policy='auto_major')
+            self.assertNextSnapshot('29-SNAPSHOT', '30-SNAPSHOT', policy='auto_major')
 
+    def testPrepare(self):
+        print self.workdir
+        shutil.copy(os.path.join(os.path.dirname(__file__), 'resources/pom.xml'), self.workdir)
+        system('git add pom.xml')
+        system('git commit -m "Original import"')
+
+        release_info = ReleaseInfo(branch='master', tag='auto', maintenance_version='8.10-HF01-SNAPSHOT', is_final=True,
+                                   auto_increment_policy='auto_major_no_zero', next_snapshot='auto')
+        release = Release(Repository(os.getcwd(), 'origin'), release_info)
+
+        orig_system_with_retries = system_with_retries
+
+        def mock_system_with_retries(cmd, failonerror=True):
+            if "git fetch origin" != cmd:
+                orig_system_with_retries(cmd, failonerror)
+
+        with patch('nxutils.system_with_retries', side_effect=mock_system_with_retries), \
+                patch('nxutils.Repository.mvn'), patch('release.Release.package'):
+            release.prepare()
+
+        tags = check_output('git tag')
+        branches = check_output('git branch')
+
+        self.assertEqual('release-8.10', tags)
+        self.assertIn('8.10', branches)
+        self.assertIn('master', branches)
+
+        system('git checkout release-8.10')
+        with open('pom.xml') as pom:
+            self.assertIn('<version>8.10</version>', pom.read())
+
+        system('git checkout 8.10')
+        with open('pom.xml') as pom:
+            self.assertIn('<version>8.10-HF01-SNAPSHOT</version>', pom.read())
+
+        system('git checkout master')
+        with open('pom.xml') as pom:
+            self.assertIn('<version>9.1-SNAPSHOT</version>', pom.read())
