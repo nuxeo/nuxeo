6b700c1efa51baabf0f0e00d30a747719c0ff0ed
Estelle Giuly <egiuly@nuxeo.com>
2016-12-05 14:07:13 +0100

    NXP-20500 Use email address pattern in ITUsersTest

--- nuxeo-distribution/nuxeo-distribution-cap-webdriver-tests/src/test/java/org/nuxeo/ftest/cap/ITUsersTest.java
+++ nuxeo-distribution/nuxeo-distribution-cap-webdriver-tests/src/test/java/org/nuxeo/ftest/cap/ITUsersTest.java
@@ -50,7 +50,7 @@ public class ITUsersTest extends AbstractTest {
         UsersTabSubPage usersTab = login().getAdminCenter().getUsersGroupsHomePage().getUsersTab();
         String username = TEST_USERNAME + System.currentTimeMillis();
 
-        usersTab.getUserCreatePage().inviteUser(username , username, "lastname1", "company1", "email1",
+        usersTab.getUserCreatePage().inviteUser(username , username, "lastname1", "company1", "email1@test.com",
                 "members");
 
         //Need few seconds to display the search view after
@@ -72,7 +72,7 @@ public class ITUsersTest extends AbstractTest {
         String username = "jsmith";
         usersTab = usersTab.searchUser(username);
         if (!usersTab.isUserFound(username)) {
-            page = usersTab.getUserCreatePage().createUser(username, firstname, "lastname1", "company1", "email1",
+            page = usersTab.getUserCreatePage().createUser(username, firstname, "lastname1", "company1", "email1@test.com",
                     TEST_PASSWORD, "members");
             // no confirmation message anymore
             // assertEquals(page.getFeedbackMessage(), "User created");
@@ -100,7 +100,7 @@ public class ITUsersTest extends AbstractTest {
         UsersTabSubPage usersTab = login().getAdminCenter().getUsersGroupsHomePage().getUsersTab();
         usersTab = usersTab.searchUser(TEST_USERNAME);
         if (!usersTab.isUserFound(TEST_USERNAME)) {
-            page = usersTab.getUserCreatePage().createUser(TEST_USERNAME, firstname, "lastname1", "company1", "email1",
+            page = usersTab.getUserCreatePage().createUser(TEST_USERNAME, firstname, "lastname1", "company1", "email1@test.com",
                     TEST_PASSWORD, "members");
             // no confirmation message anymore
             // assertEquals(page.getFeedbackMessage(), "User created");
@@ -115,7 +115,7 @@ public class ITUsersTest extends AbstractTest {
         usersTab = usersTab.exitAdminCenter().getAdminCenter().getUsersGroupsHomePage().getUsersTab();
 
         // user already exists
-        page = usersTab.getUserCreatePage().createUser(TEST_USERNAME, "firstname1", "lastname1", "company1", "email1",
+        page = usersTab.getUserCreatePage().createUser(TEST_USERNAME, "firstname1", "lastname1", "company1", "email1@test.com",
                 TEST_PASSWORD, "members");
         assertEquals("User already exists.", page.getErrorFeedbackMessage());
         // cancel
