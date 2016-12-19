cba7f129c510075b092d063f9b45cb83fe501b8f
Estelle Giuly <egiuly@nuxeo.com>
2016-12-19 15:10:22 +0100

    NXP-16831 Add MimetypeRegistry.PDF_MIMETYPE

--- nuxeo-core/nuxeo-core-mimetype/src/main/java/org/nuxeo/ecm/platform/mimetype/interfaces/MimetypeRegistry.java
+++ nuxeo-core/nuxeo-core-mimetype/src/main/java/org/nuxeo/ecm/platform/mimetype/interfaces/MimetypeRegistry.java
@@ -1,5 +1,5 @@
 /*
- * (C) Copyright 2006-2007 Nuxeo SA (http://nuxeo.com/) and others.
+ * (C) Copyright 2006-2016 Nuxeo SA (http://nuxeo.com/) and others.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,6 +15,8 @@
  *
  * Contributors:
  *     Nuxeo - initial API and implementation
+ *     Julien Anguenot <ja@nuxeo.com>
+ *     Estelle Giuly <egiuly@nuxeo.com>
  *
  * $Id: MimetypeRegistry.java 20731 2007-06-18 15:13:32Z ogrisel $
  */
@@ -32,12 +34,12 @@ import org.nuxeo.ecm.platform.mimetype.MimetypeNotFoundException;
  * MimetypeEntry registry.
  * <p>
  * Flexible registry of mimetypes.
- *
- * @author <a href="ja@nuxeo.com">Julien Anguenot</a>
  */
 public interface MimetypeRegistry {
 
     String DEFAULT_MIMETYPE = "application/octet-stream";
+
+    String PDF_MIMETYPE = "application/pdf";
 
     /**
      * Returns the mime type from a given stream.
