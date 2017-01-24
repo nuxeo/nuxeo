284127897272d79f61a149a386b9162d697da61c
Anahide Tchertchian <at@nuxeo.com>
2017-01-24 16:37:20 +0100

    NXP-21238: use helper methods to properly find elements on the page and avoid randoms on ITActionsWidgetTest

--- ftest/webdriver/src/test/java/org/nuxeo/ftest/formsLayoutDemo/ITActionsWidgetTest.java
+++ ftest/webdriver/src/test/java/org/nuxeo/ftest/formsLayoutDemo/ITActionsWidgetTest.java
@@ -48,43 +48,46 @@ public class ITActionsWidgetTest extends AbstractWidgetPageTest {
 
         String fancyXPath = "//a[@id='" + idPrefix + "nxw_actionsWidget_1_layoutDemoFancyBox_link']/span";
         String widgetId = "actionsWidgetLayout_view_form:nxl_actionsWidgetLayout_1:nxw_actionTextWidget_1";
-        assertNotNull(driver.findElement(By.xpath(linkXpath)));
-        assertNotNull(driver.findElement(By.xpath(actionsXpath)));
-        assertNotNull(driver.findElement(By.xpath(fancyXPath)));
-        assertEquals("", driver.findElement(By.id(widgetId)).getText());
+        assertNotNull(Locator.findElementAndWaitUntilEnabled(By.xpath(linkXpath)));
+        assertNotNull(Locator.findElementAndWaitUntilEnabled(By.xpath(actionsXpath)));
+        assertNotNull(Locator.findElementAndWaitUntilEnabled(By.xpath(fancyXPath)));
+        assertEquals("", Locator.findElementAndWaitUntilEnabled(By.id(widgetId)).getText());
         doSubmitDemo();
 
         assertEquals(VALUE_REQUIRED,
-                driver.findElement(
+                Locator.findElementAndWaitUntilEnabled(
                         By.id("actionsWidgetLayout_edit_form:nxl_actionsWidgetLayout:nxw_actionTextWidget_message"))
-                      .getText());
-        driver.findElement(By.id("actionsWidgetLayout_edit_form:nxl_actionsWidgetLayout:nxw_actionTextWidget"))
-              .sendKeys("test");
+                       .getText());
+        Locator.findElementAndWaitUntilEnabled(
+                By.id("actionsWidgetLayout_edit_form:nxl_actionsWidgetLayout:nxw_actionTextWidget"))
+               .sendKeys("test");
         doSubmitDemo();
 
-        Locator.waitForTextNotPresent(driver.findElement(By.xpath("//html")), VALUE_REQUIRED);
-        assertNotNull(driver.findElement(By.xpath(linkXpath)));
-        assertNotNull(driver.findElement(By.xpath(actionsXpath)));
-        assertNotNull(driver.findElement(By.xpath(fancyXPath)));
-        assertEquals("test", driver.findElement(By.id(widgetId)).getText());
+        Locator.waitForTextNotPresent(Locator.findElementAndWaitUntilEnabled(By.xpath("//html")), VALUE_REQUIRED);
+        assertNotNull(Locator.findElementAndWaitUntilEnabled(By.xpath(linkXpath)));
+        assertNotNull(Locator.findElementAndWaitUntilEnabled(By.xpath(actionsXpath)));
+        assertNotNull(Locator.findElementAndWaitUntilEnabled(By.xpath(fancyXPath)));
+        assertEquals("test", Locator.findElementAndWaitUntilEnabled(By.id(widgetId)).getText());
 
         get(HomePage.URL + pageId, Page.class);
-        assertNotNull(driver.findElement(By.xpath(linkXpath)));
-        assertNotNull(driver.findElement(By.xpath(actionsXpath)));
-        assertNotNull(driver.findElement(By.xpath(fancyXPath)));
+        assertNotNull(Locator.findElementAndWaitUntilEnabled(By.xpath(linkXpath)));
+        assertNotNull(Locator.findElementAndWaitUntilEnabled(By.xpath(actionsXpath)));
+        assertNotNull(Locator.findElementAndWaitUntilEnabled(By.xpath(fancyXPath)));
         Locator.findElementWaitUntilEnabledAndClick(By.xpath(fancyXPath));
-        Locator.waitUntilElementPresent(By.cssSelector("#nxw_actionsWidget_1_layoutDemoFancyBox_after_view_box > div > h3"));
-        assertEquals("Fancy box sample",
-                driver.findElement(By.cssSelector("#nxw_actionsWidget_1_layoutDemoFancyBox_after_view_box > div > h3")).getText());
-        assertNotNull(driver.findElement(By.id("fancybox-close")));
-        driver.findElement(By.id("fancybox-close")).click();
+        Locator.waitUntilElementPresent(
+                By.cssSelector("#nxw_actionsWidget_1_layoutDemoFancyBox_after_view_box > div > h3"));
+        assertEquals("Fancy box sample", Locator
+                                                .findElementAndWaitUntilEnabled(By.cssSelector(
+                                                        "#nxw_actionsWidget_1_layoutDemoFancyBox_after_view_box > div > h3"))
+                                                .getText());
+        Locator.findElementWaitUntilEnabledAndClick(By.id("fancybox-close"));
         Locator.waitForTextNotPresent(driver.findElement(By.xpath("//html")), "Fancy box sample");
-        assertEquals("test", driver.findElement(By.id(widgetId)).getText());
+        assertEquals("test", Locator.findElementAndWaitUntilEnabled(By.id(widgetId)).getText());
 
         navigateTo(pageId);
-        assertNotNull(driver.findElement(By.xpath(linkXpath)));
-        assertNotNull(driver.findElement(By.xpath(actionsXpath)));
-        assertNotNull(driver.findElement(By.xpath(fancyXPath)));
-        assertEquals("", driver.findElement(By.id(widgetId)).getText());
+        assertNotNull(Locator.findElementAndWaitUntilEnabled(By.xpath(linkXpath)));
+        assertNotNull(Locator.findElementAndWaitUntilEnabled(By.xpath(actionsXpath)));
+        assertNotNull(Locator.findElementAndWaitUntilEnabled(By.xpath(fancyXPath)));
+        assertEquals("", Locator.findElementAndWaitUntilEnabled(By.id(widgetId)).getText());
     }
 }
