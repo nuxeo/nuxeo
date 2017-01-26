4525db752c8c5b7439a80cd62fe76cb4bf0faf04
Florent Guillaume <fguillaume@nuxeo.com>
2017-01-26 15:04:53 +0100

    NXP-14187: periodically cleanup orphan versions

--- nuxeo-core/nuxeo-core-test/src/test/java/org/nuxeo/ecm/core/version/test/TestVersioningRemovalPolicy.java
+++ nuxeo-core/nuxeo-core-test/src/test/java/org/nuxeo/ecm/core/version/test/TestVersioningRemovalPolicy.java
@@ -29,6 +29,7 @@ import javax.inject.Inject;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.nuxeo.ecm.core.CoreService;
 import org.nuxeo.ecm.core.api.CoreSession;
 import org.nuxeo.ecm.core.api.DocumentModel;
 import org.nuxeo.ecm.core.api.DocumentModelList;
@@ -39,6 +40,7 @@ import org.nuxeo.ecm.core.event.EventService;
 import org.nuxeo.ecm.core.test.CoreFeature;
 import org.nuxeo.ecm.core.test.annotations.Granularity;
 import org.nuxeo.ecm.core.test.annotations.RepositoryConfig;
+import org.nuxeo.runtime.api.Framework;
 import org.nuxeo.runtime.test.runner.Features;
 import org.nuxeo.runtime.test.runner.FeaturesRunner;
 import org.nuxeo.runtime.test.runner.LocalDeploy;
@@ -67,31 +69,41 @@ public class TestVersioningRemovalPolicy {
         }
     }
 
-    protected DocumentModelList getVersion() throws Exception {
+    protected DocumentModelList getVersion() {
         return session.query("select * from Document where ecm:isCheckedInVersion=1");
     }
 
     @Test
-    public void shouldRemoveOrphanVersions() throws Exception {
-
+    public void shouldRemoveOrphanVersionWhenLiveRemoved() {
         DocumentModel doc = session.createDocumentModel("/", "testfile1", "File");
         doc = session.createDocument(doc);
-        VersioningDocument vdoc = doc.getAdapter(VersioningDocument.class);
-        assertNotNull(vdoc);
-        assertTrue(doc.isCheckedOut());
-        assertEquals("0.0", vdoc.getVersionLabel());
-
-        doc.checkIn(VersioningOption.MINOR, "");
-        assertFalse(doc.isCheckedOut());
-        assertEquals("0.1", vdoc.getVersionLabel());
-
+        // create version
+        doc.checkIn(VersioningOption.MINOR, null);
+        // check out live doc
         doc.checkOut();
-        assertTrue(doc.isCheckedOut());
-        assertEquals("0.1+", vdoc.getVersionLabel());
 
-        List<DocumentModel> versions = session.getVersions(doc.getRef());
-        assertEquals(1, versions.size());
+        DocumentModelList vs = getVersion();
+        assertEquals(1, vs.size());
 
+        // now remove the doc
+        session.removeDocument(doc.getRef());
+        session.save();
+        waitForAsyncCompletion();
+
+        // version should not be found
+        vs = getVersion();
+        assertEquals(0, vs.size());
+    }
+
+    @Test
+    public void shouldRemoveOrphanVersionWhenLiveCheckedInRemoved() {
+        DocumentModel doc = session.createDocumentModel("/", "testfile1", "File");
+        doc = session.createDocument(doc);
+        // create version, live doc is checked in
+        session.checkIn(doc.getRef(), VersioningOption.MINOR, null);
+        session.save();
+
+        // version is found
         DocumentModelList vs = getVersion();
         assertEquals(1, vs.size());
 
@@ -101,6 +113,7 @@ public class TestVersioningRemovalPolicy {
 
         waitForAsyncCompletion();
 
+        // version should not be found
         vs = getVersion();
         assertEquals(0, vs.size());
     }
@@ -129,6 +142,51 @@ public class TestVersioningRemovalPolicy {
 
         vs = getVersion();
         assertEquals(0, vs.size()); // version deleted through last proxy
+    }
+
+    // NXP-14187
+    @Test
+    public void shouldRemoveOrphanVersionWhenContainingFolderRemoved() {
+        DocumentModel ws = session.createDocumentModel("/", "ws", "Workspace");
+        ws = session.createDocument(ws);
+        DocumentModel folder = session.createDocumentModel("/ws", "folder", "Folder");
+        folder = session.createDocument(folder);
+        DocumentModel doc = session.createDocumentModel("/ws/folder", "file", "File");
+        doc = session.createDocument(doc);
+        // create first version
+        session.checkIn(doc.getRef(), VersioningOption.MINOR, null);
+        // create more versions
+        int N = 10;
+        for (int i = 0; i < N; i++) {
+            session.checkOut(doc.getRef());
+            session.checkIn(doc.getRef(), VersioningOption.MINOR, null);
+        }
+        session.save();
+        waitForAsyncCompletion();
+
+        long n = cleanupOrphanVersions();
+        assertEquals(0, n);
+
+        // versions found
+        DocumentModelList vs = getVersion();
+        assertEquals(N + 1, vs.size());
+
+        // delete folder containing the doc
+        session.removeDocument(folder.getRef());
+        session.save();
+        waitForAsyncCompletion();
+
+        n = cleanupOrphanVersions();
+        assertEquals(N + 1, n);
+
+        // versions should not be found
+        vs = getVersion();
+        assertEquals(0, vs.size());
+    }
+
+    protected long cleanupOrphanVersions() {
+        CoreService coreService = Framework.getService(CoreService.class);
+        return coreService.cleanupOrphanVersions(5);
     }
 
     @Test
--- nuxeo-core/nuxeo-core/src/main/java/org/nuxeo/ecm/core/CoreService.java
+++ nuxeo-core/nuxeo-core/src/main/java/org/nuxeo/ecm/core/CoreService.java
@@ -20,16 +20,32 @@
  */
 package org.nuxeo.ecm.core;
 
+import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+import java.util.concurrent.atomic.AtomicLong;
 
+import org.nuxeo.ecm.core.api.CoreInstance;
+import org.nuxeo.ecm.core.api.CoreSession;
+import org.nuxeo.ecm.core.api.IdRef;
+import org.nuxeo.ecm.core.api.IterableQueryResult;
+import org.nuxeo.ecm.core.query.sql.NXQL;
+import org.nuxeo.ecm.core.repository.RepositoryService;
 import org.nuxeo.ecm.core.versioning.DefaultVersionRemovalPolicy;
 import org.nuxeo.ecm.core.versioning.OrphanVersionRemovalFilter;
 import org.nuxeo.ecm.core.versioning.VersionRemovalPolicy;
+import org.nuxeo.runtime.api.Framework;
 import org.nuxeo.runtime.model.ComponentContext;
 import org.nuxeo.runtime.model.ComponentInstance;
 import org.nuxeo.runtime.model.DefaultComponent;
+import org.nuxeo.runtime.transaction.TransactionHelper;
 
 /**
  * Service used to register version removal policies.
@@ -123,6 +139,99 @@ public class CoreService extends DefaultComponent {
     /** Gets all the orphan version removal filters registered. */
     public Collection<OrphanVersionRemovalFilter> getOrphanVersionRemovalFilters() {
         return orphanVersionRemovalFilters.values();
+    }
+
+    /**
+     * Removes the orphan versions.
+     * <p>
+     * A version stays referenced, and therefore is not removed, if any proxy points to a version in the version history
+     * of any live document, or in the case of tree snapshot if there is a snapshot containing a version in the version
+     * history of any live document.
+     *
+     * @param commitSize the maximum number of orphan versions to delete in one transaction
+     * @return the number of orphan versions deleted
+     * @since 9.1
+     */
+    public long cleanupOrphanVersions(long commitSize) {
+        RepositoryService repositoryService = Framework.getService(RepositoryService.class);
+        if (repositoryService == null) {
+            // not initialized
+            return 0;
+        }
+        List<String> repositoryNames = repositoryService.getRepositoryNames();
+        AtomicLong count = new AtomicLong();
+        for (String repositoryName : repositoryNames) {
+            TransactionHelper.runInTransaction(() -> {
+                CoreInstance.doPrivileged(repositoryName, (CoreSession session) -> {
+                    count.addAndGet(doCleanupOrphanVersions(session, commitSize));
+                });
+            });
+        }
+        return count.get();
+    }
+
+    protected long doCleanupOrphanVersions(CoreSession session, long commitSize) {
+        // compute map of version series -> list of version ids in it
+        Map<String, List<String>> versionSeriesToVersionIds = new HashMap<>();
+        String findVersions = "SELECT " + NXQL.ECM_UUID + ", " + NXQL.ECM_VERSION_VERSIONABLEID
+                + " FROM Document WHERE " + NXQL.ECM_ISVERSION + " = 1";
+        try (IterableQueryResult res = session.queryAndFetch(findVersions, NXQL.NXQL)) {
+            for (Map<String, Serializable> map : res) {
+                String versionSeriesId = (String) map.get(NXQL.ECM_VERSION_VERSIONABLEID);
+                String versionId = (String) map.get(NXQL.ECM_UUID);
+                versionSeriesToVersionIds.computeIfAbsent(versionSeriesId, k -> new ArrayList<>(4)).add(versionId);
+            }
+        }
+        Set<String> seriesIds = new HashSet<>();
+        // find the live doc ids
+        String findLive = "SELECT " + NXQL.ECM_UUID + " FROM Document WHERE " + NXQL.ECM_ISPROXY + " = 0 AND "
+                + NXQL.ECM_ISVERSION + " = 0";
+        try (IterableQueryResult res = session.queryAndFetch(findLive, NXQL.NXQL)) {
+            for (Map<String, Serializable> map : res) {
+                String id = (String) map.get(NXQL.ECM_UUID);
+                seriesIds.add(id);
+            }
+        }
+        // find the version series for proxies
+        String findProxies = "SELECT " + NXQL.ECM_PROXY_VERSIONABLEID + " FROM Document WHERE " + NXQL.ECM_ISPROXY
+                + " = 1";
+        try (IterableQueryResult res = session.queryAndFetch(findProxies, NXQL.NXQL)) {
+            for (Map<String, Serializable> map : res) {
+                String versionSeriesId = (String) map.get(NXQL.ECM_PROXY_VERSIONABLEID);
+                seriesIds.add(versionSeriesId);
+            }
+        }
+        // all version for series ids not found from live docs or proxies can be removed
+        versionSeriesToVersionIds.keySet().removeAll(seriesIds);
+        Set<String> ids = new HashSet<>();
+        for (Entry<String, List<String>> es : versionSeriesToVersionIds.entrySet()) {
+            String versionSeriesId = es.getKey();
+            if (seriesIds.contains(versionSeriesId)) {
+                continue;
+            }
+            // not referenced -> remove
+            List<String> versionIds = es.getValue();
+            ids.addAll(versionIds);
+        }
+        // new transaction as we may have spent some time in the previous queries
+        TransactionHelper.commitOrRollbackTransaction();
+        TransactionHelper.startTransaction();
+        // remove these ids
+        if (!ids.isEmpty()) {
+            long n = 0;
+            for (String id : ids) {
+                session.removeDocument(new IdRef(id));
+                n++;
+                if (n >= commitSize) {
+                    session.save();
+                    TransactionHelper.commitOrRollbackTransaction();
+                    TransactionHelper.startTransaction();
+                    n = 0;
+                }
+            }
+            session.save();
+        }
+        return ids.size();
     }
 
 }
