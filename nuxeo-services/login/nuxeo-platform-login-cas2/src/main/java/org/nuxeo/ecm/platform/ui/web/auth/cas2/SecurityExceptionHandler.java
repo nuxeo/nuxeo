354f45b45066b009230a8e9051957514a4310f9e
Funsho David <fdavid@nuxeo.com>
2017-02-21 10:57:55 +0100

    NXP-21635: unit tests

--- nuxeo-services/login/nuxeo-platform-login-cas2/src/main/java/org/nuxeo/ecm/platform/ui/web/auth/cas2/SecurityExceptionHandler.java
+++ nuxeo-services/login/nuxeo-platform-login-cas2/src/main/java/org/nuxeo/ecm/platform/ui/web/auth/cas2/SecurityExceptionHandler.java
@@ -18,11 +18,14 @@
  */
 package org.nuxeo.ecm.platform.ui.web.auth.cas2;
 
+import static org.nuxeo.ecm.platform.ui.web.auth.NXAuthConstants.LOGINCONTEXT_KEY;
 import static org.nuxeo.ecm.platform.ui.web.auth.NXAuthConstants.SSO_INITIAL_URL_REQUEST_KEY;
 
 import java.io.IOException;
 import java.security.Principal;
+import java.util.Set;
 
+import javax.security.auth.Subject;
 import javax.security.auth.login.LoginContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.Cookie;
@@ -68,8 +71,14 @@ public class SecurityExceptionHandler extends DefaultNuxeoExceptionHandler {
 
         Principal principal = request.getUserPrincipal();
         if (principal == null) {
-            LoginContext loginContext = (LoginContext) request.getAttribute("org.nuxeo.ecm.login.context");
-            principal = (Principal) loginContext.getSubject().getPrincipals().toArray()[0];
+            LoginContext loginContext = (LoginContext) request.getAttribute(LOGINCONTEXT_KEY);
+            Subject subject = loginContext.getSubject();
+            if (subject != null) {
+                Set<Principal> principals = subject.getPrincipals();
+                if (principals != null && !principals.isEmpty()) {
+                    principal = (Principal) principals.toArray()[0];
+                }
+            }
         }
         if (principal instanceof NuxeoPrincipal) {
             NuxeoPrincipal nuxeoPrincipal = (NuxeoPrincipal) principal;
