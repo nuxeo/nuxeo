0ce3b24c933043942662b485e757bf348691f108
Thomas Roger <throger@gmail.com>
2017-03-16 15:44:42 +0100

    NXP-21825: fix parent resolving

--- nuxeo-jsf/nuxeo-platform-ui-web/src/main/java/org/nuxeo/ecm/platform/ui/web/validator/DocumentConstraintValidator.java
+++ nuxeo-jsf/nuxeo-platform-ui-web/src/main/java/org/nuxeo/ecm/platform/ui/web/validator/DocumentConstraintValidator.java
@@ -217,13 +217,11 @@ public class DocumentConstraintValidator implements Validator, PartialStateHolde
         } else if (ListItemMapper.class.isAssignableFrom(baseClass)) {
             ListItemMapper mapper = (ListItemMapper) base;
             ProtectedEditableModel model = mapper.getModel();
-            ValueExpression listVe;
-            if (model.getParent() != null) {
-                // move one level up to resolve parent list binding
-                listVe = model.getParent().getBinding();
-            } else {
-                listVe = model.getBinding();
+
+            while (model.getParent() != null) {
+                model = model.getParent();
             }
+            ValueExpression listVe = model.getBinding();
             ValueExpressionAnalyzer expressionAnalyzer = new ValueExpressionAnalyzer(listVe);
             ValueReference listRef = expressionAnalyzer.getReference(context.getELContext());
             if (isResolvable(listRef, listVe)) {
