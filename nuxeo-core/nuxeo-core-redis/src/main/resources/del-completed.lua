8e9614a8b0cbb1903341b6a18e38a508bccad271
Benoit Delbosc <bdelbosc@nuxeo.com>
2017-03-17 17:59:50 +0100

    NXP-21734: Don't raise an error when trying to delete a work without a state because this halts the cleaning and can results in OOM. Note that this is a partial backport of NXP-21734

--- nuxeo-core/nuxeo-core-redis/src/main/resources/del-completed.lua
+++ nuxeo-core/nuxeo-core-redis/src/main/resources/del-completed.lua
@@ -14,12 +14,15 @@ for i = 2, #ARGV do
     local del = true
     if beforeTime ~= 0 then
         local state = redis.call('HGET', stateKey, workId)
-        assert(type(state) == "string", tostring(state) .. " is no a string")
-        del = string.match(state,"C.*")
-        if del then
-          local stime = string.sub(state, 2) -- format is 'C' + completion time
-          local time = tonumber(stime) 
-          del = time < beforeTime
+        -- if there is no state delete the key
+        if state ~= false then
+            assert(type(state) == "string", tostring(state) .. " is no a string")
+            del = string.match(state,"C.*")
+            if del then
+                local stime = string.sub(state, 2) -- format is 'C' + completion time
+                local time = tonumber(stime)
+                del = time < beforeTime
+            end
         end
     end
     if del then
