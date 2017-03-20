90286f9ecfd3bfb397d1f59d763f7e96a90ccd2e
Jenkins Nuxeo <jenkins@nuxeo.com>
2017-03-20 10:20:40 +0100

    NXP-19660 Convert locale String to Locale for User's tasks (#623)

--- nuxeo-features/nuxeo-platform-task/nuxeo-platform-task-api/src/main/java/org/nuxeo/ecm/platform/task/providers/UserTaskPageProvider.java
+++ nuxeo-features/nuxeo-platform-task/nuxeo-platform-task-api/src/main/java/org/nuxeo/ecm/platform/task/providers/UserTaskPageProvider.java
@@ -22,6 +22,8 @@ import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 
+import org.apache.commons.lang.LocaleUtils;
+import org.apache.commons.lang.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.nuxeo.ecm.core.api.ClientRuntimeException;
@@ -98,10 +100,10 @@ public class UserTaskPageProvider extends
 
     protected Locale getLocale() {
         String locale = (String) getProperties().get("locale");
-        if (locale!=null) {
-            return new Locale(locale);
+        if (!StringUtils.isBlank(locale)) {
+            return LocaleUtils.toLocale(locale);
         }
-        return null;
+        return Locale.getDefault();
     }
 
     protected void getAllTasks() {
