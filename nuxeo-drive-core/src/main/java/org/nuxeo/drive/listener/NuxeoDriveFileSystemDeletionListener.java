e6f7952b1358c5802db1c279391c1aed4dc1f22c
Antoine Taillefer <ataillefer@nuxeo.com>
2017-03-23 12:19:09 +0100

    NXP-21941: Escape path parameter when fetching sync roots by STARTSWITH

--- nuxeo-drive-core/src/main/java/org/nuxeo/drive/listener/NuxeoDriveFileSystemDeletionListener.java
+++ nuxeo-drive-core/src/main/java/org/nuxeo/drive/listener/NuxeoDriveFileSystemDeletionListener.java
@@ -46,6 +46,7 @@ import org.nuxeo.ecm.core.event.EventListener;
 import org.nuxeo.ecm.core.event.EventProducer;
 import org.nuxeo.ecm.core.event.impl.DocumentEventContext;
 import org.nuxeo.ecm.core.event.impl.EventContextImpl;
+import org.nuxeo.ecm.core.query.sql.NXQL;
 import org.nuxeo.ecm.core.schema.FacetNames;
 import org.nuxeo.ecm.platform.audit.api.AuditLogger;
 import org.nuxeo.ecm.platform.audit.api.ExtendedInfo;
@@ -203,8 +204,8 @@ public class NuxeoDriveFileSystemDeletionListener implements EventListener {
 
     protected List<DocumentModel> getChildSyncRoots(DocumentModel doc, CoreSession session) {
         String nxql = "SELECT * FROM Document WHERE ecm:mixinType = '" + NuxeoDriveManagerImpl.NUXEO_DRIVE_FACET
-                + "' AND ecm:currentLifeCycleState != 'deleted' AND ecm:isVersion = 0 AND ecm:path STARTSWITH '"
-                + doc.getPathAsString() + "'";
+                + "' AND ecm:currentLifeCycleState != 'deleted' AND ecm:isVersion = 0 AND ecm:path STARTSWITH "
+                + NXQL.escapeString(doc.getPathAsString());
         return session.query(nxql);
     }
 
