9cbd9de9a14435d51b87cc49118b09ec0d3889ed
Kevin Leturc <kleturc@nuxeo.com>
2017-03-23 22:38:18 +0100

    NXP-21873: Exclude 'Workspace' document type in auto versioning

---
+++ nuxeo-core/nuxeo-core/src/main/java/org/nuxeo/ecm/core/versioning/NoVersioningPolicyFilter.java
@@ -0,0 +1,39 @@
+/*
+ * (C) Copyright 2017 Nuxeo (http://nuxeo.com/) and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Contributors:
+ *     Kevin Leturc
+ */
+package org.nuxeo.ecm.core.versioning;
+
+import org.nuxeo.ecm.core.api.DocumentModel;
+import org.nuxeo.ecm.core.schema.FacetNames;
+
+/**
+ * Policy filter which disables automatic versioning for System document or document which should never be versioned.
+ *
+ * @since 9.1
+ */
+public class NoVersioningPolicyFilter implements VersioningPolicyFilter {
+
+    public static final String WORKSPACE_TYPE = "Workspace";
+
+    @Override
+    public boolean test(DocumentModel previousDocument, DocumentModel currentDocument) {
+        // exclude 'Workspace' document type because it owns 'file' schema which is involved in versioning policy
+        return WORKSPACE_TYPE.equals(currentDocument.getType()) || currentDocument.hasFacet(FacetNames.SYSTEM_DOCUMENT);
+    }
+
+}
