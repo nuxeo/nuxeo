9330ca4dcaffb1e633c9ef4694ce445dcac30208
Arnaud Kervern <akervern@nuxeo.com>
2017-03-30 12:05:41 -0400

    NXP-20844: Add Copyrigths

--- nuxeo-features/nuxeo-admin-center/nuxeo-admin-center-core/src/test/java/org/nuxeo/ecm/admin/permissions/AbstractPermissionsPurge.java
+++ nuxeo-features/nuxeo-admin-center/nuxeo-admin-center-core/src/test/java/org/nuxeo/ecm/admin/permissions/AbstractPermissionsPurge.java
@@ -1,5 +1,5 @@
 /*
- * (C) Copyright 2015 Nuxeo SA (http://nuxeo.com/) and others.
+ * (C) Copyright 2017 Nuxeo (http://nuxeo.com/) and others.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,6 +15,7 @@
  *
  * Contributors:
  *     Thomas Roger
+ *     Arnaud Kervern
  */
 
 package org.nuxeo.ecm.admin.permissions;
