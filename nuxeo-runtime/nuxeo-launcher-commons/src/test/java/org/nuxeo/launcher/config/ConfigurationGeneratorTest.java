086017ba97517a9fc84903e2043620c58a2d04ec
Kevin Leturc <kleturc@nuxeo.com>
2017-04-04 16:21:42 +0200

    NXP-22031: Fix templates value overwritten by GUI Launcher after wizard setup

--- nuxeo-runtime/nuxeo-launcher-commons/src/test/java/org/nuxeo/launcher/config/ConfigurationGeneratorTest.java
+++ nuxeo-runtime/nuxeo-launcher-commons/src/test/java/org/nuxeo/launcher/config/ConfigurationGeneratorTest.java
@@ -30,6 +30,7 @@ import static org.nuxeo.launcher.config.ConfigurationGenerator.JVMCHECK_PROP;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -509,6 +510,32 @@ public class ConfigurationGeneratorTest extends AbstractConfigurationTest {
         assertEquals("jdbc://10.0.0.1", configGenerator.getUserConfig().getProperty("nuxeo.env.prop2"));
 
         assertEquals("jdbc://10.0.0.1 false", configGenerator.getUserConfig().getProperty("nuxeo.env.prop3"));
+    }
+
+    /**
+     * NXP-22031 - test the configuration reloading after wizard setup when using Nuxeo GUI launcher.
+     */
+    @Test
+    public void testReloadConfigurationWhenConfigurationFileWasEditedByAnotherGenerator() throws Exception {
+        configGenerator = new ConfigurationGenerator();
+        assertTrue(configGenerator.init());
+        // Update template - write it to nuxeo.conf
+        configGenerator.saveConfiguration(Collections.singletonMap(ConfigurationGenerator.PARAM_TEMPLATES_NAME, "default,mongodb"));
+
+        // Test configuration generator context before reloading it
+        // getUserTemplates lazy load templates in the configuration generator context and put it back to userConfig
+        // That's explain the two assertions below
+        assertEquals("default,common,testinclude", configGenerator.getUserTemplates());
+        assertEquals("default,common,testinclude", configGenerator.getUserConfig().getProperty(ConfigurationGenerator.PARAM_TEMPLATES_NAME));
+
+        // Reload it
+        // At this point we test that we flush correctly the configuration generator context
+        assertTrue(configGenerator.init(true));
+
+        // Check values
+        // userConfig was filled with values from nuxeo.conf and getUserTemplates re-load templates from userConfig
+        assertEquals("default,mongodb", configGenerator.getUserTemplates());
+        assertEquals("default,mongodb", configGenerator.getUserConfig().getProperty(ConfigurationGenerator.PARAM_TEMPLATES_NAME));
     }
 
     private static class LogCaptureAppender extends AppenderSkeleton {
