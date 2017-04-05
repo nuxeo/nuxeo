56699e127ae02cddc9d9d7b251b168da32d26b92
Guillaume Renard <grenard@nuxeo.com>
2017-04-05 16:54:02 +0200

    NXP-21831: ignore random for 9.1

--- nuxeo-distribution/nuxeo-jsf-ui-webdriver-tests/src/test/java/org/nuxeo/ftest/cap/ITSearchTabTest.java
+++ nuxeo-distribution/nuxeo-jsf-ui-webdriver-tests/src/test/java/org/nuxeo/ftest/cap/ITSearchTabTest.java
@@ -18,6 +18,12 @@
  */
 package org.nuxeo.ftest.cap;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.nuxeo.functionaltests.Constants.NXDOC_URL_FORMAT;
+import static org.nuxeo.functionaltests.Constants.WORKSPACES_PATH;
+import static org.nuxeo.functionaltests.Constants.WORKSPACE_TYPE;
+
 import java.io.File;
 import java.io.IOException;
 import java.util.Date;
@@ -29,6 +35,7 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.nuxeo.functionaltests.AbstractTest;
 import org.nuxeo.functionaltests.AjaxRequestManager;
@@ -50,13 +57,6 @@ import org.nuxeo.functionaltests.pages.search.SearchResultsSubPage;
 import org.nuxeo.functionaltests.pages.tabs.EditTabSubPage;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
-
-import static org.nuxeo.functionaltests.Constants.NXDOC_URL_FORMAT;
-import static org.nuxeo.functionaltests.Constants.WORKSPACES_PATH;
-import static org.nuxeo.functionaltests.Constants.WORKSPACE_TYPE;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
 /**
  * @since 6.0
@@ -145,6 +145,7 @@ public class ITSearchTabTest extends AbstractTest {
     }
 
     @Test
+    @Ignore
     public void testSearch() throws UserNotConnectedException, IOException {
         DocumentBasePage documentBasePage = loginAsTestUser();
 
