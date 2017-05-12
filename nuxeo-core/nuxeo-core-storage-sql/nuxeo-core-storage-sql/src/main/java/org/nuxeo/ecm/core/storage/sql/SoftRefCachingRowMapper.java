313dba975a135b5f20da05eeaf8194f264e7b6e3
Florent Guillaume <fguillaume@nuxeo.com>
2017-05-12 18:09:09 +0200

    NXP-22286: prevent document removal if it's the target of a live proxy

--- nuxeo-core/nuxeo-core-storage-sql/nuxeo-core-storage-sql/src/main/java/org/nuxeo/ecm/core/storage/sql/SoftRefCachingRowMapper.java
+++ nuxeo-core/nuxeo-core-storage-sql/nuxeo-core-storage-sql/src/main/java/org/nuxeo/ecm/core/storage/sql/SoftRefCachingRowMapper.java
@@ -19,6 +19,7 @@ import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import javax.transaction.xa.XAException;
 import javax.transaction.xa.Xid;
@@ -401,6 +402,11 @@ public class SoftRefCachingRowMapper implements RowMapper {
         return rows;
     }
 
+    @Override
+    public Set<Serializable> readSelectionsIds(SelectionType selType, List<Serializable> values) {
+        return rowMapper.readSelectionsIds(selType, values);
+    }
+
     /*
      * ----- Copy -----
      */
@@ -425,9 +431,14 @@ public class SoftRefCachingRowMapper implements RowMapper {
     }
 
     @Override
-    public List<NodeInfo> remove(NodeInfo rootInfo) {
-        List<NodeInfo> infos = rowMapper.remove(rootInfo);
-        for (NodeInfo info : infos) {
+    public List<NodeInfo> getDescendantsInfo(Serializable rootId) {
+        return rowMapper.getDescendantsInfo(rootId);
+    }
+
+    @Override
+    public void remove(Serializable rootId, List<NodeInfo> nodeInfos) {
+        rowMapper.remove(rootId, nodeInfos);
+        for (NodeInfo info : nodeInfos) {
             for (String fragmentName : model.getTypeFragments(new IdWithTypes(info.id, info.primaryType, null))) {
                 RowId rowId = new RowId(fragmentName, info.id);
                 cacheRemove(rowId);
@@ -436,8 +447,7 @@ public class SoftRefCachingRowMapper implements RowMapper {
         }
         // we only put as absent the root fragment, to avoid polluting the cache
         // with lots of absent info. the rest is removed entirely
-        cachePutAbsent(new RowId(Model.HIER_TABLE_NAME, rootInfo.id));
-        return infos;
+        cachePutAbsent(new RowId(Model.HIER_TABLE_NAME, rootId));
     }
 
 }
