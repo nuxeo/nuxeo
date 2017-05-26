7ae774f37d35e4c39c45a5ae4c34f07f1bd3abef
Kevin Leturc <kleturc@nuxeo.com>
2017-05-26 12:03:16 +0200

    NXP-22252: Fix proxy listing when targeting document without READ permission

--- nuxeo-opencmis-impl/src/main/java/org/nuxeo/ecm/core/opencmis/impl/server/NuxeoPropertyData.java
+++ nuxeo-opencmis-impl/src/main/java/org/nuxeo/ecm/core/opencmis/impl/server/NuxeoPropertyData.java
@@ -997,8 +997,7 @@ public abstract class NuxeoPropertyData<T> extends NuxeoPropertyDataBase<T> {
             if (!doc.isVersionSeriesCheckedOut()) {
                 return null;
             }
-            DocumentModel pwc = doc.getCoreSession().getWorkingCopy(doc.getRef());
-            return pwc == null ? null : pwc.getId();
+            return doc.getCoreSession().getVersionSeriesId(doc.getRef());
         }
     }
 
@@ -1021,9 +1020,9 @@ public abstract class NuxeoPropertyData<T> extends NuxeoPropertyDataBase<T> {
             if (!doc.isVersionSeriesCheckedOut()) {
                 return null;
             }
-            DocumentModel pwc = doc.getCoreSession().getWorkingCopy(doc.getRef());
+            String versionSeriesId = doc.getCoreSession().getVersionSeriesId(doc.getRef());
             // TODO not implemented
-            return pwc == null ? null : callContext.getUsername();
+            return versionSeriesId == null ? null : callContext.getUsername();
         }
     }
 
