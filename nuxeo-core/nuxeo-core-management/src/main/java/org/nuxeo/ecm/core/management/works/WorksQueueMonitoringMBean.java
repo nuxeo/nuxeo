4e808efa690ba853802d759bf364304810867d60
Kevin Leturc <kleturc@nuxeo.com>
2017-05-31 17:03:09 +0200

    NXP-22282: Expose workmanager metrics as Codahale metrics

--- nuxeo-core/nuxeo-core-management/src/main/java/org/nuxeo/ecm/core/management/works/WorksQueueMonitoringMBean.java
+++ nuxeo-core/nuxeo-core-management/src/main/java/org/nuxeo/ecm/core/management/works/WorksQueueMonitoringMBean.java
@@ -36,11 +36,10 @@ public interface WorksQueueMonitoringMBean {
 
     /**
      * Toogles processing for all queues
-     * @throws InterruptedException
      *
      * @since 8.3
      */
-    boolean toggleProcessing() throws InterruptedException;
+    boolean toggleProcessing();
 
 }
 
