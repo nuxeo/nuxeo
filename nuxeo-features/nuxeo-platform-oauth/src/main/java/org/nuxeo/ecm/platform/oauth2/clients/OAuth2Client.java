c51892b427cd8225a2bcff46d0bb26a98d663c98
Thomas Roger <troger@nuxeo.com>
2017-06-19 16:06:17 +0200

    NXP-22523: ClientRegistry -> OAuth2ClientService; remove contribution of OAuth2 clients

--- nuxeo-features/nuxeo-platform-oauth/src/main/java/org/nuxeo/ecm/platform/oauth2/clients/OAuth2Client.java
+++ nuxeo-features/nuxeo-platform-oauth/src/main/java/org/nuxeo/ecm/platform/oauth2/clients/OAuth2Client.java
@@ -18,85 +18,47 @@
  */
 package org.nuxeo.ecm.platform.oauth2.clients;
 
-import static org.nuxeo.ecm.platform.oauth2.clients.ClientRegistry.OAUTH2CLIENT_SCHEMA;
+import static org.nuxeo.ecm.platform.oauth2.clients.OAuth2ClientService.OAUTH2CLIENT_SCHEMA;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 import java.util.regex.Pattern;
 
 import org.apache.commons.lang.StringUtils;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-import org.nuxeo.common.xmap.annotation.XNode;
-import org.nuxeo.common.xmap.annotation.XObject;
 import org.nuxeo.ecm.core.api.DocumentModel;
 
 /**
  * @author <a href="mailto:ak@nuxeo.com">Arnaud Kervern</a>
  * @since 5.9.2
  */
-@XObject("client")
 public class OAuth2Client {
-
-    private static final Log log = LogFactory.getLog(OAuth2Client.class);
 
     protected static final Pattern LOCALHOST_PATTERN = Pattern.compile("http://localhost(:\\d+)?(/.*)?");
 
-    @XNode("@name")
     protected String name;
 
-    @XNode("@id")
     protected String id;
 
-    @XNode("@secret")
     protected String secret;
 
     /**
      * @since 9.2
      */
-    @XNode("@redirectURI")
     protected String redirectURI;
 
-    @XNode("@enabled")
-    protected boolean enabled = true;
-
-    public OAuth2Client() {
-    }
-
-    /**
-     * @deprecated since 9.2, use {@link #OAuth2Client(String, String, String, String)} instead
-     */
-    @Deprecated
-    public OAuth2Client(String name, String id, String secret) {
-        this(name, id, secret, null);
-    }
+    protected boolean enabled;
 
     /**
      * @since 9.2
      */
-    public OAuth2Client(String name, String id, String secret, String redirectURI) {
+    protected OAuth2Client(String name, String id, String secret, String redirectURI, boolean enabled) {
         this.name = name;
         this.id = id;
         this.secret = secret;
         this.redirectURI = redirectURI;
+        this.enabled = enabled;
     }
 
     public String getId() {
         return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getSecret() {
-        return secret;
-    }
-
-    public void setSecret(String secret) {
-        this.secret = secret;
     }
 
     /**
@@ -106,37 +68,12 @@ public class OAuth2Client {
         return redirectURI;
     }
 
-    /**
-     * @since 9.2
-     */
-    public void setRedirectURI(String redirectURI) {
-        this.redirectURI = redirectURI;
-    }
-
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public boolean isEnabled() {
         return enabled;
-    }
-
-    public void setEnabled(boolean enable) {
-        this.enabled = enable;
-    }
-
-    public Map<String, Object> toMap() {
-        Map<String, Object> doc = new HashMap<>();
-        doc.put("clientId", id);
-        doc.put("clientSecret", secret);
-        doc.put("redirectURI", redirectURI);
-        doc.put("name", name);
-        doc.put("enabled", enabled);
-        return doc;
     }
 
     public static OAuth2Client fromDocumentModel(DocumentModel doc) {
@@ -146,9 +83,7 @@ public class OAuth2Client {
         String redirectURI = (String) doc.getPropertyValue(OAUTH2CLIENT_SCHEMA + ":redirectURI");
         boolean enabled = (Boolean) doc.getPropertyValue(OAUTH2CLIENT_SCHEMA + ":enabled");
 
-        OAuth2Client client = new OAuth2Client(name, id, secret, redirectURI);
-        client.enabled = enabled;
-        return client;
+        return new OAuth2Client(name, id, secret, redirectURI, enabled);
     }
 
     /**
@@ -163,33 +98,8 @@ public class OAuth2Client {
      * @since 9.2
      */
     public static boolean isRedirectURIValid(String redirectURI) {
-        if (redirectURI.startsWith("https") || !redirectURI.startsWith("http")) {
-            return true;
-        }
-        return LOCALHOST_PATTERN.matcher(redirectURI).matches();
-    }
-
-    /**
-     * @since 9.2
-     */
-    public boolean isValid() {
-        List<String> messages = new ArrayList<>();
-        if (StringUtils.isBlank(id)) {
-            messages.add("id is required");
-        }
-        if (StringUtils.isBlank(name)) {
-            messages.add("name is required");
-        }
-        if (StringUtils.isBlank(redirectURI)) {
-            messages.add("redirectURI is required");
-        } else if (!isRedirectURIValid(redirectURI)) {
-            messages.add("redirectURI must start with https for security reasons");
-        }
-        if (messages.isEmpty()) {
-            return true;
-        }
-        log.error(String.format("Invalid OAuth2 client %s: %s.", this, String.join(", ", messages)));
-        return false;
+        return redirectURI.startsWith("https") || !redirectURI.startsWith("http")
+                || LOCALHOST_PATTERN.matcher(redirectURI).matches();
     }
 
     public boolean isValidWith(String clientId, String clientSecret) {
---
+++ nuxeo-features/nuxeo-platform-oauth/src/main/java/org/nuxeo/ecm/platform/oauth2/clients/OAuth2ClientServiceImpl.java
@@ -0,0 +1,73 @@
+/*
+ * (C) Copyright 2014-2017 Nuxeo (http://nuxeo.com/) and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Contributors:
+ *     Arnaud Kervern
+ */
+package org.nuxeo.ecm.platform.oauth2.clients;
+
+import java.io.Serializable;
+import java.util.Collections;
+import java.util.Map;
+
+import org.nuxeo.ecm.core.api.DocumentModel;
+import org.nuxeo.ecm.core.api.DocumentModelList;
+import org.nuxeo.ecm.directory.Session;
+import org.nuxeo.ecm.directory.api.DirectoryService;
+import org.nuxeo.runtime.api.Framework;
+import org.nuxeo.runtime.model.DefaultComponent;
+
+/**
+ * OAuth2 Client service
+ *
+ * @since 9.2
+ */
+public class OAuth2ClientServiceImpl extends DefaultComponent implements OAuth2ClientService {
+
+    @Override
+    public boolean hasClient(String clientId) {
+        OAuth2Client client = getClient(clientId);
+        return client != null && client.isEnabled();
+    }
+
+    @Override
+    public boolean isValidClient(String clientId, String clientSecret) {
+        OAuth2Client client = getClient(clientId);
+        return client != null && client.isValidWith(clientId, clientSecret);
+    }
+
+    @Override
+    public OAuth2Client getClient(String clientId) {
+        DocumentModel doc = getClientModel(clientId);
+        if (doc == null) {
+            return null;
+        }
+        return OAuth2Client.fromDocumentModel(doc);
+    }
+
+    protected DocumentModel getClientModel(String clientId) {
+        DirectoryService service = Framework.getService(DirectoryService.class);
+        return Framework.doPrivileged(() -> {
+            try (Session session = service.open(OAUTH2CLIENT_DIRECTORY_NAME)) {
+                Map<String, Serializable> filter = Collections.singletonMap("clientId", clientId);
+                DocumentModelList docs = session.query(filter);
+                if (docs.size() > 0) {
+                    return docs.get(0);
+                }
+            }
+            return null;
+        });
+    }
+}
