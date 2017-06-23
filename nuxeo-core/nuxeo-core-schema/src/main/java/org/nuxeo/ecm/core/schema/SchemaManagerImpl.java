a6839af51f495ebbd22aad76b511f39688c9a0e6
Kevin Leturc <kleturc@nuxeo.com>
2017-06-23 17:44:25 +0200

    NXP-22603: Make facet overridable

--- nuxeo-core/nuxeo-core-schema/src/main/java/org/nuxeo/ecm/core/schema/SchemaManagerImpl.java
+++ nuxeo-core/nuxeo-core-schema/src/main/java/org/nuxeo/ecm/core/schema/SchemaManagerImpl.java
@@ -200,6 +200,7 @@ public class SchemaManagerImpl implements SchemaManager {
     }
 
     public synchronized void registerFacet(FacetDescriptor fd) {
+        allFacets.removeIf(f -> f.getName().equals(fd.getName()));
         allFacets.add(fd);
         dirty = true;
         log.info("Registered facet: " + fd.name);
