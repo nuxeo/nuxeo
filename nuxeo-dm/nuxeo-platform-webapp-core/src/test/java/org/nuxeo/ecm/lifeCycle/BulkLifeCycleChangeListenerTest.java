c8a5db8783d9f88d79bfe232ef3112f38527c4b9
Kevin Leturc <kleturc@nuxeo.com>
2017-06-23 16:06:33 +0200

    NXP-22608: Don't recurse too much when bulk changing life cycle

--- nuxeo-dm/nuxeo-platform-webapp-core/src/test/java/org/nuxeo/ecm/lifeCycle/BulkLifeCycleChangeListenerTest.java
+++ nuxeo-dm/nuxeo-platform-webapp-core/src/test/java/org/nuxeo/ecm/lifeCycle/BulkLifeCycleChangeListenerTest.java
@@ -41,7 +41,7 @@ import org.nuxeo.runtime.test.runner.FeaturesRunner;
 import org.nuxeo.runtime.transaction.TransactionHelper;
 
 /**
- * Simple test Case for MassLifeCycleChangeListener
+ * Simple test Case for BulkLifeCycleChangeListener
  */
 @RunWith(FeaturesRunner.class)
 @Features(CoreFeature.class)
@@ -114,6 +114,44 @@ public class BulkLifeCycleChangeListenerTest {
         for (DocumentModel child : childrenCopy) {
             assertEquals("project", session.getCurrentLifeCycleState(child.getRef()));
         }
+    }
+
+    /*
+     * NXP-22608
+     */
+    @Test
+    public void testLifeCycleAPITwoLevels() {
+
+        DocumentModel folderDoc1 = session.createDocumentModel("/", "testFolder1", "Folder");
+        folderDoc1 = session.createDocument(folderDoc1);
+        DocumentModel folderDoc2 = session.createDocumentModel("/testFolder1", "testFolder2", "Folder");
+        folderDoc2 = session.createDocument(folderDoc2);
+        DocumentModel testFile1 = session.createDocumentModel("/testFolder1/testFolder2", "testFile1", "File");
+        testFile1 = session.createDocument(testFile1);
+        DocumentModel testFile2 = session.createDocumentModel("/testFolder1/testFolder2", "testFile2", "File");
+        testFile2 = session.createDocument(testFile2);
+        DocumentModel testFile3 = session.createDocumentModel("/testFolder1/testFolder2", "testFile3", "File");
+        testFile3 = session.createDocument(testFile3);
+
+        session.saveDocument(folderDoc1);
+        session.saveDocument(folderDoc2);
+        session.saveDocument(testFile1);
+        session.saveDocument(testFile2);
+        session.saveDocument(testFile3);
+
+        Collection<String> allowedStateTransitions = session.getAllowedStateTransitions(folderDoc1.getRef());
+        assertTrue(allowedStateTransitions.contains("approve"));
+
+        assertTrue(session.followTransition(folderDoc1.getRef(), "approve"));
+        session.save();
+
+        nextTransaction();
+
+        // Check that the BulkLifeCycleChangeListener has changed child folders and files to approved
+        assertEquals("approved", session.getCurrentLifeCycleState(folderDoc2.getRef()));
+        assertEquals("approved", session.getCurrentLifeCycleState(testFile1.getRef()));
+        assertEquals("approved", session.getCurrentLifeCycleState(testFile2.getRef()));
+        assertEquals("approved", session.getCurrentLifeCycleState(testFile3.getRef()));
     }
 
 }
