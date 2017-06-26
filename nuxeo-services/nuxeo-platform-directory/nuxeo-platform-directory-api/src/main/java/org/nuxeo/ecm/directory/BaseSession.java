e5d2e3815d345e478da01c20487d76c72136b860
Antoine Taillefer <ataillefer@nuxeo.com>
2017-06-26 13:02:25 +0200

    NXP-22609: Format

--- nuxeo-services/nuxeo-platform-directory/nuxeo-platform-directory-api/src/main/java/org/nuxeo/ecm/directory/BaseSession.java
+++ nuxeo-services/nuxeo-platform-directory/nuxeo-platform-directory-api/src/main/java/org/nuxeo/ecm/directory/BaseSession.java
@@ -247,8 +247,8 @@ public abstract class BaseSession implements Session {
      *
      * @since 5.3.1
      */
-    public static DocumentModel createEntryModel(String sessionId, String schema, String id,
-            Map<String, Object> values, boolean readOnly) throws PropertyException {
+    public static DocumentModel createEntryModel(String sessionId, String schema, String id, Map<String, Object> values,
+            boolean readOnly) throws PropertyException {
         DocumentModel entry = createEntryModel(sessionId, schema, id, values);
         if (readOnly) {
             setReadOnlyEntry(entry);
