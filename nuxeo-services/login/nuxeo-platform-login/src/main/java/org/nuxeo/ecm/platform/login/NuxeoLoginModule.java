f599e0c6e132642cca2f35452f09ab115510415e
Funsho David <fdavid@nuxeo.com>
2017-07-28 17:40:12 +0200

    NXP-22463: return permission denied if not authenticated

--- nuxeo-services/login/nuxeo-platform-login/src/main/java/org/nuxeo/ecm/platform/login/NuxeoLoginModule.java
+++ nuxeo-services/login/nuxeo-platform-login/src/main/java/org/nuxeo/ecm/platform/login/NuxeoLoginModule.java
@@ -267,7 +267,7 @@ public class NuxeoLoginModule extends NuxeoAbstractServerLoginModule {
             if (manager == null) {
                 principal = new NuxeoPrincipalImpl(username);
             } else {
-                principal = manager.getPrincipal(username);
+                principal = Framework.doPrivileged(() -> manager.getPrincipal(username));
                 if (principal == null) {
                     throw new LoginException(String.format("principal %s does not exist", username));
                 }
