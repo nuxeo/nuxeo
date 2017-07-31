d386865d1844f689e9d89b3b9c0d9811ae8464e4
Funsho David <fdavid@nuxeo.com>
2017-07-31 11:12:17 +0200

    NXP-22463: return permission denied if not authenticated

--- nuxeo-target-platforms-core/src/test/java/org/nuxeo/targetplatforms/core/service/test/TestTargetPlatformService.java
+++ nuxeo-target-platforms-core/src/test/java/org/nuxeo/targetplatforms/core/service/test/TestTargetPlatformService.java
@@ -35,6 +35,7 @@ import javax.inject.Inject;
 import org.junit.After;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.nuxeo.directory.test.DirectoryFeature;
 import org.nuxeo.ecm.core.api.DocumentModel;
 import org.nuxeo.ecm.directory.Session;
 import org.nuxeo.ecm.directory.api.DirectoryService;
@@ -57,11 +58,10 @@ import org.nuxeo.targetplatforms.core.service.DirectoryUpdater;
  * @since 5.7.1
  */
 @RunWith(FeaturesRunner.class)
-@Features(RuntimeFeature.class)
+@Features({ RuntimeFeature.class, DirectoryFeature.class })
 @Deploy({ "org.nuxeo.runtime.jtajca", "org.nuxeo.runtime.datasource", "org.nuxeo.ecm.core", "org.nuxeo.ecm.core.schema",
-        "org.nuxeo.ecm.directory", "org.nuxeo.ecm.directory.sql", "org.nuxeo.ecm.core.api", "org.nuxeo.ecm.core.event",
-        "org.nuxeo.ecm.core.cache", "org.nuxeo.ecm.core.io", "org.nuxeo.ecm.platform.el",
-        "org.nuxeo.targetplatforms.core" })
+        "org.nuxeo.ecm.core.api", "org.nuxeo.ecm.core.event", "org.nuxeo.ecm.core.cache", "org.nuxeo.ecm.core.io",
+        "org.nuxeo.ecm.platform.el", "org.nuxeo.targetplatforms.core" })
 @LocalDeploy({ "org.nuxeo.targetplatforms.core:OSGI-INF/test-datasource-contrib.xml",
         "org.nuxeo.targetplatforms.core:OSGI-INF/test-targetplatforms-contrib.xml" })
 public class TestTargetPlatformService {
