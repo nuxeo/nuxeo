96ff89ea79f169f6b7b907a7a64d4e29fec8b6a0
Anahide Tchertchian <at@nuxeo.com>
2017-09-06 18:36:55 +0200

    NXP-19722: add non-regression webdriver tests for NXP-22009 use case

--- nuxeo-distribution/nuxeo-functional-tests/src/main/java/org/nuxeo/functionaltests/pages/search/SearchPage.java
+++ nuxeo-distribution/nuxeo-functional-tests/src/main/java/org/nuxeo/functionaltests/pages/search/SearchPage.java
@@ -18,8 +18,10 @@
  */
 package org.nuxeo.functionaltests.pages.search;
 
+import org.nuxeo.functionaltests.AbstractTest;
 import org.nuxeo.functionaltests.AjaxRequestManager;
 import org.nuxeo.functionaltests.Required;
+import org.nuxeo.functionaltests.contentView.ContentViewElement;
 import org.nuxeo.functionaltests.forms.Select2WidgetElement;
 import org.nuxeo.functionaltests.pages.DocumentBasePage;
 import org.openqa.selenium.By;
@@ -49,6 +51,13 @@ public class SearchPage extends DocumentBasePage {
     @FindBy(id = "nxl_gridSearchLayout:nxw_searchResults_panel")
     @Required
     protected WebElement searchResultPanel;
+
+    /**
+     * @since 9.3
+     */
+    @Required
+    @FindBy(id = "nxw_searchContentView")
+    protected WebElement contentView;
 
     public SearchPage(WebDriver driver) {
         super(driver);
@@ -110,6 +119,13 @@ public class SearchPage extends DocumentBasePage {
         am.watchAjaxRequests();
         s2.selectValue(searchLabel);
         am.waitForAjaxRequests();
+    }
+
+    /**
+     * @since 9.3
+     */
+    public ContentViewElement getContentView() {
+        return AbstractTest.getWebFragment(contentView, ContentViewElement.class);
     }
 
 }
