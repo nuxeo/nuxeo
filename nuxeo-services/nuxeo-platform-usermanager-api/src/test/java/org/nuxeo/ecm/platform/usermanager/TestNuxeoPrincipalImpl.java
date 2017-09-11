312df4cfe666ba35b68fa7a519fe97e1f9825d93
Thomas Roger <troger@nuxeo.com>
2017-09-11 11:50:27 +0200

    NXP-22827: format

--- nuxeo-services/nuxeo-platform-usermanager-api/src/test/java/org/nuxeo/ecm/platform/usermanager/TestNuxeoPrincipalImpl.java
+++ nuxeo-services/nuxeo-platform-usermanager-api/src/test/java/org/nuxeo/ecm/platform/usermanager/TestNuxeoPrincipalImpl.java
@@ -19,10 +19,12 @@
 
 package org.nuxeo.ecm.platform.usermanager;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Before;
 import org.junit.Test;
-import static org.junit.Assert.*;
-
 import org.nuxeo.runtime.test.NXRuntimeTestCase;
 
 /**
