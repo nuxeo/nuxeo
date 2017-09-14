edba49ee41ced9e4e6c54dee1b5dc1b247f93b63
Miguel Nixo <mnixo@nuxeo.com>
2017-09-14 17:44:35 +0100

    NXP-23040: add image2pdf commandline converter

---
+++ nuxeo-services/nuxeo-platform-convert/src/main/java/org/nuxeo/ecm/platform/convert/plugins/Image2PDFConverter.java
@@ -0,0 +1,91 @@
+/*
+ * (C) Copyright 2017 Nuxeo (http://nuxeo.com/) and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Contributors:
+ *     Miguel Nixo
+ */
+
+package org.nuxeo.ecm.platform.convert.plugins;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.io.FilenameUtils;
+import org.nuxeo.common.utils.Path;
+import org.nuxeo.ecm.core.api.Blob;
+import org.nuxeo.ecm.core.api.Blobs;
+import org.nuxeo.ecm.core.api.blobholder.BlobHolder;
+import org.nuxeo.ecm.core.convert.api.ConversionException;
+import org.nuxeo.ecm.core.convert.cache.SimpleCachableBlobHolder;
+import org.nuxeo.ecm.platform.commandline.executor.api.CmdParameters;
+
+public class Image2PDFConverter extends CommandLineBasedConverter {
+
+    @Override
+    protected BlobHolder buildResult(List<String> cmdOutput, CmdParameters cmdParams) {
+
+        String outputPath = cmdParams.getParameter("outDirPath");
+        File outputDir = new File(outputPath);
+        File[] files = outputDir.listFiles();
+        List<Blob> blobs = new ArrayList<>();
+
+        for (File file : files) {
+            try {
+                Blob blob = Blobs.createBlob(file);
+                blob.setFilename(file.getName());
+                blobs.add(blob);
+            } catch (IOException e) {
+                throw new ConversionException("Cannot create Blob", e);
+            }
+        }
+        return new SimpleCachableBlobHolder(blobs);
+    }
+
+    @Override
+    protected Map<String, Blob> getCmdBlobParameters(BlobHolder blobHolder, Map<String, Serializable> parameters)
+        throws ConversionException {
+
+        Map<String, Blob> cmdBlobParams = new HashMap<>();
+        cmdBlobParams.put("sourceFilePath", blobHolder.getBlob());
+        return cmdBlobParams;
+    }
+
+    @Override
+    protected Map<String, String> getCmdStringParameters(BlobHolder blobHolder, Map<String, Serializable> parameters)
+        throws ConversionException {
+
+        Map<String, String> cmdStringParams = new HashMap<>();
+
+        String baseDir = getTmpDirectory(parameters);
+        Path tmpPath = new Path(baseDir).append("image2pdf_" + System.currentTimeMillis());
+
+        File outDir = new File(tmpPath.toString());
+        boolean dirCreated = outDir.mkdir();
+        if (!dirCreated) {
+            throw new ConversionException("Unable to create tmp dir for transformer output");
+        }
+
+        cmdStringParams.put("outDirPath", outDir.getAbsolutePath());
+        cmdStringParams.put("targetFilePath", outDir.getAbsolutePath() + System.getProperty("file.separator")
+            + FilenameUtils.getBaseName(blobHolder.getBlob().getFilename()) + ".pdf");
+        return cmdStringParams;
+    }
+
+}
---
+++ nuxeo-services/nuxeo-platform-convert/src/test/java/org/nuxeo/ecm/platform/convert/tests/TestImageToPDF.java
@@ -0,0 +1,62 @@
+/*
+ * (C) Copyright 2017 Nuxeo (http://nuxeo.com/) and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Contributors:
+ *     Miguel Nixo
+ */
+
+package org.nuxeo.ecm.platform.convert.tests;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.Test;
+
+import org.nuxeo.ecm.core.api.Blob;
+import org.nuxeo.ecm.core.api.blobholder.BlobHolder;
+
+/**
+ * @since 5.2
+ */
+public class TestImageToPDF extends BaseConverterTest {
+
+    @Test
+    public void testConverter() throws Exception {
+        String converterName = cs.getConverterName("image/jpeg", "application/pdf");
+        assertEquals("image2pdf", converterName);
+
+        checkConverterAvailability(converterName);
+        checkCommandAvailability("imagetopdf");
+
+        BlobHolder pdfBH = getBlobFromPath("test-docs/hello.pdf");
+        Map<String, Serializable> parameters = new HashMap<>();
+        parameters.put("targetFilePath", "hello.pdf");
+
+        BlobHolder result = cs.convert(converterName, pdfBH, parameters);
+        assertNotNull(result);
+
+        List<Blob> blobs = result.getBlobs();
+        assertNotNull(blobs);
+        assertEquals(1, blobs.size());
+
+        Blob mainBlob = result.getBlob();
+        assertEquals("hello.pdf", mainBlob.getFilename());
+    }
+}
