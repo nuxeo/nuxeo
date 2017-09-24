c90869e5345a21516e248845b755fc46226d2b7c
Mincong HUANG <mhuang@nuxeo.com>
2017-09-24 17:22:25 +0200

    NXP-22260: Double-escape multi-valued Java options in nuxeoctl.bat
    ProcessBuilder in NuxeoLauncher add extra-quotes to each Java command
    argument. So in the upstream (nuxeoctl.bat), simple escape is not
    enough: double-escape is necessary. See also:
    
    - http://bugs.java.com/view_bug.do?bug_id=6511002
    - https://stackoverflow.com/questions/12124935

--- nuxeo-distribution/nuxeo-server-tomcat/src/main/resources/tomcat/bin/nuxeoctl.bat
+++ nuxeo-distribution/nuxeo-server-tomcat/src/main/resources/tomcat/bin/nuxeoctl.bat
@@ -206,7 +206,7 @@ if "%JAVA_VERSION%" lss "%REQUIRED_JAVA_VERSION%" (
 )
 
 if "!JAVA_OPTS!" == "" set JAVA_OPTS=-Xms512m -Xmx1024m -Djava.net.preferIPv4Stack=true -Dsun.rmi.dgc.client.gcInterval=3600000 -Dsun.rmi.dgc.server.gcInterval=3600000 -Dfile.encoding=UTF-8
-set JAVA_OPTS=%JAVA_OPTS:"=""%
+set JAVA_OPTS=%JAVA_OPTS:"=\\\"%
 
 REM ***** Add third-party packages from the installer to the path *****
 set PATH=%NUXEO_HOME%\3rdparty\ffmpeg\bin;%NUXEO_HOME%\3rdparty\ImageMagick;%PATH%;%NUXEO_HOME%\3rdparty\pdftohtml;%NUXEO_HOME%\3rdparty\gs\bin;%NUXEO_HOME%\3rdparty\misc\bin
