ea588ea621b45ccd3a5a0cbcde1c45bfda7ae303
Ricardo Dias <rdias@nuxeo.com>
2017-09-27 10:35:16 +0100

    NXP-23067: use soffice commandline in any2odt converter.

--- nuxeo-template-rendering-core/src/test/java/org/nuxeo/ecm/platform/template/tests/TestOOoConvert.java
+++ nuxeo-template-rendering-core/src/test/java/org/nuxeo/ecm/platform/template/tests/TestOOoConvert.java
@@ -31,37 +31,38 @@ public class TestOOoConvert extends BaseConverterTest {
 
     @Test
     public void testOfficeConverter() throws Exception {
-        BlobHolder bh = getBlobFromPath("data/testMe.html", "text/html");
-
         ConversionService cs = Framework.getLocalService(ConversionService.class);
 
+        BlobHolder bh = getBlobFromPath("data/testMe.html", "text/html");
         String converterName = cs.getConverterName(bh.getBlob().getMimeType(), ODT_MT);
         assertEquals("any2odt", converterName);
 
         if (cs.isConverterAvailable(converterName).isAvailable()) {
-
             BlobHolder result = cs.convert(converterName, bh, null);
-
             File odtFile = Framework.createTempFile("htmlfile", "odt");
-
             result.getBlob().transferTo(odtFile);
-
-            bh = getBlobFromPath("data/testMe.md", "text/x-web-markdown");
-            assertEquals("any2odt", converterName);
-
-            File odtFile2 = Framework.createTempFile("mdfile", "odt");
-
-            result = cs.convert(converterName, bh, null);
-            result.getBlob().transferTo(odtFile2);
-
             odtFile.delete();
-            odtFile2.delete();
         }
-
     }
 
     @Test
     public void testOfficeConverter2() throws Exception {
+        ConversionService cs = Framework.getLocalService(ConversionService.class);
+
+        BlobHolder bh = getBlobFromPath("data/testMe.md", "text/x-web-markdown");
+        String converterName = cs.getConverterName(bh.getBlob().getMimeType(), ODT_MT);
+        assertEquals("any2odt", converterName);
+
+        if (cs.isConverterAvailable(converterName).isAvailable()) {
+            BlobHolder result = cs.convert(converterName, bh, null);
+            File odtFile = Framework.createTempFile("mdfile", "odt");
+            result.getBlob().transferTo(odtFile);
+            odtFile.delete();
+        }
+    }
+
+    @Test
+    public void testOfficeConverter3() throws Exception {
 
         BlobHolder bh = getBlobFromPath("data/Spec_ModelNux.odt", "application/vnd.oasis.opendocument.text");
 
