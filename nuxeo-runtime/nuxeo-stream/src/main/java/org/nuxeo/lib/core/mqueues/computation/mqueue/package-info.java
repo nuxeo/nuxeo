92a90be8ec66722a43fca758ab6fd30c50c86ce5
Benoit Delbosc <bdelbosc@nuxeo.com>
2017-10-09 09:06:18 +0200

    NXP-23252: Copy source from nuxeo-mqueues

---
+++ nuxeo-runtime/nuxeo-stream/src/main/java/org/nuxeo/lib/core/mqueues/computation/mqueue/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Computation pattern with an implementation using MQueue.
+ */
+package org.nuxeo.lib.core.mqueues.computation.mqueue;
+
---
+++ nuxeo-runtime/nuxeo-stream/src/main/java/org/nuxeo/lib/core/mqueues/pattern/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Here is a producer consumer pattern that relies on MQueues.
+ *
+ * Producer and Consumer communicate using {@link org.nuxeo.lib.core.mqueues.pattern.Message}.
+ */
+package org.nuxeo.lib.core.mqueues.pattern;
+
