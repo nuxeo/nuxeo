c1d3856ec76037a427666ff035b400155da3dff7
Gethin James <gjames@nuxeo.com>
2017-10-10 16:35:27 +0200

    NXP-19817: Correct boolean parsing for metrics.log4j, Need to call activateOptions() when programmatically creating an appender.
    https://github.com/dropwizard/metrics/issues/454

--- nuxeo-runtime/nuxeo-runtime-metrics/src/main/java/org/nuxeo/runtime/metrics/MetricsDescriptor.java
+++ nuxeo-runtime/nuxeo-runtime-metrics/src/main/java/org/nuxeo/runtime/metrics/MetricsDescriptor.java
@@ -215,7 +215,7 @@ public class MetricsDescriptor implements Serializable {
         public static final String ENABLED_PROPERTY = "metrics.log4j.enabled";
 
         @XNode("@enabled")
-        protected boolean enabled = Boolean.getBoolean(Framework.getProperty(ENABLED_PROPERTY, "false"));
+        protected boolean enabled = Boolean.parseBoolean(Framework.getProperty(ENABLED_PROPERTY, "false"));
 
         private InstrumentedAppender appender;
 
@@ -230,6 +230,7 @@ public class MetricsDescriptor implements Serializable {
             }
             LogFactory.getLog(MetricsServiceImpl.class).info(this);
             appender = new InstrumentedAppender(registry);
+            appender.activateOptions();
             LogManager.getRootLogger().addAppender(appender);
         }
 
