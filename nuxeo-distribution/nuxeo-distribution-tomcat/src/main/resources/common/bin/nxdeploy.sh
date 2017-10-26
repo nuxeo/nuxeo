38dc02192cb8512d07b5d66a3156555b7bd38220
Jenkins Nuxeo <jenkins@nuxeo.com>
2017-10-26 08:26:20 +0000

    Release 7.10, update 7.10-HF33-SNAPSHOT to 7.10-HF33-I20171026_0823

--- nuxeo-distribution/nuxeo-distribution-tomcat/src/main/resources/common/bin/nxdeploy.sh
+++ nuxeo-distribution/nuxeo-distribution-tomcat/src/main/resources/common/bin/nxdeploy.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-java -DupdateWar=y -jar ../lib/nuxeo-runtime-tomcat-adapter-7.10-HF33-SNAPSHOT.jar
+java -DupdateWar=y -jar ../lib/nuxeo-runtime-tomcat-adapter-7.10-HF33-I20171026_0823.jar
  
