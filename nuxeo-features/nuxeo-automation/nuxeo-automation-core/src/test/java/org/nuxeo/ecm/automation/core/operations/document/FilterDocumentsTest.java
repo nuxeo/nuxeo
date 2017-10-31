1736a332b3bc18b25b9b8290fcb1ac09761fbc68
Arnaud Kervern <arnaud.k@gmail.com>
2017-10-31 15:44:59 +0100

    NXP-21954: Use collector in order to use condition on each doc

---
+++ nuxeo-features/nuxeo-automation/nuxeo-automation-core/src/test/java/org/nuxeo/ecm/automation/core/operations/document/FilterDocumentsTest.java
@@ -0,0 +1,68 @@
+/*
+ * (C) Copyright 2017 Nuxeo (http://nuxeo.com/) and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *  Contributors:
+ *     Arnaud Kervern
+ */
+package org.nuxeo.ecm.automation.core.operations.document;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.inject.Inject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.nuxeo.ecm.automation.AutomationService;
+import org.nuxeo.ecm.automation.OperationContext;
+import org.nuxeo.ecm.automation.OperationException;
+import org.nuxeo.ecm.core.api.CoreSession;
+import org.nuxeo.ecm.core.api.DocumentModelList;
+import org.nuxeo.ecm.core.test.CoreFeature;
+import org.nuxeo.ecm.core.test.DefaultRepositoryInit;
+import org.nuxeo.ecm.core.test.annotations.RepositoryConfig;
+import org.nuxeo.runtime.test.runner.Deploy;
+import org.nuxeo.runtime.test.runner.Features;
+import org.nuxeo.runtime.test.runner.FeaturesRunner;
+
+@RunWith(FeaturesRunner.class)
+@Features(CoreFeature.class)
+@Deploy("org.nuxeo.ecm.automation.core")
+@RepositoryConfig(init = DefaultRepositoryInit.class)
+public class FilterDocumentsTest {
+
+    @Inject
+    protected CoreSession session;
+
+    @Inject
+    protected AutomationService service;
+
+    @Test
+    public void testFilterDocuments() throws OperationException {
+        OperationContext ctx = new OperationContext(session);
+        DocumentModelList docs = session.query("Select * from Document");
+        assertThat(docs.size()).isNotEqualTo(1);
+
+        ctx.setInput(docs);
+
+        Map<String, Object> params = new HashMap<>();
+        params.put("condition", "Document.name == 'test'");
+        DocumentModelList filtered = (DocumentModelList) service.run(ctx, FilterDocuments.ID, params);
+
+        assertThat(filtered).hasSize(1);
+    }
+}
