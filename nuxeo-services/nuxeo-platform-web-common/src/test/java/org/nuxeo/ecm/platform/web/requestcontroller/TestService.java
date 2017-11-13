0c27d6c75fd4340fe70cbea0bcdb4b64f40759dc
Antoine Taillefer <ataillefer@nuxeo.com>
2017-11-13 10:00:24 +0100

    NXP-23439: Allow Picture preview refresh by not caching the /nxfile URLs

--- nuxeo-services/nuxeo-platform-web-common/src/test/java/org/nuxeo/ecm/platform/web/requestcontroller/TestService.java
+++ nuxeo-services/nuxeo-platform-web-common/src/test/java/org/nuxeo/ecm/platform/web/requestcontroller/TestService.java
@@ -26,29 +26,38 @@ import static org.junit.Assert.assertTrue;
 
 import java.util.Map;
 
+import javax.inject.Inject;
 import javax.servlet.FilterConfig;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.nuxeo.ecm.platform.web.common.requestcontroller.service.RequestControllerManager;
 import org.nuxeo.ecm.platform.web.common.requestcontroller.service.RequestControllerService;
 import org.nuxeo.ecm.platform.web.common.requestcontroller.service.RequestFilterConfig;
-import org.nuxeo.runtime.api.Framework;
 import org.nuxeo.runtime.test.NXRuntimeTestCase;
 
 public class TestService extends NXRuntimeTestCase {
+
+    @Inject
+    protected RequestControllerService requestControllerService;
 
     @Override
     @Before
     public void setUp() throws Exception {
         super.setUp();
         deployContrib("org.nuxeo.ecm.platform.web.common", "OSGI-INF/web-request-controller-framework.xml");
+        deployContrib("org.nuxeo.ecm.platform.web.common", "OSGI-INF/web-request-controller-contrib.xml");
+    }
+
+    @Override
+    public void tearDown() throws Exception {
+        undeployContrib("org.nuxeo.ecm.platform.web.common", "OSGI-INF/web-request-controller-contrib.xml");
+        undeployContrib("org.nuxeo.ecm.platform.web.common", "OSGI-INF/web-request-controller-framework.xml");
+        super.tearDown();
     }
 
     @Test
     public void testServiceRegistration() {
-        RequestControllerManager rcm = Framework.getLocalService(RequestControllerManager.class);
-        assertNotNull(rcm);
+        assertNotNull(requestControllerService);
     }
 
     @Test
@@ -56,62 +65,57 @@ public class TestService extends NXRuntimeTestCase {
         pushInlineDeployments(
                 "org.nuxeo.ecm.platform.web.common.test:OSGI-INF/web-request-controller-contrib-test.xml");
 
-        RequestControllerManager rcm = Framework.getLocalService(RequestControllerManager.class);
-        assertNotNull(rcm);
-
-        RequestControllerService rcmTest = (RequestControllerService) rcm;
-
         String uri;
         RequestFilterConfig config;
 
         uri = "/SyncNoTx/test";
-        config = rcmTest.computeConfigForRequest(uri);
+        config = requestControllerService.computeConfigForRequest(uri);
         assertTrue(config.needSynchronization());
         assertFalse(config.needTransaction());
 
         uri = "/NoSyncTx/test";
-        config = rcmTest.computeConfigForRequest(uri);
+        config = requestControllerService.computeConfigForRequest(uri);
         assertFalse(config.needSynchronization());
         assertTrue(config.needTransaction());
 
         uri = "/SyncTx/test";
-        config = rcmTest.computeConfigForRequest(uri);
+        config = requestControllerService.computeConfigForRequest(uri);
         assertTrue(config.needSynchronization());
         assertTrue(config.needTransaction());
 
         uri = "/SyncTx/deny";
-        config = rcmTest.computeConfigForRequest(uri);
+        config = requestControllerService.computeConfigForRequest(uri);
         assertFalse(config.needSynchronization());
         assertFalse(config.needTransaction());
 
         uri = "/whatever";
-        config = rcmTest.computeConfigForRequest(uri);
+        config = requestControllerService.computeConfigForRequest(uri);
         assertFalse(config.needSynchronization());
         assertFalse(config.needTransaction());
 
         uri = "/nuxeo/TestServlet/";
-        config = rcmTest.computeConfigForRequest(uri);
+        config = requestControllerService.computeConfigForRequest(uri);
         assertTrue(config.needSynchronization());
         assertTrue(config.needTransaction());
 
         uri = "/nuxeo/TestServlet";
-        config = rcmTest.computeConfigForRequest(uri);
+        config = requestControllerService.computeConfigForRequest(uri);
         assertTrue(config.needSynchronization());
         assertTrue(config.needTransaction());
 
         uri = "/nuxeo/TestServlet/toto";
-        config = rcmTest.computeConfigForRequest(uri);
+        config = requestControllerService.computeConfigForRequest(uri);
         assertTrue(config.needSynchronization());
         assertTrue(config.needTransaction());
 
         uri = "/nuxeo/CacheDefault";
-        config = rcmTest.computeConfigForRequest(uri);
+        config = requestControllerService.computeConfigForRequest(uri);
         assertFalse(config.isCached());
         assertFalse(config.isPrivate());
         assertEquals("3599", config.getCacheTime());
 
         uri = "/nuxeo/Cache";
-        config = rcmTest.computeConfigForRequest(uri);
+        config = requestControllerService.computeConfigForRequest(uri);
         assertTrue(config.isCached());
         assertTrue(config.isPrivate());
         assertEquals("3000", config.getCacheTime());
@@ -122,22 +126,18 @@ public class TestService extends NXRuntimeTestCase {
         pushInlineDeployments(
                 "org.nuxeo.ecm.platform.web.common.test:OSGI-INF/web-request-controller-contrib-test.xml");
 
-        RequestControllerManager rcm = Framework.getLocalService(RequestControllerManager.class);
-        assertNotNull(rcm);
-
         String uri;
         FilterConfig fc;
 
-        RequestControllerService rcs = (RequestControllerService) rcm;
-        assertNull(rcs.computeCorsFilterConfigForUri("/dummy/uri"));
+        assertNull(requestControllerService.computeCorsFilterConfigForUri("/dummy/uri"));
 
         uri = "/nuxeo/site/minimal/something/long/dummy.html";
-        fc = rcs.computeCorsFilterConfigForUri(uri);
+        fc = requestControllerService.computeCorsFilterConfigForUri(uri);
         assertEquals("-1", fc.getInitParameter("cors.maxAge"));
         assertEquals(null, fc.getInitParameter("cors.allowOrigin"));
 
         uri = "/nuxeo/site/dummy/";
-        fc = rcs.computeCorsFilterConfigForUri(uri);
+        fc = requestControllerService.computeCorsFilterConfigForUri(uri);
         assertEquals("3600", fc.getInitParameter("cors.maxAge"));
         assertEquals("http://example.com http://example.com:8080", fc.getInitParameter("cors.allowOrigin"));
         assertEquals("false", fc.getInitParameter("cors.supportsCredentials"));
@@ -148,13 +148,34 @@ public class TestService extends NXRuntimeTestCase {
         pushInlineDeployments(
                 "org.nuxeo.ecm.platform.web.common.test:OSGI-INF/web-request-controller-contrib-test.xml");
 
-        RequestControllerManager rcm = Framework.getLocalService(RequestControllerManager.class);
-        assertNotNull(rcm);
-
-        Map<String, String> rh = rcm.getResponseHeaders();
-        assertEquals(2, rh.size());
+        Map<String, String> rh = requestControllerService.getResponseHeaders();
+        assertEquals(7, rh.size());
         assertTrue(rh.containsKey("WWW-Authenticate"));
         assertEquals("basic", rh.get("WWW-Authenticate"));
+    }
+
+    /**
+     * @since 9.3
+     */
+    @Test
+    public void testNXFilePatterns() {
+        // file:content
+        RequestFilterConfig config = requestControllerService.computeConfigForRequest(
+                "/nuxeo/nxfile/default/45ad4c71-f1f3-4e9d-97a1-61aa317ce105/file:content/IMG_20170910_110134.jpg");
+        assertNotNull(config);
+        assertFalse(config.isCached());
+        assertFalse(config.isPrivate());
+        assertFalse(config.needTransaction());
+        assertFalse(config.needSynchronization());
+
+        // picture:views
+        config = requestControllerService.computeConfigForRequest(
+                "/nuxeo/nxfile/default/45ad4c71-f1f3-4e9d-97a1-61aa317ce105/picture:views/3/content/FullHD_IMG_20170910_110134.jpg");
+        assertNotNull(config);
+        assertFalse(config.isCached());
+        assertFalse(config.isPrivate());
+        assertFalse(config.needTransaction());
+        assertFalse(config.needSynchronization());
     }
 
 }
