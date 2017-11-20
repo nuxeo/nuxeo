cc8db9efe31bdb46407dafbf290d950ab873ced6
Funsho David <fdavid@nuxeo.com>
2017-11-20 18:12:50 +0100

    NXP-23400: add facet in document type contribution for non core types

--- src/test/java/org/nuxeo/binary/metadata/test/TestBinaryMetadataSyncListener.java
+++ src/test/java/org/nuxeo/binary/metadata/test/TestBinaryMetadataSyncListener.java
@@ -49,7 +49,7 @@ import org.nuxeo.runtime.test.runner.LocalDeploy;
 @Features(BinaryMetadataFeature.class)
 @Deploy({ "org.nuxeo.ecm.platform.picture.api", "org.nuxeo.ecm.platform.picture.core",
         "org.nuxeo.ecm.platform.picture.convert", "org.nuxeo.ecm.platform.rendition.core",
-        "org.nuxeo.ecm.automation.core" })
+        "org.nuxeo.ecm.automation.core", "org.nuxeo.ecm.platform.tag" })
 @LocalDeploy({ "org.nuxeo.binary.metadata:binary-metadata-contrib-test.xml",
         "org.nuxeo.binary.metadata:binary-metadata-contrib-pdf-test.xml",
         "org.nuxeo.binary.metadata:binary-metadata-contrib-provider.xml"})
