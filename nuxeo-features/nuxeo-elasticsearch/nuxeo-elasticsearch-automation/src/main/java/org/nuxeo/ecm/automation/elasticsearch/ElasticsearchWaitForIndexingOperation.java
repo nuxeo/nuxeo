6f43c20137e8c45902250e82308572a678b0baab
Kevin Leturc <kleturc@nuxeo.com>
2017-12-01 15:24:08 +0100

    NXP-23725: Update ES wait for indexing operation to wait for audit

--- nuxeo-features/nuxeo-elasticsearch/nuxeo-elasticsearch-automation/src/main/java/org/nuxeo/ecm/automation/elasticsearch/ElasticsearchWaitForIndexingOperation.java
+++ nuxeo-features/nuxeo-elasticsearch/nuxeo-elasticsearch-automation/src/main/java/org/nuxeo/ecm/automation/elasticsearch/ElasticsearchWaitForIndexingOperation.java
@@ -18,9 +18,13 @@
  */
 package org.nuxeo.ecm.automation.elasticsearch;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-import org.nuxeo.ecm.automation.OperationContext;
+import static java.lang.Long.max;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+import org.nuxeo.common.utils.ExceptionUtils;
 import org.nuxeo.ecm.automation.core.Constants;
 import org.nuxeo.ecm.automation.core.annotations.Context;
 import org.nuxeo.ecm.automation.core.annotations.Operation;
@@ -28,14 +32,10 @@ import org.nuxeo.ecm.automation.core.annotations.OperationMethod;
 import org.nuxeo.ecm.automation.core.annotations.Param;
 import org.nuxeo.ecm.core.api.CoreSession;
 import org.nuxeo.ecm.core.work.api.WorkManager;
+import org.nuxeo.ecm.platform.audit.api.AuditLogger;
+import org.nuxeo.elasticsearch.ElasticSearchConstants;
 import org.nuxeo.elasticsearch.api.ElasticSearchAdmin;
 import org.nuxeo.runtime.api.Framework;
-
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
-
-import static java.lang.Long.max;
 
 /**
  * Wait for Elasticsearch indexing background job
@@ -49,10 +49,10 @@ public class ElasticsearchWaitForIndexingOperation {
 
     public static final String ID = "Elasticsearch.WaitForIndexing";
 
-    private static final Log log = LogFactory.getLog(Log.class);
-
-    @Context
-    protected OperationContext ctx;
+    /**
+     * @since 9.10
+     */
+    public static final String AUDIT_ELASTICSEARCH_ENABLED = "audit.elasticsearch.enabled";
 
     @Context
     protected ElasticSearchAdmin esa;
@@ -66,24 +66,35 @@ public class ElasticsearchWaitForIndexingOperation {
     @Param(name = "refresh", required = false)
     protected Boolean refresh = false;
 
+    @Param(name = "waitForAudit", required = false)
+    protected Boolean waitForAudit = false;
+
     @OperationMethod
     public Boolean run() {
         long start = System.currentTimeMillis();
         WorkManager workManager = Framework.getService(WorkManager.class);
+        AuditLogger auditLogger = Framework.getService(AuditLogger.class);
+        boolean waitForAuditStoredInEs = waitForAudit && Framework.isBooleanPropertyTrue(AUDIT_ELASTICSEARCH_ENABLED);
         try {
             if (!workManager.awaitCompletion(timeout, TimeUnit.SECONDS)) {
                 throw new TimeoutException();
             }
+            if (waitForAuditStoredInEs && !auditLogger.await(computeRemainingTime(start), TimeUnit.SECONDS)) {
+                throw new TimeoutException();
+            }
             esa.prepareWaitForIndexing().get(computeRemainingTime(start), TimeUnit.SECONDS);
-        } catch (TimeoutException e) {
-            return Boolean.FALSE;
-        } catch (InterruptedException e) {
-            return Boolean.FALSE;
-        } catch (ExecutionException e) {
+        } catch (TimeoutException | InterruptedException | ExecutionException e) {
+            if (ExceptionUtils.hasInterruptedCause(e)) {
+                // reset interrupted status
+                Thread.currentThread().interrupt();
+            }
             return Boolean.FALSE;
         }
         if (refresh) {
             esa.refreshRepositoryIndex(repo.getRepositoryName());
+            if (waitForAuditStoredInEs) {
+                esa.getClient().refresh(esa.getIndexNameForType(ElasticSearchConstants.ENTRY_TYPE));
+            }
         }
         return Boolean.TRUE;
     }
