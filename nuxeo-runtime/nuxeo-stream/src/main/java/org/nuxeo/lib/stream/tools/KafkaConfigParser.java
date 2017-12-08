b6909e8b65dfdea54fa6d71b4e3624d27785bd07
Benoit Delbosc <bdelbosc@nuxeo.com>
2017-12-08 14:40:30 +0100

    NXP-23757: make stream tools support kafka, add copy and help command and a stream.sh script

---
+++ nuxeo-runtime/nuxeo-stream/src/main/java/org/nuxeo/lib/stream/tools/KafkaConfigParser.java
@@ -0,0 +1,111 @@
+/*
+ * (C) Copyright 2017 Nuxeo SA (http://nuxeo.com/) and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Contributors:
+ *     bdelbosc
+ */
+package org.nuxeo.lib.stream.tools;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.Properties;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+/**
+ * Parse an xml file describing Kafka configurations, the format is the one used by KafkaConfigDescriptor. We can not
+ * use the Nuxeo descriptor directly because we are in a library without Nuxeo dependency.
+ *
+ * @since 9.10
+ */
+public class KafkaConfigParser {
+
+    protected String zkServers;
+
+    private Properties producerProperties;
+
+    private Properties consumerProperties;
+
+    private String prefix;
+
+    public KafkaConfigParser(Path path, String configName) {
+        try {
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            Document document = builder.parse(path.toFile());
+            NodeList nodes = document.getElementsByTagName("kafkaConfig");
+            for (int i = 0; i < nodes.getLength(); i++) {
+                Node node = nodes.item(i);
+                String name = node.getAttributes().getNamedItem("name").getNodeValue();
+                if (configName.equals(name)) {
+                    parseConfig(node);
+                    return;
+                }
+            }
+        } catch (SAXException | IOException | ParserConfigurationException e) {
+            throw new IllegalArgumentException("Invalid Kafka config file: " + path, e);
+        }
+        throw new IllegalArgumentException(String.format("Config: %s not found in file: %s", configName, path));
+    }
+
+    protected void parseConfig(Node node) {
+        prefix = node.getAttributes().getNamedItem("topicPrefix").getNodeValue();
+        zkServers = node.getAttributes().getNamedItem("zkServers").getNodeValue();
+        NodeList children = node.getChildNodes();
+        for (int i = 0; i < children.getLength(); i++) {
+            Node child = children.item(i);
+            if ("producer".equals(child.getNodeName())) {
+                producerProperties = decodeProperties(child);
+            } else if ("consumer".equals(child.getNodeName())) {
+                consumerProperties = decodeProperties(child);
+            }
+        }
+    }
+
+    protected Properties decodeProperties(Node node) {
+        NodeList children = node.getChildNodes();
+        Properties ret = new Properties();
+        for (int i = 0; i < children.getLength(); i++) {
+            Node child = children.item(i);
+            if ("property".equals(child.getNodeName())) {
+                ret.put(child.getAttributes().getNamedItem("name").getNodeValue(), child.getTextContent());
+            }
+        }
+        return ret;
+    }
+
+    public String getZkServers() {
+        return zkServers;
+    }
+
+    public Properties getProducerProperties() {
+        return producerProperties;
+    }
+
+    public Properties getConsumerProperties() {
+        return consumerProperties;
+    }
+
+    public String getPrefix() {
+        return prefix;
+    }
+}
---
+++ nuxeo-runtime/nuxeo-stream/src/main/java/org/nuxeo/lib/stream/tools/command/HelpCommand.java
@@ -0,0 +1,74 @@
+/*
+ * (C) Copyright 2017 Nuxeo SA (http://nuxeo.com/) and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Contributors:
+ *     bdelbosc
+ */
+package org.nuxeo.lib.stream.tools.command;
+
+import java.util.List;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Options;
+import org.nuxeo.lib.stream.log.LogManager;
+import org.nuxeo.lib.stream.tools.CommandRegistry;
+
+/**
+ * @since 9.3
+ */
+public class HelpCommand extends Command {
+
+    protected static final String NAME = "help";
+
+    protected Options options;
+
+    @Override
+    public String name() {
+        return NAME;
+    }
+
+    @Override
+    public void updateOptions(Options options) {
+        this.options = options;
+    }
+
+    @Override
+    public boolean run(LogManager manager, CommandLine cmd) throws InterruptedException {
+        List<Command> commands = new CommandRegistry().commands();
+        displayCommonOptions();
+        if (cmd.getArgList().isEmpty()) {
+            commands.stream().filter(command -> !(command instanceof HelpCommand)).forEach(this::displayHelp);
+        } else {
+            String name = cmd.getArgList().get(0);
+            commands.stream().filter(command -> name.equals(command.name())).forEach(this::displayHelp);
+        }
+        return true;
+    }
+
+    protected void displayCommonOptions() {
+        HelpFormatter formatter = new HelpFormatter();
+        formatter.setSyntaxPrefix("");
+        formatter.printHelp("Usage: stream.sh [COMMAND] [Options]\nCommon options:", options);
+    }
+
+    protected void displayHelp(Command command) {
+        Options cmdOptions = new Options();
+        command.updateOptions(cmdOptions);
+        HelpFormatter formatter = new HelpFormatter();
+        formatter.setSyntaxPrefix("Command: ");
+        formatter.printHelp(command.name(), cmdOptions);
+    }
+}
