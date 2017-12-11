cfcad13e5369a3fd4bb4cbbf9a51a3109b212bb4
Thomas Roger <troger@nuxeo.com>
2017-12-11 15:20:17 +0100

    NXP-23909: ignore not found operations when building documentation

---
+++ nuxeo-features/nuxeo-automation/nuxeo-automation-core/src/test/java/org/nuxeo/ecm/automation/core/test/TestAutomationDocumentation.java
@@ -0,0 +1,53 @@
+/*
+ * (C) Copyright 2017 Nuxeo (http://nuxeo.com/) and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Contributors:
+ *     Thomas Roger
+ *
+ */
+package org.nuxeo.ecm.automation.core.test;
+
+import static org.junit.Assert.assertFalse;
+
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.nuxeo.ecm.automation.AutomationService;
+import org.nuxeo.ecm.automation.OperationDocumentation;
+import org.nuxeo.ecm.automation.OperationException;
+import org.nuxeo.ecm.core.test.CoreFeature;
+import org.nuxeo.runtime.test.runner.Deploy;
+import org.nuxeo.runtime.test.runner.Features;
+import org.nuxeo.runtime.test.runner.FeaturesRunner;
+import org.nuxeo.runtime.test.runner.LocalDeploy;
+
+@RunWith(FeaturesRunner.class)
+@Features(CoreFeature.class)
+@Deploy("org.nuxeo.ecm.automation.core")
+public class TestAutomationDocumentation {
+
+    @Inject
+    AutomationService service;
+
+    @Test
+    @LocalDeploy("org.nuxeo.ecm.automation.core:test-chain-op-not-found.xml")
+    public void testDocumentationWithNotFoundOperation() throws OperationException {
+        List<OperationDocumentation> documentation = service.getDocumentation();
+        assertFalse(documentation.isEmpty());
+    }
+}
