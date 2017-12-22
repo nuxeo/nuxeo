25b4300962bb6bd673e85a7dc405f69b04242160
Kevin Leturc <kleturc@nuxeo.com>
2017-12-22 10:44:28 +0100

    NXP-24023: Fix event options passed to events when following transition

--- nuxeo-core/nuxeo-core/src/main/java/org/nuxeo/ecm/core/api/AbstractSession.java
+++ nuxeo-core/nuxeo-core/src/main/java/org/nuxeo/ecm/core/api/AbstractSession.java
@@ -2092,7 +2092,7 @@ public abstract class AbstractSession implements CoreSession, Serializable {
         doc.followTransition(transition);
 
         // Construct a map holding meta information about the event.
-        Map<String, Serializable> eventOptions = new HashMap<>();
+        Map<String, Serializable> eventOptions = new HashMap<>(options);
         eventOptions.put(org.nuxeo.ecm.core.api.LifeCycleConstants.TRANSTION_EVENT_OPTION_FROM, formerStateName);
         eventOptions.put(org.nuxeo.ecm.core.api.LifeCycleConstants.TRANSTION_EVENT_OPTION_TO, doc.getLifeCycleState());
         eventOptions.put(org.nuxeo.ecm.core.api.LifeCycleConstants.TRANSTION_EVENT_OPTION_TRANSITION, transition);
