d7596c353aae1d65a22edf0af7c35ce56804b588
Kevin Leturc <kleturc@nuxeo.com>
2018-01-03 15:11:16 +0100

    NXP-24090: Replace commons-lang by commons-lang3

--- nuxeo-platform-signature-core/src/main/java/org/nuxeo/ecm/platform/signature/core/sign/SignatureServiceImpl.java
+++ nuxeo-platform-signature-core/src/main/java/org/nuxeo/ecm/platform/signature/core/sign/SignatureServiceImpl.java
@@ -40,7 +40,7 @@ import java.util.List;
 import java.util.Map;
 
 import org.apache.commons.io.FilenameUtils;
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.nuxeo.ecm.core.api.Blob;
