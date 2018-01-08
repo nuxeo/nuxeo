6f812e2cdea07cdb42afa8980ea9cb9ae91f5153
Benoit Delbosc <bdelbosc@nuxeo.com>
2018-01-08 17:21:31 +0100

    NXP-24009: fix kafka bootstrap default port

--- integration/Jenkinsfiles/jenkins-slave-kafka/files/setup-kafka-properties.sh
+++ integration/Jenkinsfiles/jenkins-slave-kafka/files/setup-kafka-properties.sh
@@ -3,6 +3,6 @@
 [ -f /etc/kafka.properties ] && exit 0
 
 cat <<! > /etc/kafka.properties
-kafka.bootstrap.servers=${KAFKA_ADVERTISED_HOST_NAME:-localhost}:${KAFKA_ADVERTISED_PORT:-9902}
+kafka.bootstrap.servers=${KAFKA_ADVERTISED_HOST_NAME:-localhost}:${KAFKA_ADVERTISED_PORT:-9092}
 kafka.zkServers=${KAFKA_ZOOKEEPER_CONNECT:-localhost:2181}
 !
