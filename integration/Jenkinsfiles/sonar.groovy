10c915d732c8faef485e7bab093efddf17918651
Julien Carsique <jcarsique@nuxeo.com>
2018-01-23 18:44:33 +0100

    NXP-24011: update Sonar pipeline
    Add mandatory Discard Old Builds
    Set back job properties removed by the properties step
    - Build parameters
    - Do not allow concurrent builds
    - GitHub project page
    - Build triggers
    - Rebuild Settings
    
    Shared lib moved to nuxeo-pipeline-library
    - follow withBuildStatus() API change
    
    Follow PR comments:
    - use ${params.*}
    - don't do shallow clone
    - remove redundant junit path
    
    Upgrades:
    - org.jacoco:jacoco-maven-plugin 0.7.9 -> 0.8.0
    - org.sonarsource.scanner.maven:sonar-maven-plugin 3.2 -> 3.4.0.905

--- integration/Jenkinsfiles/sonar.groovy
+++ integration/Jenkinsfiles/sonar.groovy
@@ -1,5 +1,5 @@
 /*
- * (C) Copyright 2017 Nuxeo (http://nuxeo.com/) and others.
+ * (C) Copyright 2017-2018 Nuxeo (http://nuxeo.com/) and others.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,41 +15,41 @@
  *
  * Contributors:
  *     jcarsique
+ *
+ * SonarQube analysis for https://sonarcloud.io/dashboard?id=org.nuxeo%3Anuxeo-ecm
  */
 
- node('SLAVE') {
+properties([
+    [$class: 'BuildDiscarderProperty', strategy: [$class: 'LogRotator', daysToKeepStr: '60', numToKeepStr: '60', artifactNumToKeepStr: '1']],
+    disableConcurrentBuilds(),
+    [$class: 'GithubProjectProperty', displayName: '', projectUrlStr: 'https://github.com/nuxeo/nuxeo/'],
+    [$class: 'RebuildSettings', autoRebuild: false, rebuildDisabled: false],
+    [$class: 'ParametersDefinitionProperty', parameterDefinitions: [
+        [$class: 'StringParameterDefinition', defaultValue: 'master', description: '', name: 'BRANCH'],
+        [$class: 'StringParameterDefinition', defaultValue: 'master', description: '', name: 'PARENT_BRANCH']]],
+    pipelineTriggers([[$class: 'GitHubPushTrigger']])
+  ])
+
+node('SLAVE') {
     tool type: 'ant', name: 'ant-1.9'
     tool type: 'hudson.model.JDK', name: 'java-8-oracle'
     tool type: 'hudson.tasks.Maven$MavenInstallation', name: 'maven-3'
     timeout(time: 5, unit: 'HOURS') {
         timestamps {
             stage('clone') {
-                checkout(
-                    [$class: 'GitSCM',
-                        branches: [[name: '*/${BRANCH}']],
-                        browser: [$class: 'GithubWeb', repoUrl: 'https://github.com/nuxeo/nuxeo'],
-                        doGenerateSubmoduleConfigurations: false,
-                        extensions: [
-                            [$class: 'WipeWorkspace'],
-                            [$class: 'CleanBeforeCheckout'],
-                            [$class: 'CloneOption', depth: 5, noTags: true, reference: '', shallow: true]
-                        ],
-                        submoduleCfg: [],
-                        userRemoteConfigs: [[url: 'git@github.com:nuxeo/nuxeo.git']]
-                    ])
+                git branch: '$BRANCH', url: 'git://github.com/nuxeo/nuxeo.git'
                 sh """#!/bin/bash -xe
                       ./clone.py $BRANCH -f $PARENT_BRANCH
                 """
-                shared = load("$WORKSPACE/integration/Jenkinsfiles/shared.groovy")
                 sha = sh(returnStdout: true, script: 'git rev-parse HEAD').trim()
             }
             try {
                 stage('analysis') {
-                    shared.withBuildStatus("sonarqube", sha) {
+                    withBuildStatus("mvn verify sonar", "https://github.com/nuxeo/nuxeo", sha, BUILD_URL) {
                         withEnv(['MAVEN_OPTS=-Xmx6g -server']) {
                             withCredentials([usernamePassword(credentialsId: 'c4ced779-af65-4bce-9551-4e6c0e0dcfe5', passwordVariable: 'SONARCLOUD_PWD', usernameVariable: '')]) {
                                 sh """#!/bin/bash -ex
-                                    mvn clean install org.sonarsource.scanner.maven:sonar-maven-plugin:sonar -Dsonar.login=$SONARCLOUD_PWD -Dit.jacoco.destFile=$WORKSPACE/target/jacoco-it.dump -Paddons,distrib,qa,sonar
+                                    mvn clean verify sonar:sonar -Dsonar.login=$SONARCLOUD_PWD -Paddons,distrib,qa,sonar
                                 """
                             }
                         }
@@ -57,9 +57,9 @@
                 }
             } finally {
                 archive '**/target/failsafe-reports/*, **/target/*.png, **/target/**/*.log, **/target/**/log/*'
-                junit testDataPublishers: [[$class: 'ClaimTestDataPublisher']], '**/target/surefire-reports/*.xml, **/target/failsafe-reports/*.xml, **/target/failsafe-reports/**/*.xml'
-                shared.warningsPublisher()
-                shared.claimPublisher()
+                junit testDataPublishers: [[$class: 'ClaimTestDataPublisher']], testResults: '**/target/surefire-reports/*.xml, **/target/failsafe-reports/**/*.xml'
+                warningsPublisher()
+                claimPublisher()
             }
         }
     }
