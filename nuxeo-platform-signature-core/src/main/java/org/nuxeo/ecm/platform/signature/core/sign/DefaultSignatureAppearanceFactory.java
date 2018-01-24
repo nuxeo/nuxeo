3e109b14450837155b35ddf5328810bbfc2fe7df
Florent Guillaume <fguillaume@nuxeo.com>
2018-01-24 18:32:00 +0100

    NXP-24228: upgrade iText

--- nuxeo-platform-signature-core/src/main/java/org/nuxeo/ecm/platform/signature/core/sign/DefaultSignatureAppearanceFactory.java
+++ nuxeo-platform-signature-core/src/main/java/org/nuxeo/ecm/platform/signature/core/sign/DefaultSignatureAppearanceFactory.java
@@ -18,8 +18,6 @@
  */
 package org.nuxeo.ecm.platform.signature.core.sign;
 
-import java.awt.Color;
-
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.nuxeo.ecm.core.api.DocumentModel;
@@ -28,9 +26,10 @@ import org.nuxeo.ecm.platform.signature.api.sign.SignatureLayout;
 import org.nuxeo.ecm.platform.signature.api.sign.SignatureService;
 import org.nuxeo.runtime.api.Framework;
 
-import com.lowagie.text.Font;
-import com.lowagie.text.FontFactory;
-import com.lowagie.text.pdf.PdfSignatureAppearance;
+import com.itextpdf.text.BaseColor;
+import com.itextpdf.text.Font;
+import com.itextpdf.text.FontFactory;
+import com.itextpdf.text.pdf.PdfSignatureAppearance;
 
 public class DefaultSignatureAppearanceFactory implements SignatureAppearanceFactory {
 
@@ -40,11 +39,11 @@ public class DefaultSignatureAppearanceFactory implements SignatureAppearanceFac
     public void format(PdfSignatureAppearance pdfSignatureAppearance, DocumentModel doc, String principal, String reason) {
         pdfSignatureAppearance.setReason(reason);
         pdfSignatureAppearance.setAcro6Layers(true);
-        pdfSignatureAppearance.setRender(PdfSignatureAppearance.SignatureRenderDescription);
+        pdfSignatureAppearance.setRenderingMode(PdfSignatureAppearance.RenderingMode.DESCRIPTION);
         SignatureService service = Framework.getService(SignatureService.class);
         SignatureLayout layout = ((SignatureServiceImpl)service).getSignatureLayout();
-        Font layer2Font = FontFactory.getFont(FontFactory.TIMES, layout.getTextSize(), Font.NORMAL, new Color(
-                        0x00, 0x00, 0x00));
+        Font layer2Font = FontFactory.getFont(FontFactory.TIMES, (float) layout.getTextSize(), Font.NORMAL,
+                BaseColor.BLACK);
         pdfSignatureAppearance.setLayer2Font(layer2Font);
     }
 
