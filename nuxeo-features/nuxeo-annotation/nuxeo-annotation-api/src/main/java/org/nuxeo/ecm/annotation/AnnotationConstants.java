c8fe00b15939f9e1d9e2e6c04a5385d833c63292
Funsho David <fdavid@nuxeo.com>
2018-01-29 16:59:01 +0100

    NXP-24103: annotation service implementation

---
+++ nuxeo-features/nuxeo-annotation/nuxeo-annotation-api/src/main/java/org/nuxeo/ecm/annotation/AnnotationConstants.java
@@ -0,0 +1,60 @@
+/*
+ * (C) Copyright 2018 Nuxeo (http://nuxeo.com/) and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Contributors:
+ *     Funsho David
+ *
+ */
+
+package org.nuxeo.ecm.annotation;
+
+/**
+ * @since 10.1
+ */
+public class AnnotationConstants {
+
+    private AnnotationConstants() {
+        // utility class
+    }
+
+    public static final String ANNOTATION_DOC_TYPE = "Annotation";
+
+    public static final String ANNOTATION_COLOR_PROPERTY = "annotation:color";
+
+    public static final String ANNOTATION_DATE_PROPERTY = "annotation:date";
+
+    public static final String ANNOTATION_FLAGS_PROPERTY = "annotation:flags";
+
+    public static final String ANNOTATION_NAME_PROPERTY = "annotation:name";
+
+    public static final String ANNOTATION_DOCUMENT_ID_PROPERTY = "annotation:documentId";
+
+    public static final String ANNOTATION_XPATH_PROPERTY = "annotation:xpath";
+
+    public static final String ANNOTATION_LAST_MODIFIER_PROPERTY = "annotation:lastModifier";
+
+    public static final String ANNOTATION_PAGE_PROPERTY = "annotation:page";
+
+    public static final String ANNOTATION_POSITION_PROPERTY = "annotation:position";
+
+    public static final String ANNOTATION_CREATION_DATE_PROPERTY = "annotation:creationDate";
+
+    public static final String ANNOTATION_OPACITY_PROPERTY = "annotation:opacity";
+
+    public static final String ANNOTATION_SUBJECT_PROPERTY = "annotation:subject";
+
+    public static final String ANNOTATION_SECURITY_PROPERTY = "annotation:security";
+
+}
