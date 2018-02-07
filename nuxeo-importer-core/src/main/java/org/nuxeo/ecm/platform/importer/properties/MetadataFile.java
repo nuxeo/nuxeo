4c44a5a97c1ed7908b28462fc1991805f2d60eba
Kevin Leturc <kleturc@nuxeo.com>
2018-02-07 18:48:19 +0100

    NXP-24032: Replace life cycle deleted check by isTrashed

--- nuxeo-importer-core/src/main/java/org/nuxeo/ecm/platform/importer/properties/MetadataFile.java
+++ nuxeo-importer-core/src/main/java/org/nuxeo/ecm/platform/importer/properties/MetadataFile.java
@@ -31,7 +31,6 @@ import java.util.Properties;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.nuxeo.common.utils.StringUtils;
 import org.nuxeo.ecm.core.api.DataModel;
 import org.nuxeo.ecm.core.api.DocumentModel;
 import org.nuxeo.ecm.core.api.NuxeoException;
@@ -170,7 +169,7 @@ public class MetadataFile {
     }
 
     protected void load() {
-        for (String schema : doc.getDeclaredSchemas()) {
+        for (String schema : doc.getSchemas()) {
             addAllProperties(schema);
         }
     }
