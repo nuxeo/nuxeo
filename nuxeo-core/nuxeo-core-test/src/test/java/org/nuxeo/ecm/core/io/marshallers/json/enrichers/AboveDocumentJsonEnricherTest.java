a2e580981efc572cf5729b7d57cb885e1b30e6ce
Guillaume Renard <grenard@nuxeo.com>
2018-02-22 13:21:16 +0100

    NXP-24282: add Trash and Untrash operation + firstAccessibleAncestor Json Enricher

---
+++ nuxeo-core/nuxeo-core-test/src/test/java/org/nuxeo/ecm/core/io/marshallers/json/enrichers/AboveDocumentJsonEnricherTest.java
@@ -0,0 +1,71 @@
+/*
+ * (C) Copyright 2016 Nuxeo SA (http://nuxeo.com/) and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Contributors:
+ *     Guillaume Renard <grenard@nuxeo.com>
+ */
+
+package org.nuxeo.ecm.core.io.marshallers.json.enrichers;
+
+import javax.inject.Inject;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.nuxeo.ecm.core.api.CoreSession;
+import org.nuxeo.ecm.core.api.DocumentModel;
+import org.nuxeo.ecm.core.api.PathRef;
+import org.nuxeo.ecm.core.io.marshallers.json.AbstractJsonWriterTest;
+import org.nuxeo.ecm.core.io.marshallers.json.JsonAssert;
+import org.nuxeo.ecm.core.io.marshallers.json.document.DocumentModelJsonWriter;
+import org.nuxeo.ecm.core.io.registry.context.RenderingContext.CtxBuilder;
+import org.nuxeo.runtime.test.runner.Deploy;
+import org.nuxeo.runtime.transaction.TransactionHelper;
+
+/**
+ * @since 10.1
+ */
+@Deploy("org.nuxeo.ecm.core.io:OSGI-INF/doc-type-contrib.xml")
+public class AboveDocumentJsonEnricherTest extends AbstractJsonWriterTest.Local<DocumentModelJsonWriter, DocumentModel> {
+
+    public AboveDocumentJsonEnricherTest() {
+        super(DocumentModelJsonWriter.class, DocumentModel.class);
+    }
+
+    @Inject
+    private CoreSession session;
+
+    @Before
+    public void setup() {
+        DocumentModel document = session.createDocumentModel("/", "child1", "MyFolder");
+        document = session.createDocument(document);
+        document = session.createDocumentModel("/child1", "child2", "MyFolder");
+        document = session.createDocument(document);
+    }
+
+    @Test
+    public void test() throws Exception {
+        TransactionHelper.commitOrRollbackTransaction();
+        TransactionHelper.startTransaction();
+
+        DocumentModel root = session.getDocument(new PathRef("/child1/child2"));
+        JsonAssert json = jsonAssert(root, CtxBuilder.enrichDoc(FirstAccessibleAncestorJsonEnricher.NAME).get());
+        json = json.has("contextParameters").isObject();
+        json.properties(1);
+        json = json.has(FirstAccessibleAncestorJsonEnricher.NAME).isObject();
+        json = json.has("path");
+        json.isEquals("/child1");
+    }
+
+}
