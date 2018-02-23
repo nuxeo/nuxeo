4606ceefbf121702ed8f11a85b900a772b1154cc
Florent Guillaume <fguillaume@nuxeo.com>
2018-02-23 14:19:29 +0100

    NXP-24331: fix tests: don't use static fields in a service

--- nuxeo-services/nuxeo-platform-web-common/src/test/java/org/nuxeo/ecm/platform/web/requestcontroller/TestService.java
+++ nuxeo-services/nuxeo-platform-web-common/src/test/java/org/nuxeo/ecm/platform/web/requestcontroller/TestService.java
@@ -32,94 +32,91 @@ import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
+import javax.inject.Inject;
 import javax.servlet.http.HttpServletRequest;
 
-import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.nuxeo.ecm.platform.web.common.requestcontroller.service.RequestControllerManager;
 import org.nuxeo.ecm.platform.web.common.requestcontroller.service.RequestControllerService;
 import org.nuxeo.ecm.platform.web.common.requestcontroller.service.RequestFilterConfig;
-import org.nuxeo.runtime.api.Framework;
-import org.nuxeo.runtime.test.NXRuntimeTestCase;
+import org.nuxeo.runtime.test.runner.Deploy;
+import org.nuxeo.runtime.test.runner.Features;
+import org.nuxeo.runtime.test.runner.FeaturesRunner;
+import org.nuxeo.runtime.test.runner.RuntimeFeature;
 
 import com.thetransactioncompany.cors.CORSConfiguration;
 import com.thetransactioncompany.cors.CORSFilter;
 import com.thetransactioncompany.cors.Origin;
 
-public class TestService extends NXRuntimeTestCase {
+@RunWith(FeaturesRunner.class)
+@Features(RuntimeFeature.class)
+@Deploy({ "org.nuxeo.ecm.platform.web.common:OSGI-INF/web-request-controller-framework.xml",
+        "org.nuxeo.ecm.platform.web.common:OSGI-INF/web-request-controller-contrib-test.xml" })
+public class TestService {
 
-    @Before
-    public void setUp() throws Exception {
-        super.setUp();
-        deployContrib("org.nuxeo.ecm.platform.web.common", "OSGI-INF/web-request-controller-framework.xml");
-    }
+    @Inject
+    protected RequestControllerManager requestControllerManager;
 
     @Test
     public void testServiceRegistration() {
-        RequestControllerManager rcm = Framework.getLocalService(RequestControllerManager.class);
-        assertNotNull(rcm);
+        assertNotNull(requestControllerManager);
     }
 
     @Test
     public void testServiceContrib() throws Exception {
-        deployContrib("org.nuxeo.ecm.platform.web.common", "OSGI-INF/web-request-controller-contrib-test.xml");
-
-        RequestControllerManager rcm = Framework.getLocalService(RequestControllerManager.class);
-        assertNotNull(rcm);
-
-        RequestControllerService rcmTest = (RequestControllerService) rcm;
-
-        String uri = "";
+        RequestControllerService requestControllerService = (RequestControllerService) requestControllerManager;
+        String uri;
         RequestFilterConfig config;
 
         uri = "/SyncNoTx/test";
-        config = rcmTest.computeConfigForRequest(uri);
+        config = requestControllerService.computeConfigForRequest(uri);
         assertTrue(config.needSynchronization());
         assertFalse(config.needTransaction());
 
         uri = "/NoSyncTx/test";
-        config = rcmTest.computeConfigForRequest(uri);
+        config = requestControllerService.computeConfigForRequest(uri);
         assertFalse(config.needSynchronization());
         assertTrue(config.needTransaction());
 
         uri = "/SyncTx/test";
-        config = rcmTest.computeConfigForRequest(uri);
+        config = requestControllerService.computeConfigForRequest(uri);
         assertTrue(config.needSynchronization());
         assertTrue(config.needTransaction());
 
         uri = "/SyncTx/deny";
-        config = rcmTest.computeConfigForRequest(uri);
+        config = requestControllerService.computeConfigForRequest(uri);
         assertFalse(config.needSynchronization());
         assertFalse(config.needTransaction());
 
         uri = "/whatever";
-        config = rcmTest.computeConfigForRequest(uri);
+        config = requestControllerService.computeConfigForRequest(uri);
         assertFalse(config.needSynchronization());
         assertFalse(config.needTransaction());
 
         uri = "/nuxeo/TestServlet/";
-        config = rcmTest.computeConfigForRequest(uri);
+        config = requestControllerService.computeConfigForRequest(uri);
         assertTrue(config.needSynchronization());
         assertTrue(config.needTransaction());
 
         uri = "/nuxeo/TestServlet";
-        config = rcmTest.computeConfigForRequest(uri);
+        config = requestControllerService.computeConfigForRequest(uri);
         assertTrue(config.needSynchronization());
         assertTrue(config.needTransaction());
 
         uri = "/nuxeo/TestServlet/toto";
-        config = rcmTest.computeConfigForRequest(uri);
+        config = requestControllerService.computeConfigForRequest(uri);
         assertTrue(config.needSynchronization());
         assertTrue(config.needTransaction());
 
         uri = "/nuxeo/CacheDefault";
-        config = rcmTest.computeConfigForRequest(uri);
+        config = requestControllerService.computeConfigForRequest(uri);
         assertFalse(config.isCached());
         assertFalse(config.isPrivate());
         assertEquals("3599", config.getCacheTime());
 
         uri = "/nuxeo/Cache";
-        config = rcmTest.computeConfigForRequest(uri);
+        config = requestControllerService.computeConfigForRequest(uri);
         assertTrue(config.isCached());
         assertTrue(config.isPrivate());
         assertEquals("3000", config.getCacheTime());
@@ -127,11 +124,6 @@ public class TestService extends NXRuntimeTestCase {
 
     @Test
     public void testCorsContrib() throws Exception {
-        deployContrib("org.nuxeo.ecm.platform.web.common", "OSGI-INF/web-request-controller-contrib-test.xml");
-
-        RequestControllerManager rcm = Framework.getLocalService(RequestControllerManager.class);
-        assertNotNull(rcm);
-
         HttpServletRequest request = mock(HttpServletRequest.class);
         String uri;
         CORSFilter filter;
@@ -139,19 +131,19 @@ public class TestService extends NXRuntimeTestCase {
 
         uri = "/dummy/uri";
         when(request.getRequestURI()).thenReturn(uri);
-        filter = rcm.getCorsFilterForRequest(request);
+        filter = requestControllerManager.getCorsFilterForRequest(request);
         assertNull(filter);
 
         uri = "/nuxeo/site/minimal/something/long/dummy.html";
         when(request.getRequestURI()).thenReturn(uri);
-        filter = rcm.getCorsFilterForRequest(request);
+        filter = requestControllerManager.getCorsFilterForRequest(request);
         config = filter.getConfiguration();
         assertEquals(-1, config.maxAge);
         assertEquals(Collections.emptySet(), config.allowedOrigins);
 
         uri = "/nuxeo/site/dummy/";
         when(request.getRequestURI()).thenReturn(uri);
-        filter = rcm.getCorsFilterForRequest(request);
+        filter = requestControllerManager.getCorsFilterForRequest(request);
         config = filter.getConfiguration();
         assertEquals(3600, config.maxAge);
         Set<Origin> expectedOrigins = new HashSet<>();
@@ -163,12 +155,7 @@ public class TestService extends NXRuntimeTestCase {
 
     @Test
     public void testHeadersContrib() throws Exception {
-        deployContrib("org.nuxeo.ecm.platform.web.common", "OSGI-INF/web-request-controller-contrib-test.xml");
-
-        RequestControllerManager rcm = Framework.getLocalService(RequestControllerManager.class);
-        assertNotNull(rcm);
-
-        Map<String, String> rh = rcm.getResponseHeaders();
+        Map<String, String> rh = requestControllerManager.getResponseHeaders();
         assertEquals(2, rh.size());
         assertTrue(rh.containsKey("WWW-Authenticate"));
         assertEquals("basic", rh.get("WWW-Authenticate"));
