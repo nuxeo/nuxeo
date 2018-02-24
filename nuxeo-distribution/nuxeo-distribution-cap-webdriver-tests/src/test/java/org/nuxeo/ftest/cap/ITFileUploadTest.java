22a0a2253336ef2877052065b34414955d9c8328
Anahide Tchertchian <at@nuxeo.com>
2018-02-24 13:03:08 +0100

    NXP-21468: non-reg tests for file upload keep

--- nuxeo-distribution/nuxeo-distribution-cap-webdriver-tests/src/test/java/org/nuxeo/ftest/cap/ITFileUploadTest.java
+++ nuxeo-distribution/nuxeo-distribution-cap-webdriver-tests/src/test/java/org/nuxeo/ftest/cap/ITFileUploadTest.java
@@ -28,10 +28,11 @@ import java.util.Date;
 import org.junit.Test;
 import org.nuxeo.functionaltests.AbstractTest;
 import org.nuxeo.functionaltests.Locator;
+import org.nuxeo.functionaltests.forms.FileWidgetElement.InputFileChoice;
 import org.nuxeo.functionaltests.pages.DocumentBasePage;
 import org.nuxeo.functionaltests.pages.FileDocumentBasePage;
 import org.nuxeo.functionaltests.pages.forms.FileCreationFormPage;
-import org.nuxeo.functionaltests.pages.tabs.EditTabSubPage;
+import org.nuxeo.functionaltests.pages.tabs.FileEditTabSubPage;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
@@ -67,11 +68,12 @@ public class ITFileUploadTest extends AbstractTest {
         assertNotNull(sumContentText);
         assertFalse(sumContentText.contains("Drop here"));
         assertTrue(sumContentText.contains(uploadedFileName));
-        EditTabSubPage editPage = fileDocumentBasePage.getEditTab();
-        WebElement deleteChoice = Locator.findElementWithTimeout(By.id("document_edit:nxl_file:nxw_file:nxw_file_file:choicedelete"));
-        assertNotNull(deleteChoice);
-        deleteChoice.click();
+
+        fileDocumentBasePage.getEditTab();
+        FileEditTabSubPage editPage = asPage(FileEditTabSubPage.class);
+        editPage.getFileWidgetElement().removeFile();
         fileDocumentBasePage = editPage.save().asPage(FileDocumentBasePage.class);
+
         sumContent = Locator.findElementWithTimeout(By.xpath("//div[@class=\"content_block\"]"));
         assertNotNull(sumContent);
         sumContentText = sumContent.getText();
@@ -82,7 +84,6 @@ public class ITFileUploadTest extends AbstractTest {
         // Clean up repository
         deleteWorkspace(fileDocumentBasePage, wsTitle);
 
-        // Logout
         logout();
     }
 
@@ -111,26 +112,79 @@ public class ITFileUploadTest extends AbstractTest {
 
         // Check validation error
         assertEquals("Value is required", creationPageAfterError.getTitleMessage());
+        assertEquals("Please correct errors", creationPageAfterError.getErrorFeedbackMessage());
 
         // Check file is still there and filename is present
-        assertEquals("tempKeep", creationPageAfterError.getSelectedOption());
+        assertEquals(InputFileChoice.tempKeep.name(), creationPageAfterError.getSelectedOption());
         String filename = creationPageAfterError.getSelectedFilename();
         assertNotNull(filename);
         assertTrue("Wrong uploaded file name '" + filename + "', expected it to contain '" + filePrefix + "'",
                 filename.contains(filePrefix));
 
-        creationPageAfterError.titleTextInput.sendKeys("File title");
-        creationPageAfterError.create();
-        FileDocumentBasePage fileDocumentBasePage = asPage(FileDocumentBasePage.class);
+        creationPageAfterError.createDocument("File title", null);
         // Check uploaded file name
-        String uploadedFileName = fileDocumentBasePage.getFileSummaryTab().getMainContentFileText();
+        String uploadedFileName = asPage(FileDocumentBasePage.class).getFileSummaryTab().getMainContentFileText();
         assertTrue("Wrong uploaded file name '" + uploadedFileName + "', expected it to contain '" + filePrefix + "'",
                 uploadedFileName.contains(filePrefix));
 
         // Clean up repository
-        deleteWorkspace(fileDocumentBasePage, wsTitle);
+        deleteWorkspace(asPage(FileDocumentBasePage.class), wsTitle);
 
-        // Logout
+        logout();
+    }
+
+    /**
+     * Non-regression test for NXP-21468
+     *
+     * @since 10.1
+     */
+    @Test
+    public void testFileUploadOnValidationError2() throws Exception {
+        // Login as Administrator
+        DocumentBasePage defaultDomainPage = login();
+
+        // Init repository with a test Workspace
+        String wsTitle = "WorkspaceTitle_" + new Date().getTime();
+        DocumentBasePage testWorkspacePage = createWorkspace(defaultDomainPage, wsTitle, "");
+
+        // Create a File with an uploaded blob
+        String filePrefix = "NX-Webdriver-test-";
+        FileDocumentBasePage filePage = createFile(testWorkspacePage, "File title", "File description", true,
+                filePrefix, ".txt", "Webdriver test file content.");
+
+        // Check uploaded file name
+        String uploadedFileName = filePage.getFileSummaryTab().getMainContentFileText();
+        assertTrue("Wrong uploaded file name '" + uploadedFileName + "', expected it to contain '" + filePrefix + "'",
+                uploadedFileName.contains(filePrefix));
+
+        // Edit and check the "upload" option without filling a blob
+        filePage.getEditTab();
+        FileEditTabSubPage fileEditPage = asPage(FileEditTabSubPage.class);
+        fileEditPage.getFileWidgetElement().uploadFile(null);
+        fileEditPage.save();
+        fileEditPage = asPage(FileEditTabSubPage.class);
+
+        // Check validation error
+        assertEquals("Empty file", fileEditPage.getSelectedFileErrorMessage());
+        assertEquals("Please correct errors", fileEditPage.getErrorFeedbackMessage());
+
+        // Check file is still there and filename is present
+        assertEquals(InputFileChoice.upload.name(), fileEditPage.getSelectedOption());
+        assertTrue(fileEditPage.getFileWidgetElement().hasChoice(InputFileChoice.keep));
+        String filename = fileEditPage.getSelectedFilename();
+        assertNotNull(filename);
+        assertTrue("Wrong uploaded file name '" + filename + "', expected it to contain '" + filePrefix + "'",
+                filename.contains(filePrefix));
+
+        // check the "keep" option again
+        fileEditPage.getFileWidgetElement().keepFile();
+        fileEditPage.save();
+
+        // Check uploaded file name again
+        uploadedFileName = asPage(FileDocumentBasePage.class).getFileSummaryTab().getMainContentFileText();
+        assertTrue("Wrong uploaded file name '" + uploadedFileName + "', expected it to contain '" + filePrefix + "'",
+                uploadedFileName.contains(filePrefix));
+
         logout();
     }
 
