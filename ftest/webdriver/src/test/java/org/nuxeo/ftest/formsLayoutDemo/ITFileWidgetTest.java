4669d91dc4c4259fa82fa5345dbcd86b827af1ba
Anahide Tchertchian <at@nuxeo.com>
2018-02-24 14:15:25 +0100

    NXP-21468: non-reg tests for file upload keep

--- ftest/webdriver/src/test/java/org/nuxeo/ftest/formsLayoutDemo/ITFileWidgetTest.java
+++ ftest/webdriver/src/test/java/org/nuxeo/ftest/formsLayoutDemo/ITFileWidgetTest.java
@@ -27,6 +27,7 @@ import java.io.IOException;
 import org.junit.Test;
 import org.nuxeo.functionaltests.forms.FileWidgetElement;
 import org.nuxeo.functionaltests.forms.LayoutElement;
+import org.nuxeo.functionaltests.forms.FileWidgetElement.InputFileChoice;
 import org.openqa.selenium.By;
 
 /**
@@ -47,19 +48,34 @@ public class ITFileWidgetTest extends AbstractWidgetPageTest {
         assertNotEquals("Empty file", getEditWidgetMessage());
         doSubmitDemo();
         assertEquals("Empty file", getEditWidgetMessage());
-        LayoutElement edit = new LayoutElement(driver, pageId + "Layout_edit_form:nxl_" + pageId + "Layout");
-        FileWidgetElement w = edit.getWidget("nxw_" + pageId, FileWidgetElement.class);
-        w.uploadTestFile("file_1", ".txt", "hello");
+        getEditFileWidgetElement().uploadTestFile("file_1", ".txt", "hello");
         doSubmitDemo();
         checkValueRequired(false);
         assertNotEquals("Empty file", getEditWidgetMessage());
         String viewName = getViewWidget().getSubElement("download").getText();
-        String editName = getEditWidget().getSubElement("default_download:download").getText();
         assertTrue(viewName.startsWith("file_1"));
         assertTrue(viewName.endsWith(".txt"));
+        String editName = getEditFileWidgetElement().getFilename(true);
+        assertTrue(editName.startsWith("file_1"));
+        assertTrue(editName.endsWith(".txt"));
+        // resubmit with empty option, non regression test for NXP-21468
+        getEditFileWidgetElement().selectChoice(InputFileChoice.upload);
+        doSubmitDemo();
+        assertEquals("Empty file", getEditWidgetMessage());
+        viewName = getViewWidget().getSubElement("download").getText();
+        assertTrue(viewName.startsWith("file_1"));
+        assertTrue(viewName.endsWith(".txt"));
+        assertTrue(getEditFileWidgetElement().hasChoice(InputFileChoice.keep));
+        editName = getEditFileWidgetElement().getFilename(true);
         assertTrue(editName.startsWith("file_1"));
         assertTrue(editName.endsWith(".txt"));
         navigateTo(pageId);
         assertEquals("File widget", driver.findElement(By.id("fileWidgetLayout_view_form")).getText());
     }
+
+    protected FileWidgetElement getEditFileWidgetElement() {
+        LayoutElement edit = new LayoutElement(driver, pageId + "Layout_edit_form:nxl_" + pageId + "Layout");
+        return edit.getWidget("nxw_" + pageId, FileWidgetElement.class);
+    }
+
 }
