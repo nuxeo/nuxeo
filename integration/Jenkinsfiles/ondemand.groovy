de4533c98f906ce4e8d5dfe66eed5321876e3151
Stephane Lacoin aka nxmatic <slacoin@nuxeo.com>
2018-02-27 16:03:43 +0100

    wip

--- integration/Jenkinsfiles/ondemand.groovy
+++ integration/Jenkinsfiles/ondemand.groovy
@@ -1,124 +1,106 @@
-properties([
-    [$class: 'BuildDiscarderProperty', strategy: [$class: 'LogRotator', daysToKeepStr: '60', numToKeepStr: '60', artifactNumToKeepStr: '1']],
-    disableConcurrentBuilds(),
-    [$class: 'GithubProjectProperty', displayName: '', projectUrlStr: 'https://github.com/nuxeo/nuxeo/'],
-    [$class: 'RebuildSettings', autoRebuild: false, rebuildDisabled: false],
-    [$class: 'ParametersDefinitionProperty', parameterDefinitions: [
-        [$class: 'StringParameterDefinition', defaultValue: 'master', description: '', name: 'BRANCH'],
-        [$class: 'StringParameterDefinition', defaultValue: 'master', description: '', name: 'PARENT_BRANCH']]
-        [$class: 'BooleanParameterDefinition', defaultValue: 'true', description: '', name: 'CLEAN'],
-    ],
-    pipelineTriggers([[$class: 'GitHubPushTrigger']])
-  ])
+//if (currentBuild.previousBuild == null) { // first build, should configure job
+    properties([
+        [$class: 'BuildDiscarderProperty', strategy: [$class: 'LogRotator', daysToKeepStr: '60', numToKeepStr: '60', artifactNumToKeepStr: '1']],
+        disableConcurrentBuilds(),
+        [$class: 'GithubProjectProperty', displayName: '', projectUrlStr: 'https://github.com/nuxeo/nuxeo/'],
+        [$class: 'RebuildSettings', autoRebuild: false, rebuildDisabled: false]])
+//}
 
-
-node('SLAVE') {
-    sha = stage('clone') {
-        if (clean) {
-            deleteDir()
-        }
+node('slacoin') {
+    
+    def sha = stage('clone') {
         checkout(
             [$class: 'GitSCM',
-             branches: [[name: "*/$BRANCH"]],
+             branches: [[name: '*/${BRANCH_NAME}']],
              browser: [$class: 'GithubWeb', repoUrl: 'https://github.com/nuxeo/nuxeo'],
              doGenerateSubmoduleConfigurations: false,
-             extensions: [[$class: 'CleanBeforeCheckout']],
+             extensions: [
+                    [$class: 'CleanBeforeCheckout'],
+                    [$class: 'CloneOption', noTags: true, reference: '', shallow: false]
+                ],
              submoduleCfg: [],
-             userRemoteConfigs: [[url: 'git://github.com/nuxeo/nuxeo.git']]
+             userRemoteConfigs: [[url: 'git@github.com:nuxeo/nuxeo.git']]
             ])
-        sh '''./clone.py --fallback $PARENT_BRANCH --rebase'''
         return sh(returnStdout: true, script: 'git rev-parse HEAD').trim()
     }
-    withMaven(
-        jdk: 'java-8-oracle',
-        mvn: 'maven-3'
-    ) {
-        stage('compile') {
-            withBuildStatus("$BRANCH/compile", 'https://github.com/nuxeo/nuxeo', sha, "${BUILD_URL}") {
-                checkout(
-                    [$class: 'GitSCM',
-                     branches: [[name: "*/$branch"]],
-                     browser: [$class: 'GithubWeb', repoUrl: 'https://github.com/nuxeo/nuxeo'],
-                     doGenerateSubmoduleConfigurations: false,
-                     extensions: [
-                            [$class: 'CloneOption', depth: 5, noTags: true, reference: '', shallow: true]
-                        ],
-                     submoduleCfg: [],
-                     userRemoteConfigs: [[url: 'git://github.com/nuxeo/nuxeo.git']]
-                    ])
-                sh './clone.py'
-                sh "mvn -Paddons,distrib compile -DskipTests"
+
+    stage('rebase') {
+        withBuildStatus('rebase', 'https://github.com/nuxeo/nuxeo', sha, "${BUILD_URL}") {
+            sh "./clone.py $BRANCH_NAME -f master --rebase"
+        }
+        stash('ws')
+    }
+    
+    stage('compile') {
+        withBuildStatus('compile', 'https://github.com/nuxeo/nuxeo', sha, "${BUILD_URL}") {
+            withMaven() {
+                sh 'mvn -B test-compile -Pqa,addons,distrib -DskipTests'
             }
         }
+    }
+
+    stage('test') {
+        withBuildStatus('test', 'https://github.com/nuxeo/nuxeo', sha, "${BUILD_URL}") {
+            withMaven() {
+                sh 'mvn -B test -Pqa,addons,distrib -Dmaven.test.failure.ignore=true -Dnuxeo.tests.random.mode=STRICT'
+            }
+        }
+    }
+
+    def zipfile=stage('verify') {
+        withBuildStatus('verify', 'https://github.com/nuxeo/nuxeo', sha, "${BUILD_URL}") {
+            withMaven() {
+                sh 'mvn -B verify -Pqa,addons,distrib,tomcat -DskipTests -Dmaven.test.failure.ignore=true -Dnuxeo.tests.random.mode=STRICT'
+            }
+        }
+        return sh(returnStdout: true, script: 'ls $WORKSPACE/nuxeo-distribution/nuxeo-server-tomcat/target/nuxeo-server-tomcat-*.zip')
+    }
+
+    stage('postgresql') {
+        verifyProfile('pgsql', '9.6', sha, zipfile)
+    }
+
+    stage('mongo') {
+        verifyProfile('mongodb', '3.4', sha, zipfile)
+    }
+
+    stage('kafka') {
+        verifyProfile('kafka', '1.0', sha, zipfile)
+    }
+}
+
+def verifyProfile(String profile, String version, String sha, String zipfile) {
+    withEnv(["DBPROFILE=$profile", "DBVERSION=$version"]) {
         parallel(
-            'test': {
-                stage('test') {
-                    node('SLAVE') {
-                        ws("${WORKSPACE}-test") {
-                            unstash('ws')
-                            withBuildStatus("$BRANCH/test", 'https://github.com/nuxeo/nuxeo', sha, "${BUILD_URL}") {
-                                sh "mvn -Paddons,distrib test"
-                            }
-                        }
-                    }
-                }
+            'cmis' : emitVerifyClosure('SLAVE', sha, zipfile, 'cmis', 'nuxeo-server-cmis-tests') {
+                archive 'nuxeo-distribution/nuxeo-server-cmis-tests/target/**/failsafe-reports/*, nuxeo-distribution/nuxeo-server-cmis-tests/target/*.png, nuxeo-distribution/nuxeo-server-cmis-tests/target/*.json, nuxeo-distribution/nuxeo-server-cmis-tests/target/**/*.log, nuxeo-distribution/nuxeo-server-cmis-tests/target/**/log/*, nuxeo-distribution/nuxeo-server-cmis-tests/target/**/nxserver/config/distribution.properties, nuxeo-distribution/nuxeo-server-cmis-tests/target/nxtools-reports/*'
+                failOnServerError('nuxeo-distribution/nuxeo-server-cmis-tests/target/tomcat/log/server.log')
             },
-            'verify': {
-                stage('verify') {
-                    node('SLAVE') {
-                        ws("${WORKSPACE}-verify") {
-                            unstash('ws')
-                            withBuildStatus("$BRANCH/verify", 'https://github.com/nuxeo/nuxeo', sha, "${BUILD_URL}") {
-                                sh "mvn -Paddons,distrib -DskipTests verify"
-                            }
-                        }
-                    }
-                }
+            'webdriver' : emitVerifyClosure('SLAVE', sha, zipfile, 'webdriver', 'nuxeo-jsf-ui-webdriver-tests') {
+                archive 'nuxeo-distribution/nuxeo-jsf-ui-webdriver-tests/target/**/failsafe-reports/*, nuxeo-distribution/nuxeo-jsf-ui-webdriver-tests/target/*.png, nuxeo-distribution/nuxeo-jsf-ui-webdriver-tests/target/*.json, nuxeo-distribution/nuxeo-jsf-ui-webdriver-tests/target/**/*.log, nuxeo-distribution/nuxeo-jsf-ui-webdriver-tests/target/**/log/*, nuxeo-distribution/nuxeo-jsf-ui-webdriver-tests/target/**/nxserver/config/distribution.properties, nuxeo-distribution/nuxeo-server-cmis-tests/target/nxtools-reports/*, nuxeo-distribution/nuxeo-jsf-ui-webdriver-tests/target/results/*/*'
+                junit '**/target/surefire-reports/*.xml, **/target/failsafe-reports/*.xml, **/target/failsafe-reports/**/*.xml'
+                failOnServerError('nuxeo-distribution/nuxeo-jsf-ui-webdriver-tests/target/tomcat/log/server.log')
             }
         )
     }
 }
 
-sh '''
-  which -a java mvn
-  java -version
-  mvn -V --version
-
-  [[ &quot;\$BRANCH&quot; != &quot;\${BRANCH//[^A-Za-z0-9-_.\\/]/}&quot; ]] &amp;&amp; echo &quot;[WARNING] BRANCH name with invalid characters: \$(echo \$BRANCH|cat -v)&quot; &gt;&amp;2 || echo &quot;BRANCH name check: OK&quot;
-BRANCH=\${BRANCH//[^A-Za-z0-9-_.\\/]/}
-START=\$(date +%s)
-unset DOCKER_HOST
-
-if [ &quot;\$CLEAN&quot; = true ] || [ ! -e .git ]; then
-  rm -rf * .??*
-  git clone git@github.com:nuxeo/nuxeo.git .
-fi
-git checkout \$BRANCH 2&gt;/dev/null || git checkout \$PARENT_BRANCH
-
-. scripts/gitfunctions.sh
-if [ &quot;\$(type -t shr)&quot; != &quot;function&quot; ]; then
-  echo ERROR: the current job is not compliant with this version of gitfunctions.sh
-  exit 1
-fi
-
-if [ &quot;\$CLEAN&quot; = false ]; then
-  gitfa fetch --all
-  gitfa checkout \$PARENT_BRANCH
-  gitfa pull --rebase
-  ! gitfa -q diff --name-status --diff-filter=U | grep -B1 -e &quot;^U&quot;
-fi
-
-# switch on feature \$BRANCH if exists, else falls back on \$PARENT_BRANCH
-./clone.py \$BRANCH -f \$PARENT_BRANCH
-gitfa rebase origin/\$PARENT_BRANCH
-! gitfa -q diff --name-status --diff-filter=U | grep -B1 -e &quot;^U&quot;
-if [ &quot;\$MERGE_BEFORE_BUILD&quot; = true ]; then
-  shr -a &quot;git checkout \$PARENT_BRANCH; git pull --rebase; git merge --no-ff \$BRANCH --log&quot;
-  ! gitfa -q diff --name-status --diff-filter=U | grep -B1 -e &quot;^U&quot;
-fi
-
-rm -rf \$WORKSPACE/.repository/org/nuxeo/
-echo &quot;Init \$((\$(date +%s) - \$START))&quot; &gt; \$WORKSPACE/.ci-metrics
-echo &quot;\$(date +%s)&quot; &gt;\$WORKSPACE/.ci-metrics-mavenstart
-'''
-
+def emitVerifyClosure(String nodelabel, String sha, String zipfile, String name, String dir, Closure post) {
+    return {
+        node(nodelabel) {
+            stage(name) {
+                ws("${WORKSPACE}-${name}") {
+                    unstash 'ws'
+                    mvnopts = zipfile != "" ? "-Dzip.file=${WORKSPACE}/${zipfile}" : ""
+                    mvncmd="mvn ${mvnopts} -B -f ${WORKSPACE}/nuxeo-distribution/${dir}/pom.xml -Pqa,tomcat,${DBPROFILE} verify"
+                    echo mvncmd
+                    timeout(time: 2, unit: 'HOURS') {
+                        withBuildStatus("${DBPROFILE}-${DBVERSION}/ftest/${name}", 'https://github.com/nuxeo/nuxeo', sha, "${BUILD_URL}") {
+                            withDockerCompose("${JOB_NAME}-${BUILD_NUMBER}-${name}", "integration/Jenkinsfiles/docker-compose-${DBPROFILE}-${DBVERSION}.yml", mvncmd, post)
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
