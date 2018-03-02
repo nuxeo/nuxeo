7d6a7bd7d0046cd02f865172f69045f5098a81a4
Benoit Delbosc <bdelbosc@nuxeo.com>
2018-03-02 12:55:17 +0100

    NXP-24156: cosmit

--- nuxeo-runtime/nuxeo-stream/src/main/java/org/nuxeo/lib/stream/tools/command/PositionCommand.java
+++ nuxeo-runtime/nuxeo-stream/src/main/java/org/nuxeo/lib/stream/tools/command/PositionCommand.java
@@ -33,6 +33,7 @@ import org.nuxeo.lib.stream.log.LogTailer;
 
 /**
  * Manipulates the consumer position to the beginning, end or a specific timestamp
+ *
  * @since 10.1
  */
 public class PositionCommand extends Command {
@@ -47,77 +48,74 @@ public class PositionCommand extends Command {
     @Override
     public void updateOptions(Options options) {
         options.addOption(Option.builder("l")
-                .longOpt("log-name")
-                .desc("Log name")
-                .required()
-                .hasArg()
-                .argName("LOG_NAME")
-                .build());
+                                .longOpt("log-name")
+                                .desc("Log name")
+                                .required()
+                                .hasArg()
+                                .argName("LOG_NAME")
+                                .build());
         options.addOption(
                 Option.builder("g").longOpt("group").desc("Consumer group").hasArg().argName("GROUP").build());
+        options.addOption(
+                Option.builder().longOpt("reset").desc("Resets all committed positions for the group").build());
         options.addOption(Option.builder()
-                .longOpt("reset")
-                .desc("Resets all committed positions for the group")
-                .build());
-        options.addOption(Option.builder()
-                .longOpt("to-end")
-                .desc("Sets the committed positions to the end of partitions for the group")
-                .build());
-        options.addOption(Option.builder()
-                .longOpt("to-timestamp")
-                .desc("Sets the committed positions for the group to a specific timestamp."
-                + " The record timestamp used depends on the implementation, for Kafka this is the LogAppendTime. "
-                + " The timestamp is specified in ISO-8601 format, eg. " + Instant.now()
-                + ". If no record offset is found with an appropriate timestamp then the command fails.")
-                .hasArg()
-                .argName("TIMESTAMP")
-                .build());
+                                .longOpt("to-end")
+                                .desc("Sets the committed positions to the end of partitions for the group")
+                                .build());
+        options.addOption(
+                Option.builder()
+                      .longOpt("to-timestamp")
+                      .desc("Sets the committed positions for the group to a specific timestamp."
+                              + " The record timestamp used depends on the implementation, for Kafka this is the LogAppendTime. "
+                              + " The timestamp is specified in ISO-8601 format, eg. " + Instant.now()
+                              + ". If no record offset is found with an appropriate timestamp then the command fails.")
+                      .hasArg()
+                      .argName("TIMESTAMP")
+                      .build());
     }
 
     @Override
-    public boolean run(LogManager manager, CommandLine cmd) throws InterruptedException {
+    public boolean run(LogManager manager, CommandLine cmd) {
         String name = cmd.getOptionValue("log-name");
         String group = cmd.getOptionValue("group", "tools");
 
         if (cmd.hasOption("to-timestamp")) {
             long timestamp = parseTimestamp(cmd.getOptionValue("to-timestamp"));
-            if (timestamp < 0) {
-                return false;
+            if (timestamp >= 0) {
+                return positionToTimestamp(manager, group, name, timestamp);
             }
-            return positionToTimestamp(manager, group, name, timestamp);
         } else if (cmd.hasOption("to-end")) {
-            toEnd(manager,group,name);
+            return toEnd(manager, group, name);
         } else if (cmd.hasOption("reset")) {
-            reset(manager,group,name);
-        } else {
-            System.err.println("Invalid option, try 'help position'");
-            return false;
+            return reset(manager, group, name);
         }
-
-        return true;
+        System.err.println("Invalid option, try 'help position'");
+        return false;
     }
 
-    protected void toEnd(LogManager manager, String group, String name) {
+    protected boolean toEnd(LogManager manager, String group, String name) {
         LogLag lag = manager.getLag(name, group);
 
         try (LogTailer<Record> tailer = manager.createTailer(group, name)) {
             tailer.toEnd();
             tailer.commit();
         }
-        System.out.println(String.format("# Moved log %s, group: %s, from: %s to %s", name, group, lag.lower(), lag.upper()));
+        System.out.println(
+                String.format("# Moved log %s, group: %s, from: %s to %s", name, group, lag.lower(), lag.upper()));
+        return true;
     }
 
-    protected void reset(LogManager manager, String group, String name) {
+    protected boolean reset(LogManager manager, String group, String name) {
         LogLag lag = manager.getLag(name, group);
         long pos = lag.lower();
         try (LogTailer<Record> tailer = manager.createTailer(group, name)) {
             tailer.reset();
         }
         System.out.println(String.format("# Reset log %s, group: %s, from: %s to 0", name, group, pos));
+        return true;
     }
 
     protected boolean positionToTimestamp(LogManager manager, String group, String name, long timestamp) {
-
         try (LogTailer<Record> tailer = manager.createTailer(group, name)) {
             int size = manager.getAppender(name).size();
             boolean movedOffset = false;
@@ -130,8 +128,8 @@ public class PositionCommand extends Command {
                     System.out.println(
                             String.format("# Set log %s, group: %s, to offset %s", name, group, logOffset.offset()));
                 } else {
-                    System.err.println(
-                            String.format("# Could not find an offset for group: %s, partition: %s", group, logPartition));
+                    System.err.println(String.format("# Could not find an offset for group: %s, partition: %s", group,
+                            logPartition));
                 }
             }
             if (movedOffset) {
@@ -139,7 +137,6 @@ public class PositionCommand extends Command {
                 return true;
             }
         }
-
         return false;
     }
 
