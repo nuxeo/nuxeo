f45facab08abc741f8303473292d038d5842c432
Florent Munch <florent.munch@gmail.com>
2018-03-04 16:48:33 +0100

    NXP-24540: added a way to customize Quartz jobs and triggers
    Added an optional jobFactoryClass attribute to the schedule extension
    point of SchedulerService to allow a custom Quartz job and trigger to be
    used.
    
    Allows for example the use of @DisallowConcurrentExecution on the job or
    withMisfireHandlingInstructionIgnoreMisfires() on the trigger.

---
+++ nuxeo-core/nuxeo-core-event/src/main/java/org/nuxeo/ecm/core/scheduler/DefaultEventJobFactory.java
@@ -0,0 +1,67 @@
+/*
+ * (C) Copyright 2007-2018 Nuxeo (http://nuxeo.com/) and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Contributors:
+ *     Florent Munch
+ */
+package org.nuxeo.ecm.core.scheduler;
+
+import java.io.Serializable;
+import java.util.Map;
+
+import org.quartz.CronScheduleBuilder;
+import org.quartz.CronTrigger;
+import org.quartz.JobBuilder;
+import org.quartz.JobDataMap;
+import org.quartz.ScheduleBuilder;
+import org.quartz.TriggerBuilder;
+
+/**
+ * Default implementation of {@link EventJobFactory} instantiating an {@link EventJob} with a {@link CronTrigger}.
+ *
+ * @since 10.2
+ */
+public class DefaultEventJobFactory implements EventJobFactory {
+    @Override
+    public JobBuilder buildJob(Schedule schedule, Map<String, Serializable> parameters) {
+        JobDataMap map = new JobDataMap();
+        if (parameters != null) {
+            map.putAll(parameters);
+        }
+
+        return JobBuilder.newJob(getJobClass())
+                .withIdentity(schedule.getId(), "nuxeo")
+                .usingJobData(map)
+                .usingJobData("eventId", schedule.getEventId())
+                .usingJobData("eventCategory", schedule.getEventCategory())
+                .usingJobData("username", schedule.getUsername());
+    }
+
+    @Override
+    public TriggerBuilder<?> buildTrigger(Schedule schedule) {
+        return TriggerBuilder.newTrigger()
+                .withIdentity(schedule.getId(), "nuxeo")
+                .withSchedule(buildSchedule(schedule));
+    }
+
+    @Override
+    public ScheduleBuilder<?> buildSchedule(Schedule schedule) {
+        return CronScheduleBuilder.cronSchedule(schedule.getCronExpression());
+    }
+
+    protected Class<? extends EventJob> getJobClass() {
+        return EventJob.class;
+    }
+}
---
+++ nuxeo-core/nuxeo-core-event/src/test/java/org/nuxeo/ecm/core/scheduler/EventJobFactoryTest.java
@@ -0,0 +1,68 @@
+/*
+ * (C) Copyright 2007-2018 Nuxeo (http://nuxeo.com/) and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Contributors:
+ *     Florent Munch
+ */
+package org.nuxeo.ecm.core.scheduler;
+
+import javax.inject.Inject;
+import java.lang.reflect.Field;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.nuxeo.runtime.test.runner.Deploy;
+import org.nuxeo.runtime.test.runner.Features;
+import org.nuxeo.runtime.test.runner.FeaturesRunner;
+import org.nuxeo.runtime.test.runner.RuntimeFeature;
+import org.quartz.JobDetail;
+import org.quartz.JobKey;
+import org.quartz.Scheduler;
+import org.quartz.SchedulerException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Tests {@link EventJobFactory} by contributing a job using {@link org.quartz.DisallowConcurrentExecution}.
+ *
+ * @since 10.2
+ */
+@RunWith(FeaturesRunner.class)
+@Features(RuntimeFeature.class)
+@Deploy("org.nuxeo.ecm.core.event")
+@Deploy("org.nuxeo.ecm.core.event.test:test-jobfactory.xml") // jobs
+public class EventJobFactoryTest {
+    @Inject
+    private SchedulerService schedulerService;
+
+    @Test
+    public void test() throws ReflectiveOperationException, SchedulerException {
+        Field schedulerField = SchedulerServiceImpl.class.getDeclaredField("scheduler");
+        schedulerField.setAccessible(true);
+        Scheduler scheduler = (Scheduler) schedulerField.get(schedulerService);
+
+        // job without the DisallowConcurrentExecution annotation
+        JobKey jobKey = new JobKey("testSchedulerMultipleExecutions", "nuxeo");
+        JobDetail jobDetail = scheduler.getJobDetail(jobKey);
+        assertThat(jobDetail).isNotNull();
+        assertThat(jobDetail.isConcurrentExectionDisallowed()).isFalse();
+
+        // job with the DisallowConcurrentExecution annotation
+        jobKey = new JobKey("testSchedulerSingleExecution", "nuxeo");
+        jobDetail = scheduler.getJobDetail(jobKey);
+        assertThat(jobDetail).isNotNull();
+        assertThat(jobDetail.isConcurrentExectionDisallowed()).isTrue();
+    }
+}
