49b9cb89e53c8d592b1f725dca6cfeb8f0c48169
Benoit Delbosc <bdelbosc@nuxeo.com>
2018-03-13 15:40:34 +0100

    backport feature-NXBT-2056-kafka-test-env (#1545) only for nuxeo-runtime-stream and nuxeo-stream
        * NXP-24009 kafka compose file
    
        * NXP-24009 fixing unit tests pipeline
    
        * NXP-24009 do not reference db container in scripts
    
        * NXP-24009: Make the zookeeper and kafka boostrap configurable via system properties.
    
        * NXP-24009 fixing java opts
    
        * NXP-24009 kafka options handling through docker compose
    
        * NXP-24009 remove it.failsafe.argLine to see if conflict with other way of passing props
    
        * NXP-24009 do not pull patched slave image
    
        * NXP-24009,NXP-23997 enable kafka in functional tests
    
        * NXP-24009 do not bind services on host
    
        * NXP-24009 checkout parent's pom in sparse checkout
    
        * NXP-24009 cleanup built images and used volumes

--- nuxeo-distribution/nuxeo-server-tomcat/src/main/resources/tomcat/bin/stream.sh
+++ nuxeo-distribution/nuxeo-server-tomcat/src/main/resources/tomcat/bin/stream.sh
@@ -12,6 +12,6 @@ $ROOT/nxserver/lib/kafka_2.11-1.0.0.jar:\
 $ROOT/nxserver/lib/scala-library-2.11.7.jar:\
 $ROOT/nxserver/lib/zookeeper-3.4.8.jar:\
 $ROOT/nxserver/lib/jgrapht-core-1.0.1.jar:\
-$ROOT/nxserver/lib/nuxeo-stream-10.1-SNAPSHOT.jar
+$ROOT/nxserver/lib/nuxeo-stream-9.10-SNAPSHOT.jar
 cd $ROOT
 java -cp $CLASSPATH org.nuxeo.lib.stream.tools.Main "$@"
