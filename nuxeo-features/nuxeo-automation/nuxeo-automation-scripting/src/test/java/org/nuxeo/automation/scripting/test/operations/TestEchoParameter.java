e6e1c8533158817289d6dc050fe75d385c0f1a3f
Florent Guillaume <fguillaume@nuxeo.com>
2018-03-26 16:30:22 +0200

    NXP-24707: fix automation scripting caching

---
+++ nuxeo-features/nuxeo-automation/nuxeo-automation-scripting/src/test/java/org/nuxeo/automation/scripting/test/operations/TestEchoParameter.java
@@ -0,0 +1,36 @@
+/*
+ * (C) Copyright 2018 Nuxeo (http://nuxeo.com/) and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Contributors:
+ *      Florent Guillaume
+ */
+package org.nuxeo.automation.scripting.test.operations;
+
+import org.nuxeo.ecm.automation.core.annotations.Operation;
+import org.nuxeo.ecm.automation.core.annotations.OperationMethod;
+import org.nuxeo.ecm.automation.core.annotations.Param;
+
+@Operation(id = "Test.EchoParameter")
+public class TestEchoParameter {
+
+    @Param(name = "param")
+    String param;
+
+    @OperationMethod
+    public String run() throws Exception {
+        return param;
+    }
+
+}
