a77e44fffea3a751febda30421dbfd1a9640cea7
Florent Guillaume <fguillaume@nuxeo.com>
2018-03-29 18:50:20 +0200

    NXP-23744: uid sequencer based on key/value store

---
+++ nuxeo-core/nuxeo-core/src/main/java/org/nuxeo/ecm/core/uidgen/KeyValueStoreUIDSequencer.java
@@ -0,0 +1,83 @@
+/*
+ * (C) Copyright 2018 Nuxeo (http://nuxeo.com/) and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Contributors:
+ *     Florent Guillaume
+ */
+package org.nuxeo.ecm.core.uidgen;
+
+import static org.apache.commons.lang3.StringUtils.isBlank;
+
+import org.nuxeo.ecm.core.api.NuxeoException;
+import org.nuxeo.runtime.api.Framework;
+import org.nuxeo.runtime.kv.KeyValueService;
+import org.nuxeo.runtime.kv.KeyValueStore;
+import org.nuxeo.runtime.services.config.ConfigurationService;
+
+/**
+ * UID Sequencer based on a key/value store. The store is the same for all sequencers, but they are using different
+ * keys, prefixed by the sequencer name.
+ *
+ * @since 10.2
+ */
+public class KeyValueStoreUIDSequencer extends AbstractUIDSequencer {
+
+    /**
+     * Configuration property to specify the key/value store name. If none is specified, {@code sequence} is used.
+     */
+    public static final String STORE_NAME_PROPERTY = "nuxeo.uidseq.keyvaluestore.name";
+
+    public static final String DEFAULT_STORE_NAME = "sequence";
+
+    public static final String SEP = ".";
+
+    protected String storeName;
+
+    @Override
+    public void init() {
+        storeName = Framework.getService(ConfigurationService.class).getProperty(STORE_NAME_PROPERTY);
+        if (isBlank(storeName)) {
+            storeName = DEFAULT_STORE_NAME;
+        }
+    }
+
+    @Override
+    public void dispose() {
+        // nothing to do
+    }
+
+    protected KeyValueStore getStore() {
+        KeyValueStore store = Framework.getService(KeyValueService.class).getKeyValueStore(storeName);
+        if (store == null) {
+            throw new NuxeoException("Unknown key/value store: " + storeName);
+        }
+        return store;
+    }
+
+    protected String getKey(String key) {
+        return getName() + SEP + key;
+    }
+
+    @Override
+    public void initSequence(String key, long id) {
+        getStore().put(getKey(key), Long.valueOf(id));
+    }
+
+    @Override
+    public long getNextLong(String key) {
+        return getStore().addAndGet(getKey(key), 1);
+    }
+
+}
---
+++ nuxeo-core/nuxeo-core/src/test/java/org/nuxeo/ecm/core/uidgen/TestKeyValueStoreUIDSequencer.java
@@ -0,0 +1,141 @@
+/*
+ * (C) Copyright 2018 Nuxeo (http://nuxeo.com/) and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Contributors:
+ *     Florent Guillaume
+ */
+package org.nuxeo.ecm.core.uidgen;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+import javax.inject.Inject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.nuxeo.runtime.test.runner.Deploy;
+import org.nuxeo.runtime.test.runner.Features;
+import org.nuxeo.runtime.test.runner.FeaturesRunner;
+import org.nuxeo.runtime.test.runner.RuntimeFeature;
+
+/**
+ * This test uses the default (in-memory) key/value store.
+ */
+@RunWith(FeaturesRunner.class)
+@Features(RuntimeFeature.class)
+@Deploy("org.nuxeo.runtime.kv")
+@Deploy("org.nuxeo.ecm.core:OSGI-INF/uidgenerator-service.xml")
+@Deploy("org.nuxeo.ecm.core:OSGI-INF/uidgenerator-keyvalue-config.xml")
+@Deploy("org.nuxeo.ecm.core.tests:OSGI-INF/test-keyvaluestore-uidseq.xml")
+public class TestKeyValueStoreUIDSequencer {
+
+    @Inject
+    protected UIDGeneratorService service;
+
+    @Test
+    public void testRegistration() {
+        UIDSequencer seq = service.getSequencer();
+        assertNotNull(seq);
+        assertTrue(seq.getClass().getName(), seq instanceof KeyValueStoreUIDSequencer);
+    }
+
+    @Test
+    public void testSequencer() {
+        UIDSequencer seq = service.getSequencer();
+        String key;
+
+        key = "foo";
+        seq.initSequence(key, 0L);
+        seq.getNextLong(key);
+        seq.getNextLong(key);
+        assertThat(seq.getNextLong(key)).isGreaterThan(2);
+
+        key = "bar";
+        seq.initSequence(key, 1L);
+        assertThat(seq.getNextLong(key)).isGreaterThan(1);
+        assertThat(seq.getNextLong(key)).isLessThan(10);
+        seq.initSequence(key, 10L);
+        assertThat(seq.getNextLong(key)).isGreaterThan(10);
+        assertThat(seq.getNextLong(key)).isGreaterThan(10);
+        // we can go backward when initializing a sequence
+        seq.initSequence(key, 5L);
+        assertEquals(6, seq.getNextLong(key));
+
+        key = "baz";
+        seq.initSequence(key, 499L);
+        assertEquals(500, seq.getNextLong(key));
+        seq.initSequence(key, 9999L);
+        assertEquals(10000, seq.getNextLong(key));
+        seq.initSequence(key, (long) Integer.MAX_VALUE);
+        assertEquals(2_147_483_648L, seq.getNextLong(key)); // first long not representable as int
+    }
+
+    @Test
+    public void testInterleave() {
+        UIDSequencer seq = service.getSequencer();
+        assertNotNull(seq);
+        assertTrue(seq.getClass().getName(), seq instanceof KeyValueStoreUIDSequencer);
+
+        String key1 = "foo";
+        String key2 = "bar";
+        seq.initSequence(key1, 0L);
+        seq.initSequence(key2, 0L);
+        assertEquals(1, seq.getNextLong(key1));
+        assertEquals(2, seq.getNextLong(key1));
+        assertEquals(3, seq.getNextLong(key1));
+        assertEquals(1, seq.getNextLong(key2));
+        assertEquals(4, seq.getNextLong(key1));
+        assertEquals(2, seq.getNextLong(key2));
+    }
+
+    @Test
+    public void testNoCollisionsBetweenSequencers() {
+        UIDSequencer seq1 = service.getSequencer("uidgen");
+        UIDSequencer seq2 = service.getSequencer("myseq");
+
+        String key = "foo";
+        seq1.initSequence(key, 100L);
+        seq2.initSequence(key, 555L);
+        assertEquals(101, seq1.getNextLong(key));
+        assertEquals(556, seq2.getNextLong(key));
+    }
+
+    @SuppressWarnings("boxing")
+    @Test
+    public void testConcurrency() throws Exception {
+        UIDSequencer seq = service.getSequencer();
+        int n = 10000;
+        int poolSize = 5;
+
+        String key = "mt";
+        seq.initSequence(key, 0L);
+        ThreadPoolExecutor tpe = new ThreadPoolExecutor(poolSize, poolSize, 500L, TimeUnit.MILLISECONDS,
+                new LinkedBlockingQueue<>(n + 1));
+        for (int i = 0; i < n; i++) {
+            tpe.submit(() -> seq.getNextLong(key));
+        }
+        tpe.shutdown();
+        boolean finish = tpe.awaitTermination(20, TimeUnit.SECONDS);
+        assertTrue("timeout", finish);
+        assertEquals(n + 1, seq.getNextLong(key));
+    }
+
+}
