712def00b78782de689869a0acfaf52cbbb95e67
Gethin James <gjames@nuxeo.com>
2018-04-10 13:33:42 +0200

    NXP-24637: Fix ES upperBounds query

--- nuxeo-drive-elasticsearch/src/main/java/org/nuxeo/drive/elasticsearch/ESAuditChangeFinder.java
+++ nuxeo-drive-elasticsearch/src/main/java/org/nuxeo/drive/elasticsearch/ESAuditChangeFinder.java
@@ -21,8 +21,10 @@ package org.nuxeo.drive.elasticsearch;
 
 import java.io.IOException;
 import java.time.ZonedDateTime;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
@@ -43,6 +45,7 @@ import org.elasticsearch.search.sort.SortOrder;
 import org.nuxeo.drive.service.SynchronizationRoots;
 import org.nuxeo.drive.service.impl.AuditChangeFinder;
 import org.nuxeo.ecm.core.api.CoreSession;
+import org.nuxeo.ecm.core.api.repository.RepositoryManager;
 import org.nuxeo.ecm.platform.audit.api.ExtendedInfo;
 import org.nuxeo.ecm.platform.audit.api.LogEntry;
 import org.nuxeo.elasticsearch.ElasticSearchConstants;
@@ -225,26 +228,8 @@ public class ESAuditChangeFinder extends AuditChangeFinder {
 
     @Override
     public long getUpperBound() {
-        SearchRequestBuilder builder = getClient().prepareSearch(getESIndexName())
-                                                  .setTypes(ElasticSearchConstants.ENTRY_TYPE)
-                                                  .setSearchType(SearchType.DFS_QUERY_THEN_FETCH);
-        // TODO refactor this to use max clause
-        builder.setQuery(QueryBuilders.matchAllQuery());
-        builder.addSort("id", SortOrder.DESC);
-        builder.setSize(1);
-        logSearchRequest(builder);
-        SearchResponse searchResponse = builder.execute().actionGet();
-        logSearchResponse(searchResponse);
-        List<LogEntry> entries = new ArrayList<>();
-        SearchHits hits = searchResponse.getHits();
-        for (SearchHit hit : hits) {
-            try {
-                entries.add(AuditEntryJSONReader.read(hit.getSourceAsString()));
-            } catch (IOException e) {
-                log.error("Error while reading Audit Entry from ES", e);
-            }
-        }
-        return entries.size() > 0 ? entries.get(0).getId() : -1;
+        RepositoryManager repositoryManager = Framework.getService(RepositoryManager.class);
+        return getUpperBound(new HashSet<>(repositoryManager.getRepositoryNames()));
     }
 
     /**
@@ -268,7 +253,7 @@ public class ESAuditChangeFinder extends AuditChangeFinder {
         builder.setSize(1);
         // scroll on previous days with a times 2 step up to 32
         for (int i = 1; i <= 32; i = i * 2) {
-            ZonedDateTime lowerLogDateTime = ZonedDateTime.now().minusDays(i);
+            ZonedDateTime lowerLogDateTime = ZonedDateTime.now().truncatedTo(ChronoUnit.DAYS).minusDays(i);
             // set lower bound in query
             filterBuilder = filterBuilder.gt(Date.from(lowerLogDateTime.toInstant()));
             builder.setQuery(QueryBuilders.boolQuery().must(queryBuilder).filter(filterBuilder));
