c49b02cfb97c79d894c82e44989f3852cbb3db0c
Pierre GAUTIER <pierregautier@nuxeo.com>
2018-04-24 17:51:25 +0200

    NXP-24744: unignore testQueryOnlyProxiesDenied() that actually works

--- nuxeo-core/nuxeo-core-storage-sql/nuxeo-core-storage-sql-test/src/test/java/org/nuxeo/ecm/core/storage/sql/TestSQLBackendNoProxies.java
+++ nuxeo-core/nuxeo-core-storage-sql/nuxeo-core-storage-sql-test/src/test/java/org/nuxeo/ecm/core/storage/sql/TestSQLBackendNoProxies.java
@@ -93,7 +93,6 @@ public class TestSQLBackendNoProxies extends TestSQLBackend {
     }
 
     @Test
-    @Ignore("NXP-24842 test fails after 3h49, seems to be a deadlock there")
     public void testQueryOnlyProxiesDenied() {
         Session session = repository.getConnection();
         String sql = "SELECT * FROM Document WHERE ecm:isProxy = 1";
