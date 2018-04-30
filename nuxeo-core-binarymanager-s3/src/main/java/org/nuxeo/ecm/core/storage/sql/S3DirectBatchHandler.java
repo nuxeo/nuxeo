64de77a028ed3bdb079aa173a73e5e4e62aa69c2
Luís Duarte <lduarte@nuxeo.com>
2018-04-30 12:07:40 +0100

    NXP-24754: switch from STS:AssumeRole to STS:GetFederationToken

--- nuxeo-core-binarymanager-s3/src/main/java/org/nuxeo/ecm/core/storage/sql/S3DirectBatchHandler.java
+++ nuxeo-core-binarymanager-s3/src/main/java/org/nuxeo/ecm/core/storage/sql/S3DirectBatchHandler.java
@@ -20,6 +20,8 @@
 package org.nuxeo.ecm.core.storage.sql;
 
 import static org.apache.commons.lang3.StringUtils.defaultIfEmpty;
+import static org.apache.commons.lang3.StringUtils.defaultString;
+import static org.apache.commons.lang3.StringUtils.isBlank;
 import static org.apache.commons.lang3.StringUtils.isEmpty;
 import static org.nuxeo.ecm.core.storage.sql.S3BinaryManager.AWS_ID_PROPERTY;
 import static org.nuxeo.ecm.core.storage.sql.S3BinaryManager.AWS_SECRET_PROPERTY;
@@ -34,11 +36,14 @@ import java.util.List;
 import java.util.Map;
 import java.util.regex.Pattern;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.nuxeo.ecm.automation.server.jaxrs.batch.Batch;
 import org.nuxeo.ecm.automation.server.jaxrs.batch.handler.AbstractBatchHandler;
 import org.nuxeo.ecm.automation.server.jaxrs.batch.handler.BatchFileInfo;
 import org.nuxeo.ecm.core.api.Blob;
 import org.nuxeo.ecm.core.api.NuxeoException;
+import org.nuxeo.ecm.core.api.local.ClientLoginModule;
 import org.nuxeo.ecm.core.blob.binary.BinaryBlob;
 import org.nuxeo.ecm.core.blob.binary.LazyBinary;
 
@@ -48,8 +53,8 @@ import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.securitytoken.AWSSecurityTokenService;
 import com.amazonaws.services.securitytoken.AWSSecurityTokenServiceClientBuilder;
-import com.amazonaws.services.securitytoken.model.AssumeRoleRequest;
 import com.amazonaws.services.securitytoken.model.Credentials;
+import com.amazonaws.services.securitytoken.model.GetFederationTokenRequest;
 
 /**
  * Batch Handler allowing direct S3 upload.
@@ -58,20 +63,24 @@ import com.amazonaws.services.securitytoken.model.Credentials;
  */
 public class S3DirectBatchHandler extends AbstractBatchHandler {
 
+    private static final Log log = LogFactory.getLog(S3BinaryManager.class);
+
     protected static final Pattern REGEX_MULTIPART_ETAG = Pattern.compile("-\\d+$");
+
+    protected static final Pattern REGEX_BUCKET_PATH_PLACE_HOLDER = Pattern.compile("\\{\\{bucketPath}}");
 
     // properties passed at initialization time from extension point
 
-    public static final String ROLE_ARN_PROPERTY = "roleArn";
-
     public static final String ACCELERATE_MODE_ENABLED_PROPERTY = "accelerateMode";
+
+    public static final String POLICY_TEMPLATE_PROPERTY = "policyTemplate";
 
     protected static final List<String> MANDATORY_PROPERTIES = Arrays.asList( //
             AWS_ID_PROPERTY, //
             AWS_SECRET_PROPERTY, //
             BUCKET_NAME_PROPERTY, //
             BUCKET_REGION_PROPERTY, //
-            ROLE_ARN_PROPERTY //
+            POLICY_TEMPLATE_PROPERTY //
     );
 
     // keys in the batch properties, returned to the client
@@ -92,6 +101,8 @@ public class S3DirectBatchHandler extends AbstractBatchHandler {
 
     public static final String INFO_USE_S3_ACCELERATE = "useS3Accelerate";
 
+    public static final String INFO_POLICY_TEMPLATE = "policyTemplate";
+
     protected AWSSecurityTokenService stsClient;
 
     protected AmazonS3 amazonS3;
@@ -102,9 +113,11 @@ public class S3DirectBatchHandler extends AbstractBatchHandler {
 
     protected String bucketPrefix;
 
-    protected String roleArn;
-
     protected boolean accelerateModeEnabled;
+
+    protected int expiration;
+
+    protected String policyTemplate;
 
     @Override
     protected void initialize(Map<String, String> properties) {
@@ -116,11 +129,13 @@ public class S3DirectBatchHandler extends AbstractBatchHandler {
         }
         region = properties.get(BUCKET_REGION_PROPERTY);
         bucket = properties.get(BUCKET_NAME_PROPERTY);
-        bucketPrefix = defaultIfEmpty(properties.get(BUCKET_PREFIX_PROPERTY), "");
-        roleArn = properties.get(ROLE_ARN_PROPERTY);
+        bucketPrefix = defaultString(properties.get(BUCKET_PREFIX_PROPERTY));
         accelerateModeEnabled = Boolean.parseBoolean(properties.get(ACCELERATE_MODE_ENABLED_PROPERTY));
         String awsSecretKeyId = properties.get(AWS_ID_PROPERTY);
         String awsSecretAccessKey = properties.get(AWS_SECRET_PROPERTY);
+        expiration = Integer.parseInt(defaultIfEmpty(properties.get(INFO_EXPIRATION), "0"));
+        policyTemplate = properties.get(POLICY_TEMPLATE_PROPERTY);
+
         AWSCredentialsProvider awsCredentialsProvider = S3Utils.getAWSCredentialsProvider(awsSecretKeyId,
                 awsSecretAccessKey);
         stsClient = AWSSecurityTokenServiceClientBuilder.standard()
@@ -132,6 +147,12 @@ public class S3DirectBatchHandler extends AbstractBatchHandler {
                                         .withCredentials(awsCredentialsProvider)
                                         .withAccelerateModeEnabled(accelerateModeEnabled)
                                         .build();
+
+        if (!isBlank(bucketPrefix) && !bucketPrefix.endsWith("/")) {
+            log.warn(String.format("%s %s S3 bucket prefix should end with '/': added automatically.",
+                    BUCKET_PREFIX_PROPERTY, bucketPrefix));
+            bucketPrefix += "/";
+        }
     }
 
     @Override
@@ -143,18 +164,30 @@ public class S3DirectBatchHandler extends AbstractBatchHandler {
 
         // create the batch
         Batch batch = new Batch(batchId, parameters, getName(), getTransientStore());
-        AssumeRoleRequest assumeRoleRequest = new AssumeRoleRequest().withRoleSessionName(batch.getKey())
-                                                                     .withRoleArn(roleArn);
-        Credentials credentials = stsClient.assumeRole(assumeRoleRequest).getCredentials();
+
+        String bucketPath = bucketPrefix + batchId + "/";
+
+        String policy = REGEX_BUCKET_PATH_PLACE_HOLDER.matcher(policyTemplate).replaceAll(bucketPath);
+
+        String federatedUserName = batchId + "_" + ClientLoginModule.getCurrentPrincipal().getOriginatingUser();
+        GetFederationTokenRequest federationTokenRequest = new GetFederationTokenRequest().withPolicy(policy)
+                                                                                          .withName(federatedUserName);
+
+        if (expiration > 0) {
+            federationTokenRequest.setDurationSeconds(expiration);
+        }
+
+        Credentials credentials = stsClient.getFederationToken(federationTokenRequest).getCredentials();
         Map<String, Object> properties = batch.getProperties();
         properties.put(INFO_AWS_SECRET_KEY_ID, credentials.getAccessKeyId());
         properties.put(INFO_AWS_SECRET_ACCESS_KEY, credentials.getSecretAccessKey());
         properties.put(INFO_AWS_SESSION_TOKEN, credentials.getSessionToken());
         properties.put(INFO_BUCKET, bucket);
-        properties.put(INFO_BASE_KEY, bucketPrefix);
+        properties.put(INFO_BASE_KEY, bucketPath);
         properties.put(INFO_EXPIRATION, credentials.getExpiration().toInstant().toEpochMilli());
         properties.put(INFO_AWS_REGION, region);
         properties.put(INFO_USE_S3_ACCELERATE, accelerateModeEnabled);
+        properties.put(INFO_POLICY_TEMPLATE, policyTemplate);
 
         return batch;
     }
@@ -171,7 +204,7 @@ public class S3DirectBatchHandler extends AbstractBatchHandler {
             return false;
         }
         String mimeType = metadata.getContentType();
-        String encoding = null;
+        String encoding = metadata.getContentEncoding();
 
         ObjectMetadata newMetadata;
         if (metadata.getContentLength() > NON_MULTIPART_COPY_MAX_SIZE) {
