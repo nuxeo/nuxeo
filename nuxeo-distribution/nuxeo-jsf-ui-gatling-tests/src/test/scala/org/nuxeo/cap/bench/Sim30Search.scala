d28c8b55d94a1eeee34cbc4c823276a7ed7ac054
Benoit Delbosc <bdelbosc@nuxeo.com>
2018-05-14 08:51:05 +0200

    NXP-22737: Use proper NXQL search query in Gatling simulation

--- nuxeo-distribution/nuxeo-jsf-ui-gatling-tests/src/test/scala/org/nuxeo/cap/bench/Sim30Search.scala
+++ nuxeo-distribution/nuxeo-jsf-ui-gatling-tests/src/test/scala/org/nuxeo/cap/bench/Sim30Search.scala
@@ -36,7 +36,7 @@ object ScnSearch {
               10.0 -> exec(NuxeoRest.search("SELECT * FROM Document", sortBy = "dc:modified", sortOrder = "DESC", comment = "Search All sorted")),
               10.0 -> exec(NuxeoRest.search("SELECT * FROM Document WHERE ecm:primaryType='Folder'", sortBy = "dc:title", comment = "Search folders sorted")),
               10.0 -> exec(NuxeoRest.search("SELECT * FROM Document WHERE ecm:path STARTSWITH '/default-domain/workspaces/Bench_Gatling/u09/t/g/f' AND ecm:mixinType != 'HiddenInNavigation' AND ecm:isProxy = 0 AND ecm:isVersion = 0 AND ecm:currentLifeCycleState != 'deleted'", comment = "Search on path")),
-              60.0 -> exec(NuxeoRest.search("SELECT * FROM Document WHERE dc:title.fulltext = '${term}' AND ecm:mixinType != 'HiddenInNavigation' AND ecm:isProxy = 0 AND ecm:isVersion = 0 AND ecm:currentLifeCycleState != 'deleted'", comment = "Search fulltext"))
+              60.0 -> exec(NuxeoRest.search("SELECT * FROM Document WHERE ecm:fulltext.dc:title = '${term}' AND ecm:mixinType != 'HiddenInNavigation' AND ecm:isProxy = 0 AND ecm:isVersion = 0 AND ecm:currentLifeCycleState != 'deleted'", comment = "Search fulltext"))
            )
             .pause(pause)
         }
