ccff1b2f2e3e368b970dbba80ae49f075fea4be1
Kevin Leturc <kleturc@nuxeo.com>
2018-05-14 15:55:51 +0200

    NXP-24754: Cleanup

--- nuxeo-core-binarymanager-s3/src/main/java/org/nuxeo/ecm/core/storage/sql/S3BinaryManager.java
+++ nuxeo-core-binarymanager-s3/src/main/java/org/nuxeo/ecm/core/storage/sql/S3BinaryManager.java
@@ -251,7 +251,7 @@ public class S3BinaryManager extends AbstractCloudBinaryManager {
         }
 
         if (!isBlank(bucketNamePrefix) && !bucketNamePrefix.endsWith("/")) {
-            log.warn(String.format("%s %s S3 bucket prefix should end by '/' " + ": added automatically.",
+            log.warn(String.format("%s %s S3 bucket prefix should end with '/' : added automatically.",
                     BUCKET_PREFIX_PROPERTY, bucketNamePrefix));
             bucketNamePrefix += "/";
         }
