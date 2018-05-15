980637b4406ef45fd13992f606781d6544ed7124
Jenkins Nuxeo <jenkins@nuxeo.com>
2018-05-15 09:48:45 +0000

    Post release 7.10-HF40 Update 7.10-HF40-SNAPSHOT to 7.10-HF41-SNAPSHOT

--- nuxeo-distribution/nuxeo-distribution-tomcat/src/main/resources/common/bin/nxdeploy.sh
+++ nuxeo-distribution/nuxeo-distribution-tomcat/src/main/resources/common/bin/nxdeploy.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-java -DupdateWar=y -jar ../lib/nuxeo-runtime-tomcat-adapter-7.10-HF40-SNAPSHOT.jar
+java -DupdateWar=y -jar ../lib/nuxeo-runtime-tomcat-adapter-7.10-HF41-SNAPSHOT.jar
  
