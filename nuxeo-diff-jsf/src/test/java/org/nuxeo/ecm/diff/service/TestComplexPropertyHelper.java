40b342fc899341637a675d915e44b100448657a2
Antoine Taillefer <ataillefer@nuxeo.com>
2018-06-08 17:23:32 +0200

    NXP-24523: Handle field constraints

--- nuxeo-diff-jsf/src/test/java/org/nuxeo/ecm/diff/service/TestComplexPropertyHelper.java
+++ nuxeo-diff-jsf/src/test/java/org/nuxeo/ecm/diff/service/TestComplexPropertyHelper.java
@@ -25,9 +25,13 @@ import javax.inject.Inject;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.nuxeo.directory.test.DirectoryFeature;
 import org.nuxeo.ecm.core.api.CoreSession;
+import org.nuxeo.ecm.core.schema.SchemaManager;
 import org.nuxeo.ecm.core.schema.types.Field;
-import org.nuxeo.ecm.core.test.CoreFeature;
+import org.nuxeo.ecm.core.schema.types.ListType;
+import org.nuxeo.ecm.diff.model.PropertyType;
+import org.nuxeo.runtime.api.Framework;
 import org.nuxeo.runtime.test.runner.Deploy;
 import org.nuxeo.runtime.test.runner.Features;
 import org.nuxeo.runtime.test.runner.FeaturesRunner;
@@ -38,8 +42,11 @@ import org.nuxeo.runtime.test.runner.FeaturesRunner;
  * TODO: add tests
  */
 @RunWith(FeaturesRunner.class)
-@Features(CoreFeature.class)
-@Deploy({ "org.nuxeo.diff.test" })
+@Features(DirectoryFeature.class)
+@Deploy({ "org.nuxeo.ecm.platform.query.api", //
+        "org.nuxeo.diff.test", //
+        "org.nuxeo.diff.test:OSGI-INF/test-diff-constraint-types-contrib.xml" //
+})
 public class TestComplexPropertyHelper {
 
     @Inject
@@ -57,5 +64,55 @@ public class TestComplexPropertyHelper {
         assertNotNull(field);
         assertEquals("textarea", field.getName().getLocalName());
         assertEquals("string", field.getType().getName());
+    }
+
+    /**
+     * @since 10.2
+     */
+    @Test
+    public void testGetFieldType() {
+        // primitive
+        Field field = getField("simpletypes:string");
+        assertEquals("string", ComplexPropertyHelper.getFieldType(field));
+        field = getField("simpletypes:integer");
+        assertEquals("long", ComplexPropertyHelper.getFieldType(field));
+
+        // content
+        field = getField("file:content");
+        assertEquals(PropertyType.CONTENT, ComplexPropertyHelper.getFieldType(field));
+
+        // complex
+        field = getField("complextypes:complex");
+        assertEquals(PropertyType.COMPLEX, ComplexPropertyHelper.getFieldType(field));
+
+        // scalar list
+        field = getField("simpletypes:multivalued");
+        assertEquals(PropertyType.SCALAR_LIST, ComplexPropertyHelper.getFieldType(field));
+
+        // complex list
+        field = getField("complextypes:complexList");
+        assertEquals(PropertyType.COMPLEX_LIST, ComplexPropertyHelper.getFieldType(field));
+
+        // constrained string
+        field = getField("constraints:string");
+        assertEquals("string", ComplexPropertyHelper.getFieldType(field));
+
+        // multivalued constrained string
+        field = getField("constraints:multivaluedString");
+        assertEquals(PropertyType.SCALAR_LIST, ComplexPropertyHelper.getFieldType(field));
+        assertEquals("string", ComplexPropertyHelper.getFieldType(getListSubField(field)));
+
+        // multivalued constrained string with directory resolver
+        field = getField("constraints:multivaluedDirectory");
+        assertEquals(PropertyType.SCALAR_LIST, ComplexPropertyHelper.getFieldType(field));
+        assertEquals("string", ComplexPropertyHelper.getFieldType(getListSubField(field)));
+    }
+
+    protected Field getField(String xpath) {
+        return Framework.getService(SchemaManager.class).getField(xpath);
+    }
+
+    protected Field getListSubField(Field listField) {
+        return ((ListType) listField.getType()).getField();
     }
 }
