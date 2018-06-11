ad55ec731d9dc4e9e3725f0a5754be5fefe76514
Pierre GAUTIER <pierregautier@nuxeo.com>
2018-06-11 10:46:53 +0200

    NXP-25068: fix BulkDownload key conflicts

--- nuxeo-features/nuxeo-automation/nuxeo-automation-core/src/main/java/org/nuxeo/ecm/automation/core/operations/blob/BulkDownload.java
+++ nuxeo-features/nuxeo-automation/nuxeo-automation-core/src/main/java/org/nuxeo/ecm/automation/core/operations/blob/BulkDownload.java
@@ -18,15 +18,11 @@
  */
 package org.nuxeo.ecm.automation.core.operations.blob;
 
-import java.io.IOException;
-import java.util.Calendar;
 import java.util.Collections;
 import java.util.List;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
-import org.apache.commons.codec.digest.DigestUtils;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.nuxeo.ecm.automation.core.Constants;
 import org.nuxeo.ecm.automation.core.annotations.Context;
 import org.nuxeo.ecm.automation.core.annotations.Operation;
@@ -38,7 +34,6 @@ import org.nuxeo.ecm.core.api.CoreSession;
 import org.nuxeo.ecm.core.api.DocumentModel;
 import org.nuxeo.ecm.core.api.DocumentModelList;
 import org.nuxeo.ecm.core.api.NuxeoException;
-import org.nuxeo.ecm.core.api.blobholder.BlobHolderAdapterService;
 import org.nuxeo.ecm.core.api.impl.blob.AsyncBlob;
 import org.nuxeo.ecm.core.io.download.DownloadService;
 import org.nuxeo.ecm.core.transientstore.api.TransientStore;
@@ -56,84 +51,30 @@ import org.nuxeo.runtime.api.Framework;
 @Operation(id = BulkDownload.ID, category = Constants.CAT_BLOB, label = "Bulk Downlaod", description = "Prepare a Zip of a list of documents which is build asynchrously. Produced Zip will be available in the TransientStore with the key returned by the JSON.")
 public class BulkDownload {
 
-    private static final Log log = LogFactory.getLog(BulkDownload.class);
-
     public static final String ID = "Blob.BulkDownload";
-
-    public static final String WORKERID_KEY = "workerid";
 
     @Context
     protected CoreSession session;
 
-    @Context
-    BlobHolderAdapterService blobHolderAdapterService;
-
     @Param(name = "filename", required = false)
     protected String fileName;
 
-    protected String buildTransientStoreKey(DocumentModelList docs) {
-        StringBuffer sb = new StringBuffer();
-        for (DocumentModel doc : docs) {
-            sb.append(doc.getId());
-            sb.append("::");
-            Calendar modif = (Calendar) doc.getPropertyValue("dc:modified");
-            if (modif != null) {
-                long millis = modif.getTimeInMillis();
-                // the date may have been rounded by the storage layer, normalize it to the second
-                millis -= millis % 1000;
-                sb.append(millis);
-                sb.append("::");
-            }
-        }
-        // Rendered documents might differ according to current user
-        sb.append(session.getPrincipal().getName());
-        return DigestUtils.md5Hex(sb.toString());
-    }
-
     @OperationMethod
-    public Blob run(DocumentModelList docs) throws IOException {
-        // build the key
-        String key = buildTransientStoreKey(docs);
+    public Blob run(DocumentModelList docs) {
         TransientStoreService tss = Framework.getService(TransientStoreService.class);
-
         TransientStore ts = tss.getStore(DownloadService.TRANSIENT_STORE_STORE_NAME);
         if (ts == null) {
             throw new NuxeoException("Unable to find download Transient Store");
         }
-        List<Blob> blobs = null;
-        if (!ts.exists(key)) {
-            log.trace("No async download already initialized");
-            Work work = new BlobListZipWork(key, session.getPrincipal().getName(), this.fileName,
-                    docs.stream().map(DocumentModel::getId).collect(Collectors.toList()),
-                    DownloadService.TRANSIENT_STORE_STORE_NAME);
-            ts.setCompleted(key, false);
-            ts.putParameter(key, WORKERID_KEY, work.getId());
-            blobs = Collections.singletonList(new AsyncBlob(key));
-            ts.putBlobs(key, blobs);
-            Framework.getService(WorkManager.class).schedule(work, Scheduling.IF_NOT_SCHEDULED);
-            return blobs.get(0);
-        } else {
-            log.trace("Async download already initialized");
-            blobs = ts.getBlobs(key);
-            if (ts.isCompleted(key)) {
-                if (blobs != null && blobs.size() == 1) {
-                    Blob blob = blobs.get(0);
-                    ts.release(key);
-                    return blob;
-                } else {
-                    ts.release(key);
-                    throw new NuxeoException("Cannot retrieve blob");
-                }
-
-            } else {
-                Work work = new BlobListZipWork(key, session.getPrincipal().getName(), this.fileName,
-                        docs.stream().map(DocumentModel::getId).collect(Collectors.toList()),
-                        DownloadService.TRANSIENT_STORE_STORE_NAME);
-                WorkManager wm = Framework.getService(WorkManager.class);
-                wm.schedule(work, Scheduling.IF_NOT_SCHEDULED);
-                return new AsyncBlob(key);
-            }
-        }
+        String key = UUID.randomUUID().toString();
+        List<String> docIds = docs.stream().map(DocumentModel::getId).collect(Collectors.toList());
+        Work work = new BlobListZipWork(key, session.getPrincipal().getName(), fileName, docIds,
+                DownloadService.TRANSIENT_STORE_STORE_NAME);
+        ts.setCompleted(key, false);
+        Blob blob = new AsyncBlob(key);
+        ts.putBlobs(key, Collections.singletonList(blob));
+        Framework.getService(WorkManager.class).schedule(work, Scheduling.IF_NOT_SCHEDULED);
+        return blob;
     }
 
 }
