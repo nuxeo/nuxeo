b29cdaff126e349c1c7e33cbee605077afe97130
Stephane Lacoin aka nxmatic <slacoin@nuxeo.com>
2018-06-15 10:32:09 +0200

    NXBT-1770 cleanup workspace after build if not incremental

--- integration/Jenkinsfiles/ondemand.groovy
+++ integration/Jenkinsfiles/ondemand.groovy
@@ -18,80 +18,83 @@ timestamps {
 
     node(SLAVE) {
         
-        def sha = {
-            def emitExtensions = { isCleanRequired = false ->
-                def extensions = []
-                if (isCleanRequired) {
-                    extensions.add([$class: 'CleanBeforeCheckout'])
+        try {
+            def sha = {
+                def emitExtensions = { isCleanRequired = false ->
+                    def extensions = []
+                    if (isCleanRequired) {
+                        extensions.add([$class: 'CleanBeforeCheckout'])
+                    }
+                    extensions.add([$class: 'CloneOption', noTags: true, reference: '', shallow: false])
+                    return extensions
                 }
-                extensions.add([$class: 'CloneOption', noTags: true, reference: '', shallow: false])
-                return extensions
+                
+                timeout(time: 10, unit: 'MINUTES') {
+                    checkout(
+                        [$class: 'GitSCM',
+                         branches: [[name: '*/${BRANCH}']],
+                         browser: [$class: 'GithubWeb', repoUrl: 'https://github.com/nuxeo/nuxeo'],
+                         doGenerateSubmoduleConfigurations: false,
+                         extensions: emitExtensions(isCleanRequired=!INCREMENTAL),
+                         submoduleCfg: [],
+                         userRemoteConfigs: [[url: 'git@github.com:nuxeo/nuxeo.git']]
+                        ])
+                    def sha = sh(returnStdout: true, script: 'git rev-parse HEAD').trim()
+                    return sha
+                }
+            }.call()
+
+            stage('rebase') {
+                timeout(time: 30, unit: 'MINUTES') {
+                    withBuildStatus('ondemand/1-rebase', 'https://github.com/nuxeo/nuxeo', sha, "${BUILD_URL}") {
+                        sh "./clone.py ${BRANCH} -f ${PARENT_BRANCH} --rebase"
+                    }
+                    stash(name: 'clone', excludes: '**/target/, **/node_modules/, **/bower_components/, **/marketplace/')
+                }
             }
+
+            stage('compile') {
+                timeout(time: 2, unit: 'HOURS') {
+                    withBuildStatus('ondemand/2-compile', 'https://github.com/nuxeo/nuxeo', sha, "${BUILD_URL}") {
+                        withMaven() {
+                            sh 'mvn -nsu -B test-compile -Pqa,addons,distrib -DskipTests'
+                        }
+                    }
+                }
+            }
+
+            stage('test') {
+                timeout(time: 2, unit: 'HOURS') {
+                    withBuildStatus('ondemand/3-test', 'https://github.com/nuxeo/nuxeo', sha, "${BUILD_URL}") {
+                        withMaven() { 
+                            sh 'mvn -nsu -B test -Pqa,addons,distrib -Dmaven.test.failure.ignore=true -Dnuxeo.tests.random.mode=STRICT'
+                        }
+                    }
+                }
+            }
+
+            def zipfile = stage('verify') {
+                timeout(time: 1, unit: 'HOURS') {
+                    withBuildStatus('ondemand/4-verify', 'https://github.com/nuxeo/nuxeo', sha, "${BUILD_URL}") {
+                        withMaven() {
+                            sh 'mvn -nsu -B verify -Pqa,addons,distrib,tomcat -DskipTests -Dmaven.test.failure.ignore=true -Dnuxeo.tests.random.mode=STRICT'
+                        }
+                    }
+                    return sh(returnStdout: true, script: 'echo -n ${WORKSPACE}/nuxeo-distribution/nuxeo-server-tomcat/target/nuxeo-server-tomcat-*.zip')
+                }
+            }
+
+            verifyProfile('pgsql', '9.6', sha, zipfile)
+
+            verifyProfile('mongodb', '3.4', sha, zipfile)
             
-            timeout(time: 10, unit: 'MINUTES') {
-                checkout(
-                    [$class: 'GitSCM',
-                     branches: [[name: '*/${BRANCH}']],
-                     browser: [$class: 'GithubWeb', repoUrl: 'https://github.com/nuxeo/nuxeo'],
-                     doGenerateSubmoduleConfigurations: false,
-                     extensions: emitExtensions(isCleanRequired=!INCREMENTAL),
-                     submoduleCfg: [],
-                     userRemoteConfigs: [[url: 'git@github.com:nuxeo/nuxeo.git']]
-                    ])
-                def sha = sh(returnStdout: true, script: 'git rev-parse HEAD').trim()
-                return sha
-            }
-        }.call()
-
-        stage('rebase') {
-            timeout(time: 30, unit: 'MINUTES') {
-                withBuildStatus('ondemand/1-rebase', 'https://github.com/nuxeo/nuxeo', sha, "${BUILD_URL}") {
-                    sh "./clone.py ${BRANCH} -f ${PARENT_BRANCH} --rebase"
-                }
-                stash(name: 'clone', excludes: '**/target/, **/node_modules/, **/bower_components/, **/marketplace/')
-            }
+            verifyProfile('kafka', '1.0', sha, zipfile)
         }
-
-        stage('compile') {
-            timeout(time: 2, unit: 'HOURS') {
-                withBuildStatus('ondemand/2-compile', 'https://github.com/nuxeo/nuxeo', sha, "${BUILD_URL}") {
-                    withMaven() {
-                        sh 'mvn -nsu -B test-compile -Pqa,addons,distrib -DskipTests'
-                    }
-                }
-            }
+    } finally {
+        if (!incremental) {
+            cleanWs(cleanWhenFailure: false, notFailBuild: true)
         }
-
-        stage('test') {
-            timeout(time: 2, unit: 'HOURS') {
-                withBuildStatus('ondemand/3-test', 'https://github.com/nuxeo/nuxeo', sha, "${BUILD_URL}") {
-                    withMaven() { 
-                        sh 'mvn -nsu -B test -Pqa,addons,distrib -Dmaven.test.failure.ignore=true -Dnuxeo.tests.random.mode=STRICT'
-                    }
-                }
-            }
-        }
-
-        def zipfile = stage('verify') {
-            timeout(time: 1, unit: 'HOURS') {
-                withBuildStatus('ondemand/4-verify', 'https://github.com/nuxeo/nuxeo', sha, "${BUILD_URL}") {
-                    withMaven() {
-                        sh 'mvn -nsu -B verify -Pqa,addons,distrib,tomcat -DskipTests -Dmaven.test.failure.ignore=true -Dnuxeo.tests.random.mode=STRICT'
-                    }
-                }
-                return sh(returnStdout: true, script: 'echo -n ${WORKSPACE}/nuxeo-distribution/nuxeo-server-tomcat/target/nuxeo-server-tomcat-*.zip')
-            }
-        }
-
-        verifyProfile('pgsql', '9.6', sha, zipfile)
-
-        verifyProfile('mongodb', '3.4', sha, zipfile)
-        
-        verifyProfile('kafka', '1.0', sha, zipfile)
     }
-
-
-
 }
 
 def verifyProfile(String profile, String version, String sha, String zipfile) {
