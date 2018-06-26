f4726c421dab08d6ceacc2d670b238b4f500e930
Kevin Leturc <kleturc@nuxeo.com>
2018-06-26 20:34:11 +0200

    NXP-25097: Changes after TransactionalFeature move to nuxeo-runtime-test

--- nuxeo-diff-pictures/src/test/java/org/nuxeo/diff/pictures/test/TestContentDiffIntegration.java
+++ nuxeo-diff-pictures/src/test/java/org/nuxeo/diff/pictures/test/TestContentDiffIntegration.java
@@ -36,11 +36,11 @@ import org.nuxeo.ecm.core.api.CoreSession;
 import org.nuxeo.ecm.core.api.DocumentModel;
 import org.nuxeo.ecm.core.api.impl.blob.FileBlob;
 import org.nuxeo.ecm.core.test.CoreFeature;
-import org.nuxeo.ecm.core.test.TransactionalFeature;
 import org.nuxeo.ecm.diff.content.ContentDiffAdapter;
 import org.nuxeo.runtime.test.runner.Deploy;
 import org.nuxeo.runtime.test.runner.Features;
 import org.nuxeo.runtime.test.runner.FeaturesRunner;
+import org.nuxeo.runtime.test.runner.TransactionalFeature;
 
 import com.google.inject.Inject;
 
