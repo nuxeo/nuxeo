946c7657d65c7f7fec943925c111105cd0f18f11
Kevin Leturc <kleturc@nuxeo.com>
2018-06-26 20:35:49 +0200

    NXP-25097: Changes after TransactionalFeature move to nuxeo-runtime-test

--- nuxeo-importer-core/src/test/java/org/nuxeo/ecm/platform/importer/tests/TestImporterWithFileManager.java
+++ nuxeo-importer-core/src/test/java/org/nuxeo/ecm/platform/importer/tests/TestImporterWithFileManager.java
@@ -35,13 +35,13 @@ import org.nuxeo.ecm.core.api.CoreSession;
 import org.nuxeo.ecm.core.api.DocumentModel;
 import org.nuxeo.ecm.core.api.PathRef;
 import org.nuxeo.ecm.core.test.CoreFeature;
-import org.nuxeo.ecm.core.test.TransactionalFeature;
 import org.nuxeo.ecm.core.test.annotations.Granularity;
 import org.nuxeo.ecm.core.test.annotations.RepositoryConfig;
 import org.nuxeo.ecm.platform.importer.service.DefaultImporterService;
 import org.nuxeo.runtime.test.runner.Deploy;
 import org.nuxeo.runtime.test.runner.Features;
 import org.nuxeo.runtime.test.runner.FeaturesRunner;
+import org.nuxeo.runtime.test.runner.TransactionalFeature;
 import org.nuxeo.runtime.transaction.TransactionHelper;
 
 @RunWith(FeaturesRunner.class)
