996c26b4fad991e9f8e6663f4a6b4b25e2357bc6
Benoit Delbosc <bdelbosc@nuxeo.com>
2018-06-27 15:35:33 +0200

    NXP-24672: Remove hard coded versions in stream.sh

--- nuxeo-distribution/nuxeo-server-tomcat/src/main/resources/tomcat/bin/stream.sh
+++ nuxeo-distribution/nuxeo-server-tomcat/src/main/resources/tomcat/bin/stream.sh
@@ -1,19 +1,6 @@
 #!/bin/bash
 ROOT=$(cd $(dirname $0)/..; pwd -P)
-CLASSPATH=$ROOT/lib/jcl-over-slf4j-1.7.21.jar:\
-$ROOT/lib/slf4j-api-1.7.21.jar:\
-$ROOT/lib/slf4j-log4j12-1.7.21.jar:\
-$ROOT/lib/log4j-1.2.17.jar:\
-$ROOT/nxserver/lib/commons-cli-1.4.jar:\
-$ROOT/nxserver/lib/chronicle-queue-4.6.44.jar:\
-$ROOT/nxserver/lib/kafka-clients-1.0.0.jar:\
-$ROOT/nxserver/lib/kafka_2.11-1.0.0.jar:\
-$ROOT/nxserver/lib/scala-library-2.11.7.jar:\
-$ROOT/nxserver/lib/zookeeper-3.4.8.jar:\
-$ROOT/nxserver/lib/jgrapht-core-1.0.1.jar:\
-$ROOT/nxserver/lib/jackson-databind-2.7.4.jar:\
-$ROOT/nxserver/lib/jackson-core-2.7.4.jar:\
-$ROOT/nxserver/lib/jackson-annotations-2.7.4.jar:\
-$ROOT/nxserver/lib/nuxeo-stream-9.10-SNAPSHOT.jar
+OG4J_CONF=tools-log4j.xml
+CLASSPATH="$ROOT/lib/*:$ROOT/nxserver/lib/*:$ROOT/nxserver/bundles/*"
 cd $ROOT
 java -cp $CLASSPATH org.nuxeo.lib.stream.tools.Main "$@"
