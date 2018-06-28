4d041597ef4b211b51de1d65a75d910eab37f82a
Benoit Delbosc <bdelbosc@nuxeo.com>
2018-06-28 12:38:19 +0200

    NXP-25203: Don't create the mongodb index ecm:fulltextJobId when fulltext is completly disabled

--- nuxeo-core/nuxeo-core-storage-mongodb/src/main/java/org/nuxeo/ecm/core/storage/mongodb/MongoDBRepository.java
+++ nuxeo-core/nuxeo-core-storage-mongodb/src/main/java/org/nuxeo/ecm/core/storage/mongodb/MongoDBRepository.java
@@ -223,7 +223,9 @@ public class MongoDBRepository extends DBSRepositoryBase {
         coll.createIndex(Indexes.ascending(KEY_PRIMARY_TYPE));
         coll.createIndex(Indexes.ascending(KEY_LIFECYCLE_STATE));
         coll.createIndex(Indexes.ascending(KEY_IS_TRASHED));
-        coll.createIndex(Indexes.ascending(KEY_FULLTEXT_JOBID));
+        if(!isFulltextDisabled()) {
+            coll.createIndex(Indexes.ascending(KEY_FULLTEXT_JOBID));
+        }
         coll.createIndex(Indexes.ascending(KEY_ACP + "." + KEY_ACL + "." + KEY_ACE_USER));
         coll.createIndex(Indexes.ascending(KEY_ACP + "." + KEY_ACL + "." + KEY_ACE_STATUS));
         // TODO configure these from somewhere else
