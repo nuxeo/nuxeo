0e47b6ba663684bff85dcf65f73c53dc71de2764
Gildas Lefevre <glefevre@nuxeo.com>
2018-06-28 17:26:00 -0400

    NXP-25264: Check if the id exists after computing the multitenant id

--- nuxeo-services/nuxeo-platform-directory/nuxeo-platform-directory-sql/src/main/java/org/nuxeo/ecm/directory/sql/SQLSession.java
+++ nuxeo-services/nuxeo-platform-directory/nuxeo-platform-directory-sql/src/main/java/org/nuxeo/ecm/directory/sql/SQLSession.java
@@ -665,19 +665,21 @@ public class SQLSession extends BaseSession {
             if (rawId == null) {
                 throw new DirectoryException("Missing id");
             }
-            String id = String.valueOf(rawId);
-            if (hasEntry(id)) {
-                throw new DirectoryException(String.format("Entry with id %s already exists", id));
-            }
 
+            String id = String.valueOf(rawId);
             if (isMultiTenant()) {
                 String tenantId = getCurrentTenantId();
                 if (!StringUtils.isBlank(tenantId)) {
                     fieldMap.put(TENANT_ID_FIELD, tenantId);
                     if (computeMultiTenantId) {
-                        fieldMap.put(idFieldName, computeMultiTenantDirectoryId(tenantId, id));
+                        id = computeMultiTenantDirectoryId(tenantId, id);
+                        fieldMap.put(idFieldName, id);
                     }
                 }
+            }
+
+            if (hasEntry(id)) {
+                throw new DirectoryException(String.format("Entry with id %s already exists", id));
             }
         }
 
