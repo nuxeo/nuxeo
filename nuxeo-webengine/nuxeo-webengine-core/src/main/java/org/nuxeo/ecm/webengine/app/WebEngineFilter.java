2fb412fcc8cd4748e2ea9141c8664df776c1d42f
Florent Guillaume <fguillaume@nuxeo.com>
2018-07-31 18:46:18 +0200

    NXP-24343: simplify WebEngineFilter

--- nuxeo-webengine/nuxeo-webengine-core/src/main/java/org/nuxeo/ecm/webengine/app/WebEngineFilter.java
+++ nuxeo-webengine/nuxeo-webengine-core/src/main/java/org/nuxeo/ecm/webengine/app/WebEngineFilter.java
@@ -20,7 +20,6 @@
 package org.nuxeo.ecm.webengine.app;
 
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
@@ -31,14 +30,10 @@ import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.commons.lang3.StringUtils;
-import org.nuxeo.ecm.platform.web.common.ServletHelper;
 import org.nuxeo.ecm.webengine.model.WebContext;
 
 /**
- * This filter must be declared after the nuxeo authentication filter since it needs an authentication info. The session
- * synchronization is done only if NuxeoRequestControllerFilter was not already done it and stateful flag for the
- * request path is true.
+ * This filter sets up the {@link WebContext}, made available through {@link WebEngine#getActiveContext}.
  */
 public class WebEngineFilter implements Filter {
 
@@ -53,72 +48,14 @@ public class WebEngineFilter implements Filter {
     }
 
     @Override
-    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain chain)
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
             throws IOException, ServletException {
-        HttpServletRequest request = (HttpServletRequest) servletRequest;
-        HttpServletResponse response = (HttpServletResponse) servletResponse;
-        boolean useTx = !isStatic(request);
-
-        preRequest(request);
-        ServletHelper.doFilter(new WebContextFilterChain(chain), request, response, useTx, true);
-        postRequest(request, response);
-    }
-
-    protected boolean isStatic(HttpServletRequest request) {
-        String pathInfo = StringUtils.defaultIfEmpty(request.getPathInfo(), "/");
-        return request.getServletPath().contains("/skin") || pathInfo.contains("/skin/");
-    }
-
-    protected void preRequest(HttpServletRequest request) {
-        // need to set the encoding of characters manually
-        if (request.getCharacterEncoding() == null) {
-            try {
-                request.setCharacterEncoding("UTF-8");
-            } catch (UnsupportedEncodingException e) {
-                // cannot happen
-                throw new RuntimeException(e);
-            }
-        }
-    }
-
-    protected void postRequest(HttpServletRequest request, HttpServletResponse response) {
-        // check if the target resource don't want automatic headers to be inserted
-        if (request.getAttribute("org.nuxeo.webengine.DisableAutoHeaders") != null) {
-            // insert automatic headers
-            response.addHeader("Pragma", "no-cache");
-            response.addHeader("Cache-Control", "no-cache");
-            response.addHeader("Cache-Control", "no-store");
-            response.addHeader("Cache-Control", "must-revalidate");
-            response.addHeader("Expires", "0");
-            response.setDateHeader("Expires", 0); // prevents caching
-        }
-    }
-
-    /**
-     * Wraps a filter chain to provide the WebEngine {@link WebContext} in the request attributes.
-     * <p>
-     * The filter we're executing ({@link ServletHelper#doFilter}) may wrap the response, and it's this wrapped version
-     * that must be available in the {@link WebContext}.
-     *
-     * @since 10.3
-     */
-    protected static class WebContextFilterChain implements FilterChain {
-
-        protected final FilterChain chain;
-
-        public WebContextFilterChain(FilterChain chain) {
-            this.chain = chain;
-        }
-
-        @Override
-        public void doFilter(ServletRequest request, ServletResponse response) throws IOException, ServletException {
-            request.setAttribute(WebContext.class.getName(),
-                    new DefaultContext((HttpServletRequest) request, (HttpServletResponse) response));
-            try {
-                chain.doFilter(request, response);
-            } finally {
-                request.removeAttribute(WebContext.class.getName());
-            }
+        String name = WebContext.class.getName();
+        request.setAttribute(name, new DefaultContext((HttpServletRequest) request, (HttpServletResponse) response));
+        try {
+            chain.doFilter(request, response);
+        } finally {
+            request.removeAttribute(name);
         }
     }
 
