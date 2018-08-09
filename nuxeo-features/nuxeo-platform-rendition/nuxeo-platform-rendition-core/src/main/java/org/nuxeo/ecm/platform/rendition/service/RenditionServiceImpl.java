ea92b857c17db13bd01a24be5fc098dd43e73e94
Antoine Taillefer <ataillefer@nuxeo.com>
2018-08-09 16:59:01 +0200

    NXP-25430: Always deliver an up-to-date lazy rendition, never storing a stale rendition

--- nuxeo-features/nuxeo-platform-rendition/nuxeo-platform-rendition-core/src/main/java/org/nuxeo/ecm/platform/rendition/service/RenditionServiceImpl.java
+++ nuxeo-features/nuxeo-platform-rendition/nuxeo-platform-rendition-core/src/main/java/org/nuxeo/ecm/platform/rendition/service/RenditionServiceImpl.java
@@ -19,19 +19,16 @@
 package org.nuxeo.ecm.platform.rendition.service;
 
 import static org.nuxeo.ecm.platform.rendition.Constants.RENDITION_SOURCE_ID_PROPERTY;
-import static org.nuxeo.ecm.platform.rendition.Constants.RENDITION_SOURCE_MODIFICATION_DATE_PROPERTY;
 import static org.nuxeo.ecm.platform.rendition.Constants.RENDITION_SOURCE_VERSIONABLE_ID_PROPERTY;
 
 import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.Collections;
 import java.util.Deque;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 import java.util.stream.Collectors;
 
 import javax.script.Invocable;
@@ -214,7 +211,8 @@ public class RenditionServiceImpl extends DefaultComponent implements RenditionS
         List<Blob> renderedBlobs = rendition.getBlobs();
         Blob renderedBlob = renderedBlobs.get(0);
         String mimeType = renderedBlob.getMimeType();
-        if (mimeType != null && mimeType.contains(LazyRendition.ERROR_MARKER)) {
+        if (mimeType != null
+                && (mimeType.contains(LazyRendition.ERROR_MARKER) || mimeType.contains(LazyRendition.STALE_MARKER))) {
             return null;
         }
 
@@ -383,18 +381,6 @@ public class RenditionServiceImpl extends DefaultComponent implements RenditionS
             // or a versionable doc that is not checkedout
             rendition = getStoredRenditionManager().findStoredRendition(doc, renditionDefinition);
             if (rendition != null) {
-                // Mark stored rendition as stale if needed
-                Calendar renditionModificationDate = (Calendar) rendition.getHostDocument().getPropertyValue(
-                        RENDITION_SOURCE_MODIFICATION_DATE_PROPERTY);
-                String sourceDocumentModificationDatePropertyName = renditionDefinition.getSourceDocumentModificationDatePropertyName();
-                Calendar currentModificationDate = (Calendar) doc.getPropertyValue(
-                        sourceDocumentModificationDatePropertyName);
-                if (!Objects.equals(renditionModificationDate, currentModificationDate)) {
-                    Blob blob = rendition.getBlob();
-                    if (blob != null) {
-                        blob.setMimeType(blob.getMimeType() + ";" + LazyRendition.STALE_MARKER);
-                    }
-                }
                 return rendition;
             }
         }
