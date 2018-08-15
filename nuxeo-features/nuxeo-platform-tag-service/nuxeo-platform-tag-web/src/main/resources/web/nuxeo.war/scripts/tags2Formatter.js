c36dd2ac5e865d3587db92b04d4b3da0496cf59f
Thierry Martins <tmartins@nuxeo.com>
2018-08-15 20:08:25 +0200

    NXP-25481: sanitize tag name while presenting the suggestions

--- nuxeo-features/nuxeo-platform-tag-service/nuxeo-platform-tag-web/src/main/resources/web/nuxeo.war/scripts/tags2Formatter.js
+++ nuxeo-features/nuxeo-platform-tag-service/nuxeo-platform-tag-web/src/main/resources/web/nuxeo.war/scripts/tags2Formatter.js
@@ -2,8 +2,9 @@ var nuxeo = nuxeo || {};
 
 function createNewTag(term, data) {
     return {
-        id : term,
-        displayLabel : term,
+        // sanitize the term for the new tag
+        id : term.sanitize(true),
+        displayLabel : term.sanitize(true),
         newTag : true
     };
 }
@@ -30,4 +31,21 @@ function formatSelectedTags(tag) {
     var jsFragment = "listDocumentsForTag('" + escapeHTML(tag.displayLabel) + "');";
     return '<span class="s2newTag"><a href="' + window.nxContextPath + '/search/tag_search_results.faces?conversationId=' + currentConversationId + '" onclick="' + jsFragment + '">'
             + escapeHTML(tag.displayLabel) + '</a></span>'
+}
+
+/*
+This `sanitize` function is based on the cleanup done by TagServiceImpl#cleanLabel:
+- lowercase
+- no space
+- no slash
+- no antislash
+- no quote
+- no percent
+*/
+String.prototype.sanitize = function(ignoreCase) {
+    if (ignoreCase) {
+        return this.replace(/[\/'% \\]/g, '').toLowerCase();
+    } else {
+        return this.replace(/[\/'% \\]/g, '');
+    }
 }
