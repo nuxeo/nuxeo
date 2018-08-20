21bff4f3c66ed510a21b7cc4c09aacc54a5ad5f4
Funsho David <fdavid@nuxeo.com>
2018-08-20 16:02:35 +0200

    NXP-25405: add comment interface

---
+++ nuxeo-features/nuxeo-platform-comment/nuxeo-platform-comment-api/src/main/java/org/nuxeo/ecm/platform/comment/api/Comment.java
@@ -0,0 +1,100 @@
+/*
+ * (C) Copyright 2018 Nuxeo (http://nuxeo.com/) and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Contributors:
+ *     Funsho David
+ */
+
+package org.nuxeo.ecm.platform.comment.api;
+
+import java.time.Instant;
+
+/**
+ * Comment interface.
+ * 
+ * @since 10.3
+ */
+public interface Comment {
+
+    /**
+     * Gets commented document id.
+     *
+     * @return the commented document id
+     */
+    String getDocumentId();
+
+    /**
+     * Sets commented document id.
+     *
+     * @param documentId the commented document id
+     */
+    void setDocumentId(String documentId);
+
+    /**
+     * Gets comment author.
+     * 
+     * @return the author
+     */
+    String getAuthor();
+
+    /**
+     * Sets comment author.
+     * 
+     * @param author the author
+     */
+    void setAuthor(String author);
+
+    /**
+     * Gets comment text.
+     * 
+     * @return the text
+     */
+    String getText();
+
+    /**
+     * Sets comment text.
+     * 
+     * @param text the text
+     */
+    void setText(String text);
+
+    /**
+     * Gets comment creation date.
+     * 
+     * @return the creation date
+     */
+    Instant getCreationDate();
+
+    /**
+     * Sets comment creation date.
+     * 
+     * @param creationDate the creation date
+     */
+    void setCreationDate(Instant creationDate);
+
+    /**
+     * Sets comment modification date.
+     * 
+     * @return the modification date
+     */
+    Instant getModificationDate();
+
+    /**
+     * Sets comment modification date.
+     * 
+     * @param modificationDate the modification date
+     */
+    void setModificationDate(Instant modificationDate);
+}
