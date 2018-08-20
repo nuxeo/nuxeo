8949aa84a3cdec203379e0dbc1260e58dcce9248
Antoine Taillefer <ataillefer@nuxeo.com>
2018-08-20 14:11:27 +0200

    NXP-25551: Format + Cleanup

--- nuxeo-features/nuxeo-thumbnail/src/main/java/org/nuxeo/ecm/platform/thumbnail/listener/CheckBlobUpdateListener.java
+++ nuxeo-features/nuxeo-thumbnail/src/main/java/org/nuxeo/ecm/platform/thumbnail/listener/CheckBlobUpdateListener.java
@@ -60,8 +60,8 @@ public class CheckBlobUpdateListener implements EventListener {
 
             if (content.getValue() != null) {
                 doc.addFacet(ThumbnailConstants.THUMBNAIL_FACET);
-                Framework.getService(EventService.class).fireEvent(
-                        ThumbnailConstants.EventNames.scheduleThumbnailUpdate.name(), context);
+                Framework.getService(EventService.class)
+                         .fireEvent(ThumbnailConstants.EventNames.scheduleThumbnailUpdate.name(), context);
             }
         }
     }
