628ea0531b422e588ed6a55001f9a11ed9b47880
Guillaume Renard <grenard@nuxeo.com>
2018-08-21 16:52:30 +0200

    NXP-24298: add HasContentJsonEnricher

---
+++ nuxeo-features/rest-api/nuxeo-rest-api-server/src/main/java/org/nuxeo/ecm/restapi/server/jaxrs/enrichers/HasContentJsonEnricher.java
@@ -0,0 +1,77 @@
+/*
+ * (C) Copyright 2016 Nuxeo SA (http://nuxeo.com/) and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Contributors:
+ *     Guillaume Renard <grenard@nuxeo.com>
+ */
+
+package org.nuxeo.ecm.restapi.server.jaxrs.enrichers;
+
+import static org.nuxeo.ecm.core.io.registry.reflect.Instantiations.SINGLETON;
+import static org.nuxeo.ecm.core.io.registry.reflect.Priorities.REFERENCE;
+
+import java.io.IOException;
+
+import org.nuxeo.ecm.collections.api.CollectionManager;
+import org.nuxeo.ecm.core.api.DocumentModel;
+import org.nuxeo.ecm.core.api.DocumentModelList;
+import org.nuxeo.ecm.core.io.marshallers.json.enrichers.AbstractJsonEnricher;
+import org.nuxeo.ecm.core.io.registry.context.RenderingContext.SessionWrapper;
+import org.nuxeo.ecm.core.io.registry.reflect.Setup;
+import org.nuxeo.ecm.core.query.sql.NXQL;
+import org.nuxeo.runtime.api.Framework;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+
+/**
+ * @since 10.3
+ */
+@Setup(mode = SINGLETON, priority = REFERENCE)
+public class HasContentJsonEnricher extends AbstractJsonEnricher<DocumentModel> {
+
+    public static final String NAME = "hasContent";
+
+    public HasContentJsonEnricher() {
+        super(NAME);
+    }
+
+    @Override
+    public void write(JsonGenerator jg, DocumentModel document) throws IOException {
+        if (document.isFolder()) {
+            try (SessionWrapper wrapper = ctx.getSession(document)) {
+                String fetchChildQuery = "SELECT * FROM Document WHERE ecm:mixinType != 'HiddenInNavigation' AND ecm:isTrashed = 0 AND ecm:parentId = "
+                        + NXQL.escapeString(document.getId());
+                // Limit result set to 1 as we just want to know if there's at least one child
+                DocumentModelList children = wrapper.getSession().query(fetchChildQuery, 1);
+                jg.writeBooleanField(NAME, !children.isEmpty());
+            }
+
+        } else {
+            CollectionManager collectionManager = Framework.getService(CollectionManager.class);
+            if (collectionManager.isCollection(document)) {
+                try (SessionWrapper wrapper = ctx.getSession(document)) {
+                    String fetchMembersQuery = "SELECT * FROM Document WHERE ecm:mixinType != 'HiddenInNavigation' AND ecm:isTrashed = 0 AND collectionMember:collectionIds/* = "
+                            + NXQL.escapeString(document.getId());
+                    // Limit result set to 1 as we just want to know if there's at least one member
+                    DocumentModelList members = wrapper.getSession().query(fetchMembersQuery, 1);
+                    jg.writeBooleanField(NAME, !members.isEmpty());
+                }
+            } else {
+                jg.writeBooleanField(NAME, false);
+            }
+        }
+    }
+
+}
