04ba57df795249f6e542388e8461b2e672799a67
Kevin Leturc <kleturc@nuxeo.com>
2018-09-27 18:07:17 +0200

    NXP-24951: Fix stream.sh issue with log4j

--- nuxeo-distribution/nuxeo-server-tomcat/src/main/resources/tomcat/bin/stream.sh
+++ nuxeo-distribution/nuxeo-server-tomcat/src/main/resources/tomcat/bin/stream.sh
@@ -3,4 +3,4 @@ ROOT=$(cd $(dirname $0)/..; pwd -P)
 LOG4J_CONF=tools-log4j2.xml
 CLASSPATH="$ROOT/lib/*:$ROOT/nxserver/lib/*:$ROOT/nxserver/bundles/*"
 cd $ROOT
-java -cp $CLASSPATH -Dlog4j.configuration="$LOG4J_CONF" org.nuxeo.lib.stream.tools.Main "$@"
+java -cp $CLASSPATH -Dlog4j.configurationFile="$LOG4J_CONF" org.nuxeo.lib.stream.tools.Main "$@"
