3dd785ac8e8e9bcb7a01d1901baa6e5a382b2ad5
Funsho David <fdavid@nuxeo.com>
2018-10-11 13:30:08 +0200

    NXP-25633: add new scenario for bulk update folders

---
+++ nuxeo-distribution/nuxeo-jsf-ui-gatling-tests/src/test/scala/org/nuxeo/cap/bench/Sim25BulkUpdateFolders.scala
@@ -0,0 +1,51 @@
+/*
+ * (C) Copyright 2018 Nuxeo (http://nuxeo.com/) and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Contributors:
+ *     Funsho David
+ */
+
+package org.nuxeo.cap.bench
+
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+
+object ScnBulkUpdateFolders {
+
+  def get = (folders: Iterator[Map[String, String]]) => {
+    scenario("BulkUpdateFolders").exec(
+      asLongAs(session => Feeders.notEmpty(session), exitASAP = true) {
+        feed(Feeders.admins)
+          .feed(folders)
+          .exec(NuxeoBulk.bulkUpdateDocument("SELECT * from Document WHERE ecm:path = '" + Constants.GAT_WS_PATH + "${parentPath}'", "dc:description", "bulk folder")
+            .asJSON.check(jsonPath("$.commandId").saveAs("commandId")))
+          .exec(NuxeoBulk.waitForAction("${commandId}"))
+      }
+    )
+  }
+
+}
+
+class Sim25BulkUpdateFolders extends Simulation {
+  val httpProtocol = http
+    .baseURL(Parameters.getBaseUrl())
+    .disableWarmUp
+    .acceptEncodingHeader("gzip, deflate")
+    .connectionHeader("keep-alive")
+  val folders = Feeders.createFolderFeeder()
+  val scn = ScnBulkUpdateFolders.get(folders)
+  setUp(scn.inject(rampUsers(Parameters.getConcurrentUsers()).over(Parameters.getRampDuration())))
+    .protocols(httpProtocol)
+}
