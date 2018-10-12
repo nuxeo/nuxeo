279269717dbf0d9ce6e7e410d5ce5fcc5fc2d099
Pierre GAUTIER <pierregautier@nuxeo.com>
2018-10-12 14:44:57 +0200

    NXP-25940: allow bulk tests to log as user

--- nuxeo-core/nuxeo-core-bulk/src/test/java/org/nuxeo/ecm/core/bulk/CoreBulkFeature.java
+++ nuxeo-core/nuxeo-core-bulk/src/test/java/org/nuxeo/ecm/core/bulk/CoreBulkFeature.java
@@ -20,12 +20,8 @@ package org.nuxeo.ecm.core.bulk;
 
 import java.time.Duration;
 
-import javax.security.auth.login.LoginContext;
-import javax.security.auth.login.LoginException;
-
 import org.nuxeo.ecm.core.io.CoreIOFeature;
 import org.nuxeo.runtime.api.Framework;
-import org.nuxeo.runtime.api.login.LoginAs;
 import org.nuxeo.runtime.stream.RuntimeStreamFeature;
 import org.nuxeo.runtime.test.runner.Deploy;
 import org.nuxeo.runtime.test.runner.Features;
@@ -33,7 +29,6 @@ import org.nuxeo.runtime.test.runner.FeaturesRunner;
 import org.nuxeo.runtime.test.runner.RunnerFeature;
 import org.nuxeo.runtime.test.runner.RuntimeFeature;
 import org.nuxeo.runtime.test.runner.TransactionalFeature;
-import org.nuxeo.runtime.test.runner.TransactionalFeature.Waiter;
 
 /**
  * Intermediate feature for nuxeo-core-bulk module.
@@ -46,26 +41,10 @@ import org.nuxeo.runtime.test.runner.TransactionalFeature.Waiter;
 @Features({ RuntimeFeature.class, TransactionalFeature.class, RuntimeStreamFeature.class, CoreIOFeature.class })
 public class CoreBulkFeature implements RunnerFeature {
 
-    public static class DummyLogin implements LoginAs {
-
-        @Override
-        public LoginContext loginAs(String username) throws LoginException {
-            return Framework.login();
-        }
-    }
-
-    public static class BulkWaiter implements Waiter {
-        @Override
-        public boolean await(Duration duration) throws InterruptedException {
-            BulkService bulks = Framework.getService(BulkService.class);
-            return bulks.await(duration);
-        }
-
-    }
-
     @Override
     public void initialize(FeaturesRunner runner) {
-        runner.getFeature(TransactionalFeature.class).addWaiter(new BulkWaiter());
+        runner.getFeature(TransactionalFeature.class)
+              .addWaiter(deadline -> Framework.getService(BulkService.class).await(Duration.ofMinutes(1)));
     }
 
 }
