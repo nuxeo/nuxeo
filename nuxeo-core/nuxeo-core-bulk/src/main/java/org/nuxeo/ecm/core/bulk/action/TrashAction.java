eb6ccf7caee23a0b2ae2b3cff87b4e0ecbf0da05
Pierre GAUTIER <pierregautier@nuxeo.com>
2018-10-24 18:16:09 +0200

    NXP-26012: create, test and use TrashAction

---
+++ nuxeo-core/nuxeo-core-bulk/src/main/java/org/nuxeo/ecm/core/bulk/action/TrashAction.java
@@ -0,0 +1,154 @@
+/*
+ * (C) Copyright 2018 Nuxeo (http://nuxeo.com/) and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Contributors:
+ *     Funsho David
+ */
+
+package org.nuxeo.ecm.core.bulk.action;
+
+import static org.nuxeo.ecm.core.api.event.CoreEventConstants.REPOSITORY_NAME;
+import static org.nuxeo.ecm.core.api.event.CoreEventConstants.SESSION_ID;
+import static org.nuxeo.ecm.core.api.event.DocumentEventCategories.EVENT_DOCUMENT_CATEGORY;
+import static org.nuxeo.ecm.core.api.trash.TrashService.DOCUMENT_TRASHED;
+import static org.nuxeo.ecm.core.api.trash.TrashService.DOCUMENT_UNTRASHED;
+import static org.nuxeo.ecm.core.bulk.BulkServiceImpl.STATUS_STREAM;
+import static org.nuxeo.lib.stream.computation.AbstractComputation.INPUT_1;
+import static org.nuxeo.lib.stream.computation.AbstractComputation.OUTPUT_1;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.nuxeo.ecm.core.api.CoreSession;
+import org.nuxeo.ecm.core.api.DocumentModelList;
+import org.nuxeo.ecm.core.api.DocumentRef;
+import org.nuxeo.ecm.core.api.IdRef;
+import org.nuxeo.ecm.core.api.IterableQueryResult;
+import org.nuxeo.ecm.core.api.NuxeoException;
+import org.nuxeo.ecm.core.api.PropertyException;
+import org.nuxeo.ecm.core.bulk.action.computation.AbstractBulkComputation;
+import org.nuxeo.ecm.core.event.Event;
+import org.nuxeo.ecm.core.event.EventService;
+import org.nuxeo.ecm.core.event.impl.DocumentEventContext;
+import org.nuxeo.ecm.core.query.sql.NXQL;
+import org.nuxeo.lib.stream.computation.Topology;
+import org.nuxeo.runtime.api.Framework;
+import org.nuxeo.runtime.stream.StreamProcessorTopology;
+
+/**
+ * @since 10.3
+ */
+public class TrashAction implements StreamProcessorTopology {
+
+    public static final String ACTION_NAME = "trash";
+
+    public static final String PARAM_NAME = "value";
+
+    public static final String PROXY_QUERY_TEMPLATE = "SELECT ecm:uuid FROM Document WHERE ecm:isProxy=1 AND ecm:uuid IN ('%s')";
+
+    public static final String SYSPROP_QUERY_TEMPLATE = "SELECT ecm:uuid FROM Document WHERE ecm:isProxy=0 AND ecm:isTrashed=%s AND ecm:uuid IN ('%s')";
+
+    @Override
+    public Topology getTopology(Map<String, String> options) {
+        return Topology.builder()
+                       .addComputation(TrashComputation::new,
+                               Arrays.asList(INPUT_1 + ":" + ACTION_NAME, OUTPUT_1 + ":" + STATUS_STREAM))
+                       .build();
+    }
+
+    public static class TrashComputation extends AbstractBulkComputation {
+
+        private static final Logger log = LogManager.getLogger(TrashComputation.class);
+
+        public TrashComputation() {
+            super(ACTION_NAME);
+        }
+
+        @Override
+        protected void compute(CoreSession session, List<String> ids, Map<String, Serializable> properties) {
+            Boolean trashValue = (Boolean) properties.get(PARAM_NAME);
+            if (trashValue) {
+                removeProxies(session, ids);
+            }
+            setSystemProperty(session, ids, trashValue);
+        }
+
+        protected void removeProxies(CoreSession session, List<String> ids) {
+            Set<DocumentRef> proxies = new HashSet<>();
+            String query = String.format(PROXY_QUERY_TEMPLATE, String.join("', '", ids));
+            try (IterableQueryResult res = session.queryAndFetch(query, NXQL.NXQL)) {
+                for (Map<String, Serializable> map : res) {
+                    proxies.add(new IdRef((String) map.get(NXQL.ECM_UUID)));
+                }
+            }
+            session.removeDocuments(proxies.toArray(new DocumentRef[0]));
+            try {
+                session.save();
+            } catch (PropertyException e) {
+                // TODO send to error stream
+                log.warn("Cannot save session", e);
+            }
+        }
+
+        public void setSystemProperty(CoreSession session, List<String> ids, Boolean value) {
+            List<DocumentRef> updatedRefs = new ArrayList<>(ids.size());
+            String query = String.format(SYSPROP_QUERY_TEMPLATE, value ? "0" : "1", String.join("', '", ids));
+            try (IterableQueryResult res = session.queryAndFetch(query, NXQL.NXQL)) {
+                for (Map<String, Serializable> map : res) {
+                    DocumentRef ref = new IdRef((String) map.get(NXQL.ECM_UUID));
+                    try {
+                        session.setDocumentSystemProp(ref, "isTrashed", value);
+                        updatedRefs.add(ref);
+                    } catch (NuxeoException e) {
+                        // TODO send to error stream
+                        log.warn("Cannot set system property: isTrashed on: " + ref.toString(), e);
+                    }
+                }
+            }
+            try {
+                session.save();
+                if (!updatedRefs.isEmpty()) {
+                    fireEvent(session, value ? DOCUMENT_TRASHED : DOCUMENT_UNTRASHED, updatedRefs);
+                }
+            } catch (PropertyException e) {
+                // TODO send to error stream
+                log.warn("Cannot save session", e);
+            }
+        }
+
+        protected void fireEvent(CoreSession session, String eventId, Collection<DocumentRef> refs) {
+            EventService eventService = Framework.getService(EventService.class);
+            DocumentModelList docs = session.getDocuments(refs.toArray(new DocumentRef[0]));
+            docs.forEach(d -> {
+                DocumentEventContext ctx = new DocumentEventContext(session, session.getPrincipal(), d);
+                ctx.setProperty(REPOSITORY_NAME, session.getRepositoryName());
+                ctx.setProperty(SESSION_ID, session.getSessionId());
+                ctx.setCategory(EVENT_DOCUMENT_CATEGORY);
+                Event event = ctx.newEvent(eventId);
+                event.setImmediate(false);
+                event.setInline(false);
+                eventService.fireEvent(event);
+            });
+        }
+    }
+}
---
+++ nuxeo-core/nuxeo-core-test/src/test/java/org/nuxeo/ecm/core/bulk/TestTrashAction.java
@@ -0,0 +1,119 @@
+/*
+ * (C) Copyright 2018 Nuxeo (http://nuxeo.com/) and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Contributors:
+ *     pierre
+ */
+package org.nuxeo.ecm.core.bulk;
+
+import static java.lang.Boolean.TRUE;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.nuxeo.ecm.core.api.trash.TrashService.DOCUMENT_TRASHED;
+import static org.nuxeo.ecm.core.bulk.DocumentSetRepositoryInit.created_non_proxy;
+import static org.nuxeo.ecm.core.bulk.DocumentSetRepositoryInit.created_total;
+import static org.nuxeo.ecm.core.bulk.action.TrashAction.ACTION_NAME;
+import static org.nuxeo.ecm.core.bulk.message.BulkStatus.State.COMPLETED;
+import static org.nuxeo.ecm.core.trash.PropertyTrashService.SYSPROP_IS_TRASHED;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.HashSet;
+
+import javax.inject.Inject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.nuxeo.ecm.core.api.CoreSession;
+import org.nuxeo.ecm.core.api.DocumentModel;
+import org.nuxeo.ecm.core.api.PathRef;
+import org.nuxeo.ecm.core.bulk.message.BulkCommand;
+import org.nuxeo.ecm.core.bulk.message.BulkStatus;
+import org.nuxeo.ecm.core.event.EventService;
+import org.nuxeo.ecm.core.event.impl.EventListenerDescriptor;
+import org.nuxeo.ecm.core.test.CoreFeature;
+import org.nuxeo.ecm.core.test.annotations.RepositoryConfig;
+import org.nuxeo.runtime.test.runner.Deploy;
+import org.nuxeo.runtime.test.runner.Features;
+import org.nuxeo.runtime.test.runner.FeaturesRunner;
+import org.nuxeo.runtime.test.runner.TransactionalFeature;
+
+@RunWith(FeaturesRunner.class)
+@Features(CoreFeature.class)
+@Deploy("org.nuxeo.ecm.core.test.tests:OSGI-INF/test-repo-core-types-contrib.xml")
+@RepositoryConfig(init = DocumentSetRepositoryInit.class)
+public class TestTrashAction {
+
+    @Inject
+    public BulkService service;
+
+    @Inject
+    public CoreSession session;
+
+    @Inject
+    public EventService eventService;
+
+    @Inject
+    public TransactionalFeature txFeature;
+
+    private static class TestListener extends EventListenerDescriptor {
+
+        private int count;
+
+        private TestListener() {
+            events = new HashSet<>(Collections.singleton(DOCUMENT_TRASHED));
+        }
+
+        @Override
+        public void initListener() {
+            inLineListener = event -> count++;
+        }
+    }
+
+    @Test
+    public void test() throws Exception {
+
+        DocumentModel model = session.getDocument(new PathRef("/default-domain/workspaces/test"));
+        String nxql = String.format("SELECT * from Document where ecm:ancestorId='%s'", model.getId());
+
+        TestListener listener = new TestListener();
+
+        eventService.addEventListener(listener);
+
+        String commandId = service.submit(
+                new BulkCommand.Builder(ACTION_NAME, nxql).repository(session.getRepositoryName())
+                                                          .user(session.getPrincipal().getName())
+                                                          .param("value", Boolean.TRUE)
+                                                          .build());
+
+        assertTrue("Bulk action didn't finish", service.await(Duration.ofSeconds(600)));
+
+        BulkStatus status = service.getStatus(commandId);
+        assertNotNull(status);
+        assertEquals(COMPLETED, status.getState());
+        assertEquals(created_total, status.getProcessed());
+
+        txFeature.nextTransaction();
+
+        for (DocumentModel descendant : session.query(nxql)) {
+            assertEquals(TRUE, session.getDocumentSystemProp(descendant.getRef(), SYSPROP_IS_TRASHED, Boolean.class));
+        }
+        assertEquals(created_non_proxy, listener.count);
+
+        eventService.removeEventListener(listener);
+    }
+
+}
