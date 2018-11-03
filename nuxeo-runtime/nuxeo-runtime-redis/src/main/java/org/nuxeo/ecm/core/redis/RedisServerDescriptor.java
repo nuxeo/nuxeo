c97d2dedbe661df6994d385236e0e062827b84f4
Florent Guillaume <fguillaume@nuxeo.com>
2018-11-03 03:26:14 +0100

    NXP-26073: allow configuration of Redis TLS/SSL trustStore/keyStore

--- nuxeo-runtime/nuxeo-runtime-redis/src/main/java/org/nuxeo/ecm/core/redis/RedisServerDescriptor.java
+++ nuxeo-runtime/nuxeo-runtime-redis/src/main/java/org/nuxeo/ecm/core/redis/RedisServerDescriptor.java
@@ -18,7 +18,19 @@
  */
 package org.nuxeo.ecm.core.redis;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.security.GeneralSecurityException;
+import java.security.KeyStore;
+
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSocketFactory;
+
 import org.apache.commons.lang3.StringUtils;
+import org.apache.http.ssl.SSLContextBuilder;
+import org.apache.http.ssl.SSLContexts;
 import org.nuxeo.common.xmap.annotation.XNode;
 import org.nuxeo.common.xmap.annotation.XObject;
 
@@ -41,12 +53,50 @@ public class RedisServerDescriptor extends RedisPoolDescriptor {
     @XNode("port")
     public int port = Protocol.DEFAULT_PORT;
 
+    /** @since 10.3 */
+    @XNode("ssl")
+    public boolean ssl;
+
+    /** @since 10.3 */
+    @XNode("trustStorePath")
+    public String trustStorePath;
+
+    /** @since 10.3 */
+    @XNode("trustStorePassword")
+    public String trustStorePassword;
+
+    /** @since 10.3 */
+    @XNode("trustStoreType")
+    public String trustStoreType;
+
+    /** @since 10.3 */
+    @XNode("keyStorePath")
+    public String keyStorePath;
+
+    /** @since 10.3 */
+    @XNode("keyStorePassword")
+    public String keyStorePassword;
+
+    /** @since 10.3 */
+    @XNode("keyStoreType")
+    public String keyStoreType;
+
     @XNode("failoverTimeout")
     public int failoverTimeout = 300;
 
     @Override
     public RedisExecutor newExecutor() {
-        try (Jedis jedis = new Jedis(host, port)) {
+        SSLContext sslContext = getSSLContext();
+        boolean useSSL;
+        SSLSocketFactory sslSocketFactory;
+        if (sslContext == null) {
+            useSSL = ssl;
+            sslSocketFactory = null;
+        } else {
+            useSSL = true;
+            sslSocketFactory = sslContext.getSocketFactory();
+        }
+        try (Jedis jedis = new Jedis(host, port, useSSL, sslSocketFactory, null, null)) {
             if (StringUtils.isNotBlank(password)) {
                 jedis.auth(password);
             }
@@ -62,6 +112,40 @@ public class RedisServerDescriptor extends RedisPoolDescriptor {
         RedisExecutor base = new RedisPoolExecutor(new JedisPool(conf, host, port, timeout,
                 StringUtils.defaultIfBlank(password, null), database));
         return new RedisFailoverExecutor(failoverTimeout, base);
+    }
+
+    protected SSLContext getSSLContext() {
+        try {
+            KeyStore trustStore = loadKeyStore(trustStorePath, trustStorePassword, trustStoreType);
+            KeyStore keyStore = loadKeyStore(keyStorePath, keyStorePassword, keyStoreType);
+            if (trustStore == null && keyStore == null) {
+                return null;
+            }
+            SSLContextBuilder sslContextBuilder = SSLContexts.custom();
+            if (trustStore != null) {
+                sslContextBuilder.loadTrustMaterial(trustStore, null);
+            }
+            if (keyStore != null) {
+                sslContextBuilder.loadKeyMaterial(keyStore, null);
+            }
+            return sslContextBuilder.build();
+        } catch (GeneralSecurityException | IOException e) {
+            throw new RuntimeException("Cannot setup SSL context", e);
+        }
+    }
+
+    protected KeyStore loadKeyStore(String path, String password, String type)
+            throws GeneralSecurityException, IOException {
+        if (StringUtils.isBlank(path)) {
+            return null;
+        }
+        String keyStoreType = StringUtils.defaultIfBlank(type, KeyStore.getDefaultType());
+        KeyStore keyStore = KeyStore.getInstance(keyStoreType);
+        char[] passwordChars = StringUtils.isBlank(password) ? null : password.toCharArray();
+        try (InputStream is = Files.newInputStream(Paths.get(path))) {
+            keyStore.load(is, passwordChars);
+        }
+        return keyStore;
     }
 
 }
