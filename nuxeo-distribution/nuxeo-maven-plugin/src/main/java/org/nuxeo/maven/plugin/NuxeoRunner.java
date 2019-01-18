93f94fe1b66e42e230246abc51b14f8251925162
Kevin Leturc <kleturc@nuxeo.com>
2019-01-18 12:39:51 +0100

    Cleanup + log4j2

--- nuxeo-distribution/nuxeo-maven-plugin/src/main/java/org/nuxeo/maven/plugin/NuxeoRunner.java
+++ nuxeo-distribution/nuxeo-maven-plugin/src/main/java/org/nuxeo/maven/plugin/NuxeoRunner.java
@@ -1,55 +1,42 @@
 package org.nuxeo.maven.plugin;
 
+import static java.util.stream.Collectors.toList;
+
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.lang.reflect.Method;
-import java.net.MalformedURLException;
-import java.net.URI;
-import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.jar.Attributes;
+import java.util.jar.JarFile;
 import java.util.jar.Manifest;
 
-import org.nuxeo.osgi.BundleFile;
-import org.nuxeo.osgi.DirectoryBundleFile;
-import org.nuxeo.osgi.JarBundleFile;
+import org.nuxeo.osgi.BundleManifestReader;
 import org.nuxeo.osgi.application.loader.FrameworkLoader;
-import org.nuxeo.runtime.RuntimeServiceException;
 
 import io.github.lukehutch.fastclasspathscanner.FastClasspathScanner;
 
 public class NuxeoRunner implements Runnable {
+
+    protected static Attributes.Name NUXEO_COMPONENT_NAME = new Attributes.Name(BundleManifestReader.COMPONENT_HEADER);
 
     @Override
     public void run() {
         Thread thread = Thread.currentThread();
         ClassLoader classLoader = thread.getContextClassLoader();
         try {
-            File workingDir = File.createTempFile("nxrunner-", null, new File("target"));
-            // OSGiAdapter osgi = new OSGiAdapter(workingDir);
-            // BundleFile bf = new SystemBundleFile(workingDir);
-            // StandaloneBundleLoader bundleLoader = new StandaloneBundleLoader(osgi, classLoader);
-            //
-            // classLoader = bundleLoader.getSharedClassLoader().getLoader();
-            // Thread.currentThread().setContextClassLoader(classLoader);
-            //
-            // SystemBundle systemBundle = new SystemBundle(osgi, bf, classLoader);
-            // osgi.setSystemBundle(systemBundle);
-            //
-            // bundleLoader.setScanForNestedJARs(false); // for now
-            // bundleLoader.setExtractNestedJARs(false);
-            //
-            // BundleFile bundleFile = lookupBundle("org.nuxeo.runtime");
-            // Bundle runtimeBundle = null;
-            // runtimeBundle.start();
-            //
-            // OSGiRuntimeService runtime = (OSGiRuntimeService) Framework.getRuntime();
+            File workingDir = Files.createTempDirectory(Paths.get("target"), "nxrunner-").toFile();
             Class<?> loaderClass = classLoader.loadClass(FrameworkLoader.class.getName());
             Method initialize = loaderClass.getDeclaredMethod("initialize", ClassLoader.class, File.class, List.class,
                     Map.class);
-            List<File> bundles = new FastClasspathScanner().getUniqueClasspathElements();
+            List<File> bundles = new FastClasspathScanner().getUniqueClasspathElements()
+                                                           .stream()
+                                                           .filter(this::isNuxeoBundle)
+                                                           .collect(toList());
             initialize.invoke(null, classLoader, workingDir, bundles, Collections.emptyMap());
 
             Method start = loaderClass.getDeclaredMethod("start");
@@ -59,77 +46,32 @@ public class NuxeoRunner implements Runnable {
         }
     }
 
-    /**
-     * Lookup bundle.
-     *
-     * @param bundleName the bundle name
-     * @return the bundle file
-     * @throws Exception the exception
-     */
-    protected BundleFile lookupBundle(String bundleName) throws Exception {
-        BundleFile bundleFile;
-        // BundleFile bundleFile = bundles.get(bundleName);
-        // if (bundleFile != null) {
-        // return bundleFile;
-        // }
-        for (URL url : introspectClasspath()) {
-            URI uri = url.toURI();
-            // if (readUris.contains(uri)) {
-            // continue;
-            // }
-            File file = new File(uri);
-            // readUris.add(uri);
-            try {
-                if (file.isDirectory()) {
-                    bundleFile = new DirectoryBundleFile(file);
-                } else {
-                    bundleFile = new JarBundleFile(file);
+    protected boolean isNuxeoBundle(File f) {
+        Manifest mf;
+        try {
+            if (f.isFile()) { // jar file
+                try (JarFile jf = new JarFile(f)) {
+                    mf = jf.getManifest();
                 }
-            } catch (IOException e) {
-                // no manifest => not a bundle
-                continue;
+                if (mf == null) {
+                    return false;
+                }
+            } else if (f.isDirectory()) { // directory
+                f = new File(f, "META-INF/MANIFEST.MF");
+                if (!f.isFile()) {
+                    return false;
+                }
+                mf = new Manifest();
+                try (FileInputStream input = new FileInputStream(f)) {
+                    mf.read(input);
+                }
+            } else {
+                return false;
             }
-            String symbolicName = readSymbolicName(bundleFile);
-            if (symbolicName != null) {
-                // log.debug("Bundle '{}' has URL {}", symbolicName, url);
-                // bundles.put(symbolicName, bundleFile);
-            }
-            if (bundleName.equals(symbolicName)) {
-                return bundleFile;
-            }
+        } catch (IOException e) {
+            return false;
         }
-        throw new RuntimeServiceException(String.format("No bundle with symbolic name '%s';", bundleName));
-    }
-
-    // TODO remove it in order to optimize it
-    protected URL[] introspectClasspath() {
-        return new FastClasspathScanner().getUniqueClasspathElements().stream().map(file -> {
-            try {
-                return file.toURI().toURL();
-            } catch (MalformedURLException cause) {
-                throw new RuntimeServiceException("Could not get URL from " + file, cause);
-            }
-        }).toArray(URL[]::new);
-    }
-
-    /**
-     * Read symbolic name.
-     *
-     * @param bf the bf
-     * @return the string
-     */
-    protected String readSymbolicName(BundleFile bf) {
-        Manifest manifest = bf.getManifest();
-        if (manifest == null) {
-            return null;
-        }
-        Attributes attrs = manifest.getMainAttributes();
-        String name = attrs.getValue("Bundle-SymbolicName");
-        if (name == null) {
-            return null;
-        }
-        String[] sp = name.split(";", 2);
-        return sp[0];
+        return mf.getMainAttributes().containsKey(NUXEO_COMPONENT_NAME);
     }
 
 }
