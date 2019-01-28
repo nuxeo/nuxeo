0460f86ab1725cf1273713f8dc649c7c0c1c9247
Florent Guillaume <fguillaume@nuxeo.com>
2019-01-28 15:45:38 +0100

    NXP-26696: fix IN queries in default audit backend

--- nuxeo-features/nuxeo-platform-audit/nuxeo-platform-audit-core/src/test/java/org/nuxeo/ecm/platform/audit/AbstractAuditStorageTest.java
+++ nuxeo-features/nuxeo-platform-audit/nuxeo-platform-audit-core/src/test/java/org/nuxeo/ecm/platform/audit/AbstractAuditStorageTest.java
@@ -158,4 +158,14 @@ public abstract class AbstractAuditStorageTest {
                                        .and(Predicates.startsWith(LOG_DOC_PATH, startsWith)));
         assertEquals(eventsCount, logs.size());
     }
+
+    @Test
+    public void testIn() throws Exception {
+        setUpTestData();
+        AuditQueryBuilder builder = (AuditQueryBuilder) new AuditQueryBuilder().predicate(
+                Predicates.in(LOG_EVENT_ID, Arrays.asList(ID_FOR_AUDIT_STORAGE_TESTS, "no-such-event-id")));
+        List<LogEntry> list = auditBackend.queryLogs(builder);
+        assertEquals(NUM_OF_EVENTS, list.size());
+    }
+
 }
