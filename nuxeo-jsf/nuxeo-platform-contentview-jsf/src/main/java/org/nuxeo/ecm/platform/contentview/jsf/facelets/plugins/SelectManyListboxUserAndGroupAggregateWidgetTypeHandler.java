72913eb42647cafcd4c8c8b332dbf5d6781da29b
Guillaume Renard <grenard@nuxeo.com>
2019-01-31 17:24:14 +0100

    NXP-26637: introduce a specific handler for user and group aggregate widget to be able to resolve prefixed ids

---
+++ nuxeo-jsf/nuxeo-platform-contentview-jsf/src/main/java/org/nuxeo/ecm/platform/contentview/jsf/facelets/plugins/SelectManyListboxUserAndGroupAggregateWidgetTypeHandler.java
@@ -0,0 +1,48 @@
+/*
+ * (C) Copyright 2019 Nuxeo SA (http://nuxeo.com/) and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Contributors:
+ *     Anahide Tchertchian
+ *     Guillaume Renard
+ */
+package org.nuxeo.ecm.platform.contentview.jsf.facelets.plugins;
+
+import java.io.IOException;
+
+import javax.faces.component.UIComponent;
+import javax.faces.component.html.HtmlSelectManyListbox;
+import javax.faces.view.facelets.FaceletContext;
+import javax.faces.view.facelets.TagConfig;
+
+import org.nuxeo.ecm.platform.forms.layout.api.Widget;
+import org.nuxeo.ecm.platform.forms.layout.api.exceptions.WidgetException;
+import org.nuxeo.ecm.platform.ui.web.renderer.NxListboxRenderer;
+
+/**
+ * @since 11.1
+ */
+public class SelectManyListboxUserAndGroupAggregateWidgetTypeHandler
+        extends SelectUserAndGroupAggregateWidgetTypeHandler {
+
+    public SelectManyListboxUserAndGroupAggregateWidgetTypeHandler(TagConfig config) {
+        super(config);
+    }
+
+    @Override
+    public void apply(FaceletContext ctx, UIComponent parent, Widget widget) throws WidgetException, IOException {
+        apply(ctx, parent, widget, HtmlSelectManyListbox.COMPONENT_TYPE, NxListboxRenderer.RENDERER_TYPE);
+    }
+
+}
