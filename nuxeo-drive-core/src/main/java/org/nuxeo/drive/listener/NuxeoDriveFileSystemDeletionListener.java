1b1719c823d9c05f80269e242a7696fc9b082446
Jenkins Nuxeo <jenkins@nuxeo.com>
2019-02-05 10:44:18 +0100

    NXP-26540: don't run STARTSWITH clause on placeless document

--- nuxeo-drive-core/src/main/java/org/nuxeo/drive/listener/NuxeoDriveFileSystemDeletionListener.java
+++ nuxeo-drive-core/src/main/java/org/nuxeo/drive/listener/NuxeoDriveFileSystemDeletionListener.java
@@ -219,6 +219,9 @@ public class NuxeoDriveFileSystemDeletionListener implements EventListener {
     }
 
     protected List<DocumentModel> getChildSyncRoots(DocumentModel doc, CoreSession session) {
+        if (doc.getParentRef() == null) {
+            return new ArrayList<>();
+        }
         String nxql = "SELECT * FROM Document WHERE ecm:mixinType = '" + NuxeoDriveManagerImpl.NUXEO_DRIVE_FACET
                 + "' AND ecm:isTrashed = 0 AND ecm:isVersion = 0 AND ecm:path STARTSWITH "
                 + NXQL.escapeString(doc.getPathAsString());
