7db7fb18738f24a12bdb0a1012fad47eeb4c7597
Thomas Roger <troger@nuxeo.com>
2019-02-11 15:48:48 +0100

    NXP-26709: add test for the Libre Office user installation folder removal

--- nuxeo-services/nuxeo-platform-convert/src/test/java/org/nuxeo/ecm/platform/convert/tests/TestSOfficeConverter.java
+++ nuxeo-services/nuxeo-platform-convert/src/test/java/org/nuxeo/ecm/platform/convert/tests/TestSOfficeConverter.java
@@ -20,18 +20,24 @@
 package org.nuxeo.ecm.platform.convert.tests;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
+import java.io.IOException;
 import java.io.Serializable;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import org.junit.Test;
-
 import org.nuxeo.ecm.core.api.Blob;
 import org.nuxeo.ecm.core.api.blobholder.BlobHolder;
+import org.nuxeo.runtime.test.runner.Deploy;
 
 /**
  * @since 8.4
@@ -46,18 +52,39 @@ public class TestSOfficeConverter extends BaseConverterTest {
         checkConverterAvailability(converterName);
         checkCommandAvailability("soffice");
 
+        BlobHolder result = convertTextBlob(converterName);
+        checkPDFDocument(result);
+    }
+
+    protected BlobHolder convertTextBlob(String converterName) throws IOException {
         BlobHolder pdfBH = getBlobFromPath("test-docs/hello.txt");
         Map<String, Serializable> parameters = new HashMap<>();
 
         BlobHolder result = cs.convert(converterName, pdfBH, parameters);
         assertNotNull(result);
+        return result;
+    }
 
-        List<Blob> blobs = result.getBlobs();
+    protected void checkPDFDocument(BlobHolder bh) throws IOException {
+        List<Blob> blobs = bh.getBlobs();
         assertNotNull(blobs);
         assertEquals(1, blobs.size());
 
-        Blob mainBlob = result.getBlob();
+        Blob mainBlob = bh.getBlob();
         String text = DocumentUTUtils.readPdfText(mainBlob.getFile());
         assertTrue(text.contains("Hello") || text.contains("hello"));
     }
+
+    @Test
+    @Deploy("org.nuxeo.ecm.platform.convert:OSGI-INF/test-text2pdf-contrib.xml")
+    public void testUserInstallationFolderRemoval() throws IOException, URISyntaxException {
+        BlobHolder result = convertTextBlob("text2pdfForTest");
+        checkPDFDocument(result);
+
+        // make sure the user installation folder is removed
+        String userInstallationUri = LibreOfficeConverterForTest.USER_INSTALLATION_URI;
+        URI uri = new URI(userInstallationUri);
+        assertFalse(Files.exists(Paths.get(uri)));
+    }
+
 }
