ba2d5efa6e05130b743d730a62d32804a00c100b
Pierre GAUTIER <pierregautier@nuxeo.com>
2019-02-14 13:36:42 +0100

    NXP-26194: remove concatenation from appendables

--- nuxeo-platform-3d-core/src/main/java/org/nuxeo/ecm/platform/threed/adapter/ThreeDPreviewer.java
+++ nuxeo-platform-3d-core/src/main/java/org/nuxeo/ecm/platform/threed/adapter/ThreeDPreviewer.java
@@ -18,6 +18,9 @@
  */
 package org.nuxeo.ecm.platform.threed.adapter;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.nuxeo.ecm.core.api.Blob;
 import org.nuxeo.ecm.core.api.Blobs;
 import org.nuxeo.ecm.core.api.DocumentModel;
@@ -31,9 +34,6 @@ import org.nuxeo.ecm.platform.threed.service.ThreeDService;
 import org.nuxeo.ecm.platform.web.common.vh.VirtualHostHelper;
 import org.nuxeo.runtime.api.Framework;
 
-import java.util.ArrayList;
-import java.util.List;
-
 /*
  * 3D content mimetype previewer.
  *
@@ -41,6 +41,7 @@ import java.util.List;
  */
 public class ThreeDPreviewer extends AbstractPreviewer implements MimeTypePreviewer {
 
+    @Override
     public List<Blob> getPreview(Blob blob, DocumentModel dm) throws PreviewException {
         ThreeDDocument threeDDocument = dm.getAdapter(ThreeDDocument.class);
         ThreeDService threeDService = Framework.getService(ThreeDService.class);
@@ -52,22 +53,23 @@ public class ThreeDPreviewer extends AbstractPreviewer implements MimeTypePrevie
     }
 
     protected List<Blob> buildPreview(TransmissionThreeD transmissionThreeD, DocumentModel dm) {
-        List<Blob> blobResults = new ArrayList<>();
         String basePath = VirtualHostHelper.getContextPathProperty();
         StringBuilder html = new StringBuilder();
-        html.append("<html><head>");
-        html.append("<title>" + getPreviewTitle(dm) + "</title>");
-        html.append(String.format(
-                "<script src=\"%s/platform-3d/bower_components/webcomponentsjs/webcomponents-lite.min.js\"></script>",
-                basePath));
-        html.append(String.format("<link rel=\"import\" href=\"%s/platform-3d/nuxeo-3d-viewer.html\">", basePath));
-        html.append(String.format("<nuxeo-3d-viewer src=\"%s\" loader=\"complete\">",
-                transmissionThreeD.getBlob().getFilename()));
+        html.append("<html><head>")
+            .append("<title>")
+            .append(getPreviewTitle(dm))
+            .append("</title>")
+            .append(String.format(
+                    "<script src=\"%s/platform-3d/bower_components/webcomponentsjs/webcomponents-lite.min.js\"></script>",
+                    basePath))
+            .append(String.format("<link rel=\"import\" href=\"%s/platform-3d/nuxeo-3d-viewer.html\">", basePath))
+            .append(String.format("<nuxeo-3d-viewer src=\"%s\" loader=\"complete\">",
+                    transmissionThreeD.getBlob().getFilename()))
+            .append("</nuxeo-3d-viewer>")
+            .append("</body>");
+        List<Blob> blobResults = new ArrayList<>();
+        blobResults.add(Blobs.createBlob(html.toString(), "text/html", null, "index.html"));
         blobResults.add(transmissionThreeD.getBlob());
-        html.append("</nuxeo-3d-viewer>");
-        html.append("</body>");
-        Blob mainBlob = Blobs.createBlob(html.toString(), "text/html", null, "index.html");
-        blobResults.add(0, mainBlob);
         return blobResults;
     }
 
