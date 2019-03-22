83bc76c0745b36e0e83c4909a2011274af297c46
Pierre GAUTIER <pierregautier@nuxeo.com>
2019-03-22 15:09:42 +0100

    NXP-26976: fix, clarify and test s3 direct upload

---
+++ nuxeo-core-binarymanager-s3/src/test/java/org/nuxeo/ecm/core/storage/sql/TestS3DirectBatchHandlerWithSSE.java
@@ -0,0 +1,38 @@
+/*
+ * (C) Copyright 2019 Nuxeo SA (http://nuxeo.com/) and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Contributors:
+ *     pierre
+ */
+
+package org.nuxeo.ecm.core.storage.sql;
+
+import static org.nuxeo.ecm.core.storage.sql.S3BinaryManager.SERVERSIDE_ENCRYPTION_PROPERTY;
+import org.junit.BeforeClass;
+
+/**
+ * Tests S3DirectBatchHandler with Server Side Encryption activated.
+ *
+ * @since 11.1
+ */
+public class TestS3DirectBatchHandlerWithSSE extends TestS3DirectBatchHandler {
+
+    @BeforeClass
+    public static void beforeClass() {
+        TestS3DirectBatchHandler.beforeClass();
+        // activate server side encryption
+        System.setProperty(S3DIRECT_PREFIX + SERVERSIDE_ENCRYPTION_PROPERTY, Boolean.TRUE.toString());
+    }
+}
