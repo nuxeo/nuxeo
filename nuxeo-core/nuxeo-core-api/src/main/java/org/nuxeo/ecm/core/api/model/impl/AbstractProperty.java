3b99938d71598427002b83d87628da81216a6833
Kevin Leturc <kleturc@nuxeo.com>
2019-06-07 16:10:21 +0200

    NXP-27531: Introduce rendering parameter to not write secured propery in cases such as emptyDocumentAdapter

--- nuxeo-core/nuxeo-core-api/src/main/java/org/nuxeo/ecm/core/api/model/impl/AbstractProperty.java
+++ nuxeo-core/nuxeo-core-api/src/main/java/org/nuxeo/ecm/core/api/model/impl/AbstractProperty.java
@@ -390,8 +390,16 @@ public abstract class AbstractProperty implements Property {
     /**
      * Returns whether or not current user can edit this property.
      */
-    protected boolean isSecured() {
-        return areFlagsSet(IS_SECURED) && !ClientLoginModule.isCurrentAdministrator();
+    protected boolean isSecuredForContext() {
+        return isSecured() && !ClientLoginModule.isCurrentAdministrator();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean isSecured() {
+        return areFlagsSet(IS_SECURED);
     }
 
     @Override
@@ -402,7 +410,7 @@ public abstract class AbstractProperty implements Property {
     @Override
     public void setValue(Object value) throws PropertyException {
         // 1. check the read only flag or security flag
-        if (isReadOnly() || isSecured()) {
+        if (isReadOnly() || isSecuredForContext()) {
             throw new ReadOnlyPropertyException(
                     String.format("Cannot set the value of property: %s since it is readonly", getXPath()));
         }
