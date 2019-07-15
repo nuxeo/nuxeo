d715def9210aa219984203a0d93127a7ee3db7ba
Benoit Delbosc <bdelbosc@nuxeo.com>
2019-07-15 16:51:42 +0200

    NXP-27758: Add an AuditEventDomainProducer

---
+++ nuxeo-features/nuxeo-platform-audit/nuxeo-platform-audit-core/src/main/java/org/nuxeo/ecm/platform/audit/stream/AuditEventDomainProducer.java
@@ -0,0 +1,91 @@
+/*
+ * (C) Copyright 2019 Nuxeo SA (http://nuxeo.com/) and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Contributors:
+ *     bdelbosc
+ */
+package org.nuxeo.ecm.platform.audit.stream;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.nuxeo.ecm.core.event.Event;
+import org.nuxeo.ecm.core.stream.EventDomainProducer;
+import org.nuxeo.ecm.platform.audit.api.AuditLogger;
+import org.nuxeo.ecm.platform.audit.api.LogEntry;
+import org.nuxeo.lib.stream.codec.Codec;
+import org.nuxeo.lib.stream.computation.Record;
+import org.nuxeo.runtime.api.Framework;
+import org.nuxeo.runtime.codec.CodecService;
+
+/**
+ * @since 11.1
+ */
+public class AuditEventDomainProducer extends EventDomainProducer {
+
+    protected static final String SOURCE_NAME = "audit";
+
+    protected List<Record> records = new ArrayList<>();
+
+    public AuditEventDomainProducer(String name, String stream) {
+        super(name, stream);
+    }
+
+    @Override
+    public void addEvent(Event event) {
+        AuditLogger logger = Framework.getService(AuditLogger.class);
+        if (logger == null || event == null) {
+            return;
+        }
+        if (logger.getAuditableEventNames().contains(event.getName())) {
+            LogEntry entry = logger.buildEntryFromEvent(event);
+            if (entry != null) {
+                records.add(buildRecordFromEvent(entry));
+            }
+        }
+    }
+
+    protected Record buildRecordFromEvent(LogEntry entry) {
+        AuditEvent event = new AuditEvent();
+        event.source = SOURCE_NAME;
+        event.name = entry.getEventId();
+        event.category = entry.getCategory();
+        event.docId = entry.getDocUUID();
+        event.repository = entry.getRepositoryId();
+        event.principalName = entry.getPrincipalName();
+        event.docLifeCycle = entry.getDocLifeCycle();
+        event.docType = entry.getDocType();
+        event.docPath = entry.getDocPath();
+        event.comment = entry.getComment();
+        if (entry.getEventDate() != null) {
+            event.eventDate = entry.getEventDate().getTime();
+        } else {
+            event.eventDate = 0;
+        }
+        if (entry.getLogDate() != null) {
+            event.logDate = entry.getLogDate().getTime();
+        } else {
+            event.logDate = 0;
+        }
+        event.eventId = entry.getId();
+        Codec<AuditEvent> codec = Framework.getService(CodecService.class).getCodec("avroBinary", AuditEvent.class);
+        return Record.of(event.name, codec.encode(event));
+    }
+
+    @Override
+    public List<Record> getDomainEvents() {
+        return records;
+    }
+}
