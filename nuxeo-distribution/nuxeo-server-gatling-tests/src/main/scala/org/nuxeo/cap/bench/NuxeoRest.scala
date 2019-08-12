2a9f2c783a35d756e15d3e777125b753d9c29574
Benoit Delbosc <bdelbosc@nuxeo.com>
2019-08-12 15:21:17 +0200

    NXP-27636: Remove head request enable keepalive
    head request does not close properly the socket and create bug on the
    AWS ELB with keep alive

--- nuxeo-distribution/nuxeo-server-gatling-tests/src/main/scala/org/nuxeo/cap/bench/NuxeoRest.scala
+++ nuxeo-distribution/nuxeo-server-gatling-tests/src/main/scala/org/nuxeo/cap/bench/NuxeoRest.scala
@@ -174,7 +174,7 @@ object NuxeoRest {
   def createDocumentIfNotExists = (parent: String, name: String, docType: String) => {
     exec(
       http("Check if document exists")
-        .head(Constants.API_PATH + parent + "/" + name)
+        .get(Constants.API_PATH + parent + "/" + name)
         .headers(Headers.base)
         .header("Content-Type", "application/json")
         .basicAuth("${user}", "${password}")
@@ -200,7 +200,7 @@ object NuxeoRest {
   def createDocumentIfNotExistsAsAdmin = (parent: String, name: String, docType: String) => {
     exec(
       http("Check if document exists")
-        .head(Constants.API_PATH + parent + "/" + name)
+        .get(Constants.API_PATH + parent + "/" + name)
         .headers(Headers.base)
         .header("Content-Type", "application/json")
         .basicAuth("${adminId}", "${adminPassword}")
@@ -298,7 +298,7 @@ object NuxeoRest {
   def createUserIfNotExists = (groupName: String) => {
     exec(
       http("Check if user exists")
-        .head("/api/v1/user/${user}")
+        .get("/api/v1/user/${user}")
         .headers(Headers.base)
         .header("Content-Type", "application/json")
         .basicAuth("${adminId}", "${adminPassword}")
@@ -331,7 +331,7 @@ object NuxeoRest {
   def createGroupIfNotExists = (groupName: String) => {
     exec(
       http("Check if group exists")
-        .head("/api/v1/group/" + groupName)
+        .get("/api/v1/group/" + groupName)
         .headers(Headers.base)
         .header("Content-Type", "application/json")
         .basicAuth("${adminId}", "${adminPassword}")
