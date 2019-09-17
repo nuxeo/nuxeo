7fb26fccc9f424b4a1b35867cc7d13596233ee14
Thierry Martins <tmartins@nuxeo.com>
2019-09-17 11:19:13 +0200

    NXP-28037: fetch references only when requested.

--- nuxeo-services/nuxeo-platform-directory/nuxeo-platform-directory-mongodb/src/main/java/org/nuxeo/directory/mongodb/MongoDBSession.java
+++ nuxeo-services/nuxeo-platform-directory/nuxeo-platform-directory-mongodb/src/main/java/org/nuxeo/directory/mongodb/MongoDBSession.java
@@ -103,7 +103,7 @@ public class MongoDBSession extends BaseSession {
     public DocumentModel getEntryFromSource(String id, boolean fetchReferences) throws DirectoryException {
         String idFieldName = getPrefixedIdField();
         DocumentModelList result = doQuery(Collections.singletonMap(idFieldName, id), Collections.emptySet(),
-                Collections.emptyMap(), true, 1, 0, false);
+                Collections.emptyMap(), fetchReferences, 1, 0, false);
 
         if (result.isEmpty()) {
             return null;
