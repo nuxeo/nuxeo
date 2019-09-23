25ab2823164400016316efaba567f60438281d17
nalkotob <nalkotob@nuxeo.com>
2019-09-23 12:34:43 +0200

    NXP-27683: ExcludeParam on copy

--- nuxeo-core/nuxeo-core-storage-sql/nuxeo-core-storage-sql/src/main/java/org/nuxeo/ecm/core/storage/sql/PersistenceContext.java
+++ nuxeo-core/nuxeo-core-storage-sql/nuxeo-core-storage-sql/src/main/java/org/nuxeo/ecm/core/storage/sql/PersistenceContext.java
@@ -39,7 +39,6 @@ import org.apache.commons.collections.map.AbstractReferenceMap;
 import org.apache.commons.collections.map.ReferenceMap;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.nuxeo.common.utils.StringUtils;
 import org.nuxeo.ecm.core.api.DocumentExistsException;
 import org.nuxeo.ecm.core.api.NuxeoException;
 import org.nuxeo.ecm.core.api.model.DeltaLong;
@@ -1486,7 +1485,9 @@ public class PersistenceContext {
         checkFreeName(parentId, name, complexProp(hierFragment));
         // do the copy
         Long pos = getNextPos(parentId, false);
-        CopyResult copyResult = mapper.copy(new IdWithTypes(source), parentId, name, null);
+        // no special children (comments, annotations...) kept on document copy
+        boolean excludeSpecialChildren = true;
+        CopyResult copyResult = mapper.copy(new IdWithTypes(source), parentId, name, null, excludeSpecialChildren);
         Serializable newId = copyResult.copyId;
         // read new child in this session (updates children Selection)
         SimpleFragment copy = getHier(newId, false);
@@ -1543,7 +1544,9 @@ public class PersistenceContext {
          * Do the copy without non-complex children, with null parent.
          */
         Serializable id = node.getId();
-        CopyResult res = mapper.copy(new IdWithTypes(node), null, null, null);
+        // complex (special) children are snapshot when checking in
+        boolean excludeSpecialChildren = false;
+        CopyResult res = mapper.copy(new IdWithTypes(node), null, null, null, excludeSpecialChildren);
         Serializable newId = res.copyId;
         markInvalidated(res.invalidations);
         // add version as a new child of its parent
@@ -1620,7 +1623,10 @@ public class PersistenceContext {
         overwriteRow.putNew(Model.MAIN_CHECKED_IN_KEY, Boolean.TRUE);
         overwriteRow.putNew(Model.MAIN_BASE_VERSION_KEY, versionId);
         overwriteRow.putNew(Model.MAIN_IS_VERSION_KEY, null);
-        CopyResult res = mapper.copy(new IdWithTypes(version), node.getParentId(), null, overwriteRow);
+        // exclude special children to avoid duplicates on restore
+        boolean excludeSpecialChildren = true;
+        CopyResult res = mapper.copy(new IdWithTypes(version), node.getParentId(), null, overwriteRow,
+                excludeSpecialChildren);
         markInvalidated(res.invalidations);
     }
 
