f1022358aee7cbf994397e60ce3b9a12c2a3f30b
Florent Guillaume <fguillaume@nuxeo.com>
2019-10-04 14:49:05 +0200

    NXP-27805: make S3 digest algorithm configurable

--- nuxeo-core/nuxeo-core-api/src/main/java/org/nuxeo/ecm/core/blob/binary/LocalBinaryManager.java
+++ nuxeo-core/nuxeo-core-api/src/main/java/org/nuxeo/ecm/core/blob/binary/LocalBinaryManager.java
@@ -116,7 +116,7 @@ public class LocalBinaryManager extends AbstractBinaryManager {
         tmpDir = new File(base, TMP);
         storageDir.mkdirs();
         tmpDir.mkdirs();
-        descriptor = getDescriptor(new File(base, CONFIG_FILE));
+        setDescriptor(getDescriptor(new File(base, CONFIG_FILE)));
         createGarbageCollector();
 
         // be sure FileTracker won't steal our files !
