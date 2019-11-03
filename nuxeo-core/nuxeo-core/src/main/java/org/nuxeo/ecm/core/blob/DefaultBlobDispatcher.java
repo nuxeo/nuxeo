e7dc34fdc25bdd0eff04c752b0cd5a1837e37ec2
Florent Guillaume <fguillaume@nuxeo.com>
2019-11-03 03:00:09 +0100

    NXP-28227: allow blob dispatch on doc's ecm:path

--- nuxeo-core/nuxeo-core/src/main/java/org/nuxeo/ecm/core/blob/DefaultBlobDispatcher.java
+++ nuxeo-core/nuxeo-core/src/main/java/org/nuxeo/ecm/core/blob/DefaultBlobDispatcher.java
@@ -48,10 +48,12 @@ import org.nuxeo.runtime.api.Framework;
  * instead of the repository name.
  * <p>
  * The property name is a list of comma-separated clauses, with each clause consisting of a property, an operator and a
- * value. The property can be a {@link Document} xpath, {@code ecm:repositoryName}, or, to match the current blob being
- * dispatched, {@code blob:name}, {@code blob:mime-type}, {@code blob:encoding}, {@code blob:digest},
- * {@code blob:length} or {@code blob:xpath}. Comma-separated clauses are ANDed together. The special name
- * {@code default} defines the default provider, and must be present.
+ * value. The property can be a {@link Document} xpath, {@code ecm:repositoryName}, {@code ecm:path}, or, to match the
+ * current blob being dispatched, {@code blob:name}, {@code blob:mime-type}, {@code blob:encoding}, {@code blob:digest},
+ * {@code blob:length} or {@code blob:xpath}.
+ * <p>
+ * Comma-separated clauses are ANDed together. The special name {@code default} defines the default provider, and must
+ * be present.
  * <p>
  * Available operators between property and value are =, !=, &lt, > and ~. The operators &lt; and > work with integer
  * values. The operator ~ does glob matching using {@code ?} to match a single arbitrary character, and {@code *} to
@@ -59,14 +61,15 @@ import org.nuxeo.runtime.api.Framework;
  * <p>
  * For example, to dispatch to the "first" provider if dc:format is "video", to the "second" provider if the blob's MIME
  * type is "video/mp4", to the "third" provider if the blob is stored as a secondary attached file, to the "fourth"
- * provider if the lifecycle state is "approved" and the document is in the default repository, and otherwise to the
- * "other" provider:
+ * provider if the lifecycle state is "approved", to the "fifth" provider if the blob's document is stored in under an
+ * "images" folder, and the document is in the default repository, and otherwise to the "other" provider:
  *
  * <pre>
  * &lt;property name="dc:format=video">first&lt;/property>
  * &lt;property name="blob:mime-type=video/mp4">second&lt;/property>
  * &lt;property name="blob:xpath~files/*&#47;file">third&lt;/property>
  * &lt;property name="ecm:repositoryName=default,ecm:lifeCycleState=approved">fourth&lt;/property>
+ * &lt;property name="ecm:path~*&#47images&#47*">fifth&lt;/property>
  * &lt;property name="default">other&lt;/property>
  * </pre>
  * <p>
@@ -96,6 +99,9 @@ public class DefaultBlobDispatcher implements BlobDispatcher {
 
     /** Pseudo-property for the repository name. */
     protected static final String REPOSITORY_NAME = "ecm:repositoryName";
+
+    /** Pseudo-property for the document path. */
+    protected static final String PATH = "ecm:path";
 
     /**
      * Pseudo-property for the record state.
@@ -251,6 +257,8 @@ public class DefaultBlobDispatcher implements BlobDispatcher {
                 Object value;
                 if (xpath.equals(REPOSITORY_NAME)) {
                     value = doc.getRepositoryName();
+                } else if (xpath.equals(PATH)) {
+                    value = doc.getPath();
                 } else if (xpath.equals(IS_RECORD)) {
                     value = Boolean.valueOf(doc.isRecord());
                 } else if (xpath.startsWith(BLOB_PREFIX)) {
