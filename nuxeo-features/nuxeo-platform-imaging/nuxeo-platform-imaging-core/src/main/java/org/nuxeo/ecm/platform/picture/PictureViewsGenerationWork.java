92b8211b6f3e534b77dcebc2707b81d36020e56f
Benoit Delbosc <bdelbosc@nuxeo.com>
2019-11-08 16:51:00 +0100

    NXP-28156: Use a GroupJoin Work and remove deprecated usage of listWorkIds

--- nuxeo-features/nuxeo-platform-imaging/nuxeo-platform-imaging-core/src/main/java/org/nuxeo/ecm/platform/picture/PictureViewsGenerationWork.java
+++ nuxeo-features/nuxeo-platform-imaging/nuxeo-platform-imaging-core/src/main/java/org/nuxeo/ecm/platform/picture/PictureViewsGenerationWork.java
@@ -22,7 +22,6 @@ import static org.nuxeo.ecm.core.api.CoreSession.ALLOW_VERSION_WRITE;
 import static org.nuxeo.ecm.platform.picture.listener.PictureViewsGenerationListener.DISABLE_PICTURE_VIEWS_GENERATION_LISTENER;
 
 import java.io.IOException;
-import java.util.List;
 
 import org.nuxeo.ecm.core.api.Blob;
 import org.nuxeo.ecm.core.api.DocumentModel;
@@ -34,7 +33,6 @@ import org.nuxeo.ecm.core.event.Event;
 import org.nuxeo.ecm.core.event.EventService;
 import org.nuxeo.ecm.core.event.impl.DocumentEventContext;
 import org.nuxeo.ecm.core.work.AbstractWork;
-import org.nuxeo.ecm.core.work.api.WorkManager;
 import org.nuxeo.ecm.platform.picture.api.adapters.PictureResourceAdapter;
 import org.nuxeo.ecm.platform.picture.recompute.RecomputeViewsAction;
 import org.nuxeo.runtime.api.Framework;
@@ -84,6 +82,22 @@ public class PictureViewsGenerationWork extends AbstractWork {
     }
 
     @Override
+    public boolean isGroupJoin() {
+        // This is a GroupJoin work with a trigger that can be used on the last work execution
+        return true;
+    }
+
+    @Override
+    public String getPartitionKey() {
+        return docId;
+    }
+
+    @Override
+    public void onGroupJoinCompletion() {
+        firePictureViewsGenerationDoneEvent(session.getDocument(new IdRef(docId)));
+    }
+
+    @Override
     public void work() {
         setProgress(Progress.PROGRESS_INDETERMINATE);
         setStatus("Extracting");
@@ -129,29 +143,15 @@ public class PictureViewsGenerationWork extends AbstractWork {
         workingDocument.putContextData(DISABLE_PICTURE_VIEWS_GENERATION_LISTENER, Boolean.TRUE);
         session.saveDocument(workingDocument);
 
-        firePictureViewsGenerationDoneEvent(workingDocument);
-
         setStatus("Done");
     }
 
     /**
-     * Fire a {@code PICTURE_VIEWS_GENERATION_DONE_EVENT} event when no other PictureViewsGenerationWork is scheduled
-     * for this document.
+     * Fire a {@code PICTURE_VIEWS_GENERATION_DONE_EVENT}
      *
      * @since 5.8
      */
     protected void firePictureViewsGenerationDoneEvent(DocumentModel doc) {
-        WorkManager workManager = Framework.getService(WorkManager.class);
-        List<String> workIds = workManager.listWorkIds(CATEGORY_PICTURE_GENERATION, null);
-        int worksCount = 0;
-        for (String workId : workIds) {
-            if (workId.equals(getId())) {
-                if (++worksCount > 1) {
-                    // another work scheduled
-                    return;
-                }
-            }
-        }
         DocumentEventContext ctx = new DocumentEventContext(session, session.getPrincipal(), doc);
         Event event = ctx.newEvent(PICTURE_VIEWS_GENERATION_DONE_EVENT);
         Framework.getService(EventService.class).fireEvent(event);
