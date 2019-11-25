c9f58ae2a38ac0a2d447ceb3d1de61921bb5225e
Benoit Delbosc <bdelbosc@nuxeo.com>
2019-11-25 13:42:18 +0100

    NXP-28334: Add a ScrollService with 2 implementations
    The default scroll is based on the Repository scroll.
    A static scroll is provided to scroll on provided identifiers.

---
+++ nuxeo-core/nuxeo-core-bulk/src/main/java/org/nuxeo/ecm/core/scroll/StaticScrollRequest.java
@@ -0,0 +1,100 @@
+/*
+ * (C) Copyright 2019 Nuxeo (http://nuxeo.com/) and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Contributors:
+ *     bdelbosc
+ */
+package org.nuxeo.ecm.core.scroll;
+
+import java.time.Duration;
+import java.util.Objects;
+
+import org.nuxeo.ecm.core.api.scroll.ScrollRequest;
+
+/**
+ * @since 11.1
+ */
+public class StaticScrollRequest implements ScrollRequest {
+
+    public static final String STATIC_TYPE = "static";
+
+    public static final Duration STATIC_DURATION = Duration.ofDays(1);
+
+    protected final String query;
+
+    protected final int size;
+
+    public StaticScrollRequest(Builder builder) {
+        this.query = builder.getQuery();
+        this.size = builder.getSize();
+    }
+
+    @Override
+    public String getType() {
+        return STATIC_TYPE;
+    }
+
+    @Override
+    public String getQuery() {
+        return query;
+    }
+
+    @Override
+    public Duration getTimeout() {
+        return STATIC_DURATION;
+    }
+
+    @Override
+    public int getSize() {
+        return size;
+    }
+
+    @Override
+    public String toString() {
+        return "StaticScrollRequest{" + "query='" + query + '\'' + ", size=" + size + '}';
+    }
+
+    public static class Builder {
+
+        public static final int DEFAULT_SCROLL_SIZE = 10;
+
+        protected final String query;
+
+        protected int size;
+
+        public Builder(String identifiers) {
+            Objects.requireNonNull(identifiers, "identifiers cannot be null");
+            this.query = identifiers;
+        }
+
+        public Builder scrollSize(int size) {
+            if (size <= 0) {
+                throw new IllegalArgumentException("size must be > 0");
+            }
+            this.size = size;
+            return this;
+        }
+
+        public String getQuery() {
+            return query;
+        }
+
+        public int getSize() {
+            return size == 0 ? DEFAULT_SCROLL_SIZE : size;
+        }
+
+    }
+
+}
---
+++ nuxeo-core/nuxeo-core-bulk/src/test/java/org/nxueo/ecm/core/scroll/TestScrollService.java
@@ -0,0 +1,75 @@
+/*
+ * (C) Copyright 2019 Nuxeo (http://nuxeo.com/) and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Contributors:
+ *     bdelbosc
+ */
+package org.nxueo.ecm.core.scroll;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+import javax.inject.Inject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.nuxeo.ecm.core.api.scroll.Scroll;
+import org.nuxeo.ecm.core.api.scroll.ScrollRequest;
+import org.nuxeo.ecm.core.api.scroll.ScrollService;
+import org.nuxeo.ecm.core.bulk.CoreBulkFeature;
+import org.nuxeo.ecm.core.scroll.StaticScrollRequest;
+import org.nuxeo.runtime.test.runner.Features;
+import org.nuxeo.runtime.test.runner.FeaturesRunner;
+
+@RunWith(FeaturesRunner.class)
+@Features(CoreBulkFeature.class)
+public class TestScrollService {
+
+    @Inject
+    protected ScrollService service;
+
+    @Test
+    public void testScrollService() {
+        assertNotNull(service);
+    }
+
+    @Test
+    public void testStaticScroll() {
+        String ids = "first,2,3,4,5,6,7,8,9,last";
+        int scrollSize = 4;
+        ScrollRequest request = new StaticScrollRequest(new StaticScrollRequest.Builder(ids).scrollSize(scrollSize));
+        Scroll scroll = service.scroll(request);
+        assertNotNull(scroll);
+        assertTrue(scroll.fetch());
+
+        assertEquals(scrollSize, scroll.getIds().size());
+        assertEquals(Arrays.asList("first", "2", "3", "4"), scroll.getIds());
+
+        assertTrue(scroll.fetch());
+        assertEquals(Arrays.asList("5", "6", "7", "8"), scroll.getIds());
+
+        assertTrue(scroll.fetch());
+        assertEquals(Arrays.asList("9", "last"), scroll.getIds());
+
+        assertFalse(scroll.fetch());
+        assertEquals(Collections.emptyList(), scroll.getIds());
+    }
+
+}
