6f8f85b863becb2ab74be7902b01d3efa37af379
Benoit Delbosc <bdelbosc@nuxeo.com>
2019-11-29 09:45:40 +0100

    NXP-28357: Use a normal processor for Bulk Service
    Rely on configuration to define and start the Bulk Service processor.

--- nuxeo-core/nuxeo-core-bulk/src/main/java/org/nuxeo/ecm/core/bulk/BulkAdminServiceImpl.java
+++ nuxeo-core/nuxeo-core-bulk/src/main/java/org/nuxeo/ecm/core/bulk/BulkAdminServiceImpl.java
@@ -19,39 +19,18 @@
 package org.nuxeo.ecm.core.bulk;
 
 import static org.nuxeo.ecm.core.bulk.BulkServiceImpl.BULK_LOG_MANAGER_NAME;
-import static org.nuxeo.ecm.core.bulk.BulkServiceImpl.COMMAND_STREAM;
-import static org.nuxeo.ecm.core.bulk.BulkServiceImpl.DONE_STREAM;
-import static org.nuxeo.ecm.core.bulk.BulkServiceImpl.RECORD_CODEC;
-import static org.nuxeo.ecm.core.bulk.BulkServiceImpl.STATUS_STREAM;
-import static org.nuxeo.lib.stream.computation.AbstractComputation.INPUT_1;
-import static org.nuxeo.lib.stream.computation.AbstractComputation.OUTPUT_1;
 
 import java.time.Duration;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
-import org.nuxeo.ecm.core.bulk.computation.BulkScrollerComputation;
-import org.nuxeo.ecm.core.bulk.computation.BulkStatusComputation;
-import org.nuxeo.lib.stream.codec.Codec;
-import org.nuxeo.lib.stream.computation.ComputationPolicy;
-import org.nuxeo.lib.stream.computation.ComputationPolicyBuilder;
-import org.nuxeo.lib.stream.computation.Record;
-import org.nuxeo.lib.stream.computation.Settings;
 import org.nuxeo.lib.stream.computation.StreamManager;
 import org.nuxeo.lib.stream.computation.StreamProcessor;
-import org.nuxeo.lib.stream.computation.Topology;
 import org.nuxeo.runtime.api.Framework;
-import org.nuxeo.runtime.codec.CodecService;
 import org.nuxeo.runtime.model.Descriptor;
-import org.nuxeo.runtime.services.config.ConfigurationService;
 import org.nuxeo.runtime.stream.StreamService;
-
-import net.jodah.failsafe.RetryPolicy;
 
 /**
  * @since 10.3
@@ -62,17 +41,7 @@ public class BulkAdminServiceImpl implements BulkAdminService {
 
     public static final String STATUS_NAME = "status";
 
-    public static final String BULK_SCROLLER_CONCURRENCY_PROPERTY = "nuxeo.core.bulk.scroller.concurrency";
-
-    public static final String BULK_STATUS_CONCURRENCY_PROPERTY = "nuxeo.core.bulk.status.concurrency";
-
-    public static final String BULK_STATUS_CONTINUE_ON_FAILURE_PROPERTY = "nuxeo.core.bulk.status.continueOnFailure";
-
-    public static final String BULK_STATUS_MAX_RETRIES_PROPERTY = "nuxeo.core.bulk.status.maxRetries";
-
-    public static final String BULK_STATUS_DELAY_PROPERTY = "nuxeo.core.bulk.status.delayMillis";
-
-    public static final String BULK_STATUS_MAX_DELAY_PROPERTY = "nuxeo.core.bulk.status.maxDelayMillis";
+    public static final String BULK_SERVICE_PROCESSOR_NAME = "bulkServiceProcessor";
 
     public static final String BULK_SCROLL_SIZE_PROPERTY = "nuxeo.core.bulk.scroller.scroll.size";
 
@@ -80,17 +49,6 @@ public class BulkAdminServiceImpl implements BulkAdminService {
 
     public static final String BULK_SCROLL_PRODUCE_IMMEDIATE_PROPERTY = "nuxeo.core.bulk.scroller.produceImmediate";
 
-    public static final String BULK_SCROLL_CONTINUE_ON_FAILURE_PROPERTY = "nuxeo.core.bulk.scroller.continueOnFailure";
-
-    public static final int DEFAULT_STATUS_CONCURRENCY = 1;
-
-    public static final int DEFAULT_STATUS_MAX_RETRIES = 3;
-
-    public static final int DEFAULT_STATUS_DELAY_MILLIS = 500;
-
-    public static final int DEFAULT_STATUS_MAX_DELAY_MILLIS = 10000;
-
-    public static final int DEFAULT_SCROLLER_CONCURRENCY = 1;
 
     public static final int DEFAULT_SCROLL_SIZE = 100;
 
@@ -114,63 +72,6 @@ public class BulkAdminServiceImpl implements BulkAdminService {
                                            .collect(HashMap::new, (map, desc) -> map.put(desc.name,
                                                    desc.validationClass != null ? desc.newValidationInstance() : null),
                                                    HashMap::putAll);
-    }
-
-    protected void initProcessor() {
-        StreamService service = Framework.getService(StreamService.class);
-        ConfigurationService confService = Framework.getService(ConfigurationService.class);
-        StreamManager streamManager = service.getStreamManager(BULK_LOG_MANAGER_NAME);
-
-        CodecService codecService = Framework.getService(CodecService.class);
-        Codec<Record> codec = codecService.getCodec(RECORD_CODEC, Record.class);
-        // we don't set any partitioning because it is already defined by logConfig contribution
-        Settings settings = new Settings(1, 1, codec);
-        settings.setConcurrency(SCROLLER_NAME,
-                confService.getInteger(BULK_SCROLLER_CONCURRENCY_PROPERTY, DEFAULT_SCROLLER_CONCURRENCY));
-        settings.setConcurrency(STATUS_NAME,
-                confService.getInteger(BULK_STATUS_CONCURRENCY_PROPERTY, DEFAULT_STATUS_CONCURRENCY));
-        // we don't want any retry on scroller this creates duplicates
-        ComputationPolicy scrollerPolicy = new ComputationPolicyBuilder().continueOnFailure(
-                confService.isBooleanTrue(BULK_SCROLL_CONTINUE_ON_FAILURE_PROPERTY))
-                                                                         .retryPolicy(ComputationPolicy.NO_RETRY)
-                                                                         .build();
-        settings.setPolicy(SCROLLER_NAME, scrollerPolicy);
-        // status policy is configurable
-        RetryPolicy retryPolicy = new RetryPolicy().withMaxRetries(
-                confService.getInteger(BULK_STATUS_MAX_RETRIES_PROPERTY, DEFAULT_STATUS_MAX_RETRIES))
-                                                   .withBackoff(
-                                                           confService.getInteger(BULK_STATUS_DELAY_PROPERTY,
-                                                                   DEFAULT_STATUS_DELAY_MILLIS),
-                                                           confService.getInteger(BULK_STATUS_MAX_DELAY_PROPERTY,
-                                                                   DEFAULT_STATUS_MAX_DELAY_MILLIS),
-                                                           TimeUnit.MILLISECONDS);
-        ComputationPolicy statusPolicy = new ComputationPolicyBuilder().continueOnFailure(
-                confService.isBooleanTrue(BULK_STATUS_CONTINUE_ON_FAILURE_PROPERTY)).retryPolicy(retryPolicy).build();
-        settings.setPolicy(SCROLLER_NAME, statusPolicy);
-        int scrollSize = confService.getInteger(BULK_SCROLL_SIZE_PROPERTY, DEFAULT_SCROLL_SIZE);
-        int scrollKeepAlive = confService.getInteger(BULK_SCROLL_KEEP_ALIVE_PROPERTY, DEFAULT_SCROLL_KEEP_ALIVE);
-        boolean scrollProduceImmediate = confService.isBooleanTrue(BULK_SCROLL_PRODUCE_IMMEDIATE_PROPERTY);
-        streamProcessor = streamManager.registerAndCreateProcessor("bulk", getTopology(scrollSize, scrollKeepAlive, scrollProduceImmediate), settings);
-    }
-
-    protected Topology getTopology(int scrollBatchSize, int scrollKeepAlive, boolean scrollProduceImmediate) {
-        List<String> mapping = new ArrayList<>();
-        mapping.add(INPUT_1 + ":" + COMMAND_STREAM);
-        int i = 1;
-        for (String action : actions) {
-            mapping.add(String.format("o%s:%s", i, action));
-            i++;
-        }
-        mapping.add(String.format("o%s:%s", i, STATUS_STREAM));
-        return Topology.builder()
-                       .addComputation( //
-                               () -> new BulkScrollerComputation(SCROLLER_NAME, actions.size() + 1, scrollBatchSize,
-                                       scrollKeepAlive, scrollProduceImmediate), //
-                               mapping)
-                       .addComputation(() -> new BulkStatusComputation(STATUS_NAME),
-                               Arrays.asList(INPUT_1 + ":" + STATUS_STREAM, //
-                                       OUTPUT_1 + ":" + DONE_STREAM))
-                       .build();
     }
 
     @Override
@@ -204,7 +105,8 @@ public class BulkAdminServiceImpl implements BulkAdminService {
     }
 
     public void afterStart() {
-        initProcessor();
+        StreamManager manager = Framework.getService(StreamService.class).getStreamManager(BULK_LOG_MANAGER_NAME);
+        streamProcessor = manager.createStreamProcessor(BULK_SERVICE_PROCESSOR_NAME);
         streamProcessor.start();
     }
 
---
+++ nuxeo-core/nuxeo-core-bulk/src/main/java/org/nuxeo/ecm/core/bulk/BulkServiceProcessor.java
@@ -0,0 +1,76 @@
+/*
+ * (C) Copyright 2019 Nuxeo (http://nuxeo.com/) and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Contributors:
+ *     bdelbosc
+ */
+package org.nuxeo.ecm.core.bulk;
+
+import static org.nuxeo.ecm.core.bulk.BulkAdminServiceImpl.BULK_SCROLL_KEEP_ALIVE_PROPERTY;
+import static org.nuxeo.ecm.core.bulk.BulkAdminServiceImpl.BULK_SCROLL_PRODUCE_IMMEDIATE_PROPERTY;
+import static org.nuxeo.ecm.core.bulk.BulkAdminServiceImpl.BULK_SCROLL_SIZE_PROPERTY;
+import static org.nuxeo.ecm.core.bulk.BulkAdminServiceImpl.DEFAULT_SCROLL_KEEP_ALIVE;
+import static org.nuxeo.ecm.core.bulk.BulkAdminServiceImpl.DEFAULT_SCROLL_SIZE;
+import static org.nuxeo.ecm.core.bulk.BulkAdminServiceImpl.SCROLLER_NAME;
+import static org.nuxeo.ecm.core.bulk.BulkAdminServiceImpl.STATUS_NAME;
+import static org.nuxeo.ecm.core.bulk.BulkServiceImpl.COMMAND_STREAM;
+import static org.nuxeo.ecm.core.bulk.BulkServiceImpl.DONE_STREAM;
+import static org.nuxeo.ecm.core.bulk.BulkServiceImpl.STATUS_STREAM;
+import static org.nuxeo.lib.stream.computation.AbstractComputation.INPUT_1;
+import static org.nuxeo.lib.stream.computation.AbstractComputation.OUTPUT_1;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+import org.nuxeo.ecm.core.bulk.computation.BulkScrollerComputation;
+import org.nuxeo.ecm.core.bulk.computation.BulkStatusComputation;
+import org.nuxeo.lib.stream.computation.Topology;
+import org.nuxeo.runtime.api.Framework;
+import org.nuxeo.runtime.services.config.ConfigurationService;
+import org.nuxeo.runtime.stream.StreamProcessorTopology;
+
+/**
+ * @since 11.1
+ */
+public class BulkServiceProcessor implements StreamProcessorTopology {
+
+    @Override
+    public Topology getTopology(Map<String, String> options) {
+        List<String> mapping = new ArrayList<>();
+        mapping.add(INPUT_1 + ":" + COMMAND_STREAM);
+        List<String> actions = Framework.getService(BulkAdminService.class).getActions();
+        int i = 1;
+        for (String action : actions) {
+            mapping.add(String.format("o%s:%s", i, action));
+            i++;
+        }
+        mapping.add(String.format("o%s:%s", i, STATUS_STREAM));
+        ConfigurationService confService = Framework.getService(ConfigurationService.class);
+        int scrollBatchSize = confService.getInteger(BULK_SCROLL_SIZE_PROPERTY, DEFAULT_SCROLL_SIZE);
+        int scrollKeepAlive = confService.getInteger(BULK_SCROLL_KEEP_ALIVE_PROPERTY, DEFAULT_SCROLL_KEEP_ALIVE);
+        boolean scrollProduceImmediate = confService.isBooleanTrue(BULK_SCROLL_PRODUCE_IMMEDIATE_PROPERTY);
+        return Topology.builder()
+                       .addComputation( //
+                               () -> new BulkScrollerComputation(SCROLLER_NAME, actions.size() + 1, scrollBatchSize,
+                                       scrollKeepAlive, scrollProduceImmediate), //
+                               mapping)
+                       .addComputation(() -> new BulkStatusComputation(STATUS_NAME),
+                               Arrays.asList(INPUT_1 + ":" + STATUS_STREAM, //
+                                       OUTPUT_1 + ":" + DONE_STREAM))
+                       .build();
+    }
+}
