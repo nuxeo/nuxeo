2d8658fa58957a206a892653464075eb1fec5d73
Guillaume Renard <grenard@nuxeo.com>
2019-12-16 13:11:43 +0100

    NXP-28008: add routing task page provider + use it in task endpoint

--- addons/nuxeo-platform-document-routing/nuxeo-routing-rest-api/src/main/java/org/nuxeo/ecm/restapi/server/jaxrs/routing/TaskObject.java
+++ addons/nuxeo-platform-document-routing/nuxeo-routing-rest-api/src/main/java/org/nuxeo/ecm/restapi/server/jaxrs/routing/TaskObject.java
@@ -23,6 +23,7 @@ package org.nuxeo.ecm.restapi.server.jaxrs.routing;
 import java.util.List;
 import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.PUT;
@@ -32,26 +33,54 @@ import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 
+import org.apache.commons.lang3.StringUtils;
 import org.nuxeo.ecm.core.api.CoreSession;
 import org.nuxeo.ecm.core.api.DocumentModel;
 import org.nuxeo.ecm.core.api.IdRef;
+import org.nuxeo.ecm.platform.query.api.PageProviderDefinition;
+import org.nuxeo.ecm.platform.query.api.PageProviderService;
 import org.nuxeo.ecm.platform.routing.api.DocumentRoutingService;
 import org.nuxeo.ecm.platform.routing.core.io.TaskCompletionRequest;
 import org.nuxeo.ecm.platform.task.Task;
 import org.nuxeo.ecm.platform.task.TaskConstants;
+import org.nuxeo.ecm.restapi.server.jaxrs.PaginableObject;
 import org.nuxeo.ecm.webengine.model.WebObject;
-import org.nuxeo.ecm.webengine.model.impl.DefaultObject;
 import org.nuxeo.runtime.api.Framework;
 
 /**
  * @since 7.2
  */
 @WebObject(type = "task")
-public class TaskObject extends DefaultObject {
+public class TaskObject extends PaginableObject<Task> {
+
+    /**
+     * @since 11.1
+     */
+    public static final String PAGE_PROVIDER_NAME = "nuxeo_tasks_listing";
 
     public static final String BASE_QUERY = String.format(
             "SELECT * FROM Document WHERE ecm:mixinType = '%s' AND ecm:currentLifeCycleState = '%s'",
             TaskConstants.TASK_FACET_NAME, TaskConstants.TASK_OPENED_LIFE_CYCLE_STATE);
+
+    protected String userId;
+
+    protected String workflowInstanceId;
+
+    /**
+     * For backward compatibility only. This parameter forces post-filtering and prevents paginated results.
+     * @deprecated since 11.1
+     */
+    @Deprecated
+    protected String workflowModelName;
+
+    @Override
+    protected void initialize(Object... args) {
+        super.initialize(args);
+        HttpServletRequest request = ctx.getRequest();
+        userId = request.getParameter("userId");
+        workflowInstanceId = request.getParameter("workflowInstanceId");
+        workflowModelName = request.getParameter("workflowModelName");
+    }
 
     @PUT
     @Path("{taskId}/reassign")
@@ -85,11 +114,13 @@ public class TaskObject extends DefaultObject {
     }
 
     @GET
-    public List<Task> getUserRelatedWorkflowTasks(@QueryParam("userId") String userId,
-            @QueryParam("workflowInstanceId") String workflowInstanceId,
-            @QueryParam("workflowModelName") String workflowModelName) {
-        return Framework.getService(DocumentRoutingService.class).getTasks(null, userId, workflowInstanceId,
-                workflowModelName, getContext().getCoreSession());
+    public List<Task> getUserRelatedWorkflowTasks() {
+        if (StringUtils.isNotBlank(workflowModelName)) {
+            return Framework.getService(DocumentRoutingService.class).getTasks(null, userId, workflowInstanceId,
+                    workflowModelName, getContext().getCoreSession());
+        } else {
+            return getPaginableEntries();
+        }
     }
 
     @GET
@@ -97,6 +128,17 @@ public class TaskObject extends DefaultObject {
     public Task getTaskById(@PathParam("taskId") String taskId) {
         DocumentModel docModel = getContext().getCoreSession().getDocument(new IdRef(taskId));
         return docModel.getAdapter(Task.class);
+    }
+
+    @Override
+    protected PageProviderDefinition getPageProviderDefinition() {
+        PageProviderService pageProviderService = Framework.getService(PageProviderService.class);
+        return pageProviderService.getPageProviderDefinition(PAGE_PROVIDER_NAME);
+    }
+
+    @Override
+    protected Object[] getParams() {
+        return new Object[] { userId, workflowInstanceId };
     }
 
 }
