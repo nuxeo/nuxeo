9be445b9e5ea55d2dbbd9d641f83cac6ee8763ac
Kevin Leturc <kleturc@nuxeo.com>
2020-01-31 12:37:06 +0100

    NXP-28484: Allow to partially update comments/annotations through REST

--- nuxeo-features/nuxeo-platform-comment/nuxeo-platform-comment/src/test/java/org/nuxeo/ecm/platform/comment/TestPropertyAnnotationNotification.java
+++ nuxeo-features/nuxeo-platform-comment/nuxeo-platform-comment/src/test/java/org/nuxeo/ecm/platform/comment/TestPropertyAnnotationNotification.java
@@ -19,6 +19,19 @@
 
 package org.nuxeo.ecm.platform.comment;
 
+import static org.nuxeo.ecm.platform.comment.CommentUtils.checkDocumentEventContext;
+import static org.nuxeo.ecm.platform.comment.api.CommentEvents.COMMENT_ADDED;
+import static org.nuxeo.ecm.platform.comment.api.CommentEvents.COMMENT_REMOVED;
+import static org.nuxeo.ecm.platform.comment.api.CommentEvents.COMMENT_UPDATED;
+import static org.nuxeo.ecm.platform.comment.workflow.utils.CommentsConstants.COMMENT_PARENT_ID;
+
+import org.junit.Test;
+import org.nuxeo.ecm.core.api.DocumentModel;
+import org.nuxeo.ecm.core.api.IdRef;
+import org.nuxeo.ecm.core.event.Event;
+import org.nuxeo.ecm.core.event.test.CapturingEventListener;
+import org.nuxeo.ecm.platform.comment.api.Annotation;
+import org.nuxeo.ecm.platform.comment.api.Comment;
 import org.nuxeo.ecm.platform.comment.api.CommentManager;
 import org.nuxeo.ecm.platform.comment.impl.PropertyCommentManager;
 
@@ -30,5 +43,71 @@ public class TestPropertyAnnotationNotification extends AbstractTestAnnotationNo
     @Override
     protected Class<? extends CommentManager> getType() {
         return PropertyCommentManager.class;
+    }
+
+    @Test
+    @Override
+    public void shouldNotifyEventWhenUpdateAnnotation() {
+        Annotation annotation = createAnnotationAndAddSubscription("CommentUpdated");
+        try (CapturingEventListener listener = new CapturingEventListener(COMMENT_UPDATED)) {
+            annotation.setText("I update the annotation");
+            annotationService.updateAnnotation(session, annotation.getId(), annotation);
+            DocumentModel annotationDocumentModel = session.getDocument(new IdRef(annotation.getId()));
+            DocumentModel annotationParentDocumentModel = session.getDocument(
+                    new IdRef((String) annotationDocumentModel.getPropertyValue(COMMENT_PARENT_ID)));
+            transactionalFeature.nextTransaction();
+
+            Event expectedEvent = listener.streamCapturedEvents()
+                                          .findFirst()
+                                          .orElseThrow(() -> new AssertionError("Event wasn't fired"));
+            checkDocumentEventContext(expectedEvent, annotationDocumentModel, annotationParentDocumentModel,
+                    annotatedDocumentModel);
+        }
+    }
+
+    @Test
+    @Override
+    public void shouldNotifyEventWhenRemoveAnnotation() {
+        Annotation annotation = createAnnotation(annotatedDocumentModel);
+        DocumentModel annotationDocModel = session.getDocument(new IdRef(annotation.getId()));
+        annotationDocModel.detach(true);
+        transactionalFeature.nextTransaction();
+        try (CapturingEventListener listener = new CapturingEventListener(COMMENT_REMOVED)) {
+            annotationService.deleteAnnotation(session, annotation.getId());
+            DocumentModel annotationParentDocumentModel = session.getDocument(
+                    new IdRef((String) annotationDocModel.getPropertyValue(COMMENT_PARENT_ID)));
+            transactionalFeature.nextTransaction();
+
+            Event expectedEvent = listener.streamCapturedEvents()
+                                          .findFirst()
+                                          .orElseThrow(() -> new AssertionError("Event wasn't fired"));
+            checkDocumentEventContext(expectedEvent, annotationDocModel, annotationParentDocumentModel,
+                    annotatedDocumentModel);
+        }
+    }
+
+    @Test
+    @Override
+    public void shouldNotifyWithTheRightAnnotatedDocument() {
+        // First comment
+        Annotation annotation = createAnnotation(annotatedDocumentModel);
+        DocumentModel annotationDocModel = session.getDocument(new IdRef(annotation.getId()));
+        // before subscribing, or previous event will be notified as well
+        transactionalFeature.nextTransaction();
+        // Reply
+        addSubscriptions("CommentAdded");
+        try (CapturingEventListener listener = new CapturingEventListener(COMMENT_ADDED)) {
+            Comment reply = createAnnotation(annotationDocModel);
+            DocumentModel replyDocumentModel = session.getDocument(new IdRef(reply.getId()));
+            DocumentModel annotationParentDocumentModel = session.getDocument(
+                    new IdRef((String) replyDocumentModel.getPropertyValue(COMMENT_PARENT_ID)));
+            transactionalFeature.nextTransaction();
+
+            Event expectedEvent = listener.streamCapturedEvents()
+                                          .findFirst()
+                                          .orElseThrow(() -> new AssertionError("Event wasn't fired"));
+            checkDocumentEventContext(expectedEvent, replyDocumentModel, annotationParentDocumentModel,
+                    annotatedDocumentModel);
+        }
     }
 }
--- nuxeo-features/nuxeo-platform-comment/nuxeo-platform-comment/src/test/java/org/nuxeo/ecm/platform/comment/TestTreeCommentNotification.java
+++ nuxeo-features/nuxeo-platform-comment/nuxeo-platform-comment/src/test/java/org/nuxeo/ecm/platform/comment/TestTreeCommentNotification.java
@@ -12,15 +12,24 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *  
+ *
  * Contributors:
  *     Salem Aouana
  */
 
 package org.nuxeo.ecm.platform.comment;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.nuxeo.ecm.platform.comment.CommentUtils.createUser;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Test;
 import org.nuxeo.ecm.platform.comment.api.CommentManager;
 import org.nuxeo.ecm.platform.comment.impl.TreeCommentManager;
+import org.nuxeo.ecm.platform.ec.notification.NotificationConstants;
 import org.nuxeo.runtime.test.runner.Features;
 
 /**
@@ -33,4 +42,29 @@ public class TestTreeCommentNotification extends AbstractTestCommentNotification
     protected Class<? extends CommentManager> getType() {
         return TreeCommentManager.class;
     }
+
+    @Test
+    public void testAutoSubscribingOnlyOnceToNewComments() {
+        String john = "john";
+        String johnSubscription = NotificationConstants.USER_PREFIX + john;
+        createUser(john);
+        List<String> subscriptions = notificationService.getSubscriptionsForUserOnDocument(johnSubscription,
+                commentedDocumentModel);
+        assertEquals(0, subscriptions.size());
+        createComment(commentedDocumentModel, john, "Test message");
+        transactionalFeature.nextTransaction();
+        commentedDocumentModel.refresh();
+        subscriptions = notificationService.getSubscriptionsForUserOnDocument(johnSubscription, commentedDocumentModel);
+        List<String> expectedSubscriptions = Arrays.asList("CommentAdded", "CommentUpdated");
+        assertEquals(expectedSubscriptions.size(), subscriptions.size());
+        assertTrue(subscriptions.containsAll(expectedSubscriptions));
+        for (String subscription : subscriptions) {
+            notificationService.removeSubscription(johnSubscription, subscription, commentedDocumentModel);
+        }
+        createComment(commentedDocumentModel, john, "Test message again");
+        transactionalFeature.nextTransaction();
+        subscriptions = notificationService.getSubscriptionsForUserOnDocument(johnSubscription, commentedDocumentModel);
+        assertTrue(subscriptions.isEmpty());
+    }
+
 }
