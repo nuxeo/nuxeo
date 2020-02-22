3f9cb9edd49afd50f5f4217e396aa8681fed27f7
Florent Guillaume <fguillaume@nuxeo.com>
2020-02-22 16:54:39 +0100

    NXP-26690: simplify LockManager code

--- nuxeo-core/nuxeo-core-api/src/main/java/org/nuxeo/ecm/core/api/lock/LockManager.java
+++ nuxeo-core/nuxeo-core-api/src/main/java/org/nuxeo/ecm/core/api/lock/LockManager.java
@@ -81,11 +81,15 @@ public interface LockManager {
     /**
      * Closes the lock manager and releases resources.
      */
-    void closeLockManager();
+    default void closeLockManager() {
+        // nothing
+    }
 
     /**
      * Clears any cache held by the lock manager.
      */
-    void clearLockManagerCaches();
+    default void clearLockManagerCaches() {
+        // nothing
+    }
 
 }
