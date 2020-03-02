e3d6a4faf31dee0f66eebc03b40ad317429f1582
Kevin Leturc <kleturc@nuxeo.com>
2020-03-02 11:20:04 +0100

    NXP-28711: Set Repeat annotation on TestTransactedAudit tests

--- nuxeo-features/nuxeo-platform-audit/nuxeo-platform-audit-core/src/test/java/org/nuxeo/ecm/platform/audit/TestTransactedAudit.java
+++ nuxeo-features/nuxeo-platform-audit/nuxeo-platform-audit-core/src/test/java/org/nuxeo/ecm/platform/audit/TestTransactedAudit.java
@@ -42,11 +42,13 @@ import org.nuxeo.ecm.platform.audit.api.LogEntry;
 import org.nuxeo.runtime.api.Framework;
 import org.nuxeo.runtime.test.runner.Features;
 import org.nuxeo.runtime.test.runner.FeaturesRunner;
+import org.nuxeo.runtime.test.runner.RandomBug;
 import org.nuxeo.runtime.test.runner.TransactionalFeature;
 import org.nuxeo.runtime.transaction.TransactionHelper;
 
 @RunWith(FeaturesRunner.class)
 @Features(AuditFeature.class)
+@RandomBug.Repeat(issue = "NXP-28711: randomly failing in dev mode", onFailure = 10, onSuccess = 30)
 public class TestTransactedAudit {
 
     protected @Inject CoreSession repo;
