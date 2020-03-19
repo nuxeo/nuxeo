94b482a705b3eb14791fce2516149b897cb835c5
Antoine Taillefer <ataillefer@nuxeo.com>
2020-03-20 00:00:59 +0100

    NXBT-3273: WIP - test nuxeo Helm chart 1.0.4-PR-6-2

--- ci/Jenkinsfiles/build.groovy
+++ ci/Jenkinsfiles/build.groovy
@@ -138,6 +138,7 @@ def buildUnitTestStage(env) {
               jx step helm install ${HELM_CHART_REPOSITORY_NAME}/${HELM_CHART_NUXEO} \
                 --name=${TEST_HELM_CHART_RELEASE} \
                 --namespace=${testNamespace} \
+                --version=1.0.4-PR-6-1 \
                 ${testValues}
             """
             // wait for Redis to be ready
