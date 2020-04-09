cc30bb3ee5535586c83adc6b1cbd998faf724580
Thomas Roger <troger@nuxeo.com>
2020-04-09 15:07:46 +0200

    NXP-28841: make sure it's the username's user workspace
    The check is done with the user workspace ACLs by checking if the username
    has 'Everything' permission.
    The user workspace retriaval is as before NXP-26003.

--- modules/platform/nuxeo-user-profile/src/test/java/org/nuxeo/ecm/user/center/profile/TestUserProfileImporterOk.java
+++ modules/platform/nuxeo-user-profile/src/test/java/org/nuxeo/ecm/user/center/profile/TestUserProfileImporterOk.java
@@ -73,9 +73,6 @@ public class TestUserProfileImporterOk extends AbstractUserProfileImporterTest {
     }
 
     private void checkDocs() throws Exception {
-        // make sure user1, user2 and user3 exist
-        createUsers();
-
         File blobsFolder = getBlobsFolder();
 
         DocumentModel doc = userProfileService.getUserProfileDocument("user1", session);
@@ -116,9 +113,4 @@ public class TestUserProfileImporterOk extends AbstractUserProfileImporterTest {
                 new SimpleDateFormat(ImporterConfig.DEFAULT_DATE_FORMAT).format(birthDate.getTime()));
     }
 
-    protected void createUsers() {
-        createUser("user1", null);
-        createUser("user2", null);
-        createUser("user3", null);
-    }
 }
