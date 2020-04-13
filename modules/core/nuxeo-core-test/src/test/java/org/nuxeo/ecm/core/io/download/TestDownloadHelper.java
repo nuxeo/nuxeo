0ae0574cfa931eae50a2f02cab257fa925b495ce
Florent Guillaume <fguillaume@nuxeo.com>
2020-04-13 19:31:45 +0200

    NXP-28919: move ByteRange to nuxeo-core-api

--- modules/core/nuxeo-core-test/src/test/java/org/nuxeo/ecm/core/io/download/TestDownloadHelper.java
+++ modules/core/nuxeo-core-test/src/test/java/org/nuxeo/ecm/core/io/download/TestDownloadHelper.java
@@ -22,7 +22,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
 
 import org.junit.Test;
-import org.nuxeo.ecm.core.io.download.DownloadService.ByteRange;
+import org.nuxeo.ecm.core.blob.ByteRange;
 
 public class TestDownloadHelper {
 
