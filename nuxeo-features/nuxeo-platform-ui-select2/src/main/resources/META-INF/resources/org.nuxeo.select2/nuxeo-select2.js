7267fcd25075b585ab0cd11730d21102fd52a3ac
Nelson Silva <nsilva@nuxeo.com>
2020-04-28 09:50:28 +0100

    NXP-28990: revert changes to nuxeo-select2 done in NXP-27564

--- nuxeo-features/nuxeo-platform-ui-select2/src/main/resources/META-INF/resources/org.nuxeo.select2/nuxeo-select2.js
+++ nuxeo-features/nuxeo-platform-ui-select2/src/main/resources/META-INF/resources/org.nuxeo.select2/nuxeo-select2.js
@@ -138,6 +138,9 @@
       } else {
         temp.query = params.query;
       }
+      if (typeof temp.quotePatternParameters === "undefined") {
+        temp.quotePatternParameters = false;
+      }
     } else {
       // custom operation, pass at least the query term
       temp.searchTerm = query.term;
