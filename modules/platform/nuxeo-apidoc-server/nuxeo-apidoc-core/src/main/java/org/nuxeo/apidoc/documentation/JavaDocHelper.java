e71a01299874571484cd138d565f21b79dfc82ff
Anahide Tchertchian <at@nuxeo.com>
2020-05-19 14:06:05 +0200

    NXP-29125: remove obsolete and broken logics on javadoc helper

--- modules/platform/nuxeo-apidoc-server/nuxeo-apidoc-core/src/main/java/org/nuxeo/apidoc/documentation/JavaDocHelper.java
+++ modules/platform/nuxeo-apidoc-server/nuxeo-apidoc-core/src/main/java/org/nuxeo/apidoc/documentation/JavaDocHelper.java
@@ -25,11 +25,7 @@ public class JavaDocHelper {
 
     public static final String BASE_URL = "http://community.nuxeo.com/api/";
 
-    public static final String CM_BASE = "nuxeo-case-management";
-
     public static final String DM_BASE = "nuxeo";
-
-    public static final String DAM_BASE = "nuxeo-dam";
 
     public static final String DEFAULT_DIST = DM_BASE;
 
@@ -56,34 +52,12 @@ public class JavaDocHelper {
     }
 
     public String getBaseUrl(String className) {
-
         String base = defaultPrefix;
-
-        if (!docVersion.startsWith("5.")) {
-            // version < 5.5 : before merge of DAM/DAM/CAP/CMF ...
-            if (className.contains("org.nuxeo.cm")) {
-                base = CM_BASE;
-            } else if (className.contains("org.nuxeo.dam")) {
-                base = DAM_BASE;
-            } else {
-                base = DEFAULT_DIST;
-            }
-        }
-
         return BASE_URL + base + "/" + docVersion;
     }
 
     public static JavaDocHelper getHelper(String distribName, String distribVersion) {
-
         String base = DEFAULT_DIST;
-        if (!distribVersion.startsWith("5.")) {
-            // version < 5.5 : before merge of DAM/DAM/CAP/CMF ...
-            if (distribName.toUpperCase().contains("CM") || distribName.toUpperCase().contains("CASE")) {
-                base = CM_BASE;
-            } else if (distribName.toUpperCase().contains("DAM")) {
-                base = DAM_BASE;
-            }
-        }
         return new JavaDocHelper(base, distribVersion);
     }
 
