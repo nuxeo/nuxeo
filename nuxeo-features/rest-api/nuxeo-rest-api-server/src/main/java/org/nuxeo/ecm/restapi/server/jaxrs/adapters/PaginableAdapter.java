6a9594a1c854d185c97d22cbd6b223bb2c016ada
Salem Aouana <saouana@nuxeo.com>
2020-06-02 10:34:37 +0200

    NXP-28634: Fix sorting and pagination (infinite scroll) in page provider

--- nuxeo-features/rest-api/nuxeo-rest-api-server/src/main/java/org/nuxeo/ecm/restapi/server/jaxrs/adapters/PaginableAdapter.java
+++ nuxeo-features/rest-api/nuxeo-rest-api-server/src/main/java/org/nuxeo/ecm/restapi/server/jaxrs/adapters/PaginableAdapter.java
@@ -19,7 +19,9 @@
 package org.nuxeo.ecm.restapi.server.jaxrs.adapters;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
@@ -31,6 +33,7 @@ import org.nuxeo.ecm.automation.core.util.PaginablePageProvider;
 import org.nuxeo.ecm.core.api.DocumentModel;
 import org.nuxeo.ecm.core.api.DocumentModelList;
 import org.nuxeo.ecm.core.api.NuxeoException;
+import org.nuxeo.ecm.core.api.SortInfo;
 import org.nuxeo.ecm.platform.query.api.PageProvider;
 import org.nuxeo.ecm.platform.query.api.PageProviderDefinition;
 import org.nuxeo.ecm.platform.query.api.PageProviderService;
@@ -114,25 +117,24 @@ public abstract class PaginableAdapter<T> extends DefaultAdapter {
         PageProviderService pps = Framework.getService(PageProviderService.class);
         Map<String, Serializable> props = new HashMap<>();
         props.put(CoreQueryDocumentPageProvider.CORE_SESSION_PROPERTY, (Serializable) ctx.getCoreSession());
-        PageProvider<T> pp = (PageProvider<T>) pps.getPageProvider("", ppDefinition, getSearchDocument(), null,
-                pageSize, currentPageIndex, props, getParams());
+        List<SortInfo> sortInfos = null;
         if (!StringUtils.isBlank(sortBy)) {
             String[] sorts = sortBy.split(",");
             String[] orders = null;
             if (!StringUtils.isBlank(sortOrder)) {
                 orders = sortOrder.split(",");
             }
-            if (sorts != null) {
-                // clear potential default sort infos first
-                pp.setSortInfos(null);
-                for (int i = 0; i < sorts.length; i++) {
-                    String sort = sorts[i];
-                    boolean sortAscending = (orders != null && orders.length > i
-                            && "asc".equals(orders[i].toLowerCase())) ? true : false;
-                    pp.addSortInfo(sort, sortAscending);
-                }
+
+            sortInfos = new ArrayList<>(sorts.length);
+            for (int i = 0; i < sorts.length; i++) {
+                String sort = sorts[i];
+                boolean sortAscending = orders != null && orders.length > i && "asc".equals(orders[i].toLowerCase());
+                sortInfos.add(new SortInfo(sort, sortAscending));
             }
         }
+        PageProvider<T> pp = (PageProvider<T>) pps.getPageProvider("", ppDefinition, getSearchDocument(), sortInfos,
+                pageSize, currentPageIndex, props, getParams());
+
         return getPaginableEntries(pp);
     }
 
