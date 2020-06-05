ee9fd5318e5da4c5fbc132b25931b6dacc4b117e
Anahide Tchertchian <at@nuxeo.com>
2020-06-05 12:49:49 +0200

    NXP-29193: Fix IndexOutOfBoundException when viewing an invalid explorer distribution

--- modules/platform/nuxeo-apidoc-server/nuxeo-apidoc-repo/src/test/java/org/nuxeo/apidoc/test/TestDistributionResolver.java
+++ modules/platform/nuxeo-apidoc-server/nuxeo-apidoc-repo/src/test/java/org/nuxeo/apidoc/test/TestDistributionResolver.java
@@ -19,246 +19,82 @@
 package org.nuxeo.apidoc.test;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
-import java.io.InputStream;
-import java.io.OutputStream;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Date;
 import java.util.List;
-import java.util.Map;
 
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
-import org.nuxeo.apidoc.api.BundleGroup;
-import org.nuxeo.apidoc.api.BundleInfo;
-import org.nuxeo.apidoc.api.ComponentInfo;
-import org.nuxeo.apidoc.api.ExtensionInfo;
-import org.nuxeo.apidoc.api.ExtensionPointInfo;
-import org.nuxeo.apidoc.api.OperationInfo;
-import org.nuxeo.apidoc.api.PackageInfo;
-import org.nuxeo.apidoc.api.ServiceInfo;
-import org.nuxeo.apidoc.plugin.PluginSnapshot;
 import org.nuxeo.apidoc.snapshot.DistributionSnapshot;
 import org.nuxeo.apidoc.snapshot.SnapshotResolverHelper;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 public class TestDistributionResolver {
 
-    protected static final String[] alldistribs = { "cap-5.5", "cap-5.5-RC1", "cap-5.5-SNAPSHOT", "cap-5.6-SNAPSHOT",
-            "cap-5.6-RC1", "dm-5.5", "Nuxeo DM-5.4.1", "Nuxeo Platform-5.6-RC1" };
+    protected static final String[] ALL_DISTRIBS = {
+            // legacy
+            "cap-5.5", "cap-5.5-RC1", "cap-5.5-SNAPSHOT", "cap-5.6-SNAPSHOT", "cap-5.6-RC1", "dm-5.5", "Nuxeo DM-5.4.1",
+            "Nuxeo Platform-5.6-RC1", "server-10.10",
+            // as of 11.1
+            "server-11.1-SNAPSHOT", "server-11.1", "server-11.1.1", "server.11.1.1-SNAPSHOT",
+            "server-11.1-I20200502_2326" };
 
-    protected List<DistributionSnapshot> buildFakeSnaps() {
+    protected static final List<DistributionSnapshot> fakeSnapshots = new ArrayList<>();
 
-        List<DistributionSnapshot> snaps = new ArrayList<>();
-        for (final String distId : alldistribs) {
-
-            DistributionSnapshot snap = new DistributionSnapshot() {
-
-                String id = distId;
-
-                @Override
-                public boolean isLive() {
-                    return false;
-                }
-
-                @Override
-                public String getVersion() {
-                    return id.replace(getName() + "-", "");
-                }
-
-                @Override
-                public String getName() {
-                    return distId.split("-")[0];
-                }
-
-                @Override
-                public Date getCreationDate() {
-                    return null;
-                }
-
-                @Override
-                public Date getReleaseDate() {
-                    return null;
-                }
-
-                @Override
-                public List<String> getXmlComponentIds() {
-                    return null;
-                }
-
-                @Override
-                public List<String> getServiceIds() {
-                    return null;
-                }
-
-                @Override
-                public ServiceInfo getService(String id) {
-                    return null;
-                }
-
-                @Override
-                public List<OperationInfo> getOperations() {
-                    return null;
-                }
-
-                @Override
-                public OperationInfo getOperation(String id) {
-                    return null;
-                }
-
-                @Override
-                public String getKey() {
-                    return id;
-                }
-
-                @Override
-                public void cleanPreviousArtifacts() {
-                    // empty body
-                }
-
-                @Override
-                public boolean isLatestFT() {
-                    return false;
-                }
-
-                @Override
-                public boolean isLatestLTS() {
-                    return false;
-                }
-
-                @Override
-                public List<String> getAliases() {
-                    return Collections.emptyList();
-                }
-
-                @Override
-                public boolean isHidden() {
-                    return false;
-                }
-
-                @Override
-                public List<String> getJavaComponentIds() {
-                    return null;
-                }
-
-                @Override
-                public List<String> getExtensionPointIds() {
-                    return null;
-                }
-
-                @Override
-                public ExtensionPointInfo getExtensionPoint(String id) {
-                    return null;
-                }
-
-                @Override
-                public List<ExtensionInfo> getContributions() {
-                    return null;
-                }
-
-                @Override
-                public List<String> getContributionIds() {
-                    return null;
-                }
-
-                @Override
-                public ExtensionInfo getContribution(String id) {
-                    return null;
-                }
-
-                @Override
-                public List<String> getComponentIds() {
-                    return null;
-                }
-
-                @Override
-                public ComponentInfo getComponent(String id) {
-                    return null;
-                }
-
-                @Override
-                public List<String> getBundleIds() {
-                    return null;
-                }
-
-                @Override
-                public List<BundleInfo> getBundles() {
-                    throw new UnsupportedOperationException();
-                }
-
-                @Override
-                public List<BundleGroup> getBundleGroups() {
-                    return null;
-                }
-
-                @Override
-                public BundleGroup getBundleGroup(String groupId) {
-                    return null;
-                }
-
-                @Override
-                public BundleInfo getBundle(String id) {
-                    return null;
-                }
-
-                @Override
-                public PackageInfo getPackage(String id) {
-                    throw new UnsupportedOperationException();
-                }
-
-                @Override
-                public List<PackageInfo> getPackages() {
-                    throw new UnsupportedOperationException();
-                }
-
-                @Override
-                public ObjectMapper getJsonMapper() {
-                    throw new UnsupportedOperationException();
-                }
-
-                @Override
-                public void writeJson(OutputStream out) {
-                    throw new UnsupportedOperationException();
-                }
-
-                @Override
-                public DistributionSnapshot readJson(InputStream in) {
-                    throw new UnsupportedOperationException();
-                }
-
-                @Override
-                public Map<String, PluginSnapshot<?>> getPluginSnapshots() {
-                    throw new UnsupportedOperationException();
-                }
-
-            };
-            snaps.add(snap);
+    @BeforeClass
+    public static void buildFakeSnapshots() {
+        for (String id : ALL_DISTRIBS) {
+            DistributionSnapshot snap = mock(DistributionSnapshot.class);
+            when(snap.getKey()).thenReturn(id);
+            String name = id.split("-")[0];
+            when(snap.getName()).thenReturn(name);
+            String version = id.replace(name + "-", "");
+            when(snap.getVersion()).thenReturn(version);
+            if (id.startsWith("server-11.1.1")) {
+                when(snap.getAliases()).thenReturn(List.of("current"));
+            } else {
+                when(snap.getAliases()).thenReturn(List.of());
+            }
+            fakeSnapshots.add(snap);
         }
-
-        return snaps;
     }
 
-    protected String testResolution(List<DistributionSnapshot> snaps, String target) {
-        String match = SnapshotResolverHelper.findBestMatch(snaps, target);
-        assertNotNull(match);
-        return match;
+    @AfterClass
+    public static void clearFakeSnapshots() {
+        fakeSnapshots.clear();
+    }
+
+    protected void check(String expected, String target) {
+        String match = SnapshotResolverHelper.findBestMatch(fakeSnapshots, target);
+        assertEquals(expected, match);
     }
 
     @Test
     public void testResolver() {
-        List<DistributionSnapshot> snaps = buildFakeSnaps();
-        String res = testResolution(snaps, "dm-5.6");
-        assertEquals("Nuxeo Platform-5.6-RC1", res);
-        res = testResolution(snaps, "dm-5.5");
-        assertEquals("dm-5.5", res);
-        res = testResolution(snaps, "cap-5.6");
-        assertEquals("Nuxeo Platform-5.6-RC1", res);
-        res = testResolution(snaps, "cap-5.6-SNAPSHOT");
-        assertEquals("cap-5.6-SNAPSHOT", res);
-        res = testResolution(snaps, "cap-5.6-RC1");
-        assertEquals("cap-5.6-RC1", res);
-
+        check(null, "");
+        // legacy
+        check("Nuxeo Platform-5.6-RC1", "dm-5.6");
+        check("dm-5.5", "dm-5.5");
+        check("Nuxeo Platform-5.6-RC1", "cap-5.6");
+        check("cap-5.6-SNAPSHOT", "cap-5.6-SNAPSHOT");
+        check("cap-5.6-RC1", "cap-5.6-RC1");
+        check("server-10.10", "server-10.10");
+        // HF use case not covered
+        check("server-11.1", "server-10.10-HF32");
+        // as of 11.1
+        check("server-11.1-SNAPSHOT", "server-11.1-SNAPSHOT");
+        check("server-11.1", "server-11.1");
+        check("server-11.1-I20200502_2326", "server-11.1-I20200502_2326"); // non-regression test for NXP-29193
+        check("server-11.1-I20200502_2326", "server-11.1-I20200401_0001");
+        check("server-11.1.1", "server-11.1.1-SNAPSHOT");
+        check("server-11.1.1", "server-11.1.1");
+        check("server-11.1.1", "server-11.1.2");
+        check("server-11.1.1", "server-11.1.2-SNAPSHOT");
+        check("server-11.1.1", "server-11.2-SNAPSHOT");
+        check("server-11.1.1", "current");
+        check(null, "my-server-11.1-SNAPSHOT"); // non-regression test for NXP-29193
     }
+
 }
