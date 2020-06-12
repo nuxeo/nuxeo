c49ce1ac81b542e1ed5c4f1955778110022160d6
Thomas Roger <troger@nuxeo.com>
2020-06-12 10:02:06 +0200

    NXP-29294: improve release pipeline

--- ci/Jenkinsfiles/release.groovy
+++ ci/Jenkinsfiles/release.groovy
@@ -137,34 +137,12 @@ pipeline {
               git tag -a v${RELEASE_VERSION} -m "Release ${RELEASE_VERSION}"
             """
 
-            // increment minor version
-            def nextVersion = sh(returnStdout: true, script: "perl -pe 's/\\b(\\d+)(?=\\D*\$)/\$1+1/e' <<< ${CURRENT_VERSION}").trim()
-            echo """
-            ---------------------------------------------------------
-            Update master version from ${CURRENT_VERSION} to ${nextVersion}
-            ---------------------------------------------------------
-            """
-            sh """
-              git checkout master
-
-              # root POM
-              mvn ${MAVEN_ARGS} -Pdistrib,docker versions:set -DnewVersion=${nextVersion} -DgenerateBackupPoms=false
-              perl -i -pe 's|<nuxeo.platform.version>.*?</nuxeo.platform.version>|<nuxeo.platform.version>${nextVersion}</nuxeo.platform.version>|' pom.xml
-              perl -i -pe 's|org.nuxeo.ecm.product.version=.*|org.nuxeo.ecm.product.version=${nextVersion}|' server/nuxeo-nxr-server/src/main/resources/templates/nuxeo.defaults
-
-              # nuxeo-parent POM
-              perl -i -pe 's|<version>.*?</version>|<version>${nextVersion}</version>|' parent/pom.xml
-              mvn ${MAVEN_ARGS} -f parent/pom.xml validate
-
-              git commit -a -m "Release ${RELEASE_VERSION}, update ${CURRENT_VERSION} to ${nextVersion}"
-            """
-
             if (env.DRY_RUN != "true") {
               sh """
                 jx step git credentials
                 git config credential.helper store
 
-                git push --all origin
+                git push origin v${RELEASE_VERSION}
               """
             }
           }
@@ -218,7 +196,7 @@ pipeline {
               mvn ${MAVEN_ARGS} -f ci/release/pom.xml process-resources
 
               # Upload Nuxeo packages
-              PACKAGES_TO_UPLOAD="ci/release/target/nuxeo-*-package-*.zip"
+              PACKAGES_TO_UPLOAD="ci/release/target/packages/nuxeo-*-package-*.zip"
               for file in \$PACKAGES_TO_UPLOAD ; do
                 curl --fail -i -u "$CONNECT_PASS" -F package=@\$(ls \$file) "$CONNECT_PROD_URL"/site/marketplace/upload?batch=true ;
               done
@@ -243,6 +221,47 @@ pipeline {
           """
           promoteDockerImage("${SLIM_IMAGE_NAME}", "${params.BUILD_VERSION}", "${RELEASE_VERSION}", "${LATEST_VERSION}")
           promoteDockerImage("${NUXEO_IMAGE_NAME}", "${params.BUILD_VERSION}", "${RELEASE_VERSION}", "${LATEST_VERSION}")
+        }
+      }
+    }
+
+    stage('Bump master branch') {
+      steps {
+        container('maven') {
+          // increment minor version
+          def nextVersion = sh(returnStdout: true, script: "perl -pe 's/\\b(\\d+)(?=\\D*\$)/\$1+1/e' <<< ${CURRENT_VERSION}").trim()
+          echo """
+          -----------------------------------------------
+          Update master version from ${CURRENT_VERSION} to ${nextVersion}
+          -----------------------------------------------
+          """
+          sh """
+            git checkout master
+
+            # root POM
+            mvn ${MAVEN_ARGS} -Pdistrib,docker versions:set -DnewVersion=${nextVersion} -DgenerateBackupPoms=false
+            perl -i -pe 's|<nuxeo.platform.version>.*?</nuxeo.platform.version>|<nuxeo.platform.version>${nextVersion}</nuxeo.platform.version>|' pom.xml
+            perl -i -pe 's|org.nuxeo.ecm.product.version=.*|org.nuxeo.ecm.product.version=${nextVersion}|' server/nuxeo-nxr-server/src/main/resources/templates/nuxeo.defaults
+
+            # nuxeo-parent POM
+            perl -i -pe 's|<version>.*?</version>|<version>${nextVersion}</version>|' parent/pom.xml
+            mvn ${MAVEN_ARGS} -f parent/pom.xml validate
+
+            # nuxeo-promote-packages POM
+            perl -i -pe 's|<version>.*?</version>|<version>${nextVersion}</version>|' ci/release/pom.xml
+            mvn ${MAVEN_ARGS} -f ci/release/pom.xml validate
+
+            git commit -a -m "Release ${RELEASE_VERSION}, update ${CURRENT_VERSION} to ${nextVersion}"
+          """
+
+          if (env.DRY_RUN != "true") {
+            sh """
+              jx step git credentials
+              git config credential.helper store
+
+              git push origin master
+            """
+          }
         }
       }
     }
