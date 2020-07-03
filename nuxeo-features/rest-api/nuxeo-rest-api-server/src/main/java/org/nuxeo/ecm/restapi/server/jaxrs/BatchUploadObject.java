4f444b0e34a457d4a07b4edd0b2cebb3de395916
Nour AL KOTOB <nalkotob@nuxeo.com>
2020-07-03 13:11:14 +0200

    NXP-29246: add MHTML mime-type support

--- nuxeo-features/rest-api/nuxeo-rest-api-server/src/main/java/org/nuxeo/ecm/restapi/server/jaxrs/BatchUploadObject.java
+++ nuxeo-features/rest-api/nuxeo-rest-api-server/src/main/java/org/nuxeo/ecm/restapi/server/jaxrs/BatchUploadObject.java
@@ -77,6 +77,7 @@ import org.nuxeo.ecm.webengine.model.exceptions.IllegalParameterException;
 import org.nuxeo.ecm.webengine.model.impl.AbstractResource;
 import org.nuxeo.ecm.webengine.model.impl.ResourceTypeImpl;
 import org.nuxeo.runtime.api.Framework;
+import org.nuxeo.runtime.services.config.ConfigurationService;
 import org.nuxeo.runtime.transaction.TransactionHelper;
 
 import com.fasterxml.jackson.databind.JsonNode;
@@ -115,6 +116,9 @@ public class BatchUploadObject extends AbstractResource<ResourceTypeImpl> {
     protected static final String OPERATION_ID = "operationId";
 
     protected static final String REQUEST_HANDLER_NAME = "handlerName";
+
+    /** @since 10.10-HF30 */
+    protected static final String MULTIPART_DISABLED_CONFIG_KEY = "nuxeo.batch.upload.multipart.disabled";
 
     public static final String UPLOAD_TYPE_NORMAL = "normal";
 
@@ -232,7 +236,7 @@ public class BatchUploadObject extends AbstractResource<ResourceTypeImpl> {
         // TODO NXP-18247: should be set to the actual number of bytes uploaded instead of relying on the Content-Length
         // header which is not necessarily set
         long uploadedSize = getUploadedSize(request);
-        boolean isMultipart = contentType != null && contentType.contains("multipart");
+        boolean isMultipart = isMultipartEnabled() && contentType != null && contentType.contains("multipart");
 
         // Handle multipart case: mainly MSIE with jQueryFileupload
         if (isMultipart) {
@@ -295,6 +299,11 @@ public class BatchUploadObject extends AbstractResource<ResourceTypeImpl> {
             }
         }
         return buildResponse(status, result, isMultipart);
+    }
+
+    /** @since 10.10-HF30 */
+    protected boolean isMultipartEnabled() {
+        return Framework.getService(ConfigurationService.class).isBooleanFalse(MULTIPART_DISABLED_CONFIG_KEY);
     }
 
     protected long getUploadedSize(HttpServletRequest request) {
