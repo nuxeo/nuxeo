7491153317ef4feabfd326d6e5b373fdcc9fc76b
Benoit Delbosc <bdelbosc@nuxeo.com>
2020-08-21 10:01:17 +0200

    NXP-22843: Use Log4j2

--- nuxeo-runtime/nuxeo-launcher-commons/src/main/java/org/nuxeo/launcher/config/BackingServiceConfigurator.java
+++ nuxeo-runtime/nuxeo-launcher-commons/src/main/java/org/nuxeo/launcher/config/BackingServiceConfigurator.java
@@ -38,15 +38,15 @@ import java.util.Properties;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
-import org.apache.commons.lang3.StringUtils;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-import org.nuxeo.common.utils.TextTemplate;
-import org.nuxeo.launcher.config.backingservices.BackingChecker;
-
 import net.jodah.failsafe.Failsafe;
 import net.jodah.failsafe.FailsafeException;
 import net.jodah.failsafe.RetryPolicy;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.nuxeo.common.utils.TextTemplate;
+import org.nuxeo.launcher.config.backingservices.BackingChecker;
 
 /**
  * Calls backing services checks to verify that they are ready to use before starting Nuxeo.
@@ -55,7 +55,7 @@ import net.jodah.failsafe.RetryPolicy;
  */
 public class BackingServiceConfigurator {
 
-    protected static final Log log = LogFactory.getLog(BackingServiceConfigurator.class);
+    protected static final Logger log = LogManager.getLogger(BackingServiceConfigurator.class);
 
     public static final String PARAM_RETRY_POLICY_ENABLED = "nuxeo.backing.check.retry.enabled";
 
@@ -137,7 +137,7 @@ public class BackingServiceConfigurator {
             items.add("kafka");
             for (String item : items) {
                 try {
-                    log.debug("checker: " + item);
+                    log.debug("checker: {}", item);
                     File templateDir = getTemplateDir(item);
                     String classPath = getClasspathForTemplate(item);
                     String checkClass = configurationGenerator.getUserConfig()
@@ -145,15 +145,14 @@ public class BackingServiceConfigurator {
                     Optional<URLClassLoader> ucl = getClassLoaderForTemplate(templateDir, classPath);
                     if (ucl.isPresent()) {
                         Class<?> klass = Class.forName(checkClass, true, ucl.get());
-                        if (log.isDebugEnabled()) {
-                            log.debug("Adding checker " + item + " with class path: "
-                                    + Arrays.toString(ucl.get().getURLs()));
-                        }
+                        log.debug("Adding checker: {} with class path: {}", () -> item,
+                                () -> Arrays.toString(ucl.get().getURLs()));
                         checkers.add((BackingChecker) klass.getDeclaredConstructor().newInstance());
                     }
                 } catch (IOException e) {
-                    log.warn("Unable to read check configuration for template : " + item, e);
+                    log.warn("Unable to read check configuration for template: {}", item, e);
                 } catch (ReflectiveOperationException | ClassCastException e) {
+			log.warn(e.getMessage(), e);
                     throw new ConfigurationException("Unable to check configuration for backing service " + item,
                             e);
                 }
@@ -164,9 +163,9 @@ public class BackingServiceConfigurator {
 
     protected File getTemplateDir(String item) throws ConfigurationException {
         try {
-            return configurationGenerator.getTemplateDirectory(item);
+            return configurationGenerator.getTemplateConf(item).getParentFile();
         } catch (ConfigurationException e) {
-            return configurationGenerator.getTemplateDirectory("default");
+            return configurationGenerator.getTemplateConf("default").getParentFile();
         }
     }
 
@@ -207,7 +206,7 @@ public class BackingServiceConfigurator {
             for (File file : files) {
                 try {
                     urlsList.add(new URL("jar:file:" + file.getPath() + "!/"));
-                    log.debug("Added " + file.getPath());
+                    log.debug("Adding url: {}", file.getPath());
                 } catch (MalformedURLException e) {
                     log.error(e);
                 }
