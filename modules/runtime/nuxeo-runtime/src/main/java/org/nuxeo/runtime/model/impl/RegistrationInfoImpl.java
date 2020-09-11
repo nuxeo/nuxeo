f68d29f0eee18712325cd379f7d14a10a2f94631
Anahide Tchertchian <atchertchian@nuxeo.com>
2020-09-11 09:49:13 +0200

    NXP-29641: fix component manager events on pending registrations

--- modules/runtime/nuxeo-runtime/src/main/java/org/nuxeo/runtime/model/impl/RegistrationInfoImpl.java
+++ modules/runtime/nuxeo-runtime/src/main/java/org/nuxeo/runtime/model/impl/RegistrationInfoImpl.java
@@ -497,6 +497,8 @@ public class RegistrationInfoImpl implements RegistrationInfo {
                 ComponentManagerImpl.loadContributions(this, xt);
                 try {
                     component.registerExtension(xt);
+                    manager.sendEvent(new ComponentEvent(ComponentEvent.EXTENSION_REGISTERED,
+                            ((ComponentInstanceImpl) xt.getComponent()).ri, xt));
                 } catch (RuntimeException e) {
                     ComponentName compName = xt.getComponent().getName();
                     String msg = "Failed to register extension to: " + xt.getTargetComponent() + ", xpoint: "
