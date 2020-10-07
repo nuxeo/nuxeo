ca27d035c96074ba2f6091b63eeea8ac390f6383
charlesboidot <cboidot@nuxeo.com>
2020-10-07 15:41:48 +0200

    NXP-29697: Add null check and test files to avoid NPE for realm_access

--- modules/platform/login/nuxeo-platform-login-keycloak/src/main/java/org/nuxeo/ecm/platform/ui/web/keycloak/KeycloakAuthenticationPlugin.java
+++ modules/platform/login/nuxeo-platform-login-keycloak/src/main/java/org/nuxeo/ecm/platform/ui/web/keycloak/KeycloakAuthenticationPlugin.java
@@ -177,9 +177,10 @@ public class KeycloakAuthenticationPlugin implements NuxeoAuthenticationPlugin,
      */
     private Set<String> getRoles(AccessToken token, String keycloakNuxeoApp) {
         Set<String> allRoles = new HashSet<>();
-        Set<String> roles = token.getRealmAccess().getRoles();
-        if (roles != null) {
-            allRoles.addAll(roles);
+
+        AccessToken.Access realmAccess = token.getRealmAccess();
+        if (realmAccess != null && realmAccess.getRoles() != null) {
+            allRoles.addAll(realmAccess.getRoles());
         }
         AccessToken.Access nuxeoResource = token.getResourceAccess(keycloakNuxeoApp);
         if (nuxeoResource != null) {
