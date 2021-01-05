09abe15df7d275ae2ab1bebc5fb919b073d544b0
Thomas Roger <troger@nuxeo.com>
2021-01-05 18:39:02 +0100

    NXP-29867: cleanup/format

--- modules/core/nuxeo-core-event/src/test/java/org/nuxeo/ecm/core/event/pipe/TestEventListenerViaLocalPipe.java
+++ modules/core/nuxeo-core-event/src/test/java/org/nuxeo/ecm/core/event/pipe/TestEventListenerViaLocalPipe.java
@@ -18,7 +18,9 @@
  */
 package org.nuxeo.ecm.core.event.pipe;
 
-import org.junit.Assert;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
 import org.nuxeo.ecm.core.event.impl.EventServiceImpl;
 import org.nuxeo.ecm.core.event.pipe.local.LocalEventBundlePipe;
 import org.nuxeo.ecm.core.event.test.TestEventServiceComponent;
@@ -32,14 +34,13 @@ import org.nuxeo.runtime.test.runner.Deploy;
 @Deploy("org.nuxeo.ecm.core.event.test:test-LocalPipes.xml")
 public class TestEventListenerViaLocalPipe extends TestEventServiceComponent {
 
-    @Override
-    protected EventServiceImpl getService() {
-        EventServiceImpl service = super.getService();
-        Assert.assertEquals(service.getEventBundleDispatcher().getClass(),
+    @Test
+    public void testEventBundlePipe() {
+        EventServiceImpl eventServiceImpl = (EventServiceImpl) eventService;
+        assertEquals(eventServiceImpl.getEventBundleDispatcher().getClass(),
                 TestableSimpleEventBundlePipeDispatcher.class);
-        TestableSimpleEventBundlePipeDispatcher dispatcher = (TestableSimpleEventBundlePipeDispatcher) service.getEventBundleDispatcher();
-        Assert.assertEquals(dispatcher.getPipes().get(0).getClass(), LocalEventBundlePipe.class);
-        return service;
+        TestableSimpleEventBundlePipeDispatcher dispatcher = (TestableSimpleEventBundlePipeDispatcher) eventServiceImpl.getEventBundleDispatcher();
+        assertEquals(dispatcher.getPipes().get(0).getClass(), LocalEventBundlePipe.class);
     }
 
 }
