a4c8e3643642e32b2289f2a245f2f00cefc35779
charlesboidot <cboidot@nuxeo.com>
2021-01-07 17:20:49 +0100

    NXP-29723: send WorkflowContextualInfo on reassign and delegate

--- modules/platform/nuxeo-platform-task/nuxeo-platform-task-core/src/main/java/org/nuxeo/ecm/platform/task/core/service/TaskServiceImpl.java
+++ modules/platform/nuxeo-platform-task/nuxeo-platform-task-core/src/main/java/org/nuxeo/ecm/platform/task/core/service/TaskServiceImpl.java
@@ -484,7 +484,7 @@ public class TaskServiceImpl extends DefaultComponent implements TaskService {
 
     @Override
     public void reassignTask(CoreSession session, final String taskId, final List<String> newActors,
-            final String comment) {
+            final String comment, Map<String, Serializable> eventInfo) {
 
         new UnrestrictedSessionRunner(session) {
 
@@ -546,7 +546,7 @@ public class TaskServiceImpl extends DefaultComponent implements TaskService {
                     docs.add(session.getDocument(new IdRef(string)));
 
                 }
-                notifyEvent(session, task, docs, TaskEventNames.WORKFLOW_TASK_REASSIGNED, new HashMap<>(), comment,
+                notifyEvent(session, task, docs, TaskEventNames.WORKFLOW_TASK_REASSIGNED, eventInfo, comment,
                         session.getPrincipal(), actorIds);
 
             }
@@ -556,7 +556,7 @@ public class TaskServiceImpl extends DefaultComponent implements TaskService {
 
     @Override
     public void delegateTask(CoreSession session, final String taskId, final List<String> delegatedActors,
-            final String comment) {
+            final String comment, Map<String, Serializable> eventInfo) {
 
         new UnrestrictedSessionRunner(session) {
             @Override
@@ -604,7 +604,7 @@ public class TaskServiceImpl extends DefaultComponent implements TaskService {
                     docs.add(session.getDocument(new IdRef(string)));
 
                 }
-                notifyEvent(session, task, docs, TaskEventNames.WORKFLOW_TASK_DELEGATED, new HashMap<>(),
+                notifyEvent(session, task, docs, TaskEventNames.WORKFLOW_TASK_DELEGATED, eventInfo,
                         String.format("Task delegated by '%s' to '%s'", currentUser, StringUtils.join(actorIds, ","))
                                 + (!StringUtils.isEmpty(comment) ? " with the following comment: " + comment : ""),
                         session.getPrincipal(), actorIds);
