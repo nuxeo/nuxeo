33c087df4307edd09914c32cd25fd8193dc859ed
Antoine Taillefer <ataillefer@nuxeo.com>
2021-01-28 12:15:11 +0100

    NXP-30106: Fix variable interpolation for package upload

--- ci/Jenkinsfiles/build.groovy
+++ ci/Jenkinsfiles/build.groovy
@@ -870,12 +870,12 @@ pipeline {
           Upload Nuxeo Packages to ${CONNECT_PREPROD_URL}
           ----------------------------------------"""
           withCredentials([usernameColonPassword(credentialsId: 'connect-preprod', variable: 'CONNECT_PASS')]) {
-            sh """
+            sh '''
               PACKAGES_TO_UPLOAD="packages/nuxeo-*-package/target/nuxeo-*-package-*.zip"
               for file in \$PACKAGES_TO_UPLOAD ; do
-                curl --fail -i -u "$CONNECT_PASS" -F package=@\$(ls \$file) "$CONNECT_PREPROD_URL"/site/marketplace/upload?batch=true ;
+                curl --fail -i -u $CONNECT_PASS -F package=@\$(ls \$file) $CONNECT_PREPROD_URL/site/marketplace/upload?batch=true ;
               done
-            """
+            '''
           }
         }
       }
