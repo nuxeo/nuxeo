66d1f86d998882e0189158c87c24d5ad90e6fbd6
Florent Guillaume <fguillaume@nuxeo.com>
2021-02-10 15:02:17 +0100

    NXP-30044: make path strategies accept more keys

--- nuxeo-core/nuxeo-core-api/src/test/java/org/nuxeo/ecm/core/blob/TestPathStrategies.java
+++ nuxeo-core/nuxeo-core-api/src/test/java/org/nuxeo/ecm/core/blob/TestPathStrategies.java
@@ -20,16 +20,15 @@ package org.nuxeo.ecm.core.blob;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
 
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Arrays;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.nuxeo.ecm.core.api.NuxeoException;
 import org.nuxeo.runtime.test.runner.Features;
 import org.nuxeo.runtime.test.runner.FeaturesRunner;
 import org.nuxeo.runtime.test.runner.RuntimeFeature;
@@ -43,18 +42,59 @@ public class TestPathStrategies {
     }
 
     @Test
+    public void testSafeRegex() throws IOException {
+        String rejected = "";
+        for (char c = ' '; c <= '~'; c++) {
+            if (!PathStrategy.SAFE.matcher(String.valueOf(c)).matches()) {
+                rejected += c;
+            }
+        }
+        assertEquals("%/:\\", rejected);
+    }
+
+
+    @Test
+    public void testSafePath() throws IOException {
+        Path dir = Files.createTempDirectory("tmp_");
+        PathStrategy ps = new PathStrategyFlat(dir);
+
+        // unchanged
+        for (String key : Arrays.asList( //
+                "foo.doc", //
+                "hello world.bin" //
+                )) {
+            assertEquals(key, ps.safePath(key));
+        }
+
+        // encoded
+        String[] table = new String[] {
+                ".", "%.", //
+                "..", "%..", //
+                "a/b", "%a%2fb", //
+                "a\\b", "%a%5cb", //
+                "100%", "%100%25", //
+                "50:50", "%50%3a50", //
+                "caf\u00e9", "%caf%c3%a9" //
+        };
+        for (int i = 0; i < table.length; i += 2) {
+            String key = table[i];
+            String expected = table[i+1];
+            assertEquals(expected, ps.safePath(key));
+        }
+    }
+
+    @Test
     public void testPathStrategyFlat() throws IOException {
         Path dir = Files.createTempDirectory("tmp_");
         PathStrategy ps = new PathStrategyFlat(dir);
+
+        // valid key
         String path = stringify(ps.getPathForKey("123456789"));
         assertTrue(path, path.endsWith("/123456789"));
 
-        try {
-            ps.getPathForKey("../foo");
-            fail();
-        } catch (NuxeoException e) {
-            assertEquals("Invalid key: ../foo", e.getMessage());
-        }
+        // encoded key
+        path = stringify(ps.getPathForKey("../foo"));
+        assertTrue(path, path.endsWith("/%..%2ffoo"));
     }
 
     @Test
@@ -63,20 +103,16 @@ public class TestPathStrategies {
         PathStrategy ps = new PathStrategySubDirs(dir, 2);
 
         // valid key
-        String path = ps.getPathForKey("123456789").toString().replace(File.separatorChar, '/');
+        String path = stringify(ps.getPathForKey("123456789"));
         assertTrue(path, path.endsWith("/12/34/123456789"));
 
         // short key
         path = stringify(ps.getPathForKey("abc"));
         assertTrue(path, path.endsWith("/000/abc"));
 
-        // invalid key
-        try {
-            ps.getPathForKey("bad/key");
-            fail();
-        } catch (NuxeoException e) {
-            assertEquals("Invalid key: bad/key", e.getMessage());
-        }
+        // encoded key
+        path = stringify(ps.getPathForKey("bad/key"));
+        assertTrue(path, path.endsWith("/%b/ad/%bad%2fkey"));
     }
 
 }
