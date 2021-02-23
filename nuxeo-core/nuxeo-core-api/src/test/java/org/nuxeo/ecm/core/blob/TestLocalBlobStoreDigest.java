46243f872cbcb99b1f51b1a73ebfcb9ef6a4fb69
Florent Guillaume <fguillaume@nuxeo.com>
2021-02-23 20:47:35 +0100

    NXP-30193: make sure that internal cache copy/move is atomic

--- nuxeo-core/nuxeo-core-api/src/test/java/org/nuxeo/ecm/core/blob/TestLocalBlobStoreDigest.java
+++ nuxeo-core/nuxeo-core-api/src/test/java/org/nuxeo/ecm/core/blob/TestLocalBlobStoreDigest.java
@@ -18,8 +18,15 @@
  */
 package org.nuxeo.ecm.core.blob;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import org.junit.Test;
 import org.nuxeo.runtime.test.runner.Deploy;
@@ -33,5 +40,64 @@ public class TestLocalBlobStoreDigest extends TestLocalBlobStoreAbstract {
         assertFalse(bp.isRecordMode());
         assertTrue(bs.getKeyStrategy().useDeDuplication());
     }
+
+    @Test
+    public void testConcurrentCopy() throws IOException {
+        BlobProvider bp2 = blobManager.getBlobProvider("other");
+        LocalBlobStore bs2 = (LocalBlobStore) ((BlobStoreBlobProvider) bp2).store;
+        bs2.clear();
+
+        String key1 = bs2.writeBlob(blobContext(ID1, FOO));
+        assertEquals(FOO_MD5, key1);
+
+        List<Exception> exc = new CopyOnWriteArrayList<>();
+        AtomicInteger done = new AtomicInteger();
+
+        // this is the code that we want to be thread-safe
+        Runnable runnable = () -> {
+            try {
+                String key2 = bs.copyOrMoveBlob(ID2, bs2, key1, false);
+                assertEquals(ID2, key2);
+                done.incrementAndGet();
+            } catch (Exception e) {
+                exc.add(e);
+            }
+        };
+
+        int n = 10;
+
+        // check without concurrency first
+
+        for (int i = 0; i < n; i++) {
+            runnable.run();
+        }
+        if (!exc.isEmpty()) {
+            throw new RuntimeException(exc.get(0)); // NOSONAR
+        }
+        assertEquals(n, done.get());
+
+        // now run concurrently
+
+        done.set(0);
+        List<Thread> threads = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            threads.add(new Thread(runnable));
+        }
+        threads.forEach(Thread::start);
+        threads.forEach(t -> {
+            try {
+                t.join(1000);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                throw new RuntimeException(e); // NOSONAR
+            }
+        });
+        if (!exc.isEmpty()) {
+            throw new RuntimeException(exc.get(0)); // NOSONAR
+        }
+        assertEquals(n, done.get());
+    }
+
+
 
 }
