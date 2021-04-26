c81080c387d4dcda90aa4788504672d5569e3f6c
Kevin Leturc <kleturc@nuxeo.com>
2021-04-26 18:12:22 +0200

    NXP-30304: Mask password in logs and apply policy to nuxeoctl

--- modules/runtime/nuxeo-launcher-commons/src/main/java/org/nuxeo/log4j/MaskSensitiveDataRewritePolicy.java
+++ modules/runtime/nuxeo-launcher-commons/src/main/java/org/nuxeo/log4j/MaskSensitiveDataRewritePolicy.java
@@ -17,7 +17,7 @@
  *     Kevin Leturc <kleturc@nuxeo.com>
  */
 
-package org.nuxeo.log4j2;
+package org.nuxeo.log4j;
 
 import java.util.regex.Pattern;
 
@@ -45,6 +45,8 @@ public final class MaskSensitiveDataRewritePolicy implements RewritePolicy {
     protected static final Pattern GCP_KEY_PATTERN = Pattern.compile(
             "(AIza[0-9A-Za-z-_]{3})[0-9A-Za-z-_]{28}([0-9A-Za-z-_]{4})");
 
+    protected static final Pattern PASSWORD_PATTERN = Pattern.compile("(?i)(\\S*password\\S*)=([^\\s,]+)");
+
     @Override
     public LogEvent rewrite(LogEvent source) {
         Message msg = source.getMessage();
@@ -70,6 +72,10 @@ public final class MaskSensitiveDataRewritePolicy implements RewritePolicy {
         var gcpMatcher = GCP_KEY_PATTERN.matcher(msg);
         if (gcpMatcher.find()) {
             msg = gcpMatcher.replaceAll("$1-GCP_KEY-$2");
+        }
+        var passwordMatcher = PASSWORD_PATTERN.matcher(msg);
+        if (passwordMatcher.find()) {
+            msg = passwordMatcher.replaceAll("$1=***");
         }
         return msg;
     }
