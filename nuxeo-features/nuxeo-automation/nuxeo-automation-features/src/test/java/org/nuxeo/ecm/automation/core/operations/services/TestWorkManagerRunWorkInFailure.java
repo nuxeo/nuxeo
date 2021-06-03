046f7aa54fb9fcd15f856125bb0d3ded8fcb1bbe
Benoit Delbosc <bdelbosc@nuxeo.com>
2021-06-03 15:12:50 +0200

    NXP-30450: Add a Work category filetering when reprocessing DLQ

--- nuxeo-features/nuxeo-automation/nuxeo-automation-features/src/test/java/org/nuxeo/ecm/automation/core/operations/services/TestWorkManagerRunWorkInFailure.java
+++ nuxeo-features/nuxeo-automation/nuxeo-automation-features/src/test/java/org/nuxeo/ecm/automation/core/operations/services/TestWorkManagerRunWorkInFailure.java
@@ -35,13 +35,13 @@ import org.junit.runner.RunWith;
 import org.nuxeo.ecm.automation.AutomationService;
 import org.nuxeo.ecm.automation.OperationContext;
 import org.nuxeo.ecm.automation.core.operations.services.workmanager.WorkManagerRunWorkInFailure;
+import org.nuxeo.ecm.automation.core.util.StringList;
 import org.nuxeo.ecm.core.api.CoreSession;
 import org.nuxeo.ecm.core.api.impl.blob.JSONBlob;
 import org.nuxeo.ecm.core.test.CoreFeature;
 import org.nuxeo.ecm.core.test.annotations.Granularity;
 import org.nuxeo.ecm.core.test.annotations.RepositoryConfig;
 import org.nuxeo.ecm.core.work.SleepWork;
-import org.nuxeo.ecm.core.work.api.Work;
 import org.nuxeo.ecm.core.work.api.WorkManager;
 import org.nuxeo.runtime.api.Framework;
 import org.nuxeo.runtime.stream.RuntimeStreamFeature;
@@ -82,28 +82,58 @@ public class TestWorkManagerRunWorkInFailure {
     @Test
     public void testWorkManagerRunWorkInFailureOperation() throws Exception {
         // Submit a work that fails and goes to the dead letter queue
-        Work failWork = new FailingWork(10);
         WorkManager wm = Framework.getService(WorkManager.class);
-        wm.schedule(failWork);
+        wm.schedule(new FailingWork());
+        wm.schedule(new FailingWork());
         assertTrue(wm.awaitCompletion(2000, TimeUnit.MILLISECONDS));
 
-        // Run the operation to replay the work in failure
+        // Dry Run to replay all Works in failure
         Map<String, Serializable> params = new HashMap<>();
+        params.put("dryRun", true);
 
         JSONBlob result = (JSONBlob) service.run(ctx, WorkManagerRunWorkInFailure.ID, params);
         assertNotNull(result);
-        // One work re-processed without success
-        JSONAssert.assertEquals("{\"total\":1,\"success\":0}", result.getString(), true);
+        JSONAssert.assertEquals("{\"total\":2,\"success\":2,\"filtered\":0}", result.getString(), true);
 
-        // Run it again
+        // Do it again, should have the same result
         result = (JSONBlob) service.run(ctx, WorkManagerRunWorkInFailure.ID, params);
         assertNotNull(result);
-        // No work re-processed
-        JSONAssert.assertEquals("{\"total\":0,\"success\":0}", result.getString(), true);
+        JSONAssert.assertEquals("{\"total\":2,\"success\":2,\"filtered\":0}", result.getString(), true);
+
+        // Again, with a filtering that doesn't match
+        StringList categoryFilter = new StringList();
+        categoryFilter.add("ElasticsearchIndexing");
+        params.put("categoryFilter", categoryFilter);
+        result = (JSONBlob) service.run(ctx, WorkManagerRunWorkInFailure.ID, params);
+        assertNotNull(result);
+        JSONAssert.assertEquals("{\"total\":0,\"success\":0,\"filtered\":2}", result.getString(), true);
+
+        // Again, with a filtering that matches
+        categoryFilter.add("SleepWork");
+        params.put("categoryFilter", categoryFilter);
+        result = (JSONBlob) service.run(ctx, WorkManagerRunWorkInFailure.ID, params);
+        assertNotNull(result);
+        JSONAssert.assertEquals("{\"total\":2,\"success\":2,\"filtered\":0}", result.getString(), true);
+
+        // Run it without dry mode, expecting Works to fail again
+        params.remove("dryRun");
+        params.remove("categoryFilter");
+        result = (JSONBlob) service.run(ctx, WorkManagerRunWorkInFailure.ID, params);
+        assertNotNull(result);
+        JSONAssert.assertEquals("{\"total\":2,\"success\":0,\"filtered\":0}", result.getString(), true);
+
+        // Again, this time there is nothing to reprocess
+        result = (JSONBlob) service.run(ctx, WorkManagerRunWorkInFailure.ID, params);
+        assertNotNull(result);
+        JSONAssert.assertEquals("{\"total\":0,\"success\":0,\"filtered\":0}", result.getString(), true);
     }
 
     protected static class FailingWork extends SleepWork {
         private static final long serialVersionUID = 1L;
+
+        public FailingWork() {
+            this(10);
+        }
 
         public FailingWork(long durationMillis) {
             super(durationMillis);
