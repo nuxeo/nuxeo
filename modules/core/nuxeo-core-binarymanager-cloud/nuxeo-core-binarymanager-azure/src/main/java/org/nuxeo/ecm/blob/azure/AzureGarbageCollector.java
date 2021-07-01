f5c88577f745047356826a847a782026ccd339e6
Antoine Taillefer <ataillefer@nuxeo.com>
2021-07-01 11:39:43 +0200

    NXP-30463: Upgrade Microsoft Azure Storage Client SDK

--- modules/core/nuxeo-core-binarymanager-cloud/nuxeo-core-binarymanager-azure/src/main/java/org/nuxeo/ecm/blob/azure/AzureGarbageCollector.java
+++ modules/core/nuxeo-core-binarymanager-cloud/nuxeo-core-binarymanager-azure/src/main/java/org/nuxeo/ecm/blob/azure/AzureGarbageCollector.java
@@ -77,15 +77,8 @@ public class AzureGarbageCollector extends AbstractBinaryGarbageCollector<AzureB
 
                 CloudBlockBlob blob = (CloudBlockBlob) item;
 
-                String digest;
-                try {
-                    String name = blob.getName();
-                    digest = name.substring(binaryManager.prefix.length());
-                } catch (URISyntaxException e) {
-                    // Should never happends
-                    // @see com.microsoft.azure.storage.blob.CloudBlob.getName()
-                    continue;
-                }
+                String name = blob.getName();
+                String digest = name.substring(binaryManager.prefix.length());
 
                 if (!isMD5(digest)) {
                     // ignore files that cannot be MD5 digests for
