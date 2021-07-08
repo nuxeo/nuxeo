e290a401b103f83cf75665528e5409faded83d2a
Antoine Taillefer <ataillefer@nuxeo.com>
2021-07-08 15:56:05 +0200

    NXBT-3523: Rely on base builder up-to-date Skaffold version

--- ci/Jenkinsfiles/build.groovy
+++ ci/Jenkinsfiles/build.groovy
@@ -303,7 +303,6 @@ pipeline {
     // force ${HOME}=/root - for an unexplained reason, ${HOME} is resolved as /home/jenkins though sh 'env' shows HOME=/root
     HOME = '/root'
     HELMFILE_COMMAND = "helmfile --file ci/helm/helmfile.yaml --helm-binary /usr/bin/helm3"
-    SKAFFOLD_VERSION = 'v1.26.1'
     CURRENT_NAMESPACE = getCurrentNamespace()
     TEST_NAMESPACE_PREFIX = "$CURRENT_NAMESPACE-nuxeo-unit-tests-$BRANCH_NAME-$BUILD_NUMBER".toLowerCase()
     TEST_SERVICE_DOMAIN_SUFFIX = 'svc.cluster.local'
@@ -589,14 +588,6 @@ pipeline {
           Build Docker image
           ----------------------------------------
           Image tag: ${VERSION}
-          """
-
-          echo "Install recent version of skaffold: ${SKAFFOLD_VERSION}"
-          sh """
-            skaffold version
-            curl -Lo skaffold https://github.com/GoogleContainerTools/skaffold/releases/download/${SKAFFOLD_VERSION}/skaffold-linux-amd64 && \
-            install skaffold /usr/bin/
-            skaffold version
           """
 
           dir('docker/nuxeo-base') {
