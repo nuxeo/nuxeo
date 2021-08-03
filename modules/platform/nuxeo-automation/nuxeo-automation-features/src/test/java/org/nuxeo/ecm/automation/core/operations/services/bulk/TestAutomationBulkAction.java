10bd9d6c24951a23caa3ff4cb28696fe9d19f169
Kevin Leturc <kleturc@nuxeo.com>
2021-08-03 11:52:18 +0200

    NXP-30518: Continue on errors in automationUi action

--- modules/platform/nuxeo-automation/nuxeo-automation-features/src/test/java/org/nuxeo/ecm/automation/core/operations/services/bulk/TestAutomationBulkAction.java
+++ modules/platform/nuxeo-automation/nuxeo-automation-features/src/test/java/org/nuxeo/ecm/automation/core/operations/services/bulk/TestAutomationBulkAction.java
@@ -36,15 +36,19 @@ import org.nuxeo.ecm.automation.AutomationService;
 import org.nuxeo.ecm.automation.OperationContext;
 import org.nuxeo.ecm.core.api.CoreSession;
 import org.nuxeo.ecm.core.api.DocumentModel;
+import org.nuxeo.ecm.core.bulk.BulkService;
 import org.nuxeo.ecm.core.bulk.CoreBulkFeature;
 import org.nuxeo.ecm.core.bulk.message.BulkStatus;
+import org.nuxeo.ecm.core.bulk.message.BulkStatus.State;
 import org.nuxeo.ecm.core.test.CoreFeature;
 import org.nuxeo.ecm.core.test.DocumentSetRepositoryInit;
 import org.nuxeo.ecm.core.test.annotations.Granularity;
 import org.nuxeo.ecm.core.test.annotations.RepositoryConfig;
+import org.nuxeo.runtime.test.runner.ConsoleLogLevelThreshold;
 import org.nuxeo.runtime.test.runner.Deploy;
 import org.nuxeo.runtime.test.runner.Features;
 import org.nuxeo.runtime.test.runner.FeaturesRunner;
+import org.nuxeo.runtime.test.runner.LogFeature;
 import org.nuxeo.runtime.test.runner.TransactionalFeature;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -52,7 +56,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 /**
  * @since 10.3
  */
-@Features({ CoreFeature.class, CoreBulkFeature.class })
+@Features({ CoreFeature.class, CoreBulkFeature.class, LogFeature.class })
 @RunWith(FeaturesRunner.class)
 @Deploy("org.nuxeo.ecm.automation.core")
 @Deploy("org.nuxeo.ecm.automation.features")
@@ -65,6 +69,9 @@ public class TestAutomationBulkAction {
 
     @Inject
     protected CoreSession session;
+
+    @Inject
+    protected BulkService bulkService;
 
     @Inject
     protected AutomationService service;
@@ -136,6 +143,43 @@ public class TestAutomationBulkAction {
         assertTrue("Bulk action didn't finish", waitResult);
 
         txFeature.nextTransaction();
+    }
+
+    @Test
+    @Deploy("org.nuxeo.ecm.automation.features:test-failing-operation-contrib.xml")
+    @ConsoleLogLevelThreshold("ERROR") // hide automation trace logs which are verbose
+    public void testFailingAutomationErrorHandling() throws Exception {
+        // param for the automation bulk action
+        var actionParams = new HashMap<>();
+        actionParams.put(AutomationBulkAction.OPERATION_ID, "TestFailingOperation");
+        actionParams.put(AutomationBulkAction.OPERATION_PARAMETERS, Map.of());
+
+        // param for the automation BulkRunAction operation
+        var bulkActionParam = new HashMap<String, Serializable>();
+        bulkActionParam.put("action", AutomationBulkActionUi.ACTION_NAME);
+        bulkActionParam.put("query", "SELECT * FROM ComplexDoc");
+        bulkActionParam.put("bucketSize", "10");
+        bulkActionParam.put("batchSize", "5");
+        bulkActionParam.put("parameters", OBJECT_MAPPER.writeValueAsString(actionParams));
+        var runResult = (BulkStatus) service.run(ctx, BulkRunAction.ID, bulkActionParam);
+
+        assertNotNull(runResult);
+        // runResult is a json containing commandId
+        String commandId = runResult.getId();
+
+        var waitResult = (boolean) service.run(ctx, BulkWaitForAction.ID, Map.of("commandId", commandId));
+        assertTrue("Bulk action didn't finish", waitResult);
+
+        txFeature.nextTransaction();
+
+        var nbDocuments = session.query("SELECT * FROM ComplexDoc", null, 0, 0, true).totalSize();
+
+        var status = bulkService.getStatus(commandId);
+        assertEquals(State.COMPLETED, status.getState());
+        assertEquals(nbDocuments, status.getErrorCount());
+        assertTrue(status.getErrorMessage()
+                         .startsWith(String.format("Bulk Action Operation with commandId: %s fails on documents: ",
+                                 commandId)));
     }
 
 }
