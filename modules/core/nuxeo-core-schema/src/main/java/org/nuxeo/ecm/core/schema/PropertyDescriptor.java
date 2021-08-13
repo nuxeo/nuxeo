c40ee39fd803293f79d978fd476fc8a592bb07cd
Thomas Roger <troger@nuxeo.com>
2021-08-13 17:17:06 +0200

    Revert "NXP-29261: cleanup / format"
    This reverts commit 637901837defb2d9f4652a2b04b6f2c5fc8ceef8.

--- modules/core/nuxeo-core-schema/src/main/java/org/nuxeo/ecm/core/schema/PropertyDescriptor.java
+++ modules/core/nuxeo-core-schema/src/main/java/org/nuxeo/ecm/core/schema/PropertyDescriptor.java
@@ -1,5 +1,5 @@
 /*
- * (C) Copyright 2019-2021 Nuxeo (http://nuxeo.com/) and others.
+ * (C) Copyright 2019 Nuxeo (http://nuxeo.com/) and others.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -94,6 +94,7 @@ public class PropertyDescriptor {
         return fallback;
     }
 
+    @Override
     public String toString() {
         return ToStringBuilder.reflectionToString(this);
     }
