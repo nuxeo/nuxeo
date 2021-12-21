eb9214cfcf61b10625efd082c7ca0589fa7ba568
Guillaume Renard <grenard@nuxeo.com>
2021-12-21 12:15:57 +0100

    NXP-30757: NXP-30426: use bulk action to update read acls

---
+++ nuxeo-core/nuxeo-core-storage-dbs/src/main/java/org/nuxeo/ecm/core/storage/dbs/action/UpdateReadAclsAction.java
@@ -0,0 +1,67 @@
+/*
+ * (C) Copyright 2021 Nuxeo (http://nuxeo.com/) and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Contributors:
+ *     Guillaume RENARD
+ */
+package org.nuxeo.ecm.core.storage.dbs.action;
+
+import static org.nuxeo.ecm.core.bulk.BulkServiceImpl.STATUS_STREAM;
+import static org.nuxeo.lib.stream.computation.AbstractComputation.INPUT_1;
+import static org.nuxeo.lib.stream.computation.AbstractComputation.OUTPUT_1;
+
+import java.io.Serializable;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+import org.nuxeo.ecm.core.api.CoreSession;
+import org.nuxeo.ecm.core.bulk.action.computation.AbstractBulkComputation;
+import org.nuxeo.lib.stream.computation.Topology;
+import org.nuxeo.runtime.stream.StreamProcessorTopology;
+
+/**
+ * Action to update the Read ACLs on a list of documents, without recursion.
+ *
+ * @since 2021.11
+ */
+public class UpdateReadAclsAction implements StreamProcessorTopology {
+
+    public static final String UPDATE_READ_ACLS_ACTION = "updateReadAcls";
+
+    public static final String ACTION_FULL_NAME = UPDATE_READ_ACLS_ACTION;
+
+    @Override
+    public Topology getTopology(Map<String, String> options) {
+        return Topology.builder()
+                       .addComputation(UpdateReadAclsComputation::new,
+                               Arrays.asList(INPUT_1 + ":" + ACTION_FULL_NAME, OUTPUT_1 + ":" + STATUS_STREAM))
+                       .build();
+    }
+
+    public static class UpdateReadAclsComputation extends AbstractBulkComputation {
+
+        public UpdateReadAclsComputation() {
+            super(ACTION_FULL_NAME);
+        }
+
+        @Override
+        protected void compute(CoreSession session, List<String> ids, Map<String, Serializable> properties) {
+            session.updateReadACLs(ids);
+        }
+
+    }
+
+}
