11020bcee0729b0bb0fbb9ebdb22fecd01dfd8e8
Benoit Delbosc <bdelbosc@nuxeo.com>
2022-02-26 12:33:38 +0100

    NXP-30919 NXP-30851: Use log4j2 in KeyValueBlobTransientStore

--- nuxeo-core/nuxeo-core-cache/src/main/java/org/nuxeo/ecm/core/transientstore/keyvalueblob/KeyValueBlobTransientStore.java
+++ nuxeo-core/nuxeo-core-cache/src/main/java/org/nuxeo/ecm/core/transientstore/keyvalueblob/KeyValueBlobTransientStore.java
@@ -37,15 +37,14 @@ import java.util.function.Function;
 import java.util.stream.Stream;
 
 import org.apache.commons.lang3.SerializationUtils;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.nuxeo.ecm.core.api.Blob;
 import org.nuxeo.ecm.core.api.NuxeoException;
 import org.nuxeo.ecm.core.blob.BlobInfo;
 import org.nuxeo.ecm.core.blob.BlobManager;
 import org.nuxeo.ecm.core.blob.BlobManagerComponent;
 import org.nuxeo.ecm.core.blob.BlobProvider;
-import org.nuxeo.ecm.core.blob.ManagedBlob;
 import org.nuxeo.ecm.core.blob.binary.BinaryGarbageCollector;
 import org.nuxeo.ecm.core.transientstore.api.MaximumTransientSpaceExceeded;
 import org.nuxeo.ecm.core.transientstore.api.TransientStoreConfig;
@@ -103,7 +102,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
  */
 public class KeyValueBlobTransientStore implements TransientStoreProvider {
 
-    private static final Log log = LogFactory.getLog(KeyValueBlobTransientStore.class);
+    private static final Logger log = LogManager.getLogger(KeyValueBlobTransientStore.class);
 
     public static final String SEP = ".";
 
@@ -161,6 +160,8 @@ public class KeyValueBlobTransientStore implements TransientStoreProvider {
     /** @since 11.1 */
     protected static final long LOCK_EXPONENTIAL_BACKOFF_AFTER_NANOS = TimeUnit.MILLISECONDS.toNanos(100);
 
+    protected String name;
+
     protected String keyValueStoreName;
 
     protected String blobProviderId;
@@ -183,7 +184,8 @@ public class KeyValueBlobTransientStore implements TransientStoreProvider {
 
     @Override
     public void init(TransientStoreConfig config) {
-        String defaultName = config.getName();
+        name = config.getName();
+        String defaultName = name;
         if (!defaultName.startsWith(BlobManagerComponent.TRANSIENT_ID_PREFIX)) {
             defaultName = BlobManagerComponent.TRANSIENT_ID_PREFIX + "_" + defaultName;
         }
@@ -315,7 +317,7 @@ public class KeyValueBlobTransientStore implements TransientStoreProvider {
         try {
             return mapper.readValue(json, LIST_STRING);
         } catch (IOException e) {
-            log.error("Invalid JSON array: " + json);
+            log.error("Invalid JSON array: {}", json);
             return null;
         }
     }
@@ -327,7 +329,7 @@ public class KeyValueBlobTransientStore implements TransientStoreProvider {
         try {
             return mapper.readValue(json, MAP_STRING_STRING);
         } catch (IOException e) {
-            log.error("Invalid JSON object: " + json);
+            log.error("Invalid JSON object: {}", json);
             return null;
         }
     }
@@ -626,7 +628,8 @@ public class KeyValueBlobTransientStore implements TransientStoreProvider {
                 // ignore, the blob was removed from the blob provider
                 // maybe by a concurrent GC from this transient store
                 // or from the blob provider itself (if it's incorrectly shared)
-                log.debug("Failed to read blob: " + digest);
+                log.debug("Failed to read blob: {} in blob provider: {}  for transient store: {}", digest,
+                        blobProviderId, name);
             }
         }
         return blobs;
