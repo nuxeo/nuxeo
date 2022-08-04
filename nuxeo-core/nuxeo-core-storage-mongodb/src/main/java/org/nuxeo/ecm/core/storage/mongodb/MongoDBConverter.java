a1729ffd9bc48233db3aaaaade4040347db9d479
Kevin Leturc <kevin.leturc@hyland.com>
2022-08-04 10:22:06 +0200

    NXP-31148: Fix corrupted number handling

--- nuxeo-core/nuxeo-core-storage-mongodb/src/main/java/org/nuxeo/ecm/core/storage/mongodb/MongoDBConverter.java
+++ nuxeo-core/nuxeo-core-storage-mongodb/src/main/java/org/nuxeo/ecm/core/storage/mongodb/MongoDBConverter.java
@@ -258,6 +258,10 @@ public class MongoDBConverter {
         if (valueIsId(key)) {
             return bsonToId(val);
         }
+        // NXP-31148: numbers is sometime returned as Integer whereas we only deal Long
+        if (val instanceof Integer) {
+            return ((Integer) val).longValue();
+        }
         return (Serializable) val;
     }
 
@@ -267,6 +271,10 @@ public class MongoDBConverter {
         }
         if (valueIsId(key)) {
             return String.class;
+        }
+        // NXP-31148: numbers is sometime returned as Integer whereas we only deal Long
+        if (Integer.class.isAssignableFrom(klass)) {
+            return Long.class;
         }
         return klass;
     }
