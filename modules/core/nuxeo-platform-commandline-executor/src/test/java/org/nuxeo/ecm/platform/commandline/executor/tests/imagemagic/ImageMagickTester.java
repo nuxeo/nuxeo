f3034722c614660ce91585b521e468b05bd3ae63
Antoine Taillefer <ataillefer@nuxeo.com>
2022-12-14 11:54:28 +0100

    NXP-31458: Test cleanup

--- modules/core/nuxeo-platform-commandline-executor/src/test/java/org/nuxeo/ecm/platform/commandline/executor/tests/imagemagic/ImageMagickTester.java
+++ modules/core/nuxeo-platform-commandline-executor/src/test/java/org/nuxeo/ecm/platform/commandline/executor/tests/imagemagic/ImageMagickTester.java
@@ -1,5 +1,5 @@
 /*
- * (C) Copyright 2006-2015 Nuxeo SA (http://nuxeo.com/) and others.
+ * (C) Copyright 2006-2022 Nuxeo SA (http://nuxeo.com/) and others.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,28 +15,25 @@
  *
  * Contributors:
  *     Nuxeo - initial API and implementation
- *
  */
-
 package org.nuxeo.ecm.platform.commandline.executor.tests.imagemagic;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertSame;
 import static org.junit.Assert.assertTrue;
 
 import java.io.File;
 import java.util.List;
 
-import org.junit.Assume;
+import javax.inject.Inject;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.nuxeo.common.utils.FileUtils;
 import org.nuxeo.ecm.platform.commandline.executor.api.CmdParameters;
-import org.nuxeo.ecm.platform.commandline.executor.api.CommandAvailability;
 import org.nuxeo.ecm.platform.commandline.executor.api.CommandLineExecutorService;
+import org.nuxeo.ecm.platform.commandline.executor.api.CommandNotAvailable;
 import org.nuxeo.ecm.platform.commandline.executor.api.ExecResult;
-import org.nuxeo.runtime.api.Framework;
 import org.nuxeo.runtime.test.runner.Deploy;
 import org.nuxeo.runtime.test.runner.Features;
 import org.nuxeo.runtime.test.runner.FeaturesRunner;
@@ -51,14 +48,11 @@ import org.nuxeo.runtime.test.runner.RuntimeFeature;
 @Deploy("org.nuxeo.ecm.platform.commandline.executor:OSGI-INF/commandline-imagemagic-test-contrib.xml")
 public class ImageMagickTester {
 
+    @Inject
+    protected CommandLineExecutorService cles;
+
     @Test
-    public void testIdentifyExec() throws Exception {
-        CommandLineExecutorService cles = Framework.getService(CommandLineExecutorService.class);
-        assertNotNull(cles);
-
-        CommandAvailability ca = cles.getCommandAvailability("identify");
-        Assume.assumeTrue("ImageMagick is not available, skipping test", ca.isAvailable());
-
+    public void testIdentifyExec() throws CommandNotAvailable {
         File img = FileUtils.getResourceFileFromContext("test.png");
 
         CmdParameters params = cles.getDefaultCmdParameters();
