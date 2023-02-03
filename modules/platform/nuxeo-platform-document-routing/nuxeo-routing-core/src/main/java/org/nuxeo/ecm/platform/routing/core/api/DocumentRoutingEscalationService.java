e72e50687f9e1d90edf2de0906fe3c68ede48da5
Kevin Leturc <kevin.leturc@hyland.com>
2023-02-03 09:54:38 +0100

    NXP-31616: Make workflow escalation rules execution scalable

--- modules/platform/nuxeo-platform-document-routing/nuxeo-routing-core/src/main/java/org/nuxeo/ecm/platform/routing/core/api/DocumentRoutingEscalationService.java
+++ modules/platform/nuxeo-platform-document-routing/nuxeo-routing-core/src/main/java/org/nuxeo/ecm/platform/routing/core/api/DocumentRoutingEscalationService.java
@@ -29,25 +29,89 @@ import org.nuxeo.ecm.platform.routing.core.impl.GraphNode.EscalationRule;
  */
 public interface DocumentRoutingEscalationService {
 
+    /** @since 2023.0 */
+    String SUSPENDED_NODES_WITH_ESCALATION_QUERY = "SELECT DISTINCT ecm:uuid FROM RouteNode WHERE " //
+            + "ecm:currentLifeCycleState = 'suspended' "
+            + "AND ( rnode:escalationRules/*1/executed = 0 OR rnode:escalationRules/*1/multipleExecution = 1 )";
+
     /**
      * Query all running workflows and returns a list of nodes that are suspended and have escalation rules that can be
      * inspected. Uses an unrestricted session.
      *
      * @since 5.7.2
+     * @deprecated since 2023.0, this method doesn't scale, use the {@link CoreSession} query APIs with
+     *             {@link #SUSPENDED_NODES_WITH_ESCALATION_QUERY} instead
      */
+    @Deprecated
     List<String> queryForSuspendedNodesWithEscalation(CoreSession session);
+
+    /**
+     * Returns whether a workflow escalation execution is running on the given {@code repositoryName}.
+     *
+     * @since 2023.0
+     */
+    default boolean isExecutionRunning(String repositoryName) {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    /**
+     * Sets that a workflow escalation execution on given {@code repositoryName} is currently running.
+     *
+     * @since 2023.0
+     */
+    default void setExecutionRunning(String repositoryName) {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
 
     /**
      * Computes the list of escalation rules to be executed after their conditions are evaluated for the given node.
      *
+     * @param node the {@link GraphNode} to retrieve and evaluate from the escalation rules
      * @since 5.7.2
      */
-    List<EscalationRule> computeEscalationRulesToExecute(GraphNode node);
+    default List<EscalationRule> computeEscalationRulesToExecute(GraphNode node) {
+        return computeEscalationRulesToExecute(node, true);
+    }
+
+    /**
+     * Computes the list of escalation rules to be executed after their conditions are evaluated for the given node.
+     *
+     * @param node the {@link GraphNode} to retrieve and evaluate from the escalation rules
+     * @param handleTransaction whether the escalation rule evaluation should handle the transaction and save
+     * @since 2023.0
+     */
+    default List<EscalationRule> computeEscalationRulesToExecute(GraphNode node, boolean handleTransaction) {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    /**
+     * Executes the given escalation rule.
+     *
+     * @param rule the rule to execute
+     * @since 2023.0
+     */
+    default void executeEscalationRule(EscalationRule rule) {
+        executeEscalationRule(rule, true);
+    }
+
+    /**
+     * Executes the given escalation rule.
+     *
+     * @param rule the rule to execute
+     * @param handleTransaction whether the escalation rule execution should handle the transaction and save
+     * @since 2023.0
+     */
+    default void executeEscalationRule(EscalationRule rule, boolean handleTransaction) {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
 
     /**
      * Schedules for execution an escalation rule. Uses an unrestricted session.
      *
      * @since 5.7.2
+     * @deprecated since 2023.0, it was replaced by
+     *             {@link org.nuxeo.ecm.platform.routing.core.bulk.DocumentRoutingEscalationAction}
      */
+    @Deprecated
     void scheduleExecution(EscalationRule rule, CoreSession session);
 }
--- modules/platform/nuxeo-platform-document-routing/nuxeo-routing-core/src/main/java/org/nuxeo/ecm/platform/routing/core/impl/DocumentRoutingEscalationServiceImpl.java
+++ modules/platform/nuxeo-platform-document-routing/nuxeo-routing-core/src/main/java/org/nuxeo/ecm/platform/routing/core/impl/DocumentRoutingEscalationServiceImpl.java
@@ -19,13 +19,17 @@
 package org.nuxeo.ecm.platform.routing.core.impl;
 
 import java.io.Serializable;
+import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.nuxeo.ecm.automation.AutomationService;
 import org.nuxeo.ecm.automation.OperationContext;
+import org.nuxeo.ecm.automation.OperationException;
+import org.nuxeo.ecm.automation.core.scripting.Expression;
 import org.nuxeo.ecm.core.api.CoreSession;
 import org.nuxeo.ecm.core.api.DocumentModel;
 import org.nuxeo.ecm.core.api.IdRef;
@@ -34,9 +38,15 @@ import org.nuxeo.ecm.core.api.NuxeoException;
 import org.nuxeo.ecm.core.api.UnrestrictedSessionRunner;
 import org.nuxeo.ecm.core.work.AbstractWork;
 import org.nuxeo.ecm.core.work.api.WorkManager;
+import org.nuxeo.ecm.platform.routing.api.exception.DocumentRouteException;
 import org.nuxeo.ecm.platform.routing.core.api.DocumentRoutingEscalationService;
+import org.nuxeo.ecm.platform.routing.core.api.scripting.RoutingScriptingExpression;
+import org.nuxeo.ecm.platform.routing.core.api.scripting.RoutingScriptingFunctions;
 import org.nuxeo.ecm.platform.routing.core.impl.GraphNode.EscalationRule;
 import org.nuxeo.runtime.api.Framework;
+import org.nuxeo.runtime.kv.KeyValueService;
+import org.nuxeo.runtime.kv.KeyValueStore;
+import org.nuxeo.runtime.services.config.ConfigurationService;
 
 /**
  * @since 5.7.2
@@ -45,16 +55,27 @@ public class DocumentRoutingEscalationServiceImpl implements DocumentRoutingEsca
 
     private static final Log log = LogFactory.getLog(DocumentRoutingEscalationServiceImpl.class);
 
-    public static final String queryForSuspendedNodesWithEscalation = "Select DISTINCT ecm:uuid from RouteNode WHERE ecm:currentLifeCycleState = 'suspended' "
-            + "AND ( rnode:escalationRules/*1/executed = 0 OR rnode:escalationRules/*1/multipleExecution = 1 )";
+    /**
+     * @deprecated since 2023.0, use {@link DocumentRoutingEscalationService#SUSPENDED_NODES_WITH_ESCALATION_QUERY}
+     *             instead
+     */
+    @Deprecated
+    public static final String queryForSuspendedNodesWithEscalation = SUSPENDED_NODES_WITH_ESCALATION_QUERY;
+
+    /** @since 2023.0 */
+    protected static final String WORKFLOW_ESCALATION_KV_STORE_NAME = "workflowEscalationRunning";
+
+    /** @since 2023.0 */
+    protected static final String ESCALATION_RUNNING_TTL_KEY = "nuxeo.document.routing.escalation.running.flag.ttl.duration";
 
     @Override
+    @Deprecated
     public List<String> queryForSuspendedNodesWithEscalation(CoreSession session) {
         final List<String> nodesDocIds = new ArrayList<>();
         new UnrestrictedSessionRunner(session) {
             @Override
             public void run() {
-                IterableQueryResult results = session.queryAndFetch(queryForSuspendedNodesWithEscalation, "NXQL");
+                IterableQueryResult results = session.queryAndFetch(SUSPENDED_NODES_WITH_ESCALATION_QUERY, "NXQL");
                 for (Map<String, Serializable> result : results) {
                     nodesDocIds.add(result.get("ecm:uuid").toString());
                     log.trace("Inspecting node for escalation rules:" + result.get("ecm:uuid").toString());
@@ -66,11 +87,86 @@ public class DocumentRoutingEscalationServiceImpl implements DocumentRoutingEsca
     }
 
     @Override
-    public List<EscalationRule> computeEscalationRulesToExecute(GraphNode node) {
-        return node.evaluateEscalationRules();
+    public boolean isExecutionRunning(String repositoryName) {
+        var kvStore = getKvStore();
+        return Boolean.TRUE.toString().equals(kvStore.getString(repositoryName));
     }
 
     @Override
+    public void setExecutionRunning(String repositoryName) {
+        var kvStore = getKvStore();
+        var configurationService = Framework.getService(ConfigurationService.class);
+        kvStore.put(repositoryName, Boolean.TRUE.toString(),
+                configurationService.getDuration(ESCALATION_RUNNING_TTL_KEY, Duration.ofMinutes(3)).toSeconds());
+    }
+
+    protected static KeyValueStore getKvStore() {
+        return Framework.getService(KeyValueService.class).getKeyValueStore(WORKFLOW_ESCALATION_KV_STORE_NAME);
+    }
+
+    @Override
+    public List<EscalationRule> computeEscalationRulesToExecute(GraphNode node, boolean handleTransaction) {
+        CoreSession session = node.getDocument().getCoreSession();
+        List<EscalationRule> rulesToExecute = new ArrayList<>();
+        for (EscalationRule rule : node.getEscalationRules()) {
+            try (OperationContext context = getExecutionContext(session, node, handleTransaction)) {
+                Expression expr = new RoutingScriptingExpression(rule.getCondition(),
+                        new RoutingScriptingFunctions(context, rule));
+                Object res = expr.eval(context);
+                if (!(res instanceof Boolean)) {
+                    throw new DocumentRouteException(String.format(
+                            "Condition for rule: %s of node: %s of graph: %s does not evaluate to a boolean: %s", rule,
+                            node.getId(), node.getGraph().getName(), rule.getCondition()));
+                }
+                boolean bool = Boolean.TRUE.equals(res);
+                if ((!rule.isExecuted() || rule.isMultipleExecution()) && bool) {
+                    rulesToExecute.add(rule);
+                }
+            } catch (DocumentRouteException e) {
+                throw e;
+            } catch (RuntimeException e) {
+                throw new DocumentRouteException("Error evaluating condition: " + rule.getCondition(), e);
+            }
+        }
+        if (handleTransaction) {
+            session.saveDocument(node.getDocument());
+        }
+        return rulesToExecute;
+    }
+
+    @Override
+    public void executeEscalationRule(EscalationRule rule, boolean handleTransaction) {
+        GraphNode node = rule.getNode();
+        DocumentModel nodeDoc = node.getDocument();
+        CoreSession session = nodeDoc.getCoreSession();
+        // don't call GraphNode#executeChain in order to control the transaction
+        try (OperationContext context = getExecutionContext(session, node, handleTransaction)) {
+            AutomationService automationService = Framework.getService(AutomationService.class);
+            automationService.run(context, rule.getChain());
+
+            node.setAllVariables(context, true, handleTransaction);
+            rule.setExecuted(true);
+            if (handleTransaction) {
+                session.saveDocument(nodeDoc);
+            }
+        } catch (OperationException e) {
+            throw new DocumentRouteException(
+                    String.format("Error when running chain: %s from escalation rule: %s of node: %s", rule.getChain(),
+                            rule, node.getId()),
+                    e);
+        }
+    }
+
+    protected OperationContext getExecutionContext(CoreSession session, GraphNode node, boolean handleTransaction) {
+        var context = new OperationContext(session);
+        context.putAll(node.getWorkflowContextualInfo(session, true));
+        context.handleTransaction(handleTransaction);
+        context.setInput(context.get("documents"));
+        return context;
+    }
+
+    @Override
+    @Deprecated
     public void scheduleExecution(EscalationRule rule, CoreSession session) {
         WorkManager manager = Framework.getService(WorkManager.class);
         manager.schedule(
@@ -78,6 +174,11 @@ public class DocumentRoutingEscalationServiceImpl implements DocumentRoutingEsca
                 WorkManager.Scheduling.IF_NOT_SCHEDULED);
     }
 
+    /**
+     * @deprecated since 2023.0, it was replaced by
+     *             {@link org.nuxeo.ecm.platform.routing.core.bulk.DocumentRoutingEscalationAction}
+     */
+    @Deprecated
     public static class EscalationRuleWork extends AbstractWork {
 
         private static final long serialVersionUID = 1L;
@@ -137,22 +238,15 @@ public class DocumentRoutingEscalationServiceImpl implements DocumentRoutingEsca
                 throw new NuxeoException("Can't execute worker '" + getId() + "' : the rule '" + escalationRuleId
                         + "' was not found on the node '" + nodeDocId + "'");
             }
-            try (OperationContext context = new OperationContext(session)) {
-                context.putAll(node.getWorkflowContextualInfo(session, true));
-                context.setInput(context.get("documents"));
-                // check to see if the rule wasn't executed meanwhile
-                boolean alreadyExecuted = getExecutionStatus(rule, session);
-                if (alreadyExecuted && !rule.isMultipleExecution()) {
-                    log.trace("Rule " + rule.getId() + "on node " + node.getId() + " already executed");
-                    return;
-                }
-                node.executeChain(rule.getChain());
-                // mark the rule as resolved
-                markRuleAsExecuted(nodeDocId, escalationRuleId, session);
-            } catch (NuxeoException e) {
-                e.addInfo("Error when executing worker: " + getTitle());
-                throw e;
+            // check to see if the rule wasn't executed meanwhile
+            boolean alreadyExecuted = getExecutionStatus(rule, session);
+            if (alreadyExecuted && !rule.isMultipleExecution()) {
+                log.trace("Rule " + rule.getId() + "on node " + node.getId() + " already executed");
+                return;
             }
+            Framework.getService(DocumentRoutingEscalationService.class).executeEscalationRule(rule);
+            // mark the rule as resolved
+            markRuleAsExecuted(nodeDocId, escalationRuleId, session);
         }
 
         /**
@@ -171,9 +265,11 @@ public class DocumentRoutingEscalationServiceImpl implements DocumentRoutingEsca
         }
 
     }
-
-    private static void markRuleAsExecuted(String nodeDocId, String escalationRuleId, CoreSession session)
-            {
+    /**
+     * @deprecated since 2023.0, only used by {@link EscalationRuleWork}
+     */
+    @Deprecated
+    private static void markRuleAsExecuted(String nodeDocId, String escalationRuleId, CoreSession session) {
         DocumentModel nodeDoc = session.getDocument(new IdRef(nodeDocId));
         GraphNode node = nodeDoc.getAdapter(GraphNode.class);
         List<EscalationRule> rules = node.getEscalationRules();
