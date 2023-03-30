14ab26119cdea7841c56c9f579b1109e67452bea
Benoit Delbosc <bdelbosc@nuxeo.com>
2023-03-30 10:25:43 +0200

    NXP-31809: Add elastic 7 compat headers

--- modules/platform/nuxeo-elasticsearch/nuxeo-elasticsearch-core/src/main/java/org/nuxeo/elasticsearch/client/ESRestClient.java
+++ modules/platform/nuxeo-elasticsearch/nuxeo-elasticsearch-core/src/main/java/org/nuxeo/elasticsearch/client/ESRestClient.java
@@ -88,6 +88,11 @@ public class ESRestClient implements ESClient {
 
     protected RestHighLevelClient client;
 
+    protected RequestOptions COMPAT_ES_OPTIONS = RequestOptions.DEFAULT.toBuilder()
+            .addHeader("Accept", "application/json; compatible-with=7; charset=UTF-8")
+            .addHeader("Content-Type", "application/json; compatible-with=7; charset=UTF-8")
+            .build();
+
     public ESRestClient(RestClient lowLevelRestClient, RestHighLevelClient client) {
         this.lowLevelClient = lowLevelRestClient;
         this.client = client;
@@ -237,6 +242,7 @@ public class ESRestClient implements ESClient {
 
     protected Response performRequest(Request request) {
         try {
+            request.setOptions(COMPAT_ES_OPTIONS);
             return lowLevelClient.performRequest(request);
         } catch (IOException e) {
             throw new NuxeoException(e);
@@ -357,7 +363,7 @@ public class ESRestClient implements ESClient {
                 // use a longer timeout than the default one
                 request.timeout(LONG_TIMEOUT);
             }
-            BulkResponse response = client.bulk(request, RequestOptions.DEFAULT);
+            BulkResponse response = client.bulk(request, COMPAT_ES_OPTIONS);
             if (response.hasFailures()) {
                 for (BulkItemResponse item : response.getItems()) {
                     if (item.isFailed() && RestStatus.TOO_MANY_REQUESTS == item.getFailure().getStatus()) {
@@ -395,7 +401,7 @@ public class ESRestClient implements ESClient {
                 // use a longer timeout than the default one
                 request.timeout(LONG_TIMEOUT);
             }
-            return client.delete(request, RequestOptions.DEFAULT);
+            return client.delete(request, COMPAT_ES_OPTIONS);
         } catch (IOException e) {
             throw new NuxeoException(e);
         }
@@ -404,7 +410,7 @@ public class ESRestClient implements ESClient {
     @Override
     public SearchResponse search(SearchRequest request) {
         try (Scope ignored = getScopedSpan("elastic/_search", request.toString())) {
-            return client.search(request, RequestOptions.DEFAULT);
+            return client.search(request, COMPAT_ES_OPTIONS);
         } catch (IOException | OpenSearchStatusException e) {
             // OpenSearchStatusException is raised when using phrase prefix on keyword type
             throw new NuxeoException(e);
@@ -414,7 +420,7 @@ public class ESRestClient implements ESClient {
     @Override
     public SearchResponse searchScroll(SearchScrollRequest request) {
         try (Scope ignored = getScopedSpan("elastic/_scroll", request.toString())) {
-            return client.scroll(request, RequestOptions.DEFAULT);
+            return client.scroll(request, COMPAT_ES_OPTIONS);
         } catch (IOException e) {
             throw new NuxeoException(e);
         }
@@ -423,7 +429,7 @@ public class ESRestClient implements ESClient {
     @Override
     public GetResponse get(GetRequest request) {
         try (Scope ignored = getScopedSpan("elastic/_get", request.toString())) {
-            return client.get(request, RequestOptions.DEFAULT);
+            return client.get(request, COMPAT_ES_OPTIONS);
         } catch (IOException e) {
             throw new NuxeoException(e);
         }
@@ -454,7 +460,7 @@ public class ESRestClient implements ESClient {
                 // use a longer timeout than the default one
                 request.timeout(LONG_TIMEOUT);
             }
-            return client.index(request, RequestOptions.DEFAULT);
+            return client.index(request, COMPAT_ES_OPTIONS);
         } catch (OpenSearchStatusException e) {
             if (RestStatus.CONFLICT.equals(e.status())) {
                 throw new ConcurrentUpdateException(e);
@@ -484,7 +490,7 @@ public class ESRestClient implements ESClient {
     public ClearScrollResponse clearScroll(ClearScrollRequest request) {
         try {
             log.debug("Clearing scroll ids: {}", () -> Arrays.toString(request.getScrollIds().toArray()));
-            return client.clearScroll(request, RequestOptions.DEFAULT);
+            return client.clearScroll(request, COMPAT_ES_OPTIONS);
         } catch (OpenSearchStatusException e) {
             if (RestStatus.NOT_FOUND.equals(e.status())) {
                 log.debug("Scroll ids not found, they have certainly been already closed: {}",
@@ -499,7 +505,7 @@ public class ESRestClient implements ESClient {
 
     @Override
     public BulkProcessor.Builder bulkProcessorBuilder(BulkProcessor.Listener bulkListener) {
-        return BulkProcessor.builder((request, listener) -> client.bulkAsync(request, RequestOptions.DEFAULT, listener),
+        return BulkProcessor.builder((request, listener) -> client.bulkAsync(request, COMPAT_ES_OPTIONS, listener),
                 bulkListener);
     }
 
