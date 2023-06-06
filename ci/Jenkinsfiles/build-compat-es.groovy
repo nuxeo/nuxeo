8d8dbb1bf6961253125a7fee9261826e24dcd7b1
Kevin Leturc <kevin.leturc@hyland.com>
2023-06-06 12:59:21 +0200

    NXP-31915: Validate ES version parameter

--- ci/Jenkinsfiles/build-compat-es.groovy
+++ ci/Jenkinsfiles/build-compat-es.groovy
@@ -20,6 +20,12 @@ boolean isNuxeoTag() {
   return NUXEO_BRANCH =~ /^v.*$/
 }
 
+def checkParameters() {
+  if (!ELASTICSEARCH_IMAGE_TAG =~ /^v\d+\.\d+\.\d+/) {
+    error('ELASTICSEARCH_IMAGE_TAG parameter must be a semver version such as 7.17.9')
+  }
+}
+
 pipeline {
   agent {
     label 'jenkins-nuxeo-platform-lts-2023'
@@ -57,6 +63,7 @@ pipeline {
     stage('Initialization') {
       steps {
         script {
+          checkParameters()
           currentBuild.description = "${NUXEO_BRANCH}/${ELASTICSEARCH_MAJOR_DOT_MINOR_VERSION}"
         }
       }
