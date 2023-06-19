53225f743cedaed9bfab857472380c3c66593e87
Nour AL KOTOB <nour.alkotob@hyland.com>
2023-06-19 17:39:33 +0200

    NXP-31940: bump shared library version to v0.0.21

--- ci/Jenkinsfiles/build.groovy
+++ ci/Jenkinsfiles/build.groovy
@@ -17,7 +17,7 @@
  *     Antoine Taillefer <ataillefer@nuxeo.com>
  *     Thomas Roger <troger@nuxeo.com>
  */
-library identifier: "platform-ci-shared-library@v0.0.17"
+library identifier: "platform-ci-shared-library@v0.0.21"
 
 dockerNamespace = 'nuxeo'
 repositoryUrl = 'https://github.com/nuxeo/nuxeo-lts'
