163b7fe20de9d658d0ec6f466f7356f9f322ea12
Benoit Delbosc <bdelbosc@nuxeo.com>
2023-06-19 10:12:38 +0200

    NXP-31905: Ignore elastic refresh timeout

--- modules/platform/nuxeo-elasticsearch/nuxeo-elasticsearch-core/src/main/java/org/nuxeo/elasticsearch/client/ESRestClient.java
+++ modules/platform/nuxeo-elasticsearch/nuxeo-elasticsearch-core/src/main/java/org/nuxeo/elasticsearch/client/ESRestClient.java
@@ -59,6 +59,7 @@ import org.elasticsearch.cluster.health.ClusterHealthStatus;
 import org.elasticsearch.common.xcontent.XContentHelper;
 import org.elasticsearch.common.xcontent.XContentType;
 import org.elasticsearch.rest.RestStatus;
+import org.nuxeo.common.utils.ExceptionUtils;
 import org.nuxeo.ecm.core.api.ConcurrentUpdateException;
 import org.nuxeo.ecm.core.api.NuxeoException;
 import org.nuxeo.elasticsearch.api.ESClient;
@@ -144,7 +145,17 @@ public class ESRestClient implements ESClient {
 
     @Override
     public void refresh(String indexName) {
-        performRequestWithTracing(new Request("POST", "/" + indexName + "/_refresh"));
+        try {
+            performRequestWithTracing(new Request("POST", "/" + indexName + "/_refresh"));
+        } catch (NuxeoException e) {
+            Throwable cause = ExceptionUtils.getRootCause(e);
+            if (cause != null && cause instanceof SocketTimeoutException) {
+                // We don't want to throw failure on refresh timeout because previous indexing commands are processed
+                log.warn("Ignoring refresh timeouts: " + e.getMessage());
+            } else {
+                throw e;
+            }
+        }
     }
 
     @Override
