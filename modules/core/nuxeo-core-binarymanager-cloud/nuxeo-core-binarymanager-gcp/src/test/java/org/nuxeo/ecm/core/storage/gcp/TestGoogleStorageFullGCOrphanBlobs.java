eccf0872e6b41a8acb90bf7eac387f392d8dac10
Guillaume Renard <guillaume.renard@hyland.com>
2023-12-05 18:18:34 +0100

    NXP-31991: Test Google Storage Full GC

---
+++ modules/core/nuxeo-core-binarymanager-cloud/nuxeo-core-binarymanager-gcp/src/test/java/org/nuxeo/ecm/core/storage/gcp/TestGoogleStorageFullGCOrphanBlobs.java
@@ -0,0 +1,42 @@
+/*
+ * (C) Copyright 2023 Nuxeo (http://nuxeo.com/) and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Contributors:
+ *     Guillaume Renard
+ */
+package org.nuxeo.ecm.core.storage.gcp;
+
+import org.junit.Test;
+import org.nuxeo.ecm.core.bulk.AbstractTestFullGCOrphanBlobs;
+import org.nuxeo.ecm.core.test.CoreFeature;
+import org.nuxeo.runtime.test.runner.Features;
+
+/**
+ * @since 2023.5
+ */
+@Features({ CoreFeature.class, GoogleStorageBlobProviderFeature.class })
+public class TestGoogleStorageFullGCOrphanBlobs extends AbstractTestFullGCOrphanBlobs {
+
+    @Override
+    public int getNbFiles() {
+        return 2;
+    }
+
+    @Test
+    public void testGCBlobsAction() {
+        testGCBlobsAction(false);
+    }
+
+}
