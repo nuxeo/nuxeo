33bfe39e94d469e0f40d57edd723093deaa28b3c
Parnell Polliotti <parnell@mediawind.be>
2025-02-10 13:58:37 +0100

    fix: fixed s3 package link in init.sh

--- docker/nuxeo/init.sh
+++ docker/nuxeo/init.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 /docker-entrypoint.sh
 
-nuxeoctl mp-install --offline --relax=true --accept=true /home/nuxeo/nuxeo-web-ui-3.1.15.zip /home/nuxeo/nuxeo-mysql-package-2023.25.10.zip /home/nuxeo/amazon-s3-online-storage-2023.22.13.zip
+nuxeoctl mp-install --offline --relax=true --accept=true /home/nuxeo/nuxeo-web-ui-3.1.15.zip /home/nuxeo/nuxeo-mysql-package-2023.25.10.zip /home/nuxeo/amazon-s3-online-storage-2023.25.10.zip
