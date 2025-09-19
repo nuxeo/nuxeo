8f874af5d61d1998f408160b1c2263e3eafb5741
Guillaume Renard <guillaume.renard@hyland.com>
2025-09-19 12:00:15 +0200

    NXP-33294: Format

--- modules/platform/nuxeo-platform-imaging/nuxeo-platform-imaging-core/src/test/java/org/nuxeo/ecm/platform/picture/core/test/TestPictureConversions.java
+++ modules/platform/nuxeo-platform-imaging/nuxeo-platform-imaging-core/src/test/java/org/nuxeo/ecm/platform/picture/core/test/TestPictureConversions.java
@@ -104,20 +104,20 @@ public class TestPictureConversions {
     public void iCanMergePictureConversions() {
         for (PictureConversion pictureConversion : imagingService.getPictureConversions()) {
             switch (pictureConversion.getId()) {
-            case "Original":
-            case "OriginalJpeg":
-                assertFalse(pictureConversion.getId(), pictureConversion.isEnabled());
-                break;
-            case "Small":
-                assertEquals(50, (int) pictureConversion.getMaxSize());
-                assertTrue(pictureConversion.getDescription().contains("override"));
-                break;
-            case "Thumbnail":
-                assertEquals(320, (int) pictureConversion.getMaxSize());
-                break;
-            case "Medium":
-                assertTrue(pictureConversion.getDescription().contains("override"));
-                break;
+                case "Original":
+                case "OriginalJpeg":
+                    assertFalse(pictureConversion.getId(), pictureConversion.isEnabled());
+                    break;
+                case "Small":
+                    assertEquals(50, (int) pictureConversion.getMaxSize());
+                    assertTrue(pictureConversion.getDescription().contains("override"));
+                    break;
+                case "Thumbnail":
+                    assertEquals(320, (int) pictureConversion.getMaxSize());
+                    break;
+                case "Medium":
+                    assertTrue(pictureConversion.getDescription().contains("override"));
+                    break;
             }
         }
     }
