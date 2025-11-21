docbyte-com/nuxeo-core

NXP-33302: Store objects in S3 with INTELLIGENT_TIERING default storage class

modules/core/nuxeo-core-binarymanager-cloud/nuxeo-core-binarymanager-s3/src/main/java/org/nuxeo/ecm/blob/s3/S3BlobStore.java
@@ -20,11 +20,13 @@ package org.nuxeo.ecm.blob.s3;
 
 import static org.apache.commons.lang3.StringUtils.isNotBlank;
 import static org.nuxeo.ecm.blob.s3.S3BlobStoreConfiguration.DELIMITER;
+import static org.nuxeo.ecm.blob.s3.S3BlobStoreConfiguration.SUPPORTED_STORAGE_CLASS;
 import static org.nuxeo.ecm.blob.s3.S3Utils.sanitizeETag;
 import static org.nuxeo.ecm.core.blob.BlobProviderDescriptor.ALLOW_BYTE_RANGE;
 import static org.nuxeo.ecm.core.blob.KeyStrategy.VER_SEP;
 import static software.amazon.awssdk.services.s3.model.ObjectLockLegalHoldStatus.OFF;
 import static software.amazon.awssdk.services.s3.model.ObjectLockLegalHoldStatus.ON;
+import static software.amazon.awssdk.services.s3.model.StorageClass.STANDARD;
 
 import java.io.File;
 import java.io.IOException;
@@ -37,6 +39,7 @@ import java.util.Calendar;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Objects;
 import java.util.concurrent.CompletionException;
 
 import org.apache.commons.io.output.NullOutputStream;
@@ -290,6 +293,7 @@ public class S3BlobStore extends AbstractBlobStore {
                 }
             }
             setMetadata(b, blobContext);
+            b.storageClass(config.storageClass);
         }).addTransferListener(LoggingTransferListener.create()).source(file);
         logTrace(fileTraceSource, "->", null, "write " + Files.size(file) + " bytes");
         logTrace("hnote right: " + bucketKey);
@@ -359,8 +363,10 @@ public class S3BlobStore extends AbstractBlobStore {
                                                                .key(bucketKey)
                                                                .build();
         try {
-            String storageClass = amazonS3.headObject(headObjectRequest).storageClassAsString();
-            return storageClass == null; // null is the standard storage class for s3
+            var response = amazonS3.headObject(headObjectRequest);
+            // storage class is null for STANDARD
+            var storageClass = Objects.requireNonNullElse(response.storageClass(), STANDARD);
+            return SUPPORTED_STORAGE_CLASS.contains(storageClass);
         } catch (SdkException e) {
             if (isMissingKey(e)) {
                 return false;
@@ -631,7 +637,8 @@ public class S3BlobStore extends AbstractBlobStore {
              .sourceKey(srcs3Key.bucketKey())
              .sourceVersionId(srcs3Key.versionId())
              .destinationBucket(destinationConfig.bucketName)
-             .destinationKey(destinationKey);
+             .destinationKey(destinationKey)
+             .storageClass(config.storageClass);
             if (destinationConfig.useServerSideEncryption) {
                 // server-side encryption
                 if (isNotBlank(destinationConfig.serverSideKMSKeyID)) {
@@ -738,7 +745,7 @@ public class S3BlobStore extends AbstractBlobStore {
             }
             if (blobUpdateContext.coldStorageClass != null) {
                 StorageClass storageClass = blobUpdateContext.coldStorageClass.inColdStorage ? StorageClass.GLACIER
-                        : StorageClass.STANDARD;
+                        : config.storageClass;
                 logTrace("->", "updateStorageClass");
                 logTrace("hnote right: " + s3Key);
                 logTrace("rnote right: " + storageClass);

modules/core/nuxeo-core-binarymanager-cloud/nuxeo-core-binarymanager-s3/src/main/java/org/nuxeo/ecm/blob/s3/S3BlobStoreConfiguration.java
@@ -41,6 +41,7 @@ import java.security.PrivateKey;
 import java.security.PublicKey;
 import java.security.cert.Certificate;
 import java.time.Duration;
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.concurrent.LinkedBlockingQueue;
@@ -78,6 +79,7 @@ import software.amazon.awssdk.services.s3.model.ObjectLockEnabled;
 import software.amazon.awssdk.services.s3.model.ObjectLockRetentionMode;
 import software.amazon.awssdk.services.s3.model.ObjectLockRule;
 import software.amazon.awssdk.services.s3.model.S3Exception;
+import software.amazon.awssdk.services.s3.model.StorageClass;
 import software.amazon.awssdk.transfer.s3.S3TransferManager;
 import software.amazon.awssdk.utils.ThreadFactoryBuilder;
 import software.amazon.encryption.s3.S3AsyncEncryptionClient;
@@ -282,6 +284,13 @@ public class S3BlobStoreConfiguration extends CloudBlobStoreConfiguration {
 
     public static final ObjectLockRetentionMode DEFAULT_RETENTION_MODE = ObjectLockRetentionMode.GOVERNANCE;
 
+    /** @since 2025.8 */
+    public static final String STORAGE_CLASS_PROPERTY = "storageClass";
+
+    /** @since 2025.8 */
+    public static final List<StorageClass> SUPPORTED_STORAGE_CLASS = List.of(StorageClass.STANDARD,
+            StorageClass.INTELLIGENT_TIERING);
+
     public final CloudFrontConfiguration cloudFront;
 
     public S3Client amazonS3;
@@ -310,6 +319,13 @@ public class S3BlobStoreConfiguration extends CloudBlobStoreConfiguration {
      */
     @Deprecated(since = "2025.8", forRemoval = true)
     public final boolean s3RetentionEnabled;
+
+    /**
+     * The default storage class in s3.
+     *
+     * @since 2025.8
+     */
+    public final StorageClass storageClass;
 
     /**
      * The retention mode to use when setting the retention on an object.
@@ -426,6 +442,16 @@ public class S3BlobStoreConfiguration extends CloudBlobStoreConfiguration {
         // For compat
         s3RetentionEnabled = retentionEnabled;
 
+        var storageClassProperty = getProperty(STORAGE_CLASS_PROPERTY);
+        if (isNotBlank(storageClassProperty)) {
+            storageClass = StorageClass.fromValue(storageClassProperty.toUpperCase());
+            if (!SUPPORTED_STORAGE_CLASS.contains(storageClass)) {
+                throw new IllegalArgumentException("Unsupported S3 Storage Class: %s".formatted(storageClassProperty));
+            }
+        } else {
+            storageClass = StorageClass.STANDARD;
+        }
+        log.info("Object will be stored with S3 {} storage class", storageClass);
         transferManager = createTransferManager();
     }
 

nuxeo/nuxeo

modules/core/nuxeo-core-binarymanager-cloud/nuxeo-core-binarymanager-s3/src/main/java/org/nuxeo/ecm/blob/s3/S3BlobStore.java
@@ -20,6 +20,7 @@ package org.nuxeo.ecm.blob.s3;
 
 import static org.apache.commons.lang3.StringUtils.isNotBlank;
 import static org.nuxeo.ecm.blob.s3.S3BlobStoreConfiguration.DELIMITER;
+import static org.nuxeo.ecm.blob.s3.S3BlobStoreConfiguration.SUPPORTED_STORAGE_CLASS;
 import static org.nuxeo.ecm.core.blob.BlobProviderDescriptor.ALLOW_BYTE_RANGE;
 import static org.nuxeo.ecm.core.blob.KeyStrategy.VER_SEP;
 
@@ -34,6 +35,7 @@ import java.util.Collections;
 import java.util.Date;
 import java.util.HashSet;
 import java.util.Map;
+import java.util.Optional;
 
 import org.apache.commons.io.output.NullOutputStream;
 import org.apache.commons.lang3.mutable.MutableObject;
@@ -310,6 +312,7 @@ public class S3BlobStore extends AbstractBlobStore {
         }
         setMetadata(objectMetadata, blobContext);
         putObjectRequest.setMetadata(objectMetadata);
+        putObjectRequest.withStorageClass(config.storageClass);
         logTrace(fileTraceSource, "->", null, "write " + Files.size(file) + " bytes");
         logTrace("hnote right: " + bucketKey);
         Upload upload = config.transferManager.upload(putObjectRequest);
@@ -382,14 +385,16 @@ public class S3BlobStore extends AbstractBlobStore {
         ObjectMetadata metadata;
         try {
             metadata = config.amazonS3.getObjectMetadata(request);
+            return SUPPORTED_STORAGE_CLASS.contains(Optional.ofNullable(metadata.getStorageClass())
+                                                            .map(StorageClass::fromValue)
+                                                            // storage class is null for STANDARD
+                                                            .orElse(StorageClass.Standard));
         } catch (AmazonServiceException e) {
             if (!isMissingKey(e)) {
                 log.error("Failed to get information on blob: {}", bucketKey, e);
             }
             return false;
         }
-        String storageClass = metadata.getStorageClass();
-        return storageClass == null; // null is the standard storage class for s3
     }
 
     protected boolean bucketKeyExists(String bucketKey) {
@@ -690,6 +695,7 @@ public class S3BlobStore extends AbstractBlobStore {
             S3BlobStoreConfiguration destinationConfig, String destinationKey, boolean move) {
         CopyObjectRequest copyObjectRequest = new CopyObjectRequest(sourceConfig.bucketName, sourceKey, sourceVersionId,
                 destinationConfig.bucketName, destinationKey);
+        copyObjectRequest.withStorageClass(config.storageClass);
         if (destinationConfig.useServerSideEncryption) {
             // server-side encryption
             if (isNotBlank(destinationConfig.serverSideKMSKeyID)) {
@@ -834,7 +840,7 @@ public class S3BlobStore extends AbstractBlobStore {
             }
             if (blobUpdateContext.coldStorageClass != null) {
                 StorageClass storageClass = blobUpdateContext.coldStorageClass.inColdStorage ? StorageClass.Glacier
-                        : StorageClass.Standard;
+                        : config.storageClass;
                 CopyObjectRequest copyObjectRequest = new CopyObjectRequest(bucketName, bucketKey, bucketName,
                         bucketKey).withSourceVersionId(versionId).withStorageClass(storageClass);
                 logTrace("->", "updateStorageClass");

modules/core/nuxeo-core-binarymanager-cloud/nuxeo-core-binarymanager-s3/src/main/java/org/nuxeo/ecm/blob/s3/S3BlobStoreConfiguration.java
@@ -42,13 +42,13 @@ import java.security.PublicKey;
 import java.security.cert.Certificate;
 import java.time.Duration;
 import java.util.Date;
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.concurrent.Executors;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.nuxeo.common.Environment;
 import org.nuxeo.common.utils.ByteSize;
 import org.nuxeo.ecm.blob.CloudBlobStoreConfiguration;
 import org.nuxeo.ecm.core.api.NuxeoException;
@@ -84,6 +84,7 @@ import com.amazonaws.services.s3.model.ObjectLockConfiguration;
 import com.amazonaws.services.s3.model.ObjectLockRetentionMode;
 import com.amazonaws.services.s3.model.ObjectLockRule;
 import com.amazonaws.services.s3.model.StaticEncryptionMaterialsProvider;
+import com.amazonaws.services.s3.model.StorageClass;
 import com.amazonaws.services.s3.transfer.TransferManager;
 import com.amazonaws.services.s3.transfer.TransferManagerBuilder;
 
@@ -269,6 +270,13 @@ public class S3BlobStoreConfiguration extends CloudBlobStoreConfiguration {
 
     public static final ObjectLockRetentionMode DEFAULT_RETENTION_MODE = ObjectLockRetentionMode.GOVERNANCE;
 
+    /** @since 2025.8 */
+    public static final String STORAGE_CLASS_PROPERTY = "storageClass";
+
+    /** @since 2025.8 */
+    public static final List<StorageClass> SUPPORTED_STORAGE_CLASS = List.of(StorageClass.Standard,
+            StorageClass.IntelligentTiering);
+
     public final CloudFrontConfiguration cloudFront;
 
     public final AmazonS3 amazonS3;
@@ -295,6 +303,13 @@ public class S3BlobStoreConfiguration extends CloudBlobStoreConfiguration {
     public final boolean s3RetentionEnabled;
 
     /**
+     * The default storage class in s3.
+     *
+     * @since 2025.8
+     */
+    public final StorageClass storageClass;
+
+    /**
      * The retention mode to use when setting the retention on an object.
      */
     public final ObjectLockRetentionMode retentionMode;
@@ -312,7 +327,6 @@ public class S3BlobStoreConfiguration extends CloudBlobStoreConfiguration {
      * @since 2023.7
      */
     protected final boolean pathSeparatorIsBackslash;
-
 
     protected ByteSize minimumPartSize;
 
@@ -408,6 +422,16 @@ public class S3BlobStoreConfiguration extends CloudBlobStoreConfiguration {
             }
         }
 
+        var storageClassProperty = getProperty(STORAGE_CLASS_PROPERTY);
+        if (isNotBlank(storageClassProperty)) {
+            storageClass = StorageClass.fromValue(storageClassProperty.toUpperCase());
+            if (!SUPPORTED_STORAGE_CLASS.contains(storageClass)) {
+                throw new IllegalArgumentException("Unsupported S3 Storage Class: %s".formatted(storageClassProperty));
+            }
+        } else {
+            storageClass = StorageClass.Standard;
+        }
+        log.info("Object will be stored with S3 {} storage class", storageClass);
         transferManager = createTransferManager();
 
         abortOldUploads();