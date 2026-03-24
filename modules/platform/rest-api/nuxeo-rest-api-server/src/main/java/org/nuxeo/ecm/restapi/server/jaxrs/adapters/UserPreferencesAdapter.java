docbyte-com/nuxeo-core

NXP-33468: Add User Preferences Rest API endpoints

modules/platform/rest-api/nuxeo-rest-api-server/src/main/java/org/nuxeo/ecm/restapi/server/adapters/UserPreferencesAdapter.java
@@ -1,0 +1,95 @@
+/*
+ * (C) Copyright 2026 Nuxeo (http://nuxeo.com/) and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Contributors:
+ *     Guillaume Renard
+ */
+package org.nuxeo.ecm.restapi.server.adapters;
+
+import static jakarta.ws.rs.core.Response.Status.CREATED;
+
+import jakarta.ws.rs.DELETE;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.PUT;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.core.Response;
+
+import org.nuxeo.ecm.core.api.CoreSession;
+import org.nuxeo.ecm.core.api.DocumentModel;
+import org.nuxeo.ecm.webengine.model.WebAdapter;
+import org.nuxeo.ecm.webengine.model.impl.DefaultAdapter;
+import org.nuxeo.runtime.api.Framework;
+import org.nuxeo.user.preferences.api.UserPreference;
+import org.nuxeo.user.preferences.api.UserPreferences;
+import org.nuxeo.user.preferences.api.UserPreferencesService;
+import org.nuxeo.user.preferences.exception.UserPreferencesNotFound;
+
+/**
+ * @since 2025.16
+ */
+@WebAdapter(name = UserPreferencesAdapter.NAME, type = "userPreferencesAdapter")
+public class UserPreferencesAdapter extends DefaultAdapter {
+
+    public static final String NAME = "preferences";
+
+    protected CoreSession session;
+
+    protected UserPreferencesService ups;
+
+    @Override
+    protected void initialize(Object... args) {
+        super.initialize(args);
+        ups = Framework.getService(UserPreferencesService.class);
+        session = ctx.getCoreSession();
+    }
+
+    @DELETE
+    public Response delete() {
+        var docRef = getTarget().getAdapter(DocumentModel.class).getRef();
+        ups.delete(session, docRef);
+        return Response.noContent().build();
+    }
+
+    @GET
+    public UserPreferences get() {
+        var docRef = getTarget().getAdapter(DocumentModel.class).getRef();
+        return ups.get(session, docRef);
+    }
+
+    @GET
+    @Path("{key}")
+    public UserPreference get(@PathParam("key") String key) {
+        var docRef = getTarget().getAdapter(DocumentModel.class).getRef();
+        return ups.get(session, docRef, key).orElseThrow(() -> new UserPreferencesNotFound(key));
+    }
+
+    @PUT
+    public Response putAll(UserPreferences prefs) {
+        var docRef = getTarget().getAdapter(DocumentModel.class).getRef();
+        var response = ups.get(session, docRef).isEmpty()
+                ? Response.status(CREATED).entity(ups.create(session, docRef, prefs.preferences()))
+                : Response.ok(ups.putAll(session, docRef, prefs));
+        return response.build();
+    }
+
+    @DELETE
+    @Path("{key}")
+    public UserPreferences remove(@PathParam("key") String key) {
+        var docRef = getTarget().getAdapter(DocumentModel.class).getRef();
+        return ups.remove(session, docRef, key);
+    }
+
+}

modules/platform/rest-api/nuxeo-rest-api-server/src/main/java/org/nuxeo/ecm/restapi/server/usermanager/MeObject.java
@@ -21,6 +21,7 @@ package org.nuxeo.ecm.restapi.server.usermanager;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.PUT;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.Context;
 import jakarta.ws.rs.core.MediaType;
@@ -35,6 +36,8 @@ import org.nuxeo.ecm.platform.usermanager.UserManager;
 import org.nuxeo.ecm.webengine.model.WebObject;
 import org.nuxeo.ecm.webengine.model.impl.DefaultObject;
 import org.nuxeo.runtime.api.Framework;
+import org.nuxeo.user.preferences.api.UserPreferences;
+import org.nuxeo.user.preferences.api.UserPreferencesService;
 
 /**
  * @since 9.1
@@ -46,6 +49,20 @@ public class MeObject extends DefaultObject {
     @GET
     public NuxeoPrincipal doGet(@Context Request request) {
         return getContext().getCoreSession().getPrincipal();
+    }
+
+    @GET
+    @Path("preferences")
+    public UserPreferences preferences() {
+        return Framework.getService(UserPreferencesService.class).list(getContext().getCoreSession());
+    }
+
+    /**
+     * @since 2025.16
+     */
+    @Path("preferences/{key}")
+    public UserPreferencesObject preferences(@PathParam("key") String key) {
+        return newObject(UserPreferencesObject.class, key);
     }
 
     @PUT

modules/platform/rest-api/nuxeo-rest-api-server/src/main/java/org/nuxeo/ecm/restapi/server/usermanager/UserPreferencesObject.java
@@ -1,0 +1,80 @@
+/*
+ * (C) Copyright 2026 Nuxeo (http://nuxeo.com/) and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Contributors:
+ *     Guillaume Renard
+ */
+package org.nuxeo.ecm.restapi.server.usermanager;
+
+import static jakarta.ws.rs.core.MediaType.APPLICATION_JSON;
+import static jakarta.ws.rs.core.Response.Status.CREATED;
+
+import jakarta.ws.rs.DELETE;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.PUT;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.Response;
+
+import org.nuxeo.ecm.core.api.CoreSession;
+import org.nuxeo.ecm.webengine.model.WebObject;
+import org.nuxeo.ecm.webengine.model.impl.DefaultObject;
+import org.nuxeo.runtime.api.Framework;
+import org.nuxeo.user.preferences.api.UserPreference;
+import org.nuxeo.user.preferences.api.UserPreferencesService;
+import org.nuxeo.user.preferences.exception.UserPreferencesNotFound;
+
+/**
+ * @since 2025.16
+ */
+@WebObject(type = "preferences")
+@Produces(APPLICATION_JSON)
+public class UserPreferencesObject extends DefaultObject {
+
+    protected String key;
+
+    protected CoreSession session;
+
+    protected UserPreferencesService ups;
+
+    @Override
+    protected void initialize(Object... args) {
+        if (args.length < 1) {
+            throw new IllegalArgumentException("UserPreferencesObject takes preference key as parameter");
+        }
+        key = (String) args[0];
+        ups = Framework.getService(UserPreferencesService.class);
+        session = ctx.getCoreSession();
+    }
+
+    @DELETE
+    public Response delete() {
+        ups.delete(session, key);
+        return Response.noContent().build();
+    }
+
+    @GET
+    public UserPreference get() {
+        return ups.get(session, key).orElseThrow(() -> new UserPreferencesNotFound(key));
+    }
+
+    @PUT
+    public Response createOrUpdate(String value) {
+        return ups.get(session, key)
+                  .map(pref -> Response.ok(ups.update(session, key, value)))
+                  .orElseGet(() -> Response.status(CREATED).entity(ups.create(session, key, value)))
+                  .build();
+    }
+
+}

modules/platform/rest-api/nuxeo-rest-api-test/src/test/java/org/nuxeo/ecm/restapi/test/UserPreferencesObjectTest.java
@@ -1,0 +1,114 @@
+/*
+ * (C) Copyright 2026 Nuxeo (http://nuxeo.com/) and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Contributors:
+ *     Guillaume Renard
+ */
+package org.nuxeo.ecm.restapi.test;
+
+import static jakarta.servlet.http.HttpServletResponse.SC_OK;
+import static org.apache.http.HttpStatus.SC_CREATED;
+import static org.apache.http.HttpStatus.SC_NOT_FOUND;
+import static org.apache.http.HttpStatus.SC_NO_CONTENT;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import jakarta.inject.Inject;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.nuxeo.ecm.core.api.CoreSession;
+import org.nuxeo.ecm.core.test.annotations.Granularity;
+import org.nuxeo.ecm.core.test.annotations.RepositoryConfig;
+import org.nuxeo.http.test.HttpClientTestRule;
+import org.nuxeo.http.test.handler.HttpStatusCodeHandler;
+import org.nuxeo.http.test.handler.JsonNodeHandler;
+import org.nuxeo.runtime.test.runner.Features;
+import org.nuxeo.runtime.test.runner.FeaturesRunner;
+import org.nuxeo.user.preferences.directory.UserPreferencesFeature;
+
+/**
+ * @since 2025.16
+ */
+@RunWith(FeaturesRunner.class)
+@Features({ UserPreferencesFeature.class, RestServerFeature.class })
+@RepositoryConfig(init = RestServerInit.class, cleanup = Granularity.METHOD)
+public class UserPreferencesObjectTest {
+
+    @Inject
+    protected CoreSession session;
+
+    @Inject
+    protected RestServerFeature restServerFeature;
+
+    @Rule
+    public final HttpClientTestRule httpClient = HttpClientTestRule.defaultJsonClient(
+            () -> restServerFeature.getRestApiUrl());
+
+    @Test
+    public void testUserPreferenceCRUD() {
+        // Create
+        httpClient.buildPutRequest("/me/preferences/foo")
+                  .entity("bar")
+                  .executeAndConsume(new JsonNodeHandler(SC_CREATED),
+                          node -> assertEquals("bar", node.get("value").asText()));
+        // Create other
+        httpClient.buildPutRequest("/me/preferences/otherKey")
+                  .entity("otherValue")
+                  .executeAndConsume(new JsonNodeHandler(SC_CREATED),
+                          node -> assertEquals("otherValue", node.get("value").asText()));
+
+        // Get one
+        httpClient.buildGetRequest("/me/preferences/foo") //
+                  .executeAndConsume(new JsonNodeHandler(SC_OK),
+                          node -> assertEquals("bar", node.get("value").asText()));
+
+        // Get all
+        httpClient.buildGetRequest("/me/preferences") //
+                  .executeAndConsume(new JsonNodeHandler(SC_OK), node -> {
+                      assertTrue(node.has("preferences"));
+                      assertEquals(2, node.get("preferences").size());
+                  });
+
+        // Update
+        httpClient.buildPutRequest("/me/preferences/foo")
+                  .entity("otherValue")
+                  .executeAndConsume(new JsonNodeHandler(SC_OK),
+                          node -> assertEquals("otherValue", node.get("value").asText()));
+
+        // Delete
+        httpClient.buildDeleteRequest("/me/preferences/foo")
+                  .executeAndConsume(new HttpStatusCodeHandler(),
+                          status -> assertEquals(SC_NO_CONTENT, status.intValue()));
+        // Check delete is idempotent
+        httpClient.buildDeleteRequest("/me/preferences/foo")
+                  .executeAndConsume(new HttpStatusCodeHandler(),
+                          status -> assertEquals(SC_NO_CONTENT, status.intValue()));
+
+        // Check delete is effective
+        httpClient.buildGetRequest("/me/preferences/foo")
+                  .executeAndConsume(new HttpStatusCodeHandler(),
+                          status -> assertEquals(SC_NOT_FOUND, status.intValue()));
+
+        // Get all
+        httpClient.buildGetRequest("/me/preferences") //
+                  .executeAndConsume(new JsonNodeHandler(SC_OK), node -> {
+                      assertTrue(node.has("preferences"));
+                      assertEquals(1, node.get("preferences").size());
+                  });
+    }
+
+}

nuxeo/nuxeo

modules/platform/rest-api/nuxeo-rest-api-server/src/main/java/org/nuxeo/ecm/restapi/server/jaxrs/adapters/UserPreferencesAdapter.java
@@ -1,0 +1,95 @@
+/*
+ * (C) Copyright 2026 Nuxeo (http://nuxeo.com/) and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Contributors:
+ *     Guillaume Renard
+ */
+package org.nuxeo.ecm.restapi.server.jaxrs.adapters;
+
+import static javax.ws.rs.core.Response.Status.CREATED;
+
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Response;
+
+import org.nuxeo.ecm.core.api.CoreSession;
+import org.nuxeo.ecm.core.api.DocumentModel;
+import org.nuxeo.ecm.webengine.model.WebAdapter;
+import org.nuxeo.ecm.webengine.model.impl.DefaultAdapter;
+import org.nuxeo.runtime.api.Framework;
+import org.nuxeo.user.preferences.api.UserPreference;
+import org.nuxeo.user.preferences.api.UserPreferences;
+import org.nuxeo.user.preferences.api.UserPreferencesService;
+import org.nuxeo.user.preferences.exception.UserPreferencesNotFound;
+
+/**
+ * @since 2025.16
+ */
+@WebAdapter(name = UserPreferencesAdapter.NAME, type = "userPreferencesAdapter")
+public class UserPreferencesAdapter extends DefaultAdapter {
+
+    public static final String NAME = "preferences";
+
+    protected CoreSession session;
+
+    protected UserPreferencesService ups;
+
+    @Override
+    protected void initialize(Object... args) {
+        super.initialize(args);
+        ups = Framework.getService(UserPreferencesService.class);
+        session = ctx.getCoreSession();
+    }
+
+    @DELETE
+    public Response delete() {
+        var docRef = getTarget().getAdapter(DocumentModel.class).getRef();
+        ups.delete(session, docRef);
+        return Response.noContent().build();
+    }
+
+    @GET
+    public UserPreferences get() {
+        var docRef = getTarget().getAdapter(DocumentModel.class).getRef();
+        return ups.get(session, docRef);
+    }
+
+    @GET
+    @Path("{key}")
+    public UserPreference get(@PathParam("key") String key) {
+        var docRef = getTarget().getAdapter(DocumentModel.class).getRef();
+        return ups.get(session, docRef, key).orElseThrow(() -> new UserPreferencesNotFound(key));
+    }
+
+    @PUT
+    public Response putAll(UserPreferences prefs) {
+        var docRef = getTarget().getAdapter(DocumentModel.class).getRef();
+        var response = ups.get(session, docRef).isEmpty()
+                ? Response.status(CREATED).entity(ups.create(session, docRef, prefs.preferences()))
+                : Response.ok(ups.putAll(session, docRef, prefs));
+        return response.build();
+    }
+
+    @DELETE
+    @Path("{key}")
+    public UserPreferences remove(@PathParam("key") String key) {
+        var docRef = getTarget().getAdapter(DocumentModel.class).getRef();
+        return ups.remove(session, docRef, key);
+    }
+
+}

modules/platform/rest-api/nuxeo-rest-api-server/src/main/java/org/nuxeo/ecm/restapi/server/jaxrs/usermanager/MeObject.java
@@ -21,6 +21,7 @@ package org.nuxeo.ecm.restapi.server.jaxrs.usermanager;
 import javax.ws.rs.GET;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
@@ -35,6 +36,8 @@ import org.nuxeo.ecm.platform.usermanager.UserManager;
 import org.nuxeo.ecm.webengine.model.WebObject;
 import org.nuxeo.ecm.webengine.model.impl.DefaultObject;
 import org.nuxeo.runtime.api.Framework;
+import org.nuxeo.user.preferences.api.UserPreferences;
+import org.nuxeo.user.preferences.api.UserPreferencesService;
 
 /**
  * @since 9.1
@@ -46,6 +49,20 @@ public class MeObject extends DefaultObject {
     @GET
     public NuxeoPrincipal doGet(@Context Request request) {
         return getContext().getCoreSession().getPrincipal();
+    }
+
+    @GET
+    @Path("preferences")
+    public UserPreferences preferences() {
+        return Framework.getService(UserPreferencesService.class).list(getContext().getCoreSession());
+    }
+
+    /**
+     * @since 2025.16
+     */
+    @Path("preferences/{key}")
+    public UserPreferencesObject preferences(@PathParam("key") String key) {
+        return newObject(UserPreferencesObject.class, key);
     }
 
     @PUT

modules/platform/rest-api/nuxeo-rest-api-server/src/main/java/org/nuxeo/ecm/restapi/server/jaxrs/usermanager/UserPreferencesObject.java
@@ -1,0 +1,80 @@
+/*
+ * (C) Copyright 2026 Nuxeo (http://nuxeo.com/) and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Contributors:
+ *     Guillaume Renard
+ */
+package org.nuxeo.ecm.restapi.server.jaxrs.usermanager;
+
+import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
+import static javax.ws.rs.core.Response.Status.CREATED;
+
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
+
+import org.nuxeo.ecm.core.api.CoreSession;
+import org.nuxeo.ecm.webengine.model.WebObject;
+import org.nuxeo.ecm.webengine.model.impl.DefaultObject;
+import org.nuxeo.runtime.api.Framework;
+import org.nuxeo.user.preferences.api.UserPreference;
+import org.nuxeo.user.preferences.api.UserPreferencesService;
+import org.nuxeo.user.preferences.exception.UserPreferencesNotFound;
+
+/**
+ * @since 2025.16
+ */
+@WebObject(type = "preferences")
+@Produces(APPLICATION_JSON)
+public class UserPreferencesObject extends DefaultObject {
+
+    protected String key;
+
+    protected CoreSession session;
+
+    protected UserPreferencesService ups;
+
+    @Override
+    protected void initialize(Object... args) {
+        if (args.length < 1) {
+            throw new IllegalArgumentException("UserPreferencesObject takes preference key as parameter");
+        }
+        key = (String) args[0];
+        ups = Framework.getService(UserPreferencesService.class);
+        session = ctx.getCoreSession();
+    }
+
+    @DELETE
+    public Response delete() {
+        ups.delete(session, key);
+        return Response.noContent().build();
+    }
+
+    @GET
+    public UserPreference get() {
+        return ups.get(session, key).orElseThrow(() -> new UserPreferencesNotFound(key));
+    }
+
+    @PUT
+    public Response createOrUpdate(String value) {
+        return ups.get(session, key)
+                  .map(pref -> Response.ok(ups.update(session, key, value)))
+                  .orElseGet(() -> Response.status(CREATED).entity(ups.create(session, key, value)))
+                  .build();
+    }
+
+}

modules/platform/rest-api/nuxeo-rest-api-test/src/test/java/org/nuxeo/ecm/restapi/test/UserPreferencesObjectTest.java
@@ -1,0 +1,114 @@
+/*
+ * (C) Copyright 2026 Nuxeo (http://nuxeo.com/) and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Contributors:
+ *     Guillaume Renard
+ */
+package org.nuxeo.ecm.restapi.test;
+
+import static org.apache.http.HttpStatus.SC_CREATED;
+import static org.apache.http.HttpStatus.SC_NOT_FOUND;
+import static org.apache.http.HttpStatus.SC_NO_CONTENT;
+import static org.apache.http.HttpStatus.SC_OK;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import javax.inject.Inject;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.nuxeo.ecm.core.api.CoreSession;
+import org.nuxeo.ecm.core.test.annotations.Granularity;
+import org.nuxeo.ecm.core.test.annotations.RepositoryConfig;
+import org.nuxeo.http.test.HttpClientTestRule;
+import org.nuxeo.http.test.handler.HttpStatusCodeHandler;
+import org.nuxeo.http.test.handler.JsonNodeHandler;
+import org.nuxeo.runtime.test.runner.Features;
+import org.nuxeo.runtime.test.runner.FeaturesRunner;
+import org.nuxeo.user.preferences.directory.UserPreferencesFeature;
+
+/**
+ * @since 2025.16
+ */
+@RunWith(FeaturesRunner.class)
+@Features({ UserPreferencesFeature.class, RestServerFeature.class })
+@RepositoryConfig(init = RestServerInit.class, cleanup = Granularity.METHOD)
+public class UserPreferencesObjectTest {
+
+    @Inject
+    protected CoreSession session;
+
+    @Inject
+    protected RestServerFeature restServerFeature;
+
+    @Rule
+    public final HttpClientTestRule httpClient = HttpClientTestRule.defaultJsonClient(
+            () -> restServerFeature.getRestApiUrl());
+
+    @Test
+    public void testUserPreferenceCRUD() {
+        // Create
+        httpClient.buildPutRequest("/me/preferences/foo")
+                  .entity("bar")
+                  .executeAndConsume(new JsonNodeHandler(SC_CREATED),
+                          node -> assertEquals("bar", node.get("value").asText()));
+        // Create other
+        httpClient.buildPutRequest("/me/preferences/otherKey")
+                  .entity("otherValue")
+                  .executeAndConsume(new JsonNodeHandler(SC_CREATED),
+                          node -> assertEquals("otherValue", node.get("value").asText()));
+
+        // Get one
+        httpClient.buildGetRequest("/me/preferences/foo") //
+                  .executeAndConsume(new JsonNodeHandler(SC_OK),
+                          node -> assertEquals("bar", node.get("value").asText()));
+
+        // Get all
+        httpClient.buildGetRequest("/me/preferences") //
+                  .executeAndConsume(new JsonNodeHandler(SC_OK), node -> {
+                      assertTrue(node.has("preferences"));
+                      assertEquals(2, node.get("preferences").size());
+                  });
+
+        // Update
+        httpClient.buildPutRequest("/me/preferences/foo")
+                  .entity("otherValue")
+                  .executeAndConsume(new JsonNodeHandler(SC_OK),
+                          node -> assertEquals("otherValue", node.get("value").asText()));
+
+        // Delete
+        httpClient.buildDeleteRequest("/me/preferences/foo")
+                  .executeAndConsume(new HttpStatusCodeHandler(),
+                          status -> assertEquals(SC_NO_CONTENT, status.intValue()));
+        // Check delete is idempotent
+        httpClient.buildDeleteRequest("/me/preferences/foo")
+                  .executeAndConsume(new HttpStatusCodeHandler(),
+                          status -> assertEquals(SC_NO_CONTENT, status.intValue()));
+
+        // Check delete is effective
+        httpClient.buildGetRequest("/me/preferences/foo")
+                  .executeAndConsume(new HttpStatusCodeHandler(),
+                          status -> assertEquals(SC_NOT_FOUND, status.intValue()));
+
+        // Get all
+        httpClient.buildGetRequest("/me/preferences") //
+                  .executeAndConsume(new JsonNodeHandler(SC_OK), node -> {
+                      assertTrue(node.has("preferences"));
+                      assertEquals(1, node.get("preferences").size());
+                  });
+    }
+
+}