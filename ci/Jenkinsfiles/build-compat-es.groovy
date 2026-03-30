2736aaae18103402b29721b9b0e0a03575854c55
Antoine Taillefer <ataillefer@nuxeo.com>
2026-03-30 10:32:15 +0200

    NXBT-3951: Fix release promotion job - nxGit.commitTagPush
    Bump shared library to 0.0.82.

--- ci/Jenkinsfiles/build-compat-es.groovy
+++ ci/Jenkinsfiles/build-compat-es.groovy
@@ -14,7 +14,7 @@
  * limitations under the License.
  *
  */
-library identifier: "platform-ci-shared-library@v0.0.80"
+library identifier: "platform-ci-shared-library@v0.0.82"
 
 boolean isNuxeoTag() {
   return NUXEO_BRANCH =~ /^v.*$/
